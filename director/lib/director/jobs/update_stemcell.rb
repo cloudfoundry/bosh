b5c1ff8ca1e79d866e71b6cc0d00f5b5dc44df51
Vadim Spivak <vspivak@vmware.com>
2010-09-14 16:09:47 -0700

    missed change from last commit

--- director/lib/director/jobs/update_stemcell.rb
+++ director/lib/director/jobs/update_stemcell.rb
@@ -16,14 +16,6 @@ module Bosh::Director
         raise TaskInvalid if @task.nil?
 
         @stemcell_dir = stemcell_dir
-        stemcell_manifest_file = File.join(@stemcell_dir, "stemcell.MF")
-        stemcell_manifest = YAML.load_file(stemcell_manifest_file)
-
-        @name = safe_property(stemcell_manifest, "name", :class => String)
-        @version = safe_property(stemcell_manifest, "version", :class => Integer)
-        @cloud_properties = safe_property(stemcell_manifest, "cloud_properties", :class => Hash, :optional => true)
-        @stemcell_image = File.join(@stemcell_dir, "image")
-
         @cloud = Config.cloud
       end
 
@@ -33,12 +25,26 @@ module Bosh::Director
         @task.save!
 
         begin
+          stemcell_manifest_file = File.join(@stemcell_dir, "stemcell.MF")
+          stemcell_manifest = YAML.load_file(stemcell_manifest_file)
+
+          @name = safe_property(stemcell_manifest, "name", :class => String)
+          @version = safe_property(stemcell_manifest, "version", :class => Integer)
+          @cloud_properties = safe_property(stemcell_manifest, "cloud_properties", :class => Hash, :optional => true)
+          @stemcell_image = File.join(@stemcell_dir, "image")
+
+          raise "Invalid image" unless File.file?(@stemcell_image)
+
           cid = @cloud.create_stemcell(@stemcell_image, @cloud_properties)
           stemcell = Models::Stemcell.new
           stemcell.name = @name
           stemcell.version = @version
           stemcell.cid = cid
           stemcell.save!
+
+          @task.state = :done
+          @task.timestamp = Time.now.to_i
+          @task.save!
         rescue Exception => e
           @task.state = :error
           @task.result = e.to_s
