a69591dd415aae755cef7c4c2518e65f64557a7e
skaar <skaar@waste.org>
2010-10-10 19:55:48 -0400

    basic bosh agent skeleton

---
+++ agent/lib/agent/message/configure.rb
@@ -0,0 +1,10 @@
+module Bosh::Agent
+  module Message
+    class Configure
+      def self.process(args)
+        # TODO: skeleton for Configure message - which in this version will be
+        # called upon every start of the agent.
+      end
+    end
+  end
+end
