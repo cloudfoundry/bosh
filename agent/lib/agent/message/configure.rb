1c3a107b90864344a6c2c01e1e27dafb3b5d6c54
skaar <skaar@waste.org>
2010-10-13 19:21:09 -0400

    add initial support for agent network configure through OVF properties

--- agent/lib/agent/message/configure.rb
+++ agent/lib/agent/message/configure.rb
@@ -1,10 +1,149 @@
+require 'rexml/document'
+require 'netaddr'
+require 'erb'
+require 'tempfile'
+require 'fileutils'
+
 module Bosh::Agent
   module Message
     class Configure
       def self.process(args)
-        # TODO: skeleton for Configure message - which in this version will be
-        # called upon every start of the agent.
+        self.new(args).configure
       end
+
+      # TODO: set up iptables
+      def initialize(args)
+      end
+
+      def configure
+        load_ovf
+        update_agent_id
+        setup_networking
+      end
+
+      def load_ovf
+        ovf_env = info_get_ovfenv
+        doc = REXML::Document.new(ovf_env)
+        xpath = '//oe:Environment/oe:PropertySection/oe:Property[@key="Bosh_Agent_Properties"]'
+        element = REXML::XPath.first(doc, xpath,
+                                        {'oe' => 'http://schemas.dmtf.org/ovf/environment/1'})
+        json_props = element.attribute('value', 'http://schemas.dmtf.org/ovf/environment/1').value
+        @settings = Yajl::Parser.new.parse(json_props)
+      end
+
+      def info_get_ovfenv
+        `vmtoolsd --cmd "info-get guestinfo.ovfEnv"`
+      end
+
+      def update_agent_id
+        Bosh::Agent::Config.agent_id = @settings["agent_id"]
+      end
+
+      # TODO: factor out into it's own class
+      def detect_mac_addresses
+        mac_addresses = {}
+        Dir['/sys/class/net/*'].each do |dev_path|
+          dev = File.basename(dev_path)
+          mac = File.read(File.join(dev_path, 'address')).strip
+          mac_addresses[mac] = dev
+        end
+        mac_addresses
+      end
+
+      def setup_networking
+        mac_addresses = detect_mac_addresses
+
+        # last to update wins for now
+        @dns = []
+
+        @networks = @settings["networks"]
+        @networks.each do |k, v|
+          mac = v["mac"]
+          if mac_addresses.key?(mac)
+            v["interface"] = mac_addresses[mac]
+
+            net_cidr = NetAddr::CIDR.create("#{v['ip']} 255.255.255.0")
+            v["network"] = net_cidr.network
+            v["broadcast"] = net_cidr.broadcast
+
+            @dns = v["dns"]
+          else
+            raise Bosh::Agent::MessageHandlerError, "#{mac} from OVF not present in instance"
+          end
+        end
+
+        verify_networks
+        write_ubuntu_network_interfaces
+        write_resolv_conf
+      end
+
+      def verify_networks
+        # This only verifies that the fields has values
+        @networks.each do |k, v|
+          %w{ip network netmask broadcast gw}.each do |field|
+            unless v[field] 
+              raise Bosh::Agent::MessageHandlerError, "Missing network value for #{field} in #{v.inspect}"
+            end
+          end
+        end
+      end
+
+      def write_ubuntu_network_interfaces
+        template = ERB.new(INTERFACE_TEMPLATE, 0, '%<>')
+        result = template.result(binding)
+        update_file(result, '/etc/network/interfaces')
+      end
+
+      # TODO: do we need search option?
+      def write_resolv_conf
+        template = ERB.new("<% @dns.each do |server| %>\nnameserver <%= server %>\n<% end %>\n", 0, '%<>')
+        result = template.result(binding)
+        update_file(result, '/etc/resolv.conf')
+      end
+
+      # Poor mans idempotency
+      # FIXME: fails if original file is missing
+      def update_file(data, path)
+        name = File.basename(path)
+        dir = File.dirname(path)
+
+        if_tmp_file = Tempfile.new(name, dir)
+        if_tmp_file.write(data)
+        if_tmp_file.flush
+
+        old = Digest::SHA1.hexdigest(File.read(path))
+        new = Digest::SHA1.hexdigest(File.read(if_tmp_file.path))
+
+        updated = false
+        unless old == new
+          FileUtils.cp(if_tmp_file.path, path)
+          updated = true
+        end
+        if_tmp_file.close
+        updated
+      end
+
+      def print_settings
+        p @settings
+        # For OVF test fixtures
+        p Yajl::Encoder.encode(@settings).gsub(/\"/, "&quot;")
+      end
+
+      INTERFACE_TEMPLATE = <<TEMPLATE
+auto lo
+iface lo inet loopback
+
+<% @networks.each do |name, n| %>
+iface <%= n["interface"] %> inet static
+    address <%= n["ip"]%>
+    network <%= n["network"] %>
+    netmask <%= n["netmask"]%>
+    broadcast <%= n["broadcast"] %>
+    gateway <%= n["gw"] %>
+<% end %>
+
+TEMPLATE
+
     end
   end
 end
---
+++ agent/spec/unit/message/configure_spec.rb
@@ -0,0 +1,110 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Bosh::Agent::Message::Configure do
+
+  before(:each) do
+    @processor = Bosh::Agent::Message::Configure.new(nil)
+    @processor.stub!(:info_get_ovfenv).and_return(ovf_xml)
+
+    # We just want to avoid this to accidently be invoked on dev systems
+    @processor.stub(:update_file)
+  end
+
+  it 'should read ovf xml' do
+    @processor.load_ovf.keys.should include("networks")
+    @processor.load_ovf.keys.should include("agent_id")
+  end
+
+  it 'should setup networking' do
+    @processor.stub!(:detect_mac_addresses).and_return({"00:50:56:89:17:70" => "eth0"})
+    @processor.load_ovf
+    @processor.setup_networking
+  end
+
+  it "should fail when ovf mac doesn't match one on the system" do
+    @processor.stub!(:detect_mac_addresses).and_return({"00:50:56:89:17:71" => "eth0"})
+    @processor.load_ovf
+    lambda { @processor.setup_networking }.should raise_error(Bosh::Agent::MessageHandlerError, /from OVF not present in instance/)
+
+  end
+
+  it "should fail when network information is incomplete" do
+    @processor.stub!(:info_get_ovfenv).and_return(incomplete_ovf_xml)
+    @processor.stub!(:detect_mac_addresses).and_return({"00:50:56:89:17:70" => "eth0"})
+    @processor.load_ovf
+    lambda { @processor.setup_networking }.should raise_error(Bosh::Agent::MessageHandlerError, /Missing network value for netmask in/)
+  end
+
+  it "should generate ubuntu network files" do
+    @processor.stub!(:detect_mac_addresses).and_return({"00:50:56:89:17:70" => "eth0"})
+    @processor.stub!(:update_file) do |data, file|
+      # FIMXE: clean this mess up 
+      case file
+      when '/etc/network/interfaces'
+        data.should == "auto lo\niface lo inet loopback\n\niface eth0 inet static\n    address 11.2.3.4\n    network 11.2.3.0\n    netmask 255.255.255.0\n    broadcast 11.2.3.255\n    gateway 11.2.3.1\n\n"
+      when '/etc/resolv.conf'
+        data.should == "nameserver 11.1.2.5\nnameserver 11.1.2.6\n"
+      end
+    end
+
+    @processor.load_ovf
+    @processor.setup_networking
+  end
+
+end
+
+def ovf_xml
+  %q{
+    <?xml version="1.0" encoding="UTF-8"?>
+    <Environment
+         xmlns="http://schemas.dmtf.org/ovf/environment/1"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xmlns:oe="http://schemas.dmtf.org/ovf/environment/1"
+         xmlns:ve="http://www.vmware.com/schema/ovfenv"
+         oe:id="">
+       <PlatformSection>
+          <Kind>VMware ESXi</Kind>
+          <Version>4.1.0</Version>
+          <Vendor>VMware, Inc.</Vendor>
+          <Locale>en</Locale>
+       </PlatformSection>
+       <PropertySection>
+             <Property oe:key="Test_Property2" oe:value="{&quot;networks&quot;:{&quot;network_a&quot;:{&quot;mac&quot;:&quot;00:50:56:89:17:70&quot;,&quot;ip&quot;:&quot;1.2.3.4&quot;,&quot;cloud_properties&quot;:{&quot;name&quot;:&quot;vlan-c-172.30.38.0-21&quot;}}},&quot;agent_id&quot;:&quot;foo&quot;}"/>
+             <Property oe:key="Bosh_Agent_Properties_old" oe:value="{&quot;networks&quot;:{&quot;network_a&quot;:{&quot;netmask&quot;:&quot;255.255.255.0&quot;,&quot;broadcast&quot;:&quot;11.2.3.255&quot;,&quot;mac&quot;:&quot;00:50:56:89:17:70&quot;,&quot;network&quot;:&quot;11.2.3.0&quot;,&quot;interface&quot;:&quot;eth0&quot;,&quot;ip&quot;:&quot;11.2.3.4&quot;,&quot;gw&quot;:&quot;11.2.3.1&quot;,&quot;cloud_properties&quot;:{&quot;name&quot;:&quot;vlan-c-172.30.38.0-21&quot;}}},&quot;agent_id&quot;:&quot;foo&quot;}"/>
+             <Property oe:key="Bosh_Agent_Properties" oe:value="{&quot;networks&quot;:{&quot;network_a&quot;:{&quot;netmask&quot;:&quot;255.255.255.0&quot;,&quot;mac&quot;:&quot;00:50:56:89:17:70&quot;,&quot;gw&quot;:&quot;11.2.3.1&quot;,&quot;ip&quot;:&quot;11.2.3.4&quot;,&quot;dns&quot;:[&quot;11.1.2.5&quot;,&quot;11.1.2.6&quot;],&quot;cloud_properties&quot;:{&quot;name&quot;:&quot;vlan-c-172.30.38.0-21&quot;}}},&quot;agent_id&quot;:&quot;foo&quot;}"/>
+       </PropertySection>
+       <ve:EthernetAdapterSection>
+          <ve:Adapter ve:mac="00:50:56:87:00:1a" ve:network="VLAN436"/>
+          <ve:Adapter ve:mac="00:50:56:87:00:1b" ve:network="VLAN440"/>
+       </ve:EthernetAdapterSection>
+    </Environment>
+  }
+end
+
+def incomplete_ovf_xml
+  %q{
+    <?xml version="1.0" encoding="UTF-8"?>
+    <Environment
+         xmlns="http://schemas.dmtf.org/ovf/environment/1"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xmlns:oe="http://schemas.dmtf.org/ovf/environment/1"
+         xmlns:ve="http://www.vmware.com/schema/ovfenv"
+         oe:id="">
+       <PlatformSection>
+          <Kind>VMware ESXi</Kind>
+          <Version>4.1.0</Version>
+          <Vendor>VMware, Inc.</Vendor>
+          <Locale>en</Locale>
+       </PlatformSection>
+       <PropertySection>
+             <Property oe:key="Test_Property2" oe:value="{&quot;networks&quot;:{&quot;network_a&quot;:{&quot;mac&quot;:&quot;00:50:56:89:17:70&quot;,&quot;ip&quot;:&quot;1.2.3.4&quot;,&quot;cloud_properties&quot;:{&quot;name&quot;:&quot;vlan-c-172.30.38.0-21&quot;}}},&quot;agent_id&quot;:&quot;foo&quot;}"/>
+             <Property oe:key="Bosh_Agent_Properties_older" oe:value="{&quot;networks&quot;:{&quot;network_a&quot;:{&quot;netmask&quot;:&quot;255.255.255.0&quot;,&quot;broadcast&quot;:&quot;11.2.3.255&quot;,&quot;mac&quot;:&quot;00:50:56:89:17:70&quot;,&quot;network&quot;:&quot;11.2.3.0&quot;,&quot;interface&quot;:&quot;eth0&quot;,&quot;ip&quot;:&quot;11.2.3.4&quot;,&quot;gw&quot;:&quot;11.2.3.1&quot;,&quot;cloud_properties&quot;:{&quot;name&quot;:&quot;vlan-c-172.30.38.0-21&quot;}}},&quot;agent_id&quot;:&quot;foo&quot;}"/>
+             <Property oe:key="Bosh_Agent_Properties" oe:value="{&quot;networks&quot;:{&quot;network_a&quot;:{&quot;mac&quot;:&quot;00:50:56:89:17:70&quot;,&quot;gw&quot;:&quot;11.2.3.1&quot;,&quot;ip&quot;:&quot;11.2.3.4&quot;,&quot;dns&quot;:[&quot;11.1.2.5&quot;,&quot;11.1.2.6&quot;],&quot;cloud_properties&quot;:{&quot;name&quot;:&quot;vlan-c-172.30.38.0-21&quot;}}},&quot;agent_id&quot;:&quot;foo&quot;}"/>
+       </PropertySection>
+       <ve:EthernetAdapterSection>
+          <ve:Adapter ve:mac="00:50:56:87:00:1a" ve:network="VLAN436"/>
+          <ve:Adapter ve:mac="00:50:56:87:00:1b" ve:network="VLAN440"/>
+       </ve:EthernetAdapterSection>
+    </Environment>
+  }
+end
