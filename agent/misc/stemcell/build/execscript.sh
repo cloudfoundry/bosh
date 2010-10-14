a0af6b07edeade45f344a68f08a4d3d74c950e7c
skaar <skaar@waste.org>
2010-10-14 13:25:34 -0400

    initial/incomplete approach for automated creation of stemcell images using vmbuilder

---
+++ agent/misc/stemcell/build/execscript.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+chroot $1 /var/b29/bosh/src/prepare_instance.sh
---
+++ agent/misc/stemcell/instance/prepare_instance.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+export PATH=/var/b29/bin:$PATH
+export HOME=/root
+
+apt-get install -y --force-yes --no-install-recommends \
+  build-essential openssh-server libssl-dev linux-headers-virtual \
+  open-vm-dkms open-vm-tools monit
+
+cd /var/b29/bosh/src
+tar zxvf ruby-1.8.7-p302.tar.gz
+
+(
+  cd ruby-1.8.7-p302
+  ./configure --prefix=/var/b29 
+  make && make install
+)
+
+
+tar zxvf rubygems-1.3.7.tgz
+(
+  cd rubygems-1.3.7
+  /var/b29/bin/ruby setup.rb
+)
+
+gem install bundler
+
+mkdir -p /var/b29/bosh/agent
+cp -a lib Gemfile* /var/b29/bosh/agent/
+
+(
+  cd /var/b29/bosh/agent
+  bundle install /var/b29/share/gems
+)
