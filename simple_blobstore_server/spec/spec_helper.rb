095bca3d6cb21c866dbaf4bbb53ea8f65317bbe3
Vadim Spivak <vspivak@vmware.com>
2010-10-15 13:57:25 -0700

    simple blobstore server - upgrade to bundler 1.0 - upgrade to rspec 2.0 - create script to run it

--- simple_blobstore_server/spec/spec_helper.rb
+++ simple_blobstore_server/spec/spec_helper.rb
@@ -1,12 +1,17 @@
-$:.unshift(File.expand_path("../../lib", __FILE__))
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
+
+require "rubygems"
+Bundler.setup(:default, :test)
+
+require "rack/test"
+
+$LOAD_PATH.unshift(File.dirname(__FILE__) + "/../lib")
 
 ENV["RACK_ENV"] = "test"
 
 require "simple_blobstore_server"
 
-Bundler.require(:test)
-
-Spec::Runner.configure do |rspec_config|
+Rspec.configure do |rspec_config|
   rspec_config.before(:each) do
   end
 
