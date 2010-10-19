829ad7561c870a6edba91a8d19ec630b00fc37a1
skaar <skaar@waste.org>
2010-10-18 20:53:54 -0400

    agent installation and runit fixes

--- agent/misc/stemcell/instance/prepare_instance.sh
+++ agent/misc/stemcell/instance/prepare_instance.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export PATH=/var/b29/bin:$PATH
 export HOME=/root
@@ -12,7 +12,7 @@ tar zxvf ruby-1.8.7-p302.tar.gz
 
 (
   cd ruby-1.8.7-p302
-  ./configure --prefix=/var/b29 
+  ./configure --prefix=/var/b29
   make && make install
 )
 
@@ -24,13 +24,25 @@ tar zxvf rubygems-1.3.7.tgz
 
 gem install bundler
 
-mkdir -p /var/b29/bosh/agent
-cp -a lib Gemfile* /var/b29/bosh/agent/
+version=$(cat version)
+agent_path=/var/b29/bosh/agent_${version}_builtin
+
+mkdir -p ${agent_path}
+cp -a bin lib Gemfile* ${agent_path}
+ln -s ${agent_path} /var/b29/bosh/agent
+chmod +x /var/b29/bosh/agent/bin/agent
 
 (
   cd /var/b29/bosh/agent
-  bundle install /var/b29/share/gems
+  bundle install /var/b29/gems
 )
+
+cp -a runit/agent /etc/sv/agent
+chmod +x /etc/sv/agent/run /etc/sv/agent/log/run
+ln -s /etc/sv/agent /etc/service/agent
 
 # vmbuilder will default to dhcp when no IP is specified - wipe
 echo -e "auto lo\niface lo inet loopback\n" > /etc/network/interfaces
+
+echo 'export PATH=/var/b29/bin:$PATH' >> /root/.bashrc
+echo 'export PATH=/var/b29/bin:$PATH' >> /home/b29/.bashrc
