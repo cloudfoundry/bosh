4452783ec13edd685af120ffb571b393f8ea7a43
skaar <skaar@waste.org>
2010-11-17 16:38:55 -0500

    band-aid failing bosh agent test

--- agent/spec/unit/message/compile_package_spec.rb
+++ agent/spec/unit/message/compile_package_spec.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
+
 describe Bosh::Agent::Message::CompilePackage do
+
+  before(:each) do 
+    Bosh::Agent::Config.blobstore_options = {}
+  end
 
   it 'should have a blobstore client' do
     handler = Bosh::Agent::Message::CompilePackage.new(nil)
