b1ce425db7437d27f86e5bc92eeb5db2307e086b
Oleg Shaldybin <olegs@vmware.com>
2010-11-30 11:54:16 -0800

    Added simple bosh-cli runner to decouple running commands from parsing command line

--- cli/lib/cli/runner.rb
+++ cli/lib/cli/runner.rb
@@ -2,6 +2,37 @@ module Bosh
   module Cli
 
     class Runner
+
+      def self.run(cmd, *args)
+        new(cmd, *args).run
+      end
+
+      def initialize(cmd, *args)
+        @cmd  = cmd
+        @args = args
+      end
+
+      def run
+        method   = find_cmd_implementation
+        expected = method.arity
+
+        if expected >= 0 && @args.size != expected
+          raise ArgumentError, "wrong number of arguments for #{self.class.name}##{method.name} (#{@args.size} for #{expected})"
+        end
+
+        method.call(*@args)
+      end
+
+      private
+
+      def find_cmd_implementation
+        begin
+          self.method(@cmd)
+        rescue NameError
+          raise UnknownCommand, "unknown command '%s'" % [ @cmd ]
+        end
+      end
+      
     end
     
   end
