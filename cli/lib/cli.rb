768a489af887a36d7ce59182aaa81898bebbfa47
Oleg Shaldybin <olegs@vmware.com>
2010-11-30 18:18:09 -0800

    Added bosh-cli api client and auth/create user operations

--- cli/lib/cli.rb
+++ cli/lib/cli.rb
@@ -1,4 +1,8 @@
 require "cli/errors"
 require "cli/deployment"
+require "cli/user"
+require "cli/api_client"
+
 require "cli/runner"
+
 
---
+++ cli/lib/cli/api_client.rb
@@ -0,0 +1,24 @@
+$:.unshift File.expand_path('../../../vendor/gems/httpclient/lib', __FILE__)
+require "httpclient"
+
+module Bosh
+  module Cli
+    class ApiClient
+
+      class InvalidMethod < StandardError; end
+
+      def initialize(target_uri, username, password)
+        @target_uri   = URI.parse(target_uri)
+        @http_client  = HTTPClient.new(:agent_name => "bosh-cli")
+        @http_client.set_auth(@target_uri, username, password)
+      end
+
+      def request(verb, uri, payload = nil, content_type = "application/octet-stream")
+        raise InvalidMethod unless [ :post, :put, :get, :delete ].include?(verb.to_sym)
+        response = @http_client.request(verb, @target_uri + uri, nil, payload, "Content-Type" => content_type)
+        [ response.status, response.content ]
+      end
+      
+    end
+  end
+end
