2d49fab591166950bd541b7bb9c4d136e0fce8ba
Oleg Shaldybin <olegs@vmware.com>
2010-11-30 16:31:45 -0800

    Target and deployment now affect each other in bosh-cli

--- cli/spec/unit/runner_spec.rb
+++ cli/spec/unit/runner_spec.rb
@@ -4,26 +4,26 @@ describe Bosh::Cli::Runner do
 
   before(:all) do
     Bosh::Cli::Runner.class_eval do
-      def dummy_cmd(arg1, arg2, arg3); end
+      def cmd_dummy_cmd(arg1, arg2, arg3); end
     end
   end
 
   it "dispatches commands to appropriate methods" do
-    runner = Bosh::Cli::Runner.new(:dummy_cmd, 1, 2, 3)
-    runner.should_receive(:dummy_cmd).with(1, 2, 3)
+    runner = Bosh::Cli::Runner.new(:dummy_cmd, $stdout, 1, 2, 3)
+    runner.should_receive(:cmd_dummy_cmd).with(1, 2, 3)
     runner.run
   end
 
   it "whines on invalid arity" do
-    runner = Bosh::Cli::Runner.new(:dummy_cmd, 1, 2)
+    runner = Bosh::Cli::Runner.new(:dummy_cmd, $stdout, 1, 2)
 
     lambda {
       runner.run
-    }.should raise_error(ArgumentError, "wrong number of arguments for Bosh::Cli::Runner#dummy_cmd (2 for 3)")
+    }.should raise_error(ArgumentError, "wrong number of arguments for Bosh::Cli::Runner#cmd_dummy_cmd (2 for 3)")
   end
 
   it "whines on invalid command" do
-    runner = Bosh::Cli::Runner.new(:do_stuff, 1, 2)
+    runner = Bosh::Cli::Runner.new(:do_stuff, $stdout, 1, 2)
 
     lambda {
       runner.run
