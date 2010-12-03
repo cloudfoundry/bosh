50897fabbc331b0deea4e753f9bdb6e14717002e
skaar <skaar@waste.org>
2010-12-03 11:32:29 -0500

    straw-man bosh package compiler

--- agent/lib/agent/message/compile_package.rb
+++ agent/lib/agent/message/compile_package.rb
@@ -3,7 +3,8 @@ require 'blobstore_client'
 module Bosh::Agent
   module Message
     class CompilePackage
-      attr_accessor :package_blobstore_id, :package_sha1
+      attr_accessor :blobstore_id, :package_name, :package_version, :package_sha1
+      attr_accessor :compile_base, :install_base
       attr_reader :blobstore_client
 
       def self.process(args)
@@ -13,16 +14,131 @@ module Bosh::Agent
       def initialize(args)
         bsc_options = Bosh::Agent::Config.blobstore_options
         @blobstore_client = Bosh::Blobstore::SimpleBlobstoreClient.new(bsc_options)
-        @blobstore_id, @sha1 = args
+        @blobstore_id, @sha1, @package_name, @package_version = args
+
+        @compile_base = "/var/b29/data/compile"
+        @install_base = "/var/b29/data/packages"
       end
 
       def start
-        # TODO refactor director to include package name and version
-        # TODO get package from blob store
-        # TODO unpack in temporary directory
-        # TODO run packaging script and install to install location
-        # TODO package up install location (define contract)
-        # TODO push to blobstore
+        # TODO make long running
+        # TODO implement sha1 verification
+        # TODO propagate erros
+        begin
+          get_source_package
+          unpack_source_package
+          compile
+        rescue RuntimeError => e
+          # TODO: propagate errors
+          # TODO: logging
+        end
+      end
+
+      def get_source_package
+        compile_tmp = File.join(@compile_base, 'tmp')
+        FileUtils.mkdir_p compile_tmp
+        @source_file = File.join(compile_tmp, @blobstore_id)
+        FileUtils.rm @source_file if File.exist?(@source_file)
+
+        File.open(@source_file, 'w') do |f|
+          f.write(@blobstore_client.get(@blobstore_id))
+        end
+      end
+
+      def compile_dir
+        @compile_dir ||= File.join(@compile_base, @package_name)
+      end
+
+      def install_dir
+        @install_dir ||= File.join(@install_base, @package_name, @package_version.to_s)
+      end
+
+      def unpack_source_package
+        FileUtils.rm_rf compile_dir if File.directory?(compile_dir)
+
+        FileUtils.mkdir_p compile_dir
+        Dir.chdir(compile_dir) do
+          # TODO: error handling
+          `tar -zxf #{@source_file}`
+        end
+      end
+
+      def compile
+        FileUtils.rm_rf install_dir if File.directory?(install_dir)
+        FileUtils.mkdir_p install_dir
+
+        Dir.chdir(compile_dir) do
+          # TODO: error handling
+          ENV['BOSH_COMPILE_TARGET'] = compile_dir
+          ENV['BOSH_INSTALL_TARGET'] = install_dir
+          if File.exist?('packaging')
+            puts `bash packaging`
+          end
+        end
+      end
+
+      def compiled_package
+        File.join(@source_file + ".compiled")
+      end
+
+      def pack
+        Dir.chdir(install_dir) do
+          `tar -zcf #{compiled_package} .`
+        end
+      end
+
+      def compiled_package
+        File.open(compiled_package, 'r') do |f|
+          @blobstore_client.create(f)
+        end
+      end
+
+      def compile_dir
+        @compile_dir ||= File.join(@compile_base, @package_name)
+      end
+
+      def install_dir
+        @install_dir ||= File.join(@install_base, @package_name, @package_version.to_s)
+      end
+
+      def unpack_source_package
+        FileUtils.rm_rf compile_dir if File.directory?(compile_dir)
+
+        FileUtils.mkdir_p compile_dir
+        Dir.chdir(compile_dir) do
+          # TODO: error handling
+          `tar -zxf #{@source_file}`
+        end
+      end
+
+      def compile
+        FileUtils.rm_rf install_dir if File.directory?(install_dir)
+        FileUtils.mkdir_p install_dir
+
+        Dir.chdir(compile_dir) do
+          # TODO: error handling
+          ENV['BOSH_COMPILE_TARGET'] = compile_dir
+          ENV['BOSH_INSTALL_TARGET'] = install_dir
+          if File.exist?('packaging')
+            `bash packaging`
+          end
+        end
+      end
+
+      def compiled_package
+        File.join(@source_file + ".compiled")
+      end
+
+      def pack
+        Dir.chdir(install_dir) do
+          `tar -zcf #{compiled_package} .`
+        end
+      end
+
+      def upload
+        File.open(compiled_package, 'r') do |f|
+          @blobstore_client.create(f)
+        end
       end
 
     end
--- agent/spec/unit/message/compile_package_spec.rb
+++ agent/spec/unit/message/compile_package_spec.rb
@@ -1,15 +1,84 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
+dummy_package_data = File.open(File.dirname(__FILE__) + '/../../fixtures/dummy.package').read
 
 describe Bosh::Agent::Message::CompilePackage do
 
   before(:each) do 
     Bosh::Agent::Config.blobstore_options = {}
+    @httpclient = mock("httpclient")
+    HTTPClient.stub!(:new).and_return(@httpclient)
+
+    args = "some_blobstore_id", "some_sha1", "some_name", 1
+    @handler = Bosh::Agent::Message::CompilePackage.new(args)
+
+    @handler.compile_base = File.dirname(__FILE__) + '/../../tmp/data/compile'
+    @handler.install_base = File.dirname(__FILE__) + '/../../tmp/data/packages'
   end
 
   it 'should have a blobstore client' do
     handler = Bosh::Agent::Message::CompilePackage.new(nil)
     handler.blobstore_client.should be_an_instance_of Bosh::Blobstore::SimpleBlobstoreClient
+  end
+
+
+  # TODO: this is essentially re-testing the blobstore client, but I didn't know the API well enough
+  it 'should unpack a package' do
+    response = mock("response")
+    response.stub!(:status).and_return(200)
+    response.stub!(:content).and_return(dummy_package_data)
+    @httpclient.should_receive(:get).with("/resources/some_blobstore_id", {}, {}).and_return(response)
+
+    FileUtils.rm_rf @handler.compile_base
+
+    package_file = File.join(@handler.compile_base, 'tmp', @handler.blobstore_id)
+    File.exist?(package_file).should be_false
+    @handler.get_source_package
+    File.exist?(package_file).should be_true
+
+    compile_dir = File.join(@handler.compile_base, @handler.package_name)
+    File.directory?(compile_dir).should be_false
+    @handler.unpack_source_package
+    File.directory?(compile_dir).should be_true
+    File.exist?(File.join(compile_dir, 'packaging')).should be_true
+  end
+
+  it 'should compile a package' do
+    FileUtils.rm_rf @handler.compile_base
+    @handler.blobstore_client.stub(:get).with(@handler.blobstore_id).and_return(dummy_package_data)
+
+    @handler.get_source_package
+    @handler.unpack_source_package
+    @handler.compile
+
+    dummy_file = File.join(@handler.install_base, @handler.package_name, @handler.package_version.to_s, 'dummy.txt')
+    File.exist?(dummy_file).should be_true
+  end
+
+  it 'should pack a compiled package' do
+    FileUtils.rm_rf @handler.compile_base
+    @handler.blobstore_client.stub(:get).with(@handler.blobstore_id).and_return(dummy_package_data)
+
+    @handler.get_source_package
+    @handler.unpack_source_package
+    @handler.compile
+    @handler.pack
+  end
+
+  it 'should upload compiled package' do
+    FileUtils.rm_rf @handler.compile_base
+
+    @handler.blobstore_client.stub(:get).with(@handler.blobstore_id).and_return(dummy_package_data)
+
+    @handler.get_source_package
+    @handler.unpack_source_package
+    @handler.compile
+    @handler.pack
+
+    File.open(@handler.compiled_package) do |f|
+      @handler.blobstore_client.stub(:create).with(instance_of(File))
+      @handler.upload
+    end
   end
 
 end
