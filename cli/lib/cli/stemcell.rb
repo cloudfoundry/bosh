f7df3a2c4e88ed2206a3f34aa6523e119766622b
Oleg Shaldybin <olegs@vmware.com>
2010-12-13 17:02:49 -0800

    Simple cache for stemcell manifests in CLI

--- cli/lib/cli/stemcell.rb
+++ cli/lib/cli/stemcell.rb
@@ -4,8 +4,9 @@ module Bosh
     class Stemcell
       include Validation
       
-      def initialize(tarball_path)
+      def initialize(tarball_path, cache)
         @stemcell_file = File.expand_path(tarball_path, Dir.pwd)
+        @cache = cache
       end
 
       def upload(api_client)
@@ -20,18 +21,36 @@ module Bosh
           File.exists?(@stemcell_file) && File.readable?(@stemcell_file)
         end
 
-        step("Extract tarball", "Cannot extract tarball #{@stemcell_file}", :fatal) do
-          `tar -C #{tmp_dir} -xzf #{@stemcell_file} &> /dev/null`
-          $?.exitstatus == 0
+        cache_key = "%s_%s" % [ @stemcell_file, File.size(@stemcell_file) ]
+
+        manifest_yaml = @cache.read(cache_key)
+
+        if manifest_yaml
+          bosh_say("Using cached manifest...")
+        else
+          bosh_say("Manifest not found in cache, verifying tarball...")
+
+          step("Extract tarball", "Cannot extract tarball #{@stemcell_file}", :fatal) do
+            `tar -C #{tmp_dir} -xzf #{@stemcell_file} &> /dev/null`
+            $?.exitstatus == 0
+          end
+
+          manifest_file = File.expand_path("stemcell.MF", tmp_dir)
+
+          step("Manifest exists", "Cannot find stemcell manifest", :fatal) do
+            File.exists?(manifest_file)
+          end
+
+          step("Stemcell image file", "Stemcell image file is missing", :fatal) do
+            File.exists?(File.expand_path("image", tmp_dir))
+          end          
+
+          bosh_say("Writing manifest to cache...")
+          manifest_yaml = File.read(manifest_file)
+          @cache.write(cache_key, manifest_yaml)
         end
 
-        manifest_file = File.expand_path("stemcell.MF", tmp_dir)
-
-        step("Manifest exists", "Cannot find stemcell manifest", :fatal) do
-          File.exists?(manifest_file)
-        end
-
-        manifest = YAML.load_file(manifest_file)
+        manifest = YAML.load(manifest_yaml)
 
         step("Stemcell properties", "Manifest should contain valid name, version and cloud properties") do
           manifest.is_a?(Hash) && manifest.has_key?("name") && manifest.has_key?("version") &&
@@ -40,9 +59,9 @@ module Bosh
             manifest["cloud_properties"].is_a?(Hash)
         end
 
-        step("Stemcell image file", "Stemcell image file is missing") do
-          File.exists?(File.expand_path("image", tmp_dir))
-        end
+        bosh_say("\nStemcell manifest:")
+        bosh_say(manifest_yaml)
+
       ensure
         FileUtils.rm_rf(tmp_dir)
       end        
--- cli/lib/cli/version.rb
+++ cli/lib/cli/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Cli
-    VERSION = "0.1.7"
+    VERSION = "0.1.8"
   end
 end
