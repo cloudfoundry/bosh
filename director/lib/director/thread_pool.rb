f937873d4ae0a00b188e9aee947b210f546f2f89
Vadim Spivak <vspivak@vmware.com>
2010-12-14 13:39:12 -0800

    add ThreadPool that wraps ActionPool

---
+++ director/lib/director/thread_pool.rb
@@ -0,0 +1,37 @@
+module Bosh::Director
+
+  class ThreadPool
+
+    def initialize(options = {})
+      actionpool_options = {
+        :min_threads => options[:min_threads] || 1,
+        :min_threads => options[:max_threads] || 1,
+        :respond_thread => self
+      }
+      @pool = ActionPool::Pool.new(actionpool_options)
+      @logger = Config.logger
+      @boom = nil
+    end
+
+    def process(*args, &block)
+      @pool.process(*args, &block)
+    end
+
+    def raise(exception)
+      @logger.debug("Worker thread raised exception: #{exception}")
+      @boom = exception if @boom.nil?
+    end
+
+    def wait(interval = 0.1)
+      sleep(interval) while @boom.nil? && @pool.working + @pool.action_size > 0
+      if @boom
+        @logger.debug("One of the worker threads raised an exception, shutting down pool")
+        @pool.shutdown
+        @logger.debug("Re-raising: #{@boom}")
+        ::Kernel.raise @boom
+      end
+    end
+
+  end
+
+end
