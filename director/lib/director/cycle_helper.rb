01b659d3c77bce423262222c125ad2c7b1bbe98a
Vadim Spivak <vspivak@vmware.com>
2010-12-17 23:40:31 -0800

    cycle helper

---
+++ director/lib/director/cycle_helper.rb
@@ -0,0 +1,33 @@
+module Bosh::Director::CycleHelper
+
+  def self.check_for_cycle(vertices, options = {}, &block)
+    result = {}
+    result[:connected_vertices] = {} if options[:connected_vertices]
+    vertices.each do |vertex|
+      path = {}
+      connected_vertices = options[:connected_vertices] ? Set.new : nil
+      check_for_cycle_helper(path, vertex, connected_vertices, &block)
+      result[:connected_vertices][vertex] = connected_vertices.to_a if connected_vertices
+    end
+    result
+  end
+
+  def self.check_for_cycle_helper(path, vertex, connected_vertices, &block)
+    path[vertex] = path.size + 1
+    connected_vertices << vertex if connected_vertices && path.size > 1
+    edges = block.call(vertex)
+    if edges
+      edges.each do |edge|
+        if path.include?(edge)
+          vertex_path = []
+          path = path.invert
+          path.size.times {|index| vertex_path << path[index + 1]}
+          raise "Cycle: #{vertex_path.join("=>")}=>#{edge}"
+        end
+        check_for_cycle_helper(path, edge, connected_vertices, &block)
+      end
+    end
+    path.delete(vertex)
+  end
+
+end
