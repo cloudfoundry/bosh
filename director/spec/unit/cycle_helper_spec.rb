c8e79ee79b5a63336a3a4e89e13e8b064fbf8772
Vadim Spivak <vspivak@vmware.com>
2010-12-20 00:59:28 -0800

    detect invalid edges

--- director/spec/unit/cycle_helper_spec.rb
+++ director/spec/unit/cycle_helper_spec.rb
@@ -44,4 +44,13 @@ describe Bosh::Director::CycleHelper do
     result.should eql({:connected_vertices=>{:C=>Set.new([]), :A=>Set.new([:C, :B]), :B=>Set.new([:C])}})
   end
 
+  it "should raise an exception when an referenced edge is not found" do
+    graph = {
+      :A => [:B, :C],
+      :B => [:D]
+    }
+
+    lambda { Bosh::Director::CycleHelper.check_for_cycle([:A, :B, :C]) {|vertex| graph[vertex]} }.should raise_exception
+  end
+
 end
