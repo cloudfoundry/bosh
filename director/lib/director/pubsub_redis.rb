1811ac3529c9ac28b6c9073db8b0be4e1e8de6fb
Vadim Spivak <vspivak@vmware.com>
2010-12-29 23:47:27 -0800

    add logging to diagnose redis pubsub issue

--- director/lib/director/pubsub_redis.rb
+++ director/lib/director/pubsub_redis.rb
@@ -4,19 +4,17 @@ module Bosh::Director
     def initialize(redis_options)
       redis_options[:timeout] = 0
       @redis = Redis.new(redis_options)
+      @logger = Config.logger
+      @lock = Mutex.new
       @mapping = {}
-
-      # HACK: work around unsubscribe issue in redis.rb
-      subscribe("rpc:dummy") {}
-      sleep 1
+      setup_loop
     end
 
     def subscribe(*channels, &block)
-      channels.each {|channel| @mapping[channel] = block}
-      if @redis.subscribed?
+      @lock.synchronize do
+        channels.each {|channel| @mapping[channel] = block}
+        setup_loop unless @redis.subscribed?
         @redis.subscribe(*channels)
-      else
-        subscribe_first(*channels)
       end
     end
 
@@ -26,20 +24,34 @@ module Bosh::Director
       @redis.unsubscribe(*channels)
     end
 
-    def subscribe_first(*channels)
-      Thread.new do
-        @redis.subscribe(*channels) do |on|
-          on.subscribe do |channel, subscriptions|
-            callback = @mapping[channel]
-            callback.call(:subscribe) if callback
-          end
+    def setup_loop
+      lock = Mutex.new
+      cv = ConditionVariable.new
 
-          on.message do |channel, msg|
-            callback = @mapping[channel]
-            callback.call(:message, msg) if callback
+      Thread.new do
+        begin
+          @redis.subscribe("rpc:dummy") do |on|
+            on.subscribe do |channel, _|
+              if channel == "rpc:dummy"
+                lock.synchronize { cv.signal }
+              else
+                callback = @mapping[channel]
+                callback.call(:subscribe) if callback
+              end
+            end
+
+            on.message do |channel, msg|
+              callback = @mapping[channel]
+              callback.call(:message, msg) if callback
+            end
           end
+        rescue Exception => e
+          @logger.warn("PubSub error => #{e} - #{e.backtrace.join("\n")}")
+          raise e
         end
       end
+
+      lock.synchronize { cv.wait(lock) }
     end
 
   end
