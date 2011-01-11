8f2b62fae146c910786ee5822e7e1a137fb759c3
Oleg Shaldybin <olegs@vmware.com>
2011-01-10 18:59:04 -0800

    Updated configuration hasher specs to reflect renamed 'configuration' directory

--- director/spec/unit/configuration_hasher_spec.rb
+++ director/spec/unit/configuration_hasher_spec.rb
@@ -29,7 +29,7 @@ describe Bosh::Director::ConfigurationHasher do
 
       tar.mkdir("configuration", {:mode => "0755", :mtime => 0})
       configuration_files.each do |path, configuration_file|
-        tar.add_file("configuration/#{path}", {:mode => "0644", :mtime => 0}) do |os, _|
+        tar.add_file("config/#{path}", {:mode => "0644", :mtime => 0}) do |os, _|
           os.write(configuration_file["contents"])
         end
       end
@@ -60,7 +60,7 @@ describe Bosh::Director::ConfigurationHasher do
     Bosh::Director::Config.stub!(:blobstore).and_return(blobstore_client)
     blobstore_client.should_receive(:get).with("b_id").and_return(template_contents)
 
-    instance.should_receive(:configuration_hash=).with("fe041c2ddf59c5e2603ab137c05023b9d7c5b983")
+    instance.should_receive(:configuration_hash=).with("6baf348a9d309a244bfdc17a92dd382f448bf224")
 
     configuration_hasher = Bosh::Director::ConfigurationHasher.new(job)
     configuration_hasher.hash
@@ -85,7 +85,7 @@ describe Bosh::Director::ConfigurationHasher do
     Bosh::Director::Config.stub!(:blobstore).and_return(blobstore_client)
     blobstore_client.should_receive(:get).with("b_id").and_return(template_contents)
 
-    instance.should_receive(:configuration_hash=).with("1c3819edb5214fd53026b09dc1e262158be3c828")
+    instance.should_receive(:configuration_hash=).with("8938a11ba7e00c634f36f9cfef0ffa051e8c519a")
 
     configuration_hasher = Bosh::Director::ConfigurationHasher.new(job)
     configuration_hasher.hash
