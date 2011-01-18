5b410dd71e49691575f02a6c343647e77fe1ab29
Oleg Shaldybin <olegs@vmware.com>
2011-01-17 17:18:01 -0800

    Updated to ActiveRecord session store to avoid keeping director password in a cookie

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
