b8f70fefb8f3efde27596fc44a53f80f4938fe7e
Vadim Spivak <vspivak@vmware.com>
2011-01-27 21:49:34 -0800

    add delete release API endpoint

--- director/lib/director/release_manager.rb
+++ director/lib/director/release_manager.rb
@@ -29,5 +29,23 @@ module Bosh::Director
       task
     end
 
+    def delete_release(release, options = {})
+      task = Models::Task.new(:state => :queued, :timestamp => Time.now.to_i)
+      task.create
+
+      task_status_file = File.join(Config.base_dir, "tasks", task.id.to_s)
+      FileUtils.mkdir_p(File.dirname(task_status_file))
+      logger = Logger.new(task_status_file)
+      logger.level= Config.logger.level
+      logger.info("Enqueuing task: #{task.id}")
+
+      task.output = task_status_file
+      task.save!
+
+      Resque.enqueue(Jobs::DeleteRelease, task.id, release.name, options)
+
+      task
+    end
+
   end
 end
