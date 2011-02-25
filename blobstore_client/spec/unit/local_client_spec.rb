a8dc0ce1da6a07dcc27f8e5e0bf96fa21cc36dff
skaar <skaar@waste.org>
2011-02-25 16:21:38 -0500

    add simple read-only local client to blobstore_client

---
+++ blobstore_client/spec/unit/local_client_spec.rb
@@ -0,0 +1,31 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'blobstore_client/local_client'
+require 'tempfile'
+
+describe Bosh::Blobstore::LocalClient do
+
+  describe "options" do
+    it "should set blobstore_path" do
+      Dir.mktmpdir do |tmp_dir|
+        File.open(File.join(tmp_dir, 'foo'), 'w') do |fh|
+          fh.puts("bar")
+        end
+
+        client = Bosh::Blobstore::LocalClient.new({"blobstore_path" => tmp_dir})
+        client.get("foo").should == "bar\n"
+      end
+    end
+  end
+
+  describe "operations" do 
+    it "should raise exception on create" do
+      Dir.mktmpdir do |tmp_dir|
+        lambda {
+          client = Bosh::Blobstore::LocalClient.new({"blobstore_path" => tmp_dir})
+          client.create("foo")
+        }.should raise_error(Bosh::Blobstore::BlobstoreError)
+      end
+    end
+  end
+
+end
