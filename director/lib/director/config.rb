d1f0adf7010a2d699a4eace458d688f49ba98c4b
Vadim Spivak <vspivak@vmware.com>
2011-02-27 17:43:01 -0800

    monkey patch sequel sqlite adapter - only do it when sqlite is used - use a busy handler instead of busy timeout to avoid a dead lock

--- director/lib/director/config.rb
+++ director/lib/director/config.rb
@@ -36,6 +36,8 @@ module Bosh::Director
         @name = config["name"] || ""
 
         @blobstore = nil
+
+        patch_sqlite if config["db"].index("sqlite://") == 0
         @db = Sequel.connect(config["db"])
         @db.logger = @logger
 
@@ -118,6 +120,33 @@ module Bosh::Director
 
       def threaded
         Thread.current[:bosh] ||= {}
+      end
+
+      def patch_sqlite
+        require "sequel"
+        require "sequel/adapters/sqlite"
+
+        Sequel::SQLite::Database.class_eval do
+          def connect(server)
+            opts = server_opts(server)
+            opts[:database] = ':memory:' if blank_object?(opts[:database])
+            db = ::SQLite3::Database.new(opts[:database])
+            db.busy_handler do |retries|
+              Bosh::Director::Config.logger.debug "SQLITE BUSY, retry ##{retries}"
+              sleep(0.1)
+              retries < 20
+            end
+
+            connection_pragmas.each{|s| log_yield(s){db.execute_batch(s)}}
+
+            class << db
+              attr_reader :prepared_statements
+            end
+            db.instance_variable_set(:@prepared_statements, {})
+
+            db
+          end
+        end
       end
 
     end
