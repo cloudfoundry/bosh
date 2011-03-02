d7ee149b13eca1ab29fd44b3a24da0f3286d5077
Vadim Spivak <vspivak@vmware.com>
2011-03-02 00:06:04 -0800

    organize new test

--- director/spec/unit/ext_spec.rb
+++ director/spec/unit/ext_spec.rb
@@ -1,38 +1,41 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe :to_openstruct do
+describe "Monkey Patches" do
 
-  it "should convert a complex object to an openstruct" do
-    hash = {
-      :foo => [{:list => "test"}],
-      :test => "bad",
-      :nested => {:hash => 3}
-    }
-    result = hash.to_openstruct
+  describe :to_openstruct do
 
-    ostruct = OpenStruct.new("foo" => [OpenStruct.new("list" => "test")],
-                             "test" => "bad",
-                             "nested" => OpenStruct.new("hash" => 3))
-    result.should == ostruct
+    it "should convert a complex object to an openstruct" do
+      hash = {
+        :foo => [{:list => "test"}],
+        :test => "bad",
+        :nested => {:hash => 3}
+      }
+      result = hash.to_openstruct
+
+      ostruct = OpenStruct.new("foo" => [OpenStruct.new("list" => "test")],
+                               "test" => "bad",
+                               "nested" => OpenStruct.new("hash" => 3))
+      result.should == ostruct
+    end
+
   end
 
-end
+  describe :recursive_merge! do
 
-describe :recursive_merge! do
+    it "should recursively merge hashes" do
+      a = {:foo => {:bar => 5, :foz => 17}}
+      b = {:test => "value", :foo => {:baz => 1, :bar => "hi"}}
 
-  it "should recursively merge hashes" do
-    a = {:foo => {:bar => 5, :foz => 17}}
-    b = {:test => "value", :foo => {:baz => 1, :bar => "hi"}}
+      a.recursive_merge!(b).should == {:test=>"value", :foo=>{:bar=>"hi", :foz=>17, :baz=>1}}
+    end
 
-    a.recursive_merge!(b).should == {:test=>"value", :foo=>{:bar=>"hi", :foz=>17, :baz=>1}}
+    it "should always use the new value type, even if it's not a hash" do
+      a = {:foo => {:bar => 1}}
+      b = {:foo => "value"}
+
+      a.recursive_merge!(b).should == {:foo=>"value"}
+    end
+
   end
-
-  it "should always use the new value type, even if it's not a hash" do
-    a = {:foo => {:bar => 1}}
-    b = {:foo => "value"}
-
-    a.recursive_merge!(b).should == {:foo=>"value"}
-  end
-
 
 end
