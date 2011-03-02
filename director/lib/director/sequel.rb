a083b51de99bc5a214ec738ba128a33b15bd8150
Vadim Spivak <vspivak@vmware.com>
2011-03-02 00:04:39 -0800

    missing file

---
+++ director/lib/director/sequel.rb
@@ -0,0 +1,8 @@
+Sequel::Model.plugin :validation_helpers
+Sequel::Model.raise_on_typecast_failure = false
+
+[:exact_length, :format, :includes, :integer, :length_range, :max_length,
+ :min_length, :not_string, :numeric, :type, :presence, :unique].each do |validation|
+  Sequel::Plugins::ValidationHelpers::DEFAULT_OPTIONS[validation][:message] = validation
+end
+Sequel::Plugins::ValidationHelpers::DEFAULT_OPTIONS[:max_length][:nil_message] = :max_length
