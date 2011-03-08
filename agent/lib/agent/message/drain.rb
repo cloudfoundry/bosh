5147477c0c5bc862f17df123c51cf93ca65e44eb
skaar <skaar@waste.org>
2011-03-07 21:25:27 -0500

    add initial support for agent drain remove unused SystemTimer gem - we used this with redis add posix-spawn gem

--- agent/lib/agent/message/drain.rb
+++ agent/lib/agent/message/drain.rb
@@ -21,18 +21,96 @@ module Bosh::Agent
             raise Bosh::Agent::MessageHandlerError,
               "Drain update called without apply spec"
           end
+
+          @old_spec = Bosh::Agent::Message::State.new(nil).state
         end
       end
 
       def drain
         case @drain_type
         when "shutdown"
-          return 1
+          return 0
         when "update"
-          return 2
+          # HACK: We go through the motions below to be able to support drain scripts written as shell scripts
+          if @old_spec.key?('job') && drain_script_exists?
+            ENV['BOSH_CURRENT_STATE'] = Yajl::Encoder.encode(@old_spec)
+            ENV['BOSH_APPLY_SPEC'] = Yajl::Encoder.encode(@spec)
+
+            job_change =  if !@old_spec.key?('job')
+                            "job_new"
+                          elsif @old_spec['job']['sha1'] == @spec['job']['sha1']
+                            p @spec
+                            "job_unchanged"
+                          else
+                            "job_changed"
+                          end
+
+            hash_change = if !@old_spec.key?('configuration_hash')
+                            "hash_new"
+                          elsif @old_spec['configuration_hash'] == @spec['configuration_hash']
+                            "hash_unchanged"
+                          else
+                            "hash_changed"
+                          end
+
+            drain_time = run_drain_script(job_change, hash_change, updated_packages.flatten)
+            return drain_time
+          end
+          return 0
         else
           raise Bosh::Agent::MessageHandlerError,
             "Unknown drain type #{@drain_type}"
+        end
+      end
+
+      def run_drain_script(job_updated, hash_updated, updated_packages)
+        # Drain contract: on success the drain script should return a number exit(0)
+        child = POSIX::Spawn::Child.new(drain_script, job_updated, hash_updated, *updated_packages)
+        result = child.out
+        unless result.match(/\A\d+\Z/) && child.status.exitstatus == 0
+          raise Bosh::Agent::MessageHandlerError,
+            "Drain script exit #{child.status.exitstatus}: #{result}"
+        end
+        return result.to_i
+      end
+
+      def updated_packages
+        updated_packages = []
+
+        return updated_packages unless @old_spec.key?('packages')
+
+        # Check old packages
+        updated_packages << @old_spec['packages'].find_all do |pkg_name, pkg|
+          if @spec['packages'].key?(pkg_name)
+            pkg['sha1'] != @spec['packages'][pkg_name]['sha1']
+          else
+            false
+          end
+        end.collect { |package_name, pkg| package_name }
+
+        # New packages counts as new
+        updated_packages << @spec['packages'].find_all do |pkg_name, pkg|
+          unless @old_spec['packages'].key?(pkg_name)
+            true
+          else
+            false
+          end
+        end.collect { |package_name, pkg| package_name } 
+      end
+
+      def drain_script
+         job_name = @old_spec['job']['name']
+        "#{@base_dir}/jobs/#{job_name}/drain"
+      end
+
+      def drain_script_exists?
+        File.exists?(drain_script)
+      end
+
+      def updated
+        @udpated_packages = []
+
+        @old_spec['packages'].each do |pkg|
         end
       end
 
