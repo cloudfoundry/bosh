9cde59dac7d2f92598a36c4dabeceef37b9a2804
Vadim Spivak <vspivak@vmware.com>
2011-03-11 13:43:49 -0800

    initial monit api gem

---
+++ monit_api/lib/monit_api.rb
@@ -0,0 +1,8 @@
+require "logger"
+require "net/http"
+
+require "rubygems"
+require "crack"
+
+require "monit_api/client"
+require "monit_api/version"
---
+++ monit_api/lib/monit_api/version.rb
@@ -0,0 +1,3 @@
+module MonitApi
+  VERSION = "1.0.0"
+end
