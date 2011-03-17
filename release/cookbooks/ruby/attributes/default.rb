9bd1cf9360802014dcdb2bfb10832af275827f3e
Vadim Spivak <vspivak@vmware.com>
2011-03-16 22:23:51 -0700

    specify source for ruby in the attributes

--- release/cookbooks/ruby/attributes/default.rb
+++ release/cookbooks/ruby/attributes/default.rb
@@ -1,2 +1,3 @@
 default[:ruby][:version] = "1.8.7-p302"
+default[:ruby][:source]  = "http://ftp.ruby-lang.org//pub/ruby/1.8/ruby-#{ruby[:version]}.tar.gz"
 default[:ruby][:path]    = "/var/vcap/deploy/rubies/ruby-#{ruby[:version]}"
