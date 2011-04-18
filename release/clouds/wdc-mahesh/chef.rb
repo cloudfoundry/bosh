2fc8b6079ea380d33147d78758eac24c21ca9588
Martin Englund <martin@englund.nu>
2011-04-18 14:31:45 -0700

    removed release bits and added them to deployment repo

--- release/clouds/wdc-mahesh/chef.rb
+++
@@ -1,6 +0,0 @@
-log_level         :debug
-cookbook_path     "/var/vcap/deploy/cookbooks"
-file_cache_path   "/tmp/chef-solo"
-http_proxy        "http://wdc-proxy01.cso.vmware.com:3128"
-https_proxy       "http://wdc-proxy01.cso.vmware.com:3128"
-no_proxy          "eng.vmware.com,localhost,127.0.0.0/8,10.0.0.0/8"
