f261afdf01ae080fc3ada4d3bcda718f8c9e1cb0
Oleg Shaldybin <olegs@vmware.com>
2011-05-09 17:03:04 -0700

    Added chef cookbook for Health Monitor
    Change-Id: I0fe4e1476db5c49b8048c19978decf17aff8b1e6

--- health_monitor/lib/health_monitor/runner.rb
+++ health_monitor/lib/health_monitor/runner.rb
@@ -7,7 +7,7 @@ module Bosh::HealthMonitor
     end
 
     def initialize(config_file)
-      Bhm.config = default_config #.merge(load_yaml_file(config_file))
+      Bhm.config = load_yaml_file(config_file)
 
       @logger    = Bhm.logger
       @director  = Bhm.director
@@ -19,36 +19,12 @@ module Bosh::HealthMonitor
       @agent_manager      = AgentManager.new
     end
 
-    # TBD: extract to config file if tests feel it is natural there
-    def default_config
-      {
-        "director" => {
-          "endpoint" => "http://172.31.113.142:25555",
-          "user"     => "admin",
-          "password" => "admin"
-        },
-
-        "mbus" => {
-          "endpoint" => "nats://127.0.0.1:4222",
-          "user"     => "bosh",
-          "password" => "b0$H"
-        },
-
-        "intervals" => { # All intervals are in seconds
-          "poll_director" => 60,
-          "log_stats"     => 60,
-          "poll_agents"   => 10,
-          "agent_timeout" => 60
-        }
-      }
-    end
-
     def run
       EM.kqueue; EM.epoll
 
       EM.error_handler do |e|
         @logger.error "EM error: #{e}"
-        # @logger.error("#{e.backtrace.join("\n")}")
+        @logger.error("#{e.backtrace.join("\n")}")
       end
 
       EM.run do
@@ -98,30 +74,7 @@ module Bosh::HealthMonitor
 
     def poll_director
       @logger.debug "Getting deployments from director..."
-      Fiber.new {
-        deployments = @director.get_deployments
-
-        deployments.each do |deployment|
-          deployment_name = deployment["name"]
-
-          @logger.info "Found deployment `#{deployment_name}'"
-
-          deployment_info = @director.get_deployment(deployment_name)
-          @logger.debug "Updated deployment information for `#{deployment_name}'"
-
-          @deployment_manager.update_deployment(deployment_name, deployment_info)
-          # TODO: handle missing deployments
-
-          if deployment_info["vms"].kind_of?(Array)
-            deployment_info["vms"].each do |vm|
-              @agent_manager.update_agent(deployment_name, vm["agent_id"])
-              # TODO: handle missing agents
-            end
-          else
-            @logger.warn "Cannot get VMs list from deployment, possibly the old director version"
-          end
-        end
-      }.resume
+      Fiber.new { fetch_deployments }.resume
     end
 
     def poll_agents
@@ -130,6 +83,35 @@ module Bosh::HealthMonitor
       @agent_manager.each_agent do |agent|
         @agent_manager.update_state(agent)
       end
+    end
+
+    private
+
+    def fetch_deployments
+      deployments = @director.get_deployments
+
+      deployments.each do |deployment|
+        deployment_name = deployment["name"]
+
+        @logger.info "Found deployment `#{deployment_name}'"
+
+        deployment_info = @director.get_deployment(deployment_name)
+        @logger.debug "Updated deployment information for `#{deployment_name}'"
+
+        @deployment_manager.update_deployment(deployment_name, deployment_info)
+        # TODO: handle missing deployments
+
+        if deployment_info["vms"].kind_of?(Array)
+          deployment_info["vms"].each do |vm|
+            @agent_manager.update_agent(deployment_name, vm["agent_id"])
+            # TODO: handle missing agents
+          end
+        else
+          @logger.warn "Cannot get VMs list from deployment, possibly the old director version"
+        end
+      end
+    rescue Bosh::HealthMonitor::DirectorError => e
+      @logger.error(e)
     end
 
   end
---
+++ release/cookbooks/health_monitor/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:health_monitor][:path]       = "/var/vcap/deploy/bosh/health_monitor"
+default[:health_monitor][:tmp]        = "/var/vcap/deploy/tmp"
+default[:health_monitor][:repos_path] = "/var/vcap/deploy/repos"
+default[:health_monitor][:runner]     = "vcap"
