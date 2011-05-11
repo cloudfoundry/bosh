346b4db4c304aa4ba82b1ae99a4bbb67ee9b45a9
skaar <skaar@waste.org>
2011-05-11 09:10:12 -0400

    Retry/Guard agent lsof and unmount with ~5 minutes timeout
    Change-Id: I063ecea52e7fdb2c63a4b1072782867f48ea8578

--- agent/spec/unit/message/disk_spec.rb
+++ agent/spec/unit/message/disk_spec.rb
@@ -6,3 +6,28 @@ describe Bosh::Agent::Message::MigrateDisk do
     #handler = Bosh::Agent::Message::MigrateDisk.process(["4", "9"])
   end
 end
+
+describe Bosh::Agent::Message::UnmountDisk do
+  it 'should unmount disk' do
+    Bosh::Agent::Message::DiskUtil.stub!(:lookup_disk_by_cid).and_return('/dev/sdy')
+    Bosh::Agent::Message::DiskUtil.stub!(:mount_entry).and_return('/dev/sdy1 /foomount fstype')
+
+    handler = Bosh::Agent::Message::UnmountDisk.new
+    handler.stub!(:lsof_guard)
+    handler.stub!(:umount_guard)
+
+    handler.unmount(["4"]).should == { :message => "Unmounted /dev/sdy1 on /foomount"}
+  end
+
+  it "should fall through if mount is not present" do
+    Bosh::Agent::Message::DiskUtil.stub!(:lookup_disk_by_cid).and_return('/dev/sdx')
+    Bosh::Agent::Message::DiskUtil.stub!(:mount_entry).and_return(nil)
+
+    handler = Bosh::Agent::Message::UnmountDisk.new
+    handler.stub!(:lsof_guard)
+    handler.stub!(:umount_guard)
+
+    handler.unmount(["4"]).should == { :message => "Unknown mount for partition: /dev/sdx1" }
+  end
+end
+
