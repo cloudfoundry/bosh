a5e43426e6565981a9bc4f79eda0fdca22508544
Woon Jung <whj@vmware.com>
2011-05-17 19:08:37 -0700

    rpc-client retry methods in case of timeout errors.
    - added option 'retry_methods' of the form {method_name => num_of_retry, ...}
    - added few spec test to verify basic functionality
    - In the case of AgentClient made 'get_task' and 'get_state' to be retried upto
      10 times.
    
    Change-Id: I13d6370343f65007db1db05f676d1b5b258761f5

--- director/lib/director/agent_client.rb
+++ director/lib/director/agent_client.rb
@@ -2,7 +2,9 @@ module Bosh::Director
   class AgentClient < Client
 
     def initialize(*args)
-      super("agent", *args)
+      # retry 'get_state' and 'get_task' in case of timeout errors
+      options = {:retry_methods => {'get_state' => 10, 'get_task' => 10}}
+      super("agent", *args, options)
     end
 
   end
