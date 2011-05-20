ac556ec157325c126be04871b0c9dccc861f3754
Woon Jung <whj@vmware.com>
2011-05-19 17:18:50 -0700

    Fix Bamboo
    parameter after a splat parameter upsets ruby1.8
    
    Change-Id: Idbb28cf23e4ab2e355348d6aef385b4944d46344

--- director/lib/director/agent_client.rb
+++ director/lib/director/agent_client.rb
@@ -1,10 +1,10 @@
 module Bosh::Director
   class AgentClient < Client
 
-    def initialize(*args)
+    def initialize(id, options = {})
       # retry 'get_state' and 'get_task' in case of timeout errors
-      options = {:retry_methods => {:get_state => 10, :get_task => 10}}
-      super("agent", *args, options)
+      r = {:retry_methods => {:get_state => 10, :get_task => 10}}
+      super("agent", id, options.merge(r))
     end
 
   end
