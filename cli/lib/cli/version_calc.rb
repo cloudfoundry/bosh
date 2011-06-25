7dfd0d9537e117701162a922174b0759dc517609
Oleg Shaldybin <olegs@vmware.com>
2011-06-24 18:07:39 -0700

    CLI 0.6.7: proper version comparison for versions index. Previously it naively compared versions as float numbers which wasn't working for anything above 'x.10'.
    Change-Id: Ib0858618694fd4d075e83a652cee197c5b3d762c

---
+++ cli/lib/cli/version_calc.rb
@@ -0,0 +1,28 @@
+module Bosh::Cli
+  module VersionCalc
+
+    # Returns 0 if two versions are the same,
+    # 1 if v1 > v2
+    # -1 if v1 < v2
+    def version_cmp(v1 = "0", v2 = "0")
+      vp1 = v1.to_s.split(".")
+      vp2 = v2.to_s.split(".")
+
+      [vp1.size, vp2.size].max.times do |i|
+        result = vp1[i].to_i <=> vp2[i].to_i
+        return result unless result == 0
+      end
+
+      0
+    end
+
+    def version_greater(v1, v2)
+      version_cmp(v1, v2) > 0
+    end
+
+    def version_same(v1, v2)
+      version_cmp(v1, v2) == 0
+    end
+
+  end
+end
