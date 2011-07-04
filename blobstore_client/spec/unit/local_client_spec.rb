411a5d815e386639ca2d110c7c7b366d755c84a0
Martin Englund <martin@englund.nu>
2011-07-04 18:06:03 +0200

    added create operation to LocalClient
    Change-Id: I592c41254087fb4cc9306f81624b2d18cae949a1

--- blobstore_client/spec/unit/local_client_spec.rb
+++ blobstore_client/spec/unit/local_client_spec.rb
@@ -2,28 +2,46 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Bosh::Blobstore::LocalClient do
 
-  describe "options" do
-    it "should set blobstore_path" do
-      Dir.mktmpdir do |tmp_dir|
-        File.open(File.join(tmp_dir, 'foo'), 'w') do |fh|
-          fh.puts("bar")
-        end
+  describe "operations" do
 
-        client = Bosh::Blobstore::LocalClient.new({"blobstore_path" => tmp_dir})
-        client.get("foo").should == "bar\n"
-      end
-    end
-  end
+    describe "get" do
+      it "should retrive the correct contents" do
+        Dir.mktmpdir do |tmp_dir|
+          File.open(File.join(tmp_dir, 'foo'), 'w') do |fh|
+            fh.puts("bar")
+          end
 
-  describe "operations" do 
-    it "should raise exception on create" do
-      Dir.mktmpdir do |tmp_dir|
-        lambda {
           client = Bosh::Blobstore::LocalClient.new({"blobstore_path" => tmp_dir})
-          client.create("foo")
-        }.should raise_error(Bosh::Blobstore::BlobstoreError)
+          client.get("foo").should == "bar\n"
+        end
       end
     end
+
+    describe "create" do
+      it "should store a file" do
+        test_file = File.join(File.dirname(__FILE__), "../assets/file")
+        Dir.mktmpdir do |tmp_dir|
+          client = Bosh::Blobstore::LocalClient.new({"blobstore_path" => tmp_dir})
+          fh = File.open(test_file)
+          id = client.create(fh)
+          fh.close
+          original = File.new(test_file).readlines
+          stored = File.new(File.join(tmp_dir, id)).readlines
+          stored.should == original
+        end
+      end
+
+      it "should store a string" do
+        Dir.mktmpdir do |tmp_dir|
+          client = Bosh::Blobstore::LocalClient.new({"blobstore_path" => tmp_dir})
+          string = "foobar"
+          id = client.create(string)
+          stored = File.new(File.join(tmp_dir, id)).readlines
+          stored.should == [string]
+        end
+      end
+    end
+
   end
 
 end
