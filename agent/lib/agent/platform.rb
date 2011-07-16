25e56eafcb54595735b8bf786d60aa518656380d
Martin Englund <martin@englund.nu>
2011-07-16 14:20:13 +0200

    added better error message when a property is missing
    Change-Id: I7b9f61364147652d66cb29acef4e1ed76231849d

--- agent/lib/agent/platform.rb
+++ agent/lib/agent/platform.rb
@@ -5,18 +5,19 @@ module Bosh::Agent
   class Platform
 
     def initialize(platform_name)
+      @name = platform_name
       # TODO: add to loadpath?
       platform = File.join(File.dirname(__FILE__), 'platform', "#{platform_name}.rb")
 
       if File.exist?(platform)
         load platform
       else
-        raise UnknownPlatform
+        raise UnknownPlatform, "platform '#{platform_name}' not found"
       end
     end
 
     def platform
-      Ubuntu.new
+      instance_eval("#{@name.capitalize}.new")
     end
 
   end
