57642a878cd632962ba602e30ec4a1f43b3977eb
Woon Jung <whj@vmware.com>
2011-09-02 13:56:41 -0700

    Workaround a bug in VC(KL)
    Need to double check that the disk is REALLY detached before claimming
    success.... This shuold not be an issue in MN.
    
    Change-Id: I33a9ac9a617d55a5b5b33f10098c62232c75be99

--- director/lib/director/cloud/vsphere/cloud.rb
+++ director/lib/director/cloud/vsphere/cloud.rb
@@ -555,6 +555,16 @@ module VSphereCloud
         set_agent_env(vm, location, env)
         @logger.info("Detaching disk")
         client.reconfig_vm(vm, config)
+
+        5.times do
+          devices = client.get_property(vm, Vim::VirtualMachine, "config.hardware.device", :ensure_all => true)
+          virtual_disk = devices.find { |device| device.kind_of?(Vim::Vm::Device::VirtualDisk) &&
+            device.backing.file_name == vmdk_path }
+          break if virtual_disk.nil?
+          sleep(1.0)
+        end
+        raise "Failed to detach disk: #{disk_cid} from vm: #{vm_cid}" unless virtual_disk.nil?
+
         @logger.info("Finished detaching disk")
       end
     end
