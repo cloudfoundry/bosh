bd21a4822a0cb5be5485d3665024d75ecdffabbd
Ilia Gilderman <iliag@vmware.com>
2011-09-07 23:59:36 -0700

    There is a hack around YAML::Syck in 1.9 so in some cases the test is wrong, it is better to test YAML::Syck::Engine or something similar but the RUBY_VERSION can make the work
    Change-Id: Iffb296986e4069c2cd0b0ad47312ab4af4966afa

--- cli/lib/cli/yaml_helper.rb
+++ cli/lib/cli/yaml_helper.rb
@@ -36,12 +36,10 @@ module Bosh::Cli
       public
       def check_duplicate_keys(path)
         @syck_class = nil
-        if defined?(YAML::Syck)
-          # for ruby 1.8.7
-          @syck_class = YAML::Syck
-        else
-          # for ruby 1.9.2
+        if RUBY_VERSION =~  /^1\.9\.(1|2)/
           @syck_class = Syck
+        else
+          @syck_class = YAML::Syck
         end
 
         File.open(path) do |f|
