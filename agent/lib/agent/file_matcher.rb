23fd93d9592afc1500337aff89701528a271ebb2
Oleg Shaldybin <olegs@vmware.com>
2011-09-08 17:03:09 -0700

    Bosh log management: agent changes
    Agent now supports new long running 'logs' handler.
    It aggregates logs that matched a particular glob
    on a local machine, uploads them to blobstore and
    returns the blobstore id for this bundle.
    
    Change-Id: Ided46eeaa94b30dd98f7056802adf29f41a8b9b4

---
+++ agent/lib/agent/file_matcher.rb
@@ -0,0 +1,43 @@
+module Bosh::Agent
+
+  class FileMatcher
+    attr_writer :globs
+
+    def initialize(base_dir)
+      @base_dir = base_dir
+    end
+
+    def base_dir
+      @base_dir
+    end
+
+    def globs
+      @globs || default_globs
+    end
+
+    def default_globs
+      []
+    end
+  end
+
+  class AgentLogMatcher < FileMatcher
+    def base_dir
+      File.join(@base_dir, "bosh", "log")
+    end
+
+    def default_globs
+      [ "**/*" ]
+    end
+  end
+
+  class JobLogMatcher < FileMatcher
+    def base_dir
+      File.join(@base_dir, "sys", "log")
+    end
+
+    def default_globs
+      [ "**/*.log" ]
+    end
+  end
+
+end
