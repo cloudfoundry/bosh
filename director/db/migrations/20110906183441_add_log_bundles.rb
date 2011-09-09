9fee21f92594539190d381c8f5c39723271c724c
Oleg Shaldybin <olegs@vmware.com>
2011-09-08 17:03:39 -0700

    Bosh log management: director changes
    Director now has REST API endpoint that triggers
    fetching the long bundle from the agent and the
    endpoint for fetching resources from blobstore.
    
    Old log bundles are lazily cleaned up from the blobstore
    by director (director DB is used to persist them).
    
    Change-Id: I68d2874a81622701df95fe13d1c87217f841ebcf

---
+++ director/db/migrations/20110906183441_add_log_bundles.rb
@@ -0,0 +1,11 @@
+Sequel.migration do
+  change do
+
+    create_table :log_bundles do
+      primary_key :id
+      String :blobstore_id, :null => false, :unique => true
+      Time :timestamp, :null => false, :index => true
+    end
+
+  end
+end
---
+++ director/lib/director/instance_manager.rb
@@ -0,0 +1,23 @@
+module Bosh::Director
+
+  class InstanceManager
+    include TaskHelper
+
+    def fetch_logs(user, deployment_name, job, index, options = {})
+      if deployment_name.nil? || job.nil? || index.nil?
+        raise InvalidRequest.new("deployment, job and index parameters are required")
+      end
+
+      deployment = Models::Deployment.find(:name => deployment_name)
+      raise DeploymentNotFound.new(deployment_name) if deployment.nil?
+
+      instance = Models::Instance.find(:deployment_id => deployment.id, :job => job, :index => index)
+      raise InstanceNotFound.new("#{job}/#{index}") if instance.nil?
+
+      task = create_task(user, "fetch logs")
+      Resque.enqueue(Jobs::FetchLogs, task.id, instance.id, options)
+      task
+    end
+  end
+
+end
