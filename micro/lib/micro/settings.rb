8621748e1c0ce9ce821128087e67bfa0adf6df08
Martin Englund <martin@englund.nu>
2011-09-13 15:31:24 -0700

    mcf 1.1 alpha1
    - fixed bug in debug pager
    - refactored version handling
    - improved SSL proxy connection handling
    - improved user messages
    - display current version when there is an upgrade
    - don't blank console when idle
    
    Change-Id: I7988da6a6b1747cc807822f6d04428b647c5763f

--- micro/lib/micro/settings.rb
+++ micro/lib/micro/settings.rb
@@ -7,17 +7,31 @@ module VCAP
         OpenSSL::Random.random_bytes(n).unpack("H*")[0]
       end
 
+      def self.set_prop(properties, service, key, len)
+        properties[service] = {} unless properties[service]
+        properties[service][key] = secret(len)
+      end
+
       # TODO: template settings file
       def self.randomize_passwords(properties)
-        properties['cc']['token'] = secret(64)
-        properties['cc']['password'] = secret(64)
-        properties['router']['password'] = secret(8)
-        properties['nats']['password'] = secret(8)
-        properties['ccdb']['password'] = secret(8)
-        properties['mysql_node']['password'] = properties['ccdb']['password']
-        properties['mysql_gateway']['token'] = secret(4)
-        properties['redis_gateway']['token'] = secret(4)
-        properties['mongodb_gateway']['token'] = secret(4)
+        set_prop(properties, 'cc', 'token', 64)
+        set_prop(properties, 'cc', 'password', 64)
+        set_prop(properties, 'router', 'password', 8)
+        router = properties['router']
+        set_prop(router, 'status', 'user', 8)
+        set_prop(router, 'status', 'password', 8)
+        set_prop(properties, 'nats', 'password', 8)
+        set_prop(properties, 'ccdb', 'password', 8)
+        set_prop(properties, 'mysql_node', 'password', 8)
+        set_prop(properties, 'mysql_gateway', 'token', 4)
+        set_prop(properties, 'redis_gateway', 'token', 4)
+        set_prop(properties, 'mongodb_gateway', 'token', 4)
+        set_prop(properties, 'postgresql_node', 'admin_passwd_hash', 4)
+        set_prop(properties, 'postgresql_gateway', 'admin_passwd_hash', 4)
+        set_prop(properties, 'postgresql_gateway', 'token', 8)
+        set_prop(properties, 'rabbitmq_srs', 'backbone_password', 4)
+        set_prop(properties, 'rabbitmq_srs', 'admin_passwd_hash', 4)
+        set_prop(properties, 'rabbitmq_srs', 'token', 8)
         properties
       end
 
