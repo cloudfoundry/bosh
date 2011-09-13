caee2bf3fa7cfaa644d5f3eccfbcbb4ecba90730
Oleg Shaldybin <olegs@vmware.com>
2011-09-13 15:00:11 -0700

    Propagate job vitals (as reported by monit) in agent heartbeats
    Change-Id: I5d0c2e0646e9ac987f8537c2637df79e85a7925d

--- agent/spec/unit/heartbeat_spec.rb
+++ agent/spec/unit/heartbeat_spec.rb
@@ -18,7 +18,86 @@ describe Bosh::Agent::Heartbeat do
   end
 
   it "publishes heartbeat via nats (with job state in payload)" do
-    @nats.should_receive(:publish).with("hm.agent.heartbeat.agent-zb", Yajl::Encoder.encode({ "job_state" => "running" }))
+    monit_status = {
+      "service1" => {
+        :monitor => :yes,
+        :type => 3,
+        :status => {
+          :code => 10,
+          :message => "running"
+        },
+        :raw => {
+          "uptime" => 14000,
+          "children" => 0,
+          "memory" => {
+            "percent" => 5.2,
+            "percenttotal" => 5.2,
+            "kilobyte" => 10200,
+            "kilobytetotal" => 10200
+          },
+          "cpu" => {
+            "percent" => 0.2,
+            "percenttotal" => 0.2,
+          }
+        }
+
+      },
+      "service2" => {
+        :monitor => :yes,
+        :type => 3,
+        :status => {
+          :code => 12,
+          :message => "running"
+        },
+        :raw => {
+          "uptime" => 14000,
+          "children" => 4,
+          "memory" => {
+            "percent" => 6.8,
+            "percenttotal" => 9.2,
+            "kilobyte" => 14800,
+            "kilobytetotal" => 28800
+          },
+          "cpu" => {
+            "percent" => 0.3,
+            "percenttotal" => 2.7,
+          }
+        }
+      }
+    }
+
+    Bosh::Agent::Monit.enabled = true
+    Bosh::Agent::Monit.stub!(:get_status).and_return(monit_status)
+
+    expected_payload = {
+      "job_state" => "running",
+      "vitals" => {
+        "service1" => {
+          "uptime" => 14000,
+          "status" => "running",
+          "children" => 0,
+          "cpu" => 0.2,
+          "cpu_total" => 0.2,
+          "memory" => { "percent" => 5.2, "kb" => 10200 },
+          "memory_total" => { "percent" => 5.2, "kb" => 10200 }
+        },
+        "service2" => {
+          "uptime" => 14000,
+          "status" => "running",
+          "children" => 4,
+          "cpu" => 0.3,
+          "cpu_total" => 2.7,
+          "memory" => {"percent" => 6.8, "kb" => 14800 },
+          "memory_total" => { "percent" => 9.2, "kb" => 28800 }
+        }
+      }
+    }
+
+    @nats.should_receive(:publish) do |*args|
+      args[0].should == "hm.agent.heartbeat.agent-zb"
+      payload = Yajl::Parser.parse(args[1])
+      payload.should == expected_payload
+    end
     @heartbeat.send_via_mbus
   end
 
