456bc6fcf2b8f3a97b468ffa19879c3c8325832f
Oleg Shaldybin <olegs@vmware.com>
2011-09-19 18:53:47 -0700

    Version bump party
    Director 0.3.8
    Agent 0.2.34
    CLI 0.8.7
    
    Change-Id: I00d640ee777669b57575b7eea0f73abd351ec88e

--- agent/lib/agent/version.rb
+++ agent/lib/agent/version.rb
@@ -1,6 +1,6 @@
 module Bosh
   module Agent
-    VERSION = '0.2.32'
+    VERSION = '0.2.34'
     BOSH_PROTOCOL = "1"
   end
 end
--- director/lib/director/version.rb
+++ director/lib/director/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Director
-    VERSION = "0.3.7"
+    VERSION = "0.3.8"
   end
 end
