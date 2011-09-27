0a5af2eceef5f849e96f9927b982ae30345c0e19
Martin Englund <martin@englund.nu>
2011-09-27 03:27:20 -0700

    micro update for 1.1 rc3
    Change-Id: I3bd40b127c2b0d3f8846638209c453ee1bdfb46d

--- micro/lib/micro/settings.rb
+++ micro/lib/micro/settings.rb
@@ -3,35 +3,45 @@ module VCAP
   module Micro
     class Settings
 
+      def self.preserve(properties, service, key)
+        properties[service] && properties[service][key]
+      end
+
       def self.secret(n)
         OpenSSL::Random.random_bytes(n).unpack("H*")[0]
       end
 
-      def self.set_prop(properties, service, key, len)
+      def self.randomize_password(properties, service, key, len)
         properties[service] = {} unless properties[service]
-        properties[service][key] = secret(len)
+        unless preserve(properties, service, key)
+          properties[service][key] = secret(len)
+        end
       end
 
       # TODO: template settings file
       def self.randomize_passwords(properties)
-        set_prop(properties, 'cc', 'token', 64)
-        set_prop(properties, 'cc', 'password', 64)
-        set_prop(properties, 'router', 'password', 8)
+        randomize_password(properties, 'cc', 'token', 64)
+        randomize_password(properties, 'cc', 'password', 64)
+        randomize_password(properties, 'router', 'password', 8)
         router = properties['router']
-        set_prop(router, 'status', 'user', 8)
-        set_prop(router, 'status', 'password', 8)
-        set_prop(properties, 'nats', 'password', 8)
-        set_prop(properties, 'ccdb', 'password', 8)
-        set_prop(properties, 'mysql_node', 'password', 8)
-        set_prop(properties, 'mysql_gateway', 'token', 4)
-        set_prop(properties, 'redis_gateway', 'token', 4)
-        set_prop(properties, 'mongodb_gateway', 'token', 4)
-        set_prop(properties, 'postgresql_node', 'admin_passwd_hash', 4)
-        set_prop(properties, 'postgresql_gateway', 'admin_passwd_hash', 4)
-        set_prop(properties, 'postgresql_gateway', 'token', 8)
-        set_prop(properties, 'rabbitmq_srs', 'backbone_password', 4)
-        set_prop(properties, 'rabbitmq_srs', 'admin_passwd_hash', 4)
-        set_prop(properties, 'rabbitmq_srs', 'token', 8)
+        randomize_password(router, 'status', 'user', 8)
+        randomize_password(router, 'status', 'password', 8)
+        randomize_password(properties, 'nats', 'password', 8)
+        randomize_password(properties, 'ccdb', 'password', 8)
+        randomize_password(properties, 'mysql_node', 'password', 8)
+        randomize_password(properties, 'mysql_gateway', 'token', 4)
+        randomize_password(properties, 'redis_gateway', 'token', 4)
+        randomize_password(properties, 'mongodb_gateway', 'token', 4)
+
+        randomize_password(properties, 'postgresql_gateway', 'admin_passwd_hash', 4)
+        randomize_password(properties, 'postgresql_gateway', 'token', 8)
+
+        properties['postgresql_node']['admin_passwd_hash'] = properties['postgresql_gateway']['admin_passwd_hash']
+
+        randomize_password(properties, 'rabbitmq_srs', 'admin_passwd_hash', 4)
+        randomize_password(properties, 'rabbitmq_srs', 'token', 64)
+        randomize_password(properties, 'rabbitmq_srs', 'backbone_password', 4)
+
         properties
       end
 
