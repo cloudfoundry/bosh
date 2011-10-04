fa7bcf5cb24409d355ac8b81147240ba5e6f8ff6
Oleg Shaldybin <olegs@vmware.com>
2011-10-03 19:41:00 -0700

    Include disk usage data into agent heartbeats
    Change-Id: I12b77bbfe433112110e5a26fd71de9109ce2192b

--- agent/lib/agent/heartbeat.rb
+++ agent/lib/agent/heartbeat.rb
@@ -56,16 +56,26 @@ module Bosh::Agent
     #     "load": ["0.09","0.04","0.01"],
     #     "cpu": {"user":"0.0","sys":"0.0","wait":"0.4"},
     #     "mem": {"percent":"3.5","kb":"145996"},
-    #     "swap": {"percent":"0.0","kb":"0"}
+    #     "swap": {"percent":"0.0","kb":"0"},
+    #     "disk": {
+    #       "system": {"percent" => "82"},
+    #       "ephemeral": {"percent" => "5"},
+    #       "persistent": {"percent" => "94"}
+    #     }
     #   }
     # }
 
     def heartbeat_payload
       job_state = Bosh::Agent::Monit.service_group_state
-      vitals = Bosh::Agent::Monit.get_vitals
+      monit_vitals = Bosh::Agent::Monit.get_vitals
+
+      # TODO(?): move DiskUtil out of Message namespace
+      disk_usage = Bosh::Agent::Message::DiskUtil.get_usage
 
       job_name = @state["job"] ? @state["job"]["name"] : nil
       index = @state["index"]
+
+      vitals = monit_vitals.merge("disk" => disk_usage)
 
       Yajl::Encoder.encode("job" => job_name, "index" => index, "job_state" => job_state, "vitals" => vitals)
     end
