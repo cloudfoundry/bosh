67797b7b01c7d26546b368fbed037efa84b66f02
Oleg Shaldybin <olegs@vmware.com>
2011-11-14 11:22:54 -0800

    Cloud check director changes (POC)
    * Cloudcheck added to CLI, both manual and automatic mode
    * Added event log to cloudcheck jobs;
    * Added support for failed tasks to event log. Now if task has
      failed, event log renders it in red and shows the error
      message next to task name.
    
    Change-Id: I537a6d58761ab6984210836d3a18d557af01ae7f

---
+++ director/lib/director/models/deployment_problem.rb
@@ -0,0 +1,43 @@
+module Bosh::Director::Models
+  class DeploymentProblem < Sequel::Model
+
+    many_to_one :deployment
+
+    STATES = %w(open closed resolved)
+
+    def validate
+      validates_presence :deployment_id
+      validates_presence :resource_id
+      validates_presence :type
+      validates_presence :data_json
+      validates_presence :state
+      validates_includes STATES, :state
+    end
+
+    def before_create
+      self.created_at ||= Time.now
+      self.last_seen_at ||= Time.now
+    end
+
+    def data
+      Yajl::Parser.parse(data_json)
+    end
+
+    def data=(raw_data)
+      self.data_json = Yajl::Encoder.encode(raw_data)
+    end
+
+    def handler
+      @handler ||= Bosh::Director::ProblemHandlers::Base.create_from_model(self)
+    end
+
+    def resolutions
+      handler.resolutions
+    end
+
+    def description
+      handler.description
+    end
+
+  end
+end
