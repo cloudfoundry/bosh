7711b8027bd280923531963d5d70c862f88a88e2
Mahesh <mpatil@vmware.com>
2011-11-29 16:56:45 -0800

    Bosh SSH
    * set up interactive shell
    * run commands on jobs
    * upload/download file
    * support for cleanup of bosh ssh sessions
    
    Patch 3.
    * Fixed review comments (amongst other things)
      - Added check for free ephemeral port
      - Agent now checks for exitstatus of commands and returns errors
      - changed adduser to useradd with -p option
    
    Patch 4.
    * Added cleaner filter code pointed out by Martin
    
    Patch 5.
    * Added cli and agent unit tests.
    
    Change-Id: I821ee539a884268f24f1aeaf70f625db74356f9f

---
+++ agent/lib/agent/message/ssh.rb
@@ -0,0 +1,121 @@
+module Bosh::Agent
+  module Message
+    class Ssh < Base
+      SSH_USER_PREFIX = "bosh_"
+      SSH_USER_BASE_DIR = "var/vcap/bosh_ssh"
+
+      def self.process(args)
+        self.new(args).start
+      end
+
+      def initialize(args)
+        @command, payload = args
+        @params = Yajl::Parser.parse(payload)
+        @base_dir = File.join(Bosh::Agent::Config.system_root, SSH_USER_BASE_DIR)
+      end
+
+      def get_salt_charset
+        charset = []
+        charset.concat(("a".."z").to_a)
+        charset.concat(("A".."Z").to_a)
+        charset.concat(("0".."9").to_a)
+        charset << "."
+        charset << "/"
+        charset
+      end
+
+      def encrypt_password(plain_text)
+        @salt_charset ||= get_salt_charset
+        salt = "$6$"
+        8.times do |_|
+          salt << @salt_charset[rand(@salt_charset.size)]
+        end
+        plain_text.crypt(salt)
+      end
+
+      def shell_cmd(cmd)
+        shell_output = %x[#{cmd} 2>&1]
+        raise "'#{cmd}' failed, error: #{shell_output}" if $?.exitstatus != 0
+      end
+
+      def setup
+        begin
+          user = @params["user"]
+          password = @params["password"]
+          logger.info("Setting up ssh for user #{user}")
+
+          shell_cmd(%Q[mkdir -p #{@base_dir}])
+
+          if password
+            shell_cmd(%Q[useradd -m -b #{@base_dir} -s /bin/bash -p '#{encrypt_password(password)}' #{user}])
+          else
+            shell_cmd(%Q[useradd -m -b #{@base_dir} -s /bin/bash #{user}])
+          end
+
+          # Add user to admin and vcap group
+          shell_cmd(%Q[usermod -G admin,vcap #{user}])
+
+          # Add public key to authorized keys
+          ssh_dir = File.join(@base_dir, user, ".ssh")
+          FileUtils.mkdir_p(ssh_dir)
+
+          File.open(File.join(ssh_dir, "authorized_keys"), "w+") do |f|
+            f.write(@params["public_key"])
+          end
+          FileUtils.chown_R(user, user, ssh_dir)
+
+          # Start sshd
+          SshdMonitor.start_sshd
+          {"command" => @command, "status" => "success", "ip" => Bosh::Agent::Config.default_ip}
+        rescue => e
+          return {"command" => @command, "status" => "failure", "error" => e.message}
+        end
+      end
+
+      def cleanup
+        begin
+          return {"command" => @command, "status" => "bad_user"} if @params["user"].nil? &&
+                                                                    @params["user_regex"].nil?
+          users = []
+          user = @params["user"]
+          logger.info("Cleaning up ssh user #{user}")
+          if user
+            users << user
+          else
+            # list users
+            File.open("/etc/passwd", "r") do |f|
+              while user_entry = f.gets
+                user = /(^.*?):/.match(user_entry)[1]
+                if /#{@params["user_regex"]}/ =~ user
+                  users << user
+                end
+              end
+            end
+          end
+
+          users.each do |user|
+            # cant trust the user_regex completely, so skip unexpected users
+            next unless user =~ /^#{SSH_USER_PREFIX}/
+            logger.info("deleting user #{user}")
+            shell_cmd(%Q[userdel -r #{user}])
+          end
+
+          # Stop sshd
+          SshdMonitor.stop_sshd
+          {"command" => @command, "status" => "success"}
+        rescue => e
+          return {"command" => @command, "status" => "failure", "error" => e.message}
+        end
+      end
+
+      def start
+        case @command
+        when "setup"
+          setup
+        when "cleanup"
+          cleanup
+        end
+      end
+    end
+  end
+end
---
+++ cli/spec/unit/ssh_spec.rb
@@ -0,0 +1,63 @@
+require "spec_helper"
+
+describe Bosh::Cli::Command::Base do
+
+  before :all do
+    @public_key = File.join(Dir.mktmpdir, "public_key")
+    File.open(@public_key, "w+") do |f|
+      f.write("PUBLIC_KEY")
+    end
+  end
+
+  describe Bosh::Cli::Command::Ssh do
+    it "should get the public key" do
+      ssh = Bosh::Cli::Command::Ssh.new
+      public_key = ssh.get_public_key("public_key" => @public_key)
+      public_key.should == "PUBLIC_KEY"
+    end
+
+    it "should get the public key from users home directory or raise exception" do
+      ssh = Bosh::Cli::Command::Ssh.new
+      public_key = nil
+      begin
+        public_key = ssh.get_public_key({})
+      rescue e => Bosh::Cli::CliExit
+        public_key = "SOMETHING"
+      end
+      public_key.should_not be_nil
+    end
+
+    it "should contact director to setup ssh on the job" do
+      mock_director = mock(Object)
+      mock_director.stub(:setup_ssh).and_return([{ "status" => "success", "ip" => "127.0.0.1" }])
+      Bosh::Cli::Director.should_receive(:new).and_return(mock_director)
+      ssh = Bosh::Cli::Command::Ssh.new
+      ssh.stub(:cleanup_ssh)
+      ssh.setup_ssh("dea", 0, "temp_pass", {"public_key" => @public_key}) { |results, user|
+        results.each do |result|
+          result["status"].should == "success" && result["ip"].should == "127.0.0.1"
+        end
+      }
+    end
+
+    it "should try to setup interactive shell" do
+      ssh = Bosh::Cli::Command::Ssh.new
+      @interactive_shell = false
+      @execute_command = false
+      ssh.stub(:setup_interactive_shell) { @interactive_shell = true }
+      ssh.stub(:execute_command) { @execute_command = true }
+      ssh.shell("dea", "--index", "0")
+      @interactive_shell.should == true && @execute_command.should == false
+    end
+
+    it "should try to execute given command remotely" do
+      ssh = Bosh::Cli::Command::Ssh.new
+      @interactive_shell = false
+      @execute_command = false
+      ssh.stub(:setup_interactive_shell) { @interactive_shell = true }
+      ssh.stub(:execute_command) { @execute_command = true }
+      ssh.shell("dea", "ls -l")
+      @interactive_shell.should == false && @execute_command.should == true
+    end
+  end
+end
