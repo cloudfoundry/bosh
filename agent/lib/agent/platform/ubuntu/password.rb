d49af610999a13d2ee53eb811a1a6a6c717b2646
skaar <skaar@waste.org>
2011-12-31 21:53:33 -0800

    agent: basic support for setting passwords through resouce pool env
    Change-Id: I30051bb5d4eb47fde47da2374153e1d034b321e3

---
+++ agent/lib/agent/platform/ubuntu/password.rb
@@ -0,0 +1,31 @@
+module Bosh::Agent
+  class Platform::Ubuntu::Password
+
+    def update(settings)
+      if bosh_settings = settings['env']['bosh']
+
+        # TODO - also support user/password hash override 
+        if bosh_settings['password']
+          update_passwords(bosh_settings['password'])
+        end
+
+      end
+    end
+
+    def update_passwords(password)
+      [ 'root', BOSH_APP_USER ].each do |user|
+        update_password(user, password)
+      end
+    end
+
+    # "mkpasswd -m sha-512" to mimick default LTS passwords
+    def update_password(user, password)
+      output = `usermod -p '#{password}' #{user} 2>%`
+      exit_code = $?.exitstatus
+      unless exit_code == 0
+        raise Bosh::Agent::FatalError, "Failed set passsword for #{user} (#{exit_code}: #{output})"
+      end
+    end
+
+  end
+end
--- agent/lib/agent/version.rb
+++ agent/lib/agent/version.rb
@@ -1,6 +1,6 @@
 module Bosh
   module Agent
-    VERSION = '0.4.0'
+    VERSION = '0.4.1'
     BOSH_PROTOCOL = "1"
   end
 end
