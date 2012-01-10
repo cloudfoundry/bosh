c5b10aa0665588b423d29ee8ab48eef6a9b79ac7
mpage <mpage@vmware.com>
2012-01-10 13:50:02 -0800

    First cut at chroot building scripts
    This replaces the chroot building portion of stemcell generation with
    our own scripts. We still rely on vmbuilder to generate the final image
    for us, though a future diff may replace that as well. This also adds
    a custom kernel and includes a compressed tarball of the base stemcell
    chroot.
    
    Test plan:
    - Generated a stemcell, deployed it to the deas on my instance, ran
      BVTs.
    - Spot checked that the correct kernel was running
    
    Change-Id: I04eff583e389d9a77cea58fa690207cfe2d271d2

---
+++ agent/misc/stemcell/build/chroot/lib/configure_bosh.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+bosh_app_dir=/var/vcap
+
+export PATH=${bosh_app_dir}/bosh/bin:$PATH
+export HOME=/root
+
+chown root:root ${bosh_app_dir}/bosh
+chmod 0700 ${bosh_app_dir}/bosh
+
+dpkg -l > ${bosh_app_dir}/bosh/stemcell_dpkg_l.out
+
+cd ${bosh_app_dir}/bosh/src
+
+tar zxvf monit-5.2.4.tar.gz
+(
+  cd monit-5.2.4
+  ./configure --prefix=${bosh_app_dir}/bosh
+  make && make install
+)
+
+ruby_version="1.9.2-p180"
+tar jxvf ruby-${ruby_version}.tar.bz2
+(
+  cd ruby-${ruby_version}
+  ./configure \
+    --prefix=${bosh_app_dir}/bosh \
+    --disable-install-doc
+  make && make install
+)
+rm -fr ruby-${ruby_version}
+
+echo "gem: --no-ri --no-rdoc" > /etc/gemrc
+
+tar zxvf rubygems-1.3.7.tgz
+(
+  cd rubygems-1.3.7
+  ${bosh_app_dir}/bosh/bin/ruby setup.rb
+)
+
+gem install bundler-1.0.10.gem
+
+version=$(cat version)
+agent_path=${bosh_app_dir}/bosh/agent_${version}_builtin
+
+mkdir -p ${agent_path}
+cp -a bin lib Gemfile* vendor ${agent_path}
+ln -s ${agent_path} ${bosh_app_dir}/bosh/agent
+chmod +x ${bosh_app_dir}/bosh/agent/bin/agent
+
+(
+  cd ${bosh_app_dir}/bosh/agent
+  bundle install --path ${bosh_app_dir}/bosh/gems
+)
+
+mkdir -p ${bosh_app_dir}/bosh/log
+chown root:root ${bosh_app_dir}/bosh
+chmod 0700 ${bosh_app_dir}/bosh
+
+cp -a runit/agent /etc/sv/agent
+chmod +x /etc/sv/agent/run /etc/sv/agent/log/run
+ln -s /etc/sv/agent /etc/service/agent
+
+cp sysstat /etc/default/sysstat
+
+ln -s /etc/init.d/open-vm-tools /etc/rc2.d/S88open-vm-tools
+
+# replace vmxnet3 from included kernel
+dpkg -i vmware-tools-vmxnet3-modules-source_1.0.36.0-2_amd64.deb
+(
+  cd /usr/src
+  tar zxvf vmware-tools-vmxnet3-modules.tar.gz
+  cd modules/vmware-tools-vmxnet3-modules/vmxnet3-only
+
+  module_dir=`ls -d /lib/modules/2.6.*-server | tail -1`
+  kernel_uname_r=`basename ${module_dir}`
+
+  # Work around Makefile autodetection of environment - kernel version mismatch
+  # as the chroot reports the host OS version
+  sed "s/^VM_UNAME.*$/VM_UNAME = ${kernel_uname_r}/" Makefile > Makefile.vmxnet3_bosh
+
+  install_dir="${module_dir}/updates/vmxnet3"
+  mkdir -p $install_dir
+
+  make -f Makefile.vmxnet3_bosh
+  cp vmxnet3.ko $install_dir/
+)
+
+# vmbuilder will default to dhcp when no IP is specified - wipe
+echo -e "auto lo\niface lo inet loopback\n" > /etc/network/interfaces
+
+echo 'export PATH=/var/vcap/bosh/bin:$PATH' >> /root/.bashrc
+echo 'export PATH=/var/vcap/bosh/bin:$PATH' >> /home/vcap/.bashrc
+
+# the agent will run monit
+rm /etc/init.d/monit
+
+mkdir -p ${bosh_app_dir}/bosh/etc
+cp monitrc ${bosh_app_dir}/bosh/etc/monitrc
+chmod 0700 ${bosh_app_dir}/bosh/etc/monitrc
+
+# monit refuses to start without an include file present
+mkdir -p ${bosh_app_dir}/monit
+touch ${bosh_app_dir}/monit/empty.monitrc
+
+cp empty_state.yml ${bosh_app_dir}/bosh/state.yml
+
+# TODO: We really want to lock these down - but we're having issues
+# with both our components and users apps assuming this is writable
+# Tempfile and friends - we'll punt on this for 4/12 and revisit it
+# in the immediate release cycle after that.
+# Lock dowon /tmp and /var/tmp - jobs should use /var/vcap/data/tmp
+chmod 0770 /tmp /var/tmp
+
+# remove setuid binaries - except su/sudo (sudoedit is hardlinked)
+find / -xdev -perm +6000 -a -type f \
+  -a -not \( -name sudo -o -name su -o -name sudoedit \) \
+  -exec chmod ug-s {} \;
+
+# the bosh agent installs a config that rotates on size
+mv /etc/cron.daily/logrotate /etc/cron.hourly/logrotate
+
+# Clean out src
+cd /var/tmp
+rm -fr ${bosh_app_dir}/bosh/src
