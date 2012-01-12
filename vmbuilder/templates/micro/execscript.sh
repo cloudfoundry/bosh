2166397e0b4e68bd48e1758011c2aeefc07f44fe
Martin Englund <martin@englund.nu>
2012-01-11 20:40:20 -0800

    micro cloud 1.2 beta1
    Change-Id: Ie5d82d2d64f26599396ad06d5efb555d91683807

--- vmbuilder/templates/micro/execscript.sh
+++ vmbuilder/templates/micro/execscript.sh
@@ -7,6 +7,7 @@ chroot $1 /var/vcap/bosh/src/prepare_instance.sh
 
 chroot $1 /var/vcap/bosh/src/compile.sh
 rm $1/var/vcap/bosh/src/compile.sh
+
 umount $1/proc
 
 cp $1/var/vcap/bosh/micro_dpkg_l.out micro_dpkg_l.out
