fd240228d690e053d2ce37ef5b214b8dacb6f54f
Doug MacEachern <dougm@vmware.com>
2012-01-25 12:40:26 -0800

    Agent HTTP handler
    Use cases:
    - bosh deployer bootstrap (MountDisk, Apply)
    - bosh deployer update (UnmountDisk, MigrateDisk)
    - deployer/mc stemcell builder(s) (CompilePackage)
    
    Updates to agent:
    - if mbus uri has an 'http' scheme, listen for messages over HTTP
      rather than subscribing to NATS.
    - avoid direct calls to @nats.publish outside of Handler.publish
    - add new gem deps
    
    Test:
    - spec tests added for HTTP
    - all spec tests pass
    - CPI create_stemcell,create_vm w/ cloud.properties.mbus=http://...
    
    Change-Id: I80c29624d397efe36ed32e43d95cbb0e6f4b50e7

---
+++ agent/lib/agent/http_handler.rb
@@ -0,0 +1,108 @@
+require "thin"
+require "sinatra"
+require "monitor"
+
+module Bosh::Agent
+
+  class HTTPHandler < Handler
+
+    def self.start
+      new.start
+    end
+
+    def start
+      handler = self
+
+      EM.run do
+        uri = URI.parse(Config.mbus)
+
+        @server = Thin::Server.new(uri.host, uri.port) do
+          use Rack::CommonLogger
+
+          if uri.userinfo
+            use Rack::Auth::Basic do |user, password|
+              "#{user}:#{password}" == uri.userinfo
+            end
+          end
+
+          map "/" do
+            run AgentController.new(handler)
+          end
+        end
+
+        @server.start!
+      end
+    end
+
+    def shutdown
+      @logger.info("Exit")
+      @server.stop
+    end
+
+    def handle_message(json)
+      result = {}
+      result.extend(MonitorMixin)
+
+      cond = result.new_cond
+      timeout_time = Time.now.to_f + 30
+
+      @callback = Proc.new do |response|
+        result.synchronize do
+          result.merge!(response)
+          cond.signal
+        end
+      end
+
+      super(json)
+
+      result.synchronize do
+        while result.empty?
+          timeout = timeout_time - Time.now.to_f
+          unless timeout > 0
+            raise "Timed out"
+          end
+          cond.wait(timeout)
+        end
+      end
+
+      result
+    end
+
+    def publish(reply_to, payload, &blk)
+      response = @callback.call(payload)
+      blk.call if blk
+      response
+    end
+  end
+
+  class AgentController < Sinatra::Base
+
+    def initialize(handler)
+      super()
+      @handler = handler
+    end
+
+    configure do
+      set(:show_exceptions, false)
+      set(:raise_errors, false)
+      set(:dump_errors, false)
+    end
+
+    post "/agent" do
+      body = request.env["rack.input"].read
+      response = handle_message(body)
+      content_type(:json)
+      response
+    end
+
+    def handle_message(json)
+      begin
+        payload = @handler.handle_message(json)
+      rescue => e
+        payload = {:exception => e.inspect}
+      end
+
+      Yajl::Encoder.encode(payload, :terminator => "\n")
+    end
+  end
+end
