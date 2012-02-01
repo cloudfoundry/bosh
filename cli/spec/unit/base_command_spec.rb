d92cfc49479aa2712ed0e90b361f04408cc9d633
Ben Lisbakken <lisbakke@gmail.com>
2012-02-01 15:23:15 -0800

    Allow CLI to remember a deployment for each target.
    Change-Id: I151adc14dad73757bd36c8a54cd80120549624f5

--- cli/spec/unit/base_command_spec.rb
+++ cli/spec/unit/base_command_spec.rb
@@ -36,39 +36,6 @@ describe Bosh::Cli::Command::Base do
     cmd.director.director_uri.should == "localhost:8080"
   end
 
-  it "fetches auth information from the config file" do
-    config = {
-      "target" => "localhost:8080",
-      "deployment" => "test",
-      "auth" => {
-        "localhost:8080" => { "username" => "a", "password" => "b" },
-        "localhost:8081" => { "username" => "c", "password" => "d" }
-      }
-    }
-
-    add_config(config)
-    cmd = make_command()
-
-    cmd.logged_in?.should be_true
-    cmd.username.should == "a"
-    cmd.password.should == "b"
-
-    config["target"] = "localhost:8081"
-    add_config(config)
-
-    cmd = make_command()
-    cmd.logged_in?.should be_true
-    cmd.username.should == "c"
-    cmd.password.should == "d"
-
-    config["target"] = "localhost:8082"
-    add_config(config)
-    cmd = make_command()
-    cmd.logged_in?.should be_false
-    cmd.username.should be_nil
-    cmd.password.should be_nil
-  end
-
   it "can evaluate other commands" do
     cmd     = make_command
     new_cmd = mock(Object)
@@ -89,21 +56,6 @@ describe Bosh::Cli::Command::Base do
     lambda {
       cmd.redirect("misc", "status", :arg1, :arg2)
     }.should raise_error(Bosh::Cli::GracefulExit, "redirected to misc status arg1 arg2")
-  end
-
-  it "effectively ignores config file if it is malformed" do
-    add_config([1, 2, 3])
-    cmd = make_command()
-
-    cmd.target.should == nil
-    cmd.deployment.should == nil
-  end
-
-  it "whines on missing config file" do
-    lambda {
-      File.should_receive(:open).with(@config, "w").and_raise(Errno::EACCES)
-      make_command
-    }.should raise_error(Bosh::Cli::ConfigError)
   end
 
 end
