a862d3626ac33f0e52000de80514d82fb743b10f
Woon Jung <whj@vmware.com>
2012-02-01 10:31:15 -0800

    Change atmos blobstore client to support share-url (read-only)
    Change atmos-blobstore client to use shareable-urls.
    With shareable-urls we can allow external users to fetch objects without the
    secret key.
    
    If the secret-key is not provided the blobstore client will operate in
    read-only mode and operations such as 'create' and 'delete' will fail.
    
    Read atmos programmer's guide for more information about shareable-urls.
    
    Test:
    - Added few spec tests to verify read-only mode.
    - all spec tests passed
    - bosh create release
    
    Note:
    This change also updates the cli to use the new blobstore_client.
    
    Change-Id: I6e47a155cf5295bed26f758a5c13ebb39d78e9c9
    Note: I'll update the 'cli' version on a separate change.

--- blobstore_client/spec/unit/atmos_blobstore_client_spec.rb
+++ blobstore_client/spec/unit/atmos_blobstore_client_spec.rb
@@ -8,6 +8,13 @@ describe Bosh::Blobstore::AtmosBlobstoreClient do
     atmos_opt = {:url => "http://localhost",
                  :uid => "uid",
                  :secret => "secret"}
+
+    @http_client = mock("http-client")
+    ssl_opt = mock("ssl-opt")
+    ssl_opt.stub!(:verify_mode=)
+    @http_client.stub!(:ssl_config).and_return(ssl_opt)
+
+    HTTPClient.stub!(:new).and_return(@http_client)
     @client = Bosh::Blobstore::AtmosBlobstoreClient.new(atmos_opt)
   end
 
@@ -22,22 +29,44 @@ describe Bosh::Blobstore::AtmosBlobstoreClient do
 
     object.should_receive(:aoid).and_return("test-key")
 
-    @client.create(data).should eql("test-key")
+    object_id = @client.create(data)
+    object_info = JSON.load(Base64.decode64(URI::unescape(object_id)))
+    object_info["oid"].should eql("test-key")
+    object_info["sig"].should_not be_nil
   end
 
   it "should delete an object" do
     object = mock("object")
     @atmos.should_receive(:get).with(:id => "test-key").and_return(object)
     object.should_receive(:delete)
-
-    @client.delete("test-key")
+    id = URI::escape(Base64.encode64(JSON.dump(:oid => "test-key", :sig => "sig")))
+    @client.delete(id)
   end
 
   it "should fetch an object" do
-    object = mock("object")
-    @atmos.should_receive(:get).with(:id => "test-key").and_return(object)
-    object.should_receive(:data_as_stream).and_yield("some-content")
+    url = "http://localhost/rest/objects/test-key?uid=uid&expires=1893484800&signature=sig"
+    response = mock("response")
+    response.stub!(:status).and_return(200)
+    @http_client.should_receive(:get).with(url).and_yield("some-content").and_return(response)
+    id = URI::escape(Base64.encode64(JSON.dump(:oid => "test-key", :sig => "sig")))
+    @client.get(id).should eql("some-content")
+  end
 
-    @client.get("test-key").should eql("some-content")
+  it "should refuse to create object without the password" do
+    lambda {
+      no_pass_client = Bosh::Blobstore::AtmosBlobstoreClient.new(:url => "http://localhost", :uid => "uid")
+      no_pass_client.create("foo")
+    }.should raise_error(Bosh::Blobstore::BlobstoreError)
+  end
+
+  it "should be able to read without password" do
+    no_pass_client = Bosh::Blobstore::AtmosBlobstoreClient.new(:url => "http://localhost", :uid => "uid")
+
+    url = "http://localhost/rest/objects/test-key?uid=uid&expires=1893484800&signature=sig"
+    response = mock("response")
+    response.stub!(:status).and_return(200)
+    @http_client.should_receive(:get).with(url).and_yield("some-content").and_return(response)
+    id = URI::escape(Base64.encode64(JSON.dump(:oid => "test-key", :sig => "sig")))
+    no_pass_client.get(id).should eql("some-content")
   end
 end
