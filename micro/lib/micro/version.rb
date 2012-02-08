e58c23ed9ae98d4f0085e6f53d35d2f0d3d1bc77
Martin Englund <martin@englund.nu>
2012-02-08 13:42:33 -0800

    micro cloud 1.2 rc3
    Change-Id: Ic17582528d45ec1987493c43253f4c0b100533f0

--- micro/lib/micro/version.rb
+++ micro/lib/micro/version.rb
@@ -1,7 +1,7 @@
 module VCAP
   module Micro
     class Version
-      VERSION = "1.2.0_rc2"
+      VERSION = "1.2.0_rc3"
       FILE_REGEXP = /micro-(\d+\.\d+\.*\d*_*\S*)\.tgz/
       VERSION_REGEXP = /(\d+)\.(\d+)\.*(\d+)*_*(\S*)/
 
