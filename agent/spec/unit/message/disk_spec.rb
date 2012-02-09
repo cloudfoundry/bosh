69923be628bd2d20ae4e9c6c5f59855fef4453dc
Doug MacEachern <dougm@vmware.com>
2012-02-08 21:22:22 -0800

    fix regression in list_disk
    typo in 7f61e37f causes 'NoMethodError: undefined method `[]' for nil:NilClass'
    
    Change-Id: I047efc9871981659ad2dc863b7e8f2ab78267ace

--- agent/spec/unit/message/disk_spec.rb
+++ agent/spec/unit/message/disk_spec.rb
@@ -54,3 +54,22 @@ describe Bosh::Agent::Message::DiskUtil do
 
 end
 
+describe Bosh::Agent::Message::ListDisk do
+
+  it "should return empty list" do
+    settings = { "disks" => { } }
+    Bosh::Agent::Config.settings = settings
+    Bosh::Agent::Message::ListDisk.process([]).should == []
+  end
+
+  it "should list persistent disks" do
+    Bosh::Agent::Message::DiskUtil.stub!(:lookup_disk_by_cid).and_return('/dev/sdy')
+    Bosh::Agent::Message::DiskUtil.stub!(:mount_entry).and_return('/dev/sdy1 /foomount fstype')
+
+    settings = { "disks" => { "persistent" => { 199 => 2 }}}
+    Bosh::Agent::Config.settings = settings
+
+    Bosh::Agent::Message::ListDisk.process([]).should == [199]
+  end
+
+end
