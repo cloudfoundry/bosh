8578626c4df0d2ec1d09cb14031295a320fbfb45
Martin Englund <martin@englund.nu>
2012-02-16 10:00:07 -0800

    micro cloud 1.2 rc5
    Change-Id: I832319532d1f47372a055d96107a48c908034d5b

--- micro/lib/micro/console.rb
+++ micro/lib/micro/console.rb
@@ -199,7 +199,7 @@ module VCAP
           email = ask("\nAdmin email: ").to_s
 
           while !tos_accepted?
-            display_file(TOS)
+            display_file(TOS, true)
             if agree("\nDo you agree to the Terms of Service? ")
               FileUtils.touch(TOS_ACCEPTED)
             end
@@ -213,6 +213,8 @@ module VCAP
           @identity.nonce = token
           @network.online!
           @identity.install(ip)
+          # TOS implicitly accespted as the user has been to the cf.com web site
+          FileUtils.touch(TOS_ACCEPTED)
         end
         @identity.save
         DNS.new(ip, @identity.subdomain).generate
@@ -369,7 +371,7 @@ module VCAP
             state = @watcher.paused ? "enable" : "disable"
             menu.choice("#{state} network watcher") { toggle_watcher }
             menu.choice("reapply configuration") { reapply }
-            menu.choice("toggle cloud controller registration") { toggle_cc_registration }
+            menu.choice("toggle cloud controller user registration") { toggle_cc_registration }
             menu.choice("network trouble shooting") { network_troubleshooting }
             # nasty hack warning:
             # exec-ing causes the console program to restart when dpkg-reconfigrue exits
@@ -460,7 +462,7 @@ module VCAP
 
       # a very naïve pager
       LINES_PER_PAGE = 20
-      def display_file(file)
+      def display_file(file, quit_on_end=false)
         lines = nil
         File.open(file) do |f|
           lines = f.readlines
@@ -486,6 +488,7 @@ module VCAP
             current += LINES_PER_PAGE
             if current >= lines.size
               # last page
+              return if quit_on_end
               if lines.size > LINES_PER_PAGE
                 current = lines.size - LINES_PER_PAGE
               else
