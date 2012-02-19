bce60900df82733301b75d4d8e30aecf0c25a939
Vadim Spivak <vspivak@vmware.com>
2012-02-19 01:44:52 -0800

    cleaned up blobstore server tests
    - they were really functional, not integration
    - added Rake task to run (ci and cov)
    - updated Gems to be on par with director
    
    Change-Id: I4129fa2f713d6d0355d77866728e662eca586583

--- simple_blobstore_server/spec/spec_helper.rb
+++ simple_blobstore_server/spec/spec_helper.rb
@@ -3,6 +3,25 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 require "rubygems"
 Bundler.setup(:default, :test)
 
+if ENV["SIMPLECOV"]
+  require "simplecov"
+  require "simplecov-rcov"
+  require "simplecov-clover"
+
+  SimpleCov.formatter = Class.new do
+    def format(result)
+      SimpleCov::Formatter::CloverFormatter.new.format(result)
+      SimpleCov::Formatter::RcovFormatter.new.format(result)
+    end
+  end
+
+  SimpleCov.root(File.expand_path("../..", __FILE__))
+  SimpleCov.add_filter("spec")
+  SimpleCov.add_filter("vendor")
+  SimpleCov.coverage_dir(ENV["SIMPLECOV_DIR"] || "spec_coverage")
+  SimpleCov.start
+end
+
 require "rack/test"
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/../lib")
