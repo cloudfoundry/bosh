d3c8734d005534c8069dd3b81e814f3a4d04e900
Doug MacEachern <dougm@vmware.com>
2012-02-24 17:14:20 -0800

    Micro BOSH Deployer
    See README.rdoc for details.
    
    Change-Id: I2ef9191fdf7b59eb34c0a0c41d54f5fe4d2abf62

---
+++ deployer/lib/bosh/cli/commands/micro.rb
@@ -0,0 +1,380 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+require "deployer"
+
+module Bosh::Cli::Command
+  class Micro < Base
+    include Bosh::Cli::DeploymentHelper
+
+    MICRO_DIRECTOR_PORT = 25555
+    DEFAULT_CONFIG_PATH = File.expand_path("~/.bosh_deployer_config")
+
+    command :micro_deployment do
+      usage "micro deployment [<name>]"
+      desc  "Choose micro deployment to work with"
+      route { |args| (args.size > 0) ? [:micro, :set_current] : [:micro, :show_current] }
+    end
+
+    command :micro_status do
+      usage "micro status"
+      desc  "Display micro BOSH deployment status"
+      route :micro, :status
+    end
+
+    command :micro_list_deployments do
+      usage "micro deployments"
+      desc  "Show the list of deployments"
+      route :micro, :list
+    end
+
+    command :micro_deploy do
+      usage  "micro deploy <stemcell>"
+      desc   "Deploy a micro BOSH instance to the currently selected deployment"
+      option "--update", "update existing instance"
+      route  :micro, :perform
+    end
+
+    command :micro_delete do
+      usage  "micro delete"
+      desc   "Delete micro BOSH instance (including persistent disk)"
+      route  :micro, :delete
+    end
+
+    command :micro_agent do
+      usage "micro agent <args>"
+      desc  "Send agent messages"
+      route :micro, :agent
+    end
+
+    command :micro_apply do
+      usage "micro apply <spec>"
+      desc  "Apply spec"
+      route :micro, :apply
+    end
+
+    def initialize(options = {})
+      options[:config] ||= DEFAULT_CONFIG_PATH #hijack Cli::Config
+      super(options)
+    end
+
+    def status
+      stemcell_cid = deployer_state(:stemcell_cid)
+      stemcell_name = deployer_state(:stemcell_name)
+      vm_cid = deployer_state(:vm_cid)
+      disk_cid = deployer_state(:disk_cid)
+      deployment = config.deployment ? config.deployment.green : "not set".red
+
+      say("Stemcell CID".ljust(15) + stemcell_cid)
+      say("Stemcell name".ljust(15) + stemcell_name)
+      say("VM CID".ljust(15) + vm_cid)
+      say("Disk CID".ljust(15) + disk_cid)
+      say("Micro BOSH CID".ljust(15) + Bosh::Deployer::Config.uuid)
+      say("Deployment".ljust(15) + deployment)
+
+      update_target
+
+      target_name = full_target_name ? full_target_name.green : "not set".red
+      say("Target".ljust(15) + target_name)
+    end
+
+    def perform(*options)
+      update = options.delete("--update")
+      tarball_path = options.shift
+
+      if tarball_path.nil?
+        err "No stemcell provided"
+      end
+
+      rel_path = deployment[/#{Regexp.escape File.join(work_dir, '')}(.*)/, 1]
+
+      desc = "`#{rel_path.green}' to `#{target_name.green}'"
+
+      if update
+        unless deployer.exists?
+          err "No existing instance to update"
+        end
+
+        confirmation = "Updating"
+
+        method = :update
+      else
+        if deployer.exists?
+          err "Instance exists.  Did you mean to --update?"
+        end
+
+        confirmation = "Deploying new"
+
+        method = :create
+      end
+
+      confirm_deployment("#{confirmation} micro BOSH instance #{desc}")
+
+      stemcell = Bosh::Cli::Stemcell.new(tarball_path, cache)
+
+      say("\nVerifying stemcell...")
+      stemcell.validate
+      say("\n")
+
+      if !stemcell.valid?
+        err("Stemcell is invalid, please fix, verify and upload again")
+      end
+
+      renderer = DeployerRenderer.new
+      renderer.start
+      deployer.renderer = renderer
+
+      start_time = Time.now
+
+      deployer.send(method, tarball_path)
+
+      renderer.finish("done")
+
+      duration = renderer.duration || (Time.now - start_time)
+
+      update_target
+
+      say("Deployed #{desc}, took #{format_time(duration).green} to complete")
+    end
+
+    def delete
+      unless deployer.exists?
+        err "No existing instance to delete"
+      end
+
+      name = deployer.state.name
+
+      say "\nYou are going to delete micro BOSH deployment `#{name}'.\n\n" \
+      "THIS IS A VERY DESTRUCTIVE OPERATION AND IT CANNOT BE UNDONE!\n".red
+
+      unless confirmed?
+        say "Canceled deleting deployment".green
+        return
+      end
+
+      renderer = DeployerRenderer.new
+      renderer.start
+      deployer.renderer = renderer
+
+      start_time = Time.now
+
+      deployer.destroy
+
+      renderer.finish("done")
+
+      duration = renderer.duration || (Time.now - start_time)
+
+      say("Deleted deployment '#{name}', took #{format_time(duration).green} to complete")
+    end
+
+    def set_current(name)
+      manifest_filename = find_deployment(name)
+
+      if !File.exists?(manifest_filename)
+        err "Missing manifest for #{name} (tried '#{manifest_filename}')"
+      end
+
+      manifest = load_yaml_file(manifest_filename)
+
+      unless manifest.is_a?(Hash)
+        err "Invalid manifest format"
+      end
+
+      if manifest["network"].blank?
+        err "network is not defined in deployment manifest"
+      end
+      ip = manifest["network"]["ip"]
+      if ip.blank?
+        err "network.ip is not defined in deployment manifest"
+      end
+
+      if target
+        old_director_ip = URI.parse(target).host
+      else
+        old_director_ip = nil
+      end
+
+      if old_director_ip != ip
+        config.target = "http://#{ip}:#{MICRO_DIRECTOR_PORT}"
+        say "#{"WARNING!".red} Your target has been changed to `#{target.red}'!"
+      end
+
+      say "Deployment set to '#{manifest_filename.green}'"
+      config.set_deployment(manifest_filename)
+      config.save
+    end
+
+    def show_current
+      say(deployment ? "Current deployment is '#{deployment.green}'" : "Deployment not set")
+    end
+
+    def list
+      file = File.join(work_dir, Bosh::Deployer::InstanceManager::DEPLOYMENTS_FILE)
+      if File.exists?(file)
+        deployments = load_yaml_file(file)["instances"]
+      else
+        deployments = []
+      end
+
+      err("No deployments") if deployments.size == 0
+
+      na = "n/a"
+
+      deployments_table = table do |t|
+        t.headings = [ "Name", "VM name", "Stemcell name" ]
+        deployments.each do |r|
+          t << [ r[:name], r[:vm_cid] || na, r[:stemcell_cid] || na  ]
+        end
+      end
+
+      say("\n")
+      say(deployments_table)
+      say("\n")
+      say("Deployments total: %d" % deployments.size)
+    end
+
+    def agent(*args)
+      message = args.shift
+      args = args.map do |arg|
+        if File.exists?(arg)
+          load_yaml_file(arg)
+        else
+          arg
+        end
+      end
+
+      say(deployer.agent.send(message.to_sym, *args).pretty_inspect)
+    end
+
+    def apply(spec)
+      deployer.apply(load_yaml_file(spec.first))
+    end
+
+    private
+
+    def deployer
+      check_if_deployments_dir
+      deployment_required
+
+      if @deployer.nil?
+        manifest_filename = deployment
+
+        if !File.exists?(manifest_filename)
+          err("Cannot find deployment manifest in `#{manifest_filename}'")
+        end
+
+        manifest = load_yaml_file(manifest_filename)
+
+        manifest["dir"] ||= work_dir
+        manifest["logging"] ||= {}
+        if manifest["logging"]["level"] == "DEBUG"
+          log_dir = File.dirname(manifest_filename)
+          manifest["logging"]["file"] ||= File.join(log_dir, "bosh_micro_deploy.log")
+        end
+
+        @deployer = Bosh::Deployer::InstanceManager.new(manifest)
+
+        $stderr.reopen("/dev/null") #silence ssl warnings
+      end
+
+      @deployer
+    end
+
+    def check_if_deployments_dir
+      #force the issue to maintain central bosh-deployments.yml
+      if File.basename(work_dir) != "deployments"
+        err "Sorry, your current directory doesn't look like deployments directory"
+      end
+    end
+
+    def find_deployment(name)
+      check_if_deployments_dir
+      File.expand_path(File.join(work_dir, "#{name}", "micro_bosh.yml"))
+    end
+
+    def update_target
+      if deployer.exists?
+        director = Bosh::Cli::Director.new(target)
+
+        if options[:director_checks]
+          begin
+            status = director.get_status
+          rescue Bosh::Cli::AuthError
+            status = {}
+          rescue Bosh::Cli::DirectorError
+            err("Cannot talk to director at '#{target}', please set correct target")
+          end
+        else
+          status = { "name" => "Unknown Director", "version" => "n/a" }
+        end
+      else
+        status = {}
+      end
+
+      config.target_name = status["name"]
+      config.target_version = status["version"]
+      config.target_uuid = status["uuid"]
+
+      config.save
+    end
+
+    def confirm_deployment(msg)
+      unless confirmed?(msg)
+        cancel_deployment
+      end
+    end
+
+    def deployer_state(column)
+      if value = deployer.state.send(column)
+        value.green
+      else
+        "n/a".red
+      end
+    end
+
+    class DeployerRenderer < Bosh::Cli::EventLogRenderer
+      attr_accessor :stage, :total, :index
+
+      def start
+        @thread = Thread.new do
+          loop do
+            refresh
+            sleep(1)
+          end
+        end
+      end
+
+      def finish(state)
+        @thread.kill
+        super(state)
+      end
+
+      def enter_stage(stage, total)
+        @stage = stage
+        @total = total
+        @index = 0
+      end
+
+      def parse_event(event)
+        event
+      end
+
+      def update(state, task)
+        event = {
+          "time"     => Time.now,
+          "stage"    => @stage,
+          "task"     => task,
+          "tags"     => [],
+          "index"    => @index+1,
+          "total"    => @total,
+          "state"    => state.to_s,
+          "progress" => state == :finished ? 100 : 0
+        }
+
+        add_event(event)
+
+        @index += 1 if state == :finished
+      end
+    end
+
+  end
+end
---
+++ deployer/lib/deployer/version.rb
@@ -0,0 +1,7 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module Bosh
+  module Deployer
+    VERSION = "0.1.2"
+  end
+end
