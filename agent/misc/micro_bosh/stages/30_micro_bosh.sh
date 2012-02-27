236a46a395d7391ea7176649b36b051b7dcb75c4
Mahesh <mpatil@vmware.com>
2012-02-27 15:20:53 -0800

    Scripts to build micro bosh stemcell (Also brings in  the sysctl change)
    Adds the following targets to the stemcell rake file
    rake stemcell:basic[chroot]            # Build stemcell [chroot_dir|chroot_tgz] - optional argument chroot dir or chroot tgz
    rake stemcell:chroot_tgz               # Build chroot tgz
    rake stemcell:micro[component,manifest,tarball,chroot]  # Build micro bosh <manifest_file> <tarball> [chroot_dir|chroot_tgz]
    
    basic - Is what is being built as the stemcell today
    micro - Right now takes in micro_bosh as the component. micro_cloud should be next
    chroot - Build the basic chroot tgz, we could use it to build Basic or Micro stemcells if we so choose.
    
    Testing Done: Doug has tested the micro_bosh stemcell by deploying it using the
    cli. I tested the basic stemcell in my dev instance, deployed it, ran vmc push
    etc.
    
    Change-Id: I9420282a38a720b745a0817a018ba545afb27725

---
+++ agent/misc/micro_bosh/stages/30_micro_bosh.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+srcdir=$(dirname $(readlink -nf $0))
+basedir=$(dirname $(readlink -nf ${srcdir/..}))
+libdir=$basedir/lib
+skeleton=$basedir/skeleton
+
+. ${libdir}/helpers.sh
+
+if [ $# -ne 3 ]
+then
+  echo "Usage: env `basename $0` [chroot_target] [instance_dir] [package_dir]"
+  exit 1
+fi
+
+if [ `id -u` -ne "0" ]
+then
+  echo "Sorry, you need to be root"
+  exit 1
+fi
+
+target=$1
+instance_dir=$2
+package_dir=$3
+
+if [ ! -d $instance_dir ]
+then
+  echo "Instance directory $instance_dir doesn't exist or isn't a directory"
+  exit 1
+fi
+
+# Set up mounts
+mount --bind /dev $target/dev
+mount --bind /dev/pts $target/dev/pts
+chroot $target mount -t proc proc /proc
+add_on_exit "umount ${target}/proc $target/dev/pts $target/dev"
+
+# Prevent daemons from starting
+disable_daemon_startup $target $skeleton
+add_on_exit "enable_daemon_startup $target"
+
+# micro bosh specific packages
+run_in_chroot $target "apt-get install -y --force-yes --no-install-recommends libpq-dev genisoimage"
+
+mkdir -p $target/var/vcap/bosh/src
+cp -r $instance_dir/micro_bosh $target/var/vcap/bosh/src
+cp -r $instance_dir/micro_bosh_release $target/var/vcap/bosh/src
+
+chroot $target /var/vcap/bosh/src/micro_bosh/lib/configure_micro_bosh.sh
+
+# Copy the generated apply spec to packaging directory
+cp $target/var/vcap/micro/apply_spec.yml $package_dir
