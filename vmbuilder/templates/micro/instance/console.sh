9d076a12d37d164d5be80f8e1b4d9a750e765064
Martin Englund <martin@englund.nu>
2012-02-28 13:12:17 -0800

    de-microing bosh
    Change-Id: I499f5e111e1d560385b392fc1fcea20bdc7f5356

--- vmbuilder/templates/micro/instance/console.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# disable console blanking, we're using a VM not a real computer...
-#
-for index in $(seq 1 6)
-do
-	setterm -blank 0 -powerdown 0 -powersave off > /dev/tty${index}
-done
