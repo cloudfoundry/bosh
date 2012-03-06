669becf4afaa094439036a14ddc2bb6c40bf8b75
Martin Englund <martin@englund.nu>
2012-03-06 05:51:07 -0800

    agent should honor nats MAX_PAYLOAD_SIZE
    - exceptions now delivered as a hash
      with an optional blob that get purged once it is retrieved by the director
    - MessageHandlerError can take a blob argument which get stored in the blobstore
    - compile logs get stored in the blobstore
      and purged once it is retrieved by the director
    - passes all unit, functional & integration tests
    - passes a deployment with old director and new stemcell
    - passes a deployment with new director and old stemcell
    
    Change-Id: I52bfc886aa8c8b2dadb280c3fafef22e1a8b2287

--- director/spec/unit/api/resource_manager_spec.rb
+++ director/spec/unit/api/resource_manager_spec.rb
@@ -36,9 +36,25 @@ describe Bosh::Director::Api::ResourceManager do
 
   it "saves resource to a local file" do
     id = @blobstore.create("some data")
-    path = @manager.get_resource(id)
+    path = @manager.get_resource_path(id)
 
     File.exists?(path).should be_true
     File.read(path).should == "some data"
   end
+
+  it "should return the contents of the blobstore id" do
+    contents = "some data"
+    id = @blobstore.create(contents)
+    @manager.get_resource(id).should == contents
+  end
+
+  it "should delete a resource from the blobstore" do
+    contents = "some data"
+    id = @blobstore.create(contents)
+    @manager.delete_resource(id)
+    lambda {
+      @manager.get_resource(id)
+    }.should_not raise_error BD::ResourceError
+  end
+
 end
