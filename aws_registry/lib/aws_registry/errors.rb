843fb67dbdff233c0bed2e7fca5cb1be07b8dc77
Oleg Shaldybin <olegs@vmware.com>
2012-03-06 15:43:26 -0800

    AWS Registry component
    EC2 doesn't allow changing user-data without stopping the instance.
    However in some cases (i.e. attach/detach disk) CPI should update
    agent settings for working instance, thus we introduce a new component,
    AWS Registry. It's a simple HTTP server that looks up and returns a hash
    associated with a particular EC2 private IP address.
    
    CPI changed to update registry when needed.
    
    Test plan:
    
    * ad-hoc testing with CPI console;
    * unit tests coming soon.
    
    Change-Id: I9e332a641092612fa5558316af6d028aa2f24d66

---
+++ aws_registry/lib/aws_registry/errors.rb
@@ -0,0 +1,15 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module Bosh::AwsRegistry
+
+  class Error < StandardError
+    def self.code(code = nil)
+      define_method(:code) { code }
+    end
+  end
+
+  class FatalError < Error; code(42); end
+
+  class ConfigError < Error; code(101); end
+  class ConnectionError < Error; code(202); end
+end
