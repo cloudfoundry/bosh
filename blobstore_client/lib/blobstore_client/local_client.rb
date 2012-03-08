aacf18b1e5fe16b06aa1eb00d04081bc75190390
Martin Englund <martin@englund.nu>
2012-03-07 20:19:16 -0800

    blobstore_client fixes
    - added atmos to the list of providers on failure
    - fixed regression in local blobstore_client
    - local blobstore_client now creates dir if it doesn't exist
    
    passes all blobstore_client & cli tests
    
    Change-Id: Ib8f1d73735dfd369c5ccff4b11715a2167b73885

--- blobstore_client/lib/blobstore_client/local_client.rb
+++ blobstore_client/lib/blobstore_client/local_client.rb
@@ -5,7 +5,9 @@ module Bosh
       CHUNK_SIZE = 1024*1024
 
       def initialize(options)
-        @blobstore_path = options['blobstore_path']
+        @blobstore_path = options[:blobstore_path]
+        raise "No blobstore path given" if @blobstore_path.nil?
+        FileUtils.mkdir_p(@blobstore_path) unless File.directory?(@blobstore_path)
       end
 
       def create_file(file)
