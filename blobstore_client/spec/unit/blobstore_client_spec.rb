50dd0a9c16d0147096f95c7d9e2d81959163f88e
Martin Englund <martin@englund.nu>
2012-03-07 20:19:16 -0800

    blobstore_client fixes
    - added atmos to the list of providers on failure
    - fixed regression in local blobstore_client
    - local blobstore_client now creates dir if it doesn't exist
    
    passes all blobstore_client & cli tests
    
    Change-Id: Ib8f1d73735dfd369c5ccff4b11715a2167b73885

---
+++ blobstore_client/spec/unit/blobstore_client_spec.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Bosh::Blobstore::Client do
+
+  it "should have a local provider" do
+    Dir.mktmpdir do |tmp|
+      bs = Bosh::Blobstore::Client.create('local', {:blobstore_path => tmp})
+      bs.should be_instance_of Bosh::Blobstore::LocalClient
+    end
+  end
+
+  it "should have an simple provider" do
+    bs = Bosh::Blobstore::Client.create('simple', {})
+    bs.should be_instance_of Bosh::Blobstore::SimpleBlobstoreClient
+  end
+
+  it "should have an atmos provider" do
+    bs = Bosh::Blobstore::Client.create('atmos', {})
+    bs.should be_instance_of Bosh::Blobstore::AtmosBlobstoreClient
+  end
+
+  it "should have an s3 provider" do
+    options = {:access_key_id => "foo", :secret_access_key => "bar"}
+    bs = Bosh::Blobstore::Client.create('s3', options)
+    bs.should be_instance_of Bosh::Blobstore::S3BlobstoreClient
+  end
+
+  it "should raise an exception on an unknown client" do
+    lambda {
+      bs = Bosh::Blobstore::Client.create('foobar', {})
+      }.should raise_error /^Invalid client provider/
+  end
+end
