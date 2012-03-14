894e0670f00cd3b9f151569f05fa0d7335366f46
Martin Englund <martin@englund.nu>
2012-03-14 15:00:14 -0700

    aws_cpi unit tests
    - added guard to aws_cpi
    
    Change-Id: I7af50e7704d9c81328bc9b271117e56ad7dcffe3

--- aws_cpi/spec/unit/create_vm_spec.rb
+++ aws_cpi/spec/unit/create_vm_spec.rb
@@ -3,4 +3,33 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Bosh::AWSCloud::Cloud do
+  before(:all) do
+    @options = mock_cloud_options.merge("agent" => {})
+  end
+
+  describe "#create_vm" do
+    it "should create a vm" do
+      rc = double(Bosh::AWSCloud::RegistryClient)
+      rc.stub(:endpoint).and_return("endpoint")
+      rc.stub(:update_settings)
+      Bosh::AWSCloud::RegistryClient.should_receive(:new).and_return(rc)
+
+      cloud = make_mock_cloud(@options) do |ec2|
+        instance = double("instance")
+        instance.should_receive(:status).and_return(:mock, :running)
+        instance.stub(:id).and_return("id")
+        instance.stub(:private_ip_address)
+        ec2.instances.should_receive(:create).and_return(instance)
+      end
+
+      agent_id = "agent_id"
+      stemcell_id = "stemcell_id"
+      resource_pool = {}
+      network_spec = {}
+
+      id = cloud.create_vm(agent_id, stemcell_id, resource_pool, network_spec)
+      id.should == "id"
+    end
+  end
+
 end
--- aws_cpi/spec/unit/reboot_vm_spec.rb
+++ aws_cpi/spec/unit/reboot_vm_spec.rb
@@ -3,4 +3,21 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Bosh::AWSCloud::Cloud do
+  describe "#reboot_vm" do
+    it "should reboot a vm" do
+      instance_id = "foo"
+      cloud = make_mock_cloud(mock_cloud_options) do |ec2|
+        instance = double("instance")
+        instance.should_receive(:status).
+          and_return(:running, :stopped, :stopped, :running)
+        instance.stub(:id).and_return("id")
+
+        instance.should_receive(:stop)
+        instance.should_receive(:start)
+
+        ec2.instances.should_receive(:[]).with(instance_id).and_return(instance)
+      end
+      cloud.reboot_vm(instance_id)
+    end
+  end
 end
