d06eb82c466c1dd21dd22fa3540eb4e0b801501e
Martin Englund <martin@englund.nu>
2012-03-14 15:00:14 -0700

    aws_cpi unit tests
    - added guard to aws_cpi
    
    Change-Id: I7af50e7704d9c81328bc9b271117e56ad7dcffe3

--- aws_cpi/spec/spec_helper.rb
+++ aws_cpi/spec/spec_helper.rb
@@ -36,3 +36,56 @@ end
 def make_cloud(options = nil)
   Bosh::AWSCloud::Cloud.new(options || mock_cloud_options)
 end
+
+def make_mock_cloud(options = nil)
+  instances = double("instances")
+
+  ec2 = double(AWS::EC2)
+  ec2.stub(:instances).and_return(instances)
+  AWS::EC2.stub(:new).and_return(ec2)
+
+  yield ec2 if block_given?
+
+  Bosh::AWSCloud::Cloud.new(options || mock_cloud_options)
+end
+
+def with_warnings(flag)
+  old_verbose, $VERBOSE = $VERBOSE, flag
+  yield
+ensure
+  $VERBOSE = old_verbose
+end
+
+def constantize(string)
+  string.split('::').inject(Object) do |memo, name|
+    memo = memo.const_get(name)
+    memo
+  end
+end
+
+def parse_constant(constant)
+  source, _, constant_name = constant.to_s.rpartition('::')
+  [constantize(source), constant_name]
+end
+
+def with_constants(constants, &block)
+  saved_constants = {}
+  constants.each do |constant, val|
+    source_object, const_name = parse_constant(constant)
+
+    saved_constants[constant] = source_object.const_get(const_name)
+    with_warnings(nil) { source_object.const_set(const_name, val) }
+  end
+
+  begin
+    block.call
+  ensure
+    constants.each do |constant, val|
+      source_object, const_name = parse_constant(constant)
+
+      with_warnings(nil) do
+        source_object.const_set(const_name, saved_constants[constant])
+      end
+    end
+  end
+end
--- aws_cpi/spec/unit/delete_vm_spec.rb
+++ aws_cpi/spec/unit/delete_vm_spec.rb
@@ -3,4 +3,17 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Bosh::AWSCloud::Cloud do
+  describe "#delete_vm" do
+    it "should delete a vm" do
+      instance_id = "foo"
+      cloud = make_mock_cloud(mock_cloud_options) do |ec2|
+        instance = double("instance")
+        instance.should_receive(:status).and_return(:running, :terminated)
+        instance.stub(:id).and_return("id")
+        instance.should_receive(:terminate).and_return(instance)
+        ec2.instances.should_receive(:[]).with(instance_id).and_return(instance)
+      end
+      cloud.delete_vm(instance_id)
+    end
+  end
 end
