56d5cb5e67848e0dc9a9c77347c95db5ae72ce02
Martin Englund <martin@englund.nu>
2012-03-14 15:00:14 -0700

    aws_cpi unit tests
    - added guard to aws_cpi
    
    Change-Id: I7af50e7704d9c81328bc9b271117e56ad7dcffe3

--- aws_cpi/spec/unit/create_vm_spec.rb
+++ aws_cpi/spec/unit/create_vm_spec.rb
@@ -3,4 +3,27 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Bosh::AWSCloud::Cloud do
+  before(:all) do
+    @options = mock_cloud_options.merge("agent" => {})
+  end
+
+  describe "#create_vm" do
+    it "should create a vm" do
+      cloud = make_mock_cloud(@options) do |ec2|
+        instance = double("instance")
+        instance.should_receive(:status).and_return(:mock, :running)
+        instance.stub(:id).and_return("id")
+        ec2.instances.should_receive(:create).and_return(instance)
+      end
+
+      agent_id = "agent_id"
+      stemcell_id = "stemcell_id"
+      resource_pool = {}
+      network_spec = {}
+
+      id = cloud.create_vm(agent_id, stemcell_id, resource_pool, network_spec)
+      id.should == "id"
+    end
+  end
+
 end
