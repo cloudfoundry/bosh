723b9e5cbecb92962087c14f970d499cd6c874c5
Oleg Shaldybin <olegs@vmware.com>
2012-03-19 12:34:49 -0700

    Use soft reboot for AWS CPI reboot_vm
    Also keeping hard reboot code for potential future
    usage.
    
    Change-Id: I1bb8d293e0b2dce64318315421895251f3e3d905

--- aws_cpi/spec/unit/reboot_vm_spec.rb
+++ aws_cpi/spec/unit/reboot_vm_spec.rb
@@ -4,24 +4,37 @@ require File.expand_path("../../spec_helper", __FILE__)
 
 describe Bosh::AWSCloud::Cloud do
 
-  it "reboots an EC2 instance" do
-    instance = double("instance", :id => "i-foobar")
+  before :each do
+    @instance = double("instance", :id => "i-foobar")
 
-    cloud = mock_cloud(mock_cloud_options) do |ec2|
-      ec2.instances.stub(:[]).with("i-foobar").and_return(instance)
+    @cloud = mock_cloud(mock_cloud_options) do |ec2|
+      ec2.instances.stub(:[]).with("i-foobar").and_return(@instance)
     end
+  end
 
-    instance.should_receive(:stop).ordered
-    instance.should_receive(:status).ordered.and_return(:stopping)
-    cloud.should_receive(:wait_resource).
-      with(instance, :stopping, :stopped).ordered
+  it "reboots an EC2 instance (CPI call picks soft reboot)" do
+    @cloud.should_receive(:soft_reboot).with(@instance)
+    @cloud.reboot_vm("i-foobar")
+  end
 
-    instance.should_receive(:start)
-    instance.should_receive(:status).and_return(:starting)
-    cloud.should_receive(:wait_resource).ordered.
-      with(instance, :starting, :running)
+  it "soft reboots an EC2 instance" do
+    @instance.should_receive(:reboot)
+    @cloud.send(:soft_reboot, @instance)
+  end
 
-    cloud.reboot_vm("i-foobar")
+  it "hard reboots an EC2 instance" do
+    # N.B. This requires ebs-store instance
+    @instance.should_receive(:stop).ordered
+    @instance.should_receive(:status).ordered.and_return(:stopping)
+    @cloud.should_receive(:wait_resource).
+      with(@instance, :stopping, :stopped).ordered
+
+    @instance.should_receive(:start)
+    @instance.should_receive(:status).and_return(:starting)
+    @cloud.should_receive(:wait_resource).ordered.
+      with(@instance, :starting, :running)
+
+    @cloud.send(:hard_reboot, @instance)
   end
 
 end
