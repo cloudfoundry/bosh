0f0389b5b01766f41509e6c20d7b60e5391fa8f1
Oleg Shaldybin <olegs@vmware.com>
2012-03-22 17:02:18 -0700

    Delete deployment flow changes
    - added stop/unmount step before detach_disk to satisfy infrastructures
      that require the disk to be unmounted in order to detach it (i.e.
      AWS);
    
    - slightly refactored flow to be more resilient (no nil dereferences)
    
    Change-Id: I2e8e811a216ccde165cc070b5be60214b49bb44b

--- director/spec/unit/jobs/delete_deployment_spec.rb
+++ director/spec/unit/jobs/delete_deployment_spec.rb
@@ -8,25 +8,38 @@ describe Bosh::Director::Jobs::DeleteDeployment do
 
     before(:each) do
       @cloud = mock("cloud")
-      Bosh::Director::Config.stub!(:cloud).and_return(@cloud)
-      @job = Bosh::Director::Jobs::DeleteDeployment.new("test_deployment")
+      BD::Config.stub!(:cloud).and_return(@cloud)
+      @job = BD::Jobs::DeleteDeployment.new("test_deployment")
     end
 
     it "should delete the disk if it's not attached to the VM" do
-      instance = Bosh::Director::Models::Instance.make(:vm => nil)
-      Bosh::Director::Models::PersistentDisk.make(:disk_cid => "disk-cid", :instance_id => instance.id)
+      instance = BD::Models::Instance.make(:vm => nil)
+      BD::Models::PersistentDisk.
+        make(:disk_cid => "disk-cid", :instance_id => instance.id)
 
       @cloud.should_receive(:delete_disk).with("disk-cid")
 
       @job.delete_instance(instance)
 
-      Bosh::Director::Models::Instance[instance.id].should be_nil
+      BD::Models::Instance[instance.id].should be_nil
     end
 
     it "should detach and delete disk if there is a disk" do
-      vm = Bosh::Director::Models::Vm.make(:cid => "vm-cid")
-      instance = Bosh::Director::Models::Instance.make(:vm => vm)
-      Bosh::Director::Models::PersistentDisk.make(:disk_cid => "disk-cid", :instance_id => instance.id)
+      agent = mock("agent")
+
+      BD::AgentClient.stub(:new).with("agent-1").
+        and_return(agent)
+
+      vm = BD::Models::Vm.make(:cid => "vm-cid",
+                                           :agent_id => "agent-1")
+
+      instance = BD::Models::Instance.make(:vm => vm)
+
+      BD::Models::PersistentDisk.
+        make(:disk_cid => "disk-cid", :instance_id => instance.id)
+
+      agent.should_receive(:stop)
+      agent.should_receive(:unmount_disk).with("disk-cid")
 
       @cloud.should_receive(:detach_disk).with("vm-cid", "disk-cid")
       @cloud.should_receive(:delete_disk).with("disk-cid")
@@ -35,41 +48,50 @@ describe Bosh::Director::Jobs::DeleteDeployment do
 
       @job.delete_instance(instance)
 
-      Bosh::Director::Models::Instance[instance.id].should be_nil
+      BD::Models::Instance[instance.id].should be_nil
     end
 
     it "should only delete the VM if there is no disk" do
-      vm = Bosh::Director::Models::Vm.make(:cid => "vm-cid")
-      instance = Bosh::Director::Models::Instance.make(:vm => vm)
+      agent = mock("agent")
+
+      BD::AgentClient.stub(:new).with("agent-1").
+        and_return(agent)
+
+      vm = BD::Models::Vm.make(:cid => "vm-cid", :agent_id => "agent-1")
+      instance = BD::Models::Instance.make(:vm => vm)
+
+      agent.should_receive(:stop)
 
       @job.should_receive(:delete_vm).with(vm)
 
       @job.delete_instance(instance)
 
-      Bosh::Director::Models::Instance[instance.id].should be_nil
+      BD::Models::Instance[instance.id].should be_nil
     end
 
     it "should only delete the model if there is no VM" do
-      instance = Bosh::Director::Models::Instance.make(:vm => nil)
+      instance = BD::Models::Instance.make(:vm => nil)
 
       @job.delete_instance(instance)
 
-      Bosh::Director::Models::Instance[instance.id].should be_nil
+      BD::Models::Instance[instance.id].should be_nil
     end
 
     it "should ignore cpi errors if forced" do
-      vm = Bosh::Director::Models::Vm.make(:cid => "vm-cid")
-      instance = Bosh::Director::Models::Instance.make(:vm => vm)
-      Bosh::Director::Models::PersistentDisk.make(:disk_cid => "disk-cid", :instance_id => instance.id)
+      vm = BD::Models::Vm.make(:cid => "vm-cid")
+      instance = BD::Models::Instance.make(:vm => vm)
+      BD::Models::PersistentDisk.
+        make(:disk_cid => "disk-cid", :instance_id => instance.id)
 
-      @cloud.should_receive(:detach_disk).with("vm-cid", "disk-cid").and_raise("ERROR")
+      @cloud.should_receive(:detach_disk).
+        with("vm-cid", "disk-cid").and_raise("ERROR")
       @cloud.should_receive(:delete_disk).with("disk-cid").and_raise("ERROR")
 
-      job = Bosh::Director::Jobs::DeleteDeployment.new("test_deployment", "force" => true)
+      job = BD::Jobs::DeleteDeployment.new("test_deployment", "force" => true)
       job.should_receive(:delete_vm).with(vm)
       job.delete_instance(instance)
 
-      Bosh::Director::Models::Instance[instance.id].should be_nil
+      BD::Models::Instance[instance.id].should be_nil
     end
 
   end
@@ -78,18 +100,18 @@ describe Bosh::Director::Jobs::DeleteDeployment do
 
     before(:each) do
       @cloud = mock("cloud")
-      Bosh::Director::Config.stub!(:cloud).and_return(@cloud)
-      @job = Bosh::Director::Jobs::DeleteDeployment.new("test_deployment")
+      BD::Config.stub!(:cloud).and_return(@cloud)
+      @job = BD::Jobs::DeleteDeployment.new("test_deployment")
     end
 
     it "should delete the VM and the model" do
-      vm = Bosh::Director::Models::Vm.make(:cid => "vm-cid")
+      vm = BD::Models::Vm.make(:cid => "vm-cid")
 
       @cloud.should_receive(:delete_vm).with("vm-cid")
 
       @job.delete_vm(vm)
 
-      Bosh::Director::Models::Vm[vm.id].should be_nil
+      BD::Models::Vm[vm.id].should be_nil
     end
   end
 
@@ -97,44 +119,61 @@ describe Bosh::Director::Jobs::DeleteDeployment do
 
     before(:each) do
       @cloud = mock("cloud")
-      Bosh::Director::Config.stub!(:cloud).and_return(@cloud)
-      @job = Bosh::Director::Jobs::DeleteDeployment.new("test_deployment")
+      BD::Config.stub!(:cloud).and_return(@cloud)
+      @job = BD::Jobs::DeleteDeployment.new("test_deployment")
     end
 
     it "should delete all the associated instances, VMs, disks and problems" do
       lock = mock("lock")
-      Bosh::Director::Lock.stub!(:new).with("lock:deployment:test_deployment").and_return(lock)
+
+      agent = mock("agent")
+
+      BD::AgentClient.stub(:new).with("agent-1").
+        and_return(agent)
+
+      BD::Lock.stub!(:new).
+        with("lock:deployment:test_deployment").and_return(lock)
 
       lock.should_receive(:lock).and_yield
 
-      stemcell = Bosh::Director::Models::Stemcell.make
-      deployment = Bosh::Director::Models::Deployment.make(:name => "test_deployment")
+      stemcell = BD::Models::Stemcell.make
+      deployment = BD::Models::Deployment.
+        make(:name => "test_deployment")
+
       deployment.stemcells << stemcell
 
-      vm = Bosh::Director::Models::Vm.make(:deployment => deployment)
-      instance = Bosh::Director::Models::Instance.make(:deployment => deployment, :vm => vm)
-      problem = Bosh::Director::Models::DeploymentProblem.make(:deployment => deployment)
-      disk = Bosh::Director::Models::PersistentDisk.make(:instance => instance)
+      vm = BD::Models::Vm.
+        make(:deployment => deployment, :agent_id => "agent-1")
+
+      instance = BD::Models::Instance.
+        make(:deployment => deployment, :vm => vm)
+      problem = BD::Models::DeploymentProblem.
+        make(:deployment => deployment)
+      disk = BD::Models::PersistentDisk.
+        make(:instance => instance, :disk_cid => "disk-cid")
 
       @cloud.stub!(:delete_vm)
       @cloud.stub!(:delete_disk)
       @cloud.stub!(:detach_disk)
 
+      agent.should_receive(:stop)
+      agent.should_receive(:unmount_disk).with("disk-cid")
+
       @job.perform
 
-      Bosh::Director::Models::Deployment[deployment.id].should be_nil
+      BD::Models::Deployment[deployment.id].should be_nil
 
       stemcell.refresh
       stemcell.deployments.should be_empty
 
-      Bosh::Director::Models::Vm[vm.id].should be_nil
-      Bosh::Director::Models::Instance[instance.id].should be_nil
-      Bosh::Director::Models::DeploymentProblem[problem.id].should be_nil
-      Bosh::Director::Models::PersistentDisk[disk.id].should be_nil
+      BD::Models::Vm[vm.id].should be_nil
+      BD::Models::Instance[instance.id].should be_nil
+      BD::Models::DeploymentProblem[problem.id].should be_nil
+      BD::Models::PersistentDisk[disk.id].should be_nil
     end
 
     it "should fail if the deployment is not found" do
-      lambda { @job.perform }.should raise_exception Bosh::Director::DeploymentNotFound
+      lambda { @job.perform }.should raise_exception BD::DeploymentNotFound
     end
 
   end
