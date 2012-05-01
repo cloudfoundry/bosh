862ee4b59fb8432ac3e39cbdd243b4b8e5257822
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-01 16:16:00 -0700

    New stemcell builder
    Change-Id: Idbd75b3146f7767dde81d8e03f7332bb537f49f8

---
+++ agent/misc/stemcell/build2/stages/image_aws_prepare_stemcell/apply.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2009-2012 VMware, Inc.
+
+set -e
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+
+disk_image_name=raw.img
+
+pushd $work
+tar zcf stemcell/image $disk_image_name
+popd
