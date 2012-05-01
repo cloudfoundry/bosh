1259c2bc9a585f747e6a0cd0556e84eebc2debb5
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-01 16:16:00 -0700

    New stemcell builder
    Change-Id: Idbd75b3146f7767dde81d8e03f7332bb537f49f8

---
+++ agent/misc/stemcell/build2/etc/settings.bash
@@ -0,0 +1,28 @@
+# parameters for `base_debootstrap`
+# UBUNTU_ISO=/tmp/lucid-server-amd64.iso
+# UBUNTU_MIRROR=http://mirrors.us.kernel.org/ubuntu/
+
+# parameters for `bosh_users`
+bosh_users_password=c1oudc0w
+
+# parameters for `bosh_agent`
+# bosh_agent_src_dir=/some/path/to/the/bosh/agent
+bosh_agent_infrastructure_name=vsphere
+
+# parameters for `bosh_micro`
+# bosh_micro_enabled=yes
+# bosh_micro_package_compiler_path=/path/to/package_compiler
+# bosh_micro_manifest_yml_path=/path/to/manifest.yml
+# bosh_micro_release_tgz_path=/path/to/release.tgz
+
+# parameters for `image_create`
+# image_create_disk_size=2048
+
+# parameters for `image_vsphere_ovf`
+# image_vsphere_ovf_ovftool_path=/path/to/ovftool
+
+# parameters for `stemcell`
+stemcell_name=untitled
+stemcell_version=unknown
+stemcell_format=unknown
+bosh_protocol_version=unknown
