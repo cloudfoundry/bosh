51b0e2c23eebc04954e10d8e77ccf7ae2a1bd037
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-03 15:39:43 -0700

    Split configuration steps in assert and persist
    Change-Id: If9e16ebe7028af0287a5b535f590041c06aa029f

--- agent/misc/stemcell/build2/stages/bosh_agent/config.sh
+++ agent/misc/stemcell/build2/stages/bosh_agent/config.sh
@@ -7,5 +7,5 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_config.bash
 
-assert_dir bosh_agent_src_dir
-assert_not_empty bosh_agent_src_version
+persist_dir bosh_agent_src_dir
+persist_value bosh_agent_src_version
