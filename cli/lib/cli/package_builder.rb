ab1e8a259b4dbc540dfb35f5ba39d0fa504067d7
Oleg Shaldybin <olegs@vmware.com>
2012-05-07 15:30:09 -0700

    Release alternative src directory support
    Using submodules in release repo might be painful for developers,
    as they have to replace submodule with a symlink. This change adds
    support for alternative src directory. Any glob in package spec will
    try to match in src_alt before src, and if it yields any files, no
    further src match will be attempted.
    
    Change-Id: I19a05ea6f8b652e602ca76cbeb4a805eaa648a44

--- cli/lib/cli/package_builder.rb
+++ cli/lib/cli/package_builder.rb
@@ -4,6 +4,27 @@ module Bosh::Cli
   class PackageBuilder
     include PackagingHelper
 
+    class GlobMatch
+      # Helper class encapsulating the data we know about the glob. We need
+      # both directory and file path, as we match the same path in several
+      # directories (src, src_alt, blobs)
+      attr_reader :dir
+      attr_reader :path
+
+      def initialize(dir, path)
+        @dir = dir
+        @path = path
+      end
+
+      def full_path
+        File.join(dir, path)
+      end
+
+      def <=>(other)
+        @path <=> other.path
+      end
+    end
+
     attr_reader :name, :globs, :version, :dependencies, :tarball_path
     # We have two ways of getting/storing a package:
     # development versions of packages, kept in release directory
@@ -13,20 +34,27 @@ module Bosh::Cli
     # final builds metadata should be checked in
 
     def initialize(spec, release_dir, final, blobstore,
-        sources_dir = nil, blobs_dir = nil)
+        sources_dir = nil, blobs_dir = nil, alt_src_dir = nil)
       spec = load_yaml_file(spec) if spec.is_a?(String) && File.file?(spec)
 
       @name = spec["name"]
       @globs = spec["files"]
       @dependencies = Array(spec["dependencies"])
+
       @release_dir = release_dir
       @sources_dir = sources_dir || File.join(@release_dir, "src")
+      @alt_sources_dir = alt_src_dir || File.join(@release_dir, "src_alt")
       @blobs_dir = blobs_dir || File.join(@release_dir, "blobs")
+
       @final = final
       @blobstore = blobstore
       @artefact_type = "package"
 
       @metadata_files = %w(packaging pre_packaging)
+
+      if @final && File.exists?(@alt_sources_dir)
+        err("Please remove `#{File.basename(@alt_sources_dir)}' first")
+      end
 
       if @name.blank?
         raise InvalidPackage, "Package name is missing"
@@ -65,20 +93,8 @@ module Bosh::Cli
       @fingerprint ||= make_fingerprint
     end
 
-    def resolved_globs
+    def glob_matches
       @resolved_globs ||= resolve_globs
-    end
-
-    def source_files
-      resolved_globs[:source]
-    end
-
-    def blob_files
-      resolved_globs[:blob]
-    end
-
-    def files
-      (resolved_globs[:blob] + resolved_globs[:source]).sort
     end
 
     def build_dir
@@ -92,15 +108,14 @@ module Bosh::Cli
     def copy_files
       copied = 0
 
-      files.each do |filename|
-        file_path = get_file_path(filename)
-        destination = File.join(build_dir, filename)
+      glob_matches.each do |match|
+        destination = File.join(build_dir, match.path)
 
-        if File.directory?(file_path)
+        if File.directory?(match.full_path)
           FileUtils.mkdir_p(destination)
         else
           FileUtils.mkdir_p(File.dirname(destination))
-          FileUtils.cp(file_path, destination, :preserve => true)
+          FileUtils.cp(match.full_path, destination, :preserve => true)
           copied += 1
         end
       end
@@ -160,27 +175,18 @@ module Bosh::Cli
 
     private
 
-    def get_file_path(file)
-      file_path = File.join(@sources_dir, file)
-      if !File.exists?(file_path)
-        file_path = File.join(@blobs_dir, file)
-        unless File.exists?(file_path)
-          raise InvalidPackage, "#{file} cannot be found"
-        end
-      end
-      file_path
-    end
-
     def make_fingerprint
       contents = ""
 
-      signatures = files.map do |file|
-        path = get_file_path(file)
-        unless File.directory?(path)
-          file_digest = Digest::SHA1.file(path).hexdigest
+      signatures = glob_matches.map do |match|
+        file_digest = nil
+
+        unless File.directory?(match.full_path)
+          file_digest = Digest::SHA1.file(match.full_path).hexdigest
         end
 
-        "%s%s%s" % [file, file_digest, tracked_permissions(path)]
+        "%s%s%s" % [match.path, file_digest,
+                    tracked_permissions(match.full_path)]
       end
       contents << signatures.join("")
 
@@ -198,49 +204,62 @@ module Bosh::Cli
       Digest::SHA1.hexdigest(contents)
     end
 
+    # @return Array<GlobMatch>
     def resolve_globs
-      glob_map = { :blob => [], :source => [] }
-      blob_list = []
-      source_list = []
+      matches = []
 
       @globs.each do |glob|
-        matching_source = []
-        matching_blob = []
+        # Alternative source dir completely shadows the source dir, there can be
+        # no partial match of a particular glob in both.
+        found = false
 
-        in_sources_dir do
-          matching_source = Dir.glob(glob, File::FNM_DOTMATCH).reject do |fn|
-            [".", ".."].include?(File.basename(fn))
+        [@alt_sources_dir, @sources_dir].each do |dir|
+          next unless File.directory?(dir)
+
+          Dir.chdir(dir) do
+            dir_matches = resolve_glob_in_cwd(glob)
+
+            unless dir_matches.empty?
+              matches += dir_matches.map do |path|
+                GlobMatch.new(dir, path)
+              end
+              found = true
+            end
+          end
+
+          break if found
+        end
+
+        # Blobs directory is a little bit different: whatever matches a blob
+        # will complement already found matches, unless this particular path
+        # has already been matched.
+        if File.directory?(File.join(@blobs_dir))
+          Dir.chdir(@blobs_dir) do
+            blob_matches = resolve_glob_in_cwd(glob)
+
+            unless blob_matches.empty?
+              blob_matches.each do |path|
+                matches << GlobMatch.new(@blobs_dir, path)
+              end
+
+              found = true
+            end
           end
         end
 
-        in_blobs_dir do
-          matching_blob = Dir.glob(glob, File::FNM_DOTMATCH).reject do |fn|
-            [".", ".."].include?(File.basename(fn))
-          end
-        end
-
-        if matching_blob.size == 0 && matching_source.size ==0
+        unless found
           raise InvalidPackage, "`#{name}' has a glob that " +
-              "resolves to an empty file list: #{glob}"
+            "resolves to an empty file list: #{glob}"
         end
-
-        blob_list << matching_blob
-        source_list << matching_source
       end
-      glob_map[:blob] = blob_list.flatten.sort
-      glob_map[:source] = source_list.flatten.sort
-      glob_map
+
+      matches.sort
     end
 
-    def in_blobs_dir(&block)
-      # old release does not have 'blob'
-      if File.directory?(@blobs_dir)
-        Dir.chdir(@blobs_dir) { yield }
+    def resolve_glob_in_cwd(glob)
+      Dir.glob(glob, File::FNM_DOTMATCH).reject do |fn|
+        %w(. ..).include?(File.basename(fn))
       end
-    end
-
-    def in_sources_dir(&block)
-      Dir.chdir(@sources_dir) { yield }
     end
 
     def in_build_dir(&block)
