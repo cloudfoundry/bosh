d335d15c84d8518fbc3921ea18c168c71cbcbb1e
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-08 10:12:36 -0700

    Persist Ubuntu mirror in settings
    Change-Id: I537d524d2736cc7282c22da43703ec7f6829b2a6

--- agent/misc/stemcell/build2/etc/settings.bash
+++ agent/misc/stemcell/build2/etc/settings.bash
@@ -1,3 +1,7 @@
+# parameters for `base_debootstrap`
+# UBUNTU_ISO=/tmp/lucid-server-amd64.iso
+# UBUNTU_MIRROR=http://mirrors.us.kernel.org/ubuntu/
+
 # parameters for `bosh_agent`
 # bosh_agent_src_dir=/some/path/to/the/bosh/agent
 
