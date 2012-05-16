7bf5b3e5ca927943f6671abc2d3b53e2f9c782c0
Mahesh <mpatil@rbcon.com>
2012-05-16 10:12:07 -0700

    Reuse packages and their compiled versions.
    Before this change if you were to upload and deploy a release with a release
    name that did not exist in your director we would explicitly upload and compile
    all the packages, even if there were packages with the same sha1 in the
    director.
      This change is an optimization to speed up the above scenario by avoiding the upload
    and compilation of packages that are already registered with the director.
    
    Change-Id: I2608f87dea43b4ba45b5779b49467023ec98b68f

--- director/lib/director.rb
+++ director/lib/director.rb
@@ -45,6 +45,7 @@ require "director/version"
 require "director/config"
 require "director/event_log"
 require "director/task_result_file"
+require "director/blob_util"
 
 require "director/client"
 require "director/agent_client"
@@ -429,6 +430,20 @@ module Bosh::Director
 
       content_type(:json)
       json_encode(result)
+    end
+
+    post "/packages/matches", :consumes => :yaml do
+      manifest = YAML.load(request.body)
+      unless manifest.is_a?(Hash) && manifest["packages"].is_a?(Array)
+          raise InvalidRequest.new("bad manifest")
+      end
+
+      sha1_list =  manifest["packages"].map { |package| package["sha1"] }
+
+      package_dataset = Models::Package.dataset
+      packages =
+        package_dataset.select(:sha1).filter(:sha1 => sha1_list).distinct.all
+      Yajl::Encoder.encode(packages.map { |package| package.sha1 })
     end
 
     get "/tasks" do
