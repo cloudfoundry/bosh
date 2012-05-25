afca25c6190c2e644bb3cc5ee6e1c88ca3ed2e57
Oleg Shaldybin <olegs@vmware.com>
2012-05-24 19:19:12 -0700

    Refactored director job runner
    Extracted job runner logic into a separate class and split "perform"
    method into manageable chunks.
    
    Change-Id: I08655cf9a3448dadcc15c2a78a34d0da2d21a7b3

--- director/lib/director/jobs/base_job.rb
+++ director/lib/director/jobs/base_job.rb
@@ -4,84 +4,16 @@ module Bosh::Director
   module Jobs
     class BaseJob
 
+      def self.perform(task_id, *args)
+        Bosh::Director::JobRunner.new(self, task_id).run(*args)
+      end
+
       attr_accessor :task_id
 
       def initialize(*args)
         @logger = Config.logger
         @event_log = Config.event_log
         @result_file = Config.result
-      end
-
-      def self.perform(task_id, *args)
-        task = Models::Task[task_id]
-        raise TaskNotFound.new(task_id) if task.nil?
-
-        Config.current_job = nil
-        logger = Logger.new(File.join(task.output, "debug"))
-        logger.level = Config.logger.level
-        logger.formatter = ThreadFormatter.new
-        logger.info("Starting task: #{task_id}")
-
-        Config.event_log = Bosh::Director::EventLog.new(File.join(task.output, "event"))
-        Config.result = Bosh::Director::TaskResultFile.new(File.join(task.output, "result"))
-        Config.logger = logger
-
-        Config.db.logger = logger
-        Config.dns_db.logger = logger if Config.dns_enabled?
-
-        cloud_options = Config.cloud_options
-        if cloud_options && cloud_options["plugin"] == "vsphere"
-          cloud_options["properties"]["soap_log"] = File.join(task.output, "soap")
-          Config.cloud_options = cloud_options
-        end
-
-        started = Time.now
-        with_thread_name("task:#{task_id}") do
-          begin
-            logger.info("Creating job")
-            job = self.send(:new, *args)
-            Config.current_job = job
-            job.task_id = task_id
-            job.task_checkpoint # cancelled in the queue?
-
-            logger.info("Performing task: #{task_id}")
-            task.state = :processing
-            task.timestamp = Time.now
-            task.checkpoint_time = Time.now
-            task.save
-
-            Thread.new do
-              with_thread_name("task:#{task_id}-checkpoint") do
-                while true
-                  sleep(Config.task_checkpoint_interval)
-                  task = Models::Task[task_id]
-                  task.checkpoint_time = Time.now
-                  task.save
-                end
-              end
-            end
-            result = job.perform
-
-            logger.info("Done")
-            task.state = :done
-            task.result = truncate_str(result.to_s)
-            task.timestamp = Time.now
-            task.save
-          rescue Exception => e
-            if e.kind_of?(Bosh::Director::TaskCancelled)
-              logger.info("task #{task_id} cancelled!")
-              task.state = :cancelled
-            else
-              logger.error("#{e} - #{e.backtrace.join("\n")}")
-              task.state = :error
-            end
-            task.result = truncate_str(e.to_s)
-            task.timestamp = Time.now
-            task.save
-          end
-        end
-        ended = Time.now
-        logger.info("Task took #{Duration.duration(ended - started)} to process.")
       end
 
       def task_cancelled?
@@ -105,15 +37,6 @@ module Bosh::Director
         end
       end
 
-      def self.truncate_str(str, len = 128)
-        etc = "..."
-        stripped = str.strip[0..len]
-        if stripped.length > len
-          stripped.gsub(/\s+?(\S+)?$/, "") + etc
-        else
-          stripped
-        end
-      end
     end
   end
 end
