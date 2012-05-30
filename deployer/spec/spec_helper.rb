084917b75ee2f44b7953c7da418d1e575389d8f3
Martin Englund <martin@englund.nu>
2012-05-30 14:37:29 -0700

    deployer should be more helpful when aws_registry isn't present
    - tells user to run
      gem install bosh_aws_registry
    
    Change-Id: Ia56a6876a41c103f11fae27eeb11d9218bd2db2c

--- deployer/spec/spec_helper.rb
+++ deployer/spec/spec_helper.rb
@@ -13,6 +13,20 @@ def spec_asset(filename)
   File.expand_path("../assets/#{filename}", __FILE__)
 end
 
+def internal_to *args, &block
+  example = describe *args, &block
+  klass = args[0]
+  if klass.is_a? Class
+    saved_private_instance_methods = klass.private_instance_methods
+    example.before do
+      klass.class_eval { public *saved_private_instance_methods }
+    end
+    example.after do
+      klass.class_eval { private *saved_private_instance_methods }
+    end
+  end
+end
+
 RSpec.configure do |c|
   c.fail_fast = true if ENV['BOSH_DEPLOYER_DIR']
 end
