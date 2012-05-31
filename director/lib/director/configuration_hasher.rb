75b5775497594a4c4425bc8cd1e1d8a333e392a2
Ben Lisbakken <lisbakke@gmail.com>
2012-05-31 11:31:26 -0700

    Job-collocation support in Director.
    It changes the format of the messages sent to be forward and
    backward compatible.
    
    Change-Id: I5eeca092775c38cb859cfc5c159a67b292a847f6

--- director/lib/director/configuration_hasher.rb
+++ director/lib/director/configuration_hasher.rb
@@ -25,57 +25,79 @@ module Bosh::Director
 
     def initialize(job)
       @job = job
+      @templates = job.templates
       @logger = Config.logger
     end
 
-    def extract_template
+    def extract_template(job_template)
+      @job_template_dirs ||= {}
+      # Caching extracted templates so that we don't have to download/extract
+      # them twice.
+      if (@job_template_dirs.has_key?(job_template))
+        @template_dir = @job_template_dirs[job_template]
+        return
+      end
       @template_dir = Dir.mktmpdir("template_dir")
       temp_path = File.join(Dir.tmpdir,
                             "template-#{UUIDTools::UUID.random_create}")
       begin
         File.open(temp_path, "w") do |file|
-          Config.blobstore.get(@job.template.template.blobstore_id, file)
+          Config.blobstore.get(job_template.template.blobstore_id, file)
         end
+        # TODO(lisbakke): Check tar exit code.
         `tar -C #{@template_dir} -xzf #{temp_path}`
+        @job_template_dirs[job_template] = @template_dir
       ensure
         FileUtils.rm_f(temp_path)
       end
     end
 
     def hash
+      template_digests = {}
+      sorted_jobs = @templates.sort { |x, y| x.name <=> y.name }
       begin
-        extract_template
-        manifest = YAML.load_file(File.join(@template_dir, "job.MF"))
-
-        monit_template = template_erb("monit")
-        monit_template.filename = File.join(@job.template.name, "monit")
-
-        templates = {}
-
-        if manifest["templates"]
-          manifest["templates"].each_key do |template_name|
-            template = template_erb(File.join("templates", template_name))
-            templates[template_name] = template
-          end
-        end
-
         @job.instances.each do |instance|
-          binding_helper = BindingHelper.new(@job.name, instance.index,
-                                             @job.properties.to_openstruct,
-                                             instance.spec.to_openstruct)
-          digest = Digest::SHA1.new
-          digest << bind_template(monit_template,
-                                  binding_helper, instance.index)
+          instance_digest = Digest::SHA1.new
+          sorted_jobs.each do |job_template|
+            extract_template(job_template)
+            manifest = YAML.load_file(File.join(@template_dir, "job.MF"))
 
-          templates.keys.sort.each do |template_name|
-            template = templates[template_name]
-            template.filename = File.join(@job.template.name, template_name)
-            digest << bind_template(template, binding_helper, instance.index)
+            monit_template = template_erb("monit")
+            monit_template.filename = File.join(job_template.name, "monit")
+            templates = {}
+
+            if manifest["templates"]
+              manifest["templates"].each_key do |template_name|
+                template = template_erb(File.join("templates", template_name))
+                templates[template_name] = template
+              end
+            end
+
+            binding_helper = BindingHelper.new(@job.name, instance.index,
+                                               @job.properties.to_openstruct,
+                                               instance.spec.to_openstruct)
+            bound_templates = bind_template(monit_template, binding_helper,
+                instance.index)
+            templates.keys.sort.each do |template_name|
+              template = templates[template_name]
+              template.filename = File.join(job_template.name, template_name)
+              bound_templates << bind_template(template, binding_helper,
+                  instance.index)
+              template_digest = Digest::SHA1.new
+              template_digest << bound_templates
+              instance_digest << bound_templates
+              template_digests[instance.index] ||= {}
+              template_digests[instance.index][job_template.name] =
+                  template_digest.hexdigest
+            end
           end
-          instance.configuration_hash = digest.hexdigest
+          instance.configuration_hash = instance_digest.hexdigest
+          instance.template_hashes = template_digests[instance.index]
         end
       ensure
-        FileUtils.rm_rf(@template_dir) if @template_dir
+        @job_template_dirs.each do |job, dir|
+          FileUtils.rm_rf(dir)
+        end
       end
     end
 
