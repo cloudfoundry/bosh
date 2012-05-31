216978f708f02e68c3114afd4b428c194618e627
Ben Lisbakken <lisbakke@gmail.com>
2012-05-31 11:31:26 -0700

    Job-collocation support in Director.
    It changes the format of the messages sent to be forward and
    backward compatible.
    
    Change-Id: I5eeca092775c38cb859cfc5c159a67b292a847f6

--- director/lib/director/configuration_hasher.rb
+++ director/lib/director/configuration_hasher.rb
@@ -25,15 +25,16 @@ module Bosh::Director
 
     def initialize(job)
       @job = job
+      @templates = job.templates
       @logger = Config.logger
     end
 
-    def extract_template
+    def extract_template(job_template)
       @template_dir = Dir.mktmpdir("template_dir")
       temp_path = File.join(Dir::tmpdir, "template-#{UUIDTools::UUID.random_create}")
       begin
         File.open(temp_path, "w") do |file|
-          Config.blobstore.get(@job.template.template.blobstore_id, file)
+          Config.blobstore.get(job_template.template.blobstore_id, file)
         end
         `tar -C #{@template_dir} -xzf #{temp_path}`
       ensure
@@ -42,34 +43,44 @@ module Bosh::Director
     end
 
     def hash
-      begin
-        extract_template
-        manifest = YAML.load_file(File.join(@template_dir, "job.MF"))
-        monit_template = ERB.new(File.new(File.join(@template_dir, "monit")).read)
-        monit_template.filename = File.join(@job.template.name, "monit")
-        config_templates = {}
+      digests = {}
+      @job.instances.each do |instance|
+        digests[instance.index] = Digest::SHA1.new
+      end
+      @templates.each do |job_template|
+        begin
+          extract_template(job_template)
+          manifest = YAML.load_file(File.join(@template_dir, "job.MF"))
+          monit_template = ERB.new(File.new(File.join(@template_dir, "monit")).read)
+          monit_template.filename = File.join(job_template.name, "monit")
+          config_templates = {}
 
-        if manifest["templates"]
-          manifest["templates"].each_key do |config_file|
-            config_templates[config_file] = ERB.new(File.new(File.join(@template_dir, "templates", config_file)).read)
+          if manifest["templates"]
+            manifest["templates"].each_key do |config_file|
+              config_templates[config_file] = ERB.new(File.new(File.join(@template_dir, "templates", config_file)).read)
+            end
           end
-        end
 
-        @job.instances.each do |instance|
-          binding_helper = BindingHelper.new(@job.name, instance.index, @job.properties.to_openstruct,
-                                             instance.spec.to_openstruct)
-          digest = Digest::SHA1.new
-          digest << bind_template(monit_template, binding_helper, "monit", instance.index)
-          template_names = config_templates.keys.sort
-          template_names.each do |template_name|
-            template = config_templates[template_name]
-            template.filename = File.join(@job.template.name, template_name)
-            digest << bind_template(template, binding_helper, template_name, instance.index)
+          @job.instances.each do |instance|
+            binding_helper = BindingHelper.new(@job.name, instance.index, @job.properties.to_openstruct,
+                                               instance.spec.to_openstruct)
+            digests[instance.index] << bind_template(monit_template,
+                binding_helper, "monit", instance.index)
+            template_names = config_templates.keys.sort
+            template_names.each do |template_name|
+              template = config_templates[template_name]
+              template.filename = File.join(job_template.name, template_name)
+              digests[instance.index] << bind_template(template, binding_helper,
+                  template_name, instance.index)
+            end
           end
-          instance.configuration_hash = digest.hexdigest
+        ensure
+          FileUtils.rm_rf(@template_dir) if @template_dir
         end
-      ensure
-        FileUtils.rm_rf(@template_dir) if @template_dir
+      end
+      @job.instances.each do |instance|
+        hex = digests[instance.index].hexdigest
+        instance.configuration_hash = hex
       end
     end
 
