c53d63ea1174adac6b0d4045716c1fff348aca6b
Ben Lisbakken <lisbakke@gmail.com>
2012-05-31 11:31:26 -0700

    Job-collocation support in Director.
    It changes the format of the messages sent to be forward and
    backward compatible.
    
    Change-Id: I5eeca092775c38cb859cfc5c159a67b292a847f6

--- director/spec/unit/configuration_hasher_spec.rb
+++ director/spec/unit/configuration_hasher_spec.rb
@@ -17,7 +17,7 @@ describe Bosh::Director::ConfigurationHasher do
     job_spec.stub!(:name).and_return("foo")
     job_spec.stub!(:instances).and_return([instance_spec])
     job_spec.stub!(:properties).and_return({"foo" => "bar"})
-    job_spec.stub!(:template).and_return(template_spec)
+    job_spec.stub!(:templates).and_return([template_spec])
     instance_spec.stub!(:index).and_return(0)
     instance_spec.stub!(:spec).and_return({"test" => "spec"})
 
@@ -35,6 +35,56 @@ describe Bosh::Director::ConfigurationHasher do
     configuration_hasher.hash
   end
 
+  it "should correctly hash a job with two templates and two instances" do
+    template = Bosh::Director::Models::Template.make(:blobstore_id => "b_id")
+    template2 = Bosh::Director::Models::Template.make(:blobstore_id => "b_id2")
+
+    instance_spec = mock("instance_spec")
+    instance_spec2 = mock("instance_spec")
+    template_spec = mock("template_spec", :template => template)
+    template_spec2 = mock("template_spec", :template => template2)
+    job_spec = mock("job_spec")
+    blobstore_client = mock("blobstore_client")
+
+    template_spec.stub!(:blobstore_id).and_return("b_id")
+    template_spec.stub!(:name).and_return("router")
+    template_spec2.stub!(:blobstore_id).and_return("b_id2")
+    template_spec2.stub!(:name).and_return("dashboard")
+    job_spec.stub!(:name).and_return("foo")
+    job_spec.stub!(:instances).and_return([instance_spec, instance_spec2])
+    job_spec.stub!(:properties).and_return({"foo" => "bar"})
+    job_spec.stub!(:templates).and_return([template_spec, template_spec2])
+    instance_spec.stub!(:index).and_return(0)
+    instance_spec.stub!(:spec).and_return({"test" => "spec"})
+    instance_spec2.stub!(:index).and_return(1)
+    instance_spec2.stub!(:spec).and_return({"test" => "spec"})
+
+    template_contents = create_job("foo", "monit file",
+        {"test" => {"destination" => "test_dst",
+            "contents" => "test contents index <%= index %>"}})
+
+    template_contents2 = create_job("foo", "monit file",
+        {"test" => {"destination" => "test_dst",
+            "contents" => "test contents2 <%= index %>"}})
+
+
+    Bosh::Director::Config.stub!(:blobstore).and_return(blobstore_client)
+    blobstore_client.should_receive(:get).with("b_id", an_instance_of(File)).and_return do |_, file|
+      file.write(template_contents)
+    end
+    blobstore_client.should_receive(:get).with("b_id2", an_instance_of(File)).and_return do |_, file|
+      file.write(template_contents2)
+    end
+
+    instance_spec.should_receive(:configuration_hash=).with(
+        "9a01d5eaef2466439cf5f47c817917869bf7382b")
+    instance_spec2.should_receive(:configuration_hash=).with(
+        "1ac87f1ff406553944d7bf1e3dc2ad224d50cc80")
+
+    configuration_hasher = Bosh::Director::ConfigurationHasher.new(job_spec)
+    configuration_hasher.hash
+  end
+
   it "should expose the job context to the templates" do
     template = Bosh::Director::Models::Template.make(:blobstore_id => "b_id")
 
@@ -47,7 +97,7 @@ describe Bosh::Director::ConfigurationHasher do
     job_spec.stub!(:name).and_return("foo")
     job_spec.stub!(:instances).and_return([instance_spec])
     job_spec.stub!(:properties).and_return({"foo" => "bar"})
-    job_spec.stub!(:template).and_return(template_spec)
+    job_spec.stub!(:templates).and_return([template_spec])
     instance_spec.stub!(:index).and_return(0)
     instance_spec.stub!(:spec).and_return({"test" => "spec"})
 
@@ -77,7 +127,7 @@ describe Bosh::Director::ConfigurationHasher do
     job_spec.stub!(:name).and_return("foo")
     job_spec.stub!(:instances).and_return([instance_spec])
     job_spec.stub!(:properties).and_return({"foo" => "bar"})
-    job_spec.stub!(:template).and_return(template_spec)
+    job_spec.stub!(:templates).and_return([template_spec])
     instance_spec.stub!(:index).and_return(0)
     instance_spec.stub!(:spec).and_return({"test" => "spec"})
 
