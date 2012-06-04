da6ddf983a85c85e77d2268a6058092bbf10f7e5
Ferran Rodenas <frodenas@gmail.com>
2012-06-04 02:25:16 +0200

    Fix micro BOSH stemcell build task
    Some fixes to micro BOSH stemcell build task, as package compiler needs --cpi infraestructure name as
    an argument (due to http://reviews.cloudfoundry.org/#/c/5436/):
    - Stemcell micro task: send infraestructure name to 30_micro_bosh.sh
    - 30_micro_bosh: accept infraestructure name and send it to configure_micro_bosh.sh
    - configure_micro_bosh: accept infraestructure name and send it to agent and packacge compiler
    - configure_micro_bosh: add a sleep to wait for agent to start (package compiler doesn't retry when asking the agent)
    - Package compiler: fix unknow CPI message
    - Package compiler: cpi argument is passed as a sym
    
    Change-Id: Ie042f840e6fc3c57511e8fe8bed80a305fc9adb2

--- agent/misc/micro_bosh/lib/configure_micro_bosh.sh
+++ agent/misc/micro_bosh/lib/configure_micro_bosh.sh
@@ -2,6 +2,14 @@
 #
 # Copyright (c) 2009-2012 VMware, Inc.
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: env `basename $0` [infrastructure]"
+  exit 1
+fi
+
+infrastructure=$1
+
 bosh_src_dir=/var/vcap/bosh/src/micro_bosh
 bosh_release_dir=/var/vcap/bosh/src/micro_bosh_release
 bosh_app_dir=/var/vcap
@@ -24,12 +32,15 @@ mkdir -p ${blobstore_path}
 echo "Starting micro bosh compilation"
 
 # Start agent
-/var/vcap/bosh/agent/bin/agent -n ${agent_uri} -s ${blobstore_path} -p local &
+/var/vcap/bosh/agent/bin/agent -I ${infrastructure} -n ${agent_uri} -s ${blobstore_path} -p local &
 agent_pid=$!
 echo "Starting BOSH Agent for compiling micro bosh package, agent pid is $agent_pid"
 
+# Wait for agent to start
+sleep 10
+
 # Start compiler
-/var/vcap/bosh/bin/ruby ${bosh_src_dir}/package_compiler/bin/package_compiler compile ${bosh_release_dir}/release.yml ${bosh_release_dir}/release.tgz ${blobstore_path} ${agent_uri}
+/var/vcap/bosh/bin/ruby ${bosh_src_dir}/package_compiler/bin/package_compiler --cpi ${infrastructure} compile ${bosh_release_dir}/release.yml ${bosh_release_dir}/release.tgz ${blobstore_path} ${agent_uri}
 
 function kill_agent {
   signal=$1
