7ee402a7657eb96702c1fc9f79010362bcd3c511
Martin Englund <martin@englund.nu>
2012-06-04 13:25:39 -0700

    add which() method to common gem
    - to replace a number of similar methods spread through
      the bosh source
    
    Change-Id: Iba2209b46962f356b182a0ed4a42776a588b777f

--- common/lib/common/common.rb
+++ common/lib/common/common.rb
@@ -2,7 +2,7 @@
 
 module Bosh
 
-  # Class for common methods in BOSH
+  # Module for common methods used throughout the BOSH code.
   module Common
 
     # Converts all keys of a [Hash] to symbols. Performs deep conversion.
@@ -17,5 +17,31 @@ module Bosh
     end
 
     module_function :symbolize_keys
+
+    # @overload which(program, path)
+    #   Looks for program in the executables search path (PATH).
+    #   The file must be executable to be found.
+    #   @param [String] program
+    #   @param [String] path search path
+    #   @return [String] full path of the executable,
+    #     or nil if not found
+    # @overload which(programs, path)
+    #   Looks for one of the programs in the executables search path (PATH).
+    #   The file must be executable to be found.
+    #   @param [Array] programs
+    #   @param [String] path search path
+    #   @return [String] full path of the executable,
+    #     or nil if not found
+    def which(programs, path=ENV["PATH"])
+      path.split(File::PATH_SEPARATOR).each do |dir|
+        Array(programs).each do |bin|
+          exe = File.join(dir, bin)
+          return exe if File.executable?(exe)
+        end
+      end
+      nil
+    end
+
+    module_function :which
   end
 end
--- common/spec/unit/common_spec.rb
+++ common/spec/unit/common_spec.rb
@@ -34,4 +34,26 @@ describe Bosh::Common do
     end
   end
 
+  describe "#which" do
+    let(:path) {
+      path = ENV["PATH"]
+      path += ":#{File.expand_path('../../assets', __FILE__)}"
+    }
+
+    it "should return the path when it finds the executable" do
+      Bosh::Common.which("foo1", path).should_not be_nil
+    end
+
+    it "should return the path when it finds an executable" do
+      Bosh::Common.which(%w[foo2 foo1], path).should match(%r{/foo1$})
+    end
+
+    it "should return nil when it isn't executable" do
+      Bosh::Common.which("foo2", path).should be_nil
+    end
+
+    it "should return nil when it doesn't find an executable" do
+      Bosh::Common.which("foo1").should be_nil
+    end
+  end
 end
