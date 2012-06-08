c8ccf56b5deb347389e74523233214c92f13345d
Martin Englund <martin@englund.nu>
2012-06-07 17:31:23 -0700

    bosh common executor
    Change-Id: Idc2f693102600841ee40b62d1da4757c203f28e6

---
+++ common/lib/common/exec/adapters/open4.rb
@@ -0,0 +1,17 @@
+require "open4" unless defined?(::Open4)
+
+module Bosh::Exec::Adapters
+  # execute a command using the open4 gem
+  class Open4
+    def self.sh(command)
+      out = nil
+      err = nil
+      status = ::Open4::popen4(command) do |pid, stdin, stdout, stderr|
+        out = stdout.read.strip
+        err = stderr.read.strip
+      end
+
+      Bosh::Exec::Result.new(command, out, err, status.exitstatus)
+    end
+  end
+end
