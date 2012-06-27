444ee1247dada4d768cc7299f2ed50c7d55aca64
Martin Englund <martin@englund.nu>
2012-06-26 22:23:36 -0700

    aws_cpi & deployer version bump
    Change-Id: Id7a9630f697990530346c6f4315f5707e1b4a6d0

--- aws_cpi/lib/cloud/aws/version.rb
+++ aws_cpi/lib/cloud/aws/version.rb
@@ -2,6 +2,6 @@
 
 module Bosh
   module AwsCloud
-    VERSION = "0.4.1"
+    VERSION = "0.4.2"
   end
 end
--- deployer/lib/deployer/version.rb
+++ deployer/lib/deployer/version.rb
@@ -2,6 +2,6 @@
 
 module Bosh
   module Deployer
-    VERSION = "0.2.2"
+    VERSION = "0.2.3"
   end
 end
