65718f86c9c8559a72f43f58179dc7a1d8e36b4b
Martin Englund <martin@englund.nu>
2012-07-12 13:58:08 -0700

    add cloud_properties to stemcell manifest
    Change-Id: I164514e293313ba46a83cf4cca91399833361e7b

--- agent/misc/stemcell/build2/stages/stemcell/apply.sh
+++ agent/misc/stemcell/build2/stages/stemcell/apply.sh
@@ -9,18 +9,28 @@ source $base_dir/lib/prelude_apply.bash
 
 pushd $work/stemcell
 
+# NOTE: architecture and root_device_name aren't dynamically detected
+# as we don't have a way to persist values across stages, and until we
+# build for multiple architectures or multiple root devices, hardcoding
+# the values is fine.
+# The values are only used in AWS, but still applies to vSphere
 $ruby_bin <<EOS
 require "yaml"
 
 stemcell_name = "$stemcell_name"
 version = "$stemcell_version"
 bosh_protocol = "$bosh_protocol_version".to_i
+stemcell_infrastructure = "$stemcell_infrastructure"
 
 manifest = {
     "name" => stemcell_name,
     "version" => version,
     "bosh_protocol" => bosh_protocol,
-    "cloud_properties" => {}
+    "cloud_properties" => {
+        "infrastructure" => stemcell_infrastructure,
+        "architecture" => "x86_64",
+        "root_device_name" => "/dev/sda1"
+    }
 }
 
 File.open("stemcell.MF", "w") do |f|
