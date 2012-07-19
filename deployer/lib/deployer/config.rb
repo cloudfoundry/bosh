38ca3ab15cebb4ff2f632e9397b6d2491fd4b222
Martin Englund <martin@englund.nu>
2012-07-19 14:16:41 -0700

    micro bosh disk migration
    Change-Id: I0fc878da7cf8a333e6f7ec9940bda8819e1af134

--- deployer/lib/deployer/config.rb
+++ deployer/lib/deployer/config.rb
@@ -10,7 +10,7 @@ module Bosh::Deployer
       include Helpers
 
       attr_accessor :logger, :db, :uuid, :resources, :cloud_options,
-        :spec_properties, :bosh_ip, :env
+        :spec_properties, :bosh_ip, :env, :name
 
       def configure(config)
         plugin = cloud_plugin(config)
@@ -20,6 +20,7 @@ module Bosh::Deployer
         @base_dir = config["dir"]
         FileUtils.mkdir_p(@base_dir)
 
+        @name = config["name"]
         @cloud_options = config["cloud"]
         @net_conf = config["network"]
         @bosh_ip = @net_conf["ip"]
@@ -50,6 +51,7 @@ module Bosh::Deployer
           column :stemcell_name, :text
           column :vm_cid, :text
           column :disk_cid, :text
+          column :disk_size, :integer
         end
 
         Sequel::Model.plugin :validation_helpers
--- deployer/lib/deployer/version.rb
+++ deployer/lib/deployer/version.rb
@@ -2,6 +2,6 @@
 
 module Bosh
   module Deployer
-    VERSION = "0.2.4"
+    VERSION = "0.3.0"
   end
 end
