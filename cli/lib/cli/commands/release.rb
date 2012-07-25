efea33252b6940d760eb0fc1d9e1c9d26579daf9
Oleg Shaldybin <olegs@vmware.com>
2012-07-24 21:55:30 -0700

    Detect inconsistently named jobs/packages
    Raise error if package/job dirname doesn't match name in spec.
    
    Change-Id: I2d64c168820fc7d622a7e13207f33e13bf99459c

--- cli/lib/cli/commands/release.rb
+++ cli/lib/cli/commands/release.rb
@@ -264,7 +264,16 @@ module Bosh::Cli::Command
       end
 
       header("Building packages")
-      Dir[File.join(work_dir, "packages", "*", "spec")].each do |package_spec|
+      Dir[File.join(work_dir, "packages", "*")].each do |package_dir|
+        next unless File.directory?(package_dir)
+        package_dirname = File.basename(package_dir)
+        package_spec = load_yaml_file(File.join(package_dir, "spec"))
+
+        if package_spec["name"] != package_dirname
+          err("Found `#{package_spec["name"]}' package in " +
+              "`#{package_dirname}' directory, please fix it")
+        end
+
         package = Bosh::Cli::PackageBuilder.new(package_spec, work_dir,
                                                 final, release.blobstore)
         package.dry_run = dry_run
@@ -293,9 +302,15 @@ module Bosh::Cli::Command
       header("Building jobs")
       Dir[File.join(work_dir, "jobs", "*")].each do |job_dir|
         next unless File.directory?(job_dir)
-        prepare_script = File.join(job_dir, "prepare")
-        job_spec = File.join(job_dir, "spec")
+        job_dirname = File.basename(job_dir)
+        job_spec = load_yaml_file(File.join(job_dir, "spec"))
 
+        if job_spec["name"] != job_dirname
+          err("Found `#{job_spec["name"]}' job in " +
+              "`#{job_dirname}' directory, please fix it")
+        end
+
+        prepare_script = File.join(job_dir, "prepare")
         if File.exists?(prepare_script)
           say("Found prepare script in `#{File.basename(job_dir)}'")
           Bosh::Cli::JobBuilder.run_prepare_script(prepare_script)
