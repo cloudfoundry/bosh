ef53d6aba988d1d355eb1122d26c08d35acb578b
Ferran Rodenas <frodenas@gmail.com>
2012-07-27 01:57:51 +0200

    Add OpenStack Package Compiler
    Change-Id: Ibcd280db27b2d9ff5efc98a0542908345db73005

--- package_compiler/lib/package_compiler/compiler.rb
+++ package_compiler/lib/package_compiler/compiler.rb
@@ -108,6 +108,8 @@ module Bosh
           spec["networks"] = {"local" => {"ip" => "127.0.0.1"}}
         when "aws"
           spec["networks"] = {"type" => "dynamic"}
+        when "openstack"
+          spec["networks"] = {"type" => "dynamic"}
         else
           puts "WARNING: no CPI specified"
         end
