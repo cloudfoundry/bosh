72c1099c607dbc208a4fbf6994c690c055996401
Ferran Rodenas <frodenas@gmail.com>
2012-07-26 23:51:06 +0200

    Add OpenStack Registry
    Change-Id: Ief7abee95c84d2068ed22390fce8681cda2ed9a7

---
+++ openstack_registry/lib/openstack_registry/api_controller.rb
@@ -0,0 +1,67 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module Bosh::OpenstackRegistry
+
+  class ApiController < Sinatra::Base
+
+    not_found do
+      json(:status => "not_found")
+    end
+
+    error do
+      exception = request.env["sinatra.error"]
+      @logger.error(exception)
+      status(500)
+      json(:status => "error")
+    end
+
+    get "/servers/:server_id/settings" do
+      ip_check = authorized? ? nil : request.ip
+      settings = @server_manager.read_settings(params[:server_id])
+      json(:status => "ok", :settings => settings)
+    end
+
+    put "/servers/:server_id/settings" do
+      protected!
+      @server_manager.update_settings(params[:server_id], request.body.read)
+      json(:status => "ok")
+    end
+
+    delete "/servers/:server_id/settings" do
+      protected!
+      @server_manager.delete_settings(params[:server_id])
+      json(:status => "ok")
+    end
+
+    def initialize
+      super
+      @logger = Bosh::OpenstackRegistry.logger
+
+      @users = Set.new
+      @users << [Bosh::OpenstackRegistry.http_user,
+                 Bosh::OpenstackRegistry.http_password]
+      @server_manager = ServerManager.new
+    end
+
+    def protected!
+      unless authorized?
+        headers("WWW-Authenticate" => 'Basic realm="OpenStack Registry"')
+        halt(401, json("status" => "access_denied"))
+      end
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? &&
+        @auth.basic? &&
+        @auth.credentials &&
+        @users.include?(@auth.credentials)
+    end
+
+    def json(payload)
+      Yajl::Encoder.encode(payload)
+    end
+
+  end
+
+end
---
+++ openstack_registry/spec/unit/runner_spec.rb
@@ -0,0 +1,82 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe Bosh::OpenstackRegistry::Runner do
+
+  def make_runner(config_file)
+    Bosh::OpenstackRegistry::Runner.new(config_file)
+  end
+
+  def write_config(file, config)
+    File.open(file, "w") do |f|
+      YAML.dump(config, f)
+    end
+  end
+
+  describe "initializing" do
+    it "configures OpenStack registry using provided file" do
+      config_file = Tempfile.new("config")
+      config = { "key" => "value" }
+      write_config(config_file.path, config)
+
+      Bosh::OpenstackRegistry.should_receive(:configure).with(config)
+      make_runner(config_file.path)
+    end
+
+    it "fails if config file not found" do
+      expect {
+        make_runner("foo")
+      }.to raise_error(Bosh::OpenstackRegistry::ConfigError, "Cannot find file `foo'")
+    end
+
+    it "fails when config file has incorrect format" do
+      config_file = Tempfile.new("config")
+      config = "foo"
+      write_config(config_file.path, config)
+
+      expect {
+        make_runner(config_file.path)
+      }.to raise_error(Bosh::OpenstackRegistry::ConfigError, /Incorrect file format/)
+    end
+
+    it "fails when some syscall fails" do
+      config_file = Tempfile.new("config")
+      write_config(config_file.path, { "foo" => "bar" })
+
+      YAML.stub!(:load_file).and_raise(SystemCallError.new("baz"))
+
+      expect {
+        make_runner(config_file.path)
+      }.to raise_error(Bosh::OpenstackRegistry::ConfigError, /baz/)
+    end
+  end
+
+  describe "running/stopping" do
+    before(:each) do
+      @config_file = Tempfile.new("config")
+      write_config(@config_file.path, valid_config)
+    end
+
+    it "spins up/shuts down reactor and HTTP server" do
+      runner = make_runner(@config_file)
+      mock_thin = mock("thin")
+
+      EM.should_receive(:run).and_yield
+
+      Thin::Server.should_receive(:new).
+        with("0.0.0.0", 25777, :signals => false).
+        and_return(mock_thin)
+
+      mock_thin.should_receive(:start!)
+
+      runner.run
+
+      mock_thin.should_receive(:stop!)
+      EM.should_receive(:stop)
+
+      runner.stop
+    end
+  end
+
+end
