d974ebc58335f32557ef2bf39498b95ba7069f95
Martin Englund <martin@englund.nu>
2012-07-30 13:02:31 -0700

    blobstore_client changes for S3 read only
    - when S3 is used without access_key_id and
      secret_access_key the blobstore_client returns
      a simple_blobstore_client instead
    - encryption_key is now optional, as it doesn't
      make sense for a public blobstore to require
      an encryption key that is made public
    
    Change-Id: I15e35f61913d7a621b90ca4454e4d3acae959820

--- blobstore_client/lib/blobstore_client.rb
+++ blobstore_client/lib/blobstore_client.rb
@@ -2,6 +2,7 @@
 
 module Bosh; module Blobstore; end; end
 
+require "common/common"
 require "blobstore_client/version"
 require "blobstore_client/errors"
 
@@ -16,6 +17,8 @@ module Bosh
   module Blobstore
     class Client
 
+      S3_ENDPOINT = "https://s3.amazonaws.com"
+
       PROVIDER_MAP = {
         "simple" => SimpleBlobstoreClient,
         "s3" => S3BlobstoreClient,
@@ -23,14 +26,31 @@ module Bosh
         "local" => LocalClient
       }
 
-      def self.create(provider, options = {})
-        p = PROVIDER_MAP[provider]
-        if p
-          p.new(options)
+      def self.create(blobstore_provider, options = {})
+        opts = Bosh::Common.symbolize_keys(options)
+
+        # using S3 without credentials is a special case:
+        # it is really the simple blobstore client with a bucket name
+        if s3_read_only?(blobstore_provider, opts)
+          raise BlobstoreError, "bucket name required" unless opts[:bucket]
+          opts[:endpoint] ||= S3_ENDPOINT
+          provider = SimpleBlobstoreClient
         else
-          providers = PROVIDER_MAP.keys.sort.join(", ")
-          raise "Invalid client provider, available providers are: #{providers}"
+          provider = PROVIDER_MAP[blobstore_provider]
         end
+
+        unless provider
+          providers = PROVIDER_MAP.keys.sort.join(", ")
+          raise BlobstoreError,
+            "Invalid client provider, available providers are: #{providers}"
+        end
+
+        provider.new(opts)
+      end
+
+      def self.s3_read_only?(blobstore_provider, options)
+        blobstore_provider == "s3" &&
+          options[:access_key_id].nil? && options[:secret_access_key].nil?
       end
     end
   end
