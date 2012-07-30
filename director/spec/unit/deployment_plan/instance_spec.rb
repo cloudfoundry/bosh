cloudfoundry/bosh

commit 5287d32784c7fc45e45f285bbe9e527b0d477a28

Job-collocation support in Director.
It changes the format of the messages sent to be forward and
backward compatible.

Change-Id: I5eeca092775c38cb859cfc5c159a67b292a847f6

director/lib/director/deployment_plan/instance.rb
@@ -19,6 +19,9 @@ module Bosh::Director
       # @return [String] SHA1 hash of all of the configuration templates
       attr_accessor :configuration_hash
 
+      # @return [Hash] A hash of template SHA1 hashes.
+      attr_accessor :template_hashes
+
       # @return [Hash<String, NetworkReservation>] network reservations
       attr_accessor :network_reservations
 
@@ -252,7 +255,7 @@ module Bosh::Director
       # @return [Hash<String, Object>] instance spec
       def spec
         deployment_plan = @job.deployment
-        {
+        spec = {
             "deployment" => deployment_plan.name,
             "release" => job.release.spec,
             "job" => job.spec,
@@ -264,6 +267,8 @@ module Bosh::Director
             "configuration_hash" => configuration_hash,
             "properties" => job.properties
         }
+        spec["template_hashes"] = template_hashes unless template_hashes.nil?
+        spec
       end
     end
   end

director/lib/director/deployment_plan/job.rb
@@ -21,7 +21,7 @@ module Bosh::Director
       attr_accessor :persistent_disk # TODO: rename to 'disk_size' (?)
       attr_accessor :resource_pool
       attr_accessor :default_network
-      attr_accessor :template
+      attr_accessor :templates
       attr_accessor :properties
       attr_accessor :packages
       attr_accessor :update
@@ -54,18 +54,36 @@ module Bosh::Director
 
       # @return [Hash] Hash representation
       def spec
+        # TODO(lisbakke): Remove Legacy code when the agent has been updated
+        # to accept templates as an array.
+        first_template = @templates[0]
         result = {
           "name" => @name,
           "release" => @release.name,
-          "template" => @template.template.name,
-          "version" => @template.template.version,
-          "sha1" => @template.template.sha1,
-          "blobstore_id" => @template.template.blobstore_id,
+          "templates" => [],
+          # --- Legacy ---
+          "template" => first_template.template.name,
+          "version" => first_template.template.version,
+          "sha1" => first_template.template.sha1,
+          "blobstore_id" => first_template.template.blobstore_id
         }
+        if first_template.template.logs
+          result["logs"] = template.logs
+        end
+        # --- /Legacy ---
 
-        # TODO: refactor as a part of 'spec vs model' refactoring
-        if @template.template.logs
-          result["logs"] = @template.template.logs
+        @templates.each do |template|
+          template = template.template
+          template_entry = {
+            "name" => template.name,
+            "version" => template.version,
+            "sha1" => template.sha1,
+            "blobstore_id" => template.blobstore_id
+          }
+          if template.logs
+            template_entry["logs"] = template.logs
+          end
+          result["templates"].push(template_entry)
         end
 
         result
@@ -133,8 +151,7 @@ module Bosh::Director
       end
 
       def parse_template
-        template_name = safe_property(@job_spec, "template", :class => String)
-        @template = @release.template(template_name)
+        @templates = @release.template(safe_property(@job_spec, "template"))
       end
 
       def parse_disk

director/lib/director/configuration_hasher.rb
@@ -25,57 +25,79 @@ module Bosh::Director
 
     def initialize(job)
       @job = job
+      @templates = job.templates
       @logger = Config.logger
     end
 
-    def extract_template
+    def extract_template(job_template)
       @template_dir = Dir.mktmpdir("template_dir")
       temp_path = File.join(Dir.tmpdir,
                             "template-#{UUIDTools::UUID.random_create}")
       begin
         File.open(temp_path, "w") do |file|
-          Config.blobstore.get(@job.template.template.blobstore_id, file)
+          Config.blobstore.get(job_template.template.blobstore_id, file)
         end
+        # TODO(lisbakke): Check tar exit code.
         `tar -C #{@template_dir} -xzf #{temp_path}`
       ensure
         FileUtils.rm_f(temp_path)
       end
     end
 
-    def hash
-      begin
-        extract_template
-        manifest = YAML.load_file(File.join(@template_dir, "job.MF"))
+    def process_template(job_template)
+      extract_template(job_template)
+      manifest = YAML.load_file(File.join(@template_dir, "job.MF"))
 
-        monit_template = template_erb("monit")
-        monit_template.filename = File.join(@job.template.name, "monit")
+      monit_template = template_erb("monit")
+      monit_template.filename = File.join(job_template.name, "monit")
+      templates = {}
 
-        templates = {}
-
-        if manifest["templates"]
-          manifest["templates"].each_key do |template_name|
-            template = template_erb(File.join("templates", template_name))
-            templates[template_name] = template
-          end
+      if manifest["templates"]
+        manifest["templates"].each_key do |template_name|
+          template = template_erb(File.join("templates", template_name))
+          templates[template_name] = template
         end
+      end
+      @cached_templates[job_template.name] = {
+        "templates" => templates,
+        "monit_template" => monit_template
+      }
+    ensure
+      FileUtils.rm_rf(@template_dir) if @template_dir
+    end
 
-        @job.instances.each do |instance|
+    def hash
+      @cached_templates = {}
+      sorted_jobs = @templates.sort { |x, y| x.name <=> y.name }
+      sorted_jobs.each do |job_template|
+        process_template(job_template)
+      end
+      @job.instances.each do |instance|
+        instance_digest = Digest::SHA1.new
+        template_digests = {}
+        sorted_jobs.each do |job_template|
+          templates = @cached_templates[job_template.name]["templates"]
+          monit_template =
+              @cached_templates[job_template.name]["monit_template"]
+
           binding_helper = BindingHelper.new(@job.name, instance.index,
                                              @job.properties.to_openstruct,
                                              instance.spec.to_openstruct)
-          digest = Digest::SHA1.new
-          digest << bind_template(monit_template,
-                                  binding_helper, instance.index)
-
+          bound_templates = bind_template(monit_template, binding_helper,
+              instance.index)
           templates.keys.sort.each do |template_name|
             template = templates[template_name]
-            template.filename = File.join(@job.template.name, template_name)
-            digest << bind_template(template, binding_helper, instance.index)
+            template.filename = File.join(job_template.name, template_name)
+            bound_templates << bind_template(template, binding_helper,
+                instance.index)
+            template_digest = Digest::SHA1.new
+            template_digest << bound_templates
+            instance_digest << bound_templates
+            template_digests[job_template.name] = template_digest.hexdigest
           end
-          instance.configuration_hash = digest.hexdigest
         end
-      ensure
-        FileUtils.rm_rf(@template_dir) if @template_dir
+        instance.configuration_hash = instance_digest.hexdigest
+        instance.template_hashes = template_digests
       end
     end
 

director/lib/director/deployment_plan/release.rb
@@ -55,9 +55,20 @@ module Bosh::Director
         }
       end
 
-      # @param [String] name Template name
-      def template(name)
-        @templates[name] ||= TemplateSpec.new(name)
+      # This method takes in a name of a template or an array of template names
+      # and will initialize TemplateSpec classes for each template.  It will
+      # then return an array of these TemplateSec classes.
+      # @param [String|Array] name_or_array Template name or names.
+      # @return [Array] Returns an array of the TemplateSepc classes
+      #     initialized to the name_or_array.
+      def template(name_or_array)
+        template_names = [name_or_array] unless name_or_array.is_a?(Array)
+        templates = []
+        template_names.each do |name|
+          @templates[name] ||= TemplateSpec.new(name)
+          templates.push(@templates[name])
+        end
+        templates
       end
 
       def templates

director/lib/director/deployment_plan_compiler.rb
@@ -343,23 +343,23 @@ module Bosh::Director
           package_name_index[package.name] = package
         end
 
-        release_spec.templates.each do |template_spec|
-          @logger.info("Binding template: #{template_spec.name}")
-          template = template_name_index[template_spec.name]
-          if template.nil?
+        release_spec.templates.each do |template|
+          template_name = template.name
+          @logger.info("Binding template: #{template_name}")
+          actual_template = template_name_index[template_name]
+          if actual_template.nil?
             raise DeploymentUnknownTemplate,
-                  "Can't find template `#{template_spec.name}'"
+                  "Can't find template `#{template_name}'"
           end
-
-          template_spec.template = template
+          template.template = actual_template
 
           packages = []
-          template.package_names.each do |package_name|
+          actual_template.package_names.each do |package_name|
             packages << package_name_index[package_name]
           end
-          template_spec.packages = packages
+          template.packages = packages
 
-          @logger.debug("Bound template: #{template_spec.pretty_inspect}")
+          @logger.debug("Bound template: #{actual_template.pretty_inspect}")
         end
       end
     end
@@ -386,7 +386,9 @@ module Bosh::Director
     end
 
     def bind_configuration
-      @deployment_plan.jobs.each { |job| ConfigurationHasher.new(job).hash }
+      @deployment_plan.jobs.each do |job|
+        ConfigurationHasher.new(job).hash
+      end
     end
 
     def bind_dns

director/lib/director/package_compiler.rb
@@ -303,9 +303,11 @@ module Bosh::Director
 
           @logger.info("Job will be compiled for: " +
                          stemcell_name_version(stemcell))
-
-          job.template.packages.each do |package|
-            schedule_compilation(job, package, stemcell.stemcell)
+          templates = job.templates
+          templates.each do |template|
+            template.packages.each do |package|
+              schedule_compilation(job, package, stemcell.stemcell)
+            end
           end
         end
       end

director/spec/unit/configuration_hasher_spec.rb
@@ -17,7 +17,7 @@ describe Bosh::Director::ConfigurationHasher do
     job_spec.stub!(:name).and_return("foo")
     job_spec.stub!(:instances).and_return([instance_spec])
     job_spec.stub!(:properties).and_return({"foo" => "bar"})
-    job_spec.stub!(:template).and_return(template_spec)
+    job_spec.stub!(:templates).and_return([template_spec])
     instance_spec.stub!(:index).and_return(0)
     instance_spec.stub!(:spec).and_return({"test" => "spec"})
 
@@ -30,6 +30,64 @@ describe Bosh::Director::ConfigurationHasher do
     end
 
     instance_spec.should_receive(:configuration_hash=).with("d4b58a62d2102a315f27bf8c41b4dfef672f785b")
+    instance_spec.should_receive(:template_hashes=).with(
+        {"router"=>"d4b58a62d2102a315f27bf8c41b4dfef672f785b"})
+
+    configuration_hasher = Bosh::Director::ConfigurationHasher.new(job_spec)
+    configuration_hasher.hash
+  end
+
+  it "should correctly hash a job with two templates and two instances" do
+    template = Bosh::Director::Models::Template.make(:blobstore_id => "b_id")
+    template2 = Bosh::Director::Models::Template.make(:blobstore_id => "b_id2")
+
+    instance_spec = mock("instance_spec")
+    instance_spec2 = mock("instance_spec")
+    template_spec = mock("template_spec", :template => template)
+    template_spec2 = mock("template_spec", :template => template2)
+    job_spec = mock("job_spec")
+    blobstore_client = mock("blobstore_client")
+
+    template_spec.stub!(:blobstore_id).and_return("b_id")
+    template_spec.stub!(:name).and_return("router")
+    template_spec2.stub!(:blobstore_id).and_return("b_id2")
+    template_spec2.stub!(:name).and_return("dashboard")
+    job_spec.stub!(:name).and_return("foo")
+    job_spec.stub!(:instances).and_return([instance_spec, instance_spec2])
+    job_spec.stub!(:properties).and_return({"foo" => "bar"})
+    job_spec.stub!(:templates).and_return([template_spec, template_spec2])
+    instance_spec.stub!(:index).and_return(0)
+    instance_spec.stub!(:spec).and_return({"test" => "spec"})
+    instance_spec2.stub!(:index).and_return(1)
+    instance_spec2.stub!(:spec).and_return({"test" => "spec"})
+
+    template_contents = create_job("foo", "monit file",
+        {"test" => {"destination" => "test_dst",
+            "contents" => "test contents index <%= index %>"}})
+
+    template_contents2 = create_job("foo", "monit file",
+        {"test" => {"destination" => "test_dst",
+            "contents" => "test contents2 <%= index %>"}})
+
+
+    Bosh::Director::Config.stub!(:blobstore).and_return(blobstore_client)
+    blobstore_client.should_receive(:get).with("b_id", an_instance_of(File)).and_return do |_, file|
+      file.write(template_contents)
+    end
+    blobstore_client.should_receive(:get).with("b_id2", an_instance_of(File)).and_return do |_, file|
+      file.write(template_contents2)
+    end
+
+    instance_spec.should_receive(:configuration_hash=).with(
+        "9a01d5eaef2466439cf5f47c817917869bf7382b")
+    instance_spec2.should_receive(:configuration_hash=).with(
+        "1ac87f1ff406553944d7bf1e3dc2ad224d50cc80")
+    instance_spec.should_receive(:template_hashes=).with(
+        {"dashboard"=>"b22dc37828aa4596f715a4d1d9a77bc999fb0f68",
+         "router"=>"cdb03dd7e933d087030dc734d7515c8715dfadc0"})
+    instance_spec2.should_receive(:template_hashes=).with(
+        {"dashboard"=>"a06db619abd6eaa32a5ec848894486f162ede0ad",
+         "router"=>"924386b29900dccb55b7a559ce24b9c3c1c9eff0"})
 
     configuration_hasher = Bosh::Director::ConfigurationHasher.new(job_spec)
     configuration_hasher.hash
@@ -47,7 +105,7 @@ describe Bosh::Director::ConfigurationHasher do
     job_spec.stub!(:name).and_return("foo")
     job_spec.stub!(:instances).and_return([instance_spec])
     job_spec.stub!(:properties).and_return({"foo" => "bar"})
-    job_spec.stub!(:template).and_return(template_spec)
+    job_spec.stub!(:templates).and_return([template_spec])
     instance_spec.stub!(:index).and_return(0)
     instance_spec.stub!(:spec).and_return({"test" => "spec"})
 
@@ -60,7 +118,8 @@ describe Bosh::Director::ConfigurationHasher do
     end
 
     instance_spec.should_receive(:configuration_hash=).with("1ec0fb915dd041e4e121ccd1464b88a9aed1ee60")
-
+    instance_spec.should_receive(:template_hashes=).with(
+        {"router"=>"1ec0fb915dd041e4e121ccd1464b88a9aed1ee60"})
     configuration_hasher = Bosh::Director::ConfigurationHasher.new(job_spec)
     configuration_hasher.hash
   end
@@ -77,7 +136,7 @@ describe Bosh::Director::ConfigurationHasher do
     job_spec.stub!(:name).and_return("foo")
     job_spec.stub!(:instances).and_return([instance_spec])
     job_spec.stub!(:properties).and_return({"foo" => "bar"})
-    job_spec.stub!(:template).and_return(template_spec)
+    job_spec.stub!(:templates).and_return([template_spec])
     instance_spec.stub!(:index).and_return(0)
     instance_spec.stub!(:spec).and_return({"test" => "spec"})
 

director/spec/unit/deployment_plan_compiler_spec.rb
@@ -675,6 +675,7 @@ describe Bosh::Director::DeploymentPlanCompiler do
 
   describe :bind_configuration do
     before(:each) do
+      @template_spec = stub(:TemplateSpec)
       @job_spec = stub(:JobSpec)
       @deployment_plan.stub(:jobs).and_return([@job_spec])
     end

commit 9c115f123021c497b81ff415ccb93d3932d03cd0

Fix Director job update comparison regression.
There was a regression in commit:
5287d32784c7fc45e45f285bbe9e527b0d477a28

It changed the job spec for the director but didn't account for the
job_changed? method in instance.rb.  This change makes it so that
job_changed? will do a smart comparison of job specs.

Change-Id: I88f3ad13ac73f6d57252281aef46a60adb74fccf

director/lib/director/deployment_plan/instance.rb
@@ -242,7 +242,14 @@ module Bosh::Director
       # @return [Boolean] returns true if the expected job configuration differs
       #   from the one provided by the VM
       def job_changed?
-        @job.spec != @current_state["job"]
+        job_spec = @job.spec
+        if job_spec != @current_state["job"]
+          # The agent job spec could be in legacy form.  job_spec cannot be,
+          # though, because we got it from the spec function in job.rb which
+          # automatically makes it legacy.
+          return job_spec == Job.convert_from_legacy_spec(@current_state["job"])
+        end
+        return false
       end
 
       ##

director/lib/director/deployment_plan/job.rb
@@ -87,6 +87,28 @@ module Bosh::Director
         @unneeded_instances = []
       end
 
+      def self.is_legacy_spec?(job_spec)
+        job_spec.has_key?("templates")
+      end
+
+      # Takes in a job spec and returns a job spec in the new format, if it
+      # needs to be modified.  The new format has "templates" key, which is an
+      # array with each template's data.  This is used for job collocation,
+      # specifically for the agent's current job spec when compared to the
+      # director's.  We only convert their template to a single array entry
+      # because it should be impossible for the agent to have a job spec with
+      # multiple templates in legacy form.
+      def self.convert_from_legacy_spec(job_spec)
+        return job_spec if self.is_legacy_spec?(job_spec)
+        template = {
+          "name" => job_spec["template"],
+          "version" => job_spec["version"],
+          "sha1" => job_spec["sha1"],
+          "blobstore_id" => job_spec["blobstore_id"]
+        }
+        job_spec["templates"] = [template]
+      end
+
       # Returns job spec as a Hash. To be used by all instances of the job to
       # populate agent state.
       # @return [Hash] Hash representation

director/spec/unit/deployment_plan/instance_spec.rb
@@ -166,4 +166,43 @@ describe Bosh::Director::DeploymentPlan::Instance do
       }.to raise_error(BD::InstanceTargetStateUndefined)
     end
   end
+  describe "updating deployment" do
+    it "needs to smartly compare specs before deciding to update a job" do
+      @deployment = make_deployment("mycloud")
+      BD::DeploymentPlan::Job.any_instance.stub(:initialize)
+      @plan = mock(BD::DeploymentPlan, :model => @deployment)
+      @job = BD::DeploymentPlan::Job.new(@plan)
+
+      @job.release = mock(BD::DeploymentPlan::Release)
+      @job.release.should_receive(:name).twice.and_return("hbase-release")
+
+      mock_template = mock(BD::DeploymentPlan::Template)
+      mock_template.should_receive(:name).exactly(4).times.and_return(
+        "hbase_slave")
+      mock_template.should_receive(:version).exactly(4).times.and_return("2")
+      mock_template.should_receive(:sha1).exactly(4).times.and_return(
+        "24aeaf29768a100d500615dc02ae6126e019f99f")
+      mock_template.should_receive(:blobstore_id).exactly(4).times.and_return(
+        "4ec237cb-5f07-4658-aabe-787c82f39c76")
+      mock_template.should_receive(:logs).exactly(4).times
+
+      @job.templates = [mock_template]
+      @job.should_receive(:instance_state).and_return("some_state")
+      instance = make(@job, 0)
+      @job.stub!(:name).and_return("dea")
+      instance.current_state = {
+        "job" => {
+          "name" => "hbase_slave",
+          "release" => "hbase-release",
+          "template" => "hbase_slave",
+          "version" => "0.9-dev",
+          "sha1" => "a8ab636b7c340f98891178096a44c09487194f03",
+          "blobstore_id" => "e2e4e58e-a40e-43ec-bac5-fc50457d5563"
+        }
+      }
+      instance.job_changed?.should == false
+      # Check that the old way of comparing would say that the job has changed.
+      (@job.spec == instance.current_state).should == false
+    end
+  end
 end