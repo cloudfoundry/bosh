8379d3b4f07a562a3f267850a9a6495d662e38eb
Ferran Rodenas <frodenas@gmail.com>
2012-08-16 01:05:06 +0200

    OpenStack Stemcell Builder: extract and store kernel and ramdisk images
    Change-Id: I098f685dce7dd82f73c4230feac2d450de6a7c79

--- agent/misc/stemcell/build2/stages/stemcell_openstack/apply.sh
+++ agent/misc/stemcell/build2/stages/stemcell_openstack/apply.sh
@@ -7,6 +7,23 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
+kernel_image_name=kernel.img
+ramdisk_image_name=initrd.img
+
+pushd $work
+
+if [ ! -e "${kernel_image_name:-}" ]
+then
+  kernel_image_name=
+fi
+
+if [ ! -e "${ramdisk_image_name:-}" ]
+then
+  ramdisk_image_name=
+fi
+
+popd
+
 pushd $work/stemcell
 
 $ruby_bin <<EOS
@@ -16,6 +33,8 @@ stemcell_name = "$stemcell_name"
 version = "$stemcell_version"
 bosh_protocol = "$bosh_protocol_version".to_i
 stemcell_infrastructure = "$stemcell_infrastructure"
+kernel_file = "$kernel_image_name"
+ramdisk_file = "$ramdisk_image_name"
 
 manifest = {
     "name" => stemcell_name,
@@ -27,6 +46,14 @@ manifest = {
         "container_format" => "ami"
     }
 }
+
+unless (kernel_file.nil? || kernel_file.empty?)
+    manifest["cloud_properties"]["kernel_file"] = kernel_file
+end
+
+unless (ramdisk_file.nil? || ramdisk_file.empty?)
+    manifest["cloud_properties"]["ramdisk_file"] = ramdisk_file
+end
 
 File.open("stemcell.MF", "w") do |f|
   f.write(YAML.dump(manifest))
