6a12699d795dac60bc1be72ea0e8be7268728518
Oleg Shaldybin <olegs@vmware.com>
2012-09-04 23:54:43 -0700

    Common property code now lives in bosh_common
    Change-Id: If2fe7a6e5c4e5f78d03a5a0848884247eaf9ad92

---
+++ common/lib/common/properties.rb
@@ -0,0 +1,6 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+require "ostruct"
+require "common/properties/errors"
+require "common/properties/property_helper"
+require "common/properties/template_evaluation_context"
---
+++ common/lib/common/properties/property_helper.rb
@@ -0,0 +1,43 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module Bosh::Common
+  module PropertyHelper
+    # Copies property with a given name from src to dst.
+    # @param [Hash] dst Property destination
+    # @param [Hash] src Property source
+    # @param [String] name Property name (dot-separated)
+    # @param [Object] default Default value (if property is not in src)
+    def copy_property(dst, src, name, default = nil)
+      keys = name.split(".")
+      src_ref = src
+      dst_ref = dst
+
+      keys.each do |key|
+        src_ref = src_ref[key]
+        break if src_ref.nil? # no property with this name is src
+      end
+
+      keys[0..-2].each do |key|
+        dst_ref[key] ||= {}
+        dst_ref = dst_ref[key]
+      end
+
+      dst_ref[keys[-1]] ||= {}
+      dst_ref[keys[-1]] = src_ref || default
+    end
+
+    # @param [Hash] collection Property collection
+    # @param [String] name Dot-separated property name
+    def lookup_property(collection, name)
+      keys = name.split(".")
+      ref = collection
+
+      keys.each do |key|
+        ref = ref[key]
+        return nil if ref.nil?
+      end
+
+      ref
+    end
+  end
+end
