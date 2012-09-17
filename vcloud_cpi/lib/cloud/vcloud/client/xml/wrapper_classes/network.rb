d29a78c59f07a5cc6bfec32a8cd4e4251edf5a22
anupam <anupam@vmware.com>
2012-09-17 14:28:35 -0700

    initial commit of vcloud_cpi
    vcloud_cpi unit tests pass.
    Verified that BOSH can use the gem to create a Cloud Foundry instance on a vCloud Director infrastructure cloud.
    updating vcloud_cpi dependencies
    
    Change-Id: Iacdc6fcc9b1694fad83813c302e69854a2fc73da

---
+++ vcloud_cpi/lib/cloud/vcloud/client/xml/wrapper_classes/network.rb
@@ -0,0 +1,17 @@
+module VCloudCloud
+  module Client
+    module Xml
+      class Network < Wrapper
+
+        def name
+          @root['name']
+        end
+
+        def name=(value)
+          @root['name'] = value
+        end
+
+      end
+    end
+  end
+end
---
+++ vcloud_cpi/lib/cloud/vcloud/version.rb
@@ -0,0 +1,7 @@
+module Bosh
+  module Clouds
+    class VCloud
+      VERSION = "0.4.8"
+    end
+  end
+end
