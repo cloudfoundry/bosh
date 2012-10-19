f4c55785d749ef3e657d00208fde38c56f0931d2
Ferran Rodenas <frodenas@gmail.com>
2012-10-19 19:28:01 +0200

    CF-156: Fix bosh plugins when more than 1 plugin version is found
    Fix the way bosh plugins are discovered. Now it looks for the latest
    gem version.
    
    Change-Id: Ibdc0a11b22c73d8a353c85d2fa91327cae1512f4

--- cli/lib/cli/runner.rb
+++ cli/lib/cli/runner.rb
@@ -147,19 +147,17 @@ module Bosh::Cli
     def load_plugins
       plugins_glob = "bosh/cli/commands/*.rb"
 
-      unless Gem.respond_to?(:find_files)
+      unless Gem::Specification.respond_to?(:latest_specs) ||
+             Gem::Specification.respond_to?(:matches_for_glob)
         say("Cannot load plugins, ".yellow +
             "please run `gem update --system' to ".yellow +
             "update your RubyGems".yellow)
         return
       end
 
-      plugins = begin
-        Gem.find_files(plugins_glob, true)
-      rescue ArgumentError
-        # Handling rubygems compatibility issue
-        Gem.find_files(plugins_glob)
-      end
+      plugins = Gem::Specification.latest_specs(true).map do |spec|
+        spec.matches_for_glob(plugins_glob)
+      end.flatten
 
       plugins.each do |plugin|
         n_commands = Config.commands.size
