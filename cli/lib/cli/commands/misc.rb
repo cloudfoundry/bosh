39f27f5356d8c9c1b5d36d0319f20401b1a2e642
Martin Englund <martin@englund.nu>
2012-11-05 15:32:48 -0800

    add dns & cpi to status output
    Change-Id: Ie20ff2a02e6f1eae784cf00cb2860284d21d9231

--- cli/lib/cli/commands/misc.rb
+++ cli/lib/cli/commands/misc.rb
@@ -15,6 +15,9 @@ module Bosh::Cli::Command
     usage "status"
     desc  "Show current status (current target, user, deployment info etc)"
     def status
+      cpi = nil
+      dns = nil
+
       if config.target
         say("Updating director data...", " ")
 
@@ -26,6 +29,8 @@ module Bosh::Cli::Command
             config.target_name = status["name"]
             config.target_version = status["version"]
             config.target_uuid = status["uuid"]
+            cpi = status["cpi"]
+            dns = format_dns(status["dns"])
             config.save
             say("done".green)
           end
@@ -46,6 +51,8 @@ module Bosh::Cli::Command
         print_value("Version", config.target_version)
         print_value("User", username, "not logged in")
         print_value("UUID", config.target_uuid)
+        print_value("CPI", cpi, "n/a (update director)")
+        print_value("DNS", dns)
       end
 
       nl
@@ -313,5 +320,14 @@ module Bosh::Cli::Command
       say(t) unless t.rows.empty?
     end
 
+    def format_dns(value)
+      if value.nil?
+        "n/a (outdated director)"
+      elsif value
+        "enabled"
+      else
+        "disabled"
+      end
+    end
   end
 end
