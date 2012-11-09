ef95cd38dc7ac3580a0d20e809eca1f79b93ba87
Vadim Spivak <vspivak@vmware.com>
2012-11-09 12:04:46 -0800

    Reduce bosh vms timeout from 30 seconds to 5.
    Change-Id: Iebef52d589058b357337bd0db52c627f26735b0a

--- director/spec/unit/jobs/vm_state_spec.rb
+++ director/spec/unit/jobs/vm_state_spec.rb
@@ -5,15 +5,16 @@ require File.expand_path("../../../spec_helper", __FILE__)
 describe Bosh::Director::Jobs::VmState do
 
   before(:each) do
-    @deployment = Bosh::Director::Models::Deployment.make
+    @deployment = BD::Models::Deployment.make
     @result_file = mock("result_file")
-    Bosh::Director::Config.stub!(:result).and_return(@result_file)
+    BD::Config.stub!(:result).and_return(@result_file)
   end
 
   it "parses agent info into vm_state" do
-    vm = Bosh::Director::Models::Vm.make(:deployment => @deployment, :agent_id => "agent-1", :cid => "vm-1")
+    BDM::Vm.make(:deployment => @deployment,
+                 :agent_id => "agent-1", :cid => "vm-1")
     agent = mock("agent")
-    Bosh::Director::AgentClient.stub!(:new).with("agent-1").and_return(agent)
+    BD::AgentClient.stub!(:new).with("agent-1", :timeout => 5).and_return(agent)
     agent_state = { "vm_cid" => "vm-1",
                     "networks" => {"test" => {"ip" => "1.1.1.1"}},
                     "agent_id" => "agent-1",
@@ -30,15 +31,16 @@ describe Bosh::Director::Jobs::VmState do
       status["resource_pool"].should == "test_resource_pool"
     end
 
-    job = Bosh::Director::Jobs::VmState.new(@deployment.id)
+    job = BD::Jobs::VmState.new(@deployment.id)
     job.perform
   end
 
   it "should handle unresponsive agents" do
-    vm = Bosh::Director::Models::Vm.make(:deployment => @deployment, :agent_id => "agent-1", :cid => "vm-1")
+    BDM::Vm.make(:deployment => @deployment, :agent_id => "agent-1",
+                 :cid => "vm-1")
     agent = mock("agent")
-    Bosh::Director::AgentClient.stub!(:new).with("agent-1").and_return(agent)
-    agent.should_receive(:get_state).and_raise(Bosh::Director::RpcTimeout)
+    BD::AgentClient.stub!(:new).with("agent-1", :timeout => 5).and_return(agent)
+    agent.should_receive(:get_state).and_raise(BD::RpcTimeout)
 
     @result_file.should_receive(:write).with do |agent_status|
       status = JSON.parse(agent_status)
@@ -47,7 +49,7 @@ describe Bosh::Director::Jobs::VmState do
       status["job_state"].should == "unresponsive agent"
     end
 
-    job = Bosh::Director::Jobs::VmState.new(@deployment.id)
+    job = BD::Jobs::VmState.new(@deployment.id)
     job.perform
   end
 end
