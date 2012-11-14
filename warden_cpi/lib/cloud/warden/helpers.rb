6decb479ce48333d19cd1178bcbc6b4d3213490e
Anfernee Gui <agui@vmware.com>
2012-11-14 16:12:01 +0800

    Support create/delete stemcell in warden CPI
    Change-Id: I9becc3d9c7a961a5a0b37d9c05c94b494af95d31

--- warden_cpi/lib/cloud/warden/helpers.rb
+++ warden_cpi/lib/cloud/warden/helpers.rb
@@ -1,4 +1,15 @@
 module Bosh::WardenCloud
+
   module Helpers
+
+    def cloud_error(error)
+      unless error.instance_of? Bosh::Clouds::CloudError
+        error = Bosh::Clouds::CloudError.new error
+      end
+
+      @logger.error(error.message) if @logger
+      raise error
+    end
+
   end
 end
--- warden_cpi/spec/spec_helper.rb
+++ warden_cpi/spec/spec_helper.rb
@@ -1,10 +1,14 @@
-require "cloud"
+require "rspec"
+require "logger"
+require "tmpdir"
 
+require "cloud"
 require "cloud/warden"
 
 def cloud_options
   {
     "warden" => warden_options,
+    "stemcell" => stemcell_options,
   }
 end
 
@@ -13,3 +17,19 @@ def warden_options
     "unix_domain_socket" => "/tmp/warden.sock",
   }
 end
+
+def stemcell_options
+  {
+    "root" => "/var/vcap/stemcell",
+  }
+end
+
+module Bosh::Clouds
+  class Config
+    class << self
+      attr_accessor :logger
+    end
+  end
+end
+
+Bosh::Clouds::Config.logger = Logger.new("/dev/null")
