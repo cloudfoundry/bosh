d707adef47637e2fa05316c8179941815e9ca63f
Martin Englund <martin@englund.nu>
2012-11-28 10:44:18 -0800

    extract settings into its own class
    - also persist settings on mount_disk
    
    Change-Id: I27d9426b264eaaf8db15a5adf5a068f6408821d6

---
+++ agent/lib/agent/settings.rb
@@ -0,0 +1,60 @@
+module Bosh::Agent
+  class Settings
+
+    # Loads settings specific to the current infrastructure, and then caches
+    # the result to disk. If it can't fetch settings, it will fall back to
+    # previously cached settings
+    def self.load(cache_path=nil)
+      settings = new(cache_path || Config.settings_file)
+      settings.load
+      settings.cache
+      settings
+    end
+
+    def initialize(file)
+      @settings = {}
+      @cache_file = file
+    end
+
+    def load
+      load_from_infrastructure
+      cache
+    rescue LoadSettingsError
+      load_from_cache
+    end
+
+    def cache
+      json = Yajl::Encoder.encode(@settings)
+      File.open(@cache_file, 'w') do |file|
+        file.write(json)
+      end
+    end
+
+    def [](key)
+      @settings[key]
+    end
+
+    private
+
+    def load_from_infrastructure
+      @settings = Bosh::Agent::Config.infrastructure.load_settings
+      logger.info("loaded new infrastructure settings: #{@settings.inspect}")
+    end
+
+    def load_from_cache
+      json = File.read(@cache_file)
+      # perhaps catch json parser errors too and raise as LoadSettingsSerror?
+      @settings = Yajl::Parser.new.parse(json)
+      logger.info("failed to load infrastructre settings, " \
+                  "falling back to cached settings: #{@settings.inspect}")
+    rescue Errno::ENOENT
+      raise LoadSettingsError, "could neither load infrastructure settings " \
+        "nor cached settings from: #@cache_file"
+    end
+
+    def logger
+      Bosh::Agent::Config.logger
+    end
+
+  end
+end
