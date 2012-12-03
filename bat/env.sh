29c1fdcd1784d8631207ca33d8955d24441863d7
Glenn Oppegard <oppegard@gmail.com>
2012-12-03 13:01:47 -0800

    remove focus tag from property_spec.rb
    Change-Id: Ic6b3d00d496bef33bb316a87d136a75da2e62173

---
+++ bat/env.sh
@@ -0,0 +1,7 @@
+export BAT_DIRECTOR=10.42.49.83
+export BAT_VCAP_PASSWORD='c1oudc0w'
+export BAT_RELEASE_DIR=$HOME/projects/cf/bat-release
+export BAT_STEMCELL=$HOME/stemcells/bosh-stemcell-vsphere-0.6.4.tgz
+export BAT_DEPLOYMENT_SPEC=$HOME/projects/ac/deployments/dev109/bat.yml
+export BAT_DEBUG=true
+export BAT_FAST=true
