b57063d1e7cc8112362449e680fe3021f34471ad
Anfernee Gui <agui@vmware.com>
2012-12-06 17:08:16 +0800

    Fix warden agent so that Bosh CLI doesn't complain
    Bosh CLI thinks the stemcell without an image file is an invalid
    stemcell, so we workaround by touch an image file.
    
    Change-Id: I53cfd382c2b5cab7d08028ba61064c1521b8e5e0

--- agent/misc/stemcell/build2/stages/bosh_copy_root/apply.sh
+++ agent/misc/stemcell/build2/stages/bosh_copy_root/apply.sh
@@ -7,5 +7,9 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
+# Workaround for Bosh CLI validation. Bosh CLI thinks all the stemcell without
+# an image is an invalid stemcell
+touch $work/stemcell/image
+
 # Copy root
 time rsync -aHA $chroot/* $work/stemcell/root
