a466e5c09fa9fc03b4d88ea9752f7c5abf64c4f8
Anfernee Gui <agui@vmware.com>
2012-12-13 15:03:20 +0800

    Warden CPI should save env when attach/detach disk
    Change-Id: I26e3c0da0a7dbd0f778e05c6bd0c1519c0ead31e

--- warden_cpi/lib/cloud/warden/helpers.rb
+++ warden_cpi/lib/cloud/warden/helpers.rb
@@ -28,11 +28,22 @@ module Bosh::WardenCloud
       Bosh::Exec.sh "#{cmd}"
     end
 
+    ##
+    # This method generates a script that is run inside a container, to get an
+    # available device path.
+    #
+    # e.g.
+    # Device path is like /dev/sda
+    # Partition path is like /dev/sda1
     def attach_script(device_number, device_prefix)
       script = <<-EOF
-for i in `seq 256`; do (stat #{device_prefix}$i > /dev/null 2>&1) || break; done
-mknod #{device_prefix}$i b 7 #{device_number} > /dev/null 2>&1 && echo "#{device_prefix}$i"
+for i in a b c d e f g h; do (stat #{device_prefix}${i}1 > /dev/null 2>&1) || break; done
+mknod #{device_prefix}${i}1 b 7 #{device_number} > /dev/null 2>&1 && echo "#{device_prefix}${i}"
       EOF
+    end
+
+    def partition_path(device_path)
+      "#{device_path}1"
     end
 
   end
