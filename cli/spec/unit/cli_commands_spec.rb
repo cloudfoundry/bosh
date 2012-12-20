5f66dce7c127fc6e5ca291ad2c40d7476b705271
Glenn Oppegard <oppegard@gmail.com>
2012-12-19 17:27:04 -0800

    speed up `bosh deployments` in cli
    If target is a new director, don't download manifest for each
    deployment to get release and stemcell info. If target is an
    old director, fall back to downloading manifest (slow).
    
    Change-Id: I09e961a7d221e028e9ac0438996a5a784e3e54c4

--- cli/spec/unit/cli_commands_spec.rb
+++ cli/spec/unit/cli_commands_spec.rb
@@ -140,34 +140,46 @@ describe Bosh::Cli::Command::Base do
 
   describe Bosh::Cli::Command::Deployment do
     before :each do
+      @director = mock(Bosh::Cli::Director)
+
       @cmd = Bosh::Cli::Command::Deployment.new
       @cmd.add_option(:non_interactive, true)
-    end
-
-    it "allows deleting the deployment" do
-      mock_director = mock(Bosh::Cli::Director)
-      mock_director.should_receive(:delete_deployment).
-          with("foo", :force => false)
-
-      @cmd.stub!(:interactive?).and_return(false)
       @cmd.stub!(:target).and_return("test")
       @cmd.stub!(:username).and_return("user")
       @cmd.stub!(:password).and_return("pass")
-      @cmd.stub!(:director).and_return(mock_director)
+      @cmd.stub!(:director).and_return(@director)
+    end
+
+    it "allows deleting the deployment" do
+      @director.should_receive(:delete_deployment).
+        with("foo", :force => false)
+
+      @cmd.stub!(:interactive?).and_return(false)
       @cmd.delete("foo")
     end
 
     it "needs confirmation to delete deployment" do
-      mock_director = mock(Bosh::Cli::Director)
-      mock_director.should_not_receive(:delete_deployment)
+      @director.should_not_receive(:delete_deployment)
 
       @cmd.remove_option(:non_interactive)
-      @cmd.stub!(:target).and_return("test")
-      @cmd.stub!(:username).and_return("user")
-      @cmd.stub!(:password).and_return("pass")
-      @cmd.stub!(:director).and_return(mock_director)
       @cmd.stub!(:ask).and_return("")
       @cmd.delete("foo")
+    end
+
+    it "lists deployments and doesn't fetch manifest on new director" do
+      @director.should_receive(:list_deployments).
+        and_return([{ "name" => "foo", "releases" => [], "stemcells" => [] }])
+      @director.should_not_receive(:get_deployment)
+
+      @cmd.list
+    end
+
+    it "lists deployments and fetches manifest on old director" do
+      @director.should_receive(:list_deployments).
+        and_return([{ "name" => "foo" }])
+      @director.should_receive(:get_deployment).with("foo").and_return({})
+
+      @cmd.list
     end
   end
 
