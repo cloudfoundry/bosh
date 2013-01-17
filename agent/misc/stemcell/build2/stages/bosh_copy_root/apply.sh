4b0dfbba7c3559c67517ed44c144d07e0088a345
Anfernee Gui <agui@vmware.com>
2013-01-18 07:57:07 +0800

    Make /tmp rw-able to all in warden
    Change-Id: I5561586c9236a2084cc7a582115a5673b7ffd467

--- agent/misc/stemcell/build2/stages/bosh_copy_root/apply.sh
+++ agent/misc/stemcell/build2/stages/bosh_copy_root/apply.sh
@@ -7,6 +7,9 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
+# This only applies for warden container
+chmod 777 $chroot/tmp
+
 pushd $chroot
 tar zcvf $work/stemcell/image .
 popd
