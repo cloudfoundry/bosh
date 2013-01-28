a6a808406547eb46f30a62006ab6ea194fcfd5cc
Glenn Oppegard and Pieter Noordhuis <pair+glenn+pieter@pivotallabs.com>
2013-01-28 10:23:40 -0800

    Fix up functional tests for S3

--- blobstore_client/spec/functional/s3_spec.rb
+++ blobstore_client/spec/functional/s3_spec.rb
@@ -6,50 +6,59 @@ describe Bosh::Blobstore::S3BlobstoreClient, :s3_credentials => true do
   EXISTING_BLOB_ID = "d704cb05d9c6af7e74188f4a858f33de"
 
   def access_key_id
-    key = ENV['EC2_ACCESS_KEY']
-    raise "need to set EC2_ACCESS_KEY" unless key
+    key = ENV['AWS_ACCESS_KEY_ID']
+    raise "need to set AWS_ACCESS_KEY_ID environment variable" unless key
     key
   end
 
   def secret_access_key
-    key = ENV['EC2_SECRET_KEY']
-    raise "need to set EC2_SECRET_KEY" unless key
+    key = ENV['AWS_SECRET_ACCESS_KEY']
+    raise "need to set AWS_SECRET_ACCESS_KEY environment variable" unless key
+    key
+  end
+
+  def bucket_name
+    key = ENV['S3_BUCKET_NAME']
+    raise "need to set S3_BUCKET_NAME environment variable" unless key
     key
   end
 
   context "Read/Write" do
-    after(:each) do
-      @s3.delete(@oid) if @oid
-      @s3.delete(@oid2) if @oid2
+    let(:s3_options) do
+      {
+        :bucket_name => bucket_name,
+        :access_key_id => access_key_id,
+        :secret_access_key => secret_access_key,
+      }
     end
 
-    before(:all) do
-      s3_options = {
-          :bucket_name => "bosh-blobstore-bucket",
-          :access_key_id => access_key_id,
-          :secret_access_key => secret_access_key
-      }
-      @s3 = Bosh::Blobstore::Client.create("s3", s3_options)
+    let(:s3) do
+      Bosh::Blobstore::Client.create("s3", s3_options)
+    end
+
+    after(:each) do
+      s3.delete(@oid) if @oid
+      s3.delete(@oid2) if @oid2
     end
 
     describe "unencrypted" do
       describe "store object" do
         it "should upload a file" do
           Tempfile.new("foo") do |file|
-            @oid = @s3.create(file)
+            @oid = s3.create(file)
             @oid.should_not be_nil
           end
         end
 
         it "should upload a string" do
-          @oid = @s3.create("foobar")
+          @oid = s3.create("foobar")
           @oid.should_not be_nil
         end
 
         it "should handle uploading the same object twice" do
-          @oid = @s3.create("foobar")
+          @oid = s3.create("foobar")
           @oid.should_not be_nil
-          @oid2 = @s3.create("foobar")
+          @oid2 = s3.create("foobar")
           @oid2.should_not be_nil
           @oid.should_not == @oid2
         end
@@ -57,95 +66,96 @@ describe Bosh::Blobstore::S3BlobstoreClient, :s3_credentials => true do
 
       describe "get object" do
         it "should save to a file" do
-          @oid = @s3.create("foobar")
+          @oid = s3.create("foobar")
           file = Tempfile.new("contents")
-          @s3.get(@oid, file)
+          s3.get(@oid, file)
           file.rewind
           file.read.should == "foobar"
         end
 
         it "should return the contents" do
-          @oid = @s3.create("foobar")
-          @s3.get(@oid).should == "foobar"
+          @oid = s3.create("foobar")
+          s3.get(@oid).should == "foobar"
         end
 
         it "should raise an error when the object is missing" do
           id = "foooooo"
           expect {
-            @s3.get(id)
+            s3.get(id)
           }.to raise_error Bosh::Blobstore::BlobstoreError, "S3 object '#{id}' not found"
         end
       end
 
       describe "delete object" do
         it "should delete an object" do
-          @oid = @s3.create("foobar")
+          @oid = s3.create("foobar")
           expect {
-            @s3.delete(@oid)
+            s3.delete(@oid)
           }.to_not raise_error
           @oid = nil
         end
 
         it "should raise an error when object doesn't exist" do
           expect {
-            @s3.delete("foobar")
+            s3.delete("foobar")
           }.to raise_error Bosh::Blobstore::BlobstoreError
         end
       end
     end
 
-    context "encrypted", :focus => true do
-      before(:all) do
-        s3_options = {
-            :bucket_name => "bosh-blobstore-bucket",
-            :access_key_id => access_key_id,
-            :secret_access_key => secret_access_key,
-            :encryption_key => "kjahsdjahsgdlahs"
-        }
-        @s3 = Bosh::Blobstore::Client.create("s3", s3_options)
+    context "encrypted" do
+      let(:unencrypted_s3) do
+        s3
       end
 
-      describe "get object" do
-        it "should be backwards compatible with blobstore_client 0.5.0 blobs" do
-          oid = "c45a6478-fce0-4f74-b68c-f465111fe2f3"
-          @s3.get(oid).should == "skadim vadar"
+      let(:encrypted_s3) do
+        Bosh::Blobstore::Client.create("s3", s3_options.merge(:encryption_key => "kjahsdjahsgdlahs"))
+      end
+
+      describe "backwards compatibility" do
+        it "should work when a blob was uploaded by blobstore_client 0.5.0" do
+          @oid = unencrypted_s3.create(File.read(asset("encrypted_blob_from_blobstore_client_0.5.0")))
+          encrypted_s3.get(@oid).should == "skadim vadar"
         end
       end
 
       describe "create object" do
         it "should be encrypted" do
-          @oid = @s3.create("foobar")
-          @s3.get(@oid).should == "foobar"
+          @oid = encrypted_s3.create("foobar")
+          encrypted_s3.get(@oid).should == "foobar"
         end
       end
     end
   end
 
   context "Read-Only" do
-    before(:all) do
-      s3_options = {
-          :endpoint => "https://s3-us-west-1.amazonaws.com",
-          :bucket_name => "bosh-blobstore-bucket"
+    let(:s3_options) do
+      {
+        :endpoint => "https://s3-us-west-1.amazonaws.com",
+        :bucket_name => "bosh-blobstore-bucket"
       }
-      @s3 = Bosh::Blobstore::Client.create("s3", s3_options)
+    end
+
+    let(:s3) do
+      Bosh::Blobstore::Client.create("s3", s3_options)
     end
 
     describe "get object" do
       it "should save to a file" do
         file = Tempfile.new("contents")
-        @s3.get(EXISTING_BLOB_ID, file)
+        s3.get(EXISTING_BLOB_ID, file)
         file.rewind
         file.read.should == EXISTING_BLOB_ID
       end
 
       it "should return the contents" do
-        @s3.get(EXISTING_BLOB_ID).should == EXISTING_BLOB_ID
+        s3.get(EXISTING_BLOB_ID).should == EXISTING_BLOB_ID
       end
 
       it "should raise an error when the object is missing" do
         id = "foooooo"
         expect {
-          @s3.get(id)
+          s3.get(id)
         }.to raise_error Bosh::Blobstore::BlobstoreError, /Could not fetch object/
       end
     end
@@ -153,7 +163,7 @@ describe Bosh::Blobstore::S3BlobstoreClient, :s3_credentials => true do
     describe "create object" do
       it "should raise an error" do
         expect {
-          @s3.create("foobar")
+          s3.create("foobar")
         }.to raise_error Bosh::Blobstore::BlobstoreError, "unsupported action"
       end
     end
@@ -161,10 +171,9 @@ describe Bosh::Blobstore::S3BlobstoreClient, :s3_credentials => true do
     describe "delete object" do
       it "should raise an error" do
         expect {
-          @s3.delete(EXISTING_BLOB_ID)
+          s3.delete(EXISTING_BLOB_ID)
         }.to raise_error Bosh::Blobstore::BlobstoreError, "unsupported action"
       end
     end
-
   end
 end
