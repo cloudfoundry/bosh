7e9a8d679e9526bab509910872aad8567332c1be
Ferran Rodenas and Martin Englund <pair+ferran+martin@pivotallabs.com>
2013-02-01 18:02:51 -0800

    Added VPC test to the AWS CPI integration spec

--- bosh_aws_cpi/spec/integration/cpi_spec.rb
+++ bosh_aws_cpi/spec/integration/cpi_spec.rb
@@ -1,6 +1,6 @@
 # Copyright (c) 2009-2012 VMware, Inc.
 
-require File.expand_path("../../spec_helper", __FILE__)
+require "spec_helper"
 
 require "tempfile"
 
@@ -12,67 +12,89 @@ describe Bosh::AwsCloud::Cloud do
     end
     @config = YAML.load_file(ENV["CPI_CONFIG_FILE"])
     @logger = Logger.new(STDOUT)
+    Bosh::Clouds::Config.stub(:logger => @logger)
   end
 
-  let(:bosh_cpi) do
+  let(:cpi) do
     cpi = Bosh::AwsCloud::Cloud.new(@config)
     cpi.logger = @logger
-
-    # As we inject the configuration file from the outside, we don't care
-    # about spinning up the registry ourselves. However we don't want to bother
-    # EC2 at all if registry is not working, so just in case we perform a test
-    # health check against whatever has been provided.
-    cpi.registry.update_settings("foo", { "bar" => "baz" })
-    cpi.registry.read_settings("foo").should == { "bar" => "baz"}
+    cpi.stub(:registry => double("registry").as_null_object)
 
     cpi
   end
 
-  it "exercises a VM lifecycle" do
-    instance_id = cpi.create_vm(
-      "agent-007", "ami-809a48e9",
+  before(:each) do
+    @instance_id = nil
+    @volume_id = nil
+  end
+
+  after(:each) do
+    cpi.delete_disk(@volume_id) if @volume_id
+    cpi.delete_vm(@instance_id) if @instance_id
+  end
+
+  def vm_lifecycle(ami, network_spec, disk_locality)
+    @instance_id = cpi.create_vm(
+      "agent-007",
+      ami,
       { "instance_type" => "m1.small" },
-      { "default" => { "type" => "dynamic" }},
-      [], { "key" => "value" })
+      network_spec,
+      disk_locality,
+      { "key" => "value" })
 
-    instance_id.should_not be_nil
+    @instance_id.should_not be_nil
 
-    settings = cpi.registry.read_settings(instance_id)
-    settings["vm"].should be_a(Hash)
-    settings["vm"]["name"].should_not be_nil
-    settings["agent_id"].should == "agent-007"
-    settings["networks"].should == { "default" => { "type" => "dynamic" }}
-    settings["disks"].should == {
-      "system" => "/dev/sda",
-      "ephemeral" => "/dev/sdb",
-      "persistent" => {}
-    }
+    vm_metadata = {:job => "cpi_spec", :index => "0"}
+    cpi.set_vm_metadata(@instance_id, vm_metadata)
 
-    settings["env"].should == { "key" => "value" }
+    @volume_id = cpi.create_disk(2048, @instance_id)
+    @volume_id.should_not be_nil
 
-    volume_id = cpi.create_disk(2048)
-    volume_id.should_not be_nil
+    cpi.attach_disk(@instance_id, @volume_id)
+    cpi.detach_disk(@instance_id, @volume_id)
+  end
 
-    cpi.attach_disk(instance_id, volume_id)
-    settings = cpi.registry.read_settings(instance_id)
-    settings["disks"]["persistent"].should == { volume_id => "/dev/sdf" }
+  describe "ec2" do
+    let(:network_spec) do
+      { "default" => {
+          "type" => "dynamic",
+          "cloud_properties" => {}
+        }
+      }
+    end
 
-    cpi.detach_disk(instance_id, volume_id)
-    settings = cpi.registry.read_settings(instance_id)
-    settings["disks"]["persistent"].should == {}
+    context "without existing disks" do
+      it "should exercise the vm lifecycle" do
+        vm_lifecycle(@config["ami"], network_spec, [])
+      end
+    end
 
-    # TODO: test configure_networks (need an elastic IP at hand for that)
+    context "with existing disks" do
+      before do
+        @existing_volume_id = cpi.create_disk(2048)
+      end
 
-    cpi.delete_vm(instance_id)
-    cpi.delete_disk(volume_id)
+      after do
+        cpi.delete_disk(@existing_volume_id) if @existing_volume_id
+      end
 
-    # Test below would fail: EC2 still reports the instance as 'terminated'
-    # for some time.
-    # cpi.ec2.instances[instance_id].should be_nil
+      it "should exercise the vm lifecycle" do
+        vm_lifecycle(@config["ami"], network_spec, [@existing_volume_id])
+      end
+    end
+  end
 
-    expect {
-      cpi.registry.read_settings(instance_id)
-    }.to raise_error(/HTTP 404/)
+  describe "vpc" do
+    it "should exercise the vm lifecycle" do
+      network_spec = {
+        "default" => {
+          "type" => "manual",
+          "ip" => @config["ip"],
+          "cloud_properties" => {"subnet" => @config["subnet"]}
+        }
+      }
+      vm_lifecycle(@config["ami"], network_spec, [])
+    end
   end
 
 end
