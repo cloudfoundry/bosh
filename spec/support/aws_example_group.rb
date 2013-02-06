04708a71229b3bad089228335cd30e5fdba70400
David Stevenson and Martin Englund <pair+stevend+martin@pivotallabs.com>
2013-02-06 13:45:45 -0800

    System AWS spec successfully starts up and shuts-down microbosh in VPC

--- spec/support/aws_example_group.rb
+++ spec/support/aws_example_group.rb
@@ -1,25 +1,38 @@
 module AwsSystemExampleGroup
-  def bosh_deploy_path
-    "/tmp/bosh_deploy"
+  def vpc_outfile_path
+    `ls #{ASSETS_DIR}/aws/create-vpc-output-*.yml`.strip
+  end
+
+  def vpc_outfile
+    YAML.load_file vpc_outfile_path
+  end
+
+  def microbosh_ip
+    vpc_outfile["elastic_ips"]["bosh"]["ips"][0]
+  end
+
+  def bosh_config_path
+    @bosh_config_path ||= Tempfile.new("bosh_config").path
   end
 
   def self.included(base)
     base.before(:each) do
+      ENV['BOSH_KEY_PAIR_NAME'] = "bosh_ci"
+      ENV['BOSH_KEY_PATH'] = "/tmp/id_bosh_ci"
+
       system "rm -f #{ASSETS_DIR}/aws/create-vpc-output-*.yml"
       raise "Failed to create VPC resources" unless system "bundle exec bosh aws create vpc #{ASSETS_DIR}/aws/aws_configuration_template.yml.erb"
 
       puts "AWS RESOURCES CREATED SUCCESSFULLY!"
-
-      FileUtils.rm_rf(bosh_deploy_path)
-      FileUtils.mkdir_p(bosh_deploy_path)
-      raise "Cannot clone deployments-aws repo to #{bosh_deploy_path}" unless system("git", "clone", "git@github.com:cloudfoundry/deployments-aws.git", bosh_deploy_path)
+      p vpc_outfile
+      ENV['MICROBOSH_IP'] = vpc_outfile["elastic_ips"]["bosh"]["ips"][0]
+      ENV['BOSH_SUBNET_ID'] = vpc_outfile["vpc"]["subnets"]["bosh"]
     end
 
     base.after(:each) do
-      vpc_outfile = `ls #{ASSETS_DIR}/aws/create-vpc-output-*.yml`.strip
-      puts "Using VPC output: #{vpc_outfile}"
-      puts File.read(vpc_outfile)
-      raise "Failed to create VPC resources" unless system "bundle exec bosh aws delete vpc '#{vpc_outfile}'"
+      puts "Using VPC output: #{vpc_outfile_path}"
+      puts "Failed to terminate EC2 instances" unless system "bundle exec bosh -n aws terminate_all ec2 '#{vpc_outfile_path}'"
+      puts "Failed to create VPC resources" unless system "bundle exec bosh -n aws delete vpc '#{vpc_outfile_path}'"
       puts "CLEANUP SUCCESSFUL"
     end
   end
--- spec/system/aws/micro_bosh_spec.rb
+++ spec/system/aws/micro_bosh_spec.rb
@@ -1,12 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
 
 describe "AWS" do
-  STEMCELL_AMI = "ami-c72cb8ae"
+  STEMCELL_AMI = "ami-30d94f59"
   it "should be able to launch a MicroBosh from existing stemcell" do
-    puts "Placeholder passing test"
-    #Dir.chdir(bosh_deploy_path) do
-    #  FileUtils.mkdir_p("ci/deployments/micro")
-    #  FileUtils.cp("micro_bosh.yml", "ci/micro_bosh.yml")
-    #end
+    Dir.chdir(File.join(ASSETS_DIR, "aws", "deployments")) do
+      FileUtils.rm_f("aws_registry.log")
+      FileUtils.rm_f("bosh-deployments.yml")
+      FileUtils.rm_f("micro/bosh_micro_deploy.log")
+
+      puts "MICRO_BOSH.YML:"
+      puts ERB.new(File.read("micro/micro_bosh.yml")).result
+
+      puts ""
+      run_bosh "micro deployment micro"
+      run_bosh "micro deploy #{STEMCELL_AMI}"
+    end
+  end
+
+  def run(cmd)
+    system(cmd) || raise("Couldn't run '#{cmd}' from #{Dir.pwd}, failed with exit status #{$?}")
+  end
+
+  def run_bosh(cmd)
+    run "bundle exec bosh -n --config #{bosh_config_path} #{cmd}"
   end
 end
