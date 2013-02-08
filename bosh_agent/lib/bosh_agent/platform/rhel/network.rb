3c718473dd84bea3d1b5ed3f65310951abefda40
Anfernee Gui <agui@vmware.com>
2013-02-08 21:25:13 +0800

    RHEL network configuration
    Change-Id: Ib82330b3bd7bc4736f202992cc0011e21416782b

--- bosh_agent/lib/bosh_agent/platform/rhel/network.rb
+++ bosh_agent/lib/bosh_agent/platform/rhel/network.rb
@@ -2,5 +2,20 @@
 
 module Bosh::Agent
   class Platform::Rhel::Network < Platform::Linux::Network
+    def write_network_interfaces
+      template = ERB.new(load_erb("rhel-ifcfg.erb"), 0, '%<>-')
+      result = template.result(binding)
+      network_updated = Bosh::Agent::Util::update_file(result, '/etc/sysconfig/network-scripts/ifcfg-eth0')
+
+      if network_updated
+        logger.info("Updated networking")
+        restart_networking_service
+      end
+    end
+
+    def restart_networking_service
+      logger.info("Restarting network")
+      Bosh::Exec.sh "service network restart"
+    end
   end
 end
