5b26bc3e8e464388985577e12ad3a4ca0db3a6a4
Amit Gupta and Ferran Rodenas <pair+agupta+ferran@pivotallabs.com>
2013-02-11 12:26:55 -0800

    fix removal of previous CI builds' remnants
    [#44228637] [#43954863]

--- ci_build_micro_bosh_stemcell.sh
+++ ci_build_micro_bosh_stemcell.sh
@@ -3,11 +3,14 @@
 set -e
 
 sudo rm -rf /mnt/micro-stemcell
-rm *.tgz
+if [ -f $WORKSPACE/*.tgz]
+then
+  rm $WORKSPACE/*.tgz
+fi
 
 WORK_PATH=/mnt/micro-stemcell/work BUILD_PATH=/mnt/micro-stemcell/build ./spec/ci_build.sh stemcell:micro[aws]
 
 stemcell=`ls /mnt/micro-stemcell/work/work/*.tgz`
 stemcell_base=`basename $stemcell tgz`
 
-cp $stemcell ./$stemcell_base$BUILD_ID.tgz
+cp $stemcell $WORKSPACE/$stemcell_base$BUILD_ID.tgz
--- ci_build_stemcell.sh
+++ ci_build_stemcell.sh
@@ -3,11 +3,14 @@
 set -e
 
 sudo rm -rf /mnt/stemcell
-rm *.tgz
+if [ -f $WORKSPACE/*.tgz]
+then
+  rm $WORKSPACE/*.tgz
+fi
 
 WORK_PATH=/mnt/stemcell/work BUILD_PATH=/mnt/stemcell/build ./spec/ci_build.sh stemcell:basic[aws]
 
 stemcell=`ls /mnt/stemcell/work/work/*.tgz`
 stemcell_base=`basename $stemcell tgz`
 
-cp $stemcell ./$stemcell_base$BUILD_ID.tgz
+cp $stemcell $WORKSPACE/$stemcell_base$BUILD_ID.tgz
