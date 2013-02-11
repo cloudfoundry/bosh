8abcb818c1b448f0cfb262ce22c44a35c00b2782
Amit Gupta and Ferran Rodenas <pair+agupta+ferran@pivotallabs.com>
2013-02-11 11:43:01 -0800

    Configure Travis' bundler step to use local gems

--- ci_build.rb
+++ ci_build.rb
@@ -1,13 +1,7 @@
 #!/usr/bin/env ruby
 
-def bundle_without
-  ENV['TRAVIS'] ? "--local --without development" : "" # aka: on travis
-end
+system("bundle") unless ENV['TRAVIS'] # special bundle configuration for travis, done automatically
 
-system("bundle check || bundle #{bundle_without}")  || raise("Bundler is required.")
-
-if ENV['SUITE'] == "integration"
-  system("bundle exec rake spec:integration") || raise("failed to run spec/integration")
-else
-  system("bundle exec rake spec:unit") || raise("failed to run unit tests")
-end
+# run both suites unless one is explicitly specified
+system("bundle exec rake spec:integration") || raise("failed to run spec/integration") unless ENV['SUITE'] == 'unit'
+system("bundle exec rake spec:unit") || raise("failed to run unit tests") unless ENV['SUITE'] == 'integration'
