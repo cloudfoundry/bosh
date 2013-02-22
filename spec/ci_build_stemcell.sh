f88d2033a458dd6415ebea15e182a9e611dc53c3
Martin Englund <martin@englund.nu>
2013-02-22 14:55:59 -0800

    change ci stemcell builder to be able to build for all infrastructures

--- spec/ci_build_stemcell.sh
+++ spec/ci_build_stemcell.sh
@@ -9,22 +9,30 @@ rm -f *.tgz stemcell-ami.txt
 if [ $1 == 'micro' ]
 then
   task='micro'
-  directory='micro-stemcell'
 else
   task='basic'
-  directory='stemcell'
 fi
+
+if [ $# == 2 ]; then
+  infrastructure=$2
+else
+  infrastructure='aws'
+fi
+
+directory = "$infrastructure-$task"
 
 sudo rm -rf /mnt/$directory
 
 WORK_PATH=/mnt/$directory/work \
     BUILD_PATH=/mnt/$directory/build \
     STEMCELL_VERSION=$BUILD_ID \
-    $WORKSPACE/spec/ci_build.sh stemcell:$task[aws]
+    $WORKSPACE/spec/ci_build.sh stemcell:$task[$infrastructure]
 
 stemcell=`ls /mnt/$directory/work/work/*.tgz`
 stemcell_base=`basename $stemcell .tgz`
 
 cp $stemcell $WORKSPACE/$stemcell_base.tgz
 
-bundle exec $(dirname $0)/publish_ami.rb $WORKSPACE/$stemcell_base.tgz
+if [ $infrastructure == 'aws' ]; then
+    bundle exec $(dirname $0)/publish_ami.rb $WORKSPACE/$stemcell_base.tgz
+fi
