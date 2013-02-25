17ea5cd5920dd4eb50b46c45707e1a3b7dfe35f5
Matthew Kocher <pair+mkocher@pivotallabs.com>
2013-02-24 19:54:36 -0800

    no more fast fail

--- spec/system/aws/micro_bosh_spec.rb
+++ spec/system/aws/micro_bosh_spec.rb
@@ -52,7 +52,6 @@ describe "AWS" do
         'BAT_FAST' => 'true',
         'BAT_SKIP_SSH' => 'true',
         'BAT_DEBUG' => 'verbose',
-        'BAT_FAIL_FAST' => 'true',
         'BAT_DNS_HOST' => Resolv.getaddress(director),
     }
     system(bat_env, "rake bat").should be_true
