869654eb062312a12844097d7b518a077f5b2532
Mark Rushakoff and Martin Englund <pair+mr+martin@pivotallabs.com>
2013-03-01 12:23:18 -0800

    Handle version comparison better

--- bosh_cli/spec/unit/version_calc_spec.rb
+++ bosh_cli/spec/unit/version_calc_spec.rb
@@ -15,6 +15,10 @@ describe Bosh::Cli::VersionCalc do
     @obj.version_cmp("0.1.7.5", "0.1.7").should == 1
     @obj.version_cmp("0.1.7.4.9.9", "0.1.7.5").should == -1
 
+    @obj.version_cmp("10", "9").should == 1
+    @obj.version_cmp("9", "10").should == -1
+    @obj.version_cmp("10", "10").should == 0
+
     @obj.version_cmp(10, 11).should == -1
     @obj.version_cmp(43, 42).should == 1
     @obj.version_cmp(7, 7).should == 0
@@ -22,6 +26,8 @@ describe Bosh::Cli::VersionCalc do
     @obj.version_cmp("10.9-dev", "10.10-dev").should == -1
     @obj.version_cmp("0.2.3", "0.2.3.0.8").should == -1
     @obj.version_cmp("0.2.3-dev", "0.2.3.0.3-dev").should == -1
+
+    @obj.version_cmp("10.0.at-2013-02-27_21-38-27", "2.0.at-2013-02-26_01-26-46").should == 1
   end
 
   it 'can compare version correctly when they are timestamps' do
