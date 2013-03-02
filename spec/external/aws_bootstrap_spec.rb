7d2db958684cc28db5738113641f880721ef6004
Amit Gupta and Gary Liu <pair+agupta+gary@pivotallabs.com>
2013-03-01 17:27:59 -0800

    external specs for VPC subnet and route table creation
    also, pending specs for DHCP options, security groups, ELBs
    also Route 53; RDS; S3; the universe
    [#42828011]

--- spec/external/aws_bootstrap_spec.rb
+++ spec/external/aws_bootstrap_spec.rb
@@ -22,29 +22,47 @@ describe 'bosh_aws_bootstrap_external' do
   let(:aws_configuration_template) { File.join(File.dirname(__FILE__), '..','assets','aws','aws_configuration_template.yml.erb') }
 
   describe "VPC" do
+    let(:vpc) { ec2.vpcs.first }
+    let(:bosh_subnet) { vpc.subnets.select { |subnet| subnet.cidr_block == "10.10.0.0/24" }.first }
+    let(:cf_subnet) { vpc.subnets.select { |subnet| subnet.cidr_block == "10.10.1.0/24" }.first }
+    let(:cf_subnet_2) { vpc.subnets.select { |subnet| subnet.cidr_block == "10.10.2.0/24" }.first }
+
     before(:all) { run_bosh "aws create vpc #{aws_configuration_template}" }
+
     after(:all) { run_bosh "aws destroy #{aws_configuration_template}" }
 
     it "builds the VPC" do
-      ec2.vpcs.count.should == 1
+      vpc.should_not be_nil
     end
 
     it "builds the VPC subnets" do
-      vpc = ec2.vpcs.first
-
-      bosh_subnet = vpc.subnets.select { |subnet| subnet.cidr_block == "10.10.0.0/24" }.first
       bosh_subnet.availability_zone.name.should == ENV["BOSH_VPC_PRIMARY_AZ"]
       bosh_subnet.instances.first.tags["Name"].should == "cf_nat_box"
 
-      cf_subnet = vpc.subnets.select { |subnet| subnet.cidr_block == "10.10.1.0/24" }.first
       cf_subnet.availability_zone.name.should == ENV["BOSH_VPC_PRIMARY_AZ"]
       cf_subnet.instances.count.should == 0
 
-      cf_subnet_2 = vpc.subnets.select { |subnet| subnet.cidr_block == "10.10.2.0/24" }.first
       cf_subnet_2.availability_zone.name.should == ENV["BOSH_VPC_SECONDARY_AZ"]
       cf_subnet_2.instances.count.should == 0
+    end
 
-      #TODO add tests for route table associations
+    it "associates route tables with subnets" do
+      bosh_routes = bosh_subnet.route_table.routes
+      bosh_default_route = bosh_routes.select { |route| route.destination_cidr_block == "0.0.0.0/0" }.first
+      bosh_default_route.target.id.should match /igw/
+      bosh_local_route = bosh_routes.select { |route| route.destination_cidr_block == "10.10.0.0/16" }.first
+      bosh_local_route.target.id.should == "local"
+
+      cf_routes = cf_subnet.route_table.routes
+      cf_default_route = cf_routes.select { |route| route.destination_cidr_block == "0.0.0.0/0" }.first
+      cf_default_route.target.should == ec2.instances.first
+      cf_local_route = cf_routes.select { |route| route.destination_cidr_block == "10.10.0.0/16" }.first
+      cf_local_route.target.id.should == "local"
+
+      cf2_routes = cf_subnet_2.route_table.routes
+      cf2_routes.any? { |route| route.destination_cidr_block == "0.0.0.0/0" }.should be_false
+      cf2_local_route = cf2_routes.select { |route| route.destination_cidr_block == "10.10.0.0/16" }.first
+      cf2_local_route.target.id.should == "local"
     end
 
     pending "DHCP options"
@@ -60,20 +78,17 @@ describe 'bosh_aws_bootstrap_external' do
     #before(:all) { run_bosh "aws create rds #{aws_configuration_template}" }
     #after(:all) { run_bosh "aws destroy #{aws_configuration_template}" }
 
-    it "can create and destroy an RDS configuration" do
-    end
+    pending "can create and destroy an RDS configuration"
   end
 
   describe "S3" do
     #before(:all) { run_bosh "aws create s3 #{aws_configuration_template}" }
     #after(:all) { run_bosh "aws destroy #{aws_configuration_template}" }
 
-    it "can create and destroy an S3 configuration" do
-    end
+    pending "can create and destroy an S3 configuration"
   end
 
   describe "all resources" do
-    it "can create and destroy a configuration of VPC, RDS, Route53, and S3" do
-    end
+    pending "can create and destroy a configuration of VPC, RDS, Route53, and S3"
   end
 end
