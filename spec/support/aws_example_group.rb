6c77b4b20b1249240c26f17780316665a3518cb7
Kai Xiang <xiangkai@gmail.com>
2013-03-05 14:53:49 -0800

    fix system spec helper run_bosh for debug_on_fail logic

--- spec/support/aws_example_group.rb
+++ spec/support/aws_example_group.rb
@@ -96,7 +96,8 @@ module AwsSystemExampleGroup
   end
 
   def run_bosh(cmd, options = {})
-    debug_on_fail = options.delete(:debug_on_fail) || true
+    debug_on_fail = options.fetch(:debug_on_fail, true)
+    options.delete(:debug_on_fail)
     @run_bosh_failures ||= 0
     run "#{binstubs_path}/bosh -v -n -P 10 --config '#{bosh_config_path}' #{cmd}", options
   rescue
