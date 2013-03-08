89bebfb7cecd1703ed446559d9d30e993d76299e
Martin Englund <martin@englund.nu>
2013-03-08 08:43:24 -0800

    [director] modify package cache output

--- director/lib/director/package_compiler.rb
+++ director/lib/director/package_compiler.rb
@@ -74,7 +74,7 @@ module Bosh::Director
     # in the jobs defined by deployment plan
     # @return [void]
     def prepare_tasks
-      @event_log.begin_stage("Preparing package compilation", 1)
+      @event_log.begin_stage("Preparing package compilation")
 
       @event_log.track("Finding packages to compile") do
         @deployment_plan.jobs.each do |job|
@@ -360,8 +360,10 @@ module Bosh::Director
                      "for stemcell `#{stemcell.desc}'")
       else
         if Config.use_compiled_package_cache?
-          @event_log.track("Downloading '#{package.desc}' from global cache") do
-            compiled_package = BlobUtil.fetch_from_global_cache(package, stemcell, task.cache_key, dependency_key)
+          if BlobUtil.exists_in_global_cache?(package, task.cache_key)
+            @event_log.track("Downloading '#{package.desc}' from global cache") do
+              compiled_package = BlobUtil.fetch_from_global_cache(package, stemcell, task.cache_key, dependency_key)
+            end
           end
         end
 
