7eccdd1d277f9a5a1f02757df2ebd1ee77008daa
Ankur Chauhan <ankurc@vmware.com>
2013-03-11 13:23:32 -0700

    Fix logrotate tests

--- bosh_agent/spec/unit/platform/linux/logrotate_spec.rb
+++ bosh_agent/spec/unit/platform/linux/logrotate_spec.rb
@@ -3,14 +3,22 @@
 require 'spec_helper'
 require 'bosh_agent/platform/linux/logrotate'
 
-describe Bosh::Agent::Platform::Ubuntu::Logrotate do
+describe Bosh::Agent::Platform::Linux::Logrotate do
+  let(:system_root)         { Dir.mktmpdir }
+  let(:logrotate_dst_path)  { File.join(system_root, 'etc', 'logrotate.d', Bosh::Agent::BOSH_APP_GROUP) }
+  let(:log_rotate)          {
+
+    Bosh::Agent::Config.system_root = system_root
+    Bosh::Agent::Config.logger = Logger.new("/dev/null")
+
+    template_dir = Dir.mktmpdir
+    File.open(File.join(template_dir, "logrotate.erb"), "w") {|f| f.write("size=<%= size %>") }
+
+    Bosh::Agent::Platform::Linux::Logrotate.new(template_dir)
+  }
 
   before(:each) do
-    Bosh::Agent::Config.system_root = Dir.mktmpdir
-    Bosh::Agent::Config.logger = Logger.new(STDOUT)
-
-    @logrotate_dst_path = File.join(Bosh::Agent::Config.system_root, 'etc', 'logrotate.d', Bosh::Agent::BOSH_APP_GROUP)
-    FileUtils.mkdir_p File.dirname(@logrotate_dst_path)
+    FileUtils.mkdir_p File.dirname(logrotate_dst_path)
   end
 
   after(:each) do
@@ -19,23 +27,23 @@ describe Bosh::Agent::Platform::Ubuntu::Logrotate do
 
   it 'should create logrotate file named after BOSH_APP_GROUP' do
     spec_properties = {}
-    Bosh::Agent::Platform::Ubuntu::Logrotate.new.install(spec_properties)
-    File.exist?(@logrotate_dst_path).should == true
+    log_rotate.install(spec_properties)
+    File.exist?(logrotate_dst_path).should == true
   end
 
   it 'should default to DEFAULT_MAX_LOG_FILE_SIZE if nothing is specified in the spec_properties' do
-    default_max_log_file_size = Bosh::Agent::Platform::Ubuntu::Logrotate::DEFAULT_MAX_LOG_FILE_SIZE
-    Bosh::Agent::Platform::Ubuntu::Logrotate.new.install({})
+    default_max_log_file_size = Bosh::Agent::Platform::Linux::Logrotate::DEFAULT_MAX_LOG_FILE_SIZE
+    log_rotate.install({})
     match_expression = %r|size=#{default_max_log_file_size}|
-    File.read(@logrotate_dst_path).should match(match_expression)
+    File.read(logrotate_dst_path).should match(match_expression)
   end
 
   it 'should use MAX_LOG_FILE_SIZE if specified in the spec_properties' do
     max_log_file_size = '10M'
     spec_properties = {'properties' => {'logging' => {'max_log_file_size' => max_log_file_size}}}
-    Bosh::Agent::Platform::Ubuntu::Logrotate.new.install(spec_properties)
+    log_rotate.install(spec_properties)
     match_expression = %r|size=#{max_log_file_size}|
-    File.read(@logrotate_dst_path).should match(match_expression)
+    File.read(logrotate_dst_path).should match(match_expression)
   end
 
 end
