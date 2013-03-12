e04264286448123a7341b06238d48f246f91d4ed
Ferran Rodenas <frodenas@gmail.com>
2013-03-12 13:04:38 +0100

    [aws_cpi] Fix bug when deregistering an image

--- bosh_aws_cpi/lib/cloud/aws/stemcell.rb
+++ bosh_aws_cpi/lib/cloud/aws/stemcell.rb
@@ -23,7 +23,7 @@ module Bosh::AwsCloud
 
       # Wait for the AMI to be deregistered, or the snapshot deletion will fail,
       # as the AMI is still in use.
-      wait_resource(ami, :deleted)
+      wait_resource(ami, :deleted, :state)
 
       delete_snapshots
       logger.info("deleted stemcell '#{id}'")
--- bosh_aws_cpi/spec/unit/stemcell_spec.rb
+++ bosh_aws_cpi/spec/unit/stemcell_spec.rb
@@ -27,7 +27,7 @@ describe Bosh::AwsCloud::Stemcell do
 
         stemcell.should_receive(:memoize_snapshots).ordered
         fake_aws_ami.should_receive(:deregister).ordered
-        stemcell.should_receive(:wait_resource).with(fake_aws_ami, :deleted).ordered
+        stemcell.should_receive(:wait_resource).with(fake_aws_ami, :deleted, :state).ordered
         stemcell.should_receive(:delete_snapshots).ordered
 
         stemcell.delete
