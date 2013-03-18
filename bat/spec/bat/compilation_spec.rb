99306426c8b6a6e5bce747c2e61dfa6834591272
Alex Jackson and Martin Englund <pair+ajackson+martin@pivotallabs.com>
2013-03-18 14:14:25 -0700

    bat test for package cache

---
+++ bat/spec/bat/compilation_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+describe 'compilation' do
+
+  before(:all) do
+    requirement release
+    requirement stemcell
+  end
+
+  after(:all) do
+    cleanup release
+    cleanup stemcell
+  end
+
+  before(:each) do
+    load_deployment_spec
+  end
+
+  context 'when compiled package cache is enabled' do
+    it 'should download compiled package if it exist' do
+      pending 'global package cache not enabled' unless compiled_package_cache?
+
+      with_deployment do
+        # do nothing, just to make sure the cache is warm
+      end
+      
+      # delete release to force package compilation
+      bosh("delete release #{release.name}", :on_error => :return)
+      requirement release
+
+      with_deployment do |deployment, result|
+        puts result.output
+        events(get_task_id(result.output)).any? { |event|
+          event['task'].match(/Downloading '.+' from global cache/)
+        }.should be_true
+      end
+    end
+  end
+end
