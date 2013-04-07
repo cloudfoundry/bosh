f2890ac16d902a8253aec8bf921f9a8ca7d4cf9e
Martin Englund <martin@englund.nu>
2013-04-06 23:56:08 -0700

    [aws_cpi] better description of the timeout setting

--- bosh_aws_cpi/lib/cloud/aws/resource_wait.rb
+++ bosh_aws_cpi/lib/cloud/aws/resource_wait.rb
@@ -1,7 +1,7 @@
 module Bosh::AwsCloud
   class ResourceWait
 
-    DEFAULT_TRIES = 8 # this is saner amount of retries (~30 minutes), as AWS doesn't give us a choice
+    DEFAULT_TRIES = 8 # a sane amount of retries on AWS (~35 minutes), as things can take anywhere between a minute and forevah
     MAX_SLEEP_EXPONENT = 8
 
     def self.for_instance(args)
