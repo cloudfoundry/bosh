34a22c1dc6e584864c92035880131e1865e9e12d
David Sabeti and Tony Hansmann <pair+david+thansmann@pivotallabs.com>
2013-04-11 14:27:47 -0700

    Compile ruby from source

--- bosh_warden_cpi/chef/director/recipes/install.rb
+++ bosh_warden_cpi/chef/director/recipes/install.rb
@@ -1,6 +1,7 @@
+V_RUBY = "1.9.3-p327"
+
 packages = %w[
   debootstrap
-  ruby1.9.3
   build-essential
   quota
   libcurl4-gnutls-dev
@@ -17,6 +18,31 @@ packages = %w[
   postgresql-9.1
   libpq-dev
 ]
+
+script "install_ruby" do
+  interpreter "bash"
+  user "root"
+  creates "/usr/local/bin/ruby"
+  cwd "/usr/local/src/"
+
+  code <<-EOH
+  # First need to install libyaml
+  wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
+  tar zxvf yaml-0.1.4.tar.gz
+  cd yaml-0.1.4
+  ./configure --prefix=/usr/local
+  make
+  make install
+
+  # Install ruby
+  wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{V_RUBY}.tar.gz
+  tar zxvf ruby-#{V_RUBY}.tar.gz
+  cd ruby-#{V_RUBY}
+  ./configure --prefix=/usr/local --enable-shared --disable-install-doc --with-opt-dir=/usr/local/lib
+  make
+  make install
+  EOH
+end
 
 packages.each do |package_name|
   package package_name do
