35078a7510bc08e6d178b8b54d6b84f32ce68bc4
Alex Jackson and Vinicius Fuentes <pair+ajackson+vfuentes@pivotallabs.com>
2013-04-12 14:18:51 -0700

    Splitting migrations into two files, make tests pass.

--- bosh_cli_plugin_aws/spec/unit/migration_spec.rb
+++ bosh_cli_plugin_aws/spec/unit/migration_spec.rb
@@ -1,142 +1,70 @@
-require 'spec_helper'
+require "spec_helper"
 
-class DummyMigration < Bosh::Aws::Migration
-  def execute
-    s3.create_bucket(self.class.name)
-  end
-end
-
-describe Bosh::Aws::Migrator do
-
-  let(:config) { {'aws' => {}, 'name' => "deployment-name"} }
-  let(:subject) { described_class.new(config) }
-  let(:mock_s3) { mock("Bosh::Aws::S3").as_null_object }
-
-  before(:all) do
-    @tempdir = Dir.mktmpdir
+describe Bosh::Aws::Migration do
+  let(:config) do
+    {"aws" => {}}
   end
 
-  after(:all) do
-    FileUtils.rm_rf(@tempdir)
+  let(:s3) { double("S3") }
+  let(:receipt) do
+    {"hello" => "world"}
   end
 
   before do
-    @time = Time.now
-    Time.stub(:new) { @time }
-
-    Bosh::Aws::MigrationHelper.stub!(:aws_migration_directory).and_return(@tempdir)
-
-    Bosh::Aws::S3.stub(:new).and_return(mock_s3)
-    mock_s3.stub(:fetch_object_contents).and_return(nil)
+    Bosh::Aws::S3.stub(new: s3)
   end
 
-  it "should not create an s3 bucket if there is one already" do
-    mock_s3.should_receive(:bucket_exists?).with('deployment-name-bosh-artifacts').and_return(true)
-    mock_s3.should_not_receive(:create_bucket)
-
-    subject.migrate
+  around do |example|
+    Dir.mktmpdir do |tmpdir|
+      Dir.chdir(tmpdir) do
+        example.run
+      end
+    end
   end
 
-  it "should create an s3 bucket if one isn't there" do
-    mock_s3.should_receive(:bucket_exists?).with('deployment-name-bosh-artifacts').and_return(false)
-    mock_s3.should_receive(:create_bucket).with('deployment-name-bosh-artifacts')
+  it "saves receipts in s3" do
+    s3.should_receive(:upload_to_bucket).with("bucket", "receipts/aws_dummy_receipt.yml", YAML.dump(receipt))
 
-    subject.migrate
+    migration = described_class.new(config, 'bucket')
+    migration.save_receipt("aws_dummy_receipt", receipt)
   end
 
-  context "with migrations" do
+  it "saves receipts in the local filesystem" do
+    s3.stub(:upload_to_bucket)
 
-    before do
-      @expected_migrations = []
-      10.times do |i|
-        timestamp = Bosh::Aws::MigrationHelper.timestamp
-        name = "test_#{i}"
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        migration = described_class.new(config, 'bucket')
+        migration.save_receipt("aws_dummy_receipt", receipt)
 
-        filename = "#{@tempdir}/#{timestamp}_#{name}.rb"
-        File.open(filename, 'w+') do |f|
-          migration_text = Bosh::Aws::MigrationHelper.merge_migration_template(name)
-          migration_text.gsub!("Bosh::Aws::Migration","DummyMigration")
-          migration_text.gsub!("execute","execute_not_used")
-          f.write(migration_text)
-        end
-
-        @expected_migrations << Bosh::Aws::MigrationProxy.new(name, timestamp.to_i)
-        @time += 1 #Time marches on
+        receipt_contents = YAML.load_file("aws_dummy_receipt.yml")
+        receipt_contents.should == receipt
       end
     end
+  end
 
-    it 'should create a list of known migrations' do
-      subject.migrations.should == @expected_migrations
-    end
+  it "loads the receipt from S3" do
+    s3.should_receive(:fetch_object_contents).with("bucket", "receipts/aws_dummy_receipt.yml").and_return(YAML.dump(receipt))
 
-    context "#migrate" do
-      it "should run all the migrations if it has never been run before" do
-        mock_s3.should_receive(:bucket_exists?).with('deployment-name-bosh-artifacts').and_return(true)
+    migration = described_class.new(config, 'bucket')
+    migration.load_receipt("aws_dummy_receipt").should == receipt
+  end
 
-        ordered_expectations = mock_s3.should_receive(:create_bucket).ordered
+  it "initializes AWS helpers" do
+    elb = double("ELB")
+    ec2 = double("EC2")
+    route53 = double("Route53")
 
-        10.times do |i|
-          ordered_expectations.with("Test#{i}")
-        end
+    Bosh::Aws::S3.should_receive(:new).with(config["aws"]).and_return(s3)
+    Bosh::Aws::ELB.should_receive(:new).with(config["aws"]).and_return(elb)
+    Bosh::Aws::EC2.should_receive(:new).with(config["aws"]).and_return(ec2)
+    Bosh::Aws::Route53.should_receive(:new).with(config["aws"]).and_return(route53)
 
-        subject.migrate
-      end
-
-      it "should run the migrations that has never been run before" do
-        mock_s3.should_receive(:bucket_exists?).with('deployment-name-bosh-artifacts').and_return(true)
-        mock_s3.
-            should_receive(:fetch_object_contents).
-            with('deployment-name-bosh-artifacts', "aws_migrations/migrations.yaml").
-            and_return(YAML.dump(@expected_migrations[0..5].collect{|m|m.to_hash}))
-
-        ordered_expectations = mock_s3.should_receive(:create_bucket).ordered
-        (6..9).each do |i|
-          ordered_expectations.with("Test#{i}")
-        end
-
-        subject.migrate
-      end
-
-      it "should not run migrations that has already been run" do
-        mock_s3.should_receive(:bucket_exists?).with('deployment-name-bosh-artifacts').and_return(true)
-        mock_s3.
-            should_receive(:fetch_object_contents).
-            with('deployment-name-bosh-artifacts', "aws_migrations/migrations.yaml").
-            and_return(YAML.dump(@expected_migrations.collect{|m|m.to_hash}))
-
-        mock_s3.should_not_receive(:create_bucket)
-
-        subject.migrate
-      end
-    end
-
-    context "#needs_migration?" do
-
-      before do
-        mock_s3.should_receive(:bucket_exists?).with('deployment-name-bosh-artifacts').and_return(true)
-      end
-
-      it 'should need a migration if S3 file is missing' do
-        subject.needs_migration?.should be_true
-      end
-
-      it "should need a migration if S3 file has migrations, but not all" do
-        mock_s3.
-            should_receive(:fetch_object_contents).
-            with('deployment-name-bosh-artifacts', "aws_migrations/migrations.yaml").
-            and_return(YAML.dump(@expected_migrations[0..5].collect{|m|m.to_hash}))
-
-        subject.needs_migration?.should be_true
-      end
-
-      it 'should not need a migration if S3 file has all the migrations' do
-        mock_s3.
-            should_receive(:fetch_object_contents).
-            with('deployment-name-bosh-artifacts', "aws_migrations/migrations.yaml").
-            and_return(YAML.dump(@expected_migrations.collect{|m|m.to_hash}))
-
-        subject.needs_migration?.should be_false
-      end
-    end
+    migration = described_class.new(config, 'bucket')
+    migration.ec2.should == ec2
+    migration.s3.should == s3
+    migration.elb.should == elb
+    migration.route53.should == route53
+    migration.logger.should_not be_nil
   end
 end
