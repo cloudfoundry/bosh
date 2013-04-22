168989de6161b250fc959419cea62c4016dc14db
Glenn Oppegard and Kyriacos Souroullas <pair+goppegard+k@pivotallabs.com>
2013-04-22 15:07:18 -0700

    [Finishes #48538757, #47113117] Run 'tracker' to auto-deliver stories in ci_build.sh

--- spec/ci_build.sh
+++ spec/ci_build.sh
@@ -5,4 +5,8 @@ source .rvmrc
 gem list | grep bundler > /dev/null || gem install bundler
 bundle check || bundle install --without development
 
-bundle exec rake $@
+if [ -n "$TRACKER_PROJECT_ID" ] && [ -n "$TRACKER_TOKEN" ] ; then
+    bundle exec rake $@ && bundle list | grep "tracker-git" > /dev/null && bundle exec tracker
+else
+    bundle exec rake $@
+fi
