ed9b291e43e356baaa5e9b526e75a4579e72a621
Matthew M. Boedicker <matthewm@boedicker.org>
2013-05-04 23:02:31 -0700

    Validation error message improvements.

--- director/spec/unit/validation_helper_spec.rb
+++ director/spec/unit/validation_helper_spec.rb
@@ -16,7 +16,8 @@ describe Bosh::Director::ValidationHelper do
   it "should fail if required fields are missing" do
     lambda {
       @helper.safe_property({"testing" => 1}, "test", :required => true)
-    }.should raise_exception(Bosh::Director::ValidationMissingField)
+    }.should raise_exception(Bosh::Director::ValidationMissingField,
+                             "Required property `test' was not specified in Object")
   end
 
   it "should pass if fields match their class" do
@@ -30,7 +31,8 @@ describe Bosh::Director::ValidationHelper do
   it "should fail if fields don't match their class" do
     lambda {
       @helper.safe_property({"test" => 1}, "test", :class => Array)
-    }.should raise_exception(Bosh::Director::ValidationInvalidType)
+    }.should raise_exception(Bosh::Director::ValidationInvalidType,
+                             "Property `test' (value 1) did not match the required type `Array'")
   end
 
   it "should pass if numbers don't have constraints" do
@@ -48,13 +50,13 @@ describe Bosh::Director::ValidationHelper do
   it "should fail if numbers don't pass min constraints" do
     lambda {
       @helper.safe_property({"test" => 3}, "test", :min => 4).should eql(3)
-    }.should raise_exception(Bosh::Director::ValidationViolatedMin)
+    }.should raise_exception(Bosh::Director::ValidationViolatedMin, "`test' value (3) should be greater than 4")
   end
 
   it "should fail if numbers don't pass max constraints" do
     lambda {
       @helper.safe_property({"test" => 3}, "test", :max => 2).should eql(3)
-    }.should raise_exception(Bosh::Director::ValidationViolatedMax)
+    }.should raise_exception(Bosh::Director::ValidationViolatedMax, "`test' value (3) should be less than 2")
   end
 
 end
