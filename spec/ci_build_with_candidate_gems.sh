17f5e055c514651263889f56c8c7ccb7aedff19f
Alex Jackson and Glenn Oppegard <pair+ajackson+oppegard@pivotallabs.com>
2013-05-07 15:22:20 -0700

    use bundler for pipeline gems

--- spec/ci_build_with_candidate_gems.sh
+++ spec/ci_build_with_candidate_gems.sh
@@ -1,36 +1,12 @@
 #!/bin/bash -l
-#set -e
-source .rvmrc
+set -e
 
-rvm gemset use ci_gems --create
-rvm --force gemset empty ci_gems
+script_dir=`dirname $0`
 
-gem install --no-ri --no-rdoc rake -v $(grep "rake" Gemfile | tr "'" '"' | cut -d '"' -f 4)
-gem install --no-ri --no-rdoc ci_reporter rspec rugged parallel_tests
-gem install --no-ri --no-rdoc \
-    --source https://s3.amazonaws.com/bosh-ci-pipeline/gems/ \
-    --pre \
-    agent_client \
-    blobstore_client \
-    bosh_agent \
-    bosh_aws_cpi \
-    bosh_cli \
-    bosh_cli_plugin_aws \
-    bosh_cli_plugin_micro \
-    bosh_common \
-    bosh_cpi \
-    bosh_encryption \
-    bosh_openstack_cpi \
-    bosh_registry \
-    bosh_vcloud_cpi \
-    bosh_vsphere_cpi \
-    director \
-    health_monitor \
-    monit_api \
-    package_compiler \
-    ruby_vcloud_sdk \
-    ruby_vim_sdk \
-    simple_blobstore_server
+echo "source 'https://s3.amazonaws.com/bosh-ci-pipeline/gems/'" > gemfile_tmp
 
-bosh -v
-rake $@
+sed "s/:path.*$/\"~>1.5.0.pre3\"/g" Gemfile >> gemfile_tmp
+
+mv gemfile_tmp Gemfile
+
+$script_dir/ci_build.sh $@
