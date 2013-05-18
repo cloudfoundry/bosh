8baa279b021bed01defa357a490db12864c80ecb
Chris Brown and Martin Englund <pair+cbrown+pmenglund@pivotallabs.com>
2013-05-17 17:29:21 -0700

    Make DataDog create alerts on submitted events
    [#50018413]
    
    DataDog was not designed for this. In order to make an event fire we
    change the 'value' of the event and then alert when it crosses a certain
    threshold. Unfortunately DataDog is not completely real time and there
    seems to be some weirdness in how they calculate their rolling averages.
    The process we use to trigger the event is as follows:
    
    1. Submit a regular DataDog event so that it shows up in the event
       stream.
    
    2. Submit a new set of metrics with an initial high value (10) to
       trigger the alert which is set to go off when the rolling average over
       the last 5 minutes is over a threshold (1).
    
    3. Wait 2 minutes to make sure that DataDog has had time to catch the
       change and trigger the alert.
    
    4. After the 2 minutes DataDog will continue through the other metrics
       in the initial set which contain a low value (0) and which fire 5 times
       over the next 10 minutes in order to aggressively bring the rolling
       average back to under 1 to end the alert.
    
    The high initial value is required so that we alway trigger the
    threshold crossing even if we have had recent '0' values which will
    interfere with the rolling average. The multiple tear down values are
    required to make sure that the rolling average comes down below the
    threshold inside the rolling window otherwise the alert never ends.

--- health_monitor/spec/unit/plugins/datadog_spec.rb
+++ health_monitor/spec/unit/plugins/datadog_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Bhm::Plugins::DataDog do
   subject { described_class.new("api_key" => "api_key", "application_key" => "application_key") }
   let(:dog_client) { double("DataDog Client") }
+
   before do
     subject.stub(dog_client: dog_client)
   end
-
 
   context "processing metrics" do
     it "sends datadog metrics" do
@@ -42,33 +42,69 @@ describe Bhm::Plugins::DataDog do
   end
 
   context "processing alerts" do
-    it "sends datadog alerts" do
-      EM.should_receive(:defer).and_yield
+    let(:creation_time) { Time.now.to_i - 10 }
 
-      time = Time.now.to_i - 10
+    before do
+      EM.should_receive(:defer).and_yield
+    end
+
+    it "sends datadog alerts" do
       fake_event = double("Datadog Event")
       Dogapi::Event.should_receive(:new).with do |msg, options|
         msg.should == "Everything is down"
         options[:msg_title].should == "Test Alert"
-        options[:date_happened].should == time
+        options[:date_happened].should == creation_time
         options[:tags].should =~ ["source:mysql_node/0"]
         options[:priority].should == "normal"
       end.and_return(fake_event)
 
+      dog_client.stub(:emit_points)
       dog_client.should_receive(:emit_event).with(fake_event)
 
-      alert = make_alert(created_at: time)
+      alert = make_alert(created_at: creation_time)
       subject.process(alert)
     end
 
-    it "sends datadog a low priority event for warning alerts" do
-      EM.should_receive(:defer).and_yield
+    describe "sending metrics to datadog" do
+      it "sends datadog a metric so that we can set up alerts on it" do
+        alert = make_alert(created_at: creation_time)
+        metric_id = "bosh.healthmonitor.alerts.test_alert"
 
+        datapoints = [[Time.at(creation_time), 10],
+                      [Time.at(creation_time)+1*60, 10],
+                      [Time.at(creation_time)+4*60, 0],
+                      [Time.at(creation_time)+5*60, 0],
+                      [Time.at(creation_time)+6*60, 0],
+                      [Time.at(creation_time)+7*60, 0],
+                      [Time.at(creation_time)+8*60, 0],
+                      [Time.at(creation_time)+9*60, 0]]
+
+        tags = ["source:mysql_node/0"]
+
+        dog_client.stub(:emit_event)
+        dog_client.should_receive(:emit_points).with(metric_id, datapoints, tags: tags)
+
+        subject.process(alert)
+      end
+
+      it "sanitizes strange characters in the title" do
+        alert = make_alert(title: "!!!TEST -  aler@t")
+        metric_id = "bosh.healthmonitor.alerts.test_alert"
+
+        dog_client.stub(:emit_event)
+        dog_client.should_receive(:emit_points).with(metric_id, anything, anything)
+
+        subject.process(alert)
+      end
+    end
+
+    it "sends datadog a low priority event for warning alerts" do
       Dogapi::Event.should_receive(:new).with do |_, options|
         options[:priority].should == "low"
       end
 
       dog_client.stub(:emit_event)
+      dog_client.stub(:emit_points)
 
       alert = make_alert(severity: 4)
       subject.process(alert)
