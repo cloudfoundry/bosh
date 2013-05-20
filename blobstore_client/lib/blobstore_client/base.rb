6e2d41843b282f19b82eef7cf40e295f9d0202cb
Alex Jackson and Matthew Boedicker <pair+ajackson+matthew@pivotallabs.com>
2013-05-20 12:27:29 -0700

    WIP DAV blobstore.

--- blobstore_client/lib/blobstore_client/base.rb
+++ blobstore_client/lib/blobstore_client/base.rb
@@ -77,6 +77,7 @@ module Bosh
 
       protected
 
+      # @return [String] the id
       def create_file(id, file)
         # needs to be implemented in each subclass
         raise "#create not implemented"
