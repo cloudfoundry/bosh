2451db85cef3a388ed38008cfaed0661a3387c57
Glenn Oppegard and Sean Sweda <pair+oppegard+ssweda@pivotallabs.com>
2013-05-22 14:59:58 -0700

    Clarify BUILD_FLOW_GIT_COMMIT usage

--- spec/ci_build.sh
+++ spec/ci_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
 set -e
 
+# BUILD_FLOW_GIT_COMMIT gets set in the bosh_build_flow jenkins jobs. This is used to ensure we check out
+# the same git commit for all jenkins jobs in the flow.
 if [ -n "$BUILD_FLOW_GIT_COMMIT" ]; then
     git checkout $BUILD_FLOW_GIT_COMMIT
 fi
