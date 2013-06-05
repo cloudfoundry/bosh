612340019cc491ccd12f456684f0dae1885a625e
Abhi Hiremagalur <abhi@pivotallabs.com>
2013-06-05 14:16:26 -0700

    Revert "Revert "Fill in bat manifest template.""
    This reverts commit 1ed69f76d7ef7f6dc5fa8cc9d6f48e2fedac0bd0.
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- bosh_cli_plugin_aws/spec/unit/bat_manifest_spec.rb
+++ bosh_cli_plugin_aws/spec/unit/bat_manifest_spec.rb
@@ -4,7 +4,7 @@ describe Bosh::Aws::BatManifest do
   let(:vpc_receipt) { Psych.load_file(asset "test-output.yml") }
   let(:vpc_config) { vpc_receipt['original_configuration'] }
   let(:route53_receipt) { Psych.load_file(asset "test-aws_route53_receipt.yml") }
-  let(:stemcell_version) { '1.1.1.pre' }
+  let(:stemcell_version) { 'latest' }
   let(:manifest) { Bosh::Aws::BatManifest.new(vpc_receipt, route53_receipt, stemcell_version, 'deadbeef') }
 
   it 'returns the stemcell_version' do
@@ -39,14 +39,73 @@ describe Bosh::Aws::BatManifest do
       spec = manifest.to_y
       spec.should == <<YAML
 ---
+name: bat
+director_uuid: deadbeef
 cpi: aws
+
+release:
+  name: bat
+  version: latest
+
+resource_pools:
+- name: default
+  stemcell:
+    name: bosh-stemcell
+    version: latest
+  network: default
+  size: 1
+  cloud_properties:
+    instance_type: m1.small
+    availability_zone: us-east-1a
+
+compilation:
+  reuse_compilation_vms: true
+  workers: 8
+  network: default
+  cloud_properties:
+    instance_type: c1.medium
+    availability_zone: us-east-1a
+
+update:
+  canaries: 1
+  canary_watch_time: 3000-90000
+  update_watch_time: 3000-90000
+  max_in_flight: 1
+  max_errors: 1
+
+networks:
+
+- name: default
+  type: manual
+  subnets:
+  - range: 10.10.0.0/24
+    reserved:
+    - 10.10.0.2 - 10.10.0.9
+    static:
+    - 10.10.0.10 - 10.10.0.30
+    gateway: 10.10.0.1
+    security_groups:
+    - bat
+    cloud_properties:
+      security_groups: bat
+      subnet: subnet-4bdf6c26
+
+jobs:
+- name: "batlight"
+  template: "batlight"
+  instances: 1
+  resource_pool: default
+  networks:
+  - name: default
+    default: [dns, gateway]
+
 properties:
   static_ip: 50.200.100.2
   uuid: deadbeef
   pool_size: 1
   stemcell:
     name: bosh-stemcell
-    version: 1.1.1.pre
+    version: latest
   instances: 1
   key_name:  dev102
   mbus: nats://nats:0b450ada9f830085e2cdeff6@micro.cfdev.com:4222
@@ -60,6 +119,9 @@ properties:
     subnet: subnet-4bdf6c26
     security_groups:
     - bat
+  batlight:
+    missing: nope
+
 YAML
     end
   end
