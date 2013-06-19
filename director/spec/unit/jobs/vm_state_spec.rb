2049366810df4782c851bf2972f5341d5e8fec13
Ferran Rodenas <frodenas@gmail.com>
2013-06-19 22:09:59 +0200

    [director] Return dns info for vms

--- director/spec/unit/jobs/vm_state_spec.rb
+++ director/spec/unit/jobs/vm_state_spec.rb
@@ -8,6 +8,7 @@ describe Bosh::Director::Jobs::VmState do
     @deployment = BD::Models::Deployment.make
     @result_file = mock("result_file")
     BD::Config.stub!(:result).and_return(@result_file)
+    BD::Config.stub!(:dns_domain_name).and_return("microbosh")    
   end
 
   it "parses agent info into vm_state" do
@@ -25,6 +26,7 @@ describe Bosh::Director::Jobs::VmState do
     @result_file.should_receive(:write).with do |agent_status|
       status = JSON.parse(agent_status)
       status["ips"].should == ["1.1.1.1"]
+      status["dns"].should be_empty
       status["vm_cid"].should == "vm-1"
       status["agent_id"].should == "agent-1"
       status["job_state"].should == "running"
@@ -62,6 +64,7 @@ describe Bosh::Director::Jobs::VmState do
     @result_file.should_receive(:write).with do |agent_status|
       status = JSON.parse(agent_status)
       status["ips"].should == ["1.1.1.1"]
+      status["dns"].should be_empty
       status["vm_cid"].should == "vm-1"
       status["agent_id"].should == "agent-1"
       status["job_state"].should == "running"
@@ -75,6 +78,35 @@ describe Bosh::Director::Jobs::VmState do
     end
 
     job = BD::Jobs::VmState.new(@deployment.id, "full")
+    job.perform
+  end
+
+  it "should return DNS A records if they exist" do
+    BDM::Vm.make(:deployment => @deployment, :agent_id => "agent-1", :cid => "vm-1")
+    domain = BD::Models::Dns::Domain.make(:name => "microbosh", :type => "NATIVE")
+    BD::Models::Dns::Record.make(:domain => domain, :name => "index.job.network.deployment.microbosh",
+                                 :type => "A", :content => "1.1.1.1", :ttl => 14400)
+    agent = mock("agent")
+    BD::AgentClient.stub!(:new).with("agent-1", :timeout => 5).and_return(agent)
+    agent_state = { "vm_cid" => "vm-1",
+                    "networks" => {"test" => {"ip" => "1.1.1.1"}},
+                    "agent_id" => "agent-1",
+                    "job_state" => "running",
+                    "resource_pool" => {"name" => "test_resource_pool" }}
+    agent.should_receive(:get_state).and_return(agent_state)
+
+    @result_file.should_receive(:write).with do |agent_status|
+      status = JSON.parse(agent_status)
+      status["ips"].should == ["1.1.1.1"]
+      status["dns"].should == ["index.job.network.deployment.microbosh"]
+      status["vm_cid"].should == "vm-1"
+      status["agent_id"].should == "agent-1"
+      status["job_state"].should == "running"
+      status["resource_pool"].should == "test_resource_pool"
+      status["vitals"].should be_nil
+    end
+
+    job = BD::Jobs::VmState.new(@deployment.id, nil)
     job.perform
   end
 
