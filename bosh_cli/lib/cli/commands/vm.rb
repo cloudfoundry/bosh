2178fc94fe82a8419ce7a77d437b662e9df78198
Chris Brown <cb@pivotallabs.com>
2013-06-21 09:40:34 -0700

    Allow vm command to not need to take index if instance is unique
    Signed-off-by: Colin Humphreys <hatofmonkeys@gmail.com>

--- bosh_cli/lib/cli/commands/vm.rb
+++ bosh_cli/lib/cli/commands/vm.rb
@@ -1,9 +1,16 @@
 module Bosh::Cli::Command
   class Vm < Base
     # bosh vm
-    usage "vm resurrection"
-    desc "Enable/Disable resurrection for a given vm"
-    def resurrection_state(job, index, resurrection_value)
+    usage 'vm resurrection'
+    desc 'Enable/Disable resurrection for a given vm'
+    def resurrection_state(*args)
+      job, index, resurrection_value = parse_args(args)
+      job_must_exist_in_deployment(job)
+
+      index = 0 if job_unique_in_deployment?(job)
+      err('You should specify the job index. There is more than one instance of this job type.') if index.nil?
+
+      resurrection_value = resurrection_value.first
       manifest = prepare_deployment_manifest
       director.change_vm_resurrection(manifest['name'], job, index, resurrection_paused(resurrection_value))
     end
