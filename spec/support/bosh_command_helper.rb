0e385af6734960be8db49110c5e699b5c46d46ab
Jeffrey Peckham and aram price <pair+peckham+aprice@pivotallabs.com>
2013-07-05 12:37:42 -0700

    ci external tests to use bosh_cli from bundled environment

--- spec/support/bosh_command_helper.rb
+++ spec/support/bosh_command_helper.rb
@@ -1,19 +1,17 @@
 module Bosh::Spec
   module CommandHelper
     def run(cmd)
-      Bundler.with_clean_env do
-        lines = []
-        IO.popen(cmd).each do |line|
-          puts line.chomp
-          lines << line.chomp
-        end.close # force the process to close so that $? is set
+      lines = []
+      IO.popen(cmd).each do |line|
+        puts line.chomp
+        lines << line.chomp
+      end.close # force the process to close so that $? is set
 
-        cmd_out = lines.join("\n")
-        if $?.success?
-          return cmd_out
-        else
-          raise "Failed: '#{cmd}' from #{Dir.pwd}, with exit status #{$?.to_i}\n\n #{cmd_out}"
-        end
+      cmd_out = lines.join("\n")
+      if $?.success?
+        return cmd_out
+      else
+        raise "Failed: '#{cmd}' from #{Dir.pwd}, with exit status #{$?.to_i}\n\n #{cmd_out}"
       end
     end
 
