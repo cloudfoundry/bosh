6644012ff1c08cb0f5f841c8aec6ece1d4d63572
Abhi Hiremagalur and Jeff Schnitzer <pair+abhi+jschnitzer@pivotallabs.com>
2013-07-08 16:46:01 -0700

    Use single quotes for uninterpolated strings

--- director/spec/unit/tar_gzipper_spec.rb
+++ director/spec/unit/tar_gzipper_spec.rb
@@ -1,21 +1,21 @@
-require "spec_helper"
+require 'spec_helper'
 
-require "tmpdir"
-require "fileutils"
+require 'tmpdir'
+require 'fileutils'
 
 describe Bosh::Director::TarGzipper do
   let(:src) { Dir.mktmpdir }
-  let(:dest) { Tempfile.new("logs").path }
+  let(:dest) { Tempfile.new('logs').path }
   let(:tar_gzipper) { described_class.new }
 
   before do
-    path = File.join(src, "var", "vcap", "sys", "log1")
+    path = File.join(src, 'var', 'vcap', 'sys', 'log1')
     FileUtils.mkdir_p(path)
-    File.write(File.join(path, "hello.log"), "hello")
+    File.write(File.join(path, 'hello.log'), 'hello')
 
-    path = File.join(src, "var", "vcap", "sys", "log2")
+    path = File.join(src, 'var', 'vcap', 'sys', 'log2')
     FileUtils.mkdir_p(path)
-    File.write(File.join(path, "goodbye.log"), "goodbye")
+    File.write(File.join(path, 'goodbye.log'), 'goodbye')
   end
 
   after do
@@ -23,57 +23,57 @@ describe Bosh::Director::TarGzipper do
     FileUtils.rm_rf(dest)
   end
 
-  context "if the source directory does not exist" do
-    let(:src) { "/tmp/this/is/not/here" }
+  context 'if the source directory does not exist' do
+    let(:src) { '/tmp/this/is/not/here' }
 
     before do
       FileUtils.rm_rf(src)
     end
 
-    it "raises an error" do
+    it 'raises an error' do
       expect {
         tar_gzipper.compress(src, dest)
       }.to raise_error(Bosh::Director::TarGzipper::SourceNotFound, "The source directory #{src} could not be found.")
     end
   end
 
-  context "if the source directory is not absolute" do
-    let(:src) { "tmp/file" }
+  context 'if the source directory is not absolute' do
+    let(:src) { 'tmp/file' }
 
-    it "raises an error" do
+    it 'raises an error' do
       expect {
         tar_gzipper.compress(src, dest)
       }.to raise_error(Bosh::Director::TarGzipper::SourceNotAbsolute, "The source directory #{src} is not an absolute path.")
     end
   end
 
-  it "packages the source directory into the destination tarball" do
-    tar_gzipper.stub(tar_path: "tar")
+  it 'packages the source directory into the destination tarball' do
+    tar_gzipper.stub(tar_path: 'tar')
     tar_gzipper.compress(src, dest)
 
     `tar tzvf #{dest}`.should match(%r{log1/hello.log})
   end
 
-  it "moves the sources files to a temporary directory first to avoid errors if we change the files as we are tarring them" do
-    tar_gzipper.stub(tar_path: "tar")
+  it 'moves the sources files to a temporary directory first to avoid errors if we change the files as we are tarring them' do
+    tar_gzipper.stub(tar_path: 'tar')
     tar_gzipper.compress(src, dest)
 
-    `tar tzvf #{dest}`.should include("bosh_tgz")
+    `tar tzvf #{dest}`.should include('bosh_tgz')
   end
 
-  it "uses the correct tar path" do
-    command_runner = double("command runner")
+  it 'uses the correct tar path' do
+    command_runner = double('command runner')
     tar_gzipper.command_runner = command_runner
     command_runner.should_receive(:sh).with(%r{^/bin/tar })
 
     tar_gzipper.compress(src, dest)
   end
 
-  context "if multiple source directories are specified" do
+  context 'if multiple source directories are specified' do
     let(:sources) { ["#{src}/var/vcap/sys/log1", "#{src}/var/vcap/sys/log2"]}
 
-    it "packages the list of source directories into the destination tarball" do
-      tar_gzipper.stub(tar_path: "tar")
+    it 'packages the list of source directories into the destination tarball' do
+      tar_gzipper.stub(tar_path: 'tar')
       tar_gzipper.compress(sources, dest)
 
       tar_cmd = `tar tzvf #{dest}`
