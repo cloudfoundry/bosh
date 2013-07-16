a315e5e1335f337a63e56e550aff721f4303d11f
aram price <pair+aprice@pivotallabs.com>
2013-07-16 13:28:45 -0700

    "module Bosh; module Dev" becomes "module Bosh::Dev"

--- bosh-dev/lib/bosh/dev/build.rb
+++ bosh-dev/lib/bosh/dev/build.rb
@@ -1,70 +1,68 @@
 require 'bosh/dev/pipeline'
 
-module Bosh
-  module Dev
-    class Build
-      attr_reader :number
+module Bosh::Dev
+  class Build
+    attr_reader :number
 
-      def initialize(number)
-        @number = number
-        @job_name = ENV.fetch('JOB_NAME')
-        @pipeline = Pipeline.new
-      end
+    def initialize(number)
+      @number = number
+      @job_name = ENV.fetch('JOB_NAME')
+      @pipeline = Pipeline.new
+    end
 
-      def self.current
-        new(ENV.fetch('BUILD_NUMBER'))
-      end
+    def self.current
+      new(ENV.fetch('BUILD_NUMBER'))
+    end
 
-      def self.candidate
-        new(ENV.fetch('CANDIDATE_BUILD_NUMBER'))
-      end
+    def self.candidate
+      new(ENV.fetch('CANDIDATE_BUILD_NUMBER'))
+    end
 
-      def upload(release)
-        pipeline.s3_upload(release.tarball, "release/bosh-#{number}.tgz")
-      end
+    def upload(release)
+      pipeline.s3_upload(release.tarball, "release/bosh-#{number}.tgz")
+    end
 
-      def s3_release_url
-        File.join(s3_pipeline_uri, "release/bosh-#{number}.tgz")
-      end
+    def s3_release_url
+      File.join(s3_pipeline_uri, "release/bosh-#{number}.tgz")
+    end
 
-      def promote_artifacts(aws_credentials)
-        sync_buckets
-        update_light_micro_bosh_ami_pointer_file(aws_credentials[:access_key_id], aws_credentials[:secret_access_key])
-      end
+    def promote_artifacts(aws_credentials)
+      sync_buckets
+      update_light_micro_bosh_ami_pointer_file(aws_credentials[:access_key_id], aws_credentials[:secret_access_key])
+    end
 
-      def sync_buckets
-        Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'gems')} s3://bosh-jenkins-gems")
+    def sync_buckets
+      Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'gems')} s3://bosh-jenkins-gems")
 
-        Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'release')} s3://bosh-jenkins-artifacts")
-        Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'bosh-stemcell')} s3://bosh-jenkins-artifacts")
-        Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'micro-bosh-stemcell')} s3://bosh-jenkins-artifacts")
-      end
+      Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'release')} s3://bosh-jenkins-artifacts")
+      Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'bosh-stemcell')} s3://bosh-jenkins-artifacts")
+      Rake::FileUtilsExt.sh("s3cmd sync #{File.join(s3_pipeline_uri, 'micro-bosh-stemcell')} s3://bosh-jenkins-artifacts")
+    end
 
-      def update_light_micro_bosh_ami_pointer_file(access_key_id, secret_access_key)
-        pipeline.download_latest_stemcell(infrastructure: 'aws', name: 'micro-bosh-stemcell', light: true)
+    def update_light_micro_bosh_ami_pointer_file(access_key_id, secret_access_key)
+      pipeline.download_latest_stemcell(infrastructure: 'aws', name: 'micro-bosh-stemcell', light: true)
 
-        stemcell = Bosh::Dev::Stemcell.new(pipeline.latest_stemcell_filename('aws', 'micro-bosh-stemcell', true))
+      stemcell = Bosh::Dev::Stemcell.new(pipeline.latest_stemcell_filename('aws', 'micro-bosh-stemcell', true))
 
-        connection = fog_storage(access_key_id, secret_access_key)
-        directory = connection.directories.create(key: 'bosh-jenkins-artifacts')
-        directory.files.create(key: 'last_successful_micro-bosh-stemcell-aws_ami_us-east-1',
-                               body: stemcell.ami_id,
-                               acl: 'public-read')
-      end
+      connection = fog_storage(access_key_id, secret_access_key)
+      directory = connection.directories.create(key: 'bosh-jenkins-artifacts')
+      directory.files.create(key: 'last_successful_micro-bosh-stemcell-aws_ami_us-east-1',
+                             body: stemcell.ami_id,
+                             acl: 'public-read')
+    end
 
-      def fog_storage(access_key_id, secret_access_key)
-        Fog::Storage.new(provider: 'AWS',
-                         aws_access_key_id: access_key_id,
-                         aws_secret_access_key: secret_access_key)
-      end
+    def fog_storage(access_key_id, secret_access_key)
+      Fog::Storage.new(provider: 'AWS',
+                       aws_access_key_id: access_key_id,
+                       aws_secret_access_key: secret_access_key)
+    end
 
-      private
+    private
 
-      attr_reader :pipeline, :job_name
+    attr_reader :pipeline, :job_name
 
-      def s3_pipeline_uri
-        "s3://#{pipeline.bucket}/"
-      end
+    def s3_pipeline_uri
+      "s3://#{pipeline.bucket}/"
     end
   end
 end
--- bosh-dev/lib/bosh/dev/pipeline.rb
+++ bosh-dev/lib/bosh/dev/pipeline.rb
@@ -1,83 +1,82 @@
 require 'fog'
 require 'logger'
 
-module Bosh
-  module Dev
-    class Pipeline
+module Bosh::Dev
+  class Pipeline
 
-      def initialize(options={})
-        @fog_storage = options.fetch(:fog_storage) do
-          fog_options = {
-              provider: 'AWS',
-              aws_access_key_id: ENV.to_hash.fetch('AWS_ACCESS_KEY_ID_FOR_STEMCELLS_JENKINS_ACCOUNT'),
-              aws_secret_access_key: ENV.to_hash.fetch('AWS_SECRET_ACCESS_KEY_FOR_STEMCELLS_JENKINS_ACCOUNT')
-          }
-          Fog::Storage.new(fog_options)
+    def initialize(options={})
+      @fog_storage = options.fetch(:fog_storage) do
+        fog_options = {
+            provider: 'AWS',
+            aws_access_key_id: ENV.to_hash.fetch('AWS_ACCESS_KEY_ID_FOR_STEMCELLS_JENKINS_ACCOUNT'),
+            aws_secret_access_key: ENV.to_hash.fetch('AWS_SECRET_ACCESS_KEY_FOR_STEMCELLS_JENKINS_ACCOUNT')
+        }
+        Fog::Storage.new(fog_options)
+      end
+      @logger = options.fetch(:logger) { Logger.new(STDOUT) }
+    end
+
+    def publish_stemcell(stemcell)
+      latest_filename = latest_stemcell_filename(stemcell.infrastructure, stemcell.name, stemcell.light?)
+      s3_latest_path = File.join(stemcell.name, stemcell.infrastructure, latest_filename)
+
+      s3_path = File.join(stemcell.name, stemcell.infrastructure, File.basename(stemcell.path))
+      s3_upload(stemcell.path, s3_path)
+      s3_upload(stemcell.path, s3_latest_path)
+    end
+
+    def bucket
+      'bosh-ci-pipeline'
+    end
+
+    def s3_upload(file, remote_path)
+      directory = fog_storage.directories.get(bucket)
+      directory.files.create(key: remote_path, body: File.open(file))
+      logger.info("uploaded '#{file}' -> s3://#{bucket}/#{remote_path}")
+    end
+
+    def download_stemcell(version, options={})
+      infrastructure = options.fetch(:infrastructure)
+      name = options.fetch(:name)
+      light = options.fetch(:light)
+
+      filename = stemcell_filename(version, infrastructure, name, light)
+      bucket_files = fog_storage.directories.get(bucket).files
+
+      File.open(filename, 'w') do |file|
+        bucket_files.get(File.join(name, infrastructure, filename)) do |chunk|
+          file.write(chunk)
         end
-        @logger = options.fetch(:logger) { Logger.new(STDOUT) }
       end
 
-      def publish_stemcell(stemcell)
-        latest_filename = latest_stemcell_filename(stemcell.infrastructure, stemcell.name, stemcell.light?)
-        s3_latest_path = File.join(stemcell.name, stemcell.infrastructure, latest_filename)
+      logger.info("downloaded 's3://#{bucket}/#{File.join(name, infrastructure, filename)}' -> '#{filename}'")
+    end
 
-        s3_path = File.join(stemcell.name, stemcell.infrastructure, File.basename(stemcell.path))
-        s3_upload(stemcell.path, s3_path)
-        s3_upload(stemcell.path, s3_latest_path)
-      end
+    def download_latest_stemcell(options={})
+      infrastructure = options.fetch(:infrastructure)
+      name = options.fetch(:name)
+      light = options.fetch(:light, false)
 
-      def bucket
-        'bosh-ci-pipeline'
-      end
+      download_stemcell('latest', infrastructure: infrastructure, name: name, light: light)
+    end
 
-      def s3_upload(file, remote_path)
-        directory = fog_storage.directories.get(bucket)
-        directory.files.create(key: remote_path, body: File.open(file))
-        logger.info("uploaded '#{file}' -> s3://#{bucket}/#{remote_path}")
-      end
+    def latest_stemcell_filename(infrastructure, name, light)
+      stemcell_filename('latest', infrastructure, name, light)
+    end
 
-      def download_stemcell(version, options={})
-        infrastructure = options.fetch(:infrastructure)
-        name           = options.fetch(:name)
-        light          = options.fetch(:light)
+    private
 
-        filename = stemcell_filename(version, infrastructure, name, light)
-        bucket_files = fog_storage.directories.get(bucket).files
+    attr_reader :fog_storage, :logger
 
-        File.open(filename, 'w') do |file|
-          bucket_files.get(File.join(name, infrastructure, filename)) do |chunk|
-            file.write(chunk)
-          end
-        end
+    def stemcell_filename(version, infrastructure, name, light)
+      stemcell_filename_parts = []
+      stemcell_filename_parts << version if version == 'latest'
+      stemcell_filename_parts << 'light' if light
+      stemcell_filename_parts << name
+      stemcell_filename_parts << infrastructure
+      stemcell_filename_parts << version unless version == 'latest'
 
-        logger.info("downloaded 's3://#{bucket}/#{File.join(name, infrastructure, filename)}' -> '#{filename}'")
-      end
-
-      def download_latest_stemcell(options={})
-        infrastructure = options.fetch(:infrastructure)
-        name           = options.fetch(:name)
-        light          = options.fetch(:light, false)
-
-        download_stemcell('latest', infrastructure: infrastructure, name: name, light: light)
-      end
-
-      def latest_stemcell_filename(infrastructure, name, light)
-        stemcell_filename('latest', infrastructure, name, light)
-      end
-
-      private
-      attr_reader :fog_storage, :logger
-
-      def stemcell_filename(version, infrastructure, name, light)
-        stemcell_filename_parts = []
-        stemcell_filename_parts << version if version == 'latest'
-        stemcell_filename_parts << 'light' if light
-        stemcell_filename_parts << name
-        stemcell_filename_parts << infrastructure
-        stemcell_filename_parts << version unless version == 'latest'
-
-        "#{stemcell_filename_parts.join('-')}.tgz"
-      end
+      "#{stemcell_filename_parts.join('-')}.tgz"
     end
   end
 end
