4b1223a20fa0bac9c6cc88eea354c8d0abfccea3
Jeff Schnitzer and Jeffrey Peckham <pair+jschnitzer+peckham@pivotallabs.com>
2013-07-16 11:02:04 -0700

    TarGzipper can optionally copy files before archiving
    * This gets around the problem with logs being written as they are being
      tarred.

--- director/spec/unit/tar_gzipper_spec.rb
+++ director/spec/unit/tar_gzipper_spec.rb
@@ -7,7 +7,9 @@ describe Bosh::Director::TarGzipper do
   let(:base_dir) { Dir.mktmpdir }
   let(:sources) { %w(1 one) }
   let(:dest) { Tempfile.new('logs') } #must keep tempfile reference lest it rm
-  let(:errored_status) { double('Status', success?: false, exitstatus: 5) }
+  let(:errored_retval) { ['stdout string', 'a stderr message', double('Status', success?: false, exitstatus: 5)] }
+  let(:success_retval) { ['', '', double('Status', success?: true)] }
+
 
   before do
     path = File.join(base_dir, '1', '2')
@@ -22,6 +24,24 @@ describe Bosh::Director::TarGzipper do
   after do
     FileUtils.rm_rf(base_dir)
     FileUtils.rm_rf(dest.path)
+  end
+
+  context 'when copy first feature is enabled' do
+    it 'copies the files to a temp directory before archiving' do
+      Dir.stub(:mktmpdir).and_yield('/tempthing')
+      FileUtils.should_receive(:cp_r).with(%w(/foo/baz /foo/bar), '/tempthing/')
+      Pathname.stub(:new).and_return(instance_double('Pathname', exist?: true, absolute?: true))
+
+      Open3.should_receive(:capture3).with(*%w(tar -C /tempthing -czf /tmp/backup.tgz baz bar)).and_return(success_retval)
+
+      subject.compress('/foo', %w(baz bar), '/tmp/backup.tgz', copy_first: true)
+    end
+  end
+
+  it 'raises when a source has a path depth greater than 1' do # sources that contain a '/'
+    expect {
+      subject.compress('/var/vcap/foo', %w(foo/bar), '/tmp/backup.tgz')
+    }.to raise_error("Sources must have a path depth of 1 and contain no '/'")
   end
 
   context 'if the source directory does not exist' do
@@ -55,7 +75,7 @@ describe Bosh::Director::TarGzipper do
   end
 
   it 'raises if it fails to tar' do
-    Open3.stub(:capture3).and_return(['stdout string', 'a stderr message', errored_status])
+    Open3.stub(:capture3).and_return(errored_retval)
     expect{subject.compress(base_dir, sources, dest)}.to raise_error(RuntimeError, "tar exited 5, output: 'stdout string', error: 'a stderr message'")
   end
 end
