a942156f894cdf5a0ff2331d81f092bca6bb7171
aram price <pair+aprice@pivotallabs.com>
2013-07-16 10:56:06 -0700

    move RSpecRakeHelper into spec/support/

---
+++ bosh-dev/spec/support/rspec_rake_helper.rb
@@ -0,0 +1,24 @@
+shared_context 'rake' do
+  let(:rake) { Rake::Application.new }
+  let(:task_name) { self.class.top_level_description }
+  let(:task_path) { "rake/lib/tasks/#{task_name.split(':').first}" }
+  let(:root) { File.expand_path('../../../../', File.dirname(__FILE__))}
+  subject { rake[task_name] }
+
+  def loaded_files_excluding_current_rake_file
+    $".reject { |file| file == File.join(root, "#{task_path}.rake").to_s }
+  end
+
+  before do
+    Rake.application = rake
+    Rake.application.rake_require(task_path, [root], loaded_files_excluding_current_rake_file)
+
+    Rake::Task.define_task(:environment)
+  end
+end
+
+module RSpecRakeHelper
+  def self.included(klass)
+    klass.include_context('rake')
+  end
+end
