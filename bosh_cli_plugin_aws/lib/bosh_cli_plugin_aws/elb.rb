a6fbce06cb5ab6bf943e296ed5281a81097ed490
Abhijit Hiremagalur and Matthew Boedicker <pair+abhi+mboedicker@pivotallabs.com>
2013-07-29 16:30:17 -0700

    Use Ruby 1.9 style hashes & single quotes per new styleguide

--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/elb.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/elb.rb
@@ -1,7 +1,7 @@
 module Bosh::Aws
   class ELB
-
-    class BadCertificateError < RuntimeError; end
+    class BadCertificateError < RuntimeError;
+    end
 
     def initialize(credentials)
       @credentials = credentials
@@ -10,54 +10,54 @@ module Bosh::Aws
     end
 
     def create(name, vpc, settings, certs)
-      subnet_names = settings["subnets"]
+      subnet_names = settings['subnets']
       subnet_ids = vpc.subnets.select { |k, v| subnet_names.include?(k) }.values
-      security_group_name = settings["security_group"]
+      security_group_name = settings['security_group']
       security_group_id = vpc.security_group_by_name(security_group_name).id
       options = {
-          :listeners => [{
-                             :port => 80,
-                             :protocol => :http,
-                             :instance_port => 80,
-                             :instance_protocol => :http,
-                         }],
-          :subnets => subnet_ids,
-          :security_groups => [security_group_id]
+        :listeners => [{
+                         port: 80,
+                         protocol: :http,
+                         instance_port: 80,
+                         instance_protocol: :http,
+                       }],
+        :subnets => subnet_ids,
+        :security_groups => [security_group_id]
       }
 
-      if settings["https"]
-        domain = settings["domain"]
-        cert_name = settings["ssl_cert"]
+      if settings['https']
+        domain = settings['domain']
+        cert_name = settings['ssl_cert']
         cert = certs[cert_name]
-        dns_record = settings["dns_record"]
+        dns_record = settings['dns_record']
 
         certificate = Bosh::Ssl::Certificate.new(cert['private_key_path'],
-                                                       cert['certificate_path'],
-                                                       "#{dns_record}.#{domain}",
-                                                       cert['certificate_chain_path']
+                                                 cert['certificate_path'],
+                                                 "#{dns_record}.#{domain}",
+                                                 cert['certificate_chain_path']
         ).load_or_create
 
         uploaded_cert = upload_certificate(cert_name, certificate)
 
         options[:listeners] << {
-            :port => 443,
-            :protocol => :https,
-            :instance_port => 80,
-            :instance_protocol => :http,
-            # passing through 'ssl_certificate_id' is undocumented, but we're
-            # working around a bug filed here: https://github.com/aws/aws-sdk-ruby/issues/216
-            :ssl_certificate_id => uploaded_cert.arn
+          :port => 443,
+          :protocol => :https,
+          :instance_port => 80,
+          :instance_protocol => :http,
+          # passing through 'ssl_certificate_id' is undocumented, but we're
+          # working around a bug filed here: https://github.com/aws/aws-sdk-ruby/issues/216
+          :ssl_certificate_id => uploaded_cert.arn
         }
       end
 
       Bosh::Common.retryable(tries: 15, on: AWS::ELB::Errors::CertificateNotFound) do
         aws_elb.load_balancers.create(name, options).tap do |new_elb|
           new_elb.configure_health_check({
-                                             :healthy_threshold => 5,
-                                             :unhealthy_threshold => 2,
-                                             :interval => 5,
-                                             :timeout => 2,
-                                             :target => "TCP:80"
+                                           :healthy_threshold => 5,
+                                           :unhealthy_threshold => 2,
+                                           :interval => 5,
+                                           :timeout => 2,
+                                           :target => 'TCP:80'
                                          })
         end
       end
@@ -90,9 +90,9 @@ module Bosh::Aws
     def upload_certificate(name, cert)
       certificates = aws_iam.server_certificates
       options = {
-          name: name,
-          certificate_body: cert.certificate,
-          private_key: cert.key
+        name: name,
+        certificate_body: cert.certificate,
+        private_key: cert.key
       }
 
       options[:certificate_chain] = cert.chain if cert.chain
@@ -118,6 +118,5 @@ module Bosh::Aws
         raise BadCertificateError.new("Unable to upload ELB SSL Certificate: #{e.message}\n#{message}")
       end
     end
-
   end
 end
--- bosh_cli_plugin_aws/spec/unit/elb_spec.rb
+++ bosh_cli_plugin_aws/spec/unit/elb_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Bosh::Aws::ELB do
-  let(:creds) { {'my' => 'creds'} }
+  let(:creds) { { 'my' => 'creds' } }
   let(:elb) { described_class.new(creds) }
   let(:ec2) { Bosh::Aws::EC2.new({}) }
   let(:fake_aws_security_group) { double('security_group', id: 'sg_id', name: 'security_group_name') }
@@ -18,11 +18,11 @@ describe Bosh::Aws::ELB do
 
   describe 'creation' do
     let(:new_elb) { double('a new elb') }
-    let(:cert) { {'certificate_path' => asset('ca/ca.pem'), 'private_key_path' => asset('ca/ca.key'), 'certificate_chain_path' => asset('ca/chain.pem')} }
+    let(:cert) { { 'certificate_path' => asset('ca/ca.pem'), 'private_key_path' => asset('ca/ca.key'), 'certificate_chain_path' => asset('ca/chain.pem') } }
     let(:cert_name) { 'my-cert-name' }
-    let(:http_listener) { {port: 80, protocol: :http, instance_port: 80, instance_protocol: :http} }
-    let(:https_listener) { {port: 443, protocol: :https, instance_port: 80, instance_protocol: :http, ssl_certificate_id: 'certificate_arn'} }
-    let(:certs) { {cert_name => cert} }
+    let(:http_listener) { { port: 80, protocol: :http, instance_port: 80, instance_protocol: :http } }
+    let(:https_listener) { { port: 443, protocol: :https, instance_port: 80, instance_protocol: :http, ssl_certificate_id: 'certificate_arn' } }
+    let(:certs) { { cert_name => cert } }
     let(:certificate) { double(AWS::IAM::ServerCertificate, name: 'elb-cfrouter', arn: 'certificate_arn') }
     let(:certificates) { double(AWS::IAM::ServerCertificateCollection, map: [cert_name]) }
 
@@ -30,7 +30,7 @@ describe Bosh::Aws::ELB do
       elb.stub(:aws_elb).and_return(fake_aws_elb)
       elb.stub(:aws_iam).and_return(fake_aws_iam)
 
-      vpc.stub(:subnets).and_return({'sub_name1' => 'sub_id1', 'sub_name2' => 'sub_id2'})
+      vpc.stub(:subnets).and_return({ 'sub_name1' => 'sub_id1', 'sub_name2' => 'sub_id2' })
       vpc.stub(:security_group_by_name).with('security_group_name').and_return(fake_aws_security_group)
 
       Bosh::Common.stub(:wait)
@@ -39,34 +39,34 @@ describe Bosh::Aws::ELB do
     describe 'is successful' do
       before do
         new_elb.should_receive(:configure_health_check).with({
-                                                                 :healthy_threshold => 5,
-                                                                 :unhealthy_threshold => 2,
-                                                                 :interval => 5,
-                                                                 :timeout => 2,
-                                                                 :target => 'TCP:80'
+                                                               :healthy_threshold => 5,
+                                                               :unhealthy_threshold => 2,
+                                                               :interval => 5,
+                                                               :timeout => 2,
+                                                               :target => 'TCP:80'
                                                              })
       end
 
       it 'can create an ELB given a name and a vpc and a CIDR block' do
         fake_aws_elb.load_balancers.should_receive(:create).with('my elb name', {
-            :listeners => [http_listener],
-            :subnets => %w[sub_id1 sub_id2],
-            :security_groups => %w[sg_id]
+          :listeners => [http_listener],
+          :subnets => %w[sub_id1 sub_id2],
+          :security_groups => %w[sg_id]
         }).and_return(new_elb)
-        elb.create('my elb name', vpc, {'subnets' => %w(sub_name1 sub_name2), 'security_group' => 'security_group_name'}, certs).should == new_elb
+        elb.create('my elb name', vpc, { 'subnets' => %w(sub_name1 sub_name2), 'security_group' => 'security_group_name' }, certs).should == new_elb
       end
 
       describe 'creating a new ELB that allows HTTPS' do
         before do
           fake_aws_elb.load_balancers.should_receive(:create).with('my elb name', {
-              listeners: [http_listener, https_listener],
-              subnets: ['sub_id1', 'sub_id2'],
-              security_groups: ['sg_id'],
+            listeners: [http_listener, https_listener],
+            subnets: ['sub_id1', 'sub_id2'],
+            security_groups: ['sg_id'],
           }).and_return(new_elb)
         end
 
         context 'if the certificate is self signed (has no certificate chain)' do
-          let(:cert) { {'certificate_path' => asset('ca/ca.pem'), 'private_key_path' => asset('ca/ca.key')} }
+          let(:cert) { { 'certificate_path' => asset('ca/ca.pem'), 'private_key_path' => asset('ca/ca.key') } }
 
           before do
             certificates.should_receive(:upload).with(anything) do |args|
@@ -78,44 +78,44 @@ describe Bosh::Aws::ELB do
           end
 
           it 'can create a new ELB that is configured to allow HTTPS' do
-            elb.create('my elb name', vpc, {'subnets' => %w(sub_name1 sub_name2),
-                                            'security_group' => 'security_group_name',
-                                            'https' => true,
-                                            'ssl_cert' => cert_name,
-                                            'dns_record' => 'myapp',
-                                            'domain' => 'dev102.cf.com'}, certs).should == new_elb
+            elb.create('my elb name', vpc, { 'subnets' => %w(sub_name1 sub_name2),
+                                             'security_group' => 'security_group_name',
+                                             'https' => true,
+                                             'ssl_cert' => cert_name,
+                                             'dns_record' => 'myapp',
+                                             'domain' => 'dev102.cf.com' }, certs).should == new_elb
           end
         end
 
-        context "when amazon fails to see that the certificate was uploaded already" do
-          it "tries to upload the certificate again" do
+        context 'when amazon fails to see that the certificate was uploaded already' do
+          it 'tries to upload the certificate again' do
             fake_aws_iam.should_receive(:server_certificates).and_return(certificates)
 
             certificates.stub(:map).and_return([], [cert_name])
             certificates.should_receive(:upload).twice.and_return(certificate)
 
-            elb.create('my elb name', vpc, {'subnets' => %w(sub_name1 sub_name2),
-                                            'security_group' => 'security_group_name',
-                                            'https' => true,
-                                            'ssl_cert' => cert_name,
-                                            'dns_record' => 'myapp',
-                                            'domain' => 'dev102.cf.com'}, certs)
+            elb.create('my elb name', vpc, { 'subnets' => %w(sub_name1 sub_name2),
+                                             'security_group' => 'security_group_name',
+                                             'https' => true,
+                                             'ssl_cert' => cert_name,
+                                             'dns_record' => 'myapp',
+                                             'domain' => 'dev102.cf.com' }, certs)
           end
         end
 
-        context "when amazon fails to see the certificate and then complains the certificate was already uploaded" do
-          it "uses the certificate that has been uploaded before" do
+        context 'when amazon fails to see the certificate and then complains the certificate was already uploaded' do
+          it 'uses the certificate that has been uploaded before' do
             fake_aws_iam.should_receive(:server_certificates).and_return(certificates)
 
             certificates.should_receive(:[]).with(cert_name).and_return(certificate)
             certificates.should_receive(:upload).and_raise(AWS::IAM::Errors::EntityAlreadyExists)
 
-            elb.create('my elb name', vpc, {'subnets' => %w(sub_name1 sub_name2),
-                                            'security_group' => 'security_group_name',
-                                            'https' => true,
-                                            'ssl_cert' => cert_name,
-                                            'dns_record' => 'myapp',
-                                            'domain' => 'dev102.cf.com'}, certs)
+            elb.create('my elb name', vpc, { 'subnets' => %w(sub_name1 sub_name2),
+                                             'security_group' => 'security_group_name',
+                                             'https' => true,
+                                             'ssl_cert' => cert_name,
+                                             'dns_record' => 'myapp',
+                                             'domain' => 'dev102.cf.com' }, certs)
           end
         end
 
@@ -130,31 +130,31 @@ describe Bosh::Aws::ELB do
           end
 
           it 'can create a new ELB that is configured to allow HTTPS' do
-            elb.create('my elb name', vpc, {'subnets' => %w(sub_name1 sub_name2),
-                                            'security_group' => 'security_group_name',
-                                            'https' => true,
-                                            'ssl_cert' => cert_name,
-                                            'dns_record' => 'myapp',
-                                            'domain' => 'dev102.cf.com'}, certs).should == new_elb
+            elb.create('my elb name', vpc, { 'subnets' => %w(sub_name1 sub_name2),
+                                             'security_group' => 'security_group_name',
+                                             'https' => true,
+                                             'ssl_cert' => cert_name,
+                                             'dns_record' => 'myapp',
+                                             'domain' => 'dev102.cf.com' }, certs).should == new_elb
           end
         end
       end
     end
 
     describe 'on failure' do
-      context "when amazon rejects our certificate" do
-        it "throws an error" do
+      context 'when amazon rejects our certificate' do
+        it 'throws an error' do
           fake_aws_iam.should_receive(:server_certificates).and_return(certificates)
 
           certificates.should_receive(:upload).any_number_of_times.and_raise(AWS::IAM::Errors::MalformedCertificate)
 
           expect do
-            elb.create('my elb name', vpc, {'subnets' => %w(sub_name1 sub_name2),
-                                            'security_group' => 'security_group_name',
-                                            'https' => true,
-                                            'ssl_cert' => cert_name,
-                                            'dns_record' => 'myapp',
-                                            'domain' => 'dev102.cf.com'}, certs)
+            elb.create('my elb name', vpc, { 'subnets' => %w(sub_name1 sub_name2),
+                                             'security_group' => 'security_group_name',
+                                             'https' => true,
+                                             'ssl_cert' => cert_name,
+                                             'dns_record' => 'myapp',
+                                             'domain' => 'dev102.cf.com' }, certs)
           end.to raise_error(Bosh::Aws::ELB::BadCertificateError,
                              /Unable to upload ELB SSL Certificate.*BEGIN CERTIFICATE/m)
         end
@@ -224,7 +224,7 @@ describe Bosh::Aws::ELB do
     end
   end
 
-  describe "server certificate names" do
+  describe 'server certificate names' do
     before do
       elb.stub(:aws_iam).and_return(fake_aws_iam)
     end
