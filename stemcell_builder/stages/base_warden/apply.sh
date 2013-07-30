e865ac65df09a573a8040bcc58b0ab69955b2931
Kai Xiang <kxiang@pivotallabs.com>
2013-07-30 15:50:27 +0800

    Remove unnecessary stemcell_base.tar.gz for warden stemcell build

--- stemcell_builder/stages/base_warden/apply.sh
+++ stemcell_builder/stages/base_warden/apply.sh
@@ -7,16 +7,6 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-tmpfile=`mktemp`
-
-echo "Creating base stemcell archive at $tmpfile"
-tar -C $chroot -czf $tmpfile .
-
-echo "Storing stemcell archive in chroot at $chroot/var/vcap/stemcell_base.tar.gz"
-mkdir -p $chroot/var/vcap
-mv $tmpfile $chroot/var/vcap/stemcell_base.tar.gz
-chmod 0700 $chroot/var/vcap/stemcell_base.tar.gz
-
 # Explicit make the mount point for bind-mount
 # Otherwise using none ubuntu host will fail creating vm
 mkdir -p $chroot/warden-cpi-dev
