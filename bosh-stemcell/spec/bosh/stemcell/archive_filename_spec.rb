7661f5308cf7e054c9fe937202352bf890247d13
Abhijit Hiremagalur and Jeffrey Peckham <pair+abhi+peckham@pivotallabs.com>
2013-08-02 16:32:33 -0700

    Backfilling tests for light stemcell archive names

--- bosh-stemcell/spec/bosh/stemcell/archive_filename_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/archive_filename_spec.rb
@@ -5,46 +5,70 @@ module Bosh
   module Stemcell
     describe ArchiveFilename do
       subject(:archive_filename) do
-        ArchiveFilename.new(version, infrastructure, 'bosh-stemcell', false)
+        ArchiveFilename.new(version, infrastructure, 'bosh-stemcell', light)
       end
 
       describe '#to_s' do
-        context 'when the infrastructure has a hypervisor' do
-          let(:infrastructure) { Bosh::Dev::Infrastructure::OpenStack.new }
+
+        context 'when stemcell is light' do
+          let(:light) { true }
+          let(:infrastructure) { Bosh::Dev::Infrastructure::Vsphere.new }
 
           context 'and the version is a build number' do
             let(:version) { 123 }
-
-            it 'ends with the infrastructure, hypervisor and build number' do
-              expect(archive_filename.to_s).to eq('bosh-stemcell-openstack-kvm-123.tgz')
+            it 'prepends light before name' do
+              expect(archive_filename.to_s).to eq ('light-bosh-stemcell-vsphere-123.tgz')
             end
           end
 
           context 'and the version is latest' do
             let(:version) { 'latest' }
 
-            it 'begins with latest and ends with the infrastructure' do
-              expect(archive_filename.to_s).to eq('latest-bosh-stemcell-openstack.tgz')
+            it 'appends light after latest' do
+              expect(archive_filename.to_s).to eq ('latest-light-bosh-stemcell-vsphere.tgz')
             end
           end
         end
 
-        context 'when the infrastructure does not have a hypervisor' do
-          let(:infrastructure) { Bosh::Dev::Infrastructure::Aws.new }
+        context 'when stemcell is not light' do
+          let(:light) { false }
+          context 'when the infrastructure has a hypervisor' do
+            let(:infrastructure) { Bosh::Dev::Infrastructure::OpenStack.new }
 
-          context 'and the version is a build number' do
-            let(:version) { 123 }
+            context 'and the version is a build number' do
+              let(:version) { 123 }
 
-            it 'ends with the infrastructure and build number' do
-              expect(archive_filename.to_s).to eq('bosh-stemcell-aws-123.tgz')
+              it 'ends with the infrastructure, hypervisor and build number' do
+                expect(archive_filename.to_s).to eq('bosh-stemcell-openstack-kvm-123.tgz')
+              end
+            end
+
+            context 'and the version is latest' do
+              let(:version) { 'latest' }
+
+              it 'begins with latest and ends with the infrastructure' do
+                expect(archive_filename.to_s).to eq('latest-bosh-stemcell-openstack.tgz')
+              end
             end
           end
 
-          context 'and the version is latest' do
-            let(:version) { 'latest' }
+          context 'when the infrastructure does not have a hypervisor' do
+            let(:infrastructure) { Bosh::Dev::Infrastructure::Aws.new }
 
-            it 'begins with latest and ends with the infrastructure' do
-              expect(archive_filename.to_s).to eq('latest-bosh-stemcell-aws.tgz')
+            context 'and the version is a build number' do
+              let(:version) { 123 }
+
+              it 'ends with the infrastructure and build number' do
+                expect(archive_filename.to_s).to eq('bosh-stemcell-aws-123.tgz')
+              end
+            end
+
+            context 'and the version is latest' do
+              let(:version) { 'latest' }
+
+              it 'begins with latest and ends with the infrastructure' do
+                expect(archive_filename.to_s).to eq('latest-bosh-stemcell-aws.tgz')
+              end
             end
           end
         end
