b0aecae59cfb5e3cb8cdb49df6a2b6f38ac6f2e2
Abhijit Hiremagalur and Jeffrey Peckham <pair+abhi+peckham@pivotallabs.com>
2013-08-02 14:30:51 -0700

    Focus unit test

--- bosh-stemcell/spec/bosh/stemcell/light_stemcell_creator_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/light_stemcell_creator_spec.rb
@@ -3,69 +3,24 @@ require 'bosh/stemcell/light_stemcell_creator'
 
 module Bosh::Stemcell
   describe LightStemcellCreator do
-    describe '.create' do
+    let(:stemcell) { instance_double('Bosh::Stemcell::Stemcell') }
+    let(:light_stemcell) { instance_double('Bosh::Stemcell::Aws::LightStemcell', write_archive: nil, path: 'fake light stemcell path') }
+    let(:light_stemcell_stemcell) { instance_double('Bosh::Stemcell::Stemcell') }
 
-      let(:ami) do
-        instance_double('Bosh::Stemcell::Ami', publish: 'fake-ami-id', region: 'fake-region')
-      end
+    before do
+      Stemcell.stub(:new)
+      Aws::LightStemcell.stub(:new).and_return(light_stemcell)
+    end
 
-      let(:stemcell) do
-        Stemcell.new(spec_asset('micro-bosh-stemcell-aws.tgz'))
-      end
+    it 'writes a light stemcell archive with the specified stemcell' do
+      Bosh::Stemcell::Aws::LightStemcell.should_receive(:new).with(stemcell).and_return(light_stemcell)
+      light_stemcell.should_receive(:write_archive)
+      subject.create(stemcell)
+    end
 
-      before do
-        Bosh::Stemcell::Ami.stub(new: ami)
-        Rake::FileUtilsExt.stub(:sh)
-        FileUtils.stub(:touch)
-      end
-
-      it 'creates an ami from the stemcell' do
-        ami.should_receive(:publish)
-
-        LightStemcellCreator.create(stemcell)
-      end
-
-      it 'creates a new tgz' do
-        Rake::FileUtilsExt.should_receive(:sh) do |command|
-          command.should match(/tar xzf #{stemcell.path} --directory .*/)
-        end
-
-        expected_tarfile = File.join(File.dirname(stemcell.path), 'light-micro-bosh-stemcell-aws.tgz')
-
-        Rake::FileUtilsExt.should_receive(:sh) do |command|
-          command.should match(/sudo tar cvzf #{expected_tarfile} \*/)
-        end
-
-        LightStemcellCreator.create(stemcell)
-      end
-
-      it 'replaces the raw image with a blank placeholder' do
-        FileUtils.should_receive(:touch).and_return do |file, options|
-          expect(file).to match('image')
-          expect(options).to eq(verbose: true)
-        end
-        LightStemcellCreator.create(stemcell)
-      end
-
-      it 'adds the ami to the stemcell manifest' do
-        Psych.should_receive(:dump).and_return do |stemcell_properties, out|
-          expect(stemcell_properties['cloud_properties']['ami']).to eq({ 'fake-region' => 'fake-ami-id' })
-        end
-
-        LightStemcellCreator.create(stemcell)
-      end
-
-      it 'names the stemcell manifest correctly' do
-        # Example fails on linux without File.stub
-        File.stub(:open).and_call_original
-        File.should_receive(:open).with('stemcell.MF', 'w')
-
-        LightStemcellCreator.create(stemcell)
-      end
-
-      it 'returns a stemcell object' do
-        expect(LightStemcellCreator.create(stemcell)).to be_a Stemcell
-      end
+    it 'creates a stemcell pointing at this archive' do
+      Stemcell.should_receive(:new).with('fake light stemcell path').and_return(light_stemcell_stemcell)
+      expect(subject.create(stemcell)).to be(light_stemcell_stemcell)
     end
   end
 end
