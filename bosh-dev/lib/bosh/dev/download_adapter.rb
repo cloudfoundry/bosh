5f97c14a1bcdf6edca4e9f5f215cb996eb0115d9
Matthew Boedicker and Rob Day-Reynolds <pair+mboedicker+rdayreynolds@pivotallabs.com>
2013-08-06 14:46:04 -0700

    Created download and upload adapters to replace pipeline functionality

---
+++ bosh-dev/lib/bosh/dev/download_adapter.rb
@@ -0,0 +1,17 @@
+module Bosh::Dev
+  class DownloadAdapter
+    def download(uri, write_path)
+      Net::HTTP.start(uri.host) do |http|
+        http.request_get(uri.request_uri) do |response|
+          raise "remote file '#{uri}' not found" if response.kind_of? Net::HTTPNotFound
+
+          File.open(write_path, 'wb') do |file|
+            response.read_body do |chunk|
+              file.write(chunk)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ bosh-dev/spec/bosh/dev/download_adapter_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+require 'bosh/dev/download_adapter'
+require 'webmock/rspec'
+
+module Bosh::Dev
+  describe DownloadAdapter do
+    describe '#download' do
+      include FakeFS::SpecHelpers
+
+      let(:adapter) {DownloadAdapter.new}
+
+      before(:each) do
+        FileUtils.mkdir('/tmp')
+      end
+
+      let(:uri) { URI('http://a.sample.uri/requesting/a/test.yml') }
+      let(:write_path) { '/tmp/test.yml' }
+      let(:content) { 'content' }
+
+      context 'when the file exists' do
+        before do
+          stub_request(:get, uri.to_s).to_return(body: content)
+        end
+
+        it 'downloads the file to the specified directory' do
+          adapter.download(uri, write_path)
+
+          expect(File.read(write_path)).to eq(content)
+        end
+      end
+
+      context 'when the file does not exist' do
+        before do
+          stub_request(:get, uri.to_s).to_return(status: 404)
+        end
+
+        it 'raises an error if the file does not exist' do
+          expect {
+            adapter.download(uri, write_path)
+          }.to raise_error(%r{remote file 'http://a.sample.uri/requesting/a/test.yml' not found})
+        end
+      end
+
+    end
+  end
+end
+
