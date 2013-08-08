b6a7549bdb24311d1743749e7cfd748ac5b40cb2
Kai Xiang <kxiang@pivotallabs.com>
2013-08-08 17:10:07 +0800

    No need lucid kernel patch anymore

--- stemcell_builder/stages/base_warden/apply.sh
+++ stemcell_builder/stages/base_warden/apply.sh
@@ -11,8 +11,5 @@ source $base_dir/lib/prelude_apply.bash
 # Otherwise using none ubuntu host will fail creating vm
 mkdir -p $chroot/warden-cpi-dev
 
-# Install lucid kernel patch for Warden in Warden
-apt_get install linux-image-generic-lts-backport-natty
-
 # This is a Hacky way to force Warden in Warden to use overlayfs for now
 sed -i s/lucid/precise/ $chroot/etc/lsb-release
