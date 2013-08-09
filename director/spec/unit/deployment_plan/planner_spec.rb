8b829d5dbc8262d386d7f472b84d50905f551731
Abhijit Hiremagalur and Jeffrey Peckham <pair+abhi+peckham@pivotallabs.com>
2013-08-09 16:44:49 -0700

    Cleanup specs for Planner

--- director/spec/unit/deployment_plan/planner_spec.rb
+++ director/spec/unit/deployment_plan/planner_spec.rb
@@ -3,17 +3,19 @@ require 'spec_helper'
 module Bosh::Director
   module DeploymentPlan
     describe Planner do
-      MOCKED_METHODS = [:parse_name, :parse_properties, :parse_releases,
-                        :parse_networks, :parse_compilation, :parse_update,
-                        :parse_resource_pools, :parse_jobs]
-
       describe :initialize do
         it 'should parse the manifest' do
-          MOCKED_METHODS.each do |method_name|
-            Planner.any_instance.should_receive(method_name)
-          end
+          plan = Planner.new(some: :manifest)
 
-          plan = Planner.new({:some => :manifest})
+          plan.should_receive(:parse_name)
+          plan.should_receive(:parse_properties)
+          plan.should_receive(:parse_releases)
+          plan.should_receive(:parse_networks)
+          plan.should_receive(:parse_compilation)
+          plan.should_receive(:parse_update)
+          plan.should_receive(:parse_resource_pools)
+          plan.should_receive(:parse_jobs)
+
           plan.parse
         end
 
@@ -30,7 +32,7 @@ module Bosh::Director
 
       describe :parse_name do
         it 'should parse the raw and canonical names' do
-          plan = Planner.new({'name' => 'Test Deployment'})
+          plan = Planner.new({ 'name' => 'Test Deployment' })
           plan.parse_name
           plan.name.should == 'Test Deployment'
           plan.canonical_name.should == 'testdeployment'
@@ -39,13 +41,13 @@ module Bosh::Director
 
       describe :parse_properties do
         it 'should parse basic properties' do
-          plan = Planner.new({'properties' => {'foo' => 'bar'}})
+          plan = Planner.new({ 'properties' => { 'foo' => 'bar' } })
           plan.parse_properties
-          plan.properties.should == {'foo' => 'bar'}
+          plan.properties.should == { 'foo' => 'bar' }
         end
 
         it 'should allow not having any properties' do
-          plan = Planner.new({'name' => 'Test Deployment'})
+          plan = Planner.new({ 'name' => 'Test Deployment' })
           plan.parse_properties
           plan.properties.should == {}
         end
@@ -61,8 +63,8 @@ module Bosh::Director
 
         let(:releases_spec) do
           [
-            { 'name' => 'foo', 'version' => '27'},
-            { 'name' => 'bar', 'version' => '42'}
+            { 'name' => 'foo', 'version' => '27' },
+            { 'name' => 'bar', 'version' => '42' }
           ]
         end
 
@@ -103,9 +105,9 @@ module Bosh::Director
         it "supports either 'releases' or 'release' manifest section, not both" do
           expect {
             plan = Planner.new({
-                                                     'releases' => releases_spec,
-                                                     'release' => release_spec
-                                                   })
+                                 'releases' => releases_spec,
+                                 'release' => release_spec
+                               })
             plan.parse_releases
           }.to raise_error(/use one of the two/)
         end
@@ -113,9 +115,9 @@ module Bosh::Director
         it 'should detect duplicate release names' do
           expect {
             plan = Planner.new({
-                                                     'releases' => [release_spec,
-                                                                    release_spec]
-                                                   })
+                                 'releases' => [release_spec,
+                                                release_spec]
+                               })
             plan.parse_releases
           }.to raise_error(/duplicate release name/i)
         end
@@ -133,10 +135,10 @@ module Bosh::Director
           ManualNetwork.should_receive(:new).
             and_return do |deployment_plan, spec|
             received_plan = deployment_plan
-            spec.should == {'foo' => 'bar'}
+            spec.should == { 'foo' => 'bar' }
             network_spec
           end
-          plan = Planner.new({'networks' => [{'foo' => 'bar'}]})
+          plan = Planner.new({ 'networks' => [{ 'foo' => 'bar' }] })
           plan.parse_networks
           received_plan.should == plan
         end
@@ -151,10 +153,10 @@ module Bosh::Director
           end
 
           lambda {
-            plan = Planner.new({'networks' => [
-              {'name' => 'bar', 'cname' => 'bar'},
-              {'name' => 'Bar', 'cname' => 'bar'}
-            ]})
+            plan = Planner.new({ 'networks' => [
+              { 'name' => 'bar', 'cname' => 'bar' },
+              { 'name' => 'Bar', 'cname' => 'bar' }
+            ] })
             plan.parse_networks
           }.should raise_error(DeploymentCanonicalNetworkNameTaken,
                                "Invalid network name `Bar', canonical name already taken")
@@ -162,7 +164,7 @@ module Bosh::Director
 
         it 'should require at least one network' do
           lambda {
-            plan = Planner.new({'networks' => []})
+            plan = Planner.new({ 'networks' => [] })
             plan.parse_networks
           }.should raise_error(DeploymentNoNetworks, 'No networks specified')
 
@@ -179,9 +181,9 @@ module Bosh::Director
           CompilationConfig.
             should_receive(:new).with do |deployment_plan, spec|
             received_plan = deployment_plan
-            spec.should == {'foo' => 'bar'}
+            spec.should == { 'foo' => 'bar' }
           end
-          plan = Planner.new({'compilation' => {'foo' => 'bar'}})
+          plan = Planner.new({ 'compilation' => { 'foo' => 'bar' } })
           plan.parse_compilation
           received_plan.should == plan
         end
@@ -197,9 +199,9 @@ module Bosh::Director
       describe :parse_update do
         it 'should delegate to UpdateConfig' do
           UpdateConfig.should_receive(:new).with do |spec|
-            spec.should == {'foo' => 'bar'}
+            spec.should == { 'foo' => 'bar' }
           end
-          plan = Planner.new({'update' => {'foo' => 'bar'}})
+          plan = Planner.new({ 'update' => { 'foo' => 'bar' } })
           plan.parse_update
         end
 
@@ -221,11 +223,11 @@ module Bosh::Director
           ResourcePool.should_receive(:new).
             and_return do |deployment_plan, spec|
             received_plan = deployment_plan
-            spec.should == {'foo' => 'bar'}
+            spec.should == { 'foo' => 'bar' }
             resource_pool_spec
           end
 
-          plan = Planner.new({'resource_pools' => [{'foo' => 'bar'}]})
+          plan = Planner.new({ 'resource_pools' => [{ 'foo' => 'bar' }] })
           plan.parse_resource_pools
           plan.resource_pools.should == [resource_pool_spec]
           plan.resource_pool('foo').should == resource_pool_spec
@@ -241,7 +243,7 @@ module Bosh::Director
           end
           lambda {
             plan = Planner.new(
-              {'resource_pools' => [{'name' => 'bar'}, {'name' => 'bar'}]}
+              { 'resource_pools' => [{ 'name' => 'bar' }, { 'name' => 'bar' }] }
             )
             plan.parse_resource_pools
           }.should raise_error(DeploymentDuplicateResourcePoolName,
@@ -260,10 +262,10 @@ module Bosh::Director
           Job.should_receive(:parse).
             and_return do |deployment_plan, spec|
             received_plan = deployment_plan
-            spec.should == {'foo' => 'bar'}
+            spec.should == { 'foo' => 'bar' }
             job_spec
           end
-          plan = Planner.new({'jobs' => [{'foo' => 'bar'}]})
+          plan = Planner.new({ 'jobs' => [{ 'foo' => 'bar' }] })
           plan.parse_jobs
           received_plan.should == plan
         end
@@ -277,10 +279,10 @@ module Bosh::Director
             job_spec
           end
           lambda {
-            plan = Planner.new({'jobs' => [
-              {'name' => 'Bar', 'cname' => 'bar'},
-              {'name' => 'bar', 'cname' => 'bar'}
-            ]})
+            plan = Planner.new({ 'jobs' => [
+              { 'name' => 'Bar', 'cname' => 'bar' },
+              { 'name' => 'bar', 'cname' => 'bar' }
+            ] })
             plan.parse_jobs
           }.should raise_error(DeploymentCanonicalJobNameTaken,
                                "Invalid job name `bar', " +
@@ -290,8 +292,8 @@ module Bosh::Director
         it 'should raise exception if renamed job is being referenced in deployment' do
           lambda {
             plan = Planner.new(
-              {'jobs' => [{'name' => 'bar'}]},
-              {'job_rename' => {'old_name' => 'bar', 'new_name' => 'foo'}}
+              { 'jobs' => [{ 'name' => 'bar' }] },
+              { 'job_rename' => { 'old_name' => 'bar', 'new_name' => 'foo' } }
             )
             plan.parse_jobs
           }.should raise_error(DeploymentRenamedJobNameStillUsed,
@@ -300,7 +302,7 @@ module Bosh::Director
         end
 
         it 'should allow you to not have any jobs' do
-          plan = Planner.new({'jobs' => []})
+          plan = Planner.new({ 'jobs' => [] })
           plan.parse_jobs
 
           plan.jobs.should be_empty
@@ -308,6 +310,94 @@ module Bosh::Director
           plan = Planner.new({})
           plan.parse_jobs
           plan.jobs.should be_empty
+        end
+      end
+
+      describe :bind_model do
+
+        def make_plan(manifest)
+          Planner.new(manifest)
+        end
+
+        def find_deployment(name)
+          Models::Deployment.find(name: name)
+        end
+
+        def make_deployment(name)
+          Models::Deployment.make(name: name)
+        end
+
+        describe 'binding deployment model' do
+          it 'creates new deployment in DB using name from the manifest' do
+            plan = make_plan({ 'name' => 'mycloud' })
+            plan.parse_name
+
+            find_deployment('mycloud').should be_nil
+            plan.bind_model
+
+            plan.model.should == find_deployment('mycloud')
+            Models::Deployment.count.should == 1
+          end
+
+          it 'uses an existing deployment model if found in DB' do
+            plan = make_plan({ 'name' => 'mycloud' })
+            plan.parse_name
+
+            deployment = make_deployment('mycloud')
+            plan.bind_model
+            plan.model.should == deployment
+            Models::Deployment.count.should == 1
+          end
+
+          it 'enforces canonical name uniqueness' do
+            make_deployment('my-cloud')
+            plan = make_plan('name' => 'my_cloud')
+            plan.parse_name
+
+            expect {
+              plan.bind_model
+            }.to raise_error(DeploymentCanonicalNameTaken)
+
+            plan.model.should be_nil
+            Models::Deployment.count.should == 1
+          end
+
+          it 'only works when name and canonical name are known' do
+            plan = make_plan('name' => 'my_cloud')
+            expect {
+              plan.bind_model
+            }.to raise_error(DirectorError)
+
+            plan.parse_name
+            lambda { plan.bind_model }.should_not raise_error
+          end
+        end
+
+        describe 'getting VM models list' do
+          it 'raises an error when deployment model is unbound' do
+            plan = make_plan('name' => 'my_cloud')
+            plan.parse_name
+
+            expect {
+              plan.vms
+            }.to raise_error(DirectorError)
+
+            make_deployment('mycloud')
+            plan.bind_model
+            lambda { plan.vms }.should_not raise_error
+          end
+
+          it 'returns a list of VMs in deployment' do
+            plan = make_plan('name' => 'my_cloud')
+            plan.parse_name
+
+            deployment = make_deployment('my_cloud')
+            vm1 = Models::Vm.make(deployment: deployment)
+            vm2 = Models::Vm.make(deployment: deployment)
+
+            plan.bind_model
+            plan.vms.should =~ [vm1, vm2]
+          end
         end
       end
     end
