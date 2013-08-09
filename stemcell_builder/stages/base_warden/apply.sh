5d7cd938f815dd2dc1a990313c17f6200c80eb44
Gary Liu <garyliu@rbcon.com>
2013-08-09 15:44:01 +0800

    [ finishes # 53442603] add rsyslog in warden contrains

--- stemcell_builder/stages/base_warden/apply.sh
+++ stemcell_builder/stages/base_warden/apply.sh
@@ -13,3 +13,23 @@ mkdir -p $chroot/warden-cpi-dev
 
 # This is a Hacky way to force Warden in Warden to use overlayfs for now
 sed -i s/lucid/precise/ $chroot/etc/lsb-release
+
+
+# Run rsyslog using runit and replace /usr/sbin/service with a script which call runit
+mkdir -p $chroot/etc/sv/ $chroot/etc/service/
+cp -a $assets_dir/runit/rsyslog/ $chroot/etc/sv/rsyslog
+
+run_in_chroot $chroot "
+chmod +x /etc/sv/rsyslog/run
+ln -s /etc/sv/rsyslog /etc/service/rsyslog
+"
+
+# Replace /usr/sbin/service with a script which calls runit
+run_in_chroot $chroot "
+dpkg-divert --local --rename --add /usr/sbin/service
+"
+cp -f $assets_dir/service $chroot/usr/sbin/service
+
+run_in_chroot $chroot "
+chmod +x /usr/sbin/service
+"
