836b75941e18d318b8c2d8b54e990a7a370da821
Abhijit Hiremagalur and Rob Day-Reynolds <pair+abhi+rdayreynolds@pivotallabs.com>
2013-08-14 15:49:39 -0700

    Simplify spec setup

--- bosh-dev/spec/bosh/dev/stemcell_rake_methods_spec.rb
+++ bosh-dev/spec/bosh/dev/stemcell_rake_methods_spec.rb
@@ -4,10 +4,9 @@ require 'bosh/dev/stemcell_rake_methods'
 module Bosh::Dev
   describe StemcellRakeMethods do
     let(:env) { { 'FAKE' => 'ENV' } }
-    let(:infrastructure) { 'aws' }
-    let(:stemcell_tgz) { 'fake-stemcell-filename.tgz' }
-    let(:spec) { 'stemcell-aws' }
-    let(:source_root) { File.expand_path('../../../../..', __FILE__) }
+    let(:args) { { infrastructure: 'aws' } }
+
+    let(:gems_generator) { instance_double('Bosh::Dev::GemsGenerator', build_gems_into_release_dir: nil) }
 
     let(:build_from_spec) do
       instance_double('Bosh::Dev::BuildFromSpec', build: nil)
@@ -19,28 +18,19 @@ module Bosh::Dev
                       micro: { micro: 'options' })
     end
 
-    let(:args) do
-      {
-        infrastructure: infrastructure,
-        stemcell_tgz: stemcell_tgz,
-        stemcell_version: '123'
-      }
-    end
-
     subject(:stemcell_rake_methods) do
       StemcellRakeMethods.new(args: args, environment: env)
     end
 
     before do
       Bosh::Dev::StemcellBuilderOptions.stub(:new).with(args: args, environment: env).and_return(stemcell_builder_options)
+      Bosh::Dev::GemsGenerator.stub(:new).and_return(gems_generator)
     end
 
     describe '#build_basic_stemcell' do
-      let(:gems_generator) { instance_double('Bosh::Dev::GemsGenerator', build_gems_into_release_dir: nil) }
 
       before do
-        Bosh::Dev::BuildFromSpec.stub(:new).with(env, spec, { basic: 'options' }).and_return(build_from_spec)
-        Bosh::Dev::GemsGenerator.stub(:new).and_return(gems_generator)
+        Bosh::Dev::BuildFromSpec.stub(:new).with(env, 'stemcell-aws', { basic: 'options' }).and_return(build_from_spec)
       end
 
       it "builds bosh's gems so we have the gem for the agent" do
@@ -57,29 +47,20 @@ module Bosh::Dev
     end
 
     describe '#build_micro_stemcell' do
-      let(:gems_generator) { instance_double('Bosh::Dev::GemsGenerator', build_gems_into_release_dir: nil) }
-
       before do
-        Bosh::Dev::GemsGenerator.stub(:new).and_return(gems_generator)
-        Bosh::Dev::BuildFromSpec.stub(:new).with(env, spec, { micro: 'options' }).and_return(build_from_spec)
+        Bosh::Dev::BuildFromSpec.stub(:new).with(env, 'stemcell-aws', { micro: 'options' }).and_return(build_from_spec)
       end
 
-      context 'when a release tarball is provided' do
-        before do
-          args.merge!(tarball: 'fake/release.tgz')
-        end
+      it "builds bosh's gems so we have the gem for the agent" do
+        gems_generator.should_receive(:build_gems_into_release_dir)
 
-        it "builds bosh's gems so we have the gem for the agent" do
-          gems_generator.should_receive(:build_gems_into_release_dir)
+        stemcell_rake_methods.build_micro_stemcell
+      end
 
-          stemcell_rake_methods.build_micro_stemcell
-        end
+      it 'builds a micro stemcell with the appropriate name and options' do
+        build_from_spec.should_receive(:build)
 
-        it 'builds a micro stemcell with the appropriate name and options' do
-          build_from_spec.should_receive(:build)
-
-          stemcell_rake_methods.build_micro_stemcell
-        end
+        stemcell_rake_methods.build_micro_stemcell
       end
     end
   end
