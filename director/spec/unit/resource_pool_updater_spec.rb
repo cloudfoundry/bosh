0a81889c96bbcfa40772821394f1ddda7238b8cb
Wei Tie <nuaafe@gmail.com>
2013-08-18 23:50:52 +0800

    adjustment to the unit test

--- director/spec/unit/resource_pool_updater_spec.rb
+++ director/spec/unit/resource_pool_updater_spec.rb
@@ -94,6 +94,8 @@ describe Bosh::Director::ResourcePoolUpdater do
       BD::AgentClient.stub(:new).with("agent-1").and_return(agent)
 
       @resource_pool_updater.should_receive(:update_state).with(agent, @vm, @idle_vm)
+      @idle_vm.should_receive(:network_reservation)
+      @idle_vm.should_receive(:network_reservation)
       @idle_vm.should_receive(:vm=).with(@vm)
       @idle_vm.should_receive(:current_state=).with({"state" => "foo"})
 
@@ -106,7 +108,8 @@ describe Bosh::Director::ResourcePoolUpdater do
       BD::AgentClient.stub(:new).with("agent-1").and_return(agent)
 
       @cloud.should_receive(:delete_vm).with("vm-1")
-
+      @idle_vm.should_receive(:network_reservation)
+      @idle_vm.should_receive(:network_reservation)
       lambda {
         @resource_pool_updater.create_missing_vm(@idle_vm)
       }.should raise_error("timeout")
