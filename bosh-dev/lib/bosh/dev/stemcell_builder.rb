06ae4eb6e8c4f51ea961a130fb69ba7a89a517ca
Abhijit Hiremagalur and Aram Price <pair+abhi+aprice@pivotallabs.com>
2013-08-20 16:08:04 -0700

    Eagerly initialize collaborators, use more current names

--- bosh-dev/lib/bosh/dev/stemcell_builder.rb
+++ bosh-dev/lib/bosh/dev/stemcell_builder.rb
@@ -9,8 +9,8 @@ module Bosh::Dev
 
     def initialize(infrastructure_name, candidate = Bosh::Dev::Build.candidate)
       @candidate = candidate
-      @infrastructure_name = infrastructure_name
-
+      @infrastructure = Bosh::Stemcell::Infrastructure.for(infrastructure_name)
+      @archive_filename = Bosh::Stemcell::ArchiveFilename.new(candidate.number, infrastructure, 'bosh-stemcell', false)
       mnt = ENV.fetch('FAKE_MNT', '/mnt')
       @directory = File.join(mnt, 'stemcells', "#{infrastructure_name}")
       @work_path = File.join(directory, 'work')
@@ -24,7 +24,7 @@ module Bosh::Dev
       environment = StemcellEnvironment.new(self)
       environment.sanitize
 
-      build_task
+      build_stemcell
 
       stemcell_path!
     end
@@ -32,39 +32,29 @@ module Bosh::Dev
     private
 
     attr_reader :candidate,
-                :infrastructure_name,
+                :archive_filename,
+                :infrastructure,
                 :build_path
 
-    def new_style_path
-      File.join(work_path, 'work', new_style_name)
-    end
-
-    def new_style_name
-      infrastructure = Bosh::Stemcell::Infrastructure.for(infrastructure_name)
-      Bosh::Stemcell::ArchiveFilename.new(candidate.number, infrastructure, name, false).to_s
-    end
-
-    def name
-      'bosh-stemcell'
-    end
-
-    def build_task
+    def build_stemcell
       bosh_release_path = candidate.download_release
 
       stemcell_rake_methods = Bosh::Dev::StemcellRakeMethods.new(args: {
         tarball: bosh_release_path,
-        infrastructure: infrastructure_name,
+        infrastructure: infrastructure.name,
         stemcell_version: candidate.number,
-        stemcell_tgz: new_style_name,
+        stemcell_tgz: archive_filename.to_s,
       })
 
       stemcell_rake_methods.build_stemcell
     end
 
     def stemcell_path!
-      File.exist?(new_style_path) || raise("#{new_style_path} does not exist")
+      stemcell_path = File.join(work_path, 'work', archive_filename.to_s)
 
-      new_style_path
+      File.exist?(stemcell_path) || raise("#{stemcell_path} does not exist")
+
+      stemcell_path
     end
   end
 end
