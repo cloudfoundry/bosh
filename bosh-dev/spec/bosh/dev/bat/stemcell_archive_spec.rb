f95505a4f7248c83ad22fc393578e6b6060c96a5
Aram Price and Matthew Boedicker <pair+aprice+mboedicker@pivotallabs.com>
2013-08-19 17:12:52 -0700

    Update name of spec stemcell fixtures (bosh-dev)

--- bosh-dev/spec/bosh/dev/bat/stemcell_archive_spec.rb
+++ bosh-dev/spec/bosh/dev/bat/stemcell_archive_spec.rb
@@ -4,7 +4,7 @@ require 'bosh/dev/bat/stemcell_archive'
 module Bosh::Dev::Bat
   describe StemcellArchive do
     subject do
-      StemcellArchive.new(spec_asset('micro-bosh-stemcell-aws.tgz'))
+      StemcellArchive.new(spec_asset('fake-stemcell.tgz'))
     end
 
     its(:version) { should eq('714') }
