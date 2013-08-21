2813f6fd6da619c614a83f12f0c51825e7367cac
Aram Price and Tony Hansmann <pair+aprice+thansmann@pivotallabs.com>
2013-08-21 12:17:11 -0700

    add class to handle OperatingSystem

---
+++ bosh-stemcell/lib/bosh/stemcell/operating_system.rb
@@ -0,0 +1,31 @@
+module Bosh::Stemcell
+  module OperatingSystem
+    def self.for(operating_system_name)
+      case operating_system_name
+        when 'centos' then Centos.new
+        when 'ubuntu' then Ubuntu.new
+        else raise ArgumentError.new("invalid operating system: #{operating_system_name}")
+      end
+    end
+
+    class Base
+      attr_reader :name
+
+      def initialize(options = {})
+        @name = options.fetch(:name)
+      end
+    end
+
+    class Centos < Base
+      def initialize
+        super(name: 'centos')
+      end
+    end
+
+    class Ubuntu < Base
+      def initialize
+        super(name: 'ubuntu')
+      end
+    end
+  end
+end
