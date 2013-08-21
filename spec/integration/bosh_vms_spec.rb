af83ee39de8c0c9965fa8d1100826354c01a76ac
Brian Cunnie and Matthew Boedicker <pair+cunnie+mboedicker@pivotallabs.com>
2013-08-20 17:34:01 -0700

    Move common simple deployment code to helper.

--- spec/integration/bosh_vms_spec.rb
+++ spec/integration/bosh_vms_spec.rb
@@ -7,19 +7,8 @@ describe 'vms list' do
     manifest_hash = Bosh::Spec::Deployments.simple_manifest
     manifest_hash['release']['version'] = 'latest'
 
-    deployment_manifest = yaml_file('simple', manifest_hash)
+    deploy_simple(manifest_hash: manifest_hash)
 
-    run_bosh("target http://localhost:#{current_sandbox.director_port}")
-    run_bosh('login admin admin')
-
-    run_bosh('create release', TEST_RELEASE_DIR)
-    run_bosh('upload release', TEST_RELEASE_DIR)
-
-    run_bosh("upload stemcell #{spec_asset('valid_stemcell.tgz')}")
-
-    run_bosh("deployment #{deployment_manifest.path}")
-    run_bosh('deploy')
-    expect($?).to be_success
     vms = run_bosh('vms')
 
     expect(vms).to match /foobar\/0/
--- spec/support/integration_example_group.rb
+++ spec/support/integration_example_group.rb
@@ -1,6 +1,26 @@
 require 'yajl'
 
 module IntegrationExampleGroup
+  def deploy_simple(options={})
+    no_track = options.fetch(:no_track, false)
+    manifest_hash = options.fetch(:manifest_hash, Bosh::Spec::Deployments.simple_manifest)
+
+    deployment_manifest = yaml_file('simple', manifest_hash)
+
+    run_bosh("target http://localhost:#{current_sandbox.director_port}")
+    run_bosh('login admin admin')
+
+    run_bosh('create release', TEST_RELEASE_DIR)
+    run_bosh('upload release', TEST_RELEASE_DIR)
+
+    run_bosh("upload stemcell #{spec_asset('valid_stemcell.tgz')}")
+
+    run_bosh("deployment #{deployment_manifest.path}")
+    deploy_result = run_bosh("#{no_track ? "--no-track " : ""}deploy")
+    expect($?).to be_success
+    deploy_result
+  end
+
   def start_sandbox
     puts "Starting sandboxed environment for BOSH tests..."
     current_sandbox.start
