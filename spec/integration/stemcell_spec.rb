51b33ca7ddf8d078b03ecb4c8ecb7f7505a621b1
Tony Hansmann <thansmann@pivotallabs.com>
2013-08-22 12:31:15 -0700

    Adding stemcell in-use refuses to delete integration test
    Signed-off-by: Jeffrey Peckham <peckham@pivotallabs.com>

---
+++ spec/integration/stemcell_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'stemcell integrations' do
+  include IntegrationExampleGroup
+
+  context 'when stemcell is in use by a deployment' do
+    it 'refuses to delete it' do
+      deploy_simple
+
+      results = run_bosh('delete stemcell ubuntu-stemcell 1', failure_expected: true)
+      expect(results).to match %r{Stemcell `ubuntu-stemcell/1' is still in use by: simple}
+    end
+  end
+end
