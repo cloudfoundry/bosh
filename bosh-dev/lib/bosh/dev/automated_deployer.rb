73237c559d15d256009e7c75998e4f22ebaa6cd4
Abhijit Hiremagalur <abhi@pivotallabs.com>
2013-08-23 16:45:48 -0700

    Rebase when uploading release because we have been using dev releases

--- bosh-dev/lib/bosh/dev/automated_deployer.rb
+++ bosh-dev/lib/bosh/dev/automated_deployer.rb
@@ -29,7 +29,7 @@ module Bosh::Dev
       cli.run_bosh("login #{username} #{password}")
       cli.run_bosh("deployment #{manifest_path}")
       cli.run_bosh("upload stemcell #{stemcell_path}", ignore_failures: true)
-      cli.run_bosh("upload release #{release_path}", ignore_failures: true)
+      cli.run_bosh("upload release #{release_path} --rebase", ignore_failures: true)
       cli.run_bosh('deploy', debug_on_fail: true)
 
       cli.run_bosh("target #{bosh_target}")
