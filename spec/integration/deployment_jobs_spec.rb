019230f699a0177e9e46b967c4cf1d6c82876bd8
Jeffrey Peckham <peckham@pivotallabs.com>
2013-08-23 14:38:29 -0700

    Revert "Limit concurrent integration specs on travis to 12"
    This reverts commit 046f50663389d03d1ea4b0c4854b2fb614b41f66.

--- spec/integration/deployment_jobs_spec.rb
+++ spec/integration/deployment_jobs_spec.rb
@@ -14,9 +14,4 @@ describe 'deployment job control' do
   it 'recreates a job' do
     expect(run_bosh('recreate foobar 1')).to match %r{foobar/1 has been recreated}
   end
-
-  it 'stops and starts a job' do
-    expect(run_bosh('stop foobar 2')).to match %r{foobar/2 has been stopped}
-    expect(run_bosh('start foobar 2')).to match %r{foobar/2 has been started}
-  end
 end
