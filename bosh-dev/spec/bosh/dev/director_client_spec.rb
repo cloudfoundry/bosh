1a7b9fb13b665a0709af9bc352e1be9e723275a8
Matthew Boedicker <mboedicker@pivotallabs.com>
2013-08-26 15:59:03 -0700

    Add has_stemcell? method to director client.
    Signed-off-by: Tony Hansmann <thansmann@pivotallabs.com>

--- bosh-dev/spec/bosh/dev/director_client_spec.rb
+++ bosh-dev/spec/bosh/dev/director_client_spec.rb
@@ -9,15 +9,15 @@ module Bosh::Dev
     let (:director_handle) { instance_double 'Bosh::Cli::Director' }
     let (:valid_stemcell_list_1) {
       [
-          {'name' => 'bosh-MOCK-stemcell', 'version' => '007', 'cid' => 'ami-amazon_guid_1'},
-          {'name' => 'bosh-MOCK-stemcell', 'version' => '222', 'cid' => 'ami-amazon_guid_2'}
+          { 'name' => 'bosh-MOCK-stemcell', 'version' => '007', 'cid' => 'ami-amazon_guid_1' },
+          { 'name' => 'bosh-MOCK-stemcell', 'version' => '222', 'cid' => 'ami-amazon_guid_2' }
       ]
     }
 
     let (:valid_stemcell_list_2) {
       [
-          {'name' => 'bosh-MOCK-stemcell', 'version' => '007', 'cid' => 'ami-amazon_guid_1'},
-          {'name' => 'bosh-MOCK-stemcell', 'version' => '222', 'cid' => 'ami-amazon_guid_2'}
+          { 'name' => 'bosh-MOCK-stemcell', 'version' => '007', 'cid' => 'ami-amazon_guid_1' },
+          { 'name' => 'bosh-MOCK-stemcell', 'version' => '222', 'cid' => 'ami-amazon_guid_2' }
       ]
     }
 
@@ -27,10 +27,23 @@ module Bosh::Dev
       )
     end
 
-    it 'lists stemcells stored on director' do
-      director_handle.stub(:list_stemcells) { valid_stemcell_list_1 }
-      expect(director_client.stemcells).to eq valid_stemcell_list_2
+    describe '#stemcells' do
+      it 'lists stemcells stored on director' do
+        director_handle.stub(:list_stemcells) { valid_stemcell_list_1 }
+        expect(director_client.stemcells).to eq valid_stemcell_list_2
+      end
     end
 
+    describe '#has_stemcell?' do
+      before do
+        director_handle.stub(:list_stemcells) { valid_stemcell_list_1 }
+      end
+      it 'local stemcell exists on director' do
+        expect(director_client.has_stemcell?('bosh-MOCK-stemcell', '007')).to be_true
+      end
+      it 'local stemcell does not exists on director' do
+        expect(director_client.has_stemcell?('non-such-stemcell', '-1')).to be_false
+      end
+    end
   end
 end
