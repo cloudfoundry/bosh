b7ab8fd4c56048bb7eedca1570f1f8edaad10c6d
Tony Hansmann <thansmann@pivotallabs.com>
2013-08-27 13:44:21 -0700

    Use DirectorClient instead of Cli::Director
    Fixed newing up the wrong object.
    Extracted 2 smaller methods out of the big deploy method, but
    leaving it as the only public interface.
    Strict ordering in tests.
    
    Signed-off-by: Jesse Zhang <jz@pivotallabs.com>

--- bosh-dev/lib/bosh/dev/automated_deployer.rb
+++ bosh-dev/lib/bosh/dev/automated_deployer.rb
@@ -1,6 +1,7 @@
 require 'cli/director'
 require 'bosh/core/shell'
 require 'bosh/dev/bosh_cli_session'
+require 'bosh/dev/director_client'
 require 'bosh/dev/artifacts_downloader'
 require 'bosh/dev/aws/deployments_repository'
 
@@ -16,8 +17,8 @@ module Bosh::Dev
       @cli = options.fetch(:cli) { BoshCliSession.new }
       @artifacts_downloader = options.fetch(:artifacts_downloader) { ArtifactsDownloader.new }
       @deployments_repository = options.fetch(:deployments_repository) { Aws::DeploymentsRepository.new(path_root: '/tmp') }
-      @micro_director_client = options.fetch(:micro_director_client) { Bosh::Cli::Director.new(micro_target, username, password) }
-      @bosh_director_client = options.fetch(:bosh_director_client) { Bosh::Cli::Director.new(bosh_target, username, password) }
+      @micro_director_client = options.fetch(:micro_director_client) { Bosh::Dev::DirectorClient.new(micro_target, username, password) }
+      @bosh_director_client = options.fetch(:bosh_director_client) { Bosh::Dev::DirectorClient.new(bosh_target, username, password) }
     end
 
     def deploy
@@ -28,22 +29,28 @@ module Bosh::Dev
 
       deployments_repository.clone_or_update!
 
-      cli.run_bosh("target #{micro_target}")
-      cli.run_bosh("login #{username} #{password}")
-      cli.run_bosh("deployment #{manifest_path}")
       archive = Bosh::Stemcell::Archive.new(stemcell_path)
-      cli.run_bosh("upload stemcell #{stemcell_path}", debug_on_fail: true) unless micro_director_client.has_stemcell?(archive.name, archive.version)
+      deploy_to_micro(manifest_path, release_path, archive)
 
-      cli.run_bosh("upload release #{release_path} --rebase", debug_on_fail: true)
-      cli.run_bosh('deploy', debug_on_fail: true)
-
-      cli.run_bosh("target #{bosh_target}")
-      cli.run_bosh("login #{username} #{password}")
-      cli.run_bosh("upload stemcell #{stemcell_path}", debug_on_fail: true) unless bosh_director_client.has_stemcell?(archive.name, archive.version)
-
+      upload_stemcell_to_bosh_director(archive)
     end
 
     private
+    def upload_stemcell_to_bosh_director(archive)
+      cli.run_bosh("target #{bosh_target}")
+      cli.run_bosh("login #{username} #{password}")
+      cli.run_bosh("upload stemcell #{archive.path}", debug_on_fail: true) unless bosh_director_client.has_stemcell?(archive.name, archive.version)
+    end
+
+    def deploy_to_micro(manifest_path, release_path, archive)
+      cli.run_bosh("target #{micro_target}")
+      cli.run_bosh("login #{username} #{password}")
+      cli.run_bosh("deployment #{manifest_path}")
+      cli.run_bosh("upload stemcell #{archive.path}", debug_on_fail: true) unless micro_director_client.has_stemcell?(archive.name, archive.version)
+
+      cli.run_bosh("upload release #{release_path} --rebase", debug_on_fail: true)
+      cli.run_bosh('deploy', debug_on_fail: true)
+    end
 
     attr_reader :micro_target, :bosh_target, :cli, :artifacts_downloader, :build_number, :environment, :deployments_repository, :shell, :micro_director_client, :bosh_director_client
 
