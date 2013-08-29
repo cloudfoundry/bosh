9dec310e82417347969be919bece5a655d6551c1
Jeffrey Peckham <peckham@pivotallabs.com>
2013-08-28 18:07:20 -0700

    Revert "Extracted create_vm to VMCreator#perform"
    This reverts commit 8d6dbe42bfd5850b679f7a5b8e9a89c29da9146d.

--- bosh_vsphere_cpi/lib/cloud/vsphere/cloud.rb
+++ bosh_vsphere_cpi/lib/cloud/vsphere/cloud.rb
@@ -1,19 +1,18 @@
-require 'membrane'
-require 'ruby_vim_sdk'
+require "membrane"
+require "ruby_vim_sdk"
 
-require 'cloud/vsphere/client'
-require 'cloud/vsphere/config'
-require 'cloud/vsphere/lease_updater'
-require 'cloud/vsphere/resources'
-require 'cloud/vsphere/resources/cluster'
-require 'cloud/vsphere/resources/datacenter'
-require 'cloud/vsphere/resources/datastore'
-require 'cloud/vsphere/resources/folder'
-require 'cloud/vsphere/resources/resource_pool'
-require 'cloud/vsphere/resources/scorer'
-require 'cloud/vsphere/resources/util'
-require 'cloud/vsphere/models/disk'
-require 'cloud/vsphere/vm_creator'
+require "cloud/vsphere/client"
+require "cloud/vsphere/config"
+require "cloud/vsphere/lease_updater"
+require "cloud/vsphere/resources"
+require "cloud/vsphere/resources/cluster"
+require "cloud/vsphere/resources/datacenter"
+require "cloud/vsphere/resources/datastore"
+require "cloud/vsphere/resources/folder"
+require "cloud/vsphere/resources/resource_pool"
+require "cloud/vsphere/resources/scorer"
+require "cloud/vsphere/resources/util"
+require "cloud/vsphere/models/disk"
 
 module VSphereCloud
 
@@ -138,19 +137,129 @@ module VSphereCloud
       end
     end
 
+    def disk_spec(persistent_disks)
+      disks = []
+      if persistent_disks
+        persistent_disks.each do |disk_cid|
+          disk = Models::Disk.first(:uuid => disk_cid)
+          disks << {
+              :size => disk.size,
+              :dc_name => disk.datacenter,
+              :ds_name => disk.datastore
+          }
+        end
+      end
+      disks
+    end
+
+    def stemcell_vm(name)
+      dc = @resources.datacenters.values.first
+      client.find_by_inventory_path(
+          [dc.name, "vm", dc.template_folder.name, name])
+    end
+
     def create_vm(agent_id, stemcell, resource_pool, networks, disk_locality = nil, environment = nil)
       with_thread_name("create_vm(#{agent_id}, ...)") do
-        VMCreator.new(agent_id: agent_id,
-                      stemcell: stemcell,
-                      resource_pool: resource_pool,
-                      networks: networks,
-                      disk_locality: disk_locality,
-                      environment: environment,
-                      resources: @resources,
-                      client: client,
-                      logger: @logger,
-                      vsphere_cloud: self,
-        ).perform
+        memory = resource_pool["ram"]
+        disk = resource_pool["disk"]
+        cpu = resource_pool["cpu"]
+
+        # Make sure number of cores is a power of 2. kb.vmware.com/kb/2003484
+        if cpu & cpu - 1 != 0
+          raise "Number of vCPUs: #{cpu} is not a power of 2."
+        end
+
+        stemcell_vm = stemcell_vm(stemcell)
+        raise "Could not find stemcell: #{stemcell}" if stemcell_vm.nil?
+
+        stemcell_size = client.get_property(
+            stemcell_vm, Vim::VirtualMachine, "summary.storage.committed",
+            :ensure_all => true)
+        stemcell_size /= 1024 * 1024
+
+        disks = disk_spec(disk_locality)
+        # need to include swap and linked clone log
+        ephemeral = disk + memory + stemcell_size
+        cluster, datastore = @resources.place(memory, ephemeral, disks)
+
+        name = "vm-#{generate_unique_name}"
+        @logger.info("Creating vm: #{name} on #{cluster.mob} stored in #{datastore.mob}")
+
+        replicated_stemcell_vm = replicate_stemcell(cluster, datastore, stemcell)
+        replicated_stemcell_properties = client.get_properties(replicated_stemcell_vm, Vim::VirtualMachine,
+                                                               ["config.hardware.device", "snapshot"],
+                                                               :ensure_all => true)
+
+        devices = replicated_stemcell_properties["config.hardware.device"]
+        snapshot = replicated_stemcell_properties["snapshot"]
+
+        config = Vim::Vm::ConfigSpec.new(:memory_mb => memory, :num_cpus => cpu)
+        config.device_change = []
+
+        system_disk = devices.find { |device| device.kind_of?(Vim::Vm::Device::VirtualDisk) }
+        pci_controller = devices.find { |device| device.kind_of?(Vim::Vm::Device::VirtualPCIController) }
+
+        file_name = "[#{datastore.name}] #{name}/ephemeral_disk.vmdk"
+        ephemeral_disk_config = create_disk_config_spec(datastore.mob, file_name, system_disk.controller_key, disk,
+                                                        :create => true)
+        config.device_change << ephemeral_disk_config
+
+        dvs_index = {}
+        networks.each_value do |network|
+          v_network_name = network["cloud_properties"]["name"]
+          network_mob = client.find_by_inventory_path([cluster.datacenter.name, "network", v_network_name])
+          nic_config = create_nic_config_spec(v_network_name, network_mob, pci_controller.key, dvs_index)
+          config.device_change << nic_config
+        end
+
+        nics = devices.select { |device| device.kind_of?(Vim::Vm::Device::VirtualEthernetCard) }
+        nics.each do |nic|
+          nic_config = create_delete_device_spec(nic)
+          config.device_change << nic_config
+        end
+
+        fix_device_unit_numbers(devices, config.device_change)
+
+        @logger.info("Cloning vm: #{replicated_stemcell_vm} to #{name}")
+
+        task = clone_vm(replicated_stemcell_vm, name, cluster.datacenter.vm_folder.mob, cluster.resource_pool.mob,
+                        :datastore => datastore.mob, :linked => true, :snapshot => snapshot.current_snapshot,
+                        :config => config)
+        vm = client.wait_for_task(task)
+
+        begin
+          upload_file(cluster.datacenter.name, datastore.name, "#{name}/env.iso", "")
+
+          vm_properties = client.get_properties(vm, Vim::VirtualMachine, ["config.hardware.device"], :ensure_all => true)
+          devices = vm_properties["config.hardware.device"]
+
+          # Configure the ENV CDROM
+          config = Vim::Vm::ConfigSpec.new
+          config.device_change = []
+          file_name = "[#{datastore.name}] #{name}/env.iso"
+          cdrom_change = configure_env_cdrom(datastore.mob, devices, file_name)
+          config.device_change << cdrom_change
+          client.reconfig_vm(vm, config)
+
+          network_env = generate_network_env(devices, networks, dvs_index)
+          disk_env = generate_disk_env(system_disk, ephemeral_disk_config.device)
+          env = generate_agent_env(name, vm, agent_id, network_env, disk_env)
+          env["env"] = environment
+          @logger.info("Setting VM env: #{env.pretty_inspect}")
+
+          location = get_vm_location(vm, :datacenter => cluster.datacenter.name,
+                                         :datastore => datastore.name,
+                                         :vm => name)
+          set_agent_env(vm, location, env)
+
+          @logger.info("Powering on VM: #{vm} (#{name})")
+          client.power_on_vm(cluster.datacenter.mob, vm)
+        rescue => e
+          @logger.info("#{e} - #{e.backtrace.join("\n")}")
+          delete_vm(name)
+          raise e
+        end
+        name
       end
     end
 
