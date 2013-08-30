121ea8e5a4413e5dc0ea94a37cee07cf49520409
Abhijit Hiremagalur <abhi@pivotallabs.com>
2013-08-30 09:55:16 -0700

    Stub ENV bosh-dev & bosh-stemcell
    Avoids ENV pollution between specs and also from the real ENV of
    the Ruby process running specs
    
    [Fix #56013436]

--- bosh-dev/spec/spec_helper.rb
+++ bosh-dev/spec/spec_helper.rb
@@ -4,6 +4,10 @@ require 'rspec/fire'
 require 'fakefs/spec_helpers'
 require 'webmock/rspec'
 
+Dir.glob(File.expand_path('support/**/*.rb', File.dirname(__FILE__))).each do |support|
+  require support
+end
+
 SPEC_ROOT = File.dirname(__FILE__)
 
 def spec_asset(name)
