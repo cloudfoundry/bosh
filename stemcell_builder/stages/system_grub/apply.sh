c4ae1f7072a6a162f85098a80baa71fe64cdcfcf
Aram Price <aprice@pivotallabs.com>
2013-09-03 13:46:00 -0700

    Make system_grub stage safe for CentOS
    Signed-off-by: Jeffrey Peckham <peckham@pivotallabs.com>

--- stemcell_builder/stages/system_grub/apply.sh
+++ stemcell_builder/stages/system_grub/apply.sh
@@ -9,7 +9,10 @@ source $base_dir/lib/prelude_apply.bash
 
 # Install grub
 pkg_mgr install grub
-rsync -a $chroot/usr/lib/grub/x86*/ $chroot/boot/grub/
+if [ -d $chroot/usr/lib/grub ]
+then
+  rsync -a $chroot/usr/lib/grub/x86*/ $chroot/boot/grub/
+fi
 
 # When a kernel is installed, update-grub is run per /etc/kernel-img.conf.
 # It complains when /boot/grub/menu.lst doesn't exist, so create it.
