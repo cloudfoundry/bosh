0f4d5a100e67b9848277190e639f8fae0f2a7e0b
Jesse Zhang <jz@pivotallabs.com>
2013-09-06 13:29:13 -0700

    better wording

--- bosh_agent/spec/unit/config_spec.rb
+++ bosh_agent/spec/unit/config_spec.rb
@@ -2,13 +2,15 @@ require 'bosh_agent/config'
 
 describe Bosh::Agent::Config do
   describe '.platform' do
-    it "returns an Ubuntu if platform_name is configured to be 'ubuntu'" do
+    subject(:config_class) do
       # do not mutate state of the described class with class methods
-      klass = Class.new(described_class)
-      klass.setup('platform_name' => 'ubuntu', 'infrastructure_name' => 'dummy')
+      config_klass = Class.new(described_class)
+      config_klass.setup('platform_name' => 'ubuntu', 'infrastructure_name' => 'dummy')
+      stub_const('Bosh::Agent::Config', config_klass)
+    end
 
-      stub_const('Bosh::Agent::Config', klass)
-      klass.platform.should be_a(Bosh::Agent::Platform::Ubuntu)
+    it "returns an Ubuntu if platform_name is configured to be 'ubuntu'" do
+      expect(config_class.platform).to be_a(Bosh::Agent::Platform::Ubuntu)
     end
   end
 end
