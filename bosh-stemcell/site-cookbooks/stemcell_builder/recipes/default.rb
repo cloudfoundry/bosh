aafdf52dceb1eb5c12bfd767e91c605d4580d290
Jesse Zhang <jz@pivotallabs.com>
2013-09-05 17:09:30 -0700

    Use apt-cacher-ng to speed up iteration
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- bosh-stemcell/site-cookbooks/stemcell_builder/recipes/default.rb
+++ bosh-stemcell/site-cookbooks/stemcell_builder/recipes/default.rb
@@ -25,3 +25,24 @@ package 'qemu-utils'
 
 # CentOS building requirements
 package 'yum'
+
+# caching proxy to speed up package installation in chroot
+package 'apt-cacher-ng'
+cookbook_file '/etc/apt-cacher-ng/centos_mirrors' do
+  action :create
+  source 'centos_mirrors'
+end
+
+cookbook_file '/etc/apt-cacher-ng/acng-centos.conf' do
+  action :create
+  source 'acng-centos.conf'
+end
+
+cookbook_file '/etc/apt-cacher-ng/backends_centos' do
+  action :create
+  source 'backends_centos'
+end
+
+service 'apt-cacher-ng' do
+  action :restart
+end
