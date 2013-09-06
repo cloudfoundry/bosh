fbb73b1d320c749b101d1fd2c3dbb1fd6bd69060
Abhijit Hiremagalur <abhi@pivotallabs.com>
2013-09-06 15:12:40 -0700

    improved test for Bosh::Agent:Platform
    [#55682936]
    
    Signed-off-by: Jeffrey Peckham <peckham@pivotallabs.com>

--- bosh_agent/spec/unit/platform_spec.rb
+++ bosh_agent/spec/unit/platform_spec.rb
@@ -1,11 +1,46 @@
 require 'spec_helper'
 
-describe Bosh::Agent::Platform do
-  describe '.platform' do
-    let(:fake_ubuntu) { double('fake ubuntuy') }
-    before { Bosh::Agent::Platform::Ubuntu.stub(new: fake_ubuntu) }
-    it 'returns an Ubuntu when platform_name is ubuntu' do
-      expect(described_class.platform('ubuntu')).to eq(fake_ubuntu)
+module Bosh::Agent
+  describe Platform do
+    describe '.platform' do
+      subject(:factory) do
+        Bosh::Agent::Platform
+      end
+
+      before do
+        config_double = class_double('Bosh::Agent::Config').as_stubbed_const
+        config_double.stub(:infrastructure)
+        config_double.stub(:platform_name)
+        config_double.stub(:logger)
+        config_double.stub(base_dir: '/fake/base_dir')
+        config_double.stub(system_root: '/fake/system_root')
+      end
+
+      context 'when the platform_name is "ubuntu"' do
+        it 'returns an Ubuntu compatible platform implementation' do
+          expect(factory.platform('ubuntu')).to be_an_instance_of(Platform::Ubuntu)
+        end
+      end
+
+      context 'when the platform_name is "centos"' do
+        it 'returns an CentOS compatible platform implementation' do
+          expect(factory.platform('centos')).to be_an_instance_of(Platform::Centos)
+        end
+      end
+
+      context 'when the platform_name is "rhel"' do
+        it 'returns an CentOS compatible platform implementation' do
+          expect(factory.platform('rhel')).to be_an_instance_of(Platform::Rhel)
+        end
+      end
+
+      context 'when the platform_name is anything else' do
+        it 'blows up with a sensible error for "osx"' do
+          expect {
+            factory.platform('osx')
+          }.to raise_error UnknownPlatform, "platform 'osx' not found"
+        end
+      end
     end
   end
 end
