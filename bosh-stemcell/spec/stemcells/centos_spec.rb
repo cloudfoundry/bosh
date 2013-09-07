95f2cf420691733a36b10513a69d921d9ccd1d08
Aram Price <aprice@pivotallabs.com>
2013-09-06 18:08:32 -0700

    Update some of the packages for CentOS
    [#54320562]
    
    Signed-off-by: Tony Hansmann <thansmann@pivotallabs.com>

--- bosh-stemcell/spec/stemcells/centos_spec.rb
+++ bosh-stemcell/spec/stemcells/centos_spec.rb
@@ -14,7 +14,36 @@ describe 'CentOs Stemcell' do
       it { should be_installed }
     end
 
-    context 'installed by base_apt'
+    context 'installed by base_yum' do
+      {
+        # 'upstart'        => '0.6.5-8',
+
+        'openssl-devel'  => '1.0.0-27.el6_4.2',
+        'lsof'           => '4.82-4.el6.x86_64',
+        'libxml2'        => '2.7.6-12.el6_4.1.x86_64',
+        'quota'          => '3.17-18.el6.x86_64',
+        'rsync'          => '3.0.6-9.el6.x86_64',
+        'strace'         => '4.5.19-1.17.el6.x86_64',
+        'iptables'       => '1.4.7-9.el6.x86_64',
+        'sysstat'        => '9.0.4-20.el6.x86_64',
+        'tcpdump'        => '4.0.0-3.20090921gitdf3cb4.2.el6.x86_64',
+        'zip'            => '3.0-1.el6.x86_64',
+        'traceroute'     => '2.0.14-2.el6.x86_64',
+        'gdb'            => '7.2-60.el6_4.1.x86_64',
+        'curl'           => '7.19.7-37.el6_4.x86_64',
+        'flex'           => '2.5.35-8.el6.x86_64',
+        'openssh-server' => '5.3p1-84.1.el6',
+        'wget'           => '1.12-1.8.el6.x86_64',
+        'psmisc'         => '22.6-15.el6_0.1.x86_64',
+        'unzip'          => '6.0-1.el6.x86_64',
+        'bison'          => '2.4.1-5.el6.x86_64',
+        'cmake'          => '2.6.4-5.el6.x86_64',
+      }.each do |pkg, version|
+        describe package(pkg) do
+          it { should be_installed.with_version(version) }
+        end
+      end
+    end
 
     context 'installed by bosh_micro'
 
--- stemcell_builder/stages/base_yum/apply.sh
+++ stemcell_builder/stages/base_yum/apply.sh
@@ -5,16 +5,16 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-packages="build-essential libssl-dev lsof \
+
+# Upgrade upstart first, to prevent it from messing up our stubs and starting daemons anyway
+pkg_mgr install upstart
+
+# Install base packages needed by both the warden and bosh
+packages="build-essential openssl-devel lsof \
 strace bind9-host dnsutils tcpdump iputils-arping \
 curl wget libcurl3 libcurl3-dev bison libreadline6-dev \
 libxml2 libxml2-dev libxslt1.1 libxslt1-dev zip unzip \
 nfs-common flex psmisc apparmor-utils iptables sysstat \
 rsync openssh-server traceroute libncurses5-dev quota \
 libaio1 gdb tripwire libcap2-bin libyaml-dev cmake"
-# TODO: the only packages actually installed on CentOS are:
-# cmake                 x86_64        2.6.4-5.el6              base        5.2 M
-# openssh-server        x86_64        5.3p1-84.1.el6           base        299 k
-
-# Install base debs needed by both the warden and bosh
 pkg_mgr install $packages
