53c14e0d3b3c51b71859c5150f3f91d2eee0a264
Ferran Rodenas <frodenas@gmail.com>
2013-09-11 17:14:00 +0200

    [cli] Add command to list current locks

---
+++ bosh_cli/lib/cli/commands/locks.rb
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2013 GoPivotal, Inc.
+
+module Bosh::Cli::Command
+  class Locks < Base
+
+    usage 'locks'
+    desc 'Show list of current locks'
+    def locks
+      auth_required
+
+      locks = director.list_locks
+      err('No locks') if locks.empty?
+
+      show_locks_table(locks)
+      say("Locks total: %d" % locks.size)
+    end
+
+    private
+
+    def show_locks_table(locks)
+      locks_table = table do |t|
+        t.headings = ['Type', 'Resource', 'Expires at']
+        locks.each do |lock|
+          t << [lock['type'], lock['resource'].join(':'), Time.at(lock['timeout'].to_i).utc]
+        end
+      end
+
+      nl
+      say(locks_table)
+      nl
+    end
+  end
+end
