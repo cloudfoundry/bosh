d4c8e7ee114111862c54176a34d81dc3227ee71d
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-09-12 15:41:39 -0700

    DeploymentManager delegates find_by_name to DeploymentLookup
    Signed-off-by: Jeffrey Peckham <peckham@pivotallabs.com>

--- director/spec/unit/api/deployment_manager_spec.rb
+++ director/spec/unit/api/deployment_manager_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Bosh::Director::Api::DeploymentManager do
+  let(:deployment) { double('Deployment', name: 'DEPLOYMENT_NAME') }
   let(:task) { double('Task', id: 42) }
   let(:user) { 'FAKE_USER' }
 
@@ -37,8 +38,6 @@ describe Bosh::Director::Api::DeploymentManager do
   end
 
   describe '#delete_deployment' do
-    let(:deployment) { double('Deployment', name: 'DEPLOYMENT_NAME') }
-
     it 'enqueues a resque job' do
       Resque.should_receive(:enqueue).with(BD::Jobs::DeleteDeployment, task.id, 'DEPLOYMENT_NAME', {})
 
@@ -47,6 +46,19 @@ describe Bosh::Director::Api::DeploymentManager do
 
     it 'returns the task created by JobQueue' do
       expect(subject.delete_deployment(user, deployment)).to eq(task)
+    end
+  end
+
+  describe '#find_by_name' do
+    let(:deployment_lookup) { instance_double('Bosh::Director::Api::DeploymentLookup') }
+
+    before do
+      Bosh::Director::Api::DeploymentLookup.stub(new: deployment_lookup)
+    end
+
+    it 'delegates to DeploymentLookup' do
+      deployment_lookup.should_receive(:by_name).with(deployment.name).and_return(deployment)
+      expect(subject.find_by_name(deployment.name)).to eq deployment
     end
   end
 end
