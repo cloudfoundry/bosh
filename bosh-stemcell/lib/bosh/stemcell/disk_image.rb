4155d17a6ad6d94e60e74c68611225c460fbe7fc
Aram Price <aprice@pivotallabs.com>
2013-09-13 10:19:40 -0700

    DiskImage displays commands as they are run
    Signed-off-by: Tony Hansmann <thansmann@pivotallabs.com>

--- bosh-stemcell/lib/bosh/stemcell/disk_image.rb
+++ bosh-stemcell/lib/bosh/stemcell/disk_image.rb
@@ -9,11 +9,11 @@ module Bosh::Stemcell
     end
 
     def mount
-      shell.run("sudo mount #{stemcell_loopback_device_name} #{image_mount_point}")
+      shell.run("sudo mount #{stemcell_loopback_device_name} #{image_mount_point}", output_command: true)
     end
 
     def unmount
-      shell.run("sudo umount #{image_mount_point}")
+      shell.run("sudo umount #{image_mount_point}", output_command: true)
       unmap_image
     end
 
@@ -27,14 +27,14 @@ module Bosh::Stemcell
 
     def map_image
       unless @image_device
-        output = shell.run("sudo kpartx -av #{image_file_path}")
+        output = shell.run("sudo kpartx -av #{image_file_path}", output_command: true)
         @image_device = output.split(' ')[2]
       end
       @image_device
     end
 
     def unmap_image
-      shell.run("sudo kpartx -dv #{image_file_path}")
+      shell.run("sudo kpartx -dv #{image_file_path}", output_command: true)
     end
   end
 end
--- bosh-stemcell/spec/bosh/stemcell/disk_image_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/disk_image_spec.rb
@@ -33,15 +33,16 @@ module Bosh::Stemcell
 
     describe '#mount' do
       it 'maps the file to a loop device' do
-        shell.should_receive(:run).with('sudo kpartx -av /path/to/FAKE_IMAGE').and_return(kpartx_output)
+        shell.should_receive(:run).with('sudo kpartx -av /path/to/FAKE_IMAGE',
+                                        output_command: true).and_return(kpartx_output)
 
         image.mount
       end
 
       it 'mounts the loop device' do
-        shell.stub(:run).with('sudo kpartx -av /path/to/FAKE_IMAGE').and_return(kpartx_output)
+        shell.stub(:run).with('sudo kpartx -av /path/to/FAKE_IMAGE', output_command: true).and_return(kpartx_output)
 
-        shell.should_receive(:run).with('sudo mount /dev/mapper/FAKE_LOOP1p1 /fake/mnt')
+        shell.should_receive(:run).with('sudo mount /dev/mapper/FAKE_LOOP1p1 /fake/mnt', output_command: true)
 
         image.mount
       end
@@ -49,8 +50,8 @@ module Bosh::Stemcell
 
     describe '#unmount' do
       it 'unmounts the loop device and then unmaps the file' do
-        shell.should_receive(:run).with('sudo umount /fake/mnt').ordered
-        shell.should_receive(:run).with('sudo kpartx -dv /path/to/FAKE_IMAGE').ordered
+        shell.should_receive(:run).with('sudo umount /fake/mnt', output_command: true).ordered
+        shell.should_receive(:run).with('sudo kpartx -dv /path/to/FAKE_IMAGE', output_command: true).ordered
 
         image.unmount
       end
