8100916e0624d52c1edbded1d80154dd71ea8be7
Kai Xiang <kxiang@pivotallabs.com>
2013-09-16 16:16:30 +0800

    fix mock_sh

--- bosh_warden_cpi/spec/spec_helper.rb
+++ bosh_warden_cpi/spec/spec_helper.rb
@@ -12,7 +12,7 @@ def mock_sh (cmd, su = false, times = 1, success = true)
   zero_exit_status = mock('Process::Status', exit_status: 0)
   result = mock('Result', :success? => success)
   prefix = (su == true)? 'sudo -n ' : ''
-  Bosh::Exec.should_receive(:sh).exactly(times).times.with(/#{prefix}#{cmd}.*/, yield: :on_false).and_yield(result).and_return(zero_exit_status)
+  Bosh::Exec.should_receive(:sh).exactly(times).times.with(/#{prefix}#{cmd}/, yield: :on_false).and_yield(result).and_return(zero_exit_status)
 end
 
 RSpec.configure do |conf|
