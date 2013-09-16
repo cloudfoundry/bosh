9d44ed0730a934aae0de2785fab8020d1f7f411e
Jesse Zhang <jz@pivotallabs.com>
2013-09-16 09:40:07 -0700

    Replace explicit subjects with named ones
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotallabs.com>

--- bosh_vsphere_cpi/spec/unit/cloud/vsphere/cloud_spec.rb
+++ bosh_vsphere_cpi/spec/unit/cloud/vsphere/cloud_spec.rb
@@ -5,7 +5,7 @@ describe VSphereCloud::Cloud do
   let(:client) { instance_double('VSphereCloud::Client', login: nil, logout: nil, stub: client_stub) }
   let(:config) { { fake: 'config' } }
 
-  subject { VSphereCloud::Cloud.new(config) }
+  subject(:vsphere_cloud) { VSphereCloud::Cloud.new(config) }
 
   before do
     VSphereCloud::Config.should_receive(:configure).with(config)
@@ -18,22 +18,22 @@ describe VSphereCloud::Cloud do
 
     context 'the vm is found' do
       it 'returns true' do
-        subject.should_receive(:get_vm_by_cid).with(vm_id)
-        expect(subject.has_vm?(vm_id)).to be_true
+        vsphere_cloud.should_receive(:get_vm_by_cid).with(vm_id)
+        expect(vsphere_cloud.has_vm?(vm_id)).to be_true
       end
     end
 
     context 'the vm is not found' do
       it 'returns false' do
-        subject.should_receive(:get_vm_by_cid).with(vm_id).and_raise(Bosh::Clouds::VMNotFound)
-        expect(subject.has_vm?(vm_id)).to be_false
+        vsphere_cloud.should_receive(:get_vm_by_cid).with(vm_id).and_raise(Bosh::Clouds::VMNotFound)
+        expect(vsphere_cloud.has_vm?(vm_id)).to be_false
       end
     end
   end
 
   describe 'snapshot_disk' do
     it 'raises not implemented exception when called' do
-      expect { subject.snapshot_disk('123') }.to raise_error(Bosh::Clouds::NotImplemented)
+      expect { vsphere_cloud.snapshot_disk('123') }.to raise_error(Bosh::Clouds::NotImplemented)
     end
   end
 end
