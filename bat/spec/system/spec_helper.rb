d123064716ca316257ac8ab52c3d18844e36c8ce
Jesse Zhang <jz@pivotallabs.com>
2013-09-20 10:52:21 -0700

    Move bosh_helper and deployment_helper into bat/lib

--- bat/spec/system/spec_helper.rb
+++ bat/spec/system/spec_helper.rb
@@ -1,25 +1,24 @@
 require 'spec_helper'
 
 require 'yaml'
+require 'fileutils'
+require 'tempfile'
+require 'resolv'
+
+require 'common/exec'
+
 require 'bat/stemcell'
 require 'bat/release'
 require 'bat/deployment'
-require 'fileutils'
-
-require 'tempfile'
-require 'common/exec'
-require 'resolv'
+require 'bat/bosh_helper'
+require 'bat/deployment_helper'
 
 ASSETS_DIR = File.join(SPEC_ROOT, 'system', 'assets')
 BAT_RELEASE_DIR = File.join(ASSETS_DIR, 'bat-release')
 
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'helpers', '*_helper.rb')).each do |helper|
-  require File.expand_path(helper)
-end
-
 RSpec.configure do |config|
-  config.include(BoshHelper)
-  config.include(DeploymentHelper)
+  config.include(Bat::BoshHelper)
+  config.include(Bat::DeploymentHelper)
 
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -27,16 +26,16 @@ RSpec.configure do |config|
 
   # bosh helper isn't available, so it has to be rolled by hand
   config.before(:suite) do
-    director = BoshHelper.read_environment('BAT_DIRECTOR')
+    director = Bat::BoshHelper.read_environment('BAT_DIRECTOR')
     director.should_not be_nil
 
-    output = %x{bosh --config #{BoshHelper.bosh_cli_config_path} --user admin --password admin target #{director} 2>&1}
+    output = %x{bosh --config #{Bat::BoshHelper.bosh_cli_config_path} --user admin --password admin target #{director} 2>&1}
     output.should match /Target \w*\s*set/
     $?.exitstatus.should == 0
   end
 
   config.after(:suite) do
-    BoshHelper.delete_bosh_cli_config
+    Bat::BoshHelper.delete_bosh_cli_config
   end
 
   config.before(:each) do
