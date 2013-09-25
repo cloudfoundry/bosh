3b3341e39deac29ba0fbf0252444495bc1be85bb
Matthew Boedicker <mboedicker@pivotallabs.com>
2013-09-25 10:53:50 -0700

    use class method instead of shared_examples_for
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- bosh-stemcell/spec/bosh/stemcell/builder_options_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/builder_options_spec.rb
@@ -87,82 +87,83 @@ module Bosh::Stemcell
         expect(result['stemcell_version']).to eq('007')
       end
 
-      shared_examples_for 'setting default stemcells environment values' do
-        let(:environment_hash) do
-          {
-            'UBUNTU_ISO' => 'fake_ubuntu_iso',
-            'UBUNTU_MIRROR' => 'fake_ubuntu_mirror',
-            'RUBY_BIN' => 'fake_ruby_bin',
-          }
-        end
-
-        it 'sets default values for options based in hash' do
-          expected_release_micro_manifest_path =
-            File.join(expected_source_root, "release/micro/#{infrastructure.name}.yml")
-
-          result = stemcell_builder_options.default
-
-          expect(result['stemcell_name']).to eq(
-            "bosh-#{infrastructure.name}-#{infrastructure.hypervisor}-#{operating_system.name}")
-          expect(result['stemcell_operating_system']).to eq(operating_system.name)
-          expect(result['stemcell_infrastructure']).to eq(infrastructure.name)
-          expect(result['stemcell_hypervisor']).to eq(infrastructure.hypervisor)
-          expect(result['bosh_protocol_version']).to eq('1')
-          expect(result['UBUNTU_ISO']).to eq('fake_ubuntu_iso')
-          expect(result['UBUNTU_MIRROR']).to eq('fake_ubuntu_mirror')
-          expect(result['ruby_bin']).to eq('fake_ruby_bin')
-          expect(result['bosh_release_src_dir']).to eq(File.join(expected_source_root, '/release/src/bosh'))
-          expect(result['bosh_agent_src_dir']).to eq(File.join(expected_source_root, 'bosh_agent'))
-          expect(result['image_create_disk_size']).to eq(default_disk_size)
-          expect(result['bosh_micro_enabled']).to eq('yes')
-          expect(result['bosh_micro_package_compiler_path']).to eq(File.join(expected_source_root, 'package_compiler'))
-          expect(result['bosh_micro_manifest_yml_path']).to eq(expected_release_micro_manifest_path)
-          expect(result['bosh_micro_release_tgz_path']).to eq('fake/release.tgz')
-        end
-
-        context 'when RUBY_BIN is not set' do
-          before do
-            environment_hash.delete('RUBY_BIN')
+      # rubocop:disable MethodLength
+      def self.it_sets_correct_environment_variables
+        describe 'setting enviroment variables' do
+          let(:environment_hash) do
+            {
+              'UBUNTU_ISO' => 'fake_ubuntu_iso',
+              'UBUNTU_MIRROR' => 'fake_ubuntu_mirror',
+              'RUBY_BIN' => 'fake_ruby_bin',
+            }
           end
 
-          before do
-            RbConfig::CONFIG.stub(:[]).with('bindir').and_return('/a/path/to/')
-            RbConfig::CONFIG.stub(:[]).with('ruby_install_name').and_return('ruby')
-          end
+          it 'sets default values for options based in hash' do
+            expected_release_micro_manifest_path =
+              File.join(expected_source_root, "release/micro/#{infrastructure.name}.yml")
 
-          it 'uses the RbConfig values' do
             result = stemcell_builder_options.default
 
-            expect(result['ruby_bin']).to eq('/a/path/to/ruby')
-          end
-        end
-
-        context 'when disk_size is not passed' do
-          it 'defaults to default disk size for infrastructure' do
-            result = stemcell_builder_options.default
-
+            expect(result['stemcell_name']).to eq(
+              "bosh-#{infrastructure.name}-#{infrastructure.hypervisor}-#{operating_system.name}")
+            expect(result['stemcell_operating_system']).to eq(operating_system.name)
+            expect(result['stemcell_infrastructure']).to eq(infrastructure.name)
+            expect(result['stemcell_hypervisor']).to eq(infrastructure.hypervisor)
+            expect(result['bosh_protocol_version']).to eq('1')
+            expect(result['UBUNTU_ISO']).to eq('fake_ubuntu_iso')
+            expect(result['UBUNTU_MIRROR']).to eq('fake_ubuntu_mirror')
+            expect(result['ruby_bin']).to eq('fake_ruby_bin')
+            expect(result['bosh_release_src_dir']).to eq(File.join(expected_source_root, '/release/src/bosh'))
+            expect(result['bosh_agent_src_dir']).to eq(File.join(expected_source_root, 'bosh_agent'))
             expect(result['image_create_disk_size']).to eq(default_disk_size)
+            expect(result['bosh_micro_enabled']).to eq('yes')
+            expect(result['bosh_micro_package_compiler_path']).to eq(
+              File.join(expected_source_root, 'package_compiler'))
+            expect(result['bosh_micro_manifest_yml_path']).to eq(expected_release_micro_manifest_path)
+            expect(result['bosh_micro_release_tgz_path']).to eq('fake/release.tgz')
           end
-        end
 
-        context 'when disk_size is passed' do
-          before do
-            options.merge!(disk_size: 1234)
+          context 'when RUBY_BIN is not set' do
+            before { environment_hash.delete('RUBY_BIN') }
+
+            before do
+              RbConfig::CONFIG.stub(:[]).with('bindir').and_return('/a/path/to/')
+              RbConfig::CONFIG.stub(:[]).with('ruby_install_name').and_return('ruby')
+            end
+
+            it 'uses the RbConfig values' do
+              result = stemcell_builder_options.default
+
+              expect(result['ruby_bin']).to eq('/a/path/to/ruby')
+            end
           end
 
-          it 'allows user to override default disk_size' do
-            result = stemcell_builder_options.default
+          context 'when disk_size is not passed' do
+            it 'defaults to default disk size for infrastructure' do
+              result = stemcell_builder_options.default
 
-            expect(result['image_create_disk_size']).to eq(1234)
+              expect(result['image_create_disk_size']).to eq(default_disk_size)
+            end
+          end
+
+          context 'when disk_size is passed' do
+            before { options.merge!(disk_size: 1234) }
+
+            it 'allows user to override default disk_size' do
+              result = stemcell_builder_options.default
+
+              expect(result['image_create_disk_size']).to eq(1234)
+            end
           end
         end
       end
+      # rubocop:enable MethodLength
 
       describe 'infrastructure variation' do
         context 'when infrastruture is aws' do
           let(:infrastructure) { Infrastructure.for('aws') }
 
-          it_behaves_like 'setting default stemcells environment values'
+          it_sets_correct_environment_variables
 
           it 'has no "image_vsphere_ovf_ovftool_path" key' do
             expect(stemcell_builder_options.default).not_to have_key('image_vsphere_ovf_ovftool_path')
@@ -172,7 +173,7 @@ module Bosh::Stemcell
         context 'when infrastruture is vsphere' do
           let(:infrastructure) { Infrastructure.for('vsphere') }
 
-          it_behaves_like 'setting default stemcells environment values'
+          it_sets_correct_environment_variables
 
           it 'has an "image_vsphere_ovf_ovftool_path" key' do
             result = stemcell_builder_options.default
@@ -195,7 +196,7 @@ module Bosh::Stemcell
           let(:infrastructure) { Infrastructure.for('openstack') }
           let(:default_disk_size) { 10240 }
 
-          it_behaves_like 'setting default stemcells environment values'
+          it_sets_correct_environment_variables
 
           it 'has no "image_vsphere_ovf_ovftool_path" key' do
             expect(stemcell_builder_options.default).not_to have_key('image_vsphere_ovf_ovftool_path')
