f0bbdbd2b6ef2339e7e43dd28a77739001139f19
Jesse Zhang <jz@pivotallabs.com>
2013-09-26 16:53:08 -0700

    fixed namespace for ArtifactsDownloader
    Signed-off-by: dmitriy kalinin <cppforlife@pivotallabs.com>

--- bosh-dev/spec/bosh/dev/artifacts_downloader_spec.rb
+++ bosh-dev/spec/bosh/dev/artifacts_downloader_spec.rb
@@ -1,32 +1,30 @@
 require 'spec_helper'
 require 'bosh/dev/artifacts_downloader'
 
-module Bosh
-  module Dev
-    describe ArtifactsDownloader do
-      let(:fake_download_adapter) { DownloadAdapter.new }
-      subject(:artifacts_downloader) { ArtifactsDownloader.new }
-      before { DownloadAdapter.stub(new: fake_download_adapter) }
+module Bosh::Dev
+  describe ArtifactsDownloader do
+    let(:fake_download_adapter) { DownloadAdapter.new }
+    subject(:artifacts_downloader) { ArtifactsDownloader.new }
+    before { DownloadAdapter.stub(new: fake_download_adapter) }
 
-      describe '#download_release' do
-        it 'downloads a release and returns path' do
-          fake_download_adapter.
-              should_receive(:download).
-              with('http://s3.amazonaws.com/bosh-jenkins-artifacts/release/bosh-123.tgz', 'bosh-123.tgz').
-              and_return(File.join(Dir.pwd, 'where-it-was-written-to'))
-          expect(artifacts_downloader.download_release('123')).to eq File.join(Dir.pwd, 'where-it-was-written-to')
-        end
+    describe '#download_release' do
+      it 'downloads a release and returns path' do
+        fake_download_adapter.
+            should_receive(:download).
+            with('http://s3.amazonaws.com/bosh-jenkins-artifacts/release/bosh-123.tgz', 'bosh-123.tgz').
+            and_return(File.join(Dir.pwd, 'where-it-was-written-to'))
+        expect(artifacts_downloader.download_release('123')).to eq File.join(Dir.pwd, 'where-it-was-written-to')
       end
+    end
 
-      describe '#download_stemcell' do
-        it 'downloads a stemcell and returns path' do
-          fake_download_adapter.
-              should_receive(:download).
-              with('http://s3.amazonaws.com/bosh-jenkins-artifacts/bosh-stemcell/aws/light-bosh-stemcell-123-aws-xen-ubuntu.tgz',
-                   'light-bosh-stemcell-123-aws-xen-ubuntu.tgz').
-              and_return(File.join(Dir.pwd, 'where-it-was-written-to'))
-          expect(artifacts_downloader.download_stemcell('123')).to eq File.join(Dir.pwd, 'where-it-was-written-to')
-        end
+    describe '#download_stemcell' do
+      it 'downloads a stemcell and returns path' do
+        fake_download_adapter.
+            should_receive(:download).
+            with('http://s3.amazonaws.com/bosh-jenkins-artifacts/bosh-stemcell/aws/light-bosh-stemcell-123-aws-xen-ubuntu.tgz',
+                 'light-bosh-stemcell-123-aws-xen-ubuntu.tgz').
+            and_return(File.join(Dir.pwd, 'where-it-was-written-to'))
+        expect(artifacts_downloader.download_stemcell('123')).to eq File.join(Dir.pwd, 'where-it-was-written-to')
       end
     end
   end
