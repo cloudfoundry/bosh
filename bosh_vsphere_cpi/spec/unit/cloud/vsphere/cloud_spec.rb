0d901e35e90454e01ab8e464524d1fab99047878
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-09-30 15:45:46 -0700

    Backfill tests for #create_nic_config_spec
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- bosh_vsphere_cpi/spec/unit/cloud/vsphere/cloud_spec.rb
+++ bosh_vsphere_cpi/spec/unit/cloud/vsphere/cloud_spec.rb
@@ -183,4 +183,87 @@ describe VSphereCloud::Cloud do
     end
   end
 
+  describe '#create_nic_config_spec' do
+    let(:dvs_index) { {} }
+
+    context 'using a distributed switch' do
+      let(:v_network_name) { 'fake_network1' }
+      let(:network) { instance_double('Vim::Dvs::DistributedVirtualPortgroup', class: VimSdk::Vim::Dvs::DistributedVirtualPortgroup) }
+      let(:dvs_index) { {} }
+      let(:switch) { double() }
+      let(:portgroup_properties) { { 'config.distributedVirtualSwitch' => switch, 'config.key' => 'fake_portgroup_key' } }
+
+      before do
+        client.stub(:get_properties).with(network, VimSdk::Vim::Dvs::DistributedVirtualPortgroup,
+                                          ['config.key', 'config.distributedVirtualSwitch'],
+                                          ensure_all: true).and_return(portgroup_properties)
+
+        client.stub(:get_property).with(switch, VimSdk::Vim::DistributedVirtualSwitch,
+                                        'uuid', ensure_all: true).and_return('fake_switch_uuid')
+      end
+
+      it 'sets correct port in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        port = device_config_spec.device.backing.port
+        expect(port.switch_uuid).to eq('fake_switch_uuid')
+        expect(port.portgroup_key).to eq('fake_portgroup_key')
+      end
+
+      it 'sets correct backing in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        backing = device_config_spec.device.backing
+        expect(backing).to be_a(VimSdk::Vim::Vm::Device::VirtualEthernetCard::DistributedVirtualPortBackingInfo)
+      end
+
+      it 'adds record to dvs_index for portgroup_key' do
+        vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        expect(dvs_index['fake_portgroup_key']).to eq('fake_network1')
+      end
+
+      it 'sets correct device in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        device = device_config_spec.device
+        expect(device.key).to eq(-1)
+        expect(device.controller_key).to eq('fake_controller_key')
+      end
+
+      it 'sets correct operation in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        expect(device_config_spec.operation).to eq(VimSdk::Vim::Vm::Device::VirtualDeviceSpec::Operation::ADD)
+      end
+    end
+
+    context 'using a standard switch' do
+      let(:v_network_name) { 'fake_network1' }
+      let(:network) { double() }
+      let(:dvs_index) { {} }
+
+      it 'sets correct backing in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        backing = device_config_spec.device.backing
+        expect(backing).to be_a(VimSdk::Vim::Vm::Device::VirtualEthernetCard::NetworkBackingInfo)
+        expect(backing.device_name).to eq(v_network_name)
+      end
+
+      it 'sets correct device in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        device = device_config_spec.device
+        expect(device.key).to eq(-1)
+        expect(device.controller_key).to eq('fake_controller_key')
+      end
+
+      it 'sets correct operation in device config spec' do
+        device_config_spec = vsphere_cloud.create_nic_config_spec(v_network_name, network, 'fake_controller_key', dvs_index)
+
+        expect(device_config_spec.operation).to eq(VimSdk::Vim::Vm::Device::VirtualDeviceSpec::Operation::ADD)
+      end
+    end
+  end
 end
