029f56ab001947d362ec1d8cd74ae756cdcabc07
Jesse Zhang <jz@pivotallabs.com>
2013-09-30 00:54:13 -0700

    #mount takes options

--- bosh_agent/lib/bosh_agent/message/disk.rb
+++ bosh_agent/lib/bosh_agent/message/disk.rb
@@ -39,24 +39,23 @@ module Bosh::Agent
       end
 
       def mount_store(cid, options="")
-        Mounter.new(Config.platform, cid, options, store_path, logger, self).mount
+        Mounter.new(Config.platform, cid, store_path, logger, self).mount(options)
       end
 
       class Mounter
-        def initialize(platform, cid, options, store_path, logger, backticker)
+        def initialize(platform, cid, store_path, logger, backticker)
           @platform = platform
           @cid = cid
-          @options = options
           @store_path = store_path
           @logger = logger
           @backticker = backticker
         end
 
-        def mount
+        def mount(options)
           disk = @platform.lookup_disk_by_cid(@cid)
           partition = "#{disk}1"
           @logger.info("Mounting: #{partition} #{@store_path}")
-          @backticker.send(:`, "mount #{@options} #{partition} #{@store_path}")
+          @backticker.send(:`, "mount #{options} #{partition} #{@store_path}")
           unless $?.exitstatus == 0
             raise Bosh::Agent::MessageHandlerError, "Failed to mount: #{partition} #{@store_path} (exit code #{$?.exitstatus})"
           end
