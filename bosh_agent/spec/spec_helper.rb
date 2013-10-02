e31a6e0f6b8f12ad01b364fce0b6ab2e5804006d
Jeffrey Peckham <peckham@pivotallabs.com>
2013-10-01 18:06:21 -0700

    Start to backfill tests for MountDisk
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotallabs.com>

--- bosh_agent/spec/spec_helper.rb
+++ bosh_agent/spec/spec_helper.rb
@@ -6,6 +6,7 @@ ENV['RACK_ENV'] = 'test'
 
 require 'bosh_agent'
 require 'timecop'
+require 'fakefs/spec_helpers'
 
 Dir.glob(File.expand_path('support/**/*.rb', File.dirname(__FILE__))).each do |support|
   require support
