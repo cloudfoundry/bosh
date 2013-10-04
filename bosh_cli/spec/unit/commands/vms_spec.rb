c1b678057824c915e05a86f31bc4ae429cc03ad2
Caleb Miles <cmiles@pivotallabs.com>
2013-10-04 11:12:42 -0700

    formatting
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh_cli/spec/unit/commands/vms_spec.rb
+++ bosh_cli/spec/unit/commands/vms_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-
 require 'cli'
 
 describe Bosh::Cli::Command::Vms do
@@ -8,7 +7,7 @@ describe Bosh::Cli::Command::Vms do
   let(:deployment) { 'dep1' }
   let(:target) { 'http://example.org' }
 
-  before(:each) do
+  before do
     command.stub(:director).and_return(director)
     command.stub(:nl)
     command.stub(:logged_in? => true)
@@ -16,26 +15,16 @@ describe Bosh::Cli::Command::Vms do
   end
 
   describe 'list' do
-
     context 'with no arguments' do
-
       it 'lists vms in all deployments' do
-        director.stub(:list_deployments) {
-          [
-              {'name' => 'dep1'},
-              {'name' => 'dep2'},
-          ]
-        }
-
+        director.stub(:list_deployments) { [{ 'name' => 'dep1' }, { 'name' => 'dep2' }] }
         command.should_receive(:show_deployment).with('dep1', target: target)
         command.should_receive(:show_deployment).with('dep2', target: target)
-
         command.list
       end
 
       it 'raises an error if there are not any deployments' do
-        director.stub(:list_deployments) {[]}
-
+        director.stub(:list_deployments) { [] }
         expect {
           command.list
         }.to raise_error(Bosh::Cli::CliError, 'No deployments')
@@ -43,13 +32,10 @@ describe Bosh::Cli::Command::Vms do
     end
 
     context 'with a deployment argument' do
-
       it 'lists vms in the deployment' do
         command.should_receive(:show_deployment).with('dep1', target: target)
-
         command.list('dep1')
       end
-
     end
   end
 
@@ -69,36 +55,33 @@ describe Bosh::Cli::Command::Vms do
         'vm_cid' => 'cid1',
         'agent_id' => 'agent1',
         'vitals' => {
-            'load' => [1, 2, 3],
-            'cpu' => {
-                'user' => 4,
-                'sys' => 5,
-                'wait' => 6,
-            },
-            'mem' => {
-                'percent' => 7,
-                'kb' => 8,
-            },
-            'swap' => {
-                'percent' => 9,
-                'kb' => 10,
-            },
-            'disk' => {
-                'system' => {'percent' => 11},
-                'ephemeral' => {'percent' => 12},
-                'persistent' => {'percent' => 13},
-            },
+          'load' => [1, 2, 3],
+          'cpu' => {
+            'user' => 4,
+            'sys' => 5,
+            'wait' => 6,
+          },
+          'mem' => {
+            'percent' => 7,
+            'kb' => 8,
+          },
+          'swap' => {
+            'percent' => 9,
+            'kb' => 10,
+          },
+          'disk' => {
+            'system' => {'percent' => 11},
+            'ephemeral' => {'percent' => 12},
+            'persistent' => {'percent' => 13},
+          },
         },
-        'resurrection_paused' => true
+        'resurrection_paused' => true,
       }
     }
 
-    before(:each) do
-      director.stub(:fetch_vm_state).with(deployment) { [vm_state] }
-    end
+    before { director.stub(:fetch_vm_state).with(deployment) { [vm_state] } }
 
     context 'default' do
-
       it 'show basic vms information' do
         command.should_receive(:say).with("Deployment `#{deployment}'")
         command.should_receive(:say) do |s|
@@ -108,10 +91,8 @@ describe Bosh::Cli::Command::Vms do
           expect(s.to_s).to include '192.168.0.1'
         end
         command.should_receive(:say).with('VMs total: 1')
-
-        command.show_deployment deployment, details: details, dns: dns, vitals: vitals
+        command.show_deployment(deployment, details: details, dns: dns, vitals: vitals)
       end
-
     end
 
     context 'with details' do
@@ -129,10 +110,8 @@ describe Bosh::Cli::Command::Vms do
           expect(s.to_s).to include 'paused'
         end
         command.should_receive(:say).with('VMs total: 1')
-
-        command.show_deployment deployment, details: details, dns: dns, vitals: vitals
+        command.show_deployment(deployment, details: details, dns: dns, vitals: vitals)
       end
-
     end
 
     context 'with DNS A records' do
@@ -148,10 +127,8 @@ describe Bosh::Cli::Command::Vms do
           expect(s.to_s).to include 'index.job.network.deployment.microbosh'
         end
         command.should_receive(:say).with('VMs total: 1')
-
-        command.show_deployment deployment, details: details, dns: dns, vitals: vitals
+        command.show_deployment(deployment, details: details, dns: dns, vitals: vitals)
       end
-
     end
 
     context 'with vitals' do
@@ -175,8 +152,7 @@ describe Bosh::Cli::Command::Vms do
           expect(s.to_s).to include '13%'
         end
         command.should_receive(:say).with('VMs total: 1')
-
-        command.show_deployment deployment, details: details, dns: dns, vitals: vitals
+        command.show_deployment(deployment, details: details, dns: dns, vitals: vitals)
       end
 
       it 'shows the vm vitals with unavailable ephemeral and persistent disks' do
@@ -191,12 +167,8 @@ describe Bosh::Cli::Command::Vms do
           expect(s.to_s).to_not include '13%'
         end
         command.should_receive(:say).with('VMs total: 1')
-
         command.show_deployment deployment, details: details, dns: dns, vitals: vitals
       end
-
     end
-
   end
-
 end
