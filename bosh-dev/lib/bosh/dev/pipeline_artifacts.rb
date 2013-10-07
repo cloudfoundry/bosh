82e265ea53055f5145ef6156f42e5da5ea00e370
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-10-07 11:20:03 -0700

    removed unneeded namespace in PipelineArtifacts
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-dev/lib/bosh/dev/pipeline_artifacts.rb
+++ bosh-dev/lib/bosh/dev/pipeline_artifacts.rb
@@ -2,61 +2,59 @@ require 'bosh/stemcell/operating_system'
 require 'bosh/stemcell/archive_filename'
 require 'bosh/stemcell/infrastructure'
 
-module Bosh
-  module Dev
-    class PipelineArtifacts
-      def self.all(version)
-        matrix_names = [
-          %w(vsphere   ubuntu),
-          %w(vsphere   centos),
-          %w(aws       ubuntu),
-          %w(openstack ubuntu),
+module Bosh::Dev
+  class PipelineArtifacts
+    def self.all(version)
+      matrix_names = [
+        %w(vsphere   ubuntu),
+        %w(vsphere   centos),
+        %w(aws       ubuntu),
+        %w(openstack ubuntu),
+      ]
+
+      matrix = matrix_names.map do |(infrastructure_name, os_name)|
+        [
+          Bosh::Stemcell::Infrastructure.for(infrastructure_name),
+          Bosh::Stemcell::OperatingSystem.for(os_name),
         ]
-
-        matrix = matrix_names.map do |(infrastructure_name, os_name)|
-          [
-            Bosh::Stemcell::Infrastructure.for(infrastructure_name),
-            Bosh::Stemcell::OperatingSystem.for(os_name),
-          ]
-        end
-
-        new(version, matrix)
       end
 
-      def initialize(version, matrix)
-        @version = version
-        @matrix = matrix
-      end
+      new(version, matrix)
+    end
 
-      def list
-        artifact_names = []
+    def initialize(version, matrix)
+      @version = version
+      @matrix = matrix
+    end
 
-        matrix.each do |(infrastructure, operating_system)|
-          versions.each do |version|
-            filename = Bosh::Stemcell::ArchiveFilename.new(version, infrastructure, operating_system, 'bosh-stemcell', false)
-            artifact_names << archive_path(filename.to_s, infrastructure)
+    def list
+      artifact_names = []
 
-            if infrastructure.light?
-              light_filename = Bosh::Stemcell::ArchiveFilename.new(version, infrastructure, operating_system, 'bosh-stemcell', true)
-              artifact_names << archive_path(light_filename.to_s, infrastructure)
-            end
+      matrix.each do |(infrastructure, operating_system)|
+        versions.each do |version|
+          filename = Bosh::Stemcell::ArchiveFilename.new(version, infrastructure, operating_system, 'bosh-stemcell', false)
+          artifact_names << archive_path(filename.to_s, infrastructure)
+
+          if infrastructure.light?
+            light_filename = Bosh::Stemcell::ArchiveFilename.new(version, infrastructure, operating_system, 'bosh-stemcell', true)
+            artifact_names << archive_path(light_filename.to_s, infrastructure)
           end
         end
-
-        artifact_names
       end
 
-      private
+      artifact_names
+    end
 
-      attr_reader :version, :matrix
+    private
 
-      def versions
-        [version, 'latest']
-      end
+    attr_reader :version, :matrix
 
-      def archive_path(filename, infrastructure)
-        File.join('bosh-stemcell', infrastructure.name, filename)
-      end
+    def versions
+      [version, 'latest']
+    end
+
+    def archive_path(filename, infrastructure)
+      File.join('bosh-stemcell', infrastructure.name, filename)
     end
   end
 end
