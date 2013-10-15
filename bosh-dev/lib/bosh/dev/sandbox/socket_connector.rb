e99fa1bbfd926f0fc512936e4b5593142b0de216
Caleb Miles <cmiles@pivotallabs.com>
2013-10-15 15:31:37 -0700

    use TCPSocket when checking if port is open for sandbox processes
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-dev/lib/bosh/dev/sandbox/socket_connector.rb
+++ bosh-dev/lib/bosh/dev/sandbox/socket_connector.rb
@@ -1,18 +1,19 @@
 require 'socket'
+require 'timeout'
 require 'bosh/dev'
 
 module Bosh::Dev::Sandbox
   class SocketConnector
     def initialize(host, port, logger)
-      @addr = Socket.pack_sockaddr_in(port, host)
+      @host = host
+      @port = port
       @logger = logger
     end
 
     def try_to_connect(remaining_attempts = 20)
       remaining_attempts -= 1
-      socket = Socket.new(Socket::Constants::AF_INET, Socket::Constants::SOCK_STREAM, 0)
-      socket.connect(@addr)
-    rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT
+      Timeout.timeout(1) { TCPSocket.new(@host, @port).close }
+    rescue Timeout::Error, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ETIMEDOUT
       raise if remaining_attempts == 0
       sleep(0.2) # unfortunate fine-tuning required here
       retry
