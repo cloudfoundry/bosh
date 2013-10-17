25bddada04a8cf26b7e81c64f2bc53f930dc3d76
Erin Friesen <e.friesen@pivotallabs.com>
2013-10-17 16:52:52 -0700

    formatting
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- blobstore_client/spec/unit/base_spec.rb
+++ blobstore_client/spec/unit/base_spec.rb
@@ -1,73 +1,69 @@
-# -*- encoding: utf-8 -*-
-
 require 'spec_helper'
 
 describe Bosh::Blobstore::BaseClient do
-  let(:subject) { described_class.new({}) }
+  subject { described_class.new({}) }
 
-  describe :create do
+  describe '#create' do
     it 'should raise a NotImplemented exception' do
-      expect do
-        subject.create('contents')
-      end.to raise_error(Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
+      expect { subject.create('contents') }.to raise_error(
+        Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
     end
 
     it 'should raise BlobstoreError exceptions' do
-      subject.should_receive(:create_file).and_raise(Bosh::Blobstore::BlobstoreError, 'Could not create object')
+      subject.should_receive(:create_file).and_raise(
+        Bosh::Blobstore::BlobstoreError, 'Could not create object')
 
-      expect do
-        subject.create('contents')
-      end.to raise_error(Bosh::Blobstore::BlobstoreError, 'Could not create object')
+      expect { subject.create('contents') }.to raise_error(
+        Bosh::Blobstore::BlobstoreError, 'Could not create object')
     end
 
     it 'should trap generic exceptions and raise a BlobstoreError exception' do
-      subject.should_receive(:create_file).and_raise(Errno::ECONNRESET, 'Could not create object')
+      subject.should_receive(:create_file).and_raise(
+        Errno::ECONNRESET, 'Could not create object')
 
-      expect do
-        subject.create('contents')
-      end.to raise_error(Bosh::Blobstore::BlobstoreError,
-                         /Errno::ECONNRESET: Connection reset by peer - Could not create object/)
+      expect { subject.create('contents') }.to raise_error(
+        Bosh::Blobstore::BlobstoreError,
+        /Errno::ECONNRESET: Connection reset by peer - Could not create object/,
+      )
     end
   end
 
-  describe :get do
+  describe '#get' do
     it 'should raise a NotImplemented exception' do
-      expect do
-        subject.get('id', 'file')
-      end.to raise_error(Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
+      expect { subject.get('id', 'file') }.to raise_error(
+        Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
     end
 
     it 'should raise BlobstoreError exceptions' do
-      subject.should_receive(:get_file).and_raise(Bosh::Blobstore::BlobstoreError, 'Could not fetch object')
+      subject.should_receive(:get_file).and_raise(
+        Bosh::Blobstore::BlobstoreError, 'Could not fetch object')
 
-      expect do
-        subject.get('id', 'file')
-      end.to raise_error(Bosh::Blobstore::BlobstoreError, 'Could not fetch object')
+      expect { subject.get('id', 'file') }.to raise_error(
+        Bosh::Blobstore::BlobstoreError, 'Could not fetch object')
     end
 
     it 'should trap generic exceptions and raise a BlobstoreError exception' do
-      subject.should_receive(:get_file).and_raise(Errno::ECONNRESET, 'Could not fetch object')
+      subject.should_receive(:get_file).and_raise(
+        Errno::ECONNRESET, 'Could not fetch object')
 
-      expect do
-        subject.get('id', 'file')
-      end.to raise_error(Bosh::Blobstore::BlobstoreError,
-                         /Errno::ECONNRESET: Connection reset by peer - Could not fetch object/)
+      expect { subject.get('id', 'file') }.to raise_error(
+        Bosh::Blobstore::BlobstoreError,
+        /Errno::ECONNRESET: Connection reset by peer - Could not fetch object/,
+      )
     end
   end
 
-  describe :delete do
+  describe '#delete' do
     it 'should raise a NotImplemented exception' do
-      expect do
-        subject.delete('id')
-      end.to raise_error(Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
+      expect { subject.delete('id') }.to raise_error(
+        Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
     end
   end
 
-  describe :exists? do
+  describe '#exists?' do
     it 'should raise a NotImplemented exception' do
-      expect do
-        subject.exists?('id')
-      end.to raise_error(Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
+      expect { subject.exists?('id') }.to raise_error(
+        Bosh::Blobstore::NotImplemented, 'not supported by this blobstore')
     end
   end
 end
