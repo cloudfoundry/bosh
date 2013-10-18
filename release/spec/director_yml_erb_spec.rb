c8df5cfa589be41207019faec2f16a826c32cd6a
Jesse Zhang <jz@pivotallabs.com>
2013-10-17 18:08:12 -0700

    pull up common set-up in tests
    [finishes #55322154]
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- release/spec/director_yml_erb_spec.rb
+++ release/spec/director_yml_erb_spec.rb
@@ -4,73 +4,77 @@ require 'common/properties'
 require 'json'
 
 describe 'director.yml.erb' do
+  let(:gigantic_hash) do
+    {
+      'properties' => {
+        'ntp' => [
+          '0.north-america.pool.ntp.org',
+          '1.north-america.pool.ntp.org',
+        ],
+        'blobstore' => {
+          'address' => '10.10.0.7',
+          'port' => 25251,
+          'agent' => { 'user' => 'agent', 'password' => '75d1605f59b60' },
+          'director' => {
+            'user' => 'user',
+            'password' => 'password'
+          },
+          'provider' => 'dav',
+        },
+        'nats' => {
+          'user' => 'nats',
+          'password' => '1a0312a24c0a0',
+          'address' => '10.10.0.7',
+          'port' => 4222
+        },
+        'redis' => {
+          'address' => '127.0.0.1', 'port' => 25255, 'password' => 'R3d!S',
+          'loglevel' => 'info',
+        },
+        'director' => {
+          'name' => 'vpc-bosh-idora',
+          'backend_port' => 25556,
+          'encryption' => false,
+          'max_tasks' => 500,
+          'max_threads' => 32,
+          'enable_snapshots' => true,
+          'db' => {
+            'adapter' => 'mysql2',
+            'user' => 'ub45391e00',
+            'password' => 'p4cd567d84d0e012e9258d2da30',
+            'host' => 'bosh.hamazonhws.com',
+            'port' => 3306,
+            'database' => 'bosh',
+            'connection_options' => {},
+          },
+          'auto_fix_stateful_nodes' => true
+        },
+        'vcenter' => {
+          'address' => 'vcenter.address',
+          'user' => 'user',
+          'password' => 'vcenter.password',
+          'datacenters' => [
+            {
+              'name' => 'vcenter.datacenters.first.name',
+              'clusters' => ['cluster1']
+            },
+          ],
+        },
+      }
+    }
+  end
+
+  let(:erb_yaml) do
+    erb_yaml_path = File.join(File.dirname(__FILE__), '../jobs/director/templates/director.yml.erb')
+
+    File.read(erb_yaml_path)
+  end
+
   context 'when vcenter.password is alphanumeric' do
     it 'renders vcenter password correctly' do
-      erb_yaml_path = File.join(File.dirname(__FILE__), '../jobs/director/templates/director.yml.erb')
+      spec = gigantic_hash
 
-      erb_yaml = File.read(erb_yaml_path)
-
-      gigantic_hash = {
-        'properties' =>
-          {
-            'ntp' => [
-              '0.north-america.pool.ntp.org',
-              '1.north-america.pool.ntp.org',
-            ],
-            'blobstore' => {
-              'address' => '10.10.0.7',
-              'port' => 25251,
-              'agent' => { 'user' => 'agent', 'password' => '75d1605f59b60' },
-              'director' => {
-                'user' => 'user',
-                'password' => 'password'
-              },
-              'provider' => 'dav',
-            },
-            'nats' => {
-              'user' => 'nats',
-              'password' => '1a0312a24c0a0',
-              'address' => '10.10.0.7',
-              'port' => 4222
-            },
-            'redis' => {
-              'address' => '127.0.0.1', 'port' => 25255, 'password' => 'R3d!S',
-              'loglevel' => 'info',
-            },
-            'director' => {
-              'name' => 'vpc-bosh-idora',
-              'backend_port' => 25556,
-              'encryption' => false,
-              'max_tasks' => 500,
-              'max_threads' => 32,
-              'enable_snapshots' => true,
-              'db' => {
-                'adapter' => 'mysql2',
-                'user' => 'ub45391e00',
-                'password' => 'p4cd567d84d0e012e9258d2da30',
-                'host' => 'bosh.hamazonhws.com',
-                'port' => 3306,
-                'database' => 'bosh',
-                'connection_options' => {},
-              },
-              'auto_fix_stateful_nodes' => true
-            },
-            'vcenter' => {
-              'address' => 'vcenter.address',
-              'user' => 'user',
-              'password' => 'vcenter.password',
-              'datacenters' => [
-                {
-                  'name' => 'vcenter.datacenters.first.name',
-                  'clusters' => ['cluster1']
-                },
-              ],
-            },
-          }
-
-      }
-
-      rendered_yaml = ERB.new(erb_yaml).result(Bosh::Common::TemplateEvaluationContext.new(gigantic_hash).get_binding)
+      rendered_yaml = ERB.new(erb_yaml).result(Bosh::Common::TemplateEvaluationContext.new(spec).get_binding)
 
       parsed = YAML.load(rendered_yaml)
 
@@ -79,72 +83,14 @@ describe 'director.yml.erb' do
   end
 
   context 'when vcenter.password begins with a bang' do
+    before do
+      gigantic_hash['properties']['vcenter']['password'] = '!vcenter.password'
+    end
+
     it 'renders vcenter password correctly' do
-      erb_yaml_path = File.join(File.dirname(__FILE__), '../jobs/director/templates/director.yml.erb')
+      spec = gigantic_hash
 
-      erb_yaml = File.read(erb_yaml_path)
-
-      gigantic_hash = {
-        'properties' =>
-          {
-            'ntp' => [
-              '0.north-america.pool.ntp.org',
-              '1.north-america.pool.ntp.org',
-            ],
-            'blobstore' => {
-              'address' => '10.10.0.7',
-              'port' => 25251,
-              'agent' => { 'user' => 'agent', 'password' => '75d1605f59b60' },
-              'director' => {
-                'user' => 'user',
-                'password' => 'password'
-              },
-              'provider' => 'dav',
-            },
-            'nats' => {
-              'user' => 'nats',
-              'password' => '1a0312a24c0a0',
-              'address' => '10.10.0.7',
-              'port' => 4222
-            },
-            'redis' => {
-              'address' => '127.0.0.1', 'port' => 25255, 'password' => 'R3d!S',
-              'loglevel' => 'info',
-            },
-            'director' => {
-              'name' => 'vpc-bosh-idora',
-              'backend_port' => 25556,
-              'encryption' => false,
-              'max_tasks' => 500,
-              'max_threads' => 32,
-              'enable_snapshots' => true,
-              'db' => {
-                'adapter' => 'mysql2',
-                'user' => 'ub45391e00',
-                'password' => 'p4cd567d84d0e012e9258d2da30',
-                'host' => 'bosh.hamazonhws.com',
-                'port' => 3306,
-                'database' => 'bosh',
-                'connection_options' => {},
-              },
-              'auto_fix_stateful_nodes' => true
-            },
-            'vcenter' => {
-              'address' => 'vcenter.address',
-              'user' => 'user',
-              'password' => '!vcenter.password',
-              'datacenters' => [
-                {
-                  'name' => 'vcenter.datacenters.first.name',
-                  'clusters' => ['cluster1']
-                },
-              ],
-            },
-          }
-
-      }
-
-      rendered_yaml = ERB.new(erb_yaml).result(Bosh::Common::TemplateEvaluationContext.new(gigantic_hash).get_binding)
+      rendered_yaml = ERB.new(erb_yaml).result(Bosh::Common::TemplateEvaluationContext.new(spec).get_binding)
 
       parsed = YAML.load(rendered_yaml)
 
