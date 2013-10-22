fcfaf2569263d72a92b5c8c789af0ecf3f411239
dmitriy kalinin <dmitriy@pivotallabs.com>
2013-10-22 16:53:13 -0700

    Remove 'min_cli_version' from bosh_cli
    [#57847212]
    
    Signed-off-by: Tony Hansmann <thansmann@pivotallabs.com>

--- bosh_cli/spec/unit/release_spec.rb
+++ bosh_cli/spec/unit/release_spec.rb
@@ -15,19 +15,16 @@ describe Bosh::Cli::Release do
 
     r.dev_name.should be_nil
     r.final_name.should be_nil
-    r.min_cli_version.should be_nil
     r.latest_release_filename.should be_nil
 
     r.dev_name = "dev-release"
     r.final_name = "prod-release"
-    r.min_cli_version = "0.5.2"
     r.latest_release_filename = "foobar"
     r.save_config
 
     r2 = new_release(@release_dir)
     r.dev_name.should == "dev-release"
     r.final_name.should == "prod-release"
-    r.min_cli_version.should == "0.5.2"
     r.latest_release_filename.should == "foobar"
   end
 
@@ -35,7 +32,6 @@ describe Bosh::Cli::Release do
     r = new_release(@release_dir)
     r.dev_name = "dev-release"
     r.final_name = "prod-release"
-    r.min_cli_version = "0.5.2"
     r.save_config
 
     FileUtils.rm_rf(File.join(@release_dir, "config", "dev.yml"))
@@ -43,14 +39,12 @@ describe Bosh::Cli::Release do
     r = new_release(@release_dir)
     r.dev_name.should be_nil
     r.final_name.should == "prod-release"
-    r.min_cli_version.should == "0.5.2"
   end
 
   it "has attributes persisted in public release config" do
     r = new_release(@release_dir)
     r.dev_name = "dev-release"
     r.final_name = "prod-release"
-    r.min_cli_version = "0.5.2"
     r.save_config
 
     FileUtils.rm_rf(File.join(@release_dir, "config", "final.yml"))
@@ -58,7 +52,6 @@ describe Bosh::Cli::Release do
     r = new_release(@release_dir)
     r.dev_name.should == "dev-release"
     r.final_name.should be_nil
-    r.min_cli_version.should be_nil
   end
 
   describe "merging final.yml with private.yml" do
