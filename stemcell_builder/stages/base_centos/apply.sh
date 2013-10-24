10c4369b33f93d2627753ee2e4e63a62e8a64565
Jeffrey Peckham <peckham@pivotallabs.com>
2013-10-24 11:25:54 -0700

    Attempt to fix invalid rpmdb issues
    - Initial the rpmdb with proper option '--initdb'
    - Rebuild rpmdb after reinstalling release rpms inside chroot
    
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- stemcell_builder/stages/base_centos/apply.sh
+++ stemcell_builder/stages/base_centos/apply.sh
@@ -6,7 +6,7 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
 mkdir -p $chroot/var/lib/rpm
-rpm --root $chroot --rebuilddb
+rpm --root $chroot --initdb
 rpm --root $chroot --force --nodeps --install http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-4.el6.centos.10.x86_64.rpm
 
 cp /etc/resolv.conf $chroot/etc/resolv.conf
@@ -22,6 +22,7 @@ INSTALL_YUM
 run_in_chroot $chroot "
 rpm --force --nodeps --install http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-4.el6.centos.10.x86_64.rpm
 rpm --force --nodeps --install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm --rebuilddb
 yum --assumeyes groupinstall Base
 yum --assumeyes groupinstall 'Development Tools'
 "
