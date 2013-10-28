cc9c00fde9bd4bfc94306fe5291d32fed233e653
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-10-28 14:25:22 -0700

    Force using `send_file` and the exporter in controller spec
    Signed-off-by: Jesse Zhang <jz@pivotallabs.com>

--- bosh-director/spec/unit/api/controllers/compiled_packages_controller_spec.rb
+++ bosh-director/spec/unit/api/controllers/compiled_packages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'rack/test'
 require 'bosh/director/api/controllers/compiled_packages_controller'
+require 'tempfile'
 
 module Bosh::Director
   describe Api::Controllers::CompiledPackagesController do
@@ -35,17 +36,33 @@ module Bosh::Director
       context 'authenticated access' do
         before do
           authorize 'admin', 'admin'
+
           Models::Stemcell.make(name: 'bosh-stemcell', version: '123')
           release = Models::Release.make(name: 'cf-release')
           Models::ReleaseVersion.make(release: release, version: '456')
         end
 
         context 'when the specified stemcell and release exist' do
+          let(:package_group) { instance_double('Bosh::Director::CompiledPackageGroup') }
+          let(:fake_tgz_file) do
+            Tempfile.new('fake_tgz').tap do |f|
+              f.write('fake tgz content')
+              f.close
+            end
+          end
+          let(:exporter) { instance_double('Bosh::Director::CompiledPackagesExporter', tgz_path: fake_tgz_file.path) }
+
+          before do
+            CompiledPackageGroup.stub(:new).and_return(package_group)
+            CompiledPackagesExporter.stub(:new).and_return(exporter)
+          end
+
           it 'returns a tarball' do
             get '/stemcells/bosh-stemcell/123/releases/cf-release/456/compiled_packages'
 
             expect(last_response.status).to eq(200)
             expect(last_response.content_type).to eq('application/x-compressed')
+            expect(last_response.body).to eq('fake tgz content')
           end
         end
 
