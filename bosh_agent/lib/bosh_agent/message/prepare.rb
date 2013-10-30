43baf91c1f8b315528c002405b0442efe47aeca4
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-10-30 16:26:32 -0700

    Add new class to prepare for apply message
    Signed-off-by: Vipul Savjani <savjaniv@me.com>

---
+++ bosh_agent/lib/bosh_agent/message/prepare.rb
@@ -0,0 +1,86 @@
+module Bosh::Agent::Message
+  class Prepare < Base
+    def initialize(args)
+      @platform = Bosh::Agent::Config.platform
+
+      if args.size < 1
+        raise ArgumentError, "not enough arguments"
+      end
+
+      @new_spec = args.first
+      unless @new_spec.is_a?(Hash)
+        raise ArgumentError, "invalid spec, Hash expected, " +
+          "#{@new_spec.class} given"
+      end
+    end
+
+    def prepare
+      initialize_plans
+
+      %w(bosh jobs packages monit).each do |dir|
+        FileUtils.mkdir_p(File.join(base_dir, dir))
+      end
+
+      logger.info("Preparing: #{@new_spec.inspect}")
+
+      if @new_plan.configured?
+        begin
+          apply_job
+          apply_packages
+          log_bit_download_with_agent_state
+        rescue Exception => e
+          raise Bosh::Agent::MessageHandlerError,
+                "#{e.message}: #{e.backtrace}"
+        end
+      end
+    end
+
+    private
+
+    def initialize_plans
+      initialize_networks
+
+      @old_spec = Bosh::Agent::Config.state.to_hash
+
+      @old_plan = Bosh::Agent::ApplyPlan::Plan.new(@old_spec)
+      @new_plan = Bosh::Agent::ApplyPlan::Plan.new(@new_spec)
+    end
+
+    def initialize_networks
+      if @new_spec["networks"]
+        @new_spec["networks"].each do |network, properties|
+          infrastructure = Bosh::Agent::Config.infrastructure
+          network_settings =
+            infrastructure.get_network_settings(network, properties)
+          logger.debug("current network settings from VM: #{network_settings.inspect}")
+          logger.debug("new network settings to be applied: #{properties.inspect}")
+          if network_settings
+            @new_spec["networks"][network].merge!(network_settings)
+            logger.debug("merged network settings: #{@new_spec["networks"].inspect}")
+          end
+        end
+      end
+    end
+
+    def apply_job
+      if @new_plan.has_jobs?
+        @new_plan.install_jobs
+      else
+        logger.info("No job")
+      end
+    end
+
+    def apply_packages
+      if @new_plan.has_packages?
+        @new_plan.install_packages
+      else
+        logger.info("No packages")
+      end
+    end
+
+    def log_bit_download_with_agent_state
+      @old_spec['has_downloaded_new_bits'] = true
+      Bosh::Agent::Config.state.write(@old_spec)
+    end
+  end
+end
---
+++ bosh_agent/spec/unit/message/prepare_spec.rb
@@ -0,0 +1,110 @@
+require 'spec_helper'
+require 'bosh_agent/message/prepare'
+require 'fakefs/spec_helpers'
+
+module Bosh::Agent::Message
+  describe Prepare do
+    include FakeFS::SpecHelpers
+
+    let(:dummy_platform) { instance_double('Bosh::Agent::Platform::Linux::Adapter', update_logging: nil) }
+    let(:prepare_spec) { { prepare: 'doit' } }
+
+    subject(:prepare_message) { Prepare.new([prepare_spec]) }
+
+    before do
+      Bosh::Agent::Config.state = Bosh::Agent::State.new('state_file')
+
+      Bosh::Agent::Config.blobstore_provider = 'simple'
+      Bosh::Agent::Config.blobstore_options = {}
+      Bosh::Agent::Config.stub(platform: dummy_platform)
+      Bosh::Agent::Config.platform_name = 'dummy'
+
+      # FIXME: use Dummy platform for tests
+      system_root = Bosh::Agent::Config.system_root
+      FileUtils.mkdir_p(File.join(system_root, 'etc', 'logrotate.d'))
+
+      @httpclient = double('httpclient')
+      HTTPClient.stub(:new).and_return(@httpclient)
+    end
+
+    context 'when an empty list is passed to the initializer' do
+      it 'raises with argument error' do
+        expect { Prepare.new([]) }.to raise_error(ArgumentError, 'not enough arguments')
+      end
+    end
+
+    context 'when non hash is passed as prepare data to initializer' do
+      it 'raises with helpful error' do
+        expect { Prepare.new(['foo']) }.to raise_error(ArgumentError, 'invalid spec, Hash expected, String given')
+      end
+    end
+
+    describe '#prepare' do
+      it 'creates folders for bosh, jobs, packages, and monit' do
+        %w(bosh jobs packages monit).each do |directory|
+          File.exists?(File.join(base_dir, directory)).should be(false)
+        end
+
+        prepare_message.prepare
+
+        %w(bosh jobs packages monit).each do |directory|
+          File.exists?(File.join(base_dir, directory)).should be(true)
+        end
+      end
+
+      context 'prepare spec is configured' do
+        let(:prepare_spec) { { 'fake' => 'prepare_spec' } }
+        let(:job) { double('job') }
+        let(:package) { double('package') }
+        let(:agent_state) { double('agent state', to_hash: {'fake' => 'old_spec'}, write: nil) }
+        let(:old_plan) { double('old plan') }
+        let(:new_plan) { double('new plan', configured?: true, has_jobs?: true, has_packages?: true) }
+
+        before do
+          Bosh::Agent::Config.stub(:state).and_return(agent_state)
+          Bosh::Agent::ApplyPlan::Plan.stub(:new).and_return(old_plan)
+          Bosh::Agent::ApplyPlan::Plan.stub(:new).with(prepare_spec).and_return(new_plan)
+        end
+
+        it 'installs jobs and packages for prepare spec' do
+          new_plan.should_receive(:install_jobs)
+          new_plan.should_receive(:install_packages)
+
+          prepare_message.prepare
+        end
+
+        it 'rescues exceptions and raises a MessageHandlerError' do
+          expect { prepare_message.prepare }.to raise_error(Bosh::Agent::MessageHandlerError)
+        end
+
+        it 'adds `has_downloaded_new_bits` key to agent config state and sets to true' do
+          new_plan.stub(:install_jobs)
+          new_plan.stub(:install_packages)
+          agent_state.should_receive(:write).with({ 'fake' => 'old_spec', 'has_downloaded_new_bits' => true })
+
+          prepare_message.prepare
+        end
+      end
+
+      context 'prepare spec is not configured' do
+        let(:prepare_spec) { { 'fake' => 'prepare_spec' } }
+        let(:job) { double('job') }
+        let(:package) { double('package') }
+        let(:old_plan) { double('old plan') }
+        let(:new_plan) { double('new plan', configured?: false, has_jobs?: true, has_packages?: true) }
+
+        before do
+          Bosh::Agent::ApplyPlan::Plan.stub(:new).and_return(old_plan)
+          Bosh::Agent::ApplyPlan::Plan.stub(:new).with(prepare_spec).and_return(new_plan)
+        end
+
+        it 'does not install jobs or packages' do
+          new_plan.should_not_receive(:install_jobs)
+          new_plan.should_not_receive(:install_packages)
+
+          prepare_message.prepare
+        end
+      end
+    end
+  end
+end
