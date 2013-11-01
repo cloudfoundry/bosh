e17144de41a61e7759a05ca39ef534e7252e17cd
Matthew Boedicker <mboedicker@pivotallabs.com>
2013-11-01 10:25:43 -0700

    break up compiled packages controller into smaller methods
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-director/lib/bosh/director/api/controllers/compiled_packages_controller.rb
+++ bosh-director/lib/bosh/director/api/controllers/compiled_packages_controller.rb
@@ -9,14 +9,8 @@ module Bosh::Director
   module Api::Controllers
     class CompiledPackagesController < BaseController
       post '/compiled_package_groups/export', consumes: [:json] do
-        stemcell_manager = Api::StemcellManager.new
-        stemcell = stemcell_manager.find_by_name_and_version(
-          body_params['stemcell_name'], body_params['stemcell_version'])
-
-        release_manager = Api::ReleaseManager.new
-        release = release_manager.find_by_name(body_params['release_name'])
-        release_version = release_manager.find_version(
-          release, body_params['release_version'])
+        stemcell = find_stemcell_by_name_and_version
+        release_version = find_release_version_by_name_and_version
 
         output_dir = File.join(Dir.tmpdir, 'compiled_packages')
         FileUtils.mkdir_p(output_dir)
@@ -31,7 +25,19 @@ module Bosh::Director
         output_path = File.join(output_dir, "compiled_packages_#{Time.now.to_f}.tar.gz")
         exporter.export(output_path)
 
-        send_file output_path, type: :tgz
+        send_file(output_path, type: :tgz)
+      end
+
+      def find_stemcell_by_name_and_version
+        stemcell_manager = Api::StemcellManager.new
+        stemcell_manager.find_by_name_and_version(
+          body_params['stemcell_name'], body_params['stemcell_version'])
+      end
+
+      def find_release_version_by_name_and_version
+        release_manager = Api::ReleaseManager.new
+        release = release_manager.find_by_name(body_params['release_name'])
+        release_manager.find_version(release, body_params['release_version'])
       end
 
       def body_params
