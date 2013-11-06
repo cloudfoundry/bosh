122e1fc6250344a71cb0b414c93641e7dc43e540
Abhijit Hiremagalur <abhi@pivotallabs.com>
2013-11-05 16:12:25 -0800

    Prefer single quotes for uninterpolated strings
    Signed-off-by: Jesse Zhang <jz@pivotallabs.com>

--- bosh-director/lib/bosh/director/agent_client.rb
+++ bosh-director/lib/bosh/director/agent_client.rb
@@ -1,24 +1,22 @@
-# Copyright (c) 2009-2012 VMware, Inc.
-
 module Bosh::Director
   class AgentClient < Client
 
     DEFAULT_POLL_INTERVAL = 1.0
 
     def initialize(id, options = {})
-      # Retry 'get_state' and 'get_task' in case of timeout errors
       defaults = {
-        :retry_methods => { :get_state => 2, :get_task => 2}
+        retry_methods: { # in case of timeout errors
+                         get_state: 2,
+                         get_task: 2,
+        }
       }
 
       credentials = Bosh::Director::Models::Vm.find(:agent_id => id).credentials
       if credentials
-        defaults.merge!({
-          :credentials => credentials
-        })
+        defaults.merge!(credentials: credentials)
       end
 
-      super("agent", id, defaults.merge(options))
+      super('agent', id, defaults.merge(options))
     end
 
     # This converts the old agent response format to the new.  This can be taken
@@ -33,24 +31,24 @@ module Bosh::Director
     # @return [Hash] The message in the new message format.
     def self.convert_old_message_to_new(msg)
       if msg && msg.is_a?(Hash)
-        if !msg.has_key?("value")
+        if !msg.has_key?('value')
           # There's no value but there is state and agent_task_id.
           # Just leave it.
-          if msg.has_key?("state") && msg.has_key?("agent_task_id")
+          if msg.has_key?('state') && msg.has_key?('agent_task_id')
             return msg
           end
           # There's no value and either one or both of state/agent_task_id don't
           # exist.  Assume that the whole message response was meant to be the
           # value.
-          return {"value" => msg,
-                  "state" => msg["state"] || "done",
-                  "agent_task_id" => msg["agent_task_id"] || nil}
-        elsif !msg.has_key?("state")
+          return { 'value' => msg,
+                   'state' => msg['state'] || 'done',
+                   'agent_task_id' => msg['agent_task_id'] || nil }
+        elsif !msg.has_key?('state')
           # The message has a value section but no "state".  Mark state as
           # done.
-          return {"value" => msg["value"],
-                  "state" => "done",
-                  "agent_task_id" => msg["agent_task_id"] || nil}
+          return { 'value' => msg['value'],
+                   'state' => 'done',
+                   'agent_task_id' => msg['agent_task_id'] || nil }
         else
           # The message was good from the start!
           return msg
@@ -58,7 +56,7 @@ module Bosh::Director
       end
       # If the message was anything other than a hash (float, int, string,
       # array, etc.) then we want to just make that be the "value".
-      {"state" => "done", "value" => msg, "agent_task_id" => nil}
+      { 'state' => 'done', 'value' => msg, 'agent_task_id' => nil }
     end
 
     [
@@ -74,11 +72,11 @@ module Bosh::Director
     ].each do |method|
       define_method (method) do |*args|
         task = AgentClient.convert_old_message_to_new(super(*args))
-        while task["state"] == "running"
+        while task['state'] == 'running'
           sleep(DEFAULT_POLL_INTERVAL)
-          task = AgentClient.convert_old_message_to_new(get_task(task["agent_task_id"]))
+          task = AgentClient.convert_old_message_to_new(get_task(task['agent_task_id']))
         end
-        task["value"]
+        task['value']
       end
     end
   end
