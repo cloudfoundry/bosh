9923adeecdd6ad72f7f0832f0ffa882804130ebb
Abhijit Hiremagalur <abhi@pivotallabs.com>
2013-11-14 15:45:25 -0800

    Wire in new, tested, public stemcells presenter and index
    Signed-off-by: Tony Hansmann <thansmann@pivotallabs.com>

--- bosh_cli/spec/unit/commands/stemcell_spec.rb
+++ bosh_cli/spec/unit/commands/stemcell_spec.rb
@@ -1,67 +1,128 @@
-# Copyright (c) 2009-2013 GoPivotal, Inc.
-
 require 'spec_helper'
 
-describe Bosh::Cli::Command::Stemcell do
-  let(:command) { described_class.new }
-  let(:director) { double(Bosh::Cli::Client::Director) }
-  let(:stemcell_archive) { spec_asset("valid_stemcell.tgz") }
-  let(:stemcell_manifest) { {'name' => 'ubuntu-stemcell', 'version' => 1} }
-  let(:stemcell) { double('stemcell', :manifest => stemcell_manifest) }
+module Bosh::Cli
+  describe Command::Stemcell do
+    let(:director) { double(Bosh::Cli::Client::Director) }
+    let(:stemcell_archive) { spec_asset("valid_stemcell.tgz") }
+    let(:stemcell_manifest) { {'name' => 'ubuntu-stemcell', 'version' => 1} }
+    let(:stemcell) { double('stemcell', :manifest => stemcell_manifest) }
 
-  before do
-    command.stub(:director).and_return(director)
-    Bosh::Cli::Stemcell.stub(:new).and_return(stemcell)
-  end
+    subject(:command) do
+      Bosh::Cli::Command::Stemcell.new
+    end
 
-  describe 'upload stemcell' do
-    it_behaves_like 'a command which requires user is logged in', ->(command) { command.upload('http://stemcell_location') }
+    before do
+      command.stub(:director).and_return(director)
+      Bosh::Cli::Stemcell.stub(:new).and_return(stemcell)
+    end
 
-    context 'when the user is logged in' do
+    describe 'upload stemcell' do
+      it_behaves_like 'a command which requires user is logged in', ->(command) { command.upload('http://stemcell_location') }
+
+      context 'when the user is logged in' do
+        before do
+          command.stub(:logged_in? => true)
+          command.options[:target] = 'http://bosh-target.example.com'
+        end
+
+        context 'local stemcell' do
+          it 'should upload the stemcell' do
+            stemcell.should_receive(:validate)
+            stemcell.should_receive(:valid?).and_return(true)
+            director.should_receive(:list_stemcells).and_return([])
+            stemcell.should_receive(:stemcell_file).and_return(stemcell_archive)
+            director.should_receive(:upload_stemcell).with(stemcell_archive)
+
+            command.upload(stemcell_archive)
+          end
+
+          it 'should not upload the stemcell if is invalid' do
+            stemcell.should_receive(:validate)
+            stemcell.should_receive(:valid?).and_return(false)
+            director.should_not_receive(:upload_stemcell)
+
+            expect {
+              command.upload(stemcell_archive)
+            }.to raise_error(Bosh::Cli::CliError, /Stemcell is invalid/)
+          end
+
+          it 'should not upload the stemcell if already exist' do
+            stemcell.should_receive(:validate)
+            stemcell.should_receive(:valid?).and_return(true)
+            director.should_receive(:list_stemcells).and_return([stemcell_manifest])
+            director.should_not_receive(:upload_stemcell)
+
+            expect {
+              command.upload(stemcell_archive)
+            }.to raise_error(Bosh::Cli::CliError, /already exists/)
+          end
+        end
+
+        context 'remote stemcell' do
+          it 'should upload the stemcell' do
+            director.should_receive(:upload_remote_stemcell).with('http://stemcell_location')
+
+            command.upload('http://stemcell_location')
+          end
+        end
+      end
+    end
+
+    describe 'public stemcells' do
+      let(:public_stemcell_presenter) do
+        instance_double('Bosh::Cli::PublicStemcellPresenter', list: nil)
+      end
+
+      let(:public_stemcell_index) do
+        instance_double('Bosh::Cli::PublicStemcellIndex')
+      end
+
       before do
-        command.stub(:logged_in? => true)
-        command.options[:target] = 'http://bosh-target.example.com'
+        PublicStemcellIndex.stub(:download).and_return(public_stemcell_index)
+        PublicStemcellPresenter.stub(:new).and_return(public_stemcell_presenter)
       end
 
-      context 'local stemcell' do
-        it 'should upload the stemcell' do
-          stemcell.should_receive(:validate)
-          stemcell.should_receive(:valid?).and_return(true)
-          director.should_receive(:list_stemcells).and_return([])
-          stemcell.should_receive(:stemcell_file).and_return(stemcell_archive)
-          director.should_receive(:upload_stemcell).with(stemcell_archive)
+      it 'lists public stemcells in the index' do
+        command.options = double('options')
 
-          command.upload(stemcell_archive)
-        end
+        command.list_public
 
-        it 'should not upload the stemcell if is invalid' do
-          stemcell.should_receive(:validate)
-          stemcell.should_receive(:valid?).and_return(false)
-          director.should_not_receive(:upload_stemcell)
-
-          expect {
-            command.upload(stemcell_archive)
-          }.to raise_error(Bosh::Cli::CliError, /Stemcell is invalid/)
-        end
-
-        it 'should not upload the stemcell if already exist' do
-          stemcell.should_receive(:validate)
-          stemcell.should_receive(:valid?).and_return(true)
-          director.should_receive(:list_stemcells).and_return([stemcell_manifest])
-          director.should_not_receive(:upload_stemcell)
-
-          expect {
-            command.upload(stemcell_archive)
-          }.to raise_error(Bosh::Cli::CliError, /already exists/)
-        end
+        expect(public_stemcell_presenter).to have_received(:list).with(command.options)
       end
 
-      context 'remote stemcell' do
-        it 'should upload the stemcell' do
-          director.should_receive(:upload_remote_stemcell).with('http://stemcell_location')
+      it 'properly wires a stemcell index with a presenter' do
+        command.list_public
 
-          command.upload('http://stemcell_location')
-        end
+        expect(PublicStemcellIndex).to have_received(:download).with(command)
+        expect(PublicStemcellPresenter).to have_received(:new).with(command, public_stemcell_index)
+      end
+    end
+
+    describe 'download public stemcell' do
+      let(:public_stemcell_presenter) do
+        instance_double('Bosh::Cli::PublicStemcellPresenter', download: nil)
+      end
+
+      let(:public_stemcell_index) do
+        instance_double('Bosh::Cli::PublicStemcellIndex')
+      end
+
+      before do
+        PublicStemcellIndex.stub(:download).and_return(public_stemcell_index)
+        PublicStemcellPresenter.stub(:new).and_return(public_stemcell_presenter)
+      end
+
+      it 'lists public stemcells in the index' do
+        command.download_public('stemcell.tgz')
+
+        expect(public_stemcell_presenter).to have_received(:download).with('stemcell.tgz')
+      end
+
+      it 'properly wires a stemcell index with a presenter' do
+        command.download_public('stemcell.tgz')
+
+        expect(PublicStemcellIndex).to have_received(:download).with(command)
+        expect(PublicStemcellPresenter).to have_received(:new).with(command, public_stemcell_index)
       end
     end
   end
