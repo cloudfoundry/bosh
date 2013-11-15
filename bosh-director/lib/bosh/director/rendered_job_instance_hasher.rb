f8b0d9060d8db3a0194706c24dc8335d54427758
Jeffrey Peckham <peckham@pivotallabs.com>
2013-11-15 11:44:18 -0800

    separate computation of configuration_hash and template_hashes
    Signed-off-by: Jesse Zhang <jz@pivotallabs.com>

--- bosh-director/lib/bosh/director/rendered_job_instance_hasher.rb
+++ bosh-director/lib/bosh/director/rendered_job_instance_hasher.rb
@@ -5,22 +5,8 @@ module Bosh::Director
     end
 
     def configuration_hash
-      instance_digest, _ = render_digest
-      instance_digest
-    end
-
-    def template_hashes
-      _, template_digests = render_digest
-      template_digests
-    end
-
-    private
-    attr_reader :job_templates
-
-    def render_digest
       instance_digest = Digest::SHA1.new
-      template_digests = {}
-      job_templates.sort { |x, y| x.name <=> y.name }.each do |rendered_job_template|
+      sorted_jobs.each do |rendered_job_template|
         bound_templates = ''
         bound_templates << rendered_job_template.monit
 
@@ -28,12 +14,31 @@ module Bosh::Director
           bound_templates << rendered_job_template.templates[src_name]
           instance_digest << bound_templates
         end
-
-        template_digest = Digest::SHA1.new
-        template_digest << bound_templates
-        template_digests[rendered_job_template.name] = template_digest.hexdigest
       end
-      return instance_digest.hexdigest, template_digests
+
+      instance_digest.hexdigest
+    end
+
+    def template_hashes
+      sorted_jobs.reduce({}) do |h, rendered_job_template|
+        h.merge(rendered_job_template.name => template_hash(rendered_job_template))
+      end
+    end
+
+    private
+    attr_reader :job_templates
+
+    def sorted_jobs
+      job_templates.sort { |x, y| x.name <=> y.name }
+    end
+
+    def template_hash(job_template)
+      template_digest = Digest::SHA1.new
+      template_digest << job_template.monit
+      job_template.templates.keys.sort.each do |src_name|
+        template_digest << job_template.templates[src_name]
+      end
+      template_digest.hexdigest
     end
   end
 end
