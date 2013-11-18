6aceba3a1210ef0ee4d3f9cd971613fe56ab3a5b
Brian Cunnie <cunnie@pivotallabs.com>
2013-11-18 15:25:03 -0800

    removed unncessary module redefinition
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-dev/lib/bosh/dev/aws/deployment_account.rb
+++ bosh-dev/lib/bosh/dev/aws/deployment_account.rb
@@ -1,38 +1,36 @@
 require 'bosh/core/shell'
 
-module Bosh::Dev
-  module Aws
-    class DeploymentAccount
-      def initialize(deployment_name, deployments_repository)
-        @deployment_name = deployment_name
-        @deployments_repository = deployments_repository
-        @shell = Bosh::Core::Shell.new
-        deployments_repository.clone_or_update!
-      end
+module Bosh::Dev::Aws
+  class DeploymentAccount
+    def initialize(deployment_name, deployments_repository)
+      @deployment_name = deployment_name
+      @deployments_repository = deployments_repository
+      @shell = Bosh::Core::Shell.new
+      deployments_repository.clone_or_update!
+    end
 
-      def manifest_path
-        @manifest_path ||= File.join(deployments_repository.path, deployment_name, 'deployments/bosh/bosh.yml')
-      end
+    def manifest_path
+      @manifest_path ||= File.join(deployments_repository.path, deployment_name, 'deployments/bosh/bosh.yml')
+    end
 
-      def bosh_user
-        @bosh_user ||= shell.run(". #{deployment_bosh_environment_path} && echo $BOSH_USER").chomp
-      end
+    def bosh_user
+      @bosh_user ||= shell.run(". #{deployment_bosh_environment_path} && echo $BOSH_USER").chomp
+    end
 
-      def bosh_password
-        @bosh_password ||= shell.run(". #{deployment_bosh_environment_path} && echo $BOSH_PASSWORD").chomp
-      end
+    def bosh_password
+      @bosh_password ||= shell.run(". #{deployment_bosh_environment_path} && echo $BOSH_PASSWORD").chomp
+    end
 
-      def run_with_env(command)
-        shell.run(". #{deployment_bosh_environment_path} && #{command}")
-      end
+    def run_with_env(command)
+      shell.run(". #{deployment_bosh_environment_path} && #{command}")
+    end
 
-      private
+    private
 
-      attr_reader :deployment_name, :shell, :deployments_repository
+    attr_reader :deployment_name, :shell, :deployments_repository
 
-      def deployment_bosh_environment_path
-        File.join(deployments_repository.path, deployment_name, 'bosh_environment')
-      end
+    def deployment_bosh_environment_path
+      File.join(deployments_repository.path, deployment_name, 'bosh_environment')
     end
   end
 end
