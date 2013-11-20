c2fb7a1c3155b185e38296732c7ab477cf0863f3
Jeffrey Peckham <peckham@pivotallabs.com>
2013-11-19 17:38:46 -0800

    wire in CompressedRenderedJobTemplates
    Temporarily uploads a fake archive to the null blobstore
    
    [#60724646]
    
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

---
+++ bosh-director/lib/bosh/director/compressed_rendered_job_templates.rb
@@ -0,0 +1,10 @@
+module Bosh::Director
+  class CompressedRenderedJobTemplates
+    def initialize(rendered_job_templates)
+    end
+
+    def contents
+      'contents of fake-compressed-job-templates.tgz'
+    end
+  end
+end
--- bosh-director/lib/bosh/director/rendered_templates_uploader.rb
+++ bosh-director/lib/bosh/director/rendered_templates_uploader.rb
@@ -1,8 +1,16 @@
 require 'bosh/director/rendered_templates_writer'
+require 'bosh/director/compressed_rendered_job_templates'
+require 'blobstore_client/null_blobstore_client'
 
 module Bosh::Director
   class RenderedTemplatesUploader
-    def upload(rendered_templates)
+    def initialize(blobstore = Bosh::Blobstore::NullBlobstoreClient.new)
+      @blobstore = blobstore
+    end
+
+    def upload(rendered_job_templates)
+      compressed_archive = CompressedRenderedJobTemplates.new(rendered_job_templates)
+      @blobstore.create(compressed_archive.contents)
     end
   end
 end
