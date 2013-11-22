b1e485cd658ff5677bbe3d6107e458daf3c2b881
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-11-21 17:34:41 -0800

    extracted Bosh::Dev::ReleaseCreator
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-dev/lib/bosh/dev/bosh_release.rb
+++ bosh-dev/lib/bosh/dev/bosh_release.rb
@@ -1,20 +1,35 @@
 require 'rake'
 
 module Bosh::Dev
-  class BoshRelease
-    def self.build
-      new(BoshCliSession.new)
-    end
-
+  class ReleaseCreator
     def initialize(cli_session)
       @cli_session = cli_session
     end
 
-    def tarball_path
+    def create(options)
+      is_final = !!options[:final]
+
       Dir.chdir('release') do
-        output = @cli_session.run_bosh('create release --force --with-tarball')
+        output = @cli_session.run_bosh("create release #{'--final ' if is_final}--with-tarball")
         output.scan(/Release tarball\s+\(.+\):\s+(.+)$/).first.first
       end
+    end
+  end
+
+  class BoshRelease
+    def self.build
+      bosh_cli_session = BoshCliSession.new
+      release_creator = ReleaseCreator.new(bosh_cli_session)
+      new(release_creator)
+    end
+
+    def initialize(release_creator)
+      @release_creator = release_creator
+    end
+
+    def tarball_path
+      @release_creator.create({})
+      @release_creator.create(final: true)
     end
   end
 end
--- bosh-dev/spec/bosh/dev/bosh_release_spec.rb
+++ bosh-dev/spec/bosh/dev/bosh_release_spec.rb
@@ -6,34 +6,65 @@ module Bosh::Dev
   describe BoshRelease do
     include FakeFS::SpecHelpers
 
+    subject { described_class.new(release_creator) }
+    let(:release_creator) { instance_double('Bosh::Dev::ReleaseCreator') }
+
+    describe '#tarball_path' do
+      it 'creates dev and then final release' do
+        release_creator.should_receive(:create).with({}).ordered.and_return('fake-dev-tarball-path')
+        release_creator.should_receive(:create).with(final: true).ordered.and_return('fake-final-tarball-path')
+        subject.tarball_path.should == 'fake-final-tarball-path'
+      end
+    end
+  end
+
+  describe ReleaseCreator do
+    include FakeFS::SpecHelpers
+
     subject { described_class.new(cli_session) }
     let(:cli_session) { instance_double('Bosh::Dev::BoshCliSession') }
 
-    describe '#tarball_path' do
+    describe '#create' do
       before { FileUtils.mkdir_p('release') }
+
+      let(:options) { {} }
 
       create_release_output = <<-OUTPUT
         ...
-        Release version: 3.1-dev
-        Release manifest: /tmp/project-release/dev_releases/dummy-3.1-dev.yml
-        Release tarball (2.4K): /tmp/project-release/dev_releases/dummy-3.1-dev.tgz
+        Release version: 3
+        Release manifest: /tmp/project-release/releases/dummy-3.yml
+        Release tarball (2.4K): /tmp/project-release/releases/dummy-3.tgz
       OUTPUT
 
-      it 'is inside release directory when creating dev release' do
-        cli_session.stub(:run_bosh) do
+      it 'is inside release directory when creating final release' do
+        cli_session.stub(:run_bosh).with(/create release/) do
           @current_dir = Dir.pwd
           create_release_output
         end
-        subject.tarball_path
+        subject.create(options)
         expect(@current_dir).to eq(File.expand_path('../../../../release', __FILE__))
       end
 
-      it 'creates a new release tarball' do
-        cli_session
-          .should_receive(:run_bosh)
-          .with('create release --force --with-tarball')
-          .and_return(create_release_output)
-        expect(subject.tarball_path).to eq('/tmp/project-release/dev_releases/dummy-3.1-dev.tgz')
+      context 'when final option is set' do
+        before { options.merge!(final: true) }
+
+        it 'creates a new final release tarball' do
+          cli_session
+            .should_receive(:run_bosh)
+            .with('create release --final --with-tarball')
+            .and_return(create_release_output)
+          expect(subject.create(options)).to eq('/tmp/project-release/releases/dummy-3.tgz')
+        end
+      end
+
+      context 'when final option is not set' do
+        it 'creates a new dev release tarball' do
+          cli_session
+            .should_receive(:run_bosh)
+            .with('create release --with-tarball')
+            .and_return(create_release_output)
+          expect(subject.create(options)).to eq('/tmp/project-release/releases/dummy-3.tgz')
+        end
       end
     end
   end
