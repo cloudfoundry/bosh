ebca4d2494c69470879df1250a66cf1b05329fdd
Brian Cunnie <cunnie@pivotallabs.com>
2013-11-26 10:04:56 -0800

    make NullBlobstoreClient behave similar to the rest of the blobstore clients
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- blobstore_client/spec/unit/null_blobstore_client_spec.rb
+++ blobstore_client/spec/unit/null_blobstore_client_spec.rb
@@ -3,18 +3,35 @@ require 'blobstore_client/null_blobstore_client'
 
 module Bosh::Blobstore
   describe NullBlobstoreClient do
-    subject(:client) do
-      NullBlobstoreClient.new
-    end
+    subject(:client) { described_class.new }
 
     it_implements_base_client_interface
 
     describe '#create' do
-      it 'does nothing' do
+      it 'always returns random uuid' do
+        id = subject.create('fake-contents')
+        expect(id).to be_an_instance_of(String)
+        expect(id.size).to be >= 1
+      end
+    end
+
+    describe '#get' do
+      it 'always raises NotFound error' do
         expect {
-          client.create('fake contents')
-          client.create('fake contents', 'fake id')
-        }.not_to raise_error
+          subject.get('fake-id')
+        }.to raise_error(NotFound, /fake-id/)
+      end
+    end
+
+    describe '#delete' do
+      it 'always returns nil' do
+        expect(subject.delete('fake-id')).to be_nil
+      end
+    end
+
+    describe '#exists?' do
+      it 'always returns false' do
+        expect(subject.exists?('fake-id')).to be(false)
       end
     end
   end
