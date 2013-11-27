6ab038d0dbedacd3a28758a074a92b68a181ebbd
Iwasaki <yudai@arielworks.com>
2013-11-27 19:40:17 +0900

    Fix test for delete_vm

--- bosh_cloudstack_cpi/spec/unit/delete_vm_spec.rb
+++ bosh_cloudstack_cpi/spec/unit/delete_vm_spec.rb
@@ -13,15 +13,14 @@ describe Bosh::CloudStackCloud::Cloud do
     @ephemeral_settings =
       {
         "disks" => {
-          "persistent"=>{},
-          "ephemeral"=>"/dev/sdb",
+          "persistent" => {},
+          "ephemeral" => "/dev/sdb",
         }
       }
-    @no_ephemeral_settings =
-      {
-        "disks" => {
-          "persistent"=>{},
-          "ephemeral"=>nil
+    @no_ephemeral_settings = {
+      "disks" => {
+        "persistent" => {},
+        "ephemeral" => nil,
         }
       }
   end
@@ -36,7 +35,7 @@ describe Bosh::CloudStackCloud::Cloud do
   it "deletes the CloudStack server without an ephemeral disk" do
     cloud = mock_cloud do |compute|
       compute.servers.should_receive(:get).with("i-no_ephemeral").and_return(@server)
-      compute.volumes.should_receive(:get).with(nil).and_return(nil)
+      compute.volumes.should_not_receive(:select)
     end
     @server.should_receive(:id).and_return("i-no_ephemeral")
     @server.should_receive(:name).exactly(2).and_return("i-no_ephemeral")
@@ -49,15 +48,21 @@ describe Bosh::CloudStackCloud::Cloud do
   end
 
   it "deletes the CloudStack server with the ephemeral disk" do
+    volume_tobe_deleted = double("sdb", :id => "ok", :device_id => 1)
     cloud = mock_cloud do |compute|
       compute.servers.should_receive(:get).with("i-ephemeral").and_return(@server)
-      compute.volumes.should_receive(:get).with("/dev/sdb").and_return(@volume)
+      compute.volumes.should_receive(:select).and_return([
+          double("sda", :id => "ng", :device_id => 0),
+          volume_tobe_deleted,
+          double("sdc", :id => "yetanotherng", :device_id => 2),
+        ])
     end
+
+    @registry.should_receive(:read_settings).with("i-ephemeral").and_return(@ephemeral_settings)
     @server.should_receive(:id).and_return("i-ephemeral")
     @server.should_receive(:name).exactly(2).and_return("i-ephemeral")
-    @volume.should_receive(:id).and_return("volume-ephemeral")
-    @registry.should_receive(:read_settings).with("i-ephemeral").and_return(@ephemeral_settings)
-    cloud.should_receive(:delete_disk).with("volume-ephemeral")
+    cloud.should_receive(:wait_resource).with(volume_tobe_deleted, :"", :server_id)
+    cloud.should_receive(:delete_disk).with("ok")
     @server.should_receive(:destroy).and_return(@job)
     cloud.should_receive(:wait_job).with(@job)
     @registry.should_receive(:delete_settings).with("i-ephemeral")
