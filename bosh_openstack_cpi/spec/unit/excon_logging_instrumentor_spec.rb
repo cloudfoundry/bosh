17910185e50376bcce948a26b63b8fda00c4b922
Ruben Koster <rkoster@starkandwayne.com>
2013-12-09 19:58:48 +0100

    Small refactoring + unit tests for excon instrumentor

---
+++ bosh_openstack_cpi/spec/unit/excon_logging_instrumentor_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe Bosh::OpenStackCloud::ExconLoggingInstrumentor do
+  describe "#redact_params" do
+    let(:secret) { "secret_string" }
+    subject(:redacted_params) {
+      Bosh::OpenStackCloud::ExconLoggingInstrumentor::RedactedParams.new(params)
+    }
+
+    context "when containing a password" do
+      let(:params) { {password: secret } }
+      it "redacts password" do
+        expect(redacted_params.to_s).to_not include secret
+      end
+    end
+
+    context "when containing a authorization header" do
+      let(:params) { {headers: { 'Authorization' => secret } } }
+      it "redacts authorization" do
+        expect(redacted_params.to_s).to_not include secret
+      end
+    end
+
+  end
+end
