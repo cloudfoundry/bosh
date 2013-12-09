06f9f885f7d5a814e4e2c8bbe3581bd07b3ebf82
Ruben Koster <rkoster@starkandwayne.com>
2013-12-09 19:58:48 +0100

    Small refactoring + unit tests for excon instrumentor

--- bosh_openstack_cpi/lib/cloud/openstack/excon_logging_instrumentor.rb
+++ bosh_openstack_cpi/lib/cloud/openstack/excon_logging_instrumentor.rb
@@ -1,19 +1,40 @@
 module Bosh::OpenStackCloud
   class ExconLoggingInstrumentor
-    REDACTED = "[REDACTED]"
     def self.instrument(name, params = {}, &block)
-      params = params.dup
-      if params.has_key?(:headers) && params[:headers].has_key?('Authorization')
-        params[:headers] = params[:headers].dup
-        params[:headers]['Authorization'] = REDACTED
-      end
-      if params.has_key?(:password)
-        params[:password] = REDACTED
-      end
-      Bosh::Clouds::Config.logger.debug("#{name}  #{params.inspect}")
+      params = RedactedParams.new(params)
+      Bosh::Clouds::Config.logger.debug("#{name}  #{params}")
       if block_given?
         yield
       end
     end
+
+    class RedactedParams
+      REDACTED = "[REDACTED]"
+
+      def initialize(params)
+        @params = params
+        redact_authorization_params
+        redact_password_params
+      end
+
+      def to_s
+        @params.inspect
+      end
+
+      private
+
+      def redact_authorization_params
+        if @params.has_key?(:headers) && @params[:headers].has_key?('Authorization')
+          @params[:headers]['Authorization'] = REDACTED
+        end
+      end
+
+      def redact_password_params
+        if @params.has_key?(:password)
+          @params[:password] = REDACTED
+        end
+      end
+    end
+
   end
 end
