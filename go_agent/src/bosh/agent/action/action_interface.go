728e0ba5cb73ffbf142ab8df726139dd0d56a386
Damien Le Berrigaud <damien@pivotallabs.com>
2013-12-11 08:21:08 -0800

    Added some documentation to action interface

--- go_agent/src/bosh/agent/action/action_interface.go
+++ go_agent/src/bosh/agent/action/action_interface.go
@@ -3,6 +3,15 @@ package action
 type Action interface {
 	IsAsynchronous() bool
 
-	// See Runner for run method signature details
+	// Action should implement Run
+	// Arguments should be the list of arguments the payload will include
+	// and necessary for running the action
+	//
+	// It should return:
+	//	* a value, used as the response value. It will be converted to JSON
+	//	* an error, used to return an error response instead
+	//
 	// Run(...) (value interface{}, err error)
+	//
+	// See Runner for more details
 }
