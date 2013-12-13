c03f5048c5fa13600c8158eb4c9f471b4ab8d6f8
Mike Stallard <mstallard@pivotallabs.com>
2013-12-13 15:59:43 -0800

    added ParallelMultiJobUpdater
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-director/lib/bosh/director/deployment_plan/multi_job_updater.rb
+++ bosh-director/lib/bosh/director/deployment_plan/multi_job_updater.rb
@@ -10,5 +10,20 @@ module Bosh::Director
         end
       end
     end
+
+    class ParallelMultiJobUpdater
+      def run(base_job, deployment_plan, jobs)
+        base_job.task_checkpoint
+        ThreadPool.new(max_threads: jobs.size).wrap do |pool|
+          jobs.each do |j|
+            pool.process do
+              base_job.logger.info("Updating job: #{j.name}")
+              job_updater = JobUpdater.new(deployment_plan, j)
+              job_updater.update
+            end
+          end
+        end
+      end
+    end
   end
 end
--- bosh-director/spec/unit/deployment_plan/multi_job_updater_spec.rb
+++ bosh-director/spec/unit/deployment_plan/multi_job_updater_spec.rb
@@ -40,3 +40,59 @@ describe Bosh::Director::DeploymentPlan::SerialMultiJobUpdater do
     end
   end
 end
+
+describe Bosh::Director::DeploymentPlan::ParallelMultiJobUpdater do
+  describe '#run' do
+    let(:base_job) { instance_double('Bosh::Director::Jobs::BaseJob', task_checkpoint: nil, logger: logger) }
+    let(:deployment_plan) { instance_double('Bosh::Director::Jobs::BaseJob') }
+    let(:job1) { instance_double('Bosh::Director::DeploymentPlan::Job', name: 'fake-job1-name') }
+    let(:job2) { instance_double('Bosh::Director::DeploymentPlan::Job', name: 'fake-job2-name') }
+
+    let(:thread_pool) { instance_double('Bosh::Director::ThreadPool') }
+    let(:logger) { Logger.new('/dev/null') }
+
+    before { allow(Bosh::Director::ThreadPool).to receive(:new).and_return(thread_pool) }
+
+    it 'marks a task checkpoint on the base job before running jobs in parallel' do
+      expect(base_job).to receive(:task_checkpoint).with(no_args).ordered
+      expect(thread_pool).to receive(:wrap).ordered
+      subject.run(base_job, deployment_plan, [])
+    end
+
+    it 'does not update jobs outside of the created thread pool' do
+      allow(Bosh::Director::ThreadPool).to receive(:new).with(max_threads: 2).and_return(thread_pool)
+      expect(thread_pool).to receive(:process).twice
+      expect(thread_pool).to receive(:wrap).and_yield(thread_pool)
+
+      # make sure that job updates did not happen outside of the thread pool #process
+      expect(Bosh::Director::JobUpdater).to_not receive(:new)
+
+      subject.run(base_job, deployment_plan, [job1, job2])
+    end
+
+    it 'enqueues all given jobs to run in parallel' do
+      enqueued = []
+      expect(thread_pool).to receive(:process).twice.and_return { |&blk| enqueued << blk }
+
+      expect(thread_pool).to receive(:wrap).and_return do |&blk|
+        blk.call(thread_pool)
+        # all jobs were enqueued by the time wrap block executes
+        expect(enqueued.size).to eq(2)
+      end
+
+      subject.run(base_job, deployment_plan, [job1, job2])
+
+      # first job was enqueued
+      job_updater1 = instance_double('Bosh::Director::JobUpdater', update: nil)
+      expect(Bosh::Director::JobUpdater).to receive(:new).with(deployment_plan, job1).and_return(job_updater1)
+      enqueued.first.call
+      expect(job_updater1).to have_received(:update).with(no_args)
+
+      # second job was enqueued
+      job_updater2 = instance_double('Bosh::Director::JobUpdater', update: nil)
+      expect(Bosh::Director::JobUpdater).to receive(:new).with(deployment_plan, job2).and_return(job_updater2)
+      enqueued.last.call
+      expect(job_updater2).to have_received(:update).with(no_args)
+    end
+  end
+end
