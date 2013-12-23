1761ed87e9a6877e3abba904afdc08a21bfe9d24
Matthew Boedicker <mboedicker@pivotallabs.com>
2013-12-23 11:28:31 -0800

    Test cleanup for DeploymentManager
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotallabs.com>

--- bosh-director/spec/unit/api/deployment_manager_spec.rb
+++ bosh-director/spec/unit/api/deployment_manager_spec.rb
@@ -1,64 +1,61 @@
 require 'spec_helper'
 
-describe Bosh::Director::Api::DeploymentManager do
-  let(:deployment) { double('Deployment', name: 'DEPLOYMENT_NAME') }
-  let(:task) { double('Task', id: 42) }
-  let(:user) { 'FAKE_USER' }
-
-  before do
-    Resque.stub(:enqueue)
-    BD::JobQueue.any_instance.stub(create_task: task)
-  end
-
-  describe '#create_deployment' do
+module Bosh::Director
+  describe Api::DeploymentManager do
+    let(:deployment) { double('Deployment', name: 'DEPLOYMENT_NAME') }
+    let(:task) { double('Task') }
+    let(:username) { 'FAKE_USER' }
+    let(:job_queue) { instance_double('Bosh::Director::JobQueue') }
+    let(:options) { { foo: 'bar' } }
 
     before do
-      subject.stub(:write_file)
+      JobQueue.stub(:new).and_return(job_queue)
     end
 
-    context 'when sufficient disk space is available' do
+    describe '#create_deployment' do
+
       before do
-        subject.stub(check_available_disk_space: true)
+        subject.stub(:write_file)
       end
 
+      context 'when sufficient disk space is available' do
+        before do
+          subject.stub(check_available_disk_space: true)
+        end
+
+        it 'enqueues a resque job' do
+          SecureRandom.stub(uuid: 'FAKE_UUID')
+          Dir.stub(tmpdir: 'FAKE_TMPDIR')
+          expected_manifest_path = File.join('FAKE_TMPDIR', 'deployment-FAKE_UUID')
+
+          job_queue.should_receive(:enqueue).with(
+            username, Jobs::UpdateDeployment, 'create deployment', [expected_manifest_path, options]).and_return(task)
+
+          expect(subject.create_deployment(username, 'FAKE_DEPLOYMENT_MANIFEST', options)).to eq(task)
+        end
+      end
+    end
+
+    describe '#delete_deployment' do
       it 'enqueues a resque job' do
-        SecureRandom.stub(uuid: 'FAKE_UUID')
-        Dir.stub(tmpdir: 'FAKE_TMPDIR')
-        expected_manifest_path = File.join('FAKE_TMPDIR', 'deployment-FAKE_UUID')
+        job_queue.should_receive(:enqueue).with(
+          username, Jobs::DeleteDeployment, "delete deployment #{deployment.name}", [deployment.name, options]).and_return(task)
 
-        Resque.should_receive(:enqueue).with(BD::Jobs::UpdateDeployment, task.id, expected_manifest_path, {})
-
-        subject.create_deployment(user, 'FAKE_DEPLOYMENT_MANIFEST')
-      end
-
-      it 'returns the task created by JobQueue' do
-        expect(subject.create_deployment(user, 'FAKE_DEPLOYMENT_MANIFEST')).to eq(task)
+        expect(subject.delete_deployment(username, deployment, options)).to eq(task)
       end
     end
-  end
 
-  describe '#delete_deployment' do
-    it 'enqueues a resque job' do
-      Resque.should_receive(:enqueue).with(BD::Jobs::DeleteDeployment, task.id, 'DEPLOYMENT_NAME', {})
+    describe '#find_by_name' do
+      let(:deployment_lookup) { instance_double('Bosh::Director::Api::DeploymentLookup') }
 
-      subject.delete_deployment(user, deployment)
-    end
+      before do
+        Api::DeploymentLookup.stub(new: deployment_lookup)
+      end
 
-    it 'returns the task created by JobQueue' do
-      expect(subject.delete_deployment(user, deployment)).to eq(task)
-    end
-  end
-
-  describe '#find_by_name' do
-    let(:deployment_lookup) { instance_double('Bosh::Director::Api::DeploymentLookup') }
-
-    before do
-      Bosh::Director::Api::DeploymentLookup.stub(new: deployment_lookup)
-    end
-
-    it 'delegates to DeploymentLookup' do
-      deployment_lookup.should_receive(:by_name).with(deployment.name).and_return(deployment)
-      expect(subject.find_by_name(deployment.name)).to eq deployment
+      it 'delegates to DeploymentLookup' do
+        deployment_lookup.should_receive(:by_name).with(deployment.name).and_return(deployment)
+        expect(subject.find_by_name(deployment.name)).to eq deployment
+      end
     end
   end
 end
