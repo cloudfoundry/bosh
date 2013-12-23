4aca85a1feed07b7494e3f68750fb5cf5b7c8fc5
Yulia Tolskaya <ytolskaya@pivotallabs.com>
2013-12-23 12:27:34 -0800

    corrected ami_spec to properly stub out #images
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-stemcell/spec/bosh/stemcell/aws/ami_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/aws/ami_spec.rb
@@ -3,25 +3,34 @@ require 'bosh/stemcell/aws/ami'
 
 module Bosh::Stemcell::Aws
   describe Ami do
+    subject(:ami) { Ami.new(stemcell, region) }
+
     let(:stemcell) do
-      stemcell = double('Bosh::Dev::Stemcell')
-      stemcell_manifest = { 'cloud_properties' => { 'ami' => '' } }
-      stemcell.stub(:extract).and_yield('/foo/bar', stemcell_manifest)
-      stemcell
+      instance_double('Bosh::Dev::Stemcell').tap do |s|
+        s.stub(:extract).and_yield('/foo/bar', {
+          'cloud_properties' => { 'ami' => '' }
+        })
+      end
     end
 
-    subject(:ami) do
-      Ami.new(stemcell, instance_double('Bosh::Stemcell::Aws::Region', name: 'fake-region'))
-    end
+    let(:region) { instance_double('Bosh::Stemcell::Aws::Region', name: 'fake-region') }
 
-    before do
-      Logger.stub(:new)
-    end
+    before { Logger.stub(:new) }
 
-    describe 'publish' do
-      it 'creates a new ami' do
-        provider = instance_double('Bosh::AwsCloud::Cloud', create_stemcell: 'fake-ami-id').as_null_object
-        Bosh::Clouds::Provider.stub(create: provider)
+    describe '#publish' do
+      it 'creates a new ami and makes it public' do
+        image = instance_double('AWS::EC2::Image')
+        expect(image).to receive(:public=).with(true)
+
+        ec2 = instance_double('AWS::EC2', images: { 'fake-ami-id' => image })
+
+        cpi = instance_double(
+          'Bosh::AwsCloud::Cloud',
+          create_stemcell: 'fake-ami-id',
+          ec2: ec2,
+        )
+
+        Bosh::Clouds::Provider.stub(create: cpi)
 
         expect(ami.publish).to eq('fake-ami-id')
       end
