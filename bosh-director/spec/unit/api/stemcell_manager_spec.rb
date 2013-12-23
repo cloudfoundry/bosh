f5d5b6c26e4b734e459f5355c15c3c8e34bb4785
Matthew Boedicker <mboedicker@pivotallabs.com>
2013-12-23 10:26:26 -0800

    Fix level of abstraction in stemcell_manager_spec
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotallabs.com>

--- bosh-director/spec/unit/api/stemcell_manager_spec.rb
+++ bosh-director/spec/unit/api/stemcell_manager_spec.rb
@@ -2,39 +2,52 @@
 
 require 'spec_helper'
 
-describe Bosh::Director::Api::StemcellManager do
-  let(:tmpdir) { Dir::tmpdir }
-  let(:user) { Bosh::Director::Models::User.make }
-  let(:task_id) { 1 }
-  let(:task) { double('task', :id => task_id) }
-  let(:stemcell) { 'stemcell_location' }
+module Bosh::Director
+  describe Api::StemcellManager do
+    let(:tmpdir) { Dir::tmpdir }
+    let(:username) { 'username-1' }
+    let(:task_id) { 1 }
+    let(:task) { double('task', :id => task_id) }
+    let(:stemcell) { 'stemcell_location' }
+    let(:job_queue) { instance_double('Bosh::Director::JobQueue') }
 
-  describe 'create_stemcell' do
-    before do
-      BD::JobQueue.any_instance.stub(create_task: task)
-    end
-
-    context 'local stemcell' do
+    describe 'create_stemcell' do
       before do
-        SecureRandom.stub(:uuid).and_return('FAKE_UUID')
+        JobQueue.stub(:new).and_return(job_queue)
       end
 
-      it 'enqueues a task to upload a local stemcell' do
-        subject.stub(check_available_disk_space: true)
-        subject.stub(:write_file)
+      context 'local stemcell' do
+        let(:options) { { foo: 'bar' } }
 
-        Resque.should_receive(:enqueue).with(BD::Jobs::UpdateStemcell, task_id, "#{tmpdir}/stemcell-FAKE_UUID", {})
-        
-        expect(subject.create_stemcell(user, stemcell)).to eql(task)
-      end      
-    end
-    
-    context 'remote stemcell' do
-      it 'enqueues a task to upload a remote stemcell' do
-        Resque.should_receive(:enqueue).with(BD::Jobs::UpdateStemcell, task_id, stemcell, remote: true)
-        
-        expect(subject.create_stemcell(user, stemcell, remote: true)).to eql(task)
+        before do
+          SecureRandom.stub(:uuid).and_return('FAKE_UUID')
+        end
+
+        it 'enqueues a task to upload a local stemcell' do
+          subject.stub(check_available_disk_space: true)
+          subject.stub(:write_file)
+
+          job_queue.should_receive(:enqueue).with(username,
+                                                  Jobs::UpdateStemcell,
+                                                  'create stemcell',
+                                                  [File.join(tmpdir, 'stemcell-FAKE_UUID'), options]).and_return(task)
+
+          expect(subject.create_stemcell(username, stemcell, options)).to eql(task)
+        end
+      end
+
+      context 'remote stemcell' do
+        let(:options) { { remote: true } }
+
+        it 'enqueues a task to upload a remote stemcell' do
+          job_queue.should_receive(:enqueue).with(username,
+                                                  Jobs::UpdateStemcell,
+                                                  'create stemcell',
+                                                  [stemcell, options]).and_return(task)
+
+          expect(subject.create_stemcell(username, stemcell, options)).to eql(task)
+        end
       end
     end
-  end  
+  end
 end
