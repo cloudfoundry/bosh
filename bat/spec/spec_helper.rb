30f0ee584c9b0d433f2314a2d5c5f15b6a1ee5a4
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-12-27 14:02:27 -0800

    include rspec/its in bats
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bat/spec/spec_helper.rb
+++ bat/spec/spec_helper.rb
@@ -1,9 +1,7 @@
 require 'rspec'
-
+require 'rspec/its'
 require 'fakefs/spec_helpers'
 
 SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
 
-Dir.glob(File.expand_path('support/**/*.rb', File.dirname(__FILE__))).each do |support|
-  require support
-end
+Dir.glob(File.expand_path('support/**/*.rb', SPEC_ROOT)).each { |f| require(f) }
