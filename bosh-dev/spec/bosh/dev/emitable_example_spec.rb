4a1c4925c60eaaa6e8782ce135dc6e1a6bf663ee
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-12-27 16:08:47 -0800

    formatting
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-dev/spec/bosh/dev/emitable_example_spec.rb
+++ bosh-dev/spec/bosh/dev/emitable_example_spec.rb
@@ -3,22 +3,20 @@ require 'bosh/dev/emitable_example'
 
 module Bosh::Dev
   describe EmitableExample do
-    let(:run_time) { 3.14 }
-    let(:example) do
-      double(RSpec::Core::Example, metadata:
-        {
-          full_description: 'Some context should, r3sult in:some_behavior.',
-          execution_result: { run_time: run_time }
-        })
-    end
-
     subject do
       ENV.stub(:fetch).with('BAT_INFRASTRUCTURE').and_return('openstack')
       EmitableExample.new(example)
     end
 
-    its(:metric) { should eq 'ci.bosh.bat.duration' }
-    its(:value) { should eq run_time }
-    its(:options) { should eq(tags: %w[infrastructure:openstack example:some-context-should-r3sult-in-some-behavior]) }
+    let(:example) do
+      instance_double('RSpec::Core::Example', metadata: {
+        full_description: 'Some context should, result in:some_behavior.',
+        execution_result: { run_time: 3.14 }
+      })
+    end
+
+    its(:metric)  { should eq('ci.bosh.bat.duration') }
+    its(:value)   { should eq(3.14) }
+    its(:options) { should eq(tags: %w[infrastructure:openstack example:some-context-should-result-in-some-behavior]) }
   end
 end
