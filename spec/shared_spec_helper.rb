c976f3c7cd4261029ed11f70d5d3c377ba3dffa1
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2013-12-27 16:37:19 -0800

    leave verify_partial_doubles turned off for nwo
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- spec/shared_spec_helper.rb
+++ spec/shared_spec_helper.rb
@@ -6,6 +6,9 @@ RSpec.configure do |config|
   config.deprecation_stream = StringIO.new
 
   config.mock_with :rspec do |mocks|
+    # Turn on after fixing several specs that stub out private methods
+    # mocks.verify_partial_doubles = true
+
     mocks.verify_doubled_constant_names = true
   end
 end
