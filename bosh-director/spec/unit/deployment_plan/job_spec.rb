060b6a904351e641843aee118ecbf20c2b1760f2
Matthew Boedicker <mboedicker@pivotallabs.com>
2014-01-07 14:51:06 -0800

    rspec3 for job spec
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-director/spec/unit/deployment_plan/job_spec.rb
+++ bosh-director/spec/unit/deployment_plan/job_spec.rb
@@ -1,100 +1,104 @@
 require 'spec_helper'
 
 describe Bosh::Director::DeploymentPlan::Job do
+  subject(:job) { described_class.new(plan, spec) }
+
+  let(:deployment) { Bosh::Director::Models::Deployment.make }
+  let(:plan) { double(Bosh::Director::DeploymentPlan, model: deployment) }
+  let(:spec) do
+    {
+      'name' => 'foobar',
+      'template' => 'foo',
+      'release' => 'appcloud',
+      'resource_pool' => 'dea'
+    }
+  end
+
   describe 'parsing job spec' do
-    before do
-      @deployment = make_deployment('mycloud')
-      @plan = make_plan(@deployment)
-      @spec = make_spec
+    describe 'name key' do
+      it 'parses name' do
+        job.parse_name
+        expect(job.name).to eq('foobar')
+      end
     end
 
-    it 'parses name' do
-      job = described_class.new(@plan, @spec)
-      job.parse_name
-      job.name.should == 'foobar'
-    end
-
-    it 'parses release' do
-      job = described_class.new(@plan, @spec)
-      release = double(BD::DeploymentPlan::ReleaseVersion)
-      @plan.should_receive(:release).with('appcloud').and_return(release)
-      job.parse_release
-      job.release.should == release
-    end
-
-    it 'complains about unknown release' do
-      job = described_class.new(@plan, @spec)
-      @plan.should_receive(:release).with('appcloud').and_return(nil)
-      expect {
+    describe 'release key' do
+      it 'parses release' do
+        release = instance_double('Bosh::Director::DeploymentPlan::ReleaseVersion')
+        allow(plan).to receive(:release).with('appcloud').and_return(release)
         job.parse_release
-      }.to raise_error(BD::JobUnknownRelease)
+        expect(job.release).to eq(release)
+      end
+
+      it 'complains about unknown release' do
+        allow(plan).to receive(:release).with('appcloud').and_return(nil)
+        expect {
+          job.parse_release
+        }.to raise_error(Bosh::Director::JobUnknownRelease)
+      end
     end
 
-    it 'parses a single template' do
-      job = described_class.new(@plan, @spec)
-      release = double(BD::DeploymentPlan::ReleaseVersion)
-      template = double(BD::DeploymentPlan::Template)
+    describe 'template key' do
+      it 'parses a single template' do
+        release = instance_double(Bosh::Director::DeploymentPlan::ReleaseVersion)
+        template = instance_double(Bosh::Director::DeploymentPlan::Template)
 
-      @plan.should_receive(:release).with('appcloud').and_return(release)
-      release.should_receive(:use_template_named).with('foo')
-      release.should_receive(:template).with('foo').and_return(template)
+        allow(plan).to receive(:release).with('appcloud').and_return(release)
+        expect(release).to receive(:use_template_named).with('foo')
+        expect(release).to receive(:template).with('foo').and_return(template)
 
-      job.parse_release
-      job.parse_template
-      job.templates.should == [template]
+        job.parse_release
+        job.parse_template
+        expect(job.templates).to eq([template])
+      end
+
+      it 'parses multiple templates' do
+        spec['template'] = %w(foo bar)
+        release = instance_double('Bosh::Director::DeploymentPlan::ReleaseVersion')
+        foo_template = instance_double('Bosh::Director::DeploymentPlan::Template')
+        bar_template = instance_double('Bosh::Director::DeploymentPlan::Template')
+
+        expect(plan).to receive(:release).with('appcloud').and_return(release)
+
+        expect(release).to receive(:use_template_named).with('foo')
+        expect(release).to receive(:use_template_named).with('bar')
+
+        expect(release).to receive(:template).with('foo').and_return(foo_template)
+        expect(release).to receive(:template).with('bar').and_return(bar_template)
+
+        job.parse_release
+        job.parse_template
+        expect(job.templates).to eq([foo_template, bar_template])
+      end
     end
 
-    it 'parses multiple templates' do
-      @spec['template'] = %w(foo bar)
-      job = described_class.new(@plan, @spec)
-      release = double(BD::DeploymentPlan::ReleaseVersion)
-      foo_template = double(BD::DeploymentPlan::Template)
-      bar_template = double(BD::DeploymentPlan::Template)
+    describe 'persistent_disk key' do
+      it 'parses persistent disk if present' do
+        spec['persistent_disk'] = 300
+        job.parse_disk
+        expect(job.persistent_disk).to eq(300)
+      end
 
-      @plan.should_receive(:release).with('appcloud').and_return(release)
-
-      release.should_receive(:use_template_named).with('foo')
-      release.should_receive(:use_template_named).with('bar')
-
-      release.should_receive(:template).with('foo').and_return(foo_template)
-      release.should_receive(:template).with('bar').and_return(bar_template)
-
-      job.parse_release
-      job.parse_template
-      job.templates.should == [foo_template, bar_template]
+      it 'uses 0 for persistent disk if not present' do
+        job.parse_disk
+        expect(job.persistent_disk).to eq(0)
+      end
     end
 
-    it 'parses persistent disk if present' do
-      @spec['persistent_disk'] = 300
-      job = described_class.new(@plan, @spec)
-      job.parse_disk
-      job.persistent_disk.should == 300
-    end
-
-    it 'uses 0 for persistent disk if not present' do
-      job = described_class.new(@plan, @spec)
-      job.parse_disk
-      job.persistent_disk.should == 0
-    end
-
-    it 'parses resource pool' do
-      job = described_class.new(@plan, @spec)
-
-      resource_pool = double(BD::DeploymentPlan::ResourcePool)
-      @plan.should_receive(:resource_pool).with('dea').and_return(resource_pool)
-
-      job.parse_resource_pool
-      job.resource_pool.should == resource_pool
-    end
-
-    it 'complains about unknown resource pool' do
-      job = described_class.new(@plan, @spec)
-
-      @plan.should_receive(:resource_pool).with('dea').and_return(nil)
-
-      expect {
+    describe 'resource_pool key' do
+      it 'parses resource pool' do
+        resource_pool = instance_double('Bosh::Director::DeploymentPlan::ResourcePool')
+        expect(plan).to receive(:resource_pool).with('dea').and_return(resource_pool)
         job.parse_resource_pool
-      }.to raise_error(BD::JobUnknownResourcePool)
+        expect(job.resource_pool).to eq(resource_pool)
+      end
+
+      it 'complains about unknown resource pool' do
+        expect(plan).to receive(:resource_pool).with('dea').and_return(nil)
+        expect {
+          job.parse_resource_pool
+        }.to raise_error(Bosh::Director::JobUnknownResourcePool)
+      end
     end
 
     describe 'binding properties' do
@@ -118,25 +122,23 @@ describe Bosh::Director::DeploymentPlan::Job do
       end
 
       let(:bar_properties) do
-        { 'dea_max_memory' => { 'default' => 2048 } }
+        {'dea_max_memory' => {'default' => 2048}}
       end
 
-      let(:job) { described_class.new(@plan, @spec) }
-
       before do
-        @spec['properties'] = props
-        @spec['template'] = %w(foo bar)
+        spec['properties'] = props
+        spec['template'] = %w(foo bar)
 
         release = instance_double('Bosh::Director::DeploymentPlan::ReleaseVersion')
 
-        @plan.stub(:properties).and_return(props)
-        @plan.should_receive(:release).with('appcloud').and_return(release)
+        allow(plan).to receive(:properties).and_return(props)
+        expect(plan).to receive(:release).with('appcloud').and_return(release)
 
-        release.should_receive(:use_template_named).with('foo')
-        release.should_receive(:use_template_named).with('bar')
+        expect(release).to receive(:use_template_named).with('foo')
+        expect(release).to receive(:use_template_named).with('bar')
 
-        release.should_receive(:template).with('foo').and_return(foo_template)
-        release.should_receive(:template).with('bar').and_return(bar_template)
+        expect(release).to receive(:template).with('foo').and_return(foo_template)
+        expect(release).to receive(:template).with('bar').and_return(bar_template)
 
         job.parse_name
         job.parse_release
@@ -145,47 +147,58 @@ describe Bosh::Director::DeploymentPlan::Job do
       end
 
       context 'when all the job specs (aka templates) specify properties' do
-        let(:foo_template) { instance_double('Bosh::Director::DeploymentPlan::Template', :properties => foo_properties) }
-        let(:bar_template) { instance_double('Bosh::Director::DeploymentPlan::Template', :properties => bar_properties) }
+        let(:foo_template) do
+          instance_double('Bosh::Director::DeploymentPlan::Template', properties: foo_properties)
+        end
+
+        let(:bar_template) do
+          instance_double('Bosh::Director::DeploymentPlan::Template', properties: bar_properties)
+        end
 
         before { job.bind_properties }
 
         it 'should drop deployment manifest properties not specified in the job spec properties' do
-          job.properties.should_not have_key 'cc'
-          job.properties['deep_property'].should_not have_key 'unneeded'
+          expect(job.properties).to_not have_key('cc')
+          expect(job.properties['deep_property']).to_not have_key('unneeded')
         end
 
         it 'should include properties that are in the job spec properties but not in the deployment manifest' do
-          job.properties['dea_min_memory'].should == 512
-          job.properties['deep_property']['new_property'].should == 'jkl'
+          expect(job.properties['dea_min_memory']).to eq(512)
+          expect(job.properties['deep_property']['new_property']).to eq('jkl')
         end
 
         it 'should not override deployment manifest properties with job_template defaults' do
-          job.properties['dea_max_memory'].should == 1024
-          job.properties['deep_property']['dont_override'].should == 'def'
+          expect(job.properties['dea_max_memory']).to eq(1024)
+          expect(job.properties['deep_property']['dont_override']).to eq('def')
         end
       end
 
       context 'when none of the job specs (aka templates) specify properties' do
-        let(:foo_template) { instance_double('Bosh::Director::DeploymentPlan::Template', :properties => nil) }
-        let(:bar_template) { instance_double('Bosh::Director::DeploymentPlan::Template', :properties => nil) }
+        let(:foo_template) {
+          instance_double('Bosh::Director::DeploymentPlan::Template', properties: nil) }
+        let(:bar_template) {
+          instance_double('Bosh::Director::DeploymentPlan::Template', properties: nil) }
 
         before { job.bind_properties }
 
         it 'should use the properties specified throughout the deployment manifest' do
-          job.properties.should == props
+          expect(job.properties).to eq(props)
         end
       end
 
       context "when some job specs (aka templates) specify properties and some don't" do
-        let(:foo_template) { instance_double('Bosh::Director::DeploymentPlan::Template', :properties => nil) }
-        let(:bar_template) { instance_double('Bosh::Director::DeploymentPlan::Template', :properties => bar_properties) }
+        let(:foo_template) {
+          instance_double('Bosh::Director::DeploymentPlan::Template', properties: nil)
+        }
+        let(:bar_template) {
+          instance_double('Bosh::Director::DeploymentPlan::Template', properties: bar_properties)
+        }
 
         it 'should raise an error' do
           expect {
             job.bind_properties
           }.to raise_error(
-            BD::JobIncompatibleSpecs,
+            Bosh::Director::JobIncompatibleSpecs,
             "Job `foobar' has specs with conflicting property definition styles" +
             ' between its job spec templates.  This may occur if colocating jobs, one of which has a spec file' +
             " including `properties' and one which doesn't."
@@ -207,72 +220,54 @@ describe Bosh::Director::DeploymentPlan::Job do
           }
         }
 
-        @spec['properties'] = props
-        @spec['property_mappings'] = {'db' => 'ccdb', 'mem' => 'dea.max_memory'}
-        @spec['template'] = 'foo'
-
-        foo_p = {
-          'db.user' => {'default' => 'root'},
-          'db.password' => {},
-          'db.host' => {'default' => 'localhost'},
-          'mem' => {'default' => 256}
-        }
+        spec['properties'] = props
+        spec['property_mappings'] = {'db' => 'ccdb', 'mem' => 'dea.max_memory'}
+        spec['template'] = 'foo'
 
         release = instance_double('Bosh::Director::DeploymentPlan::ReleaseVersion')
-        foo_template = instance_double('Bosh::Director::DeploymentPlan::Template', :properties => foo_p)
+        foo_template = instance_double(
+          'Bosh::Director::DeploymentPlan::Template',
+          properties: {
+            'db.user' => { 'default' => 'root' },
+            'db.password' => {},
+            'db.host' => { 'default' => 'localhost' },
+            'mem' => { 'default' => 256 },
+          },
+        )
 
-        @plan.stub(:properties).and_return(props)
-        @plan.should_receive(:release).with('appcloud').and_return(release)
+        allow(plan).to receive(:properties).and_return(props)
+        expect(plan).to receive(:release).with('appcloud').and_return(release)
 
-        release.should_receive(:template).with('foo').and_return(foo_template)
-        release.should_receive(:use_template_named).with('foo')
+        expect(release).to receive(:template).with('foo').and_return(foo_template)
+        expect(release).to receive(:use_template_named).with('foo')
 
-        job = described_class.new(@plan, @spec)
         job.parse_release
         job.parse_template
         job.parse_properties
         job.bind_properties
 
-        job.properties.should == {
+        expect(job.properties).to eq(
           'db' => {
             'user' => 'admin',
             'password' => '12321',
             'host' => 'localhost'
           },
           'mem' => 2048,
-        }
+        )
       end
 
       it 'complains about unsatisfiable property mappings' do
         props = {'foo' => 'bar'}
 
-        @spec['properties'] = props
-        @spec['property_mappings'] = {'db' => 'ccdb'}
+        spec['properties'] = props
+        spec['property_mappings'] = {'db' => 'ccdb'}
 
-        @plan.stub(:properties).and_return(props)
+        allow(plan).to receive(:properties).and_return(props)
 
-        job = described_class.new(@plan, @spec)
         expect {
           job.parse_properties
-        }.to raise_error(BD::JobInvalidPropertyMapping)
+        }.to raise_error(Bosh::Director::JobInvalidPropertyMapping)
       end
     end
-  end
-
-  def make_spec
-    {
-      'name' => 'foobar',
-      'template' => 'foo',
-      'release' => 'appcloud',
-      'resource_pool' => 'dea'
-    }
-  end
-
-  def make_deployment(name)
-    BD::Models::Deployment.make(:name => name)
-  end
-
-  def make_plan(deployment)
-    double(BD::DeploymentPlan, :model => deployment)
   end
 end
