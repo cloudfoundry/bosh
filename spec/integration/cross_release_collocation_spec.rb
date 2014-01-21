756f31fbee3dc8328cb4af5cd12ecdc5b9380445
Jesse Zhang <jz@pivotallabs.com>
2014-01-21 10:25:30 -0800

    integration test for cross release collocation
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- spec/integration/cross_release_collocation_spec.rb
+++ spec/integration/cross_release_collocation_spec.rb
@@ -3,39 +3,39 @@ require 'spec_helper'
 describe 'collocating templates from multiple releases' do
   include IntegrationExampleGroup
 
-  #context 'successfully collocating templates from different releases' do
-  #  let(:manifest) do
-  #    {
-  #      'releases' => [
-  #        { 'name' => 'dummy', 'version' => 'latest' },
-  #        { 'name' => 'dummy2', 'version' => 'latest' },
-  #      ],
-  #      'jobs' => [{
-  #        'name' => 'foobar',
-  #        'templates' => [
-  #          { 'name' => 'dummy_with_package', 'release' => 'dummy' },
-  #          { 'name' => 'dummy',              'release' => 'dummy2' },
-  #        ],
-  #        'resource_pool' => 'a',
-  #        'instances' => 1,
-  #        'networks' => [{ 'name' => 'a' }]
-  #      }]
-  #    }
-  #  end
-  #
-  #  it 'successfully deploys' do
-  #    target_and_login
-  #
-  #    run_bosh("upload release #{spec_asset('dummy-release.tgz')}")
-  #    run_bosh("upload release #{spec_asset('dummy2-release.tgz')}")
-  #    run_bosh("upload stemcell #{spec_asset('valid_stemcell.tgz')}")
-  #
-  #    manifest_hash = Bosh::Spec::Deployments.simple_manifest.merge(manifest)
-  #    deployment_manifest = yaml_file('simple', manifest_hash)
-  #    run_bosh("deployment #{deployment_manifest.path}")
-  #    run_bosh("deploy")
-  #  end
-  #end
+  context 'successfully collocating templates from different releases' do
+    let(:manifest) do
+      {
+        'releases' => [
+          { 'name' => 'dummy', 'version' => 'latest' },
+          { 'name' => 'dummy2', 'version' => 'latest' },
+        ],
+        'jobs' => [{
+          'name' => 'foobar',
+          'templates' => [
+            { 'name' => 'dummy_with_package', 'release' => 'dummy' },
+            { 'name' => 'dummy',              'release' => 'dummy2' },
+          ],
+          'resource_pool' => 'a',
+          'instances' => 1,
+          'networks' => [{ 'name' => 'a' }]
+        }]
+      }
+    end
+
+    it 'successfully deploys' do
+      target_and_login
+
+      run_bosh("upload release #{spec_asset('dummy-release.tgz')}")
+      run_bosh("upload release #{spec_asset('dummy2-release.tgz')}")
+      run_bosh("upload stemcell #{spec_asset('valid_stemcell.tgz')}")
+
+      manifest_hash = Bosh::Spec::Deployments.simple_manifest.merge(manifest)
+      deployment_manifest = yaml_file('simple', manifest_hash)
+      run_bosh("deployment #{deployment_manifest.path}")
+      run_bosh("deploy")
+    end
+  end
 
   context 'when 2 templates depend on packages with the same name from different releases' do
     let(:manifest) do
