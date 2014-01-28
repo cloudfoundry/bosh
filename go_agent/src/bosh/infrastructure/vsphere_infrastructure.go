4d13439250ac0f72062d01400e577fcadeab5b51
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2014-01-28 11:05:05 -0700

    Implemented GetSettings on vsphere infrastructure
    Signed-off-by: Ben Smith <bsmith@pivotallabs.com>

--- go_agent/src/bosh/infrastructure/vsphere_infrastructure.go
+++ go_agent/src/bosh/infrastructure/vsphere_infrastructure.go
@@ -1,14 +1,17 @@
 package infrastructure
 
 import (
+	bosherr "bosh/errors"
 	boshsettings "bosh/settings"
+	"encoding/json"
 )
 
 type vsphereInfrastructure struct {
 	cdromDelegate CDROMDelegate
 }
 
-func newVsphereInfrastructure(delegate CDROMDelegate) (infrastructure vsphereInfrastructure) {
+func newVsphereInfrastructure(delegate CDROMDelegate) (inf vsphereInfrastructure) {
+	inf.cdromDelegate = delegate
 	return
 }
 
@@ -17,6 +20,17 @@ func (inf vsphereInfrastructure) SetupSsh(delegate SshSetupDelegate, username st
 }
 
 func (inf vsphereInfrastructure) GetSettings() (settings boshsettings.Settings, err error) {
+	contents, err := inf.cdromDelegate.GetFileContentsFromCDROM("env")
+	if err != nil {
+		err = bosherr.WrapError(err, "Reading contents from CDROM")
+		return
+	}
+
+	err = json.Unmarshal(contents, &settings)
+	if err != nil {
+		err = bosherr.WrapError(err, "Unmarshalling settings from CDROM")
+	}
+
 	return
 }
 
--- go_agent/src/bosh/infrastructure/vsphere_infrastructure_test.go
+++ go_agent/src/bosh/infrastructure/vsphere_infrastructure_test.go
@@ -1,8 +1,24 @@
 package infrastructure
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestStub(t *testing.T) {
+type FakeCDROMDelegate struct {
+}
+
+func (fakeCDROMDelegate FakeCDROMDelegate) GetFileContentsFromCDROM(_ string) (contents []byte, err error) {
+	contents = []byte(`{"agent_id": "123"}`)
+	return
+}
+
+func TestVsphereGetSettings(t *testing.T) {
+	cdromDelegate := FakeCDROMDelegate{}
+	vsphere := newVsphereInfrastructure(cdromDelegate)
+
+	settings, err := vsphere.GetSettings()
+
+	assert.NoError(t, err)
+	assert.Equal(t, settings.AgentId, "123")
 }
