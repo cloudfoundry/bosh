d25c0f23fa5bf8ecf96baf74f550e83a9f99844d
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2014-01-29 15:10:31 -0800

    move erb rendering into SourceErb
    Signed-off-by: Jeffrey Peckham <peckham@pivotallabs.com>

---
+++ bosh-director-core/lib/bosh/director/core/templates/source_erb.rb
@@ -0,0 +1,35 @@
+require 'bosh/director/core/templates'
+require 'common/properties'
+
+module Bosh::Director::Core::Templates
+  class SourceErb
+    attr_reader :src_name, :dest_name, :erb
+
+    def initialize(src_name, dest_name, erb_contents, template_name)
+      @src_name = src_name
+      @dest_name = dest_name
+      erb = ERB.new(erb_contents)
+      erb.filename = File.join(template_name, src_name)
+      @erb = erb
+    end
+
+    def render(context, job_name, index, logger)
+      erb.result(context.get_binding)
+      # rubocop:disable RescueException
+    rescue Exception => e
+      # rubocop:enable RescueException
+
+      logger.debug(e.inspect)
+      job_desc = "#{job_name}/#{index}"
+      line_index = e.backtrace.index { |l| l.include?(erb.filename) }
+      line = line_index ? e.backtrace[line_index] : '(unknown):(unknown)'
+      template_name, line = line.split(':')
+
+      message = "Error filling in template `#{File.basename(template_name)}' " +
+        "for `#{job_desc}' (line #{line}: #{e})"
+
+      logger.debug("#{message}\n#{e.backtrace.join("\n")}")
+      raise message
+    end
+  end
+end
--- bosh-director-core/lib/bosh/director/core/templates/src_file_template.rb
+++
@@ -1,5 +0,0 @@
-require 'bosh/director/core/templates'
-
-module Bosh::Director::Core::Templates
-  SrcFileTemplate = Struct.new(:src_name, :dest_name, :erb_file)
-end
