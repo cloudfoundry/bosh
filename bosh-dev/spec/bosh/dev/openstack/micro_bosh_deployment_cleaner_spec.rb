eea9be94a7893d7d70431c51c2989b5178e964a7
Mike Stallard <mstallard@pivotallabs.com>
2014-01-30 14:50:51 -0800

    Revert "Comment out openstack bats image and volume cleanup for now."
    This reverts commit 46462e44542a9ef37dc285ccd3591e71427d47f2.

--- bosh-dev/spec/bosh/dev/openstack/micro_bosh_deployment_cleaner_spec.rb
+++ bosh-dev/spec/bosh/dev/openstack/micro_bosh_deployment_cleaner_spec.rb
@@ -144,7 +144,6 @@ module Bosh::Dev::Openstack
         before { image_collection.stub(all: [image_to_be_deleted_1, image_to_be_deleted_2, image_to_be_ignored]) }
 
         it 'deletes all images' do
-          pending
           cleaner.clean
 
           expect(image_to_be_deleted_1).to have_received(:destroy)
@@ -152,8 +151,7 @@ module Bosh::Dev::Openstack
           expect(image_to_be_ignored).to_not have_received(:destroy)
         end
 
-        it 'logs messages' do
-          pending
+        it 'logs message' do
           cleaner.clean
 
           expect(logger).to have_received(:info).with('Destroying image BOSH-fake-image-1')
@@ -182,7 +180,6 @@ module Bosh::Dev::Openstack
         before { volume_collection.stub(all: [volume1, volume2]) }
 
         it 'deletes all unattached volumes' do
-          pending
           expect(volume1).to receive(:destroy)
           expect(volume2).to_not receive(:destroy)
 
@@ -190,7 +187,6 @@ module Bosh::Dev::Openstack
         end
 
         it 'logs messages' do
-          pending
           expect(logger).to receive(:info).with('Destroying volume fake-volume-1')
 
           cleaner.clean
