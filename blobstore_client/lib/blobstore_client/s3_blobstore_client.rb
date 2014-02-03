52f1ae5adbbc62c2ed6ae4d683a894ad21893560
Alex Stupakov and Karen Wang <pair+astupakov+khwang@pivotallabs.com>
2014-02-03 12:11:54 -0800

    use fetch for default values in aws_options

--- blobstore_client/lib/blobstore_client/s3_blobstore_client.rb
+++ blobstore_client/lib/blobstore_client/s3_blobstore_client.rb
@@ -1,5 +1,3 @@
-# Copyright (c) 2009-2012 VMware, Inc.
-
 require 'openssl'
 require 'digest/sha1'
 require 'base64'
@@ -34,9 +32,9 @@ module Bosh
         aws_options = {
           access_key_id: @options[:access_key_id],
           secret_access_key: @options[:secret_access_key],
-          use_ssl: @options[:use_ssl].nil? ? true : @options[:use_ssl],
-          s3_port: @options[:port].nil? ? 443 : @options[:port],
-          s3_endpoint: @options[:host] || URI.parse(S3BlobstoreClient::ENDPOINT).host,
+          use_ssl: @options.fetch(:use_ssl, true),
+          s3_port: @options.fetch(:port, 443),
+          s3_endpoint: @options.fetch(:host, URI.parse(S3BlobstoreClient::ENDPOINT).host),
           s3_force_path_style: true,
         }
 
