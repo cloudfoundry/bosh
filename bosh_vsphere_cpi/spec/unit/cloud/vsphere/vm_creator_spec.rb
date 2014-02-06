1edf5b04ca33a8eecda9b59d34b661b6f765cd38
Jesse Zhang <jz@pivotallabs.com>
2014-02-05 17:00:09 -0800

    Stricter tests
    * Adds argument matchers to all stubs
    * Set return value to non-nil to all stubs
    * Adds coverage for inner iterations
    * better matcher for vCenter data types
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- bosh_vsphere_cpi/spec/unit/cloud/vsphere/vm_creator_spec.rb
+++ bosh_vsphere_cpi/spec/unit/cloud/vsphere/vm_creator_spec.rb
@@ -32,47 +32,71 @@ describe VSphereCloud::VmCreator do
       disk_spec = double('disk spec')
       disk_locality = ['disk1_cid']
 
-      networks = {}
-      vdisk_controller_key = double('virtual disk controller key')
-      system_disk = double('system disk', :controller_key => vdisk_controller_key)
-      devices = double('devices', :find => system_disk, :select => [])
+      networks = {
+        'network_name' => {
+          'cloud_properties' => {
+            'name' => 'network_name',
+          },
+        },
+      }
       stemcell_vm = double('stemcell vm')
       current_snapshot = double('current snapshot')
       snapshot = double('snapshot', :current_snapshot => current_snapshot)
-      stemcell_properties = { 'config.hardware.device' => devices, 'snapshot' => snapshot }
+
+      vdisk_controller_key = double('virtual disk controller key')
+      system_disk = VimSdk::Vim::Vm::Device::VirtualDisk.new(controller_key: vdisk_controller_key)
+      virtual_nic = VimSdk::Vim::Vm::Device::VirtualEthernetCard.new
+      pci_controller_key = double('virtual pci controller key')
+      pci_controller = VimSdk::Vim::Vm::Device::VirtualPCIController.new(key: pci_controller_key)
+      devices = [ system_disk, virtual_nic, pci_controller ]
+      stemcell_properties = {
+        'config.hardware.device' => devices,
+        'snapshot' => snapshot,
+      }
+
+      delete_nic_spec = double('nic config')
+      allow(cpi).to receive(:create_delete_device_spec).with(virtual_nic).and_return(delete_nic_spec)
 
       allow(cpi).to receive(:stemcell_vm).with('stemcell_cid').and_return(stemcell_vm)
       network_env = double('network env rv')
-      allow(cpi).to receive(:generate_network_env).with(devices, {}, {}).and_return(network_env)
+      allow(cpi).to receive(:generate_network_env).with(devices, networks, {}).and_return(network_env)
       allow(cpi).to receive(:disk_spec).with(disk_locality).and_return(disk_spec)
-      allow(cpi).to receive(:upload_file)
+      allow(cpi).to receive(:upload_file).with('datacenter name', 'datastore name', 'vm-vm_unique_name/env.iso', '')
 
       allow(vsphere_client).to receive(:get_property).with(stemcell_vm, anything, anything, anything).and_return(1024*1024)
       replicated_stemcell_vm = double('replicated vm')
       allow(vsphere_client).to receive(:get_properties).with(replicated_stemcell_vm, VimSdk::Vim::VirtualMachine, ['config.hardware.device', 'snapshot'], ensure_all: true).and_return(stemcell_properties)
-      allow(vsphere_client).to receive(:reconfig_vm)
-      allow(vsphere_client).to receive(:power_on_vm)
 
       datastore = double('datastore')
       allow(datastore).to receive(:name).with(no_args).and_return("datastore name")
       datacenter_mob = double('datacenter mob')
       folder_mob = double('folder managed object')
-      datacenter = double('datacenter', :name => 'datacenter', :vm_folder => double('vm_folder', :mob => folder_mob), mob: datacenter_mob)
+      datacenter = double('datacenter', :name => 'datacenter name', :vm_folder => double('vm_folder', :mob => folder_mob), mob: datacenter_mob)
       resource_pool_mob = double('resource pool managed object')
       cluster = double('cluster', :datacenter => datacenter, :resource_pool => double('resource pool', :mob => resource_pool_mob))
 
       allow(cpi).to receive(:generate_unique_name).with(no_args).and_return("vm_unique_name")
       allow(cpi).to receive(:replicate_stemcell).with(cluster, datastore, "stemcell_cid").and_return(replicated_stemcell_vm)
 
+      network_mob = double('standard network managed object')
+      allow(vsphere_client).to receive(:find_by_inventory_path).
+        with(['datacenter name', 'network', 'network_name']).
+        and_return(network_mob)
+      add_nic_spec = double('add virtual nic spec')
+      allow(cpi).to receive(:create_nic_config_spec).with(
+        'network_name',
+        network_mob,
+        pci_controller_key,
+        {},
+      ).and_return(add_nic_spec)
+
       disk_device = double('disk device')
       ephemeral_disk_config = double('ephemeral disk config', :device => disk_device)
-      allow(cpi).to receive(:fix_device_unit_numbers).with(devices, [ephemeral_disk_config])
+      allow(cpi).to receive(:fix_device_unit_numbers).with(devices, [ephemeral_disk_config, add_nic_spec, delete_nic_spec])
       disk_env = double('disk env')
       allow(cpi).to receive(:generate_disk_env).with(system_disk, disk_device).and_return(disk_env)
-      allow(cpi).to receive(:get_vm_location)
-      allow(cpi).to receive(:set_agent_env)
       datastore_mob = double('datastore mob')
-      allow(datastore).to receive(:mob).and_return(datastore_mob)
+      allow(datastore).to receive(:mob).with(no_args).and_return(datastore_mob)
       allow(cpi).to receive(:create_disk_config_spec).with(
                       datastore_mob,
                       '[datastore name] vm-vm_unique_name/ephemeral_disk.vmdk',
@@ -81,8 +105,9 @@ describe VSphereCloud::VmCreator do
                       :create => true,
                     ).and_return(ephemeral_disk_config)
       allow(logger).to receive(:info)
-      allow(cluster).to receive(:mob).and_return(double('cluster mob'))
-      allow(cpi).to receive(:configure_env_cdrom).with(datastore_mob, devices, '[datastore name] vm-vm_unique_name/env.iso').and_return(double('configure env cdrom rv'))
+      allow(cluster).to receive(:mob).with(no_args).and_return(double('cluster mob'))
+      add_cdrom_spec = double('configure env cdrom rv')
+      allow(cpi).to receive(:configure_env_cdrom).with(datastore_mob, devices, '[datastore name] vm-vm_unique_name/env.iso').and_return(add_cdrom_spec)
 
       clone_vm_task = double('cloned vm task')
       allow(cpi).to receive(:clone_vm).with(
@@ -97,7 +122,7 @@ describe VSphereCloud::VmCreator do
           config: match_attributes(
             memory_mb: 1024,
             num_cpus: 1,
-            device_change: [ephemeral_disk_config],
+            device_change: [ephemeral_disk_config, add_nic_spec, delete_nic_spec],
           ),
         },
       ).and_return(clone_vm_task)
@@ -105,6 +130,24 @@ describe VSphereCloud::VmCreator do
       allow(vsphere_client).to receive(:wait_for_task).with(clone_vm_task).and_return(vm_double)
       allow(vsphere_client).to receive(:get_properties).with(vm_double, VimSdk::Vim::VirtualMachine, ['config.hardware.device'], ensure_all: true).and_return(stemcell_properties)
       allow(cpi).to receive(:generate_agent_env).with("vm-vm_unique_name", vm_double, 'agent_id', network_env, disk_env).and_return({})
+
+      allow(vsphere_client).to receive(:reconfig_vm).with(
+        vm_double,
+        match_attributes(
+          device_change: [ add_cdrom_spec ],
+        ),
+      )
+
+      vm_location = double('vm location')
+      allow(cpi).to receive(:get_vm_location).with(
+                      vm_double,
+                      datacenter: 'datacenter name',
+                      datastore: 'datastore name',
+                      vm: 'vm-vm_unique_name',
+                    ).and_return(vm_location)
+      allow(cpi).to receive(:set_agent_env).with(vm_double, vm_location, {'env' => {}})
+
+      allow(vsphere_client).to receive(:power_on_vm).with(datacenter_mob, vm_double)
 
       expect(placer).to receive(:place).with(1024, 10241025, disk_spec).
                           and_return([cluster, datastore])
