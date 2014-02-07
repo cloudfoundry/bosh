b2856bbf8c1cc1f762d4a763c7600d41dca63060
Jeffrey Peckham <peckham@pivotallabs.com>
2014-02-07 10:43:02 -0800

    remove BundleCollectionOld

--- go_agent/src/bosh/agent/applier/bundlecollection/bundle_collection_interface.go
+++ go_agent/src/bosh/agent/applier/bundlecollection/bundle_collection_interface.go
@@ -8,21 +8,6 @@ type BundleDefinition interface {
 	BundleVersion() string
 }
 
-// BundleCollection is responsible for managing multiple bundles
-// where bundles can be installed and then enabled.
-// e.g. Used to manage currently installed/enabled jobs and packages.
-type BundleCollectionOld interface {
-
-	// Instead of returning filesys/path it would be nice
-	// to return a Directory object that would really represent
-	// some location (s3 bucket, fs, etc.)
-	Install(defintion BundleDefinition) (boshsys.FileSystem, string, error)
-
-	GetDir(defintion BundleDefinition) (fs boshsys.FileSystem, path string, err error)
-
-	Enable(defintion BundleDefinition) error
-}
-
 type BundleCollection interface {
 	Get(defintion BundleDefinition) (bundle Bundle, err error)
 }
