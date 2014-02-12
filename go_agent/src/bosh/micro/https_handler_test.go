a4862dda6c2fb2464c1f5fbb604b5fed838ecdd3
Ben Smith <bsmith@pivotallabs.com>
2014-02-12 08:42:29 -0800

    converted https_handler_test to ginkgo

--- go_agent/src/bosh/micro/https_handler_test.go
+++ go_agent/src/bosh/micro/https_handler_test.go
@@ -8,228 +8,204 @@ import (
 	fakesys "bosh/system/fakes"
 	"crypto/tls"
 	"errors"
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
-	"testing"
+	"time"
 )
 
-/********** POST /agent *************/
+var _ = Describe("HttpsHandler", func() {
+	var (
+		serverURL       string
+		handler         HttpsHandler
+		fs              *fakesys.FakeFileSystem
+		receivedRequest boshhandler.Request
+		httpClient      http.Client
+	)
 
-func TestStartAgentEndpoint(t *testing.T) {
-	serverURL, handler, _ := startServer()
-	defer stopServer(handler)
+	BeforeEach(func() {
+		serverURL = "https://user:pass@127.0.0.1:6900"
+		mbusUrl, _ := url.Parse(serverURL)
+		logger := boshlog.NewLogger(boshlog.LEVEL_NONE)
+		fs = fakesys.NewFakeFileSystem()
+		dirProvider := boshdir.NewDirectoriesProvider("/var/vcap")
+		handler = NewHttpsHandler(mbusUrl, logger, fs, dirProvider)
 
-	postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
-	postPayload := strings.NewReader(postBody)
-	client := getHTTPClient()
+		go handler.Start(func(req boshhandler.Request) (resp boshhandler.Response) {
+			receivedRequest = req
+			return boshhandler.NewValueResponse("expected value")
+		})
 
-	httpResponse, err := client.Post(serverURL+"/agent", "application/json", postPayload)
-	for err != nil {
-		httpResponse, err = client.Post(serverURL+"/agent", "application/json", postPayload)
-	}
-	defer httpResponse.Body.Close()
-
-	assert.Equal(t, receivedRequest.ReplyTo, "reply to me!")
-	assert.Equal(t, receivedRequest.Method, "ping")
-	expectedPayload := []byte(postBody)
-	assert.Equal(t, receivedRequest.GetPayload(), expectedPayload)
-
-	httpBody, readErr := ioutil.ReadAll(httpResponse.Body)
-	assert.NoError(t, readErr)
-	assert.Equal(t, httpBody, []byte(`{"value":"expected value"}`))
-}
-
-func TestStartAgentEndpointWithIncorrectHTTPMethod(t *testing.T) {
-	serverURL, handler, _ := startServer()
-	defer stopServer(handler)
-
-	waitForServerToStart(serverURL, "agent")
-
-	client := getHTTPClient()
-	httpResponse, err := client.Get(serverURL + "/agent")
-	defer httpResponse.Body.Close()
-
-	assert.NoError(t, err)
-	assert.Equal(t, httpResponse.StatusCode, 404)
-}
-
-/********** GET /blobs *************/
-
-func TestStartGETBlobsEndpoint(t *testing.T) {
-	serverURL, handler, fakeFs := startServer()
-	defer stopServer(handler)
-	fakeFs.WriteToFile("/var/vcap/micro_bosh/data/cache/123-456-789", "Some data")
-
-	client := getHTTPClient()
-
-	httpResponse, err := client.Get(serverURL + "/blobs/a5/123-456-789")
-	for err != nil {
-		httpResponse, err = client.Get(serverURL + "/blobs/a5/123-456-789")
-	}
-	defer httpResponse.Body.Close()
-
-	httpBody, readErr := ioutil.ReadAll(httpResponse.Body)
-	assert.NoError(t, readErr)
-	assert.Equal(t, httpResponse.StatusCode, 200)
-	assert.Equal(t, httpBody, []byte("Some data"))
-}
-
-func TestStartGETBlobsEndpointWithIncorrectHTTPMethod(t *testing.T) {
-	serverURL, handler, _ := startServer()
-	defer stopServer(handler)
-
-	waitForServerToStart(serverURL, "blobs")
-
-	postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
-	postPayload := strings.NewReader(postBody)
-	client := getHTTPClient()
-
-	httpResponse, err := client.Post(serverURL+"/blobs/123", "application/json", postPayload)
-	defer httpResponse.Body.Close()
-
-	assert.NoError(t, err)
-	assert.Equal(t, httpResponse.StatusCode, 404)
-}
-
-func TestStartGETBlobsEndpointWhenFileNotFound(t *testing.T) {
-	serverURL, handler, _ := startServer()
-	defer stopServer(handler)
-
-	waitForServerToStart(serverURL, "blobs")
-
-	client := getHTTPClient()
-
-	httpResponse, err := client.Get(serverURL + "/blobs/123")
-	defer httpResponse.Body.Close()
-
-	assert.NoError(t, err)
-	assert.Equal(t, httpResponse.StatusCode, 404)
-}
-
-/********** PUT /blobs *************/
-
-func TestStartPUTBlobsEndpoint(t *testing.T) {
-	serverURL, handler, fakeFs := startServer()
-	defer stopServer(handler)
-	fakeFs.WriteToFile("/var/vcap/micro_bosh/data/cache/123-456-789", "Some data")
-
-	putBody := `Updated data`
-	putPayload := strings.NewReader(putBody)
-	client := getHTTPClient()
-
-	waitForServerToStart(serverURL, "blobs")
-
-	request, err := http.NewRequest("PUT", serverURL+"/blobs/a5/123-456-789", putPayload)
-	assert.NoError(t, err)
-
-	httpResponse, err := client.Do(request)
-	defer httpResponse.Body.Close()
-
-	assert.NoError(t, err)
-	assert.Equal(t, httpResponse.StatusCode, 201)
-	contents, err := fakeFs.ReadFile("/var/vcap/micro_bosh/data/cache/123-456-789")
-	assert.NoError(t, err)
-	assert.Equal(t, contents, "Updated data")
-}
-
-func TestStartPUTBlobsEndpoint500WhenManagerErrs(t *testing.T) {
-	serverURL, handler, fs := startServer()
-	defer stopServer(handler)
-
-	fs.WriteToFileError = errors.New("oops")
-
-	putBody := `Updated data`
-	putPayload := strings.NewReader(putBody)
-	client := getHTTPClient()
-
-	waitForServerToStart(serverURL, "blobs")
-
-	request, err := http.NewRequest("PUT", serverURL+"/blobs/a5/123-456-789", putPayload)
-	assert.NoError(t, err)
-
-	httpResponse, err := client.Do(request)
-	defer httpResponse.Body.Close()
-	assert.Equal(t, httpResponse.StatusCode, 500)
-
-	responseBody, err := ioutil.ReadAll(httpResponse.Body)
-	assert.NoError(t, err)
-	assert.Contains(t, string(responseBody), "oops")
-}
-
-/********** defaults *************/
-
-func TestStartWithIncorrectURIPath(t *testing.T) {
-	serverURL, handler, _ := startServer()
-	defer stopServer(handler)
-
-	waitForServerToStart(serverURL, "agent")
-
-	postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
-	postPayload := strings.NewReader(postBody)
-	client := getHTTPClient()
-	httpResponse, err := client.Post(serverURL+"/bad_url", "application/json", postPayload)
-	defer httpResponse.Body.Close()
-
-	assert.NoError(t, err)
-	assert.Equal(t, httpResponse.StatusCode, 404)
-}
-
-func TestStartWithIncorrectUsernameAndPassword(t *testing.T) {
-	serverURL, handler, _ := startServer()
-	defer stopServer(handler)
-
-	waitForServerToStart(serverURL, "agent")
-
-	postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
-	postPayload := strings.NewReader(postBody)
-	client := getHTTPClient()
-	httpResponse, err := client.Post(strings.Replace(serverURL, "pass", "wrong", -1)+"/agent", "application/json", postPayload)
-	defer httpResponse.Body.Close()
-
-	assert.NoError(t, err)
-	assert.Equal(t, httpResponse.StatusCode, 401)
-	assert.Equal(t, httpResponse.Header.Get("WWW-Authenticate"), `Basic realm=""`)
-}
-
-func getHTTPClient() (httpClient http.Client) {
-	httpTransport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-	httpClient = http.Client{Transport: httpTransport}
-	return
-}
-
-func waitForServerToStart(serverURL string, endpoint string) (httpResponse *http.Response) {
-	postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
-	postPayload := strings.NewReader(postBody)
-	client := getHTTPClient()
-
-	httpResponse, err := client.Post(serverURL+"/"+endpoint, "application/json", postPayload)
-	for err != nil {
-		httpResponse, err = client.Post(serverURL+"/"+endpoint, "application/json", postPayload)
-	}
-	defer httpResponse.Body.Close()
-	return
-}
-
-var receivedRequest boshhandler.Request
-
-func startServer() (serverURL string, handler HttpsHandler, fs *fakesys.FakeFileSystem) {
-	serverURL = "https://user:pass@127.0.0.1:6900"
-	mbusUrl, _ := url.Parse(serverURL)
-	logger := boshlog.NewLogger(boshlog.LEVEL_NONE)
-	fs = fakesys.NewFakeFileSystem()
-	dirProvider := boshdir.NewDirectoriesProvider("/var/vcap")
-	handler = NewHttpsHandler(mbusUrl, logger, fs, dirProvider)
-
-	go handler.Start(func(req boshhandler.Request) (resp boshhandler.Response) {
-		receivedRequest = req
-		return boshhandler.NewValueResponse("expected value")
+		httpTransport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+		httpClient = http.Client{Transport: httpTransport}
 	})
-	return
-}
 
-func stopServer(handler HttpsHandler) {
-	handler.Stop()
+	AfterEach(func() {
+		handler.Stop()
+		time.Sleep(1 * time.Millisecond)
+	})
+
+	Describe("POST /agent", func() {
+		It("receives request and responds", func() {
+			postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
+			postPayload := strings.NewReader(postBody)
+
+			httpResponse, err := httpClient.Post(serverURL+"/agent", "application/json", postPayload)
+			for err != nil {
+				httpResponse, err = httpClient.Post(serverURL+"/agent", "application/json", postPayload)
+			}
+			defer httpResponse.Body.Close()
+
+			Expect(receivedRequest.ReplyTo).To(Equal("reply to me!"))
+			Expect(receivedRequest.Method).To(Equal("ping"))
+			expectedPayload := []byte(postBody)
+			Expect(receivedRequest.GetPayload()).To(Equal(expectedPayload))
+
+			httpBody, readErr := ioutil.ReadAll(httpResponse.Body)
+			Expect(readErr).ToNot(HaveOccurred())
+			Expect(httpBody).To(Equal([]byte(`{"value":"expected value"}`)))
+		})
+
+		Context("when incorrect http method is used", func() {
+			It("returns a 404", func() {
+				waitForServerToStart(serverURL, "agent", httpClient)
+
+				httpResponse, err := httpClient.Get(serverURL + "/agent")
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(httpResponse.StatusCode).To(Equal(404))
+			})
+		})
+	})
+
+	Describe("GET /blobs", func() {
+		It("returns data from file system", func() {
+			fs.WriteToFile("/var/vcap/micro_bosh/data/cache/123-456-789", "Some data")
+
+			httpResponse, err := httpClient.Get(serverURL + "/blobs/a5/123-456-789")
+			for err != nil {
+				httpResponse, err = httpClient.Get(serverURL + "/blobs/a5/123-456-789")
+			}
+			defer httpResponse.Body.Close()
+
+			httpBody, readErr := ioutil.ReadAll(httpResponse.Body)
+			Expect(readErr).ToNot(HaveOccurred())
+			Expect(httpResponse.StatusCode).To(Equal(200))
+			Expect(httpBody).To(Equal([]byte("Some data")))
+		})
+
+		Context("when incorrect http method is used", func() {
+			It("returns a 404", func() {
+				waitForServerToStart(serverURL, "blobs", httpClient)
+
+				postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
+				postPayload := strings.NewReader(postBody)
+
+				httpResponse, err := httpClient.Post(serverURL+"/blobs/123", "application/json", postPayload)
+				defer httpResponse.Body.Close()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(httpResponse.StatusCode).To(Equal(404))
+			})
+		})
+
+		Context("when file does not exist", func() {
+			It("returns a 404", func() {
+				waitForServerToStart(serverURL, "blobs", httpClient)
+
+				httpResponse, err := httpClient.Get(serverURL + "/blobs/123")
+				defer httpResponse.Body.Close()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(httpResponse.StatusCode).To(Equal(404))
+			})
+		})
+	})
+
+	Describe("PUT /blobs", func() {
+		It("updates the blob on the file system", func() {
+			fs.WriteToFile("/var/vcap/micro_bosh/data/cache/123-456-789", "Some data")
+
+			putBody := `Updated data`
+			putPayload := strings.NewReader(putBody)
+
+			waitForServerToStart(serverURL, "blobs", httpClient)
+
+			request, err := http.NewRequest("PUT", serverURL+"/blobs/a5/123-456-789", putPayload)
+			Expect(err).ToNot(HaveOccurred())
+
+			httpResponse, err := httpClient.Do(request)
+			defer httpResponse.Body.Close()
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(httpResponse.StatusCode).To(Equal(201))
+			contents, err := fs.ReadFile("/var/vcap/micro_bosh/data/cache/123-456-789")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(contents).To(Equal("Updated data"))
+		})
+
+		Context("when manager errors", func() {
+			It("returns a 500", func() {
+				fs.WriteToFileError = errors.New("oops")
+
+				putBody := `Updated data`
+				putPayload := strings.NewReader(putBody)
+
+				waitForServerToStart(serverURL, "blobs", httpClient)
+
+				request, err := http.NewRequest("PUT", serverURL+"/blobs/a5/123-456-789", putPayload)
+				Expect(err).ToNot(HaveOccurred())
+
+				httpResponse, err := httpClient.Do(request)
+				defer httpResponse.Body.Close()
+				Expect(httpResponse.StatusCode).To(Equal(500))
+
+				responseBody, err := ioutil.ReadAll(httpResponse.Body)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(responseBody)).To(ContainSubstring("oops"))
+			})
+		})
+	})
+
+	Describe("routing and auth", func() {
+		Context("when an incorrect uri is specificed", func() {
+			It("returns a 404", func() {
+				postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
+				postPayload := strings.NewReader(postBody)
+				httpResponse, err := httpClient.Post(serverURL+"/bad_url", "application/json", postPayload)
+				defer httpResponse.Body.Close()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(httpResponse.StatusCode).To(Equal(404))
+			})
+		})
+
+		Context("when an incorrect username/password was provided", func() {
+			It("returns a 401", func() {
+				postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
+				postPayload := strings.NewReader(postBody)
+				httpResponse, err := httpClient.Post(strings.Replace(serverURL, "pass", "wrong", -1)+"/agent", "application/json", postPayload)
+				defer httpResponse.Body.Close()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(httpResponse.StatusCode).To(Equal(401))
+				Expect(httpResponse.Header.Get("WWW-Authenticate")).To(Equal(`Basic realm=""`))
+			})
+		})
+	})
+})
+
+func waitForServerToStart(serverURL string, endpoint string, httpClient http.Client) (httpResponse *http.Response) {
+	postBody := `{"method":"ping","arguments":["foo","bar"], "reply_to": "reply to me!"}`
+	postPayload := strings.NewReader(postBody)
+
+	httpResponse, err := httpClient.Post(serverURL+"/"+endpoint, "application/json", postPayload)
+	for err != nil {
+		httpResponse, err = httpClient.Post(serverURL+"/"+endpoint, "application/json", postPayload)
+	}
+	defer httpResponse.Body.Close()
+	return
 }
