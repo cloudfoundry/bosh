a44f219834ee4ade3b6ff86fef49323260f5e50b
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2014-02-18 17:23:42 -0800

    Refactor DirectorUuid to use new `bosh status --uuid`   * previously parsed full `bosh status` output for uuid

--- bosh-dev/lib/bosh/dev/bat/director_uuid.rb
+++ bosh-dev/lib/bosh/dev/bat/director_uuid.rb
@@ -2,19 +2,12 @@ require 'bosh/dev'
 
 module Bosh::Dev::Bat
   class DirectorUuid
-    class UnknownUuidError < RuntimeError; end
-
     def initialize(cli_session)
       @cli_session = cli_session
     end
 
     def value
-      status_output = @cli_session.run_bosh('status')
-
-      matches = /UUID(\s)+(?<uuid>(\w+-)+\w+)/x.match(status_output)
-      raise UnknownUuidError, status_output unless matches
-
-      matches[:uuid]
+      @cli_session.run_bosh('status --uuid').strip
     end
   end
 end
--- bosh-dev/spec/bosh/dev/bat/director_uuid_spec.rb
+++ bosh-dev/spec/bosh/dev/bat/director_uuid_spec.rb
@@ -9,34 +9,22 @@ module Bosh::Dev::Bat
       let(:cli_session) { instance_double('Bosh::Dev::BoshCliSession') }
 
       context 'when we are able to extract uuid' do
-        before { cli_session.stub(:run_bosh).and_return(<<-OUTPUT) }
-Director
-  Name       bosh_micro01
-  URL        https://192.168.56.2:25555
-  Version    1.5.0.pre.896 (release:f694e3d2 bosh:f694e3d2)
-  User       admin
-  UUID       39be73f2-103a-4968-9645-9cd6406aedce
-  CPI        workstation
-  dns        enabled (domain_name: microbosh)
+        before { cli_session.stub(:run_bosh).with('status --uuid').and_return(<<-OUTPUT) }
+39be73f2-103a-4968-9645-9cd6406aedce
         OUTPUT
 
-        it 'fetches output via bosh status' do
-          cli_session.should_receive(:run_bosh).with('status')
-          subject.value
-        end
-
-        it 'returns fetched uuid via `bosh status`' do
+        it 'returns fetched uuid via `bosh status --uuid`' do
           expect(subject.value).to eq('39be73f2-103a-4968-9645-9cd6406aedce')
         end
       end
 
-      context 'when we are not able to extract uuid' do
-        before { cli_session.stub(:run_bosh).and_return('output') }
+      context 'when we are not able to obtain a uuid' do
+        before { cli_session.stub(:run_bosh).with('status --uuid') { Bosh::Core::Shell.new.run('false') } }
 
         it 'raises an error' do
           expect {
             subject.value
-          }.to raise_error(described_class::UnknownUuidError, /output/)
+          }.to raise_error
         end
       end
     end
