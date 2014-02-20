6b29cf9350e891621c5170e8d1414c98fa8d2729
Guillaume Berche <nobody@nobody.com>
2014-02-20 13:05:27 +0100

    Adding http proxy support to bosh director download_helper
    Added test on retried exceptions

--- bosh_cli_plugin_micro/spec/unit/bosh/deployer/instance_manager_spec.rb
+++ bosh_cli_plugin_micro/spec/unit/bosh/deployer/instance_manager_spec.rb
@@ -46,5 +46,11 @@ describe Bosh::Deployer::InstanceManager do
       expect(described_class).to have_received(:new).
                                    with(config, 'fake-config-sha1', ui_messager, 'fake-plugin')
     end
+
+    it 'retries on HTTPClient::ConnectTimeoutError, HTTPClient::BadResponseError when waiting for agent and director' do
+      connect_exceptions = Bosh::Deployer::InstanceManager.const_get(:CONNECTION_EXCEPTIONS)
+      connect_exceptions.should include HTTPClient::ConnectTimeoutError
+      connect_exceptions.should include HTTPClient::BadResponseError
+    end
   end
 end
