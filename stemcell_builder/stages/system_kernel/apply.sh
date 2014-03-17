9c9333cea4a2f2d6b5564978ec1da317c8458724
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2014-03-17 12:31:38 -0700

    Update system_kernel stage to use kernel .debs
    Signed-off-by: caleb miles <cmiles@pivotallabs.com>

--- stemcell_builder/stages/system_kernel/apply.sh
+++ stemcell_builder/stages/system_kernel/apply.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#
-# Copyright (c) 2009-2012 VMware, Inc.
 
 set -e
 
@@ -8,12 +6,12 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-if [ `codename` == "lucid" ]
-then
-  variant="lts-backport-oneiric"
+mkdir -p $chroot/tmp
 
-  # Headers are needed for open-vm-tools
-  pkg_mgr install linux-image-virtual-${variant} linux-headers-virtual-${variant}
-else
-  pkg_mgr install linux-image-virtual
-fi
+cp $assets_dir/*.deb $chroot/tmp/
+
+run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.0.0-32_3.0.0-32.51~lucid1_all.deb"
+run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.0.0-32-generic_3.0.0-32.51~lucid1_amd64.deb"
+run_in_chroot $chroot "dpkg -i /tmp/linux-image-3.0.0-32-generic_3.0.0-32.51~lucid1_amd64.deb"
+
+rm $chroot/tmp/*.deb
