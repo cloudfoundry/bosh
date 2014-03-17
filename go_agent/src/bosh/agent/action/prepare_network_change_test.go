87e00c0064a72d89d152adbedbdf549bd01afd46
Adam Stegman <astegman@pivotallabs.com>
2014-03-17 15:49:07 -0700

    formatting prepare_network_change_test.go
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- go_agent/src/bosh/agent/action/prepare_network_change_test.go
+++ go_agent/src/bosh/agent/action/prepare_network_change_test.go
@@ -1,36 +1,34 @@
 package action_test
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	. "bosh/agent/action"
 	fakeplatform "bosh/platform/fakes"
 	boshsys "bosh/system"
-	. "github.com/onsi/ginkgo"
-	"github.com/stretchr/testify/assert"
 )
 
-func buildPrepareAction() (action PrepareNetworkChangeAction, fs boshsys.FileSystem) {
+func buildPrepareNetworkChangeAction() (PrepareNetworkChangeAction, boshsys.FileSystem) {
 	platform := fakeplatform.NewFakePlatform()
-	fs = platform.GetFs()
-	action = NewPrepareNetworkChange(platform)
-
-	return
+	return NewPrepareNetworkChange(platform), platform.GetFs()
 }
-func init() {
-	Describe("Testing with Ginkgo", func() {
-		It("prepare network change should be synchronous", func() {
-			action, _ := buildPrepareAction()
-			assert.False(GinkgoT(), action.IsAsynchronous())
-		})
-		It("prepare network change", func() {
 
-			action, fs := buildPrepareAction()
+func init() {
+	Describe("prepareNetworkChange", func() {
+		It("is synchronous", func() {
+			action, _ := buildPrepareNetworkChangeAction()
+			Expect(action.IsAsynchronous()).To(BeFalse())
+		})
+
+		It("removes the network rules file", func() {
+			action, fs := buildPrepareNetworkChangeAction()
 			fs.WriteFile("/etc/udev/rules.d/70-persistent-net.rules", []byte{})
 
 			resp, err := action.Run()
-
-			assert.NoError(GinkgoT(), err)
-			assert.Equal(GinkgoT(), "ok", resp)
-			assert.False(GinkgoT(), fs.FileExists("/etc/udev/rules.d/70-persistent-net.rules"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp).To(Equal("ok"))
+			Expect(fs.FileExists("/etc/udev/rules.d/70-persistent-net.rules")).To(BeFalse())
 		})
 	})
 }
