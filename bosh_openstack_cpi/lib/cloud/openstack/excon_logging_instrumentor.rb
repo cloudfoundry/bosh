acfee1fbd021a155c2a1b7fb28ab4bd6ae423ac5
Ruben Koster <rkoster@starkandwayne.com>
2014-03-18 21:50:26 +0100

    Added unit tests for .instrument

--- bosh_openstack_cpi/lib/cloud/openstack/excon_logging_instrumentor.rb
+++ bosh_openstack_cpi/lib/cloud/openstack/excon_logging_instrumentor.rb
@@ -2,7 +2,7 @@ module Bosh::OpenStackCloud
   class ExconLoggingInstrumentor
     def self.instrument(name, params = {}, &block)
       params = RedactedParams.new(params)
-      Bosh::Clouds::Config.logger.debug("#{name}  #{params}")
+      Bosh::Clouds::Config.logger.debug("#{name} #{params}")
       if block_given?
         yield
       end
