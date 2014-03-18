79a3935f65864581e3a0c6fb149afcfa51f309d8
Ruben Koster <rkoster@starkandwayne.com>
2014-03-18 21:50:26 +0100

    Added unit tests for .instrument

--- bosh_openstack_cpi/spec/unit/excon_logging_instrumentor_spec.rb
+++ bosh_openstack_cpi/spec/unit/excon_logging_instrumentor_spec.rb
@@ -1,25 +1,52 @@
 require "spec_helper"
 
 describe Bosh::OpenStackCloud::ExconLoggingInstrumentor do
-  describe "#redact_params" do
-    let(:secret) { "secret_string" }
+  describe ".instrument" do
+    let(:name) { "foo" }
+    let(:params) { { foo: "bar" } }
+    let(:logger) { instance_double("Logger") }
     subject(:redacted_params) {
-      Bosh::OpenStackCloud::ExconLoggingInstrumentor::RedactedParams.new(params)
+      Bosh::OpenStackCloud::ExconLoggingInstrumentor.instrument(name, params)
     }
 
-    context "when containing a password" do
-      let(:params) { {password: secret } }
-      it "redacts password" do
-        expect(redacted_params.to_s).to_not include secret
-      end
+    it "logs requests" do
+      expect(Bosh::Clouds::Config).to receive(:logger).and_return(logger)
+      expect(logger).to receive(:debug).with("#{name} #{params}")
+      subject
     end
 
-    context "when containing a authorization header" do
-      let(:params) { {headers: { 'Authorization' => secret } } }
-      it "redacts authorization" do
-        expect(redacted_params.to_s).to_not include secret
-      end
+    it "redacts params" do
+      expect(Bosh::OpenStackCloud::ExconLoggingInstrumentor::RedactedParams).
+        to receive(:new).with(params)
+      subject
     end
 
+    it "yields" do
+      expect{ |b| Bosh::OpenStackCloud::ExconLoggingInstrumentor.
+        instrument(name, params, &b) }.to yield_control
+    end
+  end
+
+  describe Bosh::OpenStackCloud::ExconLoggingInstrumentor::RedactedParams do
+    describe "#redact_params" do
+      let(:secret) { "secret_string" }
+      subject(:redacted_params) {
+        Bosh::OpenStackCloud::ExconLoggingInstrumentor::RedactedParams.new(params)
+      }
+
+      context "when containing a password" do
+        let(:params) { {password: secret } }
+        it "redacts password" do
+          expect(redacted_params.to_s).to_not include secret
+        end
+      end
+
+      context "when containing a authorization header" do
+        let(:params) { {headers: { 'Authorization' => secret } } }
+        it "redacts authorization" do
+          expect(redacted_params.to_s).to_not include secret
+        end
+      end
+    end
   end
 end
