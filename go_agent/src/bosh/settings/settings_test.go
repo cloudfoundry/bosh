268a840160859a742cd6e1acef4a3420fd5a768c
Adam Stegman <astegman@pivotallabs.com>
2014-03-27 14:01:11 -0700

    format settings_test.go
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- go_agent/src/bosh/settings/settings_test.go
+++ go_agent/src/bosh/settings/settings_test.go
@@ -1,107 +1,110 @@
 package settings_test
 
 import (
-	. "bosh/settings"
 	. "github.com/onsi/ginkgo"
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/gomega"
+
+	. "bosh/settings"
 )
 
 func init() {
-	Describe("Testing with Ginkgo", func() {
-		It("default network for when networks is empty", func() {
+	Describe("Networks", func() {
+		Describe("DefaultNetworkFor", func() {
+			It("when networks is empty", func() {
+				networks := Networks{}
+				_, found := networks.DefaultNetworkFor("dns")
+				Expect(found).To(BeFalse())
+			})
 
-			networks := Networks{}
+			It("with single network", func() {
+				networks := Networks{
+					"bosh": Network{
+						Dns: []string{"xx.xx.xx.xx"},
+					},
+				}
 
-			_, found := networks.DefaultNetworkFor("dns")
-			assert.False(GinkgoT(), found)
+				settings, found := networks.DefaultNetworkFor("dns")
+				Expect(found).To(BeTrue())
+				Expect(settings).To(Equal(networks["bosh"]))
+			})
+
+			It("with multiple networks and default is found for dns", func() {
+				networks := Networks{
+					"bosh": Network{
+						Default: []string{"dns"},
+						Dns:     []string{"xx.xx.xx.xx", "yy.yy.yy.yy", "zz.zz.zz.zz"},
+					},
+					"vip": Network{
+						Default: []string{},
+						Dns:     []string{"aa.aa.aa.aa"},
+					},
+				}
+
+				settings, found := networks.DefaultNetworkFor("dns")
+				Expect(found).To(BeTrue())
+				Expect(settings).To(Equal(networks["bosh"]))
+			})
+
+			It("with multiple networks and default is not found", func() {
+				networks := Networks{
+					"bosh": Network{
+						Default: []string{"foo"},
+						Dns:     []string{"xx.xx.xx.xx", "yy.yy.yy.yy", "zz.zz.zz.zz"},
+					},
+					"vip": Network{
+						Default: []string{},
+						Dns:     []string{"aa.aa.aa.aa"},
+					},
+				}
+
+				_, found := networks.DefaultNetworkFor("dns")
+				Expect(found).To(BeFalse())
+			})
 		})
-		It("default network for with single network", func() {
 
-			networks := Networks{
-				"bosh": Network{
-					Dns: []string{"xx.xx.xx.xx"},
-				},
-			}
+		Describe("DefaultIp", func() {
+			It("with two networks", func() {
+				networks := Networks{
+					"bosh": Network{
+						Ip: "xx.xx.xx.xx",
+					},
+					"vip": Network{
+						Ip: "aa.aa.aa.aa",
+					},
+				}
 
-			settings, found := networks.DefaultNetworkFor("dns")
-			assert.True(GinkgoT(), found)
-			assert.Equal(GinkgoT(), settings, networks["bosh"])
-		})
-		It("default network for with multiple networks and default is found for dns", func() {
+				ip, found := networks.DefaultIp()
+				Expect(found).To(BeTrue())
+				Expect(ip).To(Equal("xx.xx.xx.xx"))
+			})
 
-			networks := Networks{
-				"bosh": Network{
-					Default: []string{"dns"},
-					Dns:     []string{"xx.xx.xx.xx", "yy.yy.yy.yy", "zz.zz.zz.zz"},
-				},
-				"vip": Network{
-					Default: []string{},
-					Dns:     []string{"aa.aa.aa.aa"},
-				},
-			}
+			It("with two networks only with defaults", func() {
+				networks := Networks{
+					"bosh": Network{
+						Ip: "xx.xx.xx.xx",
+					},
+					"vip": Network{
+						Ip:      "aa.aa.aa.aa",
+						Default: []string{"dns"},
+					},
+				}
 
-			settings, found := networks.DefaultNetworkFor("dns")
-			assert.True(GinkgoT(), found)
-			assert.Equal(GinkgoT(), settings, networks["bosh"])
-		})
-		It("default network for with multiple networks and default is not found", func() {
+				ip, found := networks.DefaultIp()
+				Expect(found).To(BeTrue())
+				Expect(ip).To(Equal("aa.aa.aa.aa"))
+			})
 
-			networks := Networks{
-				"bosh": Network{
-					Default: []string{"foo"},
-					Dns:     []string{"xx.xx.xx.xx", "yy.yy.yy.yy", "zz.zz.zz.zz"},
-				},
-				"vip": Network{
-					Default: []string{},
-					Dns:     []string{"aa.aa.aa.aa"},
-				},
-			}
+			It("when none specified", func() {
+				networks := Networks{
+					"bosh": Network{},
+					"vip": Network{
+						Default: []string{"dns"},
+					},
+				}
 
-			_, found := networks.DefaultNetworkFor("dns")
-			assert.False(GinkgoT(), found)
-		})
-		It("default ip with two networks", func() {
-
-			networks := Networks{
-				"bosh": Network{
-					Ip: "xx.xx.xx.xx",
-				},
-				"vip": Network{
-					Ip: "aa.aa.aa.aa",
-				},
-			}
-
-			ip, found := networks.DefaultIp()
-			assert.True(GinkgoT(), found)
-			assert.Equal(GinkgoT(), "xx.xx.xx.xx", ip)
-		})
-		It("default ip with two networks only with defaults", func() {
-
-			networks := Networks{
-				"bosh": Network{
-					Ip: "xx.xx.xx.xx",
-				},
-				"vip": Network{
-					Ip:      "aa.aa.aa.aa",
-					Default: []string{"dns"},
-				},
-			}
-
-			ip, found := networks.DefaultIp()
-			assert.True(GinkgoT(), found)
-			assert.Equal(GinkgoT(), "aa.aa.aa.aa", ip)
-		})
-		It("default ip when none specified", func() {
-
-			networks := Networks{
-				"bosh": Network{},
-				"vip": Network{
-					Default: []string{"dns"},
-				},
-			}
-
-			_, found := networks.DefaultIp()
-			assert.False(GinkgoT(), found)
+				_, found := networks.DefaultIp()
+				Expect(found).To(BeFalse())
+			})
 		})
 	})
 }
