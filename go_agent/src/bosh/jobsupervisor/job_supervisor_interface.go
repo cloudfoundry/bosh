779a0a9b7730d3f1f6e477b25e3396541efbc31d
dmitriy kalinin <dmitriy@pivotallabs.com>
2014-04-01 12:32:46 -0700

    note that Start/Stop command should still work after Unmonitor command is executed

--- go_agent/src/bosh/jobsupervisor/job_supervisor_interface.go
+++ go_agent/src/bosh/jobsupervisor/job_supervisor_interface.go
@@ -12,6 +12,11 @@ type JobSupervisor interface {
 	// Actions taken on all services
 	Start() error
 	Stop() error
+
+	// Start and Stop should still function after Unmonitor.
+	// Calling Start after Unmonitor should re-monitor all jobs.
+	// Calling Stop after Unmonitor should not re-monitor all jobs.
+	// (Monit complies to above requirements.)
 	Unmonitor() error
 
 	Status() string
