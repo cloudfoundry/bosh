a181fa9c0f1b2e287807c2efff2f8168d2e2a594
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2014-04-01 09:31:54 -0700

    Copy resolv.conf from stemcell building VM into chroot for stemcell builds.

--- stemcell_builder/stages/untar_base_os_image/apply.sh
+++ stemcell_builder/stages/untar_base_os_image/apply.sh
@@ -7,3 +7,5 @@ source $base_dir/lib/prelude_apply.bash
 
 # the tar_base_os_image stage creates a tar ball of the chroot with no enclosing directory
 tar zxf $os_image_tgz -C $chroot
+
+cp /etc/resolv.conf $chroot/etc/resolv.conf
