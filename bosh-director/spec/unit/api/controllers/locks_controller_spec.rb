c965ff26d0e44573440e681f2cc96b345ffe8b4e
Maria Shaldibina <mariash@pivotallabs.com>
2014-04-04 11:57:52 -0700

    Refactor locks unit tests
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- bosh-director/spec/unit/api/controllers/locks_controller_spec.rb
+++ bosh-director/spec/unit/api/controllers/locks_controller_spec.rb
@@ -13,13 +13,13 @@ module Bosh::Director
     let(:redis) { double('Redis') }
 
     before do
-      BD::Config.stub(:redis).and_return(redis)
+      allow(BD::Config).to receive(:redis).and_return(redis)
     end
 
     context 'authenticated access' do
       before { authorize 'admin', 'admin' }
 
-      before { redis.should_receive(:keys).with('lock:*').and_return(locks) }
+      before { allow(redis).to receive(:keys).with('lock:*').and_return(locks) }
 
       context 'when there are not any locks' do
         let(:locks) { [] }
@@ -29,8 +29,7 @@ module Bosh::Director
           expect(last_response.status).to eq 200
 
           body = Yajl::Parser.parse(last_response.body)
-          expect(body).to be_kind_of(Array)
-          expect(body.size).to eql(locks.size)
+          expect(body).to eq([])
         end
       end
 
@@ -43,12 +42,12 @@ module Bosh::Director
             'lock:compile:test-package:test-stemcell'
           ]
         }
-        let(:lock_timeout) { Time.now.to_f }
+        let(:lock_timeout) { Time.now.to_f.to_s }
         let(:lock_id) { SecureRandom.uuid }
 
         before do
           locks.each do |lock|
-            redis.should_receive(:get).with(lock).and_return("#{lock_timeout}:#{lock_id}")
+            allow(redis).to receive(:get).with(lock).and_return("#{lock_timeout}:#{lock_id}")
           end
         end
 
@@ -57,8 +56,12 @@ module Bosh::Director
           expect(last_response.status).to eq 200
 
           body = Yajl::Parser.parse(last_response.body)
-          expect(body).to be_kind_of(Array)
-          expect(body.size).to eql(locks.size)
+          expect(body).to eq([
+            { 'type' => 'deployment', 'resource' => %w(test-deployment), 'timeout' => lock_timeout },
+            { 'type' => 'stemcells', 'resource' => %w(test-stemcell 1), 'timeout' => lock_timeout },
+            { 'type' => 'release', 'resource' => %w(test-release), 'timeout' => lock_timeout },
+            { 'type' => 'compile', 'resource' => %w(test-package test-stemcell), 'timeout' => lock_timeout },
+          ])
         end
       end
     end
--- bosh_cli/spec/unit/commands/locks_spec.rb
+++ bosh_cli/spec/unit/commands/locks_spec.rb
@@ -6,28 +6,26 @@ require 'cli'
 
 describe Bosh::Cli::Command::Locks do
   let(:command) { described_class.new }
-  let(:director) { double(Bosh::Cli::Client::Director) }
+  let(:director) { instance_double('Bosh::Cli::Client::Director') }
   let(:target) { 'http://example.org' }
 
   before(:each) do
-    command.stub(:director).and_return(director)
-    command.stub(:nl)
-    command.stub(:logged_in? => true)
+    allow(command).to receive(:director).and_return(director)
+    allow(command).to receive(:nl)
+    allow(command).to receive(:logged_in?).and_return(true)
     command.options[:target] = target
   end
 
-  describe :list do
+  describe 'list' do
     before do
-      director.should_receive(:list_locks).and_return(locks)
+      allow(director).to receive(:list_locks).and_return(locks)
     end
 
     context 'when there are not any locks' do
       let(:locks) { [] }
 
       it 'should raise a Cli Error' do
-        expect do
-          command.locks
-        end.to raise_error(Bosh::Cli::CliError, 'No locks')
+        expect { command.locks }.to raise_error(Bosh::Cli::CliError, 'No locks')
       end
     end
 
@@ -35,22 +33,20 @@ describe Bosh::Cli::Command::Locks do
       let(:lock_timeout) { Time.now.to_i }
       let(:locks) {
         [
-          { 'type'  => 'deployment', 'resource' => ['test-deployment'],               'timeout' => lock_timeout },
-          { 'type'  => 'stemcells',  'resource' => ['test-stemcell', '1'],            'timeout' => lock_timeout },
-          { 'type'  => 'release',    'resource' => ['test-release'],                  'timeout' => lock_timeout },
-          { 'type'  => 'compile',    'resource' => ['test-package', 'test-stemcell'], 'timeout' => lock_timeout },
+          { 'type'  => 'deployment', 'resource' => %w(test-deployment),               'timeout' => lock_timeout },
+          { 'type'  => 'stemcells',  'resource' => %w(test-stemcell 1),            'timeout' => lock_timeout },
+          { 'type'  => 'release',    'resource' => %w(test-release),                  'timeout' => lock_timeout },
+          { 'type'  => 'compile',    'resource' => %w(test-package test-stemcell), 'timeout' => lock_timeout },
         ]
       }
 
       it 'should list current locks' do
-        command.should_receive(:say) do |s|
-          locks.each do |lock|
-            expect(s.to_s).to include lock['type']
-            expect(s.to_s).to include lock['resource'].join(':')
-          end
+        expect(command).to receive(:say) do |s|
+          expect(locks).to be_all { |lock| s.to_s.include?(lock['type']) }
+          expect(locks).to be_all { |lock| s.to_s.include?(lock['resource'].join(':')) }
           expect(s.to_s).to include Time.at(lock_timeout).utc.to_s
         end
-        command.should_receive(:say).with("Locks total: #{locks.size}")
+        expect(command).to receive(:say).with("Locks total: #{locks.size}")
 
         command.locks
       end
