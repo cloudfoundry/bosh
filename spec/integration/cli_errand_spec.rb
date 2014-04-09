2be827a0d2af570e7856211f00b798b247a461c8
Michael Maximilien <maxim@us.ibm.com>
2014-04-09 16:25:52 -0700

    make all cli_errand integration tests consistent
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- spec/integration/cli_errand_spec.rb
+++ spec/integration/cli_errand_spec.rb
@@ -205,11 +205,10 @@ describe 'cli: errand', type: :integration do
   end
 
   context 'when deploying with insufficient resources for all errands' do
-    with_reset_sandbox_before_each
+    with_reset_sandbox_before_all
 
-    it 'returns 1 as exit code and mentions insufficient resources' do
+    before(:all) do
       manifest_hash = Bosh::Spec::Deployments.simple_manifest
-
       manifest_hash['resource_pools'].first['size'] += 1
 
       # Errand with sufficient resources
@@ -239,9 +238,12 @@ describe 'cli: errand', type: :integration do
       upload_stemcell
       set_deployment(manifest_hash: manifest_hash)
 
-      output, exit_code = deploy(failure_expected: true, return_exit_code: true)
-      expect(output).to include("Resource pool `a' is not big enough: 5 VMs needed, capacity is 4")
-      expect(exit_code).to eq(1)
+      @output, @exit_code = deploy(failure_expected: true, return_exit_code: true)
+    end
+
+    it 'returns 1 as exit code and mentions insufficient resources' do
+      expect(@output).to include("Resource pool `a' is not big enough: 5 VMs needed, capacity is 4")
+      expect(@exit_code).to eq(1)
     end
   end
 
