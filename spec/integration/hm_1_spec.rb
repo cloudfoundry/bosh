42f18167d9dc6fc822fde5799c0ed1f4097d1295
dmitriy kalinin <dmitriy@pivotallabs.com>
2014-04-09 09:35:51 -0700

    consolidated hm1,2 integration tests to be a bit speedier

--- spec/integration/hm_1_spec.rb
+++ spec/integration/hm_1_spec.rb
@@ -6,14 +6,12 @@ describe 'health_monitor: 1', type: :integration do
   before { current_sandbox.health_monitor_process.start }
   after { current_sandbox.health_monitor_process.stop }
 
-  before do
+  # ~50s
+  it 'HM can be queried for stats' do
     deployment_hash = Bosh::Spec::Deployments.simple_manifest
     deployment_hash['jobs'][0]['instances'] = 1
     deploy_simple(manifest_hash: deployment_hash)
-  end
 
-  # ~50s
-  it 'HM can be queried for stats' do
     varz = {}
     20.times do
       varz_json = RestClient.get("http://admin:admin@localhost:#{current_sandbox.hm_port}/varz")
@@ -26,19 +24,50 @@ describe 'health_monitor: 1', type: :integration do
     expect(varz['agents_count']).to_not eq(0)
   end
 
-  # ~1m20s
-  it 'resurrects stateless nodes' do
-    original_vm = director.vm('foobar/0')
-    original_vm.kill_agent
-    resurrected_vm = director.wait_for_vm('foobar/0', 300)
-    expect(resurrected_vm.cid).to_not eq(original_vm.cid)
+  # ~4m
+  it 'only resurrects stateless nodes that are configured to be resurrected' do
+    deployment_hash = Bosh::Spec::Deployments.simple_manifest
+    deployment_hash['jobs'][0]['instances'] = 2
+    deploy_simple(manifest_hash: deployment_hash)
+
+    run_bosh('vm resurrection foobar 1 off')
+
+    original_0_vm = director.vm('foobar/0')
+    original_1_vm = director.vm('foobar/1')
+
+    # Kill VMs as close as possible
+    original_0_vm.kill_agent
+    original_1_vm.kill_agent
+
+    new_0_vm = director.wait_for_vm('foobar/0', 150)
+    expect(new_0_vm.cid).to_not eq(original_0_vm.cid)
+
+    # Since at this point 0th VM is back up, assume that
+    # if 1st VM would be resurrected it would've already happened
+    # (i.e do not wait for long time)
+    new_1_vm = director.wait_for_vm('foobar/1', 10)
+    expect(new_1_vm).to be_nil
   end
 
-  # ~8m
-  it 'does not resurrect stateless nodes when paused' do
-    run_bosh('vm resurrection foobar 0 off')
-    original_vm = director.vm('foobar/0')
-    original_vm.kill_agent
-    expect(director.wait_for_vm('foobar/0', 300)).to be_nil
+  # ~3m
+  it 'resurrects vms that were down before resurrector started' do
+    # Turn resurrector off
+    current_sandbox.reconfigure_health_monitor('health_monitor_without_resurrector.yml.erb')
+
+    deployment_hash = Bosh::Spec::Deployments.simple_manifest
+    deployment_hash['jobs'][0]['instances'] = 2
+    deploy_simple(manifest_hash: deployment_hash)
+
+    director.vm('foobar/0').kill_agent
+    director.vm('foobar/1').kill_agent
+
+    _, exit_code = run_bosh('cck --report', failure_expected: true, return_exit_code: true)
+    expect(exit_code).to eq(1)
+
+    # Turn resurrector back on
+    current_sandbox.reconfigure_health_monitor('health_monitor.yml.erb')
+
+    expect(director.wait_for_vm('foobar/0', 150)).to_not be_nil
+    expect(director.wait_for_vm('foobar/1', 150)).to_not be_nil
   end
 end
--- spec/integration/hm_2_spec.rb
+++ spec/integration/hm_2_spec.rb
@@ -13,8 +13,9 @@ describe 'health_monitor: 2', type: :integration do
     deployment_hash['jobs'][0]['persistent_disk'] = 20_480
     deploy_simple(manifest_hash: deployment_hash)
 
+    # wait_for_vm will wait here maximum amount of time!
     director.vm('foobar/0').kill_agent
-    expect(director.wait_for_vm('foobar/0', 300)).to be_nil
+    expect(director.wait_for_vm('foobar/0', 150)).to be_nil
   end
 
   # ~2m
@@ -29,31 +30,7 @@ describe 'health_monitor: 2', type: :integration do
 
     original_vm = director.vm('foobar/0')
     original_vm.kill_agent
-    resurrected_vm = director.wait_for_vm('foobar/0', 300)
+    resurrected_vm = director.wait_for_vm('foobar/0', 150)
     expect(resurrected_vm.cid).to_not eq(original_vm.cid)
-  end
-
-  context 'when there are open problems before resurrector starts' do
-    # ~3m
-    it 'resolves the problems' do
-      # Turn resurrector off
-      current_sandbox.reconfigure_health_monitor('health_monitor_without_resurrector.yml.erb')
-
-      deployment_hash = Bosh::Spec::Deployments.simple_manifest
-      deployment_hash['jobs'][0]['instances'] = 2
-      deploy_simple(manifest_hash: deployment_hash)
-
-      director.vm('foobar/0').kill_agent
-      director.vm('foobar/1').kill_agent
-
-      _, exit_code = run_bosh('cck --report', failure_expected: true, return_exit_code: true)
-      expect(exit_code).to eq(1)
-
-      # Turn resurrector back on
-      current_sandbox.reconfigure_health_monitor('health_monitor.yml.erb')
-
-      expect(director.wait_for_vm('foobar/0', 300)).to_not be_nil
-      expect(director.wait_for_vm('foobar/1', 300)).to_not be_nil
-    end
   end
 end
