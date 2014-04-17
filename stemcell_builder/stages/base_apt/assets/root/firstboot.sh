e448b0e8b0967288488c929fbbf953b22a046d1d
caleb miles <cmiles@pivotallabs.com>
2014-04-17 12:35:15 -0700

    Restart network for Ubuntu 14.04
    /etc/init.d/networking does not restart network in Ubuntu 14.04
    
    Signed-off-by: Maria Shaldibina <mariash@pivotallabs.com>

--- stemcell_builder/stages/base_apt/assets/root/firstboot.sh
+++ stemcell_builder/stages/base_apt/assets/root/firstboot.sh
@@ -2,9 +2,17 @@
 
 # ubuntu trusty+ needs /etc/resolv.conf to be a symlink, so delete contents
 # instead of removing the file to preserve the link
-cat /dev/null > /etc/resolv.conf
+> /etc/resolv.conf
 rm /etc/ssh/ssh_host*key*
 
-/etc/init.d/networking restart
+DISTRIB_CODENAME=$(lsb_release --codename | cut -f2)
+if [ $DISTRIB_CODENAME == "trusty" ]; then
+  ifdown -a --no-loopback
+  ifup -a --no-loopback
+else
+  ifdown -a --exclude=lo
+  ifup -a --exclude=lo
+fi
+
 dpkg-reconfigure -fnoninteractive -pcritical openssh-server
 dpkg-reconfigure -fnoninteractive sysstat
