5494a93fa6d505331547ed2fe707ebfd4237d32c
Maria Shaldibina <mariash@pivotallabs.com>
2014-04-25 14:00:50 -0700

    Add tests for vsphere datacenter folders
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- bosh_vsphere_cpi/spec/integration/vsphere_cpi_spec.rb
+++ bosh_vsphere_cpi/spec/integration/vsphere_cpi_spec.rb
@@ -7,6 +7,10 @@ describe VSphereCloud::Cloud do
   let(:bin_path) { File.expand_path('../../../bin/vsphere_cpi', __FILE__) }
   let(:db_path) { Tempfile.new('vsphere_cpi.db').path }
 
+  let(:host) { ENV.fetch('BOSH_VSPHERE_CPI_HOST') }
+  let(:user) { ENV.fetch('BOSH_VSPHERE_CPI_USER') }
+  let(:password) { ENV.fetch('BOSH_VSPHERE_CPI_PASSWORD', '') }
+  let(:datacenter_name) { ENV.fetch('BOSH_VSPHERE_CPI_DATACENTER', 'BOSH_DC') }
   let(:config) do
     {
       'db' => {
@@ -18,16 +22,16 @@ describe VSphereCloud::Cloud do
             'ntp' => ['10.80.0.44'],
           },
           'vcenters' => [{
-            'host' => ENV.fetch('BOSH_VSPHERE_CPI_HOST'),
-            'user' => ENV.fetch('BOSH_VSPHERE_CPI_USER'),
-            'password' => ENV.fetch('BOSH_VSPHERE_CPI_PASSWORD', ''),
+            'host' => host,
+            'user' => user,
+            'password' => password,
             'datacenters' => [{
-              'name' => ENV.fetch('BOSH_VSPHERE_CPI_DATACENTER', 'BOSH_DC'),
-              'vm_folder' => 'ACCEPTANCE_BOSH_VMs',
-              'template_folder' => 'ACCEPTANCE_BOSH_Templates',
-              'disk_path' => 'ACCEPTANCE_BOSH_Disks',
-              'datastore_pattern' => 'jalapeno',
-              'persistent_datastore_pattern' => 'jalapeno',
+              'name' => datacenter_name,
+              'vm_folder' => ENV.fetch('BOSH_VSPHERE_CPI_VM_FOLDER', 'ACCEPTANCE_BOSH_VMs'),
+              'template_folder' => ENV.fetch('BOSH_VSPHERE_CPI_TEMPLATE_FOLDER', 'ACCEPTANCE_BOSH_Templates'),
+              'disk_path' => ENV.fetch('BOSH_VSPHERE_CPI_DISK_PATH', 'ACCEPTANCE_BOSH_Disks'),
+              'datastore_pattern' => ENV.fetch('BOSH_VSPHERE_CPI_DATASTORE_PATTERN', 'jalapeno'),
+              'persistent_datastore_pattern' => ENV.fetch('BOSH_VSPHERE_CPI_PERSISTENT_DATASTORE_PATTERN', 'jalapeno'),
               'allow_mixed_datastores' => true,
               'clusters' => [{
                 'BOSH_CL' => { 'resource_pool' => 'ACCEPTANCE_RP' },
@@ -39,14 +43,12 @@ describe VSphereCloud::Cloud do
     }
   end
 
-  let(:json) { { 'method' => 'ping', 'arguments' => [], 'context' => { 'director_uuid' => 'abc' } } }
-
   before do
     @config_path = Tempfile.new('vsphere_cpi_config').path
     File.open(@config_path, 'w') { |f| f.write(YAML.dump(config)) }
   end
 
-  def run_vsphere_cpi
+  def run_vsphere_cpi(json)
     stdin, stdout, stderr, exit_status = Open3.popen3(bin_path, @config_path)
     stdin.puts(JSON.dump(json))
     stdin.close
@@ -56,16 +58,17 @@ describe VSphereCloud::Cloud do
   end
 
   describe 'running commands' do
+    let(:json) { { 'method' => 'ping', 'arguments' => [], 'context' => { 'director_uuid' => 'abc' } } }
+
     it 'ping-pongs' do
-      output = run_vsphere_cpi
+      output = run_vsphere_cpi(json)
       expect(output).to eq('{"result":"pong","error":null}')
     end
   end
 
   describe 'migrations' do
     it 'runs migrations on database from config' do
-      run_vsphere_cpi
-
+      run_vsphere_cpi({})
       db = Sequel.sqlite(database: db_path)
       result = db["SELECT * FROM schema_migrations"]
       expect(result.count).to be > 0
--- bosh_vsphere_cpi/spec/unit/cloud/vsphere/client_spec.rb
+++ bosh_vsphere_cpi/spec/unit/cloud/vsphere/client_spec.rb
@@ -3,17 +3,18 @@ require 'cloud/vsphere/client'
 
 module VSphereCloud
   describe Client do
+    subject(:client) { Client.new('http://www.example.com') }
+
+    let(:fake_search_index) { double }
+    let(:fake_service_content) { double('service content', root_folder: double('fake-root-folder')) }
+
+    before do
+      fake_instance = double('service instance', content: fake_service_content)
+      VimSdk::Vim::ServiceInstance.stub(new: fake_instance)
+      fake_service_content.stub(search_index: fake_search_index)
+    end
+
     describe '#find_by_inventory_path' do
-      subject(:client) { Client.new('http://www.example.com') }
-
-      let(:fake_search_index) { double }
-      before do
-        fake_service_content = double('service content')
-        fake_instance = double('service instance', content: fake_service_content)
-        VimSdk::Vim::ServiceInstance.stub(new: fake_instance)
-        fake_service_content.stub(search_index: fake_search_index)
-      end
-
       context 'given a string' do
         it 'passes the path to a SearchIndex object when path contains no slashes' do
           fake_search_index.should_receive(:find_by_inventory_path).with('foobar')
@@ -52,11 +53,54 @@ module VSphereCloud
           client.find_by_inventory_path(['foo', ['bar', 'baz/jaz']])
         end
       end
+    end
 
-      describe '#soap_stub' do
-        it 'returns the soap stub adapter' do
-          expect(client.soap_stub).to be_a(VimSdk::Soap::StubAdapter)
-        end
+    describe '#soap_stub' do
+      it 'returns the soap stub adapter' do
+        expect(client.soap_stub).to be_a(VimSdk::Soap::StubAdapter)
+      end
+    end
+
+    describe '#create_folder' do
+      it 'calls create folder on service content root folder' do
+        expect(fake_service_content.root_folder).to receive(:create_folder).with('fake-folder-name')
+        client.create_folder('fake-folder-name')
+      end
+    end
+
+    describe '#move_into_folder' do
+      let(:folder) { instance_double('VimSdk::Vim::Folder') }
+
+      it 'calls move_into on folder and waits for task' do
+        things_to_move = double('fake-things-to-move')
+        task = double('fake-task')
+
+        expect(folder).to receive(:move_into).with(things_to_move).and_return(task)
+        expect(client).to receive(:wait_for_task).with(task)
+        client.move_into_folder(folder, things_to_move)
+      end
+    end
+
+    describe '#move_into_root_folder' do
+      it 'moves into root folder and waits for task' do
+        things_to_move = double('fake-things-to-move')
+        task = double('fake-task')
+
+        expect(fake_service_content.root_folder).to receive(:move_into).with(things_to_move).and_return(task)
+        expect(client).to receive(:wait_for_task).with(task)
+        client.move_into_root_folder(things_to_move)
+      end
+    end
+
+    describe '#delete_folder' do
+      let(:folder) { instance_double('VimSdk::Vim::Folder') }
+
+      it 'calls destroy on folder and waits for task' do
+        task = double('fake-task')
+
+        expect(folder).to receive(:destroy).and_return(task)
+        expect(client).to receive(:wait_for_task).with(task)
+        client.delete_folder(folder)
       end
     end
   end
