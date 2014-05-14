8975328b5706d1681265d1d3daee8d560c7d7393
Maria Shaldibina <mariash@pivotallabs.com>
2014-05-13 17:13:35 -0700

    Add integration test for external cpi logs

--- spec/integration/external_cpi_spec.rb
+++ spec/integration/external_cpi_spec.rb
@@ -13,4 +13,15 @@ describe 'external CPIs', type: :integration do
     deploy_results = bosh_runner.run('task last --debug')
     expect(deploy_results).to include('External CPI sending request')
   end
+
+  it 'saves external CPI logs' do
+    current_sandbox.external_cpi_enabled = true
+    current_sandbox.reconfigure_director
+    current_sandbox.reconfigure_workers
+
+    expect(deploy_simple).to match /Task (\d+) done/
+
+    deploy_results = bosh_runner.run('task last --cpi')
+    expect(deploy_results).to include('Dummy: create_vm')
+  end
 end
