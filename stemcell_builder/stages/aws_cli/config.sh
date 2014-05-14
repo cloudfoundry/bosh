120b823d2e452ca2f8fccd8d2ca8776faf2f7093
Phan Le <ple@pivotallabs.com>
2014-05-14 14:31:47 -0700

    lock down s3cli to specific sha
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -5,9 +5,9 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_config.bash
 
-# download CLI source or release from github into assets directory
+# Download CLI source or release from github into assets directory
 cd $assets_dir
 rm -rf s3cli
-curl -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball
+curl -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball/a34ef740de9c4cc07bcd7bb7383bb8275298a9ab
 mkdir s3cli
 tar -xzf s3cli.tar.gz -C s3cli/ --strip-components 1
