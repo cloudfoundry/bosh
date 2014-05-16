d93893f9bc9cbcaa594cb7ea99c0eb210b171508
Phan Le <ple@pivotallabs.com>
2014-05-16 14:51:29 -0700

    Make the vsphere_cpi_spec thinner as we have it covered in the life cycle spec.
    Signed-off-by: caleb miles <cmiles@pivotallabs.com>

--- bosh_cpi/lib/bosh/cpi/tasks/spec.rake
+++ bosh_cpi/lib/bosh/cpi/tasks/spec.rake
@@ -2,20 +2,9 @@ require 'rspec'
 require 'rspec/core/rake_task'
 
 namespace :spec do
-  namespace :lifecycle do
-    desc 'Run VM lifecycle specs'
-    RSpec::Core::RakeTask.new(:internal_cpi) do |t|
-      t.pattern = 'spec/integration'
-      t.rspec_opts = %w(--format documentation --color --tag ~external_cpi)
-    end
-
-    RSpec::Core::RakeTask.new(:external_cpi) do |t|
-      t.pattern = 'spec/integration'
-      t.rspec_opts = %w(--format documentation --color --tag external_cpi)
-    end
+  desc 'Run VM lifecycle specs'
+  RSpec::Core::RakeTask.new(:lifecycle) do |t|
+    t.pattern = 'spec/integration'
+    t.rspec_opts = %w(--format documentation --color)
   end
-
-  # for temporary backwards compatibility, create a task called
-  # lifecycle which uses the internal CPI
-  task :lifecycle => ['lifecycle:internal_cpi']
 end
--- bosh_vsphere_cpi/spec/integration/vsphere_cpi_spec.rb
+++ bosh_vsphere_cpi/spec/integration/vsphere_cpi_spec.rb
@@ -30,9 +30,9 @@ describe VSphereCloud::Cloud, external_cpi: true do
   def config
     return @config if @config
     @db_path = Tempfile.new('vsphere_cpi.db').path
-    @datacenter_name          = ENV.fetch('BOSH_VSPHERE_CPI_DATACENTER', 'BOSH_DC')
-    @cluster                  = ENV.fetch('BOSH_VSPHERE_CPI_CLUSTER', 'BOSH_CL')
-    @second_cluster           = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_CLUSTER', 'BOSH_CL2')
+    datacenter_name          = ENV.fetch('BOSH_VSPHERE_CPI_DATACENTER', 'BOSH_DC')
+    cluster                  = ENV.fetch('BOSH_VSPHERE_CPI_CLUSTER', 'BOSH_CL')
+    second_cluster           = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_CLUSTER', 'BOSH_CL2')
 
     host                      = ENV.fetch('BOSH_VSPHERE_CPI_HOST')
     user                      = ENV.fetch('BOSH_VSPHERE_CPI_USER')
@@ -48,11 +48,11 @@ describe VSphereCloud::Cloud, external_cpi: true do
     template_folder_name = ENV.fetch('BOSH_VSPHERE_CPI_TEMPLATE_FOLDER', 'ACCEPTANCE_BOSH_Templates')
     disk_folder_name = ENV.fetch('BOSH_VSPHERE_CPI_DISK_PATH', 'ACCEPTANCE_BOSH_Disks')
 
-    prepare_tests_folder(client, @datacenter_name, vm_folder_name)
-    prepare_tests_folder(client, @datacenter_name, template_folder_name)
-    prepare_tests_folder(client, @datacenter_name, disk_folder_name)
+    prepare_tests_folder(client, datacenter_name, vm_folder_name)
+    prepare_tests_folder(client, datacenter_name, template_folder_name)
+    prepare_tests_folder(client, datacenter_name, disk_folder_name)
 
-    {
+    @config = {
       'db' => {
         'database' => @db_path,
         'adapter' => 'sqlite',
@@ -67,7 +67,7 @@ describe VSphereCloud::Cloud, external_cpi: true do
             'user' => user,
             'password' => password,
             'datacenters' => [{
-              'name' => @datacenter_name,
+              'name' => datacenter_name,
               'vm_folder' => "#{vm_folder_name}/lifecycle_tests",
               'template_folder' => "#{template_folder_name}/lifecycle_tests",
               'disk_path' => "#{disk_folder_name}/lifecycle_tests",
@@ -76,10 +76,10 @@ describe VSphereCloud::Cloud, external_cpi: true do
               'allow_mixed_datastores' => true,
               'clusters' => [
                 {
-                  @cluster => { 'resource_pool' => resource_pool_name },
+                  cluster => { 'resource_pool' => resource_pool_name },
                 },
                 {
-                  @second_cluster  => { 'resource_pool' => second_resource_pool_name }
+                  second_cluster  => { 'resource_pool' => second_resource_pool_name }
                 }
               ],
             }]
@@ -123,90 +123,6 @@ describe VSphereCloud::Cloud, external_cpi: true do
     response['result']
   end
 
-  let(:network_spec) do
-    {
-      'static' => {
-        'ip' => '169.254.1.1',
-        'netmask' => '255.255.254.0',
-        'cloud_properties' => { 'name' => vlan },
-        'default' => ['dns', 'gateway'],
-        'dns' => ['169.254.1.2'],
-        'gateway' => '169.254.1.3'
-      }
-    }
-  end
-  let(:vlan) { ENV['BOSH_VSPHERE_VLAN'] || raise('Missing BOSH_VSPHERE_VLAN') }
-
-  let(:original_resource_pool) {
-    {
-      'ram' => 1024,
-      'disk' => 2048,
-      'cpu' => 1,
-    }
-  }
-  let(:resource_pool) { original_resource_pool }
-
-  let(:disk_locality) { nil }
-
-  def it_exercises_vm_lifecycle
-    by 'creating vm' do
-      @vm_id = external_cpi_result(:create_vm,
-        'agent-007',
-        @stemcell_id,
-        resource_pool,
-        network_spec,
-        disk_locality,
-        {'key' => 'value'}
-      )
-
-      expect(@vm_id).to_not be_nil
-      expect(external_cpi_result(:has_vm, @vm_id)).to be(true)
-    end
-
-    and_by 'setting vm metadata' do
-      metadata = { deployment: 'deployment', job: 'cpi_spec', index: '0' }
-      external_cpi_result(:set_vm_metadata, @vm_id, metadata)
-    end
-
-    and_by 'creating disk' do
-      @disk_id = external_cpi_result(:create_disk, 2048, @vm_id)
-      expect(@disk_id).to_not be_nil
-    end
-
-    and_by 'attaching disk' do
-      external_cpi_result(:attach_disk, @vm_id, @disk_id)
-    end
-
-    and_by 'snapshotting disk' do
-      expect(
-        external_cpi_response(:snapshot_disk, @disk_id, {})['error']['type']
-      ).to eq('Bosh::Clouds::NotImplemented')
-    end
-
-    and_by 'deleting snapshot' do
-      expect(
-        external_cpi_response(:delete_snapshot, 123)['error']['type']
-      ).to eq('Bosh::Clouds::NotImplemented')
-    end
-
-    and_by 'detaching disk' do
-      external_cpi_result(:detach_disk, @vm_id, @disk_id)
-    end
-  end
-
-  describe 'running commands' do
-    it 'ping-pongs' do
-      json = {
-        'method' => 'ping',
-        'arguments' => [],
-        'context' => { 'director_uuid' => 'abc' }
-      }
-
-      output = run_vsphere_cpi(json)
-      expect(output).to match /{"result":"pong","error":null,"log":".*Request.*"}/
-    end
-  end
-
   describe 'migrations' do
     it 'runs migrations on database from config' do
       run_vsphere_cpi({})
@@ -217,65 +133,17 @@ describe VSphereCloud::Cloud, external_cpi: true do
     end
   end
 
-  describe 'lifecycle' do
-    before { reset_vm_and_disk }
-    after do
-      external_cpi_result(:delete_vm, @vm_id) if @vm_id
-      external_cpi_result(:delete_disk, @disk_id) if @disk_id
-      reset_vm_and_disk
-    end
-
-    def reset_vm_and_disk
-      @vm_id = nil
-      @disk_id = nil
-    end
-
-    it 'should exercise the vm lifecycle without existing disks' do
-      it_exercises_vm_lifecycle
-    end
-
-    context 'without existing disks and placer' do
-      let(:first_cluster) { { @cluster => {} } }
-      let(:second_cluster) { { @second_cluster => {} } }
-      let(:resource_pool) do
-        original_resource_pool.merge(
-          'datacenters' => [{ 'name' => @datacenter_name, 'clusters' => [cluster]}]
-        )
-      end
-
-      context 'when the first cluster is chosen' do
-        let(:cluster) { first_cluster }
-
-        it 'should exercise the vm lifecycle' do
-          it_exercises_vm_lifecycle do
-            vm = external_cpi_result(:get_vm_by_cid, @vm_id)
-            vm_info = external_cpi_result(get_vm_host_info, vm)
-            expect(vm_info['cluster']).to eq(@cluster)
-          end
-        end
-      end
-
-      context 'when the second cluster is chosen' do
-        let(:cluster) { second_cluster }
-
-        it 'should exercise the vm lifecycle' do
-          it_exercises_vm_lifecycle do
-            vm = external_cpi_result(:get_vm_by_cid, @vm_id)
-            vm_info = external_cpi_result(get_vm_host_info, vm)
-            expect(vm_info['cluster']).to eq(@second_cluster)
-          end
-        end
-      end
-    end
-
-    context 'with existing disks' do
-      let(:existing_volume_id) { external_cpi_result(:create_disk, 2048) }
-      let(:disk_locality) { [existing_volume_id] }
-      after { external_cpi_result(:delete_disk, existing_volume_id) if existing_volume_id }
-
-      it 'should exercise the vm lifecycle' do
-        it_exercises_vm_lifecycle
-      end
+  # Thin integration test so
+  # We have had coverage in the lifecycle_spec
+  describe 'getting vms' do
+    it 'is successful' do
+      json = {
+        'method' => 'has_vm',
+        'arguments' => ['1234567'],
+        'context' => { 'director_uuid' => 'abc' }
+      }
+      output = run_vsphere_cpi(json)
+      expect(output).to match /"result":false,"error":null,"log":".*Request.*"}/
     end
   end
 end
