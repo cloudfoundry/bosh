4fef83a2f79c91e68ce21e76385fda121270f257
Phan Le <ple@pivotallabs.com>
2014-05-23 14:40:34 -0700

    bump s3cli for go_agent
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -8,6 +8,6 @@ source $base_dir/lib/prelude_config.bash
 # Download CLI source or release from github into assets directory
 cd $assets_dir
 rm -rf s3cli
-curl -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball/a34ef740de9c4cc07bcd7bb7383bb8275298a9ab
+curl -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball/2c4a7f0ceef411532bb051e7ca55a490a565cf60
 mkdir s3cli
 tar -xzf s3cli.tar.gz -C s3cli/ --strip-components 1
