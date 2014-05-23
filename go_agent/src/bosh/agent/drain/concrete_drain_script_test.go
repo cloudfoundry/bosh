19acb69d6b4395876f2c447b821b7925b3f81b51
Phan Le <ple@pivotallabs.com>
2014-05-22 17:39:22 -0700

    set BOSH_JOB_STATE and BOSH_JOB_NEXT_STATE
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- go_agent/src/bosh/agent/drain/concrete_drain_script_test.go
+++ go_agent/src/bosh/agent/drain/concrete_drain_script_test.go
@@ -16,41 +16,49 @@ type fakeDrainParams struct {
 	hashChange      string
 	updatedPackages []string
 
-	jobState     string
-	jobNextState string
+	jobState    string
+	jobStateErr error
+
+	jobNextState    string
+	jobNextStateErr error
 }
 
 func (p fakeDrainParams) JobChange() (change string)       { return p.jobChange }
 func (p fakeDrainParams) HashChange() (change string)      { return p.hashChange }
 func (p fakeDrainParams) UpdatedPackages() (pkgs []string) { return p.updatedPackages }
 
-func (p fakeDrainParams) JobState() (string, error)     { return p.jobState, nil }
-func (p fakeDrainParams) JobNextState() (string, error) { return p.jobNextState, nil }
-
-func buildDrainScript(commandResult fakesys.FakeCmdResult) (
-	drainScript ConcreteDrainScript,
-	params fakeDrainParams,
-	runner *fakesys.FakeCmdRunner,
-	fs *fakesys.FakeFileSystem,
-) {
-	fs = fakesys.NewFakeFileSystem()
-	runner = fakesys.NewFakeCmdRunner()
-	drainScript = NewConcreteDrainScript(fs, runner, "/fake/script")
-	params = fakeDrainParams{
-		jobChange:       "job_shutdown",
-		hashChange:      "hash_unchanged",
-		updatedPackages: []string{"foo", "bar"},
-	}
-
-	runner.AddCmdResult("/fake/script"+" job_shutdown hash_unchanged foo bar", commandResult)
-
-	return
-}
+func (p fakeDrainParams) JobState() (string, error)     { return p.jobState, p.jobStateErr }
+func (p fakeDrainParams) JobNextState() (string, error) { return p.jobNextState, p.jobNextStateErr }
 
 var _ = Describe("ConcreteDrainScript", func() {
+	var (
+		runner      *fakesys.FakeCmdRunner
+		fs          *fakesys.FakeFileSystem
+		drainScript ConcreteDrainScript
+	)
+
+	BeforeEach(func() {
+		fs = fakesys.NewFakeFileSystem()
+		runner = fakesys.NewFakeCmdRunner()
+		drainScript = NewConcreteDrainScript(fs, runner, "/fake/script")
+	})
+
 	Describe("Run", func() {
+		var (
+			params fakeDrainParams
+		)
+
+		BeforeEach(func() {
+			params = fakeDrainParams{
+				jobChange:       "job_shutdown",
+				hashChange:      "hash_unchanged",
+				updatedPackages: []string{"foo", "bar"},
+			}
+		})
+
 		It("runs drain script", func() {
-			drainScript, params, runner, _ := buildDrainScript(fakesys.FakeCmdResult{Stdout: "1"})
+			commandResult := fakesys.FakeCmdResult{Stdout: "1"}
+			runner.AddCmdResult("/fake/script job_shutdown hash_unchanged foo bar", commandResult)
 
 			_, err := drainScript.Run(params)
 			Expect(err).ToNot(HaveOccurred())
@@ -68,7 +76,8 @@ var _ = Describe("ConcreteDrainScript", func() {
 		})
 
 		It("returns parsed stdout", func() {
-			drainScript, params, _, _ := buildDrainScript(fakesys.FakeCmdResult{Stdout: "1"})
+			commandResult := fakesys.FakeCmdResult{Stdout: "1"}
+			runner.AddCmdResult("/fake/script job_shutdown hash_unchanged foo bar", commandResult)
 
 			value, err := drainScript.Run(params)
 			Expect(err).ToNot(HaveOccurred())
@@ -76,7 +85,8 @@ var _ = Describe("ConcreteDrainScript", func() {
 		})
 
 		It("returns parsed stdout after trimming", func() {
-			drainScript, params, _, _ := buildDrainScript(fakesys.FakeCmdResult{Stdout: "-56\n"})
+			commandResult := fakesys.FakeCmdResult{Stdout: "-56\n"}
+			runner.AddCmdResult("/fake/script job_shutdown hash_unchanged foo bar", commandResult)
 
 			value, err := drainScript.Run(params)
 			Expect(err).ToNot(HaveOccurred())
@@ -84,23 +94,94 @@ var _ = Describe("ConcreteDrainScript", func() {
 		})
 
 		It("returns error with non integer stdout", func() {
-			drainScript, params, _, _ := buildDrainScript(fakesys.FakeCmdResult{Stdout: "hello!"})
+			commandResult := fakesys.FakeCmdResult{Stdout: "hello!"}
+			runner.AddCmdResult("/fake/script job_shutdown hash_unchanged foo bar", commandResult)
 
 			_, err := drainScript.Run(params)
 			Expect(err).To(HaveOccurred())
 		})
 
 		It("returns error when running command errors", func() {
-			drainScript, params, _, _ := buildDrainScript(fakesys.FakeCmdResult{Error: errors.New("woops")})
+			commandResult := fakesys.FakeCmdResult{Error: errors.New("woops")}
+			runner.AddCmdResult("/fake/script job_shutdown hash_unchanged foo bar", commandResult)
 
 			_, err := drainScript.Run(params)
 			Expect(err).To(HaveOccurred())
+		})
+
+		Describe("job state", func() {
+			It("sets the BOSH_JOB_STATE env variable if job state is present", func() {
+				params.jobState = "fake-job-state"
+
+				_, err := drainScript.Run(params)
+				Expect(err).To(HaveOccurred())
+
+				Expect(len(runner.RunComplexCommands)).To(Equal(1))
+
+				env := runner.RunComplexCommands[0].Env
+				Expect(env["BOSH_JOB_STATE"]).To(Equal("fake-job-state"))
+			})
+
+			It("does not set the BOSH_JOB_STATE env variable if job state is empty", func() {
+				params.jobState = ""
+
+				_, err := drainScript.Run(params)
+				Expect(err).To(HaveOccurred())
+
+				Expect(len(runner.RunComplexCommands)).To(Equal(1))
+				Expect(runner.RunComplexCommands[0].Env).ToNot(HaveKey("BOSH_JOB_STATE"))
+			})
+
+			It("returns error when cannot get the job state and does not run drain script", func() {
+				params.jobStateErr = errors.New("fake-job-state-err")
+
+				_, err := drainScript.Run(params)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-job-state-err"))
+
+				Expect(len(runner.RunComplexCommands)).To(Equal(0))
+			})
+		})
+
+		Describe("job next state", func() {
+			It("sets the BOSH_JOB_NEXT_STATE env variable if job next state is present", func() {
+				params.jobNextState = "fake-job-next-state"
+
+				_, err := drainScript.Run(params)
+				Expect(err).To(HaveOccurred())
+
+				Expect(len(runner.RunComplexCommands)).To(Equal(1))
+
+				env := runner.RunComplexCommands[0].Env
+				Expect(env["BOSH_JOB_NEXT_STATE"]).To(Equal("fake-job-next-state"))
+			})
+
+			It("does not set the BOSH_JOB_NEXT_STATE env variable if job next state is empty", func() {
+				params.jobNextState = ""
+
+				_, err := drainScript.Run(params)
+				Expect(err).To(HaveOccurred())
+
+				Expect(len(runner.RunComplexCommands)).To(Equal(1))
+				Expect(runner.RunComplexCommands[0].Env).ToNot(HaveKey("BOSH_JOB_NEXT_STATE"))
+			})
+
+			It("returns error when cannot get the job next state and does not run drain script", func() {
+				params.jobNextStateErr = errors.New("fake-job-next-state-err")
+
+				_, err := drainScript.Run(params)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("fake-job-next-state-err"))
+
+				Expect(len(runner.RunComplexCommands)).To(Equal(0))
+			})
 		})
 	})
 
 	Describe("Exists", func() {
 		It("returns bool", func() {
-			drainScript, _, _, fs := buildDrainScript(fakesys.FakeCmdResult{Stdout: "1"})
+			commandResult := fakesys.FakeCmdResult{Stdout: "1"}
+			runner.AddCmdResult("/fake/script job_shutdown hash_unchanged foo bar", commandResult)
 
 			Expect(drainScript.Exists()).To(BeFalse())
 
