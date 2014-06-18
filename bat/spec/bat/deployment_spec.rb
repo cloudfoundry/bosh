3d9fc1916807874a98f29d778ad6ce50e1e77f91
Adam Stegman <astegman@pivotallabs.com>
2014-06-18 10:49:19 -0700

    fix bat test after updating to latest fakefs
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bat/spec/bat/deployment_spec.rb
+++ bat/spec/bat/deployment_spec.rb
@@ -3,70 +3,38 @@ require 'bat/deployment'
 require 'fileutils'
 
 describe Bat::Deployment do
-  include FakeFS::SpecHelpers
+  subject(:deployment) { Bat::Deployment.new('FAKE_DEPLOYMENT_SPEC') }
 
-  let(:fake_template_evaluation_context) do
-    class FakeTemplateEvaluationContext
-      attr_reader :fake_name
-
-      def initialize
-        @fake_name = 'FAKE_DEPLOYMENT_NAME'
-      end
-
-      def get_binding
-        binding
-      end
-    end
-
-    FakeTemplateEvaluationContext.new
-  end
-
-  let(:template_dir) { File.expand_path(File.join(SPEC_ROOT, '..', 'templates')) }
-
-  let(:fake_template_path) { File.join(template_dir, 'FAKE_SPEC_CPI.yml.erb') }
-
-  let(:deployment_spec) { 'FAKE_DEPLOYMENT_SPEC' }
-
-  subject(:deployment) { Bat::Deployment.new(deployment_spec) }
+  # Make sure mktmpdir is always same but unique
+  let!(:tmp_dir) { Dir.mktmpdir }
+  before { Dir.stub(:mktmpdir).and_return(tmp_dir) }
 
   before do
-    Dir.stub(:mktmpdir) do
-      FileUtils.mkdir_p('/fake/tmpdir')
-      '/fake/tmpdir'
-    end
-
-    FileUtils.mkdir_p(template_dir)
-
-    File.open(fake_template_path, 'w') do |f|
-      f.write("---\nname: <%= fake_name %>")
-    end
-
-    fake_template_evaluation_context.stub_chain(:spec, cpi: 'FAKE_SPEC_CPI')
-    Bosh::Common::TemplateEvaluationContext.stub(new: fake_template_evaluation_context)
+    template_dir = File.expand_path(File.join(SPEC_ROOT, '..', 'templates'))
+    @cpi_template_path = File.join(template_dir, 'FAKE_SPEC_CPI.yml.erb')
+    File.open(@cpi_template_path, 'w') { |f| f.write("---\nname: <%= fake_name %>") }
   end
+  after { FileUtils.rm_f(@cpi_template_path) }
+
+  before { Bosh::Common::TemplateEvaluationContext.stub(new: template_evaluation_context) }
+  let(:template_evaluation_context) { FakeTemplateEvaluationContext.new }
+
+  before { template_evaluation_context.stub_chain(:spec, cpi: 'FAKE_SPEC_CPI') }
 
   describe '#initialize' do
     it 'generates a deployment manifest' do
-      Bosh::Common::TemplateEvaluationContext.should_receive(:new).
-        with(deployment_spec).and_return(fake_template_evaluation_context)
-
-      Bat::Deployment.new(deployment_spec)
-
-      expect(File.read('/fake/tmpdir/deployment')).to eq("---\nname: FAKE_DEPLOYMENT_NAME")
+      Bosh::Common::TemplateEvaluationContext.should_receive(:new).with('FAKE_DEPLOYMENT_SPEC')
+      Bat::Deployment.new('FAKE_DEPLOYMENT_SPEC')
+      expect(File.read("#{tmp_dir}/deployment")).to eq("---\nname: FAKE_DEPLOYMENT_NAME")
     end
   end
 
   describe '#generate_deployment_manifest' do
-    let(:deployment_spec) { 'FAKE_DEPLOYMENT_SPEC' }
-
     it 'generates a deployment manifest' do
-      Bosh::Common::TemplateEvaluationContext.should_receive(:new).with(deployment_spec).twice do
-        fake_template_evaluation_context
-      end
-
-      deployment.generate_deployment_manifest(deployment_spec)
-
-      expect(File.read('/fake/tmpdir/deployment')).to eq("---\nname: FAKE_DEPLOYMENT_NAME")
+      deployment # force load
+      Bosh::Common::TemplateEvaluationContext.should_receive(:new).with('FAKE_DEPLOYMENT_SPEC')
+      deployment.generate_deployment_manifest('FAKE_DEPLOYMENT_SPEC')
+      expect(File.read("#{tmp_dir}/deployment")).to eq("---\nname: FAKE_DEPLOYMENT_NAME")
     end
   end
 
@@ -78,15 +46,26 @@ describe Bat::Deployment do
 
   describe '#to_path' do
     it 'returns the path of the stored manifest' do
-      expect(deployment.to_path).to eq('/fake/tmpdir/deployment')
+      expect(deployment.to_path).to eq("#{tmp_dir}/deployment")
     end
   end
 
   describe '#delete' do
     it 'returns the path of the stored manifest' do
       manifest_path = deployment.to_path
-
       expect { deployment.delete }.to change { File.exists?(manifest_path) }.to(false)
+    end
+  end
+
+  class FakeTemplateEvaluationContext
+    attr_reader :fake_name
+
+    def initialize
+      @fake_name = 'FAKE_DEPLOYMENT_NAME'
+    end
+
+    def get_binding
+      binding
     end
   end
 end
