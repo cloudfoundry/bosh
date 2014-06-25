dcd0b0a286da6d565fc1bcdf8487268a1a7c406d
Karl Isenberg <kisenberg@pivotallabs.com>
2014-06-25 16:37:10 -0700

    removing unused internal_to hack
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh_aws_cpi/spec/spec_helper.rb
+++ bosh_aws_cpi/spec/spec_helper.rb
@@ -6,20 +6,6 @@ require 'cloud/aws'
 MOCK_AWS_ACCESS_KEY_ID = 'foo'
 MOCK_AWS_SECRET_ACCESS_KEY = 'bar'
 
-def internal_to(*args, &block)
-  example = describe *args, &block
-  klass = args[0]
-  if klass.is_a? Class
-    saved_private_instance_methods = klass.private_instance_methods
-    example.before do
-      klass.class_eval { public *saved_private_instance_methods }
-    end
-    example.after do
-      klass.class_eval { private *saved_private_instance_methods }
-    end
-  end
-end
-
 def mock_cloud_options
   {
     'plugin' => 'aws',
--- bosh_openstack_cpi/spec/spec_helper.rb
+++ bosh_openstack_cpi/spec/spec_helper.rb
@@ -7,20 +7,6 @@ include Archive::Tar
 
 require 'cloud/openstack'
 
-def internal_to(*args, &block)
-  example = describe *args, &block
-  klass = args[0]
-  if klass.is_a? Class
-    saved_private_instance_methods = klass.private_instance_methods
-    example.before do
-      klass.class_eval { public *saved_private_instance_methods }
-    end
-    example.after do
-      klass.class_eval { private *saved_private_instance_methods }
-    end
-  end
-end
-
 def mock_cloud_options
   {
     'plugin' => 'openstack',
