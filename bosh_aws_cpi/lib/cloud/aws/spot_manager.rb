363521bef0eaca5e68a364a5705e9c509fcb06a6
Maria Shaldibina <mariash@pivotallabs.com>
2014-07-01 10:17:42 -0700

    Refactor AWS spot manager + tests
    Signed-off-by: Kris Hicks <krishicks@gmail.com>

--- bosh_aws_cpi/lib/cloud/aws/spot_manager.rb
+++ bosh_aws_cpi/lib/cloud/aws/spot_manager.rb
@@ -3,6 +3,8 @@ require "time"
 
 module Bosh::AwsCloud
   class SpotManager
+    TOTAL_WAIT_TIME_IN_SECONDS = 300
+    RETRY_COUNT = 10
 
     def initialize(region)
       @region = region
@@ -12,106 +14,112 @@ module Bosh::AwsCloud
     def create(instance_params, spot_bid_price)
       spot_request_spec = create_spot_request_spec(instance_params, spot_bid_price)
       @logger.debug("Requesting spot instance with: #{spot_request_spec.inspect}")
-      
-      spot_instance_requests = @region.client.request_spot_instances(spot_request_spec) 
-      @logger.debug("Got spot instance requests: #{spot_instance_requests.inspect}") 
-      
-      wait_for_spot_instance_request_to_be_active spot_instance_requests
 
-      @instance
-    end
+      @spot_instance_requests = @region.client.request_spot_instances(spot_request_spec)
+      @logger.debug("Got spot instance requests: #{@spot_instance_requests.inspect}")
 
-    def wait_for_spot_instance_request_to_be_active(spot_instance_requests)
-      begin
-        # Query the spot request state until it becomes "active".
-        # This can result in the errors listed below; this is normally because AWS has 
-        # been slow to update its state so the correct response is to wait a bit and try again.
-        errors = [AWS::EC2::Errors::InvalidSpotInstanceRequestID::NotFound]
-        Bosh::Common.retryable(sleep: (total_spot_instance_request_wait_time/10), tries: 10, on: errors) do |tries, error|
-          @logger.warn("Retrying after expected error: #{error}") if error
+      wait_for_spot_instance_request_to_be_active
 
-          status = get_spot_instance_request_status(spot_instance_requests)
-
-          # Failure states+status.  If any of these occur we should give up, since waiting won't help
-          if status[:state] == 'failed' 
-            fail_spot_creation(spot_instance_requests, "VM spot instance creation failed: #{status.inspect}") end
-          if status[:state] == 'open' and (status[:status] != nil and status[:status][:code] == "price-too-low")
-            fail_spot_creation(spot_instance_requests, "Cannot create VM spot instance because bid price is too low: #{status.inspect}") end
-
-          # Success!  We have a VM; lets use it
-          if status[:state] == 'active'
-             @logger.info("Spot request instances fulfilled: #{status.inspect}")
-             instance_id = status[:instance_id]
-             @instance = @region.instances[instance_id]
-             return true
-          end
-        end
-      rescue Bosh::Common::RetryCountExceeded => e
-        @logger.warn("Timed out waiting for spot request #{spot_instance_requests.inspect} to be fulfilled")
-        cancel_pending_spot_requests(spot_instance_requests)
-        raise Bosh::Clouds::VMCreationFailed.new(true)
-      end
+      request_spot_instance
     end
 
     private
 
-    def create_spot_request_spec(instance_params, spot_price) {
-      spot_price: "#{spot_price}",
-      instance_count: 1,
-      launch_specification: {
-        image_id: instance_params[:image_id],
-        key_name: instance_params[:key_name],
-        instance_type: instance_params[:instance_type],
-        user_data: Base64.encode64(instance_params[:user_data]),
-        placement: {
-          availability_zone: instance_params[:availability_zone]
-        },
-        network_interfaces: [ 
-          { 
-            subnet_id: instance_params[:subnet].subnet_id,
-            groups: resolve_security_group_ids(instance_params[:security_groups]),
-            device_index: 0,
-            private_ip_address: instance_params[:private_ip_address]
-          } 
-        ]
-      }
-    }
+    def request_spot_instance
+      instance = nil
+
+      # Query the spot request state until it becomes "active".
+      # This can result in the errors listed below; this is normally because AWS has
+      # been slow to update its state so the correct response is to wait a bit and try again.
+      errors = [AWS::EC2::Errors::InvalidSpotInstanceRequestID::NotFound]
+      Bosh::Common.retryable(sleep: TOTAL_WAIT_TIME_IN_SECONDS/RETRY_COUNT, tries: RETRY_COUNT, on: errors) do |_, error|
+        @logger.warn("Retrying after expected error: #{error}") if error
+
+        status = spot_instance_request_status
+        case status[:state]
+          when 'failed'
+            fail_spot_creation("VM spot instance creation failed: #{status.inspect}")
+          when 'open'
+            if status[:status] != nil && status[:status][:code] == 'price-too-low'
+              fail_spot_creation("Cannot create VM spot instance because bid price is too low: #{status.inspect}")
+            end
+          when 'active'
+            @logger.info("Spot request instances fulfilled: #{status.inspect}")
+            instance = @region.instances[status[:instance_id]]
+            true
+        end
+      end
+
+      instance
+    rescue Bosh::Common::RetryCountExceeded
+      @logger.warn("Timed out waiting for spot request #{@spot_instance_requests.inspect} to be fulfilled")
+      cancel_pending_spot_requests
+      raise Bosh::Clouds::VMCreationFailed.new(true)
     end
 
-    def get_spot_instance_request_status(spot_instance_requests)
-      @logger.debug("Checking state of spot instance requests...")
-      response = @region.client.describe_spot_instance_requests( \
-        :spot_instance_request_ids => get_spot_instance_request_ids(spot_instance_requests) )
-      status = response[:spot_instance_request_set][0] #There is only ever 1 
+    def create_spot_request_spec(instance_params, spot_price)
+      spec = {
+        spot_price: "#{spot_price}",
+        instance_count: 1,
+        launch_specification: {
+          image_id: instance_params[:image_id],
+          key_name: instance_params[:key_name],
+          instance_type: instance_params[:instance_type],
+          user_data: Base64.encode64(instance_params[:user_data]),
+          placement: {
+            availability_zone: instance_params[:availability_zone]
+          },
+          network_interfaces: [
+            {
+              subnet_id: instance_params[:subnet].subnet_id,
+              device_index: 0,
+              private_ip_address: instance_params[:private_ip_address]
+            }
+          ]
+        }
+      }
+      security_groups = resolve_security_group_ids(instance_params[:security_groups])
+      unless security_groups.empty?
+        spec[:launch_specification][:network_interfaces][0][:groups] = security_groups
+      end
+
+      spec
+    end
+
+    def spot_instance_request_status
+      @logger.debug('Checking state of spot instance requests...')
+      response = @region.client.describe_spot_instance_requests(
+        spot_instance_request_ids: spot_instance_request_ids
+      )
+      status = response[:spot_instance_request_set][0] # There is only ever 1
       @logger.debug("Spot instance request status: #{status.inspect}")
       status
     end
 
-    def fail_spot_creation(spot_instance_requests, message)
+    def fail_spot_creation(message)
       @logger.error(message)
-      cancel_pending_spot_requests(spot_instance_requests)
+      cancel_pending_spot_requests
       raise Bosh::Clouds::VMCreationFailed.new(false), message
     end
 
-    def get_spot_instance_request_ids(spot_instance_requests)
-      spot_instance_requests[:spot_instance_request_set].map { |r| r[:spot_instance_request_id] } 
+    def spot_instance_request_ids
+      @spot_instance_requests[:spot_instance_request_set].map { |r| r[:spot_instance_request_id] }
     end
 
-    def cancel_pending_spot_requests(spot_instance_requests)
-      @logger.warn("Failed to create spot instance: #{spot_instance_requests.inspect}.  Cancelling request...")
-      cancel_response = @region.client.cancel_spot_instance_requests(:spot_instance_request_ids => get_spot_instance_request_ids(spot_instance_requests))
+    def cancel_pending_spot_requests
+      @logger.warn("Failed to create spot instance: #{@spot_instance_requests.inspect}. Cancelling request...")
+      cancel_response = @region.client.cancel_spot_instance_requests(
+        spot_instance_request_ids: spot_instance_request_ids
+      )
       @logger.warn("Spot cancel request returned: #{cancel_response.inspect}")
     end
 
     def resolve_security_group_ids(security_group_names)
-      security_group_ids = []
-      @region.security_groups.each do |group|
-         security_group_ids << group.security_group_id if security_group_names.include?(group.name)
+      return [] unless security_group_names
+      @region.security_groups.inject([]) do |security_group_ids, group|
+        security_group_ids << group.security_group_id if security_group_names.include?(group.name)
+        security_group_ids
       end
-      security_group_ids
     end
-
-    # total time to wait for a spot instance request to be fulfilled - 300 = 5 minutes
-    def total_spot_instance_request_wait_time; 300; end 
   end
 end
