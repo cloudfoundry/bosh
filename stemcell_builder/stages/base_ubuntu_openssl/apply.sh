ccdf8435a185e9e4af718497037ba83ac02afbcf
Maria Shaldibina <mariash@pivotallabs.com>
2014-07-03 14:15:04 -0700

    Use lucid specific openssl package
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- stemcell_builder/stages/base_ubuntu_openssl/apply.sh
+++ stemcell_builder/stages/base_ubuntu_openssl/apply.sh
@@ -11,8 +11,16 @@ cp $dir/assets/*.deb $chroot/tmp/
 # openssl is a dummy package that was genearated by equivs
 # and is installed to satisfy dependency of other packages
 
-run_in_chroot $chroot "
-dpkg --force-overwrite -i /tmp/libssl-dev_1.0.1h-1_amd64.deb
-dpkg -i /tmp/openssl_1.0.1h_all.deb
-rm -f /tmp/*.deb
-"
+if [ "${DISTRIB_CODENAME}" == "lucid" ]; then
+  run_in_chroot $chroot "
+  dpkg --force-overwrite -i /tmp/libssl-dev_1.0.1h-1_amd64_lucid.deb
+  dpkg -i /tmp/openssl_1.0.1h_all.deb
+  rm -f /tmp/*.deb
+  "
+else
+  run_in_chroot $chroot "
+  dpkg --force-overwrite -i /tmp/libssl-dev_1.0.1h-1_amd64.deb
+  dpkg -i /tmp/openssl_1.0.1h_all.deb
+  rm -f /tmp/*.deb
+  "
+fi
