b8dc97c20b8808e81c474b4bee2c22de27a43105
Tom Xing <xingzhou@cn.ibm.com>
2014-07-21 17:15:46 -0700

    Revert "Allowing multiple releases with the same content"
    This reverts commit 8a504f76766c7efbd6bba41ef1ea592468212a1b.
    Reverted due to error in creating dev releases using existing
    jobs and packages from a previously created final release.

--- bosh_cli/spec/unit/release_builder_spec.rb
+++ bosh_cli/spec/unit/release_builder_spec.rb
@@ -1,224 +1,219 @@
 require 'spec_helper'
 
-module Bosh::Cli
-  describe ReleaseBuilder do
+describe Bosh::Cli::ReleaseBuilder do
 
-    before do
-      @release_dir = Dir.mktmpdir
-      FileUtils.mkdir_p(File.join(@release_dir, 'config'))
-      @release = Bosh::Cli::Release.new(@release_dir)
+  before(:each) do
+    @release_dir = Dir.mktmpdir
+    FileUtils.mkdir_p(File.join(@release_dir, 'config'))
+    @release = Bosh::Cli::Release.new(@release_dir)
+  end
+
+  def new_builder(options = {})
+    Bosh::Cli::ReleaseBuilder.new(@release, [], [], options)
+  end
+
+  context 'when there is a final release' do
+    it 'bumps the least significant segment for the next version' do
+      final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
+        File.join(@release_dir, 'releases')))
+      final_index.add_version('deadbeef',
+                              { 'version' => '7.4.1' },
+                              get_tmp_file_path('payload'))
+      final_index.add_version('deadcafe',
+                              { 'version' => '7.3.1' },
+                              get_tmp_file_path('payload'))
+
+      builder = new_builder(final: true)
+      builder.version.should == '7.4.2'
+      builder.build
     end
 
-    def new_builder(options = {})
-      Bosh::Cli::ReleaseBuilder.new(@release, [], [], options)
-    end
-
-    context 'when there is a final release' do
-      it 'bumps the least significant segment for the next version' do
-        final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
-          File.join(@release_dir, 'releases')))
-        final_index.add_version('deadbeef',
-                                { 'version' => '7.4.1' },
-                                get_tmp_file_path('payload'))
-        final_index.add_version('deadcafe',
-                                { 'version' => '7.3.1' },
-                                get_tmp_file_path('payload'))
-
-        builder = new_builder(final: true)
-        builder.version.should == '7.4.2'
-        builder.build
-      end
-
-      it 'creates a dev version in sync with latest final version' do
-        final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
-          File.join(@release_dir, 'releases')))
-        final_index.add_version('deadbeef',
-                                { 'version' => '7.4' },
-                                get_tmp_file_path('payload'))
-        final_index.add_version('deadcafe',
-                                { 'version' => '7.3.1' },
-                                get_tmp_file_path('payload'))
-
-        builder = new_builder
-        builder.version.should == '7.4+dev.1'
-        builder.build
-      end
-
-      it 'bumps the dev version matching the latest final release' do
-        final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
-          File.join(@release_dir, 'releases')))
-        final_index.add_version('deadbeef',
-                                { 'version' => '7.3' },
-                                get_tmp_file_path('payload'))
-        final_index.add_version('deadcafe',
-                                { 'version' => '7.2' },
-                                get_tmp_file_path('payload'))
-
-        dev_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
-          File.join(@release_dir, 'dev_releases')))
-        dev_index.add_version('deadabcd',
-                              { 'version' => '7.4.1-dev' },
+    it 'creates a dev version in sync with latest final version' do
+      final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
+        File.join(@release_dir, 'releases')))
+      final_index.add_version('deadbeef',
+                              { 'version' => '7.4' },
                               get_tmp_file_path('payload'))
-        dev_index.add_version('deadbeef',
-                              { 'version' => '7.3.2.1-dev' },
-                              get_tmp_file_path('payload'))
-        dev_index.add_version('deadturkey',
-                              { 'version' => '7.3.2-dev' },
-                              get_tmp_file_path('payload'))
-        dev_index.add_version('deadcafe',
-                              { 'version' => '7.3.1-dev' },
+      final_index.add_version('deadcafe',
+                              { 'version' => '7.3.1' },
                               get_tmp_file_path('payload'))
 
-        builder = new_builder
-        builder.version.should == '7.3+dev.3'
-        builder.build
-      end
-    end
-
-    context 'when there are no final releases' do
-      it 'starts with version 0+dev.1' do
-        new_builder.version.should == '0+dev.1'
-      end
-
-      it 'increments the dev version' do
-        dev_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
-          File.join(@release_dir, 'dev_releases')))
-        dev_index.add_version('deadbeef',
-                              { 'version' => '0.1-dev' },
-                              get_tmp_file_path('payload'))
-
-        new_builder.version.should == '0+dev.2'
-      end
-    end
-
-    it 'builds a release' do
       builder = new_builder
+      builder.version.should == '7.4+dev.1'
       builder.build
-
-      expected_tarball_path = File.join(@release_dir,
-        'dev_releases',
-        'bosh_release-0+dev.1.tgz')
-
-      builder.tarball_path.should == expected_tarball_path
-      File.file?(expected_tarball_path).should be(true)
     end
 
-    it 'should include git hash and uncommitted change state in manifest' do
-      options = {commit_hash: '12345678', uncommitted_changes: true}
-      builder = Bosh::Cli::ReleaseBuilder.new(@release, [], [], options)
+    it 'bumps the dev version matching the latest final release' do
+      final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
+        File.join(@release_dir, 'releases')))
+      final_index.add_version('deadbeef',
+                              { 'version' => '7.3' },
+                              get_tmp_file_path('payload'))
+      final_index.add_version('deadcafe',
+                              { 'version' => '7.2' },
+                              get_tmp_file_path('payload'))
+
+      dev_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
+        File.join(@release_dir, 'dev_releases')))
+      dev_index.add_version('deadabcd',
+                            { 'version' => '7.4.1-dev' },
+                            get_tmp_file_path('payload'))
+      dev_index.add_version('deadbeef',
+                            { 'version' => '7.3.2.1-dev' },
+                            get_tmp_file_path('payload'))
+      dev_index.add_version('deadturkey',
+                            { 'version' => '7.3.2-dev' },
+                            get_tmp_file_path('payload'))
+      dev_index.add_version('deadcafe',
+                            { 'version' => '7.3.1-dev' },
+                            get_tmp_file_path('payload'))
+
+      builder = new_builder
+      builder.version.should == '7.3+dev.3'
       builder.build
+    end
+  end
 
-      manifest = Psych.load_file(builder.manifest_path)
-      manifest['commit_hash'].should == '12345678'
-      manifest['uncommitted_changes'].should be(true)
+  context 'when there are no final releases' do
+    it 'starts with version 0+dev.1' do
+      new_builder.version.should == '0+dev.1'
     end
 
-    it 'allows building a new release when no content has changed' do
-      expect(File).to_not exist(File.join(@release_dir, 'dev_releases', 'bosh_release-0+dev.1.tgz'))
+    it 'increments the dev version' do
+      dev_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
+        File.join(@release_dir, 'dev_releases')))
+      dev_index.add_version('deadbeef',
+                            { 'version' => '0.1-dev' },
+                            get_tmp_file_path('payload'))
 
-      new_builder.build
-      expect(File).to exist(File.join(@release_dir, 'dev_releases', 'bosh_release-0+dev.1.tgz'))
-      expect(File).to_not exist(File.join(@release_dir, 'dev_releases', 'bosh_release-0+dev.2.tgz'))
-
-      new_builder.build
-      expect(File).to exist(File.join(@release_dir, 'dev_releases', 'bosh_release-0+dev.2.tgz'))
+      new_builder.version.should == '0+dev.2'
     end
+  end
 
-    it 'errors when trying to re-create the same final version' do
-      new_builder({:version => '1', :final => true}).build
-      expect(File).to exist(File.join(@release_dir, 'releases', 'bosh_release-1.tgz'))
+  it 'builds a release' do
+    builder = new_builder
+    builder.build
 
-      expect{
-        new_builder({:version => '1', :final => true}).build
-      }.to raise_error(Bosh::Cli::ReleaseVersionError, 'Release version already exists')
-    end
+    expected_tarball_path = File.join(@release_dir,
+      'dev_releases',
+      'bosh_release-0+dev.1.tgz')
 
-    it 'has a list of jobs affected by building this release' do
-      jobs = []
-      jobs << double(:job, :new_version? => true, :packages => %w(bar baz), :name => 'job1')
-      jobs << double(:job, :new_version? => false, :packages => %w(foo baz), :name => 'job2')
-      jobs << double(:job, :new_version? => false, :packages => %w(baz zb), :name => 'job3')
-      jobs << double(:job, :new_version? => false, :packages => %w(bar baz), :name => 'job4')
+    builder.tarball_path.should == expected_tarball_path
+    File.file?(expected_tarball_path).should be(true)
+  end
 
-      packages = []
-      packages << double(:package, :name => 'foo', :new_version? => true)
-      packages << double(:package, :name => 'bar', :new_version? => false)
-      packages << double(:package, :name => 'baz', :new_version? => false)
-      packages << double(:package, :name => 'zb', :new_version? => true)
+  it 'should include git hash and uncommitted change state in manifest' do
+    options = {commit_hash: '12345678', uncommitted_changes: true}
+    builder = Bosh::Cli::ReleaseBuilder.new(@release, [], [], options)
+    builder.build
 
-      builder = Bosh::Cli::ReleaseBuilder.new(@release, packages, jobs)
+    manifest = Psych.load_file(builder.manifest_path)
+    manifest['commit_hash'].should == '12345678'
+    manifest['uncommitted_changes'].should be(true)
+  end
 
-      expect(builder.affected_jobs).to eq(jobs[0...-1]) # exclude last job
-    end
+  it "doesn't build a new release if nothing has changed" do
+    builder = new_builder
+    builder.build
+    builder.build
 
-    it 'has packages and jobs fingerprints in spec' do
-      job = double(
-        Bosh::Cli::JobBuilder,
-        :name => 'job1',
-        :version => '1.1',
-        :new_version? => true,
-        :packages => %w(foo),
-        :fingerprint => 'deadbeef',
-        :checksum => 'cafebad'
-      )
+    File.file?(File.join(@release_dir, 'dev_releases',
+      'bosh_release-0+dev.1.tgz')).
+        should be(true)
+    File.file?(File.join(@release_dir, 'dev_releases',
+      'bosh_release-0+dev.2.tgz')).
+        should be(false)
+  end
 
-      package = double(
-        Bosh::Cli::PackageBuilder,
-        :name => 'foo',
-        :version => '42',
-        :new_version? => true,
-        :fingerprint => 'deadcafe',
-        :checksum => 'baddeed',
-        :dependencies => []
-      )
+  it 'has a list of jobs affected by building this release' do
+    job1 = double(:job, :new_version? => true,
+                :packages => %w(bar baz), :name => 'job1')
+    job2 = double(:job, :new_version? => false,
+                :packages => %w(foo baz), :name => 'job2')
+    job3 = double(:job, :new_version? => false,
+                :packages => %w(baz zb), :name => 'job3')
+    job4 = double(:job, :new_version? => false,
+                :packages => %w(bar baz), :name => 'job4')
 
-      builder = Bosh::Cli::ReleaseBuilder.new(@release, [package], [job])
-      builder.should_receive(:copy_jobs)
-      builder.should_receive(:copy_packages)
+    package1 = double(:package, :name => 'foo', :new_version? => true)
+    package2 = double(:package, :name => 'bar', :new_version? => false)
+    package3 = double(:package, :name => 'baz', :new_version? => false)
+    package4 = double(:package, :name => 'zb', :new_version? => true)
 
-      builder.build
+    builder = Bosh::Cli::ReleaseBuilder.new(@release,
+                                            [package1, package2,
+                                             package3, package4],
+                                            [job1, job2, job3, job4])
+    builder.affected_jobs.should =~ [job1, job2, job3]
+  end
 
-      manifest = Psych.load_file(builder.manifest_path)
+  it 'has packages and jobs fingerprints in spec' do
+    job = double(
+      Bosh::Cli::JobBuilder,
+      :name => 'job1',
+      :version => '1.1',
+      :new_version? => true,
+      :packages => %w(foo),
+      :fingerprint => 'deadbeef',
+      :checksum => 'cafebad'
+    )
 
-      manifest['jobs'][0]['fingerprint'].should == 'deadbeef'
-      manifest['packages'][0]['fingerprint'].should == 'deadcafe'
-    end
+    package = double(
+      Bosh::Cli::PackageBuilder,
+      :name => 'foo',
+      :version => '42',
+      :new_version? => true,
+      :fingerprint => 'deadcafe',
+      :checksum => 'baddeed',
+      :dependencies => []
+    )
 
-    context 'when version options is passed into initializer' do
-      context 'when creating final release' do
-        context 'when given release version already exists' do
-          it 'raises error' do
-            final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
-              File.join(@release_dir, 'releases')))
-            final_index.add_version('deadbeef',
-                                    { 'version' => '7.3' },
-                                    get_tmp_file_path('payload'))
-            FileUtils.touch(File.join(@release_dir, 'releases', 'bosh_release-7.3.tgz'))
+    builder = Bosh::Cli::ReleaseBuilder.new(@release, [package], [job])
+    builder.should_receive(:copy_jobs)
+    builder.should_receive(:copy_packages)
 
-            expect { new_builder({ final: true, version: '7.3' }) }.to raise_error(Bosh::Cli::ReleaseVersionError, 'Release version already exists')
-          end
-        end
+    builder.build
 
-        context 'when given version does not exist' do
-          it 'uses given version' do
-            builder = new_builder({ final: true, version: '3.123' })
-            expect(builder.version).to eq('3.123')
-            builder.build
-          end
+    manifest = Psych.load_file(builder.manifest_path)
+
+    manifest['jobs'][0]['fingerprint'].should == 'deadbeef'
+    manifest['packages'][0]['fingerprint'].should == 'deadcafe'
+  end
+
+  context 'when version options is passed into initializer' do
+    context 'when creating final release' do
+      context 'when given release version already exists' do
+        it 'raises error' do
+          final_index = Bosh::Cli::CachingVersionsIndex.new(Bosh::Cli::VersionsIndex.new(
+            File.join(@release_dir, 'releases')))
+          final_index.add_version('deadbeef',
+                                  { 'version' => '7.3' },
+                                  get_tmp_file_path('payload'))
+          FileUtils.touch(File.join(@release_dir, 'releases', 'bosh_release-7.3.tgz'))
+
+          expect { new_builder({ final: true, version: '7.3' }) }.to raise_error(Bosh::Cli::ReleaseVersionError, 'Release version already exists')
         end
       end
 
-      context 'when creating dev release' do
-        it 'does not allow a version to be specified for dev releases' do
+      context 'when given version does not exist' do
+        it 'uses given version' do
           builder = new_builder({ final: true, version: '3.123' })
           expect(builder.version).to eq('3.123')
           builder.build
-
-          expect{ new_builder({ version: '3.123.1-dev' }) }.to raise_error(
-            Bosh::Cli::ReleaseVersionError,
-            'Version numbers cannot be specified for dev releases'
-          )
         end
+      end
+    end
+
+    context 'when creating dev release' do
+      it 'does not allow a version to be specified for dev releases' do
+        builder = new_builder({ final: true, version: '3.123' })
+        expect(builder.version).to eq('3.123')
+        builder.build
+
+        expect{ new_builder({ version: '3.123.1-dev' }) }.to raise_error(
+          Bosh::Cli::ReleaseVersionError,
+          'Version numbers cannot be specified for dev releases'
+        )
       end
     end
   end
--- spec/integration/cli_release_spec.rb
+++ spec/integration/cli_release_spec.rb
@@ -17,62 +17,6 @@ describe 'cli releases', type: :integration do
     end
   end
 
-  it 'allows creation of new final releases with the same content as the latest final release' do
-    Dir.chdir(TEST_RELEASE_DIR) do
-      out = bosh_runner.run_in_current_dir('create release --final', failure_expected: false)
-      expect(out).to match(/Release version: 1/)
-
-      out = bosh_runner.run_in_current_dir('create release --final --force', failure_expected: false)
-      expect(out).to match(/Release version: 2/)
-
-      out = bosh_runner.run_in_current_dir('create release --final --force', failure_expected: false)
-      expect(out).to match(/Release version: 3/)
-    end
-  end
-
-  it 'allows creation of new dev releases with the same content as the latest dev release' do
-    Dir.chdir(TEST_RELEASE_DIR) do
-      out = bosh_runner.run_in_current_dir('create release', failure_expected: false)
-      expect(out).to match(/Release version: 0\+dev.1/)
-
-      out = bosh_runner.run_in_current_dir('create release --force', failure_expected: false)
-      expect(out).to match(/Release version: 0\+dev.2/)
-
-      out = bosh_runner.run_in_current_dir('create release --force', failure_expected: false)
-      expect(out).to match(/Release version: 0\+dev.3/)
-    end
-  end
-
-  it 'allows creation of new final releases with the same content as a previous final release' do
-    Dir.chdir(TEST_RELEASE_DIR) do
-      out = bosh_runner.run_in_current_dir('create release --final', failure_expected: false)
-      expect(out).to match(/Release version: 1/)
-
-      with_changed_release do
-        out = bosh_runner.run_in_current_dir('create release --final --force', failure_expected: false)
-        expect(out).to match(/Release version: 2/)
-      end
-
-      out = bosh_runner.run_in_current_dir('create release --final --force', failure_expected: false)
-      expect(out).to match(/Release version: 3/)
-    end
-  end
-
-  it 'allows creation of new dev releases with the same content as a previous dev release' do
-    Dir.chdir(TEST_RELEASE_DIR) do
-      out = bosh_runner.run_in_current_dir('create release', failure_expected: false)
-      expect(out).to match(/Release version: 0\+dev.1/)
-
-      with_changed_release do
-        out = bosh_runner.run_in_current_dir('create release --force', failure_expected: false)
-        expect(out).to match(/Release version: 0\+dev.2/)
-      end
-
-      out = bosh_runner.run_in_current_dir('create release --force', failure_expected: false)
-      expect(out).to match(/Release version: 0\+dev.3/)
-    end
-  end
-
   it 'allows creation of a final release without an existing dev release', no_reset: false do
     Dir.chdir(TEST_RELEASE_DIR) do
       expect(File.exist?('releases/bosh-release-1.yml')).to eq(false)
@@ -120,14 +64,15 @@ describe 'cli releases', type: :integration do
         )
       end
 
-      bosh_runner.run_in_current_dir('create release --force')
-      bosh_runner.run_in_current_dir('create release --final --force')
+      runner = bosh_runner_in_work_dir(TEST_RELEASE_DIR)
+      runner.run_in_current_dir('create release --force')
+      runner.run_in_current_dir('create release --final --force')
       expect(File.exists?(release_1)).to be(true)
 
       # modify a release file to force a new version
       `echo ' ' >> #{File.join(TEST_RELEASE_DIR, 'jobs', 'foobar', 'templates', 'foobar_ctl')}`
-      bosh_runner.run_in_current_dir('create release --force')
-      bosh_runner.run_in_current_dir('create release --final --force')
+      runner.run_in_current_dir('create release --force')
+      runner.run_in_current_dir('create release --final --force')
       expect(File.exists?(release_2)).to be(true)
     end
   end
@@ -135,14 +80,14 @@ describe 'cli releases', type: :integration do
   it 'creates and deploys a new final release with a user defined version' do
     target_and_login
 
+    dev_release_1 = File.join(TEST_RELEASE_DIR, 'dev_releases/bosh-release-0+dev.1.yml')
+    release_1 = File.join(TEST_RELEASE_DIR, 'releases/bosh-release-1.0.yml')
+    dev_release_2 = File.join(TEST_RELEASE_DIR, 'dev_releases/bosh-release-1.0+dev.1.yml')
+    release_2 = File.join(TEST_RELEASE_DIR, 'releases/bosh-release-2.0.yml')
+
     commit_hash = ''
 
     Dir.chdir(TEST_RELEASE_DIR) do
-      dev_release_1 = File.join('dev_releases/bosh-release-0+dev.1.yml')
-      release_1 = File.join('releases/bosh-release-1.0.yml')
-      dev_release_2 = File.join('dev_releases/bosh-release-1.0+dev.1.yml')
-      release_2 = File.join('releases/bosh-release-2.0.yml')
-
       File.open('config/final.yml', 'w') do |final|
         final.puts YAML.dump(
           'blobstore' => {
@@ -161,18 +106,19 @@ describe 'cli releases', type: :integration do
         )
       end
 
-      bosh_runner.run_in_current_dir('create release --force')
-      expect(Dir[File.join('dev_releases/bosh-release-*.yml')]).to eq([dev_release_1])
-      bosh_runner.run_in_current_dir('create release --final --force --version 1.0')
-      expect(Dir[File.join('releases/bosh-release-*.yml')]).to eq([release_1])
+      runner = bosh_runner_in_work_dir(TEST_RELEASE_DIR)
+      runner.run_in_current_dir('create release --force')
+      expect(Dir[File.join(TEST_RELEASE_DIR, 'dev_releases/bosh-release-*.yml')]).to eq([dev_release_1])
+      runner.run_in_current_dir('create release --final --force --version 1.0')
+      expect(Dir[File.join(TEST_RELEASE_DIR, 'releases/bosh-release-*.yml')]).to eq([release_1])
 
       with_changed_release do
-        bosh_runner.run_in_current_dir('create release --force')
-        expect(Dir[File.join('dev_releases/bosh-release-*.yml')].sort).to eq([dev_release_1, dev_release_2].sort)
-        bosh_runner.run_in_current_dir('create release --final --force --version 2.0')
-        expect(Dir[File.join('releases/bosh-release-*.yml')].sort).to eq([release_1, release_2].sort)
+        runner.run_in_current_dir('create release --force')
+        expect(Dir[File.join(TEST_RELEASE_DIR, 'dev_releases/bosh-release-*.yml')].sort).to eq([dev_release_1, dev_release_2].sort)
+        runner.run_in_current_dir('create release --final --force --version 2.0')
+        expect(Dir[File.join(TEST_RELEASE_DIR, 'releases/bosh-release-*.yml')].sort).to eq([release_1, release_2].sort)
       end
-      bosh_runner.run_in_current_dir('upload release')
+      runner.run('upload release')
 
       commit_hash = `git show-ref --head --hash=8 2> /dev/null`.split.first
     end
@@ -352,16 +298,15 @@ describe 'cli releases', type: :integration do
   it 'fails to delete release in use but deletes a different release' do
       target_and_login
 
-      Dir.chdir(TEST_RELEASE_DIR) do
-        bosh_runner.run_in_current_dir('create release')
-        bosh_runner.run_in_current_dir('upload release')
+      runner = bosh_runner_in_work_dir(TEST_RELEASE_DIR)
+      runner.run('create release')
+      runner.run('upload release')
 
-        # change something in TEST_RELEASE_DIR
-        FileUtils.touch(File.join('src', 'bar', 'pretend_something_changed'))
+      # change something in TEST_RELEASE_DIR
+      FileUtils.touch(File.join(TEST_RELEASE_DIR, 'src', 'bar', 'pretend_something_changed'))
 
-        bosh_runner.run_in_current_dir('create release --force')
-        bosh_runner.run_in_current_dir('upload release')
-      end
+      runner.run('create release --force')
+      runner.run('upload release')
 
       bosh_runner.run("upload stemcell #{spec_asset('valid_stemcell.tgz')}")
 
@@ -379,19 +324,20 @@ describe 'cli releases', type: :integration do
 
   # ~57s
   it 'release lifecycle: create, upload, update (w/sparse upload), delete' do
+    runner = bosh_runner_in_work_dir(TEST_RELEASE_DIR)
+
+    release_1 = File.join(TEST_RELEASE_DIR, 'dev_releases/bosh-release-0+dev.1.yml')
+    release_2 = File.join(TEST_RELEASE_DIR, 'dev_releases/bosh-release-0+dev.2.yml')
     commit_hash = ''
 
     Dir.chdir(TEST_RELEASE_DIR) do
-      release_1 = File.join('dev_releases/bosh-release-0+dev.1.yml')
-      release_2 = File.join('dev_releases/bosh-release-0+dev.2.yml')
-
       commit_hash = `git show-ref --head --hash=8 2> /dev/null`.split.first
 
-      bosh_runner.run_in_current_dir('create release')
+      runner.run_in_current_dir('create release')
       expect(File.exists?(release_1)).to be(true)
 
       target_and_login
-      bosh_runner.run_in_current_dir("upload release #{release_1}")
+      runner.run_in_current_dir("upload release #{release_1}")
 
       with_changed_release do
         #  In an ephemeral git repo
@@ -399,7 +345,7 @@ describe 'cli releases', type: :integration do
         `git commit -m 'second dev release'`
         expect(File.exists?(release_1)).to be(true)
 
-        bosh_runner.run_in_current_dir('create release')
+        runner.run_in_current_dir('create release')
         expect(File.exists?(release_2)).to be(true)
       end
 
