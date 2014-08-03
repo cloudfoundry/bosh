ed5bb5358058a97e6715582f94a61763598f94d3
Dr Nic Williams <drnicwilliams@gmail.com>
2014-08-03 13:09:29 -0700

    spiking out GET /errands

--- bosh-director/lib/bosh/director/api/controllers/errands_controller.rb
+++ bosh-director/lib/bosh/director/api/controllers/errands_controller.rb
@@ -4,6 +4,25 @@ require 'bosh/director/api/controllers/base_controller'
 module Bosh::Director
   module Api::Controllers
     class ErrandsController < BaseController
+      get '/deployments/:deployment_name/errands' do
+        # Bosh::Director::Models::Deployment
+        deployment = @deployment_manager.find_by_name(params[:deployment_name])
+
+        manifest = Psych.load(deployment.manifest)
+        # Bosh::Director::DeploymentPlan::Planner
+        deployment = DeploymentPlan::Planner.parse(manifest, {}, Config.event_log, Config.logger)
+
+        errands = deployment.jobs.select { |job| job.can_run_as_errand? }
+        # Bosh::Director::DeploymentPlan::Job
+        errand_data = errands.map do |errand|
+          {
+            "name" => errand.name
+          }
+        end
+
+        json_encode(errand_data)
+      end
+
       post '/deployments/:deployment_name/errands/:errand_name/runs' do
         deployment_name = params[:deployment_name]
         errand_name = params[:errand_name]
