81132ea7924e26d9227dd0ea49775f514f419365
Mihnea Olteanu <molteanu@vmware.com>
2014-08-07 10:10:48 -0700

    stemcell_builder: set infrastructure to vsphere for vcloud stemcell

--- stemcell_builder/stages/system_parameters/apply.sh
+++ stemcell_builder/stages/system_parameters/apply.sh
@@ -7,5 +7,11 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
+# set the infrastructure for the agent to "vsphere" when building the vcloud stemcell
+if [ "${stemcell_infrastructure}" == "vcloud" ]
+then
+  stemcell_infrastructure=vsphere
+fi
+
 echo -n $stemcell_infrastructure > $chroot/var/vcap/bosh/etc/infrastructure
 echo -n $stemcell_operating_system > $chroot/var/vcap/bosh/etc/operating_system
