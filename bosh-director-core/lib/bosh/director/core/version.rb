2db9d1f14f22ea64440c890e8878e2d25b500af9
Jenkins User <jenkins@jenkins-slave2.sf.pivotallabs.com>
2014-08-06 21:36:17 -0700

    Adding final release for build 2681

--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.2671.0'
+      VERSION = '1.2681.0'
     end
   end
 end
--- bosh_vsphere_cpi/lib/cloud/vsphere/version.rb
+++ bosh_vsphere_cpi/lib/cloud/vsphere/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Clouds
     class VSphere
-      VERSION = '1.2671.0'
+      VERSION = '1.2681.0'
     end
   end
 end
