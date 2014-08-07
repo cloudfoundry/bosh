ac63dca22687d6525adf0c9a1791bd4fe642b59d
Karl Isenberg <kisenberg@pivotallabs.com>
2014-08-07 16:26:26 -0700

    Made Postgres DB calls silent, like MySQL
    Signed-off-by: Tom Xing <xingzhou@cn.ibm.com>

--- bosh-dev/lib/bosh/dev/sandbox/postgresql.rb
+++ bosh-dev/lib/bosh/dev/sandbox/postgresql.rb
@@ -19,22 +19,22 @@ module Bosh::Dev::Sandbox
     # login via psql without entering password.
     def create_db
       @logger.info("Creating postgres database #{db_name}")
-      @runner.run(%Q{psql -U postgres -c 'create database "#{db_name}";' > /dev/null})
+      @runner.run(%Q{psql -U postgres -c 'create database "#{db_name}";' > /dev/null 2>&1})
     end
 
     def drop_db
       @logger.info("Dropping postgres database #{db_name}")
-      @runner.run(%Q{psql -U postgres -c 'drop database "#{db_name}";' > /dev/null})
+      @runner.run(%Q{psql -U postgres -c 'drop database "#{db_name}";' > /dev/null 2>&1})
     end
 
     def truncate_db
       @logger.info("Truncating postgres database #{db_name}")
       table_name_cmd = %Q{psql -U postgres #{db_name} -c "select tablename from pg_tables where schemaname='public';"}
-      table_names = @runner.run(table_name_cmd).lines.to_a[2...-2] || []
+      table_names = `#{table_name_cmd}`.lines.to_a[2...-2] || []
       table_names.map!(&:strip)
       table_names.reject! { |name| name == "schema_migrations" }
       table_names.each do |table_name|
-        @runner.run(%Q{psql -U postgres #{db_name} -c 'truncate table "#{table_name}" cascade;' 2>&1})
+        @runner.run(%Q{psql -U postgres #{db_name} -c 'truncate table "#{table_name}" cascade;' > /dev/null 2>&1})
       end
     end
   end
