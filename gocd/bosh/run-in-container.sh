d23e43c171def1c15ccb19a38fc2219723f3b73d
Karl Isenberg <kisenberg@pivotallabs.com>
2014-08-14 16:55:16 -0700

    Adding script to run other scripts in the bosh-container
    [#76642222]
    
    Signed-off-by: Tom Xing <xingzhou@cn.ibm.com>

---
+++ gocd/bosh/run-in-container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Argument required: <path/to/script.sh>"
+  exit 1
+fi
+
+IMAGE_TAG=bosh-container
+DOCKER_REGISTRY=docker.gocd.cf-app.com:5000
+
+docker pull $DOCKER_REGISTRY/$IMAGE_TAG
+
+docker run -v ../bosh:/opt/bosh $1
