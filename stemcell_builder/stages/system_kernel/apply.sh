66a5e3490a43a461a0e9259d5a470d605235229c
Kris Hicks <krishicks@gmail.com>
2014-08-15 13:55:27 -0700

    Install trusty kernel from package manager
    The version previously installed was used in lieu of the package manager
    version due to it not being available in the package manager at the time.
    
    The minimum version we now want is available in the package manager, so we're
    returning to using the package manager.
    
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- stemcell_builder/stages/system_kernel/apply.sh
+++ stemcell_builder/stages/system_kernel/apply.sh
@@ -17,17 +17,7 @@ if [ "${DISTRIB_CODENAME}" == "lucid" ]; then
   run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.0.0-32-virtual_3.0.0-32.51~lucid1_amd64.deb"
   run_in_chroot $chroot "dpkg -i /tmp/linux-image-3.0.0-32-virtual_3.0.0-32.51~lucid1_amd64.deb"
 
-elif [ "${DISTRIB_CODENAME}" == "trusty" ]; then
-  cp $assets_dir/trusty/*.deb $chroot/tmp/
-
-  run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.13.0-32_3.13.0-32.56_all.deb"
-  run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.13.0-32-generic_3.13.0-32.56_amd64.deb"
-  run_in_chroot $chroot "dpkg -i /tmp/linux-image-3.13.0-32-generic_3.13.0-32.56_amd64.deb"
-  run_in_chroot $chroot "dpkg -i /tmp/linux-image-extra-3.13.0-32-generic_3.13.0-32.56_amd64.deb"
-
+  rm $chroot/tmp/*.deb
 else
-  echo "Unknown OS, exiting"
-  exit 2
+  pkg_mgr install linux-generic
 fi
-
-rm $chroot/tmp/*.deb
