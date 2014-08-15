06c431bd752f22dac697307626689c98d6cf692e
Karl Isenberg <kisenberg@pivotallabs.com>
2014-08-15 11:59:26 -0700

    Updated GoCD scripts to execute from the right dir automatically
    [#76642222]
    
    Signed-off-by: Michael Maximilien <maximilien@acm.org>

--- gocd/bosh/tests/integration/job.sh
+++ gocd/bosh/tests/integration/job.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+# Pipe all output to stderr because docker can only attach to one stream
+exec 1>&2
+
+SCRIPT_DIR=$(cd ./$(dirname $0) && pwd)
+BOSH_DIR=$(cd $SCRIPT_DIR/../../../.. && pwd)
+echo "BOSH_DIR: $BOSH_DIR"
+cd $BOSH_DIR
+
 if [ "$RUBY_VERSION" == "" ]; then
   echo "RUBY_VERSION environment variable is required!"
   exit 1
--- gocd/docker/build/image/job.sh
+++ gocd/docker/build/image/job.sh
@@ -3,10 +3,13 @@
 set -e
 set -x
 
+SCRIPT_DIR=$(cd ./$(dirname $0) && pwd)
+BOSH_DOCKER_DIR=$(cd $SCRIPT_DIR/../.. && pwd)
+echo "BOSH_DOCKER_DIR: $BOSH_DOCKER_DIR"
+cd $BOSH_DOCKER_DIR
+
 # builds and pushes a new 'bosh-container' to the Pivotal GoCD Docker Registry
 VM_NAME=bosh-docker-builder
-
-cd gocd/docker
 
 [ -e .vagrant/machines/$VM_NAME/virtualbox/id ] && vagrant destroy $VM_NAME --force
 vagrant up --provider virtualbox
