a6f1102597cb9485682ab2a81ad6056927493592
Karl Isenberg <kisenberg@pivotallabs.com>
2014-08-21 14:41:49 -0700

    Cleaned up GoCD scripts to not build automatically
    Signed-off-by: Mike Dillion <mike.dillion@pistoncloud.com>

--- gocd/docker/build/image/job.sh
+++ gocd/docker/build/image/job.sh
@@ -3,15 +3,13 @@
 set -e
 set -x
 
+IMAGE_TAG=bosh-container
+DOCKER_REGISTRY=docker.gocd.cf-app.com:5000
+
 SCRIPT_DIR=$(cd ./$(dirname $0) && pwd)
 BOSH_DOCKER_DIR=$(cd $SCRIPT_DIR/../.. && pwd)
 echo "BOSH_DOCKER_DIR: $BOSH_DOCKER_DIR"
 cd $BOSH_DOCKER_DIR
 
-# builds and pushes a new 'bosh-container' to the Pivotal GoCD Docker Registry
-VM_NAME=bosh-docker-builder
-
-[ -e .vagrant/machines/$VM_NAME/virtualbox/id ] && vagrant destroy $VM_NAME --force
-vagrant up --provider virtualbox
-[ -e .vagrant/machines/$VM_NAME/virtualbox/id ] && cat .vagrant/machines/$VM_NAME/virtualbox/id
-[ -e .vagrant/machines/$VM_NAME/virtualbox/id ] && vagrant destroy $VM_NAME --force
+docker build -t $DOCKER_REGISTRY/$IMAGE_TAG
+docker push $DOCKER_REGISTRY/$IMAGE_TAG
