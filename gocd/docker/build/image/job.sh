4428ecb2bf37cbcd6a7602dc1b9e278e6fc8eb07
Karl Isenberg <kisenberg@pivotallabs.com>
2014-08-21 16:23:59 -0700

    Docker image builder now takes a DOCKER_IMAGE var
    - by default Docker Hub is used (requires login)
    
    Signed-off-by: Mike Dillion <mike.dillion@pistoncloud.com>

--- gocd/docker/build/image/job.sh
+++ gocd/docker/build/image/job.sh
@@ -3,13 +3,25 @@
 set -e
 set -x
 
-IMAGE_TAG=bosh-container
-DOCKER_REGISTRY=docker.gocd.cf-app.com:5000
+# Pushing to Docker Hub requires login
+DOCKER_IMAGE=${DOCKER_IMAGE:-bosh/integration}
+
+# To push to the Pivotal GoCD Docker Registry (behind firewall):
+# DOCKER_IMAGE=docker.gocd.cf-app.com:5000/bosh-container
 
 SCRIPT_DIR=$(cd ./$(dirname $0) && pwd)
 BOSH_DOCKER_DIR=$(cd $SCRIPT_DIR/../.. && pwd)
 echo "BOSH_DOCKER_DIR: $BOSH_DOCKER_DIR"
 cd $BOSH_DOCKER_DIR
 
-docker build -t $DOCKER_REGISTRY/$IMAGE_TAG .
-docker push $DOCKER_REGISTRY/$IMAGE_TAG
+echo "Downloading latest image to prime build cache..."
+# failure to pull should not stop the build
+set +e
+docker pull $DOCKER_IMAGE
+set -e
+
+echo "Building docker image..."
+docker build -t $DOCKER_IMAGE .
+
+echo "Pushing docker image..."
+docker push $DOCKER_IMAGE
