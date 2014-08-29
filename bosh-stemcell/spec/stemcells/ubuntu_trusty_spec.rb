8fa420e1bb7677dafe2748272960edc19c1fc4ef
Maria Shaldibina <mariash@pivotallabs.com>
2014-08-29 15:02:33 -0700

    Do not create root disk partitions in CentOS
    parted only works on a live disk in Trusty, not in CentOS
    
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- bosh-stemcell/spec/stemcells/ubuntu_trusty_spec.rb
+++ bosh-stemcell/spec/stemcells/ubuntu_trusty_spec.rb
@@ -96,4 +96,16 @@ LABEL="cdrom_end"
 HERE
     end
   end
+
+  context 'installed by bosh_openstack_agent_settings', {
+    exclude_on_aws: true,
+    exclude_on_vcloud: true,
+    exclude_on_vsphere: true,
+    exclude_on_warden: true,
+  } do
+    describe file('/var/vcap/bosh/agent.json') do
+      it { should be_valid_json_file }
+      it { should contain('"CreatePartitionIfNoEphemeralDisk": true') }
+    end
+  end
 end
