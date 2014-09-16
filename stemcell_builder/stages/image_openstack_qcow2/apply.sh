3694741c98908e15f7bd2163045b090cf1d4d328
zoujin <zoujin@huawei.com>
2014-09-16 17:58:20 +0800

    Add a parameter to compress the image file.
    '-c' indicates that target image must be compressed (qcow format only)

--- stemcell_builder/stages/image_openstack_qcow2/apply.sh
+++ stemcell_builder/stages/image_openstack_qcow2/apply.sh
@@ -7,5 +7,5 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-qemu-img convert -O qcow2 $work/${stemcell_image_name} $work/root.qcow2
+qemu-img convert -c -O qcow2 $work/${stemcell_image_name} $work/root.qcow2
 ln $work/root.qcow2 $work/root.img
