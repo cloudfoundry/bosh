679f6705598a0c051b3cd7212091eeaee6194cb4
Kris Hicks <krishicks@gmail.com>
2014-09-25 17:10:11 -0700

    Convert openstack image to qcow2 0.10 compat
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>
    Signed-off-by: Kris Hicks <krishicks@gmail.com>

--- bosh-stemcell/spec/stemcells/openstack_spec.rb
+++ bosh-stemcell/spec/stemcells/openstack_spec.rb
@@ -6,4 +6,18 @@ describe 'OpenStack Stemcell', stemcell_image: true do
       it { should contain('openstack') }
     end
   end
+
+  context 'installed by image_openstack_qcow2 stage' do
+    describe 'converts to qcow2 0.10 compat' do
+      # environment is cleaned up inside rspec context
+      stemcell_image = ENV['STEMCELL_IMAGE']
+
+      subject do
+        cmd = "qemu-img info #{File.join(File.dirname(stemcell_image), 'root.qcow2')}"
+        `#{cmd}`
+      end
+
+      it { should include('compat: 0.10') }
+    end
+  end
 end
