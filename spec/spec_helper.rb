6bf00c3499fa3424abea9a5f8509a6837678b7d4
Karl Isenberg <kisenberg@pivotal.io>
2014-10-07 12:07:42 -0700

    Change double-quotes to single-quotes
    Signed-off-by: Andrea Law <alaw@pivotal.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,27 @@
 require File.expand_path('../shared_spec_helper', __FILE__)
 
-require "fileutils"
-require "digest/sha1"
-require "tmpdir"
-require "tempfile"
-require "set"
-require "yaml"
-require "nats/client"
-require "redis"
-require "restclient"
-require "bosh/director"
+require 'fileutils'
+require 'digest/sha1'
+require 'tmpdir'
+require 'tempfile'
+require 'set'
+require 'yaml'
+require 'nats/client'
+require 'redis'
+require 'restclient'
+require 'bosh/director'
 
 SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
 Dir.glob("#{SPEC_ROOT}/support/**/*.rb") { |f| require(f) }
 
 SANDBOX_DIR = Dir.mktmpdir
-TEST_RELEASE_DIR = File.join(SANDBOX_DIR, "test_release")
-BOSH_WORK_DIR    = File.join(SANDBOX_DIR, "bosh_work_dir")
-BOSH_CONFIG      = File.join(SANDBOX_DIR, "bosh_config.yml")
+TEST_RELEASE_DIR = File.join(SANDBOX_DIR, 'test_release')
+BOSH_WORK_DIR    = File.join(SANDBOX_DIR, 'bosh_work_dir')
+BOSH_CONFIG      = File.join(SANDBOX_DIR, 'bosh_config.yml')
 
-ASSETS_DIR = File.join(SPEC_ROOT, "assets")
-TEST_RELEASE_TEMPLATE = File.join(ASSETS_DIR, "test_release_template")
-BOSH_WORK_TEMPLATE    = File.join(ASSETS_DIR, "bosh_work_dir")
+ASSETS_DIR = File.join(SPEC_ROOT, 'assets')
+TEST_RELEASE_TEMPLATE = File.join(ASSETS_DIR, 'test_release_template')
+BOSH_WORK_TEMPLATE    = File.join(ASSETS_DIR, 'bosh_work_dir')
 
 STDOUT.sync = true
 
@@ -30,5 +30,5 @@ module Bosh
 end
 
 RSpec.configure do |c|
-  c.filter_run :focus => true if ENV["FOCUS"]
+  c.filter_run :focus => true if ENV['FOCUS']
 end
