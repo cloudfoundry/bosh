bd421a876e293f647bdde4604e86e684b88fbe0a
Phan Le <ple@pivotal.io>
2014-10-14 12:09:02 -0700

    Refactor deployments and errands spec to use expect syntax
    - Refactor deployments spec to use expect syntax
    - Refactor errands integration test to use expect syntax
    - Refactor errands integration test to be slightly faster
    
    Signed-off-by: Zachary Auerbach <zauerbach@pivotal.io>

--- spec/integration/errand/run_errand_success_spec.rb
+++ spec/integration/errand/run_errand_success_spec.rb
@@ -73,19 +73,14 @@ describe 'run errand success', type: :integration, with_tmp_dir: true do
       manifest_hash
     end
 
-    it 'can list errands' do
-      deploy_simple(manifest_hash: manifest_hash)
-      output, _ = bosh_runner.run('errands')
-      expect(output).to include('errand1-name')
-      expect(output).to include('errand2-name')
-    end
-
     context 'with a fixed size resource pool size' do
       before { manifest_hash['resource_pools'].first['size'] = 3 }
 
       it 'allocates enough empty VMs for the largest errand on deploy and reallocates empty VMs after each errand run' do
         deploy_simple(manifest_hash: manifest_hash)
         expect_running_vms(%w(foobar/0 unknown/unknown unknown/unknown))
+
+        expect_errands('errand1-name', 'errand2-name')
 
         output, exit_code = bosh_runner.run('run errand errand1-name', return_exit_code: true)
         expect(output).to include('some-errand1-stdout')
@@ -102,9 +97,11 @@ describe 'run errand success', type: :integration, with_tmp_dir: true do
     context 'with a dynamically sized resource pool size' do
       before { manifest_hash['resource_pools'].first.delete('size') }
 
-      it 'allocates and deallocates errand vms for each errand run' do
+      it 'allocates and de-allocates errand vms for each errand run' do
         deploy_simple(manifest_hash: manifest_hash)
         expect_running_vms(%w(foobar/0))
+
+        expect_errands('errand1-name', 'errand2-name')
 
         output, exit_code = bosh_runner.run('run errand errand1-name', return_exit_code: true)
         expect(output).to include('some-errand1-stdout')
@@ -149,6 +146,8 @@ describe 'run errand success', type: :integration, with_tmp_dir: true do
 
       deploy_simple(manifest_hash: manifest_hash)
 
+      expect_errands('errand1-name')
+
       @output, @exit_code = bosh_runner.run("run errand errand1-name --download-logs --logs-dir #{@tmp_dir}",
                                             {return_exit_code: true})
     end
@@ -179,5 +178,12 @@ describe 'run errand success', type: :integration, with_tmp_dir: true do
     vms = director.vms
     expect(vms.map(&:job_name_index)).to match_array(job_name_index_list)
     expect(vms.map(&:last_known_state).uniq).to eq(['running'])
+  end
+
+  def expect_errands(*expected_errands)
+    output, _ = bosh_runner.run('errands')
+    expected_errands.each do |errand|
+      expect(output).to include(errand)
+    end
   end
 end
