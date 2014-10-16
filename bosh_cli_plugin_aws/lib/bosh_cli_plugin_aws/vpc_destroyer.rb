5e51a44461279f4fe941aa1fc2a5b7d713f5aa13
Sean Keery <skibum55@hotmail.com>
2014-10-16 12:16:47 -0700

    Prompt for deletion of each vpc
    Prompt for deletion of each vpc so that default vpc can be kept

--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/vpc_destroyer.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/vpc_destroyer.rb
@@ -23,7 +23,8 @@ module Bosh::Aws
         if vpc.instances_count > 0
           raise "#{vpc.instances_count} instance(s) running in #{vpc.vpc_id} - delete them first"
         end
-
+        
+        next unless @ui.confirmed?("Do NOT delete your default VPC.  Are you sure you want to delete #{vpc_id}")
         dhcp_options << vpc.dhcp_options
 
         vpc.delete_network_interfaces
