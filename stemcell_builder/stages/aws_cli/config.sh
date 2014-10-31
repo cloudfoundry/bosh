3df427db0c6c3353ac6844f3a457a9cde4713e48
Andrea Law <alaw@pivotal.io>
2014-10-31 11:25:59 -0400

    Fix errors in using github OAuth
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -8,8 +8,8 @@ source $base_dir/lib/prelude_config.bash
 # Download CLI source or release from github into assets directory
 cd $assets_dir
 rm -rf s3cli
-
-if [ -e $GITHUB_OAUTH_TOKEN ]; then
+set +u
+if [ ! -z "${GITHUB_OAUTH_TOKEN}" ]; then
   AUTHENTICATION="-u ${GITHUB_OAUTH_TOKEN}:x-oauth-basic"
 fi
 curl $AUTHENTICATION -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball/2c4a7f0ceef411532bb051e7ca55a490a565cf60
