4967ad03509d3d17a2c83f6c5aba7025cde2e36b
Kris Hicks <krishicks@gmail.com>
2014-11-13 14:59:44 -0800

    Add missing tests for VPC deletion confirmation
    Signed-off-by: Edward Zhang <zhuadl@cn.ibm.com>

--- bosh_cli_plugin_aws/spec/unit/vpc_destroyer_spec.rb
+++ bosh_cli_plugin_aws/spec/unit/vpc_destroyer_spec.rb
@@ -11,19 +11,28 @@ module Bosh::Aws
       let(:ec2) { instance_double('Bosh::Aws::EC2') }
 
       context 'when there is at least one vpc' do
-        before { allow(ec2).to receive(:vpcs).and_return([aws_vpc]) }
-        let(:aws_vpc) { instance_double('AWS::EC2::VPC', id: 'fake-vpc-id') }
+        before { allow(ec2).to receive(:vpcs).and_return([aws_vpc, aws_vpc2]) }
+        let(:aws_vpc) { instance_double('AWS::EC2::VPC', id: 'fake-vpc-id-1') }
+        let(:aws_vpc2) { instance_double('AWS::EC2::VPC', id: 'fake-vpc-id-2') }
 
-        before { allow(Bosh::Aws::VPC).to receive(:find).with(ec2, 'fake-vpc-id').and_return(vpc) }
-        let(:vpc) { instance_double('Bosh::Aws::VPC', vpc_id: 'fake-vpc-id') }
+        before do
+          allow(Bosh::Aws::VPC).to receive(:find).with(ec2, 'fake-vpc-id-1').and_return(vpc)
+          allow(Bosh::Aws::VPC).to receive(:find).with(ec2, 'fake-vpc-id-2').and_return(vpc2)
+        end
+        let(:vpc) { instance_double('Bosh::Aws::VPC', vpc_id: 'fake-vpc-id-1') }
+        let(:vpc2) { instance_double('Bosh::Aws::VPC', vpc_id: 'fake-vpc-id-2') }
 
-        before { allow(vpc).to receive(:dhcp_options).and_return(aws_dhcp_options) }
-        let(:aws_dhcp_options) { instance_double('AWS::EC2::DHCPOptions', id: 'fake-dhcp-options-id') }
+        before do
+          allow(vpc).to receive(:dhcp_options).and_return(vpc_aws_dhcp_options)
+          allow(vpc2).to receive(:dhcp_options).and_return(vpc_2_aws_dhcp_options)
+        end
+        let(:vpc_aws_dhcp_options) { instance_double('AWS::EC2::DHCPOptions', id: 'fake-dhcp-options-id-1') }
+        let(:vpc_2_aws_dhcp_options) { instance_double('AWS::EC2::DHCPOptions', id: 'fake-dhcp-options-id-2') }
 
         context 'when user confirms deletion' do
-          before { allow(ui).to receive(:confirmed?).and_return(true) }
+          before { allow(ui).to receive(:confirmed?).and_return(true, false, true) }
 
-          context 'when vpc has at least one instance' do
+          context 'when a vpc has at least one instance' do
             before { allow(vpc).to receive(:instances_count).and_return(1) }
 
             it 'raises an error' do
@@ -33,21 +42,33 @@ module Bosh::Aws
             end
           end
 
-          context 'when vpc does not have any instances' do
-            before { allow(vpc).to receive(:instances_count).and_return(0) }
-
-            it 'delete any vps resource' do
+          context 'when no vpc has any instances' do
+            before do
+              allow(vpc).to receive(:instances_count).and_return(0)
+              allow(vpc2).to receive(:instances_count).and_return(0)
               allow(ec2).to receive(:internet_gateway_ids).and_return('fake-gateway-ids')
+            end
 
-              expect(vpc).to receive(:delete_network_interfaces)
-              expect(vpc).to receive(:delete_security_groups)
-              expect(ec2).to receive(:delete_internet_gateways).with('fake-gateway-ids')
-              expect(vpc).to receive(:delete_subnets)
-              expect(vpc).to receive(:delete_route_tables)
-              expect(vpc).to receive(:delete_vpc)
-              expect(aws_dhcp_options).to receive(:delete)
+            context "when the user confirms deletion" do
+              it 'deletes each vpc the user confirmed deletion for' do
+                expect(vpc).not_to receive(:delete_network_interfaces)
+                expect(vpc).not_to receive(:delete_security_groups)
+                expect(vpc).not_to receive(:delete_subnets)
+                expect(vpc).not_to receive(:delete_route_tables)
+                expect(vpc).not_to receive(:delete_vpc)
+                expect(vpc_aws_dhcp_options).not_to receive(:delete)
 
-              vpc_destroyer.delete_all
+                expect(vpc2).to receive(:delete_network_interfaces)
+                expect(vpc2).to receive(:delete_security_groups)
+                expect(vpc2).to receive(:delete_subnets)
+                expect(vpc2).to receive(:delete_route_tables)
+                expect(vpc2).to receive(:delete_vpc)
+
+                expect(ec2).to receive(:delete_internet_gateways).with('fake-gateway-ids')
+                expect(vpc_2_aws_dhcp_options).to receive(:delete)
+
+                vpc_destroyer.delete_all
+              end
             end
           end
         end
@@ -62,8 +83,14 @@ module Bosh::Aws
             expect(vpc).not_to receive(:delete_route_tables)
             expect(vpc).not_to receive(:delete_vpc)
 
+            expect(vpc2).not_to receive(:delete_network_interfaces)
+            expect(vpc2).not_to receive(:delete_security_groups)
+            expect(vpc2).not_to receive(:delete_subnets)
+            expect(vpc2).not_to receive(:delete_route_tables)
+            expect(vpc2).not_to receive(:delete_vpc)
+
             expect(ec2).not_to receive(:delete_internet_gateways)
-            expect(aws_dhcp_options).not_to receive(:delete)
+            expect(vpc_aws_dhcp_options).not_to receive(:delete)
 
             vpc_destroyer.delete_all
           end
