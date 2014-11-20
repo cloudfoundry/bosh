22785fc346caa9760bc07e8f28a29f56ad064d12
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2014-11-20 12:09:05 -0800

    Adding final release for build 2776

--- agent_client/lib/agent_client/version.rb
+++ agent_client/lib/agent_client/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Agent
     class Client
-      VERSION = '1.2768.0'
+      VERSION = '1.2776.0'
     end
   end
 end
--- blobstore_client/lib/blobstore_client/version.rb
+++ blobstore_client/lib/blobstore_client/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Blobstore
     class Client
-      VERSION = '1.2768.0'
+      VERSION = '1.2776.0'
     end
   end
 end
