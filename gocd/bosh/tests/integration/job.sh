dd88642ec57d07f5ad9a6ea88b46217f2e02f912
Dies Koper <diesk@fast.au.fujitsu.com>
2014-12-09 09:15:04 +1100

    prevent param expansion to exit script if DB not set

--- gocd/bosh/tests/integration/job.sh
+++ gocd/bosh/tests/integration/job.sh
@@ -23,6 +23,6 @@ chruby $RUBY_VERSION
 
 bundle install --local --without development
 
-echo "DB: ${DB:?'<default>'}"
+echo `echo "DB: ${DB:?'<default>'}"`
 echo "Installing Go & Running integration tests..."
 bundle exec rake --trace go spec:integration
