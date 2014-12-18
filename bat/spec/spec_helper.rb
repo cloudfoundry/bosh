b3c8c6bcf59d2e5252f803a3e66e5e19bbcd9b13
Stev Witzel <switzel@pivotal.io>
2014-12-18 09:39:48 -0800

    Allow to implicitly disable OS bats by setting BOSH_OS_BATS to false
    Signed-off-by: Edward Zhang <zhuadl@cn.ibm.com>

--- bat/spec/spec_helper.rb
+++ bat/spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'fakefs/spec_helpers'
 SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
 
 RSpec.configure do |config|
-  unless ENV['BOSH_OS_BATS']
+  unless ENV['BOSH_OS_BATS'] =~ (/^(true|yes|y|1)$/i)
     puts "!!! EXCLUDING SYSTEM SERVICE SPECS !!!"
     config.filter_run_excluding :type => :os
   end
