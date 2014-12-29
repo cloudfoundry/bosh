3f9dfcdd387cb2d35ab52dca84a04f4274d6725e
Christian Williams <cwilliams@pivotal.io>
2014-12-29 11:14:05 -0800

    Check for git changes using git
    Update specs to use real git
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- bosh-dev/lib/bosh/dev/git_repo_updater.rb
+++ bosh-dev/lib/bosh/dev/git_repo_updater.rb
@@ -10,22 +10,33 @@ module Bosh::Dev
 
     def update_directory(dir, commit_message)
       Dir.chdir(dir) do
-        stdout, stderr, status = exec_cmd('git add .')
-        raise "Failed adding untracked files in #{dir}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
+        add_any_changes_to_index
+        return if no_changes?
 
-        # Terrible patch until we start working with a git abstraction rather than shelling out like fools
-        # Check git status for "nothing to commit"...we know
-        stdout, stderr, status = exec_cmd('git status')
-        raise "Failed getting git repo status in #{dir}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
-
-        return if stdout.match(/^nothing to commit.*working directory clean.*$/)
-
-        stdout, stderr, status = exec_cmd("git commit -a -m '#{commit_message}'")
-        raise "Failed committing modified files in #{dir}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
-
-        stdout, stderr, status = exec_cmd('git push')
-        raise "Failed git pushing from #{dir}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
+        commit_changes(commit_message)
+        push
       end
+    end
+
+    private
+    def add_any_changes_to_index
+      stdout, stderr, status = exec_cmd('git add .')
+      raise "Failed adding untracked files in #{Dir.pwd}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
+    end
+
+    def no_changes?
+      _, _, status = exec_cmd('git diff-index --quiet HEAD --')
+      status.exitstatus == 0
+    end
+
+    def commit_changes(commit_message)
+      stdout, stderr, status = exec_cmd("git commit -a -m '#{commit_message}'")
+      raise "Failed committing modified files in #{Dir.pwd}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
+    end
+
+    def push
+      stdout, stderr, status = exec_cmd('git push')
+      raise "Failed git pushing from #{Dir.pwd}: stdout: '#{stdout}', stderr: '#{stderr}'" unless status.success?
     end
   end
 end
