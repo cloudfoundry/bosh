ae9609936f766aa7d28678f2257f9e45788edcf8
Christian Williams <cwilliams@pivotal.io>
2014-12-29 10:19:57 -0800

    Remove tmp dir in integration tests
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- spec/support/tmp_dir_setup.rb
+++ spec/support/tmp_dir_setup.rb
@@ -4,6 +4,8 @@ require 'fileutils'
 require 'bosh/dev/sandbox/debug_logs'
 
 RSpec.configure do |config|
+  tmp_dir = nil
+
   config.before do
     FileUtils.mkdir_p(Bosh::Dev::Sandbox::DebugLogs.logs_dir)
     tmp_dir = Dir.mktmpdir('spec-', Bosh::Dev::Sandbox::DebugLogs.logs_dir)
@@ -14,6 +16,8 @@ RSpec.configure do |config|
   config.after do |example|
     if example.exception
       example.exception.message << "\nTest directory: #{Bosh::Dev::Sandbox::DebugLogs.logs_dir}"
+    else
+      FileUtils.rm_rf(tmp_dir)
     end
   end
 end
