fb9b768b5da7070aceb283ab31fd32e3bb3ea0ba
Christian Williams <cwilliams@pivotal.io>
2015-01-06 10:59:46 -0800

    Repair migration that failed on mysql [#65509840]
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- bosh-director/db/migrations/director/20150102234124_denormalize_task_user_id_to_task_username.rb
+++ bosh-director/db/migrations/director/20150102234124_denormalize_task_user_id_to_task_username.rb
@@ -1,15 +1,34 @@
 Sequel.migration do
   up do
-    alter_table(:tasks) do
-      add_column(:username, String)
+    # mysql doesn't let us drop tasks' user_id column because it has a foreign key,
+    # and sequel won't let us drop the foreign key, so we're recreating the table
+    # from scratch. Sorry.
+
+    create_table :tasks_new do
+      primary_key :id
+      String :state, :null => false
+      Time :timestamp, :null => false
+      String :description, :null => false
+      String :result, :text => true, :null => true
+      String :output, :null => true
+      Time :checkpoint_time
+      String :type, :null => false
+      String :username, :null => true
     end
 
-    self[:users].all.each do |user|
-      self[:tasks].where(:user_id => user[:id]).update(:username => user[:username])
-    end
+    run "INSERT INTO tasks_new
+           (state, timestamp, description, result, output, checkpoint_time, type, username)
+         SELECT t.state, t.timestamp, t.description, t.result, t.output, t.checkpoint_time, t.type, u.username
+           FROM tasks t
+             LEFT OUTER JOIN users u ON t.user_id = u.id"
 
-    alter_table(:tasks) do
-      drop_column(:user_id)
+    drop_table :tasks
+    rename_table :tasks_new, :tasks
+
+    alter_table :tasks do
+      add_index :state
+      add_index :timestamp
+      add_index :description
     end
   end
 
