b67d83d1aae35c2b6f834d0ba8524ce09dbca713
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-01-08 10:31:02 -0800

    Adding final release for build 2810

--- bosh_vsphere_cpi/lib/cloud/vsphere/version.rb
+++ bosh_vsphere_cpi/lib/cloud/vsphere/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Clouds
     class VSphere
-      VERSION = '1.2809.0'
+      VERSION = '1.2810.0'
     end
   end
 end
--- simple_blobstore_server/lib/simple_blobstore_server/version.rb
+++ simple_blobstore_server/lib/simple_blobstore_server/version.rb
@@ -1,3 +1,3 @@
 module Bosh::SimpleBlobstoreServer
-  VERSION = '1.2809.0'
+  VERSION = '1.2810.0'
 end
