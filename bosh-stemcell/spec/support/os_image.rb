544a35d5e1b59d8b5e0b712968e6d2bd8f506cb2
Colin OByrne <cobyrne@pivotal.io>
2015-01-21 14:01:50 -0800

    Refactor OS image spec
    Signed-off-by: Christian Williams <cwilliams@pivotal.io>

--- bosh-stemcell/spec/support/os_image.rb
+++ bosh-stemcell/spec/support/os_image.rb
@@ -3,13 +3,13 @@ require 'rspec/core/formatters/console_codes'
 RSpec.configure do |config|
   if ENV['OS_IMAGE']
     config.before(:all) do
-      @os_image_dir = Dir.mktmpdir('os-image')
+      @os_image_dir = Dir.mktmpdir('os-image-rspec')
       Bosh::Core::Shell.new.run("sudo tar xf #{ENV['OS_IMAGE']} -C #{@os_image_dir}")
       SpecInfra::Backend::Exec.instance.chroot_dir = @os_image_dir
     end
 
     config.after(:all) do
-      FileUtils.rm_rf(@os_image_dir) if ENV['OS_IMAGE']
+      FileUtils.rm_rf(@os_image_dir)
     end
   else
     warning = 'All OS_IMAGE tests are being skipped. ENV["OS_IMAGE"] must be set to test OS images'
