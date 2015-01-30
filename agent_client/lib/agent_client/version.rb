e1a708ef8cf3fdc974aa29576ada5d85b13c9f0d
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-01-30 14:51:35 -0800

    Adding final release for build 2829

--- agent_client/lib/agent_client/version.rb
+++ agent_client/lib/agent_client/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Agent
     class Client
-      VERSION = '1.2827.0'
+      VERSION = '1.2829.0'
     end
   end
 end
--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Aws
-    VERSION = '1.2827.0'
+    VERSION = '1.2829.0'
   end
 end
