85d022c0c443912b09f6b7e0767f5868e58ec65c
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-02-10 18:36:26 -0800

    Adding final release for build 2839

--- bosh-template/lib/bosh/template/version.rb
+++ bosh-template/lib/bosh/template/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Template
-    VERSION = '1.2831.0'
+    VERSION = '1.2839.0'
   end
 end
--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Aws
-    VERSION = '1.2831.0'
+    VERSION = '1.2839.0'
   end
 end
