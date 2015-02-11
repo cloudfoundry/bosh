9eb188b53a515cb71aba81035c134741daa320e2
Colin OByrne <cobyrne@pivotal.io>
2015-02-11 14:00:12 -0800

    SSH hardening: disallow CBC Ciphers and weak MACs
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- bosh-stemcell/spec/stemcells/common_spec.rb
+++ bosh-stemcell/spec/stemcells/common_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 describe 'All OSes and Infrastructures', stemcell_image: true do
   context 'installed by bosh_harden' do
-    describe 'disallow root login' do
-      subject { file('/etc/ssh/sshd_config') }
+    subject(:sshd_config) { file('/etc/ssh/sshd_config') }
 
-      it { should contain /^PermitRootLogin no$/ }
+    it 'disallows root login' do
+      expect(sshd_config).to contain(/^PermitRootLogin no$/)
+      end
+
+    it 'disallows CBC ciphers' do
+      expect(sshd_config).to contain(/^Ciphers arcfour,arcfour128,arcfour256,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com$/)
+    end
+
+    it 'disallows insecure HMACs' do
+      expect(sshd_config).to contain(/^MACs hmac-sha1,hmac-sha2-256,hmac-sha2-512,hmac-ripemd160,hmac-ripemd160@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha1-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com$/)
     end
   end
 end
--- stemcell_builder/stages/bosh_harden/apply.sh
+++ stemcell_builder/stages/bosh_harden/apply.sh
@@ -25,3 +25,11 @@ find / -xdev -perm +6000 -a -type f \
 # No root ssh
 sed "/^ *PermitRootLogin/d" -i $chroot/etc/ssh/sshd_config
 echo 'PermitRootLogin no' >> $chroot/etc/ssh/sshd_config
+
+# Disallow CBC Ciphers
+sed "/^ *Ciphers/d" -i $chroot/etc/ssh/sshd_config
+echo 'Ciphers arcfour,arcfour128,arcfour256,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com' >> $chroot/etc/ssh/sshd_config
+
+# Disallow Weak MACs
+sed "/^ *MACs/d" -i $chroot/etc/ssh/sshd_config
+echo 'MACs hmac-sha1,hmac-sha2-256,hmac-sha2-512,hmac-ripemd160,hmac-ripemd160@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha1-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com' >> $chroot/etc/ssh/sshd_config
