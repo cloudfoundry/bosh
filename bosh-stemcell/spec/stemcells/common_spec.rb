90ca88eae38d453e1d8f0b1075c25d69bcc0014f
Colin OByrne <cobyrne@pivotal.io>
2015-02-11 15:11:40 -0800

    [#88065724] Update Cipher and MACs settings to match the Chef settings from telekomlabs
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- bosh-stemcell/spec/stemcells/common_spec.rb
+++ bosh-stemcell/spec/stemcells/common_spec.rb
@@ -6,14 +6,6 @@ describe 'All OSes and Infrastructures', stemcell_image: true do
 
     it 'disallows root login' do
       expect(sshd_config).to contain(/^PermitRootLogin no$/)
-      end
-
-    it 'disallows CBC ciphers' do
-      expect(sshd_config).to contain(/^Ciphers arcfour,arcfour128,arcfour256,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com$/)
-    end
-
-    it 'disallows insecure HMACs' do
-      expect(sshd_config).to contain(/^MACs hmac-sha1,hmac-sha2-256,hmac-sha2-512,hmac-ripemd160,hmac-ripemd160@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha1-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com$/)
     end
   end
 end
--- stemcell_builder/stages/base_ssh/apply.sh
+++ stemcell_builder/stages/base_ssh/apply.sh
@@ -8,3 +8,25 @@ source $base_dir/lib/prelude_apply.bash
 # Turn-off reverse DNS resolution in sshd
 sed "/^ *UseDNS/d" -i $chroot/etc/ssh/sshd_config
 echo 'UseDNS no' >> $chroot/etc/ssh/sshd_config
+
+# No root ssh
+sed "/^ *PermitRootLogin/d" -i $chroot/etc/ssh/sshd_config
+echo 'PermitRootLogin no' >> $chroot/etc/ssh/sshd_config
+
+if [ "${stemcell_operating_system}" == "centos" ]; then
+  # Disallow CBC Ciphers
+  sed "/^ *Ciphers/d" -i $chroot/etc/ssh/sshd_config
+  echo 'Ciphers aes256-ctr,aes192-ctr,aes128-ctr' >> $chroot/etc/ssh/sshd_config
+
+  # Disallow Weak MACs
+  sed "/^ *MACs/d" -i $chroot/etc/ssh/sshd_config
+  echo 'MACs hmac-sha2-512,hmac-sha2-256,hmac-ripemd160' >> $chroot/etc/ssh/sshd_config
+else
+  # Disallow CBC Ciphers
+  sed "/^ *Ciphers/d" -i $chroot/etc/ssh/sshd_config
+  echo 'Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr' >> $chroot/etc/ssh/sshd_config
+
+  # Disallow Weak MACs
+  sed "/^ *MACs/d" -i $chroot/etc/ssh/sshd_config
+  echo 'MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160' >> $chroot/etc/ssh/sshd_config
+fi
