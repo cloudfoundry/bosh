43caa4cce505455010d1e53528cdd442011461ac
Tyler Schultz <tschultz@pivotal.io>
2015-02-18 10:40:36 -0800

    Extract director routing configuration
    In preparation of configuration specific controller injection
    
    [#88003022]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

---
+++ bosh-director/lib/bosh/director/api/route_configuration.rb
@@ -0,0 +1,37 @@
+module Bosh
+  module Director
+    module Api
+      class RouteConfiguration
+        def initialize(config)
+          @config = config
+        end
+
+        def controllers
+          director_app = Bosh::Director::App.new(@config)
+          identity_provider = Bosh::Director::Api::LocalIdentityProvider.new Bosh::Director::Api::UserManager.new
+          controllers = {}
+          controllers['/info'] = Bosh::Director::Api::Controllers::InfoController.new identity_provider
+          controllers['/tasks'] = Bosh::Director::Api::Controllers::TasksController.new identity_provider
+          controllers['/backups'] = Bosh::Director::Api::Controllers::BackupsController.new identity_provider
+          controllers['/deployments'] = Bosh::Director::Api::Controllers::DeploymentsController.new identity_provider
+          controllers['/packages'] = Bosh::Director::Api::Controllers::PackagesController.new identity_provider
+          controllers['/releases'] = Bosh::Director::Api::Controllers::ReleasesController.new identity_provider
+          controllers['/resources'] = Bosh::Director::Api::Controllers::ResourcesController.new(
+            identity_provider,
+            Bosh::Director::Api::ResourceManager.new(director_app.blobstores.blobstore)
+          )
+          controllers['/resurrection'] = Bosh::Director::Api::Controllers::ResurrectionController.new identity_provider
+          controllers['/stemcells'] = Bosh::Director::Api::Controllers::StemcellsController.new identity_provider
+          controllers['/task'] = Bosh::Director::Api::Controllers::TaskController.new identity_provider
+          controllers['/users'] = Bosh::Director::Api::Controllers::UsersController.new identity_provider
+          controllers['/compiled_package_groups'] = Bosh::Director::Api::Controllers::CompiledPackagesController.new(
+            identity_provider,
+            Bosh::Director::Api::CompiledPackageGroupManager.new
+          )
+          controllers['/locks'] = Bosh::Director::Api::Controllers::LocksController.new identity_provider
+          controllers
+        end
+      end
+    end
+  end
+end
