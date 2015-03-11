655f4cd6cdaa6c5cc31eaab379f5ce75fa46c450
Maria Shaldibina <mshaldibina@pivotal.io>
2015-03-11 14:22:29 -0700

    Refactor director config
    Signed-off-by: Marco Voelz <marco.voelz@sap.com>

---
+++ bosh-dev/lib/bosh/dev/sandbox/director_config.rb
@@ -0,0 +1,45 @@
+module Bosh::Dev::Sandbox
+  class DirectorConfig
+    attr_reader :director_ruby_port,
+      :nats_port,
+      :redis_port,
+      :sandbox_root,
+      :external_cpi_enabled,
+      :blobstore_storage_dir,
+      :external_cpi_config,
+      :database,
+      :director_fix_stateful_nodes,
+      :cloud_storage_dir,
+      :user_authentication,
+      :uaa_url
+
+
+    def initialize(attrs, port_provider)
+      @director_ruby_port = port_provider.get_port(:director_ruby)
+      @nats_port = port_provider.get_port(:nats)
+      @redis_port = port_provider.get_port(:redis)
+
+      @sandbox_root = attrs.fetch(:sandbox_root)
+
+      @database = attrs.fetch(:database)
+
+      @blobstore_storage_dir = attrs.fetch(:blobstore_storage_dir)
+
+      @director_fix_stateful_nodes = attrs.fetch(:director_fix_stateful_nodes, false)
+
+      @external_cpi_enabled = attrs.fetch(:external_cpi_enabled, false)
+      @external_cpi_config = attrs.fetch(:external_cpi_config)
+
+      @cloud_storage_dir = attrs.fetch(:cloud_storage_dir)
+
+      @user_authentication = attrs.fetch(:user_authentication)
+      @uaa_url = "http://localhost:#{port_provider.get_port(:uaa)}/uaa"
+    end
+
+    def render(template_path)
+      template_contents = File.read(template_path)
+      template = ERB.new(template_contents)
+      template.result(binding)
+    end
+  end
+end
---
+++ bosh-dev/lib/bosh/dev/sandbox/port_provider.rb
@@ -0,0 +1,15 @@
+module Bosh::Dev::Sandbox
+  class PortProvider
+    BASE_PORT = 61000
+
+    def initialize(test_env_number)
+      @port_offset = test_env_number * 100
+      @port_names = []
+    end
+
+    def get_port(name)
+      @port_names << name unless @port_names.include?(name)
+      BASE_PORT + @port_offset + @port_names.index(name)
+    end
+  end
+end
