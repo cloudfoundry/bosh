19edf4be93d08f0b766c5232f5b69058663a938e
Marco Voelz <marco.voelz@sap.com>
2015-03-16 19:07:09 -0700

    Fix download
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- gocd/docker/install-java.sh
+++ gocd/docker/install-java.sh
@@ -4,11 +4,9 @@ set -e
 set -x
 
 INSTALL_PREFIX=/usr/lib/jvm
-DOWNLOAD_URL=http://cdn.azulsystems.com/zulu/2014-10-8.4-bin/zulu1.8.0_25-8.4.0.1-x86lx64.zip
-DOWNLOAD_ARGS="-O /tmp/zulu1.8.0_25-8.4.0.1-x86lx64.zip --continue --header=\"Referer: http://www.azulsystems.com/products/zulu/downloads\""
 
 echo "Downloading Java..."
-wget $DOWNLOAD_ARGS $DOWNLOAD_URL
+wget -O /tmp/zulu1.8.0_25-8.4.0.1-x86lx64.zip --continue --header=\"Referer: http://www.azulsystems.com/products/zulu/downloads\" http://cdn.azulsystems.com/zulu/2014-10-8.4-bin/zulu1.8.0_25-8.4.0.1-x86lx64.zip
 
 echo "Extracting Java..."
 mkdir -p $INSTALL_PREFIX
