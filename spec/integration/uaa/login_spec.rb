64dfaf085639d812d290e52b6c23754f42dbd94e
Marco Voelz <marco.voelz@sap.com>
2015-03-18 15:17:57 -0700

    Add test to verify UAA access
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- spec/integration/uaa/login_spec.rb
+++ spec/integration/uaa/login_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe 'Logging into a director with UAA authentication', type: :integration do
   with_reset_sandbox_before_each(user_authentication: 'uaa')
 
-  before { bosh_runner.run("target #{current_sandbox.director_url}") }
+  before do
+    bosh_runner.run("target #{current_sandbox.director_url}")
+    bosh_runner.run('logout')
+  end
 
   it 'logs in successfully' do
     bosh_runner.run_interactively('login') do |runner|
@@ -15,6 +18,11 @@ describe 'Logging into a director with UAA authentication', type: :integration d
       runner.send_keys 'dontcare' # UAA only uses this for SAML, but always prompts for it
       expect(runner).to have_output "Logged in as `marissa'"
     end
+
+    output = bosh_runner.run('deployments',
+      { failure_expected: true } # If you have no deployments, exit status is non-zero
+    )
+    expect(output).to match /No deployments/
   end
 
   it 'fails to log in when incorrect credentials were provided' do
@@ -27,5 +35,10 @@ describe 'Logging into a director with UAA authentication', type: :integration d
       runner.send_keys 'dontcare'
       expect(runner).to have_output 'Failed to log in'
     end
+
+    output = bosh_runner.run('deployments',
+      { failure_expected: true }
+    )
+    expect(output).to match /Please log in first/
   end
 end
