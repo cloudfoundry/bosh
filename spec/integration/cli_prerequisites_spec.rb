7b23c4a77e59720c5c237b3cccfb58a28e85373e
Kihyeon Kim <kihyeon.kim@pivotal.io>
2015-03-18 10:36:39 -0700

    Clears bosh config file before running cli_prerequisites spec.
    Attempting to resolve potential test pollution with target already
    being set.
    
    [#90565828]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- spec/integration/cli_prerequisites_spec.rb
+++ spec/integration/cli_prerequisites_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe 'cli: deployment prerequisites', type: :integration do
   with_reset_sandbox_before_each
 
+  before { bosh_runner.reset }
+
   it 'requires target and login' do
     expect(bosh_runner.run('deploy', :failure_expected => true)).to match(/Please choose target first/)
     bosh_runner.run("target #{current_sandbox.director_url}")
