c2f83181067cf677a46cd4de44b70bbaf8218b16
Maria Shaldibina <mshaldibina@pivotal.io>
2015-03-27 10:16:10 -0700

    Remove default values for lifecycle environment variables
    So it's easier to know what needs to be set when you're running them
    locally
    
    Signed-off-by: Colin Obyrne <cobyrne@pivotal.io>

--- bosh_vsphere_cpi/spec/integration/lifecycle_spec.rb
+++ bosh_vsphere_cpi/spec/integration/lifecycle_spec.rb
@@ -5,27 +5,27 @@ require 'yaml'
 
 describe VSphereCloud::Cloud, external_cpi: false do
   before(:all) do
-    @host          = ENV['BOSH_VSPHERE_CPI_HOST']     || raise('Missing BOSH_VSPHERE_CPI_HOST')
-    @user          = ENV['BOSH_VSPHERE_CPI_USER']     || raise('Missing BOSH_VSPHERE_CPI_USER')
-    @password      = ENV['BOSH_VSPHERE_CPI_PASSWORD'] || raise('Missing BOSH_VSPHERE_CPI_PASSWORD')
-    @vlan          = ENV['BOSH_VSPHERE_VLAN']         || raise('Missing BOSH_VSPHERE_VLAN')
-    @stemcell_path = ENV['BOSH_VSPHERE_STEMCELL']     || raise('Missing BOSH_VSPHERE_STEMCELL')
+    @host = ENV.fetch('BOSH_VSPHERE_CPI_HOST')
+    @user = ENV.fetch('BOSH_VSPHERE_CPI_USER')
+    @password = ENV.fetch('BOSH_VSPHERE_CPI_PASSWORD')
+    @vlan = ENV.fetch('BOSH_VSPHERE_VLAN')
+    @stemcell_path = ENV.fetch('BOSH_VSPHERE_STEMCELL')
 
-    @second_datastore = ENV['BOSH_VSPHERE_CPI_SECOND_DATASTORE'] ||
-      raise('Missing BOSH_VSPHERE_CPI_SECOND_DATASTORE')
+    @second_datastore = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_DATASTORE')
 
-    @datacenter_name              = ENV.fetch('BOSH_VSPHERE_CPI_DATACENTER', 'BOSH_DC')
-    @vm_folder                    = ENV.fetch('BOSH_VSPHERE_CPI_VM_FOLDER', 'ACCEPTANCE_BOSH_VMs')
-    @template_folder              = ENV.fetch('BOSH_VSPHERE_CPI_TEMPLATE_FOLDER', 'ACCEPTANCE_BOSH_Templates')
-    @disk_path                    = ENV.fetch('BOSH_VSPHERE_CPI_DISK_PATH', 'ACCEPTANCE_BOSH_Disks')
-    @datastore_pattern            = ENV.fetch('BOSH_VSPHERE_CPI_DATASTORE_PATTERN', 'jalapeno')
-    @persistent_datastore_pattern = ENV.fetch('BOSH_VSPHERE_CPI_PERSISTENT_DATASTORE_PATTERN', 'jalapeno')
-    @cluster                      = ENV.fetch('BOSH_VSPHERE_CPI_CLUSTER', 'BOSH_CL')
-    @resource_pool_name           = ENV.fetch('BOSH_VSPHERE_CPI_RESOURCE_POOL', 'ACCEPTANCE_RP')
-    @second_cluster               = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_CLUSTER', 'BOSH_CL2')
-    @second_resource_pool_name    = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_RESOURCE_POOL', 'ACCEPTANCE_RP')
-    @second_datastore_pattern     = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_DATASTORE_PATTERN', @datastore_pattern)
-    @second_persistent_datastore_pattern = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_PERSISTENT_DATASTORE_PATTERN', @persistent_datastore_pattern)
+    @datacenter_name = ENV.fetch('BOSH_VSPHERE_CPI_DATACENTER')
+    @vm_folder = ENV.fetch('BOSH_VSPHERE_CPI_VM_FOLDER')
+    @template_folder = ENV.fetch('BOSH_VSPHERE_CPI_TEMPLATE_FOLDER')
+    @disk_path = ENV.fetch('BOSH_VSPHERE_CPI_DISK_PATH')
+    @datastore_pattern = ENV.fetch('BOSH_VSPHERE_CPI_DATASTORE_PATTERN')
+    @persistent_datastore_pattern = ENV.fetch('BOSH_VSPHERE_CPI_PERSISTENT_DATASTORE_PATTERN')
+    @cluster = ENV.fetch('BOSH_VSPHERE_CPI_CLUSTER')
+    @resource_pool_name = ENV.fetch('BOSH_VSPHERE_CPI_RESOURCE_POOL')
+
+    @second_cluster = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_CLUSTER')
+    @second_cluster_resource_pool_name = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_RESOURCE_POOL')
+    @second_cluster_ephemeral_datastore_pattern = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_DATASTORE_PATTERN')
+    @second_cluster_persistent_datastore_pattern = ENV.fetch('BOSH_VSPHERE_CPI_SECOND_PERSISTENT_DATASTORE_PATTERN')
   end
 
   def build_cpi(options = {})
@@ -37,25 +37,25 @@ describe VSphereCloud::Cloud, external_cpi: false do
         'ntp' => ['10.80.0.44'],
       },
       'vcenters' => [{
-        'host' => @host,
-        'user' => @user,
-        'password' => @password,
-        'datacenters' => [{
-          'name' => @datacenter_name,
-          'vm_folder' => @vm_folder,
-          'template_folder' => @template_folder,
-          'disk_path' => @disk_path,
-          'datastore_pattern' => datastore_pattern,
-          'persistent_datastore_pattern' => persistent_datastore_pattern,
-          'allow_mixed_datastores' => true,
-          'clusters' => [{
-              @cluster => { 'resource_pool' => @resource_pool_name },
-            },
-            {
-              @second_cluster  => { 'resource_pool' => @second_resource_pool_name }
-            }],
+          'host' => @host,
+          'user' => @user,
+          'password' => @password,
+          'datacenters' => [{
+              'name' => @datacenter_name,
+              'vm_folder' => @vm_folder,
+              'template_folder' => @template_folder,
+              'disk_path' => @disk_path,
+              'datastore_pattern' => datastore_pattern,
+              'persistent_datastore_pattern' => persistent_datastore_pattern,
+              'allow_mixed_datastores' => true,
+              'clusters' => [{
+                  @cluster => {'resource_pool' => @resource_pool_name},
+                },
+                {
+                  @second_cluster => {'resource_pool' => @second_cluster_resource_pool_name}
+                }],
+            }]
         }]
-      }]
     )
   end
 
@@ -64,11 +64,11 @@ describe VSphereCloud::Cloud, external_cpi: false do
   subject(:cpi) { @cpi }
 
   before(:all) do
-     Dir.mktmpdir do |temp_dir|
-       output = `tar -C #{temp_dir} -xzf #{@stemcell_path} 2>&1`
-       raise "Corrupt image, tar exit status: #{$?.exitstatus} output: #{output}" if $?.exitstatus != 0
-       @stemcell_id = @cpi.create_stemcell("#{temp_dir}/image", nil)
-     end
+    Dir.mktmpdir do |temp_dir|
+      output = `tar -C #{temp_dir} -xzf #{@stemcell_path} 2>&1`
+      raise "Corrupt image, tar exit status: #{$?.exitstatus} output: #{output}" if $?.exitstatus != 0
+      @stemcell_id = @cpi.create_stemcell("#{temp_dir}/image", nil)
+    end
   end
 
   after(:all) { @cpi.delete_stemcell(@stemcell_id) if @stemcell_id }
@@ -81,7 +81,7 @@ describe VSphereCloud::Cloud, external_cpi: false do
       'static' => {
         'ip' => '169.254.1.1',
         'netmask' => '255.255.254.0',
-        'cloud_properties' => { 'name' => @vlan },
+        'cloud_properties' => {'name' => @vlan},
         'default' => ['dns', 'gateway'],
         'dns' => ['169.254.1.2'],
         'gateway' => '169.254.1.3'
@@ -161,7 +161,7 @@ describe VSphereCloud::Cloud, external_cpi: false do
     context 'without existing disks and placer' do
       it 'should exercise the vm lifecycle and select the cluster in the resource pool datacenters' do
         begin
-          resource_pool['datacenters'] = [{ 'name' => @datacenter_name, 'clusters' => [{@cluster => {}}]}]
+          resource_pool['datacenters'] = [{'name' => @datacenter_name, 'clusters' => [{@cluster => {}}]}]
           vm_lifecycle([], resource_pool)
 
           vm = @cpi.vm_provider.find(@vm_id)
@@ -171,9 +171,9 @@ describe VSphereCloud::Cloud, external_cpi: false do
         end
 
         begin
-          @cpi = build_cpi(datastore_pattern: @second_datastore_pattern, persistent_datastore_pattern: @second_persistent_datastore_pattern)
+          @cpi = build_cpi(datastore_pattern: @second_cluster_ephemeral_datastore_pattern, persistent_datastore_pattern: @second_cluster_persistent_datastore_pattern)
 
-          resource_pool['datacenters'] = [{ 'name' => @datacenter_name, 'clusters' => [{@second_cluster => {}}]}]
+          resource_pool['datacenters'] = [{'name' => @datacenter_name, 'clusters' => [{@second_cluster => {}}]}]
           vm_lifecycle([], resource_pool)
 
           vm = @cpi.vm_provider.find(@vm_id)
