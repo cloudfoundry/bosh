69d9876fff164c9169bfdebe46f5dd327115146e
Corey Innis <cinnis@pivotal.io>
2015-04-14 12:17:59 -0700

    UAA server port should be configurable in integration tests
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- bosh-dev/lib/bosh/dev/sandbox/uaa.rb
+++ bosh-dev/lib/bosh/dev/sandbox/uaa.rb
@@ -23,8 +23,9 @@ module Bosh::Dev::Sandbox
       raise out unless $? == 0
     end
 
-    def initialize(port, log_base, logger, runner = Bosh::Core::Shell.new)
-      @port = port
+    def initialize(http_port, server_port, log_base, logger, runner = Bosh::Core::Shell.new)
+      @http_port = http_port
+      @server_port = server_port
       @log_base = log_base
       @logger = logger
       @runner = runner
@@ -36,7 +37,8 @@ module Bosh::Dev::Sandbox
       server_xml = File.join(UAA_CONFIG_DIR, 'tomcat-server.xml')
       log_path = "#{@log_base}.uaa.out"
       opts = {
-        "uaa.http_port" => @port,
+        "uaa.http_port" => @http_port,
+        "uaa.server_port" => @server_port,
         "uaa.access_log_dir" => File.dirname(log_path),
       }
       @service = Service.new([executable_path, 'run', '-config', server_xml],
@@ -50,7 +52,7 @@ module Bosh::Dev::Sandbox
         @logger,
       )
 
-      @uaa_socket_connector = SocketConnector.new('uaa', '127.0.0.1', @port, @logger)
+      @uaa_socket_connector = SocketConnector.new('uaa', '127.0.0.1', @http_port, @logger)
 
       @service.start
     end
