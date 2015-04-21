50a35c9c1e501bb0a5b696dbcd226b541e807614
Jonathan Barnes <jbarnes@pivotal.io>
2015-04-21 10:02:07 -0700

    Revert "Revert "Changes signature of LockHelper#with_release_locks to take release names""
    This reverts commit 8ac1334a69a100a8e054e90eaf34b0101661040b.

--- bosh-director/lib/bosh/director/lock_helper.rb
+++ bosh-director/lib/bosh/director/lock_helper.rb
@@ -1,13 +1,5 @@
 module Bosh::Director
-  # Helper for managing BOSH locks.
   module LockHelper
-
-    # Surround with deployment lock.
-    #
-    # @param [DeploymentPlan|String] deployment plan or name.
-    # @option opts [Number] timeout how long to wait before giving up
-    # @return [void]
-    # @yield [void] block to surround
     def with_deployment_lock(deployment, opts = {})
       if deployment.respond_to?(:name)
         name = deployment.name
@@ -21,13 +13,6 @@ module Bosh::Director
       Lock.new("lock:deployment:#{name}", :timeout => timeout).lock { yield }
     end
 
-    # Surround with stemcell lock.
-    #
-    # @param [String] name stemcell name.
-    # @param [String] version stemcell version.
-    # @option opts [Number] timeout how long to wait before giving up
-    # @return [void]
-    # @yield [void] block to surround
     def with_stemcell_lock(name, version, opts = {})
       timeout = opts[:timeout] || 10
       Config.logger.info("Acquiring stemcell lock on #{name}:#{version}")
@@ -35,31 +20,12 @@ module Bosh::Director
           lock { yield }
     end
 
-    # Surround with release lock.
-    #
-    # @param [String] release name.
-    # @option opts [Number] timeout how long to wait before giving up
-    # @return [void]
-    # @yield [void] block to surround
-    def with_release_lock(release, opts = {})
-      timeout = opts[:timeout] || 10
-      Config.logger.info("Acquiring release lock on #{release}")
-      Lock.new("lock:release:#{release}", :timeout => timeout).lock { yield }
+    def with_release_lock(release_name, opts = {})
+      with_release_locks([release_name], opts) { yield }
     end
 
-    # Surround with deployment releases lock.
-    #
-    # @param [DeploymentPlan] deployment plan.
-    # @option opts [Number] timeout how long to wait before giving up
-    # @return [void]
-    # @yield [void] block to surround
-    def with_release_locks(deployment_plan, opts = {})
+    def with_release_locks(release_names, opts = {})
       timeout = opts[:timeout] || 10
-      release_names = deployment_plan.releases.map do |release|
-        release.name
-      end
-
-      # Sorting to enforce lock order to avoid deadlocks
       locks = release_names.sort.map do |release_name|
         Config.logger.info("Acquiring release lock: #{release_name}")
         Lock.new("lock:release:#{release_name}", :timeout => timeout)
@@ -73,13 +39,6 @@ module Bosh::Director
       end
     end
 
-    # Surround with compile lock.
-    #
-    # @param [String|Number] package_id package id.
-    # @param [String|Number] stemcell_id stemcell id.
-    # @option opts [Number] timeout how long to wait before giving up
-    # @return [void]
-    # @yield [void] block to surround
     def with_compile_lock(package_id, stemcell_id, opts = {})
       timeout = opts[:timeout] || 15 * 60 # 15 minutes
 
