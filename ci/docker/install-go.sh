19da5fa3c1302974ec44d19bd0d40d047530ee38
Aaron Hurley <ahurley@pivotal.io>
2015-05-04 11:29:56 -0700

    Add golang to bosh/integration docker image.
    [#93481960]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

---
+++ ci/docker/install-go.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+GOPATH=/home/vagrant/go
+GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz
+GO_ARCHIVE=/tmp/$(basename $GO_ARCHIVE_URL)
+
+echo "Downloading go..."
+mkdir -p $(dirname $GOROOT)
+wget -q $GO_ARCHIVE_URL -O $GO_ARCHIVE
+tar xf $GO_ARCHIVE -C $(dirname $GOROOT)
+chmod -R a+w $GOROOT
+
+if [ ! -d $TMPDIR ]; then
+  mkdir -p $TMPDIR
+fi
+
+rm -f $GO_ARCHIVE
