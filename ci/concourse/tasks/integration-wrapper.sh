4f51c02be375a931b6d4f10422bd880ac6b0bebe
Aaron Hurley <ahurley@pivotal.io>
2015-05-06 16:36:05 -0700

    Execute postgres as current user (vcap).
    [#93481926]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/tasks/integration-wrapper.sh
+++ ci/concourse/tasks/integration-wrapper.sh
@@ -4,13 +4,31 @@ set -e
 set -x
 
 echo "Starting $DB..."
-sudo service $DB start
+
+case "$DB" in
+  mysql)
+    sudo service mysql start
+    ;;
+  postgresql)
+    pg_ctl start -l /var/log/postgresql/server.log
+    ;;
+  *)
+    echo $"Usage: $0 {mysql|postgresql}"
+    exit 1
+esac
 
 set +e
 $@
 exitcode=$?
 
 echo "Stopping $DB..."
-sudo service $DB stop
+case "$DB" in
+  mysql)
+    sudo service mysql stop
+    ;;
+  postgresql)
+    pg_ctl stop
+    ;;
+esac
 
 exit $exitcode
