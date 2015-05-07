7c025cede4bd4de576c2bc10094722c49e4fb4d2
Aaron Hurley <ahurley@pivotal.io>
2015-05-06 17:31:26 -0700

    Attempt to allow for non-privileged execution (for bosh-agent tests)
    [#93481926]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/tasks/integration-wrapper.sh
+++ ci/concourse/tasks/integration-wrapper.sh
@@ -5,11 +5,17 @@ set -x
 
 echo "Starting $DB..."
 
+sudo mkdir -p /var/postgres && chown vcap:vcap /var/postgres
+sudo mkdir -p /var/run/postgresql && chown vcap:vcap /var/run/postgresql
+sudo mkdir -p /var/log/postgresql && chown vcap:vcap /var/log/postgresql
+sudo pg_ctl init && pg_ctl start -l /var/log/postgresql/server.log && sleep 4 && createuser -U vcap --superuser postgres && createdb -U vcap && pg_ctl stop
+
 case "$DB" in
   mysql)
     sudo service mysql start
     ;;
   postgresql)
+#    su - vcap # TODO: remove this once we can run as non-privileged
     pg_ctl start -l /var/log/postgresql/server.log
     ;;
   *)
@@ -27,6 +33,7 @@ case "$DB" in
     sudo service mysql stop
     ;;
   postgresql)
+#    su - vcap # TODO: remove this once we can run as non-privileged
     pg_ctl stop
     ;;
 esac
