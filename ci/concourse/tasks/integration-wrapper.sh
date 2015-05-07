95301fd73347e5ff4cef7ee46fd61a5f59984374
Maria Shaldibina <mshaldibina@pivotal.io>
2015-05-07 14:48:22 -0700

    Attempt to finish running postgres as non-privileged.
    [#93481926]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/tasks/integration-wrapper.sh
+++ ci/concourse/tasks/integration-wrapper.sh
@@ -5,17 +5,11 @@ set -x
 
 echo "Starting $DB..."
 
-sudo mkdir -p /var/postgres && chown vcap:vcap /var/postgres
-sudo mkdir -p /var/run/postgresql && chown vcap:vcap /var/run/postgresql
-sudo mkdir -p /var/log/postgresql && chown vcap:vcap /var/log/postgresql
-sudo pg_ctl init && pg_ctl start -l /var/log/postgresql/server.log && sleep 4 && createuser -U vcap --superuser postgres && createdb -U vcap && pg_ctl stop
-
 case "$DB" in
   mysql)
     sudo service mysql start
     ;;
   postgresql)
-#    su - vcap # TODO: remove this once we can run as non-privileged
     pg_ctl start -l /var/log/postgresql/server.log
     ;;
   *)
@@ -33,7 +27,6 @@ case "$DB" in
     sudo service mysql stop
     ;;
   postgresql)
-#    su - vcap # TODO: remove this once we can run as non-privileged
     pg_ctl stop
     ;;
 esac
