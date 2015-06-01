6c5dfd49b2304bed9c89ecf3eb7680f019156d16
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-06-01 16:33:31 -0700

    Adding final release for build 2978

--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.2977.0'
+      VERSION = '1.2978.0'
     end
   end
 end
--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Aws
-    VERSION = '1.2977.0'
+    VERSION = '1.2978.0'
   end
 end
