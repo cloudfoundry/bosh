b7441eab51fbe14e5f4c18652a9e5fcd19f01765
Corey Innis <cinnis@pivotal.io>
2015-06-05 11:28:55 -0700

    Pipeline tweaks...
    - Do not print out task script commmands.
    - Specify TERM, so git command works.
    - Print last git commit and local changes, useful for capturing state of
      'fly execute' runs.

--- ci/concourse/tasks/rake.sh
+++ ci/concourse/tasks/rake.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
-set -e -x
+set -e
 
 export BOSH_CLI_SILENCE_SLOW_LOAD_WARNING=true
 
-env | sort
-
+source $(dirname $0)/environment.sh
 source /etc/profile.d/chruby.sh
 chruby $RUBY_VERSION
+
+env | sort
+
 cd bosh-src
+echo "--- Show git state in `pwd` @ `date` ---"
+echo " -> last commit..."
+git log -1
+echo "    ---"
+echo " -> local changes (e.g., from 'fly execute')..."
+git status
+echo "    ---"
 
 echo "--- Starting bundle install in `pwd` @ `date` ---"
 if [ -f .bundle/config ]; then
   echo ".bundle/config:"
   cat .bundle/config
 fi
-
 bundle install
+
 echo "--- Starting rake task @ `date` ---"
 bundle exec rake "$@"
