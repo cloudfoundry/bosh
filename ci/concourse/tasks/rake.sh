46b5eaa092e278609a990f389a547bc9ddc2f31a
Corey Innis <cinnis@pivotal.io>
2015-06-05 16:42:48 -0700

    Fixes ENV var check in ci task for rake.

--- ci/concourse/tasks/rake.sh
+++ ci/concourse/tasks/rake.sh
@@ -17,15 +17,17 @@ env | sort
 
 echo "--- Show git state in `pwd` @ `date` ---"
 # optionally switch to branch (e.g., from 'fly execute')
-if [ -z ${GIT_BRANCH+x} ] ; then
+if [ -v GIT_BRANCH ] ; then
+  echo "--> switching to $GIT_BRANCH..."
   git checkout $GIT_BRANCH
+  echo "---"
 fi
-echo " -> last commit..."
+echo "--> last commit..."
 git log -1
-echo "    ---"
-echo " -> local changes (e.g., from 'fly execute')..."
+echo "---"
+echo "--> local changes (e.g., from 'fly execute')..."
 git status
-echo "    ---"
+echo "---"
 
 echo "--- Starting bundle install in `pwd` @ `date` ---"
 if [ -f .bundle/config ] ; then
