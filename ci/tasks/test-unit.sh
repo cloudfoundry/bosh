8d137a20e1fbce5ae4d1cf836dce3b44f6642251
Corey Innis <cinnis@pivotal.io>
2015-06-10 12:01:47 -0700

    Simplifies the concourse test-unit task
    Extracts the requirements to run the spec:unit rake task from within
    concourse to make it easier to understand what is needed.  We are no
    longer running through rake.sh and run.sh and passing things around via
    env variables which should make it clearer for outside developers to
    re-create the test runs.
    
    Also extracts shared functionality to functions in ci/tasks/utils.sh to
    dry up the tasks and maintain readability.
    
    [#95665222](https://www.pivotaltracker.com/story/show/95665222)
    Signed-off-by: Jonathan Barnes <jbarnes@pivotal.io>

---
+++ ci/tasks/test-unit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+source bosh-src/ci/tasks/utils.sh
+
+check_param RUBY_VERSION
+
+source /etc/profile.d/chruby.sh
+chruby $RUBY_VERSION
+
+cd bosh-src
+
+print_git_state
+
+bundle install --local
+
+bundle exec rake go spec:unit
---
+++ ci/tasks/utils.sh
@@ -0,0 +1,17 @@
+check_param() {
+  local name=$1
+  local value=$(eval echo '$'$name)
+  if [ "$value" == 'replace-me' ]; then
+    echo "environment variable $name must be set"
+    exit 1
+  fi
+}
+
+print_git_state() {
+  echo "--> last commit..."
+  TERM=xterm-256color git log -1
+  echo "---"
+  echo "--> local changes (e.g., from 'fly execute')..."
+  TERM=xterm-256color git status --verbose
+  echo "---"
+}
