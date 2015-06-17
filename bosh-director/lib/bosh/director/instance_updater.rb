cloudfoundry/bosh

commit 185b70bcee566c2913a8b47110ef3a023a0edcfc

Refactor: Removed explicit estimation of step count
The existing logic was already incorrect when we found it
(was claiming 7 or 8 steps when the real count was now
8 or 9) and we were adding another step.

The new way will work without needing to remember to update
the logic that estimates how many steps will be taken.

Also-authored-by: Jamil Shamy <jshamy@pivotal.io>
Signed-off-by: Jonathan Fuerth <jfuerth@pivotal.io>

bosh-director/lib/bosh/director/instance_updater.rb
@@ -4,7 +4,6 @@ module Bosh::Director
   class InstanceUpdater
     include DnsHelper
 
-    UPDATE_STEPS = 7
     WATCH_INTERVALS = 10
 
     attr_reader :current_state
@@ -33,55 +32,59 @@ module Bosh::Director
       @agent = AgentClient.with_defaults(@vm.agent_id)
     end
 
-    def step
-      yield
-      report_progress
+    def report_progress(num_steps)
+      @event_log_task.advance(100.0 / num_steps)
     end
 
-    def report_progress
-      @event_log_task.advance(100.0 / update_steps())
-    end
-
-    def update_steps
-      @instance.job_changed? || @instance.packages_changed? ? UPDATE_STEPS + 1 : UPDATE_STEPS
-    end
-
-    def update(options = {})
+    def update_steps(options = {})
+      steps = []
       @canary = options.fetch(:canary, false)
-
-      @logger.info("Updating instance #{@instance}, changes: #{@instance.changes.to_a.join(', ')}")
-      trusted_certs_change_only = trusted_certs_change_only?
 
       # Optimization to only update DNS if nothing else changed.
       if dns_change_only?
-        update_dns
-        return
+        steps << proc { update_dns }
+        return steps
       end
 
-      step { Preparer.new(@instance, agent, @logger).prepare }
-      step { stop }
-      step { take_snapshot }
+      steps << proc { Preparer.new(@instance, agent, @logger).prepare }
+      steps << proc { stop }
+      steps << proc { take_snapshot }
 
       if @target_state == "detached"
-        vm_updater.detach
-        return
+        steps << proc { vm_updater.detach }
+        return steps
       end
 
-      step { recreate_vm(nil) }
-      step { update_networks }
-      step { update_dns }
-      step { update_persistent_disk }
-      step { update_settings }
+      steps << proc { recreate_vm(nil) }
+      steps << proc { update_networks }
+      steps << proc { update_dns }
+      steps << proc { update_persistent_disk }
+      steps << proc { update_settings }
 
-      if !trusted_certs_change_only
-        VmMetadataUpdater.build.update(@vm, {})
-        step { apply_state(@instance.spec) }
-        RenderedJobTemplatesCleaner.new(@instance.model, @blobstore).clean
+      if !trusted_certs_change_only?
+        steps << proc {
+          VmMetadataUpdater.build.update(@vm, {})
+          apply_state(@instance.spec)
+          RenderedJobTemplatesCleaner.new(@instance.model, @blobstore).clean
+        }
       end
 
-      start! if need_start?
+      steps << proc { start! if need_start? }
 
-      step { wait_until_running }
+      steps << proc { wait_until_running }
+
+      steps
+    end
+
+    def update(options = {})
+      steps = update_steps(options)
+
+      @logger.info("Updating instance #{@instance}, changes: #{@instance.changes.to_a.join(', ')}")
+
+      for step in steps
+        step.call
+        report_progress(steps.length)
+      end
 
       if @target_state == "started" && current_state["job_state"] != "running"
         raise AgentJobNotRunning, "`#{@instance}' is not running after update"

bosh-director/spec/unit/instance_updater_spec.rb
@@ -92,36 +92,9 @@ module Bosh::Director
       let(:event_log_task) { instance_double('Bosh::Director::EventLog::Task') }
 
       it 'advances the ticker' do
-        allow(subject).to receive(:update_steps).and_return(200)
+        allow(subject).to receive(:update_steps).and_return(['dummy_step'] * 200)
         expect(event_log_task).to receive(:advance).with(0.5)
-        subject.report_progress
-      end
-    end
-
-    describe '#update_steps' do
-      context 'when neither the job nor the packages have changed' do
-        describe '#update_steps' do
-          subject { super().update_steps }
-          it { is_expected.to eq(described_class::UPDATE_STEPS) }
-        end
-      end
-
-      context 'when the job has changed' do
-        let(:job_changed) { true }
-
-        describe '#update_steps' do
-          subject { super().update_steps }
-          it { is_expected.to eq(described_class::UPDATE_STEPS + 1) }
-        end
-      end
-
-      context 'when the packages have changed' do
-        let(:packages_changed) { true }
-
-        describe '#update_steps' do
-          subject { super().update_steps }
-          it { is_expected.to eq(described_class::UPDATE_STEPS + 1) }
-        end
+        subject.report_progress(200)
       end
     end
 
@@ -160,10 +133,16 @@ module Bosh::Director
       context 'with only a dns change' do
         let(:changes) { [:dns].to_set }
 
+        before do
+          allow(subject).to receive(:current_state).and_return({'job_state' => 'running'})
+        end
+
         it 'should only call update_dns' do
+          steps = subject.update_steps
+          expect(steps.length).to eq 1
+
           expect(subject).to receive(:update_dns)
-          expect(subject).to_not receive(:step)
-          subject.update
+          steps[0].call
         end
 
         it 'does not prepare instance' do

commit 2996bb678b71ab062bd04b0bc69e67a17ced6150

Fixes rubocop failure introduced from commit: 185b70b
Signed-off-by: Corey Innis <cinnis@pivotal.io>

bosh-director/lib/bosh/director/instance_updater.rb
@@ -81,7 +81,7 @@ module Bosh::Director
 
       @logger.info("Updating instance #{@instance}, changes: #{@instance.changes.to_a.join(', ')}")
 
-      for step in steps
+      steps.each do |step|
         step.call
         report_progress(steps.length)
       end