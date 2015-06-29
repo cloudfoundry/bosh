94fc36d5075e2165b1addbcc65a7ba36cc82ca8b
Tyler Schultz <tschultz@pivotal.io>
2015-06-29 09:46:20 -0700

    Bump length of generated rsa keys to 2048
    [Finishes #97684304](https://www.pivotaltracker.com/story/show/97684304)

--- bosh_common/spec/assets/ca/generate.sh
+++ bosh_common/spec/assets/ca/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl req -nodes -new -newkey rsa:1024 -out ca.csr -keyout ca.key -subj '/C=US/O=Pivotal/CN=myapp.dev102.cf.com'
+openssl req -nodes -new -newkey rsa:2048 -out ca.csr -keyout ca.key -subj '/C=US/O=Pivotal/CN=myapp.dev102.cf.com'
 openssl x509 -req -days 3650 -in ca.csr -signkey ca.key -out ca.pem
