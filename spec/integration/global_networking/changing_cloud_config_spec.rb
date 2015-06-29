b1e044ed3f9514f83fde3b304950f3f6a5cbbd99
Tim Hausler <thausler@pivotal.io>
2015-06-29 10:00:13 -0700

    Adds coverage for shifting IP ranges and redeploying
    Only VMs that need to move get moved.
    
    [#95199966](https://www.pivotaltracker.com/story/show/95199966)
    
    Signed-off-by: Colin Obyrne <cobyrne@pivotal.io>

--- spec/integration/global_networking/changing_cloud_config_spec.rb
+++ spec/integration/global_networking/changing_cloud_config_spec.rb
@@ -10,7 +10,7 @@ describe 'Changing cloud config', type: :integration do
   end
 
   describe 'changing the cloud config while deploying' do
-    it 'should continue to use the original cloud config' do
+    it 'should continue to use the original cloud config when deploying a job' do
       cloud_config = Bosh::Spec::NetworkingManifest.cloud_config(available_ips: 1)
       deployment_manifest = Bosh::Spec::NetworkingManifest.deployment_manifest(instances: 1)
 
@@ -22,7 +22,7 @@ describe 'Changing cloud config', type: :integration do
       Bosh::Spec::DeployHelper.wait_for_deploy_to_succeed(task_id)
     end
 
-    it 'should successfully finish errand' do
+    it 'should continue to use the original cloud config when running an errand' do
       cloud_config = Bosh::Spec::NetworkingManifest.cloud_config(available_ips: 1)
       manifest_with_errand = Bosh::Spec::NetworkingManifest.errand_manifest(instances: 1)
       upload_cloud_config(cloud_config_hash: cloud_config)
--- spec/support/networking_manifest_helper.rb
+++ spec/support/networking_manifest_helper.rb
@@ -10,13 +10,20 @@ module Bosh::Spec
     def self.cloud_config(opts)
       ip_range = NetAddr::CIDR.create('192.168.1.0/24')
       cloud_config = Bosh::Spec::Deployments.simple_cloud_config
-      ip_to_reserve_from = ip_range.nth(opts.fetch(:available_ips)+2) # first IP is gateway, range is inclusive, so +2
+      ip_range_shift = opts.fetch(:shift_ip_range_by, 0)
+      ip_to_reserve_from = ip_range.nth(opts.fetch(:available_ips)+2+ip_range_shift) # first IP is gateway, range is inclusive, so +2
+
+      reserved_ips = ["#{ip_to_reserve_from}-#{ip_range.last}"]
+      if ip_range_shift > 0
+        reserved_ips << "#{ip_range.nth(2)}-#{ip_range.nth(ip_range_shift+1)}"
+      end
+
       cloud_config['networks'].first['subnets'] = [{
           'range' => ip_range.to_s,
           'gateway' => ip_range.nth(1),
           'dns' => [],
           'static' => [],
-          'reserved' => ["#{ip_to_reserve_from}-#{ip_range.last}"],
+          'reserved' => reserved_ips,
           'cloud_properties' => {},
         }]
       cloud_config
