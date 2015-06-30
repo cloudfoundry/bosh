88bc28eb787d5329eb96542bcef767ab872f1131
Tim Hausler <thausler@pivotal.io>
2015-06-30 13:59:29 -0700

    Added test coverage for legacy networking to match global networking.
    [#95199966](https://www.pivotaltracker.com/story/show/95199966)
    
    Signed-off-by: Colin Obyrne <cobyrne@pivotal.io>

--- spec/integration/global_networking/changing_ip_range_spec.rb
+++ spec/integration/global_networking/changing_ip_range_spec.rb
@@ -36,5 +36,30 @@ describe 'Changing ip ranges', type: :integration do
       expect(new_instance_0.cid).to_not eq(original_instance_0.cid)
       expect(new_instance_1.cid).to eq(original_instance_1.cid)
     end
+
+    context 'using legacy network configuration (no cloud config)' do
+      it 'should recreate VMs outside of the range in the new range, but not touch VMs that are ok' do
+        deployment_manifest = Bosh::Spec::NetworkingManifest.legacy_deployment_manifest(instances: 2, available_ips: 2)
+        deploy_simple_manifest(manifest_hash: deployment_manifest)
+
+        original_instance_0 = director.vms.find { |vm| vm.job_name_index == 'foobar/0' }
+        original_instance_1 = director.vms.find { |vm| vm.job_name_index == 'foobar/1' }
+
+        expect(original_instance_0.ips).to eq('192.168.1.2')
+        expect(original_instance_1.ips).to eq('192.168.1.3')
+
+        deployment_manifest = Bosh::Spec::NetworkingManifest.legacy_deployment_manifest(instances: 2, available_ips: 2, shift_ip_range_by: 1)
+        deploy_simple_manifest(manifest_hash: deployment_manifest)
+
+        new_instance_0 = director.vms.find { |vm| vm.job_name_index == 'foobar/0' }
+        new_instance_1 = director.vms.find { |vm| vm.job_name_index == 'foobar/1' }
+
+        expect(new_instance_0.ips).to eq('192.168.1.4')
+        expect(new_instance_1.ips).to eq('192.168.1.3')
+
+        expect(new_instance_0.cid).to_not eq(original_instance_0.cid)
+        expect(new_instance_1.cid).to eq(original_instance_1.cid)
+      end
+    end
   end
 end
--- spec/support/networking_manifest_helper.rb
+++ spec/support/networking_manifest_helper.rb
@@ -7,9 +7,22 @@ module Bosh::Spec
       manifest
     end
 
+    def self.legacy_deployment_manifest(opts)
+      manifest = Bosh::Spec::Deployments.legacy_manifest
+      manifest['name'] = opts.fetch(:name, 'simple')
+      manifest['jobs'].first['instances'] = opts.fetch(:instances, 1)
+      manifest['networks'].first['subnets'] = [make_subnet(opts)]
+      manifest
+    end
+
     def self.cloud_config(opts)
-      ip_range = NetAddr::CIDR.create('192.168.1.0/24')
       cloud_config = Bosh::Spec::Deployments.simple_cloud_config
+      cloud_config['networks'].first['subnets'] = [make_subnet(opts)]
+      cloud_config
+    end
+
+    def self.make_subnet(opts)
+      ip_range = NetAddr::CIDR.create('192.168.1.0/24')
       ip_range_shift = opts.fetch(:shift_ip_range_by, 0)
       ip_to_reserve_from = ip_range.nth(opts.fetch(:available_ips)+2+ip_range_shift) # first IP is gateway, range is inclusive, so +2
 
@@ -18,15 +31,14 @@ module Bosh::Spec
         reserved_ips << "#{ip_range.nth(2)}-#{ip_range.nth(ip_range_shift+1)}"
       end
 
-      cloud_config['networks'].first['subnets'] = [{
-          'range' => ip_range.to_s,
-          'gateway' => ip_range.nth(1),
-          'dns' => [],
-          'static' => [],
-          'reserved' => reserved_ips,
-          'cloud_properties' => {},
-        }]
-      cloud_config
+      {
+        'range' => ip_range.to_s,
+        'gateway' => ip_range.nth(1),
+        'dns' => [],
+        'static' => [],
+        'reserved' => reserved_ips,
+        'cloud_properties' => {},
+      }
     end
 
     def self.errand_manifest(opts)
