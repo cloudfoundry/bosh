39dc13b0f6e564f2da38e904038755c8f6922c34
Colin Obyrne <cobyrne@pivotal.io>
2015-07-07 11:28:19 -0700

    Download adapter now retries establishing a connection after 60 sec.
    Builds were failing on CI, and we think they were timing out on
    connecting, not reading, so this should fix that.
    
    [#98510142](https://www.pivotaltracker.com/story/show/98510142)
    
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- bosh-dev/lib/bosh/dev/download_adapter.rb
+++ bosh-dev/lib/bosh/dev/download_adapter.rb
@@ -18,49 +18,67 @@ module Bosh::Dev
 
     def download_file(uri, write_path)
       proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : NullUri.new
-
       proxy = NullUri.new if bypass_proxy?(uri)
 
-      Net::HTTP.start(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password) do |http|
+      open_http_connection(proxy, uri) do |http|
         http.read_timeout = 300
-
-        begin
-          File.open(write_path, 'wb') do |file|
-            tries = 0
-            begin
-              headers = {}
-              headers['Range'] = "bytes=#{file.tell}-" if tries > 0
-              http.request_get(uri.request_uri, headers) do |response|
-                unless response.kind_of? Net::HTTPSuccess
-                  raise "error #{response.code} while downloading '#{uri}'"
-                end
-
-                starting_byte = 0
-                starting_byte = response.content_range.first if response['Content-Range']
-                file.seek(starting_byte)
-                file.truncate(starting_byte)
-                if tries > 0
-                  @logger.info("Resuming download of #{uri} from #{starting_byte} bytes")
-                end
-
-                response.read_body do |chunk|
-                  file.write(chunk)
-                end
-              end
-            rescue Timeout::Error => e
-              @logger.info("Download of #{uri} timed out.")
-
-              raise e unless tries < 3
-              tries += 1
-
-              @logger.debug("Retrying ...")
-              retry
-            end
-          end
-        rescue Exception => e
-          File.delete(write_path) if File.exist?(write_path)
-          raise e
+        File.open(write_path, 'wb') do |file|
+          write_to_file(http, uri, file)
         end
+      end
+    rescue Exception => e
+      File.delete(write_path) if File.exist?(write_path)
+      raise e
+    end
+
+    def open_http_connection(proxy, uri, &block)
+      tries = 0
+      begin
+        http = Timeout.timeout(60) do
+          Net::HTTP.start(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
+        end
+      rescue Timeout::Error => e
+        @logger.info("Connecting to #{uri} timed out.")
+        raise e unless tries < 3
+        tries += 1
+        @logger.debug("Retrying ...")
+        retry
+      end
+      block.call(http)
+    ensure
+      http.finish() if defined?(http)
+    end
+
+    def write_to_file(http, uri, file)
+      tries = 0
+      begin
+        headers = {}
+        headers['Range'] = "bytes=#{file.tell}-" if tries > 0
+        http.request_get(uri.request_uri, headers) do |response|
+          unless response.kind_of? Net::HTTPSuccess
+            raise "error #{response.code} while downloading '#{uri}'"
+          end
+
+          starting_byte = 0
+          starting_byte = response.content_range.first if response['Content-Range']
+          file.seek(starting_byte)
+          file.truncate(starting_byte)
+          if tries > 0
+            @logger.info("Resuming download of #{uri} from #{starting_byte} bytes")
+          end
+
+          response.read_body do |chunk|
+            file.write(chunk)
+          end
+        end
+      rescue Timeout::Error => e
+        @logger.info("Download of #{uri} timed out.")
+
+        raise e unless tries < 3
+        tries += 1
+
+        @logger.debug("Retrying ...")
+        retry
       end
     end
 
