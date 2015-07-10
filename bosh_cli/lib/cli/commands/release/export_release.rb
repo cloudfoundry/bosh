93ff58d85e518c9eeb67260ce497c4995af3b330
Nader Ziada <nziada@pivotal.io>
2015-07-10 16:50:42 -0400

    The export release command downloads the tarball automatically
    [#97328978]
    https://www.pivotaltracker.com/story/show/97328978
    
    Signed-off-by: Sukhil Suresh <ssuresh@pivotal.io>
    
    Signed-off-by: Sukhil Suresh <ssuresh@pivotal.io>

--- bosh_cli/lib/cli/commands/release/export_release.rb
+++ bosh_cli/lib/cli/commands/release/export_release.rb
@@ -1,5 +1,6 @@
 require 'cli/name_version_pair'
 require 'cli/client/export_release_client'
+require 'yajl'
 
 module Bosh::Cli::Command
   module Release
@@ -21,6 +22,33 @@ module Bosh::Cli::Command
         client = Bosh::Cli::Client::ExportReleaseClient.new(director)
         status, task_id = client.export(manifest.name, release.name, release.version, stemcell_os, stemcell.version)
         task_report(status, task_id)
+
+        task_result_file = director.get_task_result_log(task_id)
+        task_result = Yajl::Parser.parse(task_result_file)
+        tarball_blobstore_id = task_result['blobstore_id']
+        tarball_sha1 = task_result['sha1']
+
+        tarball_file_name = "release-#{release.name}-#{release.version}-on-#{stemcell_os}-stemcell-#{stemcell.version}.tgz"
+        tarball_file_path = File.join(Dir.pwd, tarball_file_name)
+
+        nl
+        progress_renderer.start(tarball_file_name, "downloading...")
+        tmpfile = director.download_resource(tarball_blobstore_id)
+        File.open(tarball_file_path, "wb") do |file|
+          file << File.open(tmpfile).read
+        end
+        progress_renderer.finish(tarball_file_name, "downloaded")
+
+        if file_checksum(tarball_file_path) != tarball_sha1
+          err("Checksum mismatch for downloaded blob `#{tarball_file_path}'")
+        end
+
+      end
+
+      # Returns file SHA1 checksum
+      # @param [String] path File path
+      def file_checksum(path)
+        Digest::SHA1.file(path).hexdigest
       end
     end
   end
--- spec/integration/release/export_release_spec.rb
+++ spec/integration/release/export_release_spec.rb
@@ -95,4 +95,32 @@ describe 'export release', type: :integration do
       expect(files).to include("compiled_packages","compiled_release.MF","jobs")
     end
   end
+
+  it 'downloads a tarball from the blobstore to the current directory' do
+    Dir.chdir(ClientSandbox.test_release_dir) do
+      File.open('config/final.yml', 'w') do |final|
+        final.puts YAML.dump(
+                       'blobstore' => {
+                           'provider' => 'local',
+                           'options' => { 'blobstore_path' => current_sandbox.blobstore_storage_dir },
+                       },
+                   )
+      end
+    end
+
+    out = bosh_runner.run("export release appcloud/0.1 toronto-os/1")
+    expect(out).to match /release-appcloud-0.1-on-toronto-os-stemce... downloading.../
+    expect(out).to match /release-appcloud-0.1-on-toronto-os-stemce... downloaded/
+
+    dir = File.join(Bosh::Dev::Sandbox::Workspace.dir, "client-sandbox", "bosh_work_dir")
+    files = Dir.entries(dir)
+    expect(files).to include("release-appcloud-0.1-on-toronto-os-stemcell-1.tgz")
+
+    Dir.mktmpdir do |temp_dir|
+      tarball_path = File.join(dir, "release-appcloud-0.1-on-toronto-os-stemcell-1.tgz")
+      `tar xzf #{tarball_path} -C #{temp_dir}`
+      files = Dir.entries(temp_dir)
+      expect(files).to include("compiled_packages","compiled_release.MF","jobs")
+    end
+  end
 end
