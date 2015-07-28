ca776923e696c06b7ce1dd89fa56b5d63ff5ce04
Colin Obyrne <cobyrne@pivotal.io>
2015-07-28 16:15:41 -0700

    Make bad dependencies more obvious
    Use null objects since we're not expecting them to be used for anything.
    
    We need the IpProviderFactory and GlobalNetworkResolver when parsing so
    we can give subnets an IpProvider. This is kind of crazy, especially in
    api/cloud_config_manager where we're really just trying to validate a
    manifest, not reserve any IPs.
    
    IP reservation probably needs a refactor so that parsing no longer
    depends on these objects.
    
    Signed-off-by: Luke Woydziak <luke.woydziak@emc.com>

--- bosh-director/lib/bosh/director/api/cloud_config_manager.rb
+++ bosh-director/lib/bosh/director/api/cloud_config_manager.rb
@@ -21,21 +21,24 @@ module Bosh
         private
 
         def validate_manifest!(cloud_config)
-          # FIXME: pass in null ip/networking objects
-          # these objects won't work if you actually try to reserve IPs with them since the cloud_planner is empty,
-          # but we really just need to validate the manifest, we don't care about the subnets being able to reserve IPs here
-          cloud_planner = Bosh::Director::DeploymentPlan::CloudPlanner.new({
-              networks: [],
-              disk_pools: [],
-              availability_zones: [],
-              resource_pools: [],
-              compilation: nil,
-            })
-          ip_provider_factory = Bosh::Director::DeploymentPlan::IpProviderFactory.new(cloud_planner.model, Config.logger, global_networking: cloud_planner.using_global_networking?)
-          global_network_resolver = Bosh::Director::DeploymentPlan::GlobalNetworkResolver.new(cloud_planner)
+          # FIXME: we really just need to validate the manifest, we don't care about the subnets being able to reserve IPs here
+          ip_provider_factory = NullIpProviderFactory.new
+          global_network_resolver = NullGlobalNetworkResolver.new
 
           parser = Bosh::Director::DeploymentPlan::CloudManifestParser.new(Config.logger)
           _ = parser.parse(cloud_config.manifest, ip_provider_factory, global_network_resolver) # valid if this doesn't blow up
+        end
+
+        class NullIpProviderFactory
+          def create(*args)
+            nil
+          end
+        end
+
+        class NullGlobalNetworkResolver
+          def reserved_legacy_ranges(something)
+            []
+          end
         end
       end
     end
