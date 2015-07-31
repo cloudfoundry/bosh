8528e4f890afac99f0972d77b30a90adef2eb119
Xuebin He <xuebin.he@emc.com>
2015-07-31 11:28:46 -0700

    Drop foreign key constraint and foreign key column in ip_addresses.
    CI was failing on drop_column but we were unable to reproduce it
    locally.  We could use drop_foreign_key but it's not supported by some
    mysql versions, according to a previous migration.
    
    [#98156130](https://www.pivotaltracker.com/story/show/98156130)
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- bosh-director/db/migrations/director/20150729170853_add_instance_to_ip_addressses.rb
+++ bosh-director/db/migrations/director/20150729170853_add_instance_to_ip_addressses.rb
@@ -2,6 +2,7 @@ Sequel.migration do
   change do
     alter_table(:ip_addresses) do
       add_foreign_key :instance_id, :instances
+      drop_constraint :ip_addresses_deployment_id_fkey
       drop_column :deployment_id
     end
   end
