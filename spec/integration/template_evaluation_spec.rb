1bf8f5815ec9c648f5746cbbf59ce3702e20a550
Tim Hausler <thausler@pivotal.io>
2015-08-06 09:44:57 -0700

    Added uuids for legacy instances in database migration.
    [#100245910](https://www.pivotaltracker.com/story/show/100245910)
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- spec/integration/template_evaluation_spec.rb
+++ spec/integration/template_evaluation_spec.rb
@@ -24,4 +24,36 @@ describe 'template', type: :integration do
     puts template.inspect
     expect(template['id']).to match /[a-f0-9\-]/
   end
+
+
+  it 'gives VMs the same id on `deploy --recreate`' do
+    manifest_hash = Bosh::Spec::Deployments.simple_manifest
+    manifest_hash['jobs'] = [
+      {
+        'name' => 'id_job',
+        'templates' => ['name' => 'id_job'],
+        'resource_pool' => 'a',
+        'instances' => 1,
+        'networks' => [{
+            'name' => 'a',
+          }],
+        'properties' => {},
+      }
+    ]
+
+    deploy_from_scratch(manifest_hash: manifest_hash)
+
+    id_vm = director.vm('id_job/0')
+    template = YAML.load(id_vm.read_job_template('id_job', 'config.yml'))
+    original_id = template['id']
+    expect(original_id).to match /[a-f0-9\-]/
+
+    deploy_simple_manifest(manifest_hash: manifest_hash, recreate: true)
+    id_vm = director.vm('id_job/0')
+    template = YAML.load(id_vm.read_job_template('id_job', 'config.yml'))
+    new_id = template['id']
+    expect(new_id).to match /[a-f0-9\-]/
+
+    expect(new_id).to eq(original_id)
+  end
 end
