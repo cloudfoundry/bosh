0f206abcb9945ce04addd796f9ae40f79f5f972b
Maria Shaldibina <mshaldibina@pivotal.io>
2015-08-06 15:36:06 -0700

    Fill up tests for network reservations
    Signed-off-by: Luke Woydziak <luke.woydziak@emc.com>

--- bosh-director/spec/unit/network_reservation_spec.rb
+++ bosh-director/spec/unit/network_reservation_spec.rb
@@ -5,55 +5,100 @@ module Bosh::Director
     let(:instance) { instance_double(DeploymentPlan::Instance) }
     let(:network) { instance_double(DeploymentPlan::Network) }
 
-    describe :initialize do
-      it 'should store the IP as an int' do
-        reservation = StaticNetworkReservation.new(instance, network, '0.0.0.1')
-        expect(reservation.ip).to eq(1)
+    describe StaticNetworkReservation do
+      let(:reservation) { StaticNetworkReservation.new(instance, network, '0.0.0.1') }
+
+      describe :bind_existing do
+        it "should bind to the static reservation if it's valid" do
+          other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
+          allow(other).to receive(:reserved?).and_return(true)
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(true)
+          expect(reservation.ip).to eq(1)
+        end
+
+        it 'should not take the reservation if it is not existing' do
+          other = DynamicNetworkReservation.new(instance, network)
+          allow(other).to receive(:reserved?).and_return(true)
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(false)
+        end
+
+        it 'should not take the existing reservation if the IP differs' do
+          other = ExistingNetworkReservation.new(instance, network, '0.0.0.2')
+          allow(other).to receive(:reserved?).and_return(true)
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(false)
+          expect(reservation.ip).to eq(1)
+        end
+
+        it 'should not take the reservation if it is not reserved' do
+          other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(false)
+          expect(reservation.ip).to eq(1)
+        end
       end
     end
 
-    describe :bind_existing do
-      it "should bind to the dynamic reservation if it's valid" do
-        reservation = DynamicNetworkReservation.new(instance, network)
-        other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
-        allow(other).to receive(:reserved?).and_return(true)
-        reservation.bind_existing(other)
-        expect(reservation.reserved?).to eq(true)
-        expect(reservation.ip).to eq(1)
-      end
+    describe DynamicNetworkReservation do
+      let(:reservation) { DynamicNetworkReservation.new(instance, network) }
 
-      it "should bind to the static reservation if it's valid" do
-        reservation = StaticNetworkReservation.new(instance, network, '0.0.0.1')
-        other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
-        allow(other).to receive(:reserved?).and_return(true)
-        reservation.bind_existing(other)
-        expect(reservation.reserved?).to eq(true)
-        expect(reservation.ip).to eq(1)
-      end
+      describe :bind_existing do
+        it "should bind to the dynamic reservation if it's valid" do
+          other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
+          allow(other).to receive(:reserved?).and_return(true)
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(true)
+          expect(reservation.ip).to eq(1)
+        end
 
-      it 'should not take the reservation if it is not existing' do
-        reservation = StaticNetworkReservation.new(instance, network, '0.0.0.1')
-        other = DynamicNetworkReservation.new(instance, network)
-        allow(other).to receive(:reserved?).and_return(true)
-        reservation.bind_existing(other)
-        expect(reservation.reserved?).to eq(false)
-      end
+        it 'should not take the reservation if it is not existing' do
+          other = StaticNetworkReservation.new(instance, network, '0.0.0.1')
+          allow(other).to receive(:reserved?).and_return(true)
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(false)
+        end
 
-      it 'should not take the existing reservation for static if the IP differs' do
-        reservation = StaticNetworkReservation.new(instance, network, '0.0.0.1')
-        other = ExistingNetworkReservation.new(instance, network, '0.0.0.2')
-        allow(other).to receive(:reserved?).and_return(true)
-        reservation.bind_existing(other)
-        expect(reservation.reserved?).to eq(false)
-        expect(reservation.ip).to eq(1)
+        it 'should not take the reservation if it is not reserved' do
+          other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
+          reservation.bind_existing(other)
+          expect(reservation.reserved?).to eq(false)
+          expect(reservation.ip).to eq(nil)
+        end
       end
+    end
 
-      it 'should not take the reservation if it is not reserved' do
-        reservation = DynamicNetworkReservation.new(instance, network)
-        other = ExistingNetworkReservation.new(instance, network, '0.0.0.1')
-        reservation.bind_existing(other)
-        expect(reservation.reserved?).to eq(false)
-        expect(reservation.ip).to eq(nil)
+    describe ExistingNetworkReservation do
+      let(:reservation) { ExistingNetworkReservation.new(instance, network, '0.0.0.1') }
+      describe :reserve do
+        it 'reserves on a network' do
+          expect(network).to receive(:reserve).with(reservation)
+          reservation.reserve
+          expect(reservation.reserved?).to eq(true)
+        end
+
+        context 'when IP is outside of subnet range' do
+          before do
+            allow(network).to receive(:reserve).with(reservation).and_raise(NetworkReservationIpOutsideSubnet)
+          end
+
+          it 'does not reserve IP' do
+            reservation.reserve
+            expect(reservation.reserved?).to eq(false)
+          end
+        end
+
+        context 'when IP is in reserved range' do
+          before do
+            allow(network).to receive(:reserve).with(reservation).and_raise(NetworkReservationIpReserved)
+          end
+
+          it 'does not reserve IP' do
+            reservation.reserve
+            expect(reservation.reserved?).to eq(false)
+          end
+        end
       end
     end
   end
