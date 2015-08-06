46be0f2ae111b63aa4fb939d1e550dad800f64a6
Maria Shaldibina <mshaldibina@pivotal.io>
2015-08-06 15:47:18 -0700

    Detached VM should keep its IP
    Thanks to binding instances instead of VMs
    
    [#96245208](https://www.pivotaltracker.com/story/show/96245208)
    
    Signed-off-by: Luke Woydziak <luke.woydziak@emc.com>

--- spec/integration/vm_state_spec.rb
+++ spec/integration/vm_state_spec.rb
@@ -21,5 +21,27 @@ describe 'vm state', type: :integration do
       expect(director.vms.map(&:job_name_index)).to contain_exactly('foobar/0', 'foobar/1', 'foobar/2')
       expect(current_sandbox.cpi.disk_cids).to eq(disks_before_detaching)
     end
+
+    it 'keeps IP reservation' do
+      manifest_hash = Bosh::Spec::NetworkingManifest.deployment_manifest(instances: 1)
+      deploy_from_scratch(manifest_hash: manifest_hash)
+      deployed_vms = director.vms
+      expect(deployed_vms.size).to eq(1)
+      expect(deployed_vms.first.ips).to eq('192.168.1.2')
+
+      expect(bosh_runner.run('stop foobar 0 --hard')).to match %r{foobar/0 has been detached}
+      expect( director.vms.size).to eq(0)
+
+      manifest_hash = Bosh::Spec::NetworkingManifest.deployment_manifest(instances: 2)
+      deploy_simple_manifest(manifest_hash: manifest_hash)
+      deployed_vms = director.vms
+      expect(deployed_vms.size).to eq(1)
+      expect(deployed_vms.first.ips).to eq('192.168.1.3')
+
+      bosh_runner.run('start foobar 0')
+      deployed_vms = director.vms
+      expect(deployed_vms.size).to eq(2)
+      expect(deployed_vms.map(&:ips)).to match_array(['192.168.1.2', '192.168.1.3'])
+    end
   end
 end
