6954c2530550ed937933c68ad322499bc5c7d392
Wagner Camarao <wcamarao@vmware.com>
2015-08-10 11:05:01 -0700

    Pass connection options as is instead of converting to integer
    [#100786866](https://www.pivotaltracker.com/story/show/100786866)
    
    Signed-off-by: Edwin Xie <exie@pivotal.io>

--- bosh_openstack_cpi/spec/integration/lifecycle_spec.rb
+++ bosh_openstack_cpi/spec/integration/lifecycle_spec.rb
@@ -49,9 +49,9 @@ describe Bosh::OpenStackCloud::Cloud do
         'config_drive' => config_drive,
         'ignore_server_availability_zone' => @ignore_server_az,
         'connection_options' => {
-          'connect_timeout' => @connect_timeout.to_i,
-          'read_timeout' => @read_timeout.to_i,
-          'write_timeout' => @write_timeout.to_i,
+          'connect_timeout' => @connect_timeout,
+          'read_timeout' => @read_timeout,
+          'write_timeout' => @write_timeout,
         }
       },
       'registry' => {
