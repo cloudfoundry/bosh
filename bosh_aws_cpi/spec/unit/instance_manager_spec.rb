9ddbbd6ba5abff333059ddce73bc240d5d9f4e1a
Zhou Xing <xingzhou@cn.ibm.com>
2015-08-11 15:04:48 +0800

    support creating instances with dedicated tenancy
    [#100702698](https://www.pivotaltracker.com/story/show/100702698)
    
    Signed-off-by: Hua Zhang <zhuadl@cn.ibm.com>

--- bosh_aws_cpi/spec/unit/instance_manager_spec.rb
+++ bosh_aws_cpi/spec/unit/instance_manager_spec.rb
@@ -807,6 +807,49 @@ describe Bosh::AwsCloud::InstanceManager do
           end
         end
       end
+
+      describe 'tenancy_parameter' do
+        context 'when resource pool has tenancy parameter' do
+          let(:resource_pool) do
+            {
+              'tenancy' => 'dedicated',
+            }
+          end
+
+          it 'should set the dedicated_tenancy when tenancy is dedicated' do
+            allow(aws_instances).to receive(:create) { aws_instance }
+
+            create_instance
+
+            expect(aws_instances).to have_received(:create) do |instance_params|
+              expect(instance_params[:dedicated_tenancy]).to be_truthy
+            end
+          end
+
+          it 'should not set the dedicated_tenancy when tenancy is not dedicated' do
+            resource_pool['tenancy'] = 'default'
+            allow(aws_instances).to receive(:create) { aws_instance }
+
+            create_instance
+
+            expect(aws_instances).to have_received(:create) do |instance_params|
+              expect(instance_params[:dedicated_tenancy]).to be_nil
+            end
+          end
+        end
+
+        context 'when resource pool does not have tenancy parameter' do
+          it 'should not set the dedicated_tenancy' do
+            allow(aws_instances).to receive(:create) { aws_instance }
+
+            create_instance
+
+            expect(aws_instances).to have_received(:create) do |instance_params|
+              expect(instance_params[:dedicated_tenancy]).to be_nil
+            end
+          end
+        end
+      end
     end
 
   end
