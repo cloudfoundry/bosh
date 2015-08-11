cf3cae14204d8cb2f8315acb5fd25a064a7847e4
Corey Innis <cinnis@pivotal.io>
2015-08-11 09:44:51 -0700

    Updates CI unit test runner to run as vcap user, instead of root.
    A single bosh-agent test requires non-root privileges to pass. The
    following change will update the agent submodule to bring that test back
    in... this commit is in preparation for that.
    
    [#100257616](https://www.pivotaltracker.com/story/show/100257616)

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -2,17 +2,24 @@
 
 set -e
 
-source bosh-src/ci/tasks/utils.sh
+# NOTE:
+#   1. bosh-agent unit tests must be run as non-root (syslog suite).
+#   2. at some point, the vcap user may no longer be made available by
+#      garden linux. we will need to add that user in our Dockerfile.
 
-check_param RUBY_VERSION
+sudo chown -R vcap:vcap .
+su vcap -c '
+  source bosh-src/ci/tasks/utils.sh
+  check_param RUBY_VERSION
 
-source /etc/profile.d/chruby.sh
-chruby $RUBY_VERSION
+  source /etc/profile.d/chruby.sh
+  chruby $RUBY_VERSION
 
-cd bosh-src
+  cd bosh-src
+  print_git_state
 
-print_git_state
-
-bundle install --local
-
-bundle exec rake --trace go spec:unit
+  export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
+  export GOPATH=$(pwd)/go
+  bundle install --local
+  bundle exec rake --trace go spec:unit
+'
