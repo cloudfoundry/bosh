e36eeacbc269ae8d762d61ce8806af7c9e39fb83
Maria Shaldibina <mshaldibina@pivotal.io>
2015-08-13 12:06:48 -0700

    Add info method to IpAddress model for debugging
    [#101006998](https://www.pivotaltracker.com/story/show/101006998)
    
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- bosh-director/lib/bosh/director/models/ip_address.rb
+++ bosh-director/lib/bosh/director/models/ip_address.rb
@@ -5,11 +5,16 @@ module Bosh::Director::Models
     def validate
       validates_presence :instance_id
       validates_presence :task_id
+      validates_presence :address
       validates_unique [:address, :network_name]
     end
 
     def before_create
       self.created_at ||= Time.now
+    end
+
+    def info
+      "#{self.instance.deployment.name}.#{self.instance.job}/#{self.instance.index} - #{self.network_name} - #{NetAddr::CIDR.create(self.address).ip}"
     end
   end
 end
