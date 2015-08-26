b2c913cdd6f787013dc404d82d8932f7121b3840
Colin Obyrne <cobyrne@pivotal.io>
2015-08-26 15:35:22 -0700

    add a note about a slow test
    its confusing when running it focused that theres no output for so long
    
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- spec/integration/release/create_bosh_release_spec.rb
+++ spec/integration/release/create_bosh_release_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'create bosh release', type: :integration do
   it 'creates a bosh dev release successfully' do
+    # this test is slow. it's running but there's no output until it finishes
     bosh_source_path = File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', '..')
     Dir.mktmpdir('bosh-release-test') do |test_dir|
       cloned_bosh_dir = File.join(test_dir, 'cloned-bosh')
