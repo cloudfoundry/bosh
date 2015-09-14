a9ef19c7943e398f317812c66b2578c56e2f65b9
Yulia Gaponenko <yulia.gaponenko@gmail.com>
2015-09-14 16:56:32 -0700

    Bump s3cli
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -8,6 +8,6 @@ source $base_dir/lib/prelude_config.bash
 # Download CLI source or release from github into assets directory
 cd $assets_dir
 rm -rf s3cli
-curl -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball/2c4a7f0ceef411532bb051e7ca55a490a565cf60
+curl -L -o s3cli.tar.gz https://api.github.com/repos/pivotal-golang/s3cli/tarball/d8ad6e8d05784195f2a83ebb459d6f755cf69d17
 mkdir s3cli
 tar -xzf s3cli.tar.gz -C s3cli/ --strip-components 1
