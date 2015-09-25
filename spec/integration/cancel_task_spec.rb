ff5ddd3147096027925c5c1e8847dc6e4800876a
Maria Shaldibina <mshaldibina@pivotal.io>
2015-09-25 08:29:01 -0700

    Revert commented test
    Signed-off-by: Yulia Gaponenko <yulia.gaponenko@gmail.com>

--- spec/integration/cancel_task_spec.rb
+++ spec/integration/cancel_task_spec.rb
@@ -16,23 +16,23 @@ describe 'cancel task', type: :integration do
       manifest_hash: manifest_hash,
       no_track: true
     )
-    # task_id = Bosh::Spec::OutputParser.new(deploy_result).task_id('running')
-    #
-    # director.wait_for_first_available_vm
-    #
-    # output, exit_code = bosh_runner.run("cancel task #{task_id}", return_exit_code: true)
-    # expect(output).to include("Task #{task_id} is getting canceled")
-    # expect(exit_code).to eq(0)
-    #
-    # first_vm = director.vms.first
-    # expect(first_vm).to_not be_nil
-    #
-    # first_vm.unblock_package
-    #
-    # task_event = events(task_id).last
-    # expect(task_event).to include('error')
-    # expect(task_event['error']['code']).to eq(10001)
-    # expect(task_event['error']['message']).to eq("Task #{task_id} cancelled")
+    task_id = Bosh::Spec::OutputParser.new(deploy_result).task_id('running')
+
+    director.wait_for_first_available_vm
+
+    output, exit_code = bosh_runner.run("cancel task #{task_id}", return_exit_code: true)
+    expect(output).to include("Task #{task_id} is getting canceled")
+    expect(exit_code).to eq(0)
+
+    first_vm = director.vms.first
+    expect(first_vm).to_not be_nil
+
+    first_vm.unblock_package
+
+    task_event = events(task_id).last
+    expect(task_event).to include('error')
+    expect(task_event['error']['code']).to eq(10001)
+    expect(task_event['error']['message']).to eq("Task #{task_id} cancelled")
   end
 
   def events(task_id)
