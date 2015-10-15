6386fb780a6663ea9f97091e4f803e835f66c4bf
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-10-15 09:44:42 -0700

    Adding final release for build 3104

--- bosh-registry/lib/bosh/registry/version.rb
+++ bosh-registry/lib/bosh/registry/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Registry
-    VERSION = '1.3100.0'
+    VERSION = '1.3104.0'
   end
 end
--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module AwsCliPlugin
-    VERSION = '1.3100.0'
+    VERSION = '1.3104.0'
   end
 end
