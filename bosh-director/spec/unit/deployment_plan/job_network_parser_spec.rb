b1980e9e26f56825944b2eb9b0ebdc2a0230d1d3
Shatarupa Nandi <snandi@pivotal.io>
2015-10-15 17:44:21 -0700

    Backfilling unit tests for JobNetworksParser.
    [#104677826](https://www.pivotaltracker.com/story/show/104677826)
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- bosh-director/spec/unit/deployment_plan/job_network_parser_spec.rb
+++ bosh-director/spec/unit/deployment_plan/job_network_parser_spec.rb
@@ -3,14 +3,54 @@ require 'spec_helper'
 module Bosh::Director::DeploymentPlan
   describe JobNetworksParser do
     let(:job_networks_parser) { JobNetworksParser.new(Network::VALID_DEFAULTS) }
-    let(:job_spec) { Bosh::Spec::Deployments.simple_manifest['jobs'].first }
-    let(:manifest_networks) { [Network.new('my-network', logger)] }
+    let(:job_spec) do
+      job = Bosh::Spec::Deployments.simple_manifest['jobs'].first
+      job_network = job['networks'].first
+      job_network['static_ips'] = ['192.168.1.1']
+      job
+    end
+    let(:manifest_networks) { [ManualNetwork.new('a', [], logger)] }
 
     context 'when job references a network not mentioned in the networks spec' do
+      let(:manifest_networks) { [ManualNetwork.new('my-network', [], logger)] }
+
       it 'raises JobUnknownNetwork' do
         expect {
           job_networks_parser.parse(job_spec, 'job-name', manifest_networks)
         }.to raise_error BD::JobUnknownNetwork, "Job 'job-name' references an unknown network 'a'"
+      end
+    end
+
+    context 'when job spec is missing network information' do
+      let(:job_spec) do
+        job = Bosh::Spec::Deployments.simple_manifest['jobs'].first
+        job['networks'] = []
+        job
+      end
+
+      it 'raises JobMissingNetwork' do
+        expect {
+          job_networks_parser.parse(job_spec, 'job-name', manifest_networks)
+        }.to raise_error BD::JobMissingNetwork, "Job `job-name' must specify at least one network"
+      end
+    end
+
+    context 'when called with a valid job spec' do
+      it 'adds static ips to job networks' do
+        networks = job_networks_parser.parse(job_spec, 'job-name', manifest_networks)
+
+        expect(networks.count).to eq(1)
+        expect(networks.first).to be_a_job_network(
+            JobNetwork.new('a', ['192.168.1.1'], ['dns', 'gateway'], manifest_networks.first)
+          )
+      end
+    end
+
+    RSpec::Matchers.define :be_a_job_network do |expected|
+      match do |actual|
+        actual.name == expected.name &&
+          actual.static_ips == expected.static_ips.map { |ip_to_i| NetAddr::CIDR.create(ip_to_i) } &&
+          actual.deployment_network == expected.deployment_network
       end
     end
   end
