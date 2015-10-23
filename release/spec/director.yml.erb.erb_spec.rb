31fb85f012523738c92f84b60d407d5cd766dd87
Alexey Miroshkin <alexey.miroshkin@ru.ibm.com>
2015-10-23 18:10:53 +0300

    Convert director.yml.erb to use top level JSON.dump
    JSON.dump doesn't work for strings in ruby 1.9.3, also escaping is
    required for each property. This fix constructs ruby hash and then
    convert it to JSON/YAML by a single JSON.dump call.
    
    Also additional test for vsphere was introduced to catch conversion
    errors. Verified on AWS, verification on the rest of CPIs is required.
    
    [#104923910] (https://www.pivotaltracker.com/story/show/104923910)
    
    Signed-off-by: Yulia Gaponenko <yulia.gaponenko@ru.ibm.com>

--- release/spec/director.yml.erb.erb_spec.rb
+++ release/spec/director.yml.erb.erb_spec.rb
@@ -142,6 +142,14 @@ describe 'director.yml.erb.erb' do
       }
     end
 
+    it 'renders correctly' do
+      expect(parsed_yaml['cloud']['properties']['vcenters'][0]['host']).to eq('vcenter.address')
+      expect(parsed_yaml['cloud']['properties']['vcenters'][0]['user']).to eq('user')
+      expect(parsed_yaml['cloud']['properties']['vcenters'][0]['password']).to eq('vcenter.password')
+      expect(parsed_yaml['cloud']['properties']['vcenters'][0]['datacenters'][0]['name']).to eq('vcenter.datacenters.first.name')
+      expect(parsed_yaml['cloud']['properties']['vcenters'][0]['datacenters'][0]['clusters'][0]).to eq('cluster1')
+    end
+
     context 'when vcenter.address contains special characters' do
       before do
         deployment_manifest_fragment['properties']['vcenter']['address'] = "!vcenter.address''"
