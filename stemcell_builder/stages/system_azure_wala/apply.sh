b78f3c0d374e711e59f2f982172b0e87d137cbfd
Abel Hu <abelch@microsoft.com>
2015-10-26 19:19:59 +0800

    Add SHA1 verification before installing WALinuxAgent for Azure.

--- stemcell_builder/stages/system_azure_wala/apply.sh
+++ stemcell_builder/stages/system_azure_wala/apply.sh
@@ -9,10 +9,20 @@ packages="python python-pyasn1"
 pkg_mgr install $packages
 
 wala_release=2.0.14
+wala_expected_sha1=373f5decdf9281c90be650f32dd1f283a5f6b045
+
+curl -L https://raw.githubusercontent.com/Azure/WALinuxAgent/WALinuxAgent-${wala_release}/waagent > /tmp/waagent
+sha1=$(cat /tmp/waagent | openssl dgst -sha1  | awk 'BEGIN {FS="="}; {gsub(/ /,"",$2); print $2}')
+if [ "${sha1}" != "${wala_expected_sha1}" ]; then
+  echo "SHA1 of downloaded WALinuxAgent-${wala_release} ${sha1} does not match expected SHA1 ${wala_expected_sha1}."
+  rm -f /tmp/waagent
+  exit 1
+fi
+
+mv -f /tmp/waagent $chroot/usr/sbin/waagent
+
 run_in_chroot $chroot "
-  curl -L https://raw.githubusercontent.com/Azure/WALinuxAgent/WALinuxAgent-${wala_release}/waagent > /tmp/waagent
-  chmod 0755 /tmp/waagent
-  cp -a /tmp/waagent /usr/sbin/waagent
+  chmod 0755 /usr/sbin/waagent
 "
 
 cp -f $dir/assets/etc/waagent.conf $chroot/etc/waagent.conf
