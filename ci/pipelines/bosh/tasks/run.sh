037443b55012445dd41ae449644fed936c7b9e89
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2015-10-27 09:52:13 -0700

    Restructure BOSH pipeline to conform to team Concourse standards.
    [#95665326](https://www.pivotaltracker.com/story/show/95665326)
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/pipelines/bosh/tasks/run.sh
+++ ci/pipelines/bosh/tasks/run.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-source bosh-src/ci/tasks/utils.sh
+source bosh-src/ci/pipelines/utils.sh
 
-check_param CODECLIMATE_REPO_TOKEN
 check_param RUBY_VERSION
 
 source /etc/profile.d/chruby.sh
 chruby $RUBY_VERSION
 
 cd bosh-src
-
 print_git_state
 
 bundle install --local
-
-COVERAGE=true bundle exec rake go spec:unit ci:publish_coverage_report
+bundle exec "$COMMAND"
