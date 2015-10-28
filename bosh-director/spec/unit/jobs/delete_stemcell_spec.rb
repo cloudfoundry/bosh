5c25392df5d548de98413e0206c462976496e213
Shatarupa Nandi <snandi@pivotal.io>
2015-10-28 10:27:56 -0700

    Move Jobs::DeleteStemcell specs in to Jobs::Helpers::StemcellDeleter spec
    [#104951996](https://www.pivotaltracker.com/story/show/104951996)
    
    Signed-off-by: Kam Leung <kleung@pivotal.io>

--- bosh-director/spec/unit/jobs/delete_stemcell_spec.rb
+++ bosh-director/spec/unit/jobs/delete_stemcell_spec.rb
@@ -6,7 +6,6 @@ module Bosh::Director
       let(:blobstore) { double('Blobstore') }
 
       before do
-        fake_locks
         @cloud = instance_double('Bosh::Cloud')
         allow(Config).to receive(:cloud).and_return(@cloud)
       end
@@ -20,67 +19,6 @@ module Bosh::Director
         job = Jobs::DeleteStemcell.new('test_stemcell', 'test_version', blobstore: blobstore)
 
         expect { job.perform }.to raise_exception(StemcellNotFound)
-      end
-
-      it "should fail if CPI can't delete the stemcell" do
-        Models::Stemcell.make(name: 'test_stemcell', version: 'test_version', cid: 'stemcell_cid')
-
-        expect(@cloud).to receive(:delete_stemcell).with('stemcell_cid').
-          and_raise('error')
-
-        job = Jobs::DeleteStemcell.new('test_stemcell', 'test_version', blobstore: blobstore)
-
-        expect { job.perform }.to raise_exception('error')
-      end
-
-      it 'should not fail of the CPI raises an error and the force options is used' do
-        Models::Stemcell.make(name: 'test_stemcell', version: 'test_version', cid: 'stemcell_cid')
-
-        expect(@cloud).to receive(:delete_stemcell).with('stemcell_cid').
-          and_raise('error')
-
-        job = Jobs::DeleteStemcell.new('test_stemcell', 'test_version', 'force' => true, blobstore: blobstore)
-        expect { job.perform }.not_to raise_error
-      end
-
-      it 'should fail if the deployments still reference this stemcell' do
-        stemcell = Models::Stemcell.make(name: 'test_stemcell', version: 'test_version', cid: 'stemcell_cid')
-
-        deployment = Models::Deployment.make
-        deployment.add_stemcell(stemcell)
-
-        job = Jobs::DeleteStemcell.new('test_stemcell', 'test_version', blobstore: blobstore)
-        expect { job.perform }.to raise_exception(StemcellInUse)
-      end
-
-      it 'should delete the stemcell meta if the CPI deleted the stemcell' do
-        stemcell = Models::Stemcell.make(name: 'test_stemcell', version: 'test_version', cid: 'stemcell_cid')
-
-        expect(@cloud).to receive(:delete_stemcell).with('stemcell_cid')
-
-        job = Jobs::DeleteStemcell.new('test_stemcell', 'test_version', blobstore: blobstore)
-        job.perform
-
-        expect(Models::Stemcell[stemcell.id]).to be_nil
-      end
-
-      it 'should delete the associated compiled packages' do
-        stemcell = Models::Stemcell.make(name: 'test_stemcell', version: 'test_version', cid: 'stemcell_cid')
-
-        package = Models::Package.make
-        compiled_package = Models::CompiledPackage.make(package: package,
-                                                        stemcell: stemcell,
-                                                        blobstore_id: 'compiled-package-blb-id')
-
-        expect(@cloud).to receive(:delete_stemcell).with('stemcell_cid')
-
-        expect(blobstore).to receive(:delete).with('compiled-package-blb-id')
-
-        job = Jobs::DeleteStemcell.new('test_stemcell', 'test_version', blobstore: blobstore)
-        job.perform
-
-        expect(Models::Stemcell[stemcell.id]).to be_nil
-        expect(Models::CompiledPackage[compiled_package.id]).to be_nil
       end
     end
   end
---
+++ bosh-director/spec/unit/jobs/helpers/stemcell_deleter_spec.rb
@@ -0,0 +1,101 @@
+require 'spec_helper'
+
+module Bosh::Director
+  describe Jobs::Helpers::StemcellDeleter do
+    let(:blobstore) { double('Blobstore') }
+    let(:event_log) { EventLog::Log.new }
+    let(:cloud) { instance_double(Bosh::Cloud) }
+    let(:stemcell_deleter) { Jobs::Helpers::StemcellDeleter.new(cloud, blobstore, logger, event_log) }
+    let(:stemcell) { Models::Stemcell.make(name: 'test_stemcell', version: 'test_version', cid: 'stemcell_cid') }
+
+    before do
+      fake_locks
+      allow(Config).to receive(:cloud).and_return(cloud)
+      allow(event_log).to receive(:begin_stage)
+    end
+
+    context 'when stemcell deletion fails' do
+      it "should raise error if CPI can't delete the stemcell" do
+        expect(event_log).to receive(:begin_stage).with('Deleting stemcell from cloud', 1)
+        expect(cloud).to receive(:delete_stemcell).with('stemcell_cid').and_raise('error')
+
+        expect {
+          stemcell_deleter.delete(stemcell)
+        }.to raise_error
+      end
+
+      it 'should raise error if the deployments still reference this stemcell' do
+        deployment_1 = Models::Deployment.make(name: 'test-1')
+        deployment_1.add_stemcell(stemcell)
+        deployment_2 = Models::Deployment.make(name: 'test-2')
+        deployment_2.add_stemcell(stemcell)
+
+        expect {
+          stemcell_deleter.delete(stemcell)
+        }.to raise_error StemcellInUse, "Stemcell `test_stemcell/test_version' is still in use by: test-1, test-2"
+      end
+    end
+
+    context 'when CPI raises an error AND the "force" option is used' do
+      it 'should not raise an error' do
+        expect(event_log).to receive(:begin_stage).with('Deleting stemcell from cloud', 1)
+        expect(cloud).to receive(:delete_stemcell).with('stemcell_cid').and_raise('error')
+
+        expect { stemcell_deleter.delete(stemcell, 'force' => true) }.not_to raise_error
+      end
+
+      it 'should delete stemcell metadata' do
+        expect(event_log).to receive(:begin_stage).with('Deleting stemcell from cloud', 1)
+        expect(cloud).to receive(:delete_stemcell).with('stemcell_cid').and_raise('error')
+
+        stemcell_deleter.delete(stemcell, 'force' => true)
+        expect(Models::Stemcell.all).to be_empty
+      end
+
+      it 'should delete associated compiled packages' do
+        associated_package = Models::CompiledPackage.make(
+          package: Models::Package.make,
+          stemcell: stemcell,
+          blobstore_id: 'compiled-package-blb-1')
+
+        expect(cloud).to receive(:delete_stemcell).with('stemcell_cid').and_raise('error')
+
+        expect(blobstore).to receive(:delete).with('compiled-package-blb-1')
+
+        stemcell_deleter.delete(stemcell, 'force' => true)
+        expect(Models::CompiledPackage[associated_package.id]).to be_nil
+      end
+    end
+
+    context 'when stemcell deletion succeeds' do
+      it 'should delete the stemcell metadata if the CPI deleted the stemcell' do
+        expect(event_log).to receive(:begin_stage).with('Deleting stemcell from cloud', 1)
+        expect(cloud).to receive(:delete_stemcell).with('stemcell_cid')
+
+        stemcell_deleter.delete(stemcell)
+        expect(Models::Stemcell.all).to be_empty
+      end
+
+      it 'should delete the associated compiled packages' do
+        expect(event_log).to receive(:begin_stage).with('Deleting stemcell from cloud', 1)
+        expect(event_log).to receive(:begin_stage).with('Deleting compiled packages', 1, ['test_stemcell', 'test_version'])
+
+        associated_package = Models::CompiledPackage.make(
+          package: Models::Package.make,
+          stemcell: stemcell,
+          blobstore_id: 'compiled-package-blb-1')
+        unassociated_package = Models::CompiledPackage.make(
+          package: Models::Package.make,
+          blobstore_id: 'compiled-package-blb-2')
+
+        expect(cloud).to receive(:delete_stemcell).with('stemcell_cid')
+
+        expect(blobstore).to receive(:delete).with('compiled-package-blb-1')
+
+        stemcell_deleter.delete(stemcell)
+        expect(Models::CompiledPackage[associated_package.id]).to be_nil
+        expect(Models::CompiledPackage[unassociated_package.id]).not_to be_nil
+      end
+    end
+  end
+end
