bdedb0aeecbee39688456179d83adf6842394556
Maria Shaldibina <mshaldibina@pivotal.io>
2015-10-29 15:02:02 -0700

    Director ssh accepts instance id
    Signed-off-by: Victoria Lo <victorialo@berkeley.edu>
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- bosh-director/lib/bosh/director/jobs/ssh.rb
+++ bosh-director/lib/bosh/director/jobs/ssh.rb
@@ -13,7 +13,7 @@ module Bosh::Director
 
       def initialize(deployment_id, options = {})
         @deployment_id = deployment_id
-        @target = options["target"]
+        @target_payload = options["target"]
         @command = options["command"]
         @params = options["params"]
         @blobstore = options.fetch(:blobstore) { App.instance.blobstores.blobstore }
@@ -21,29 +21,32 @@ module Bosh::Director
       end
 
       def perform
-        job = @target["job"]
-        indexes = @target["indexes"]
+        target = Target.new(@target_payload)
 
         filter = {
           :deployment_id => @deployment_id
         }
 
-        if indexes && indexes.size > 0
-          filter[:index] = indexes
+        if target.id_provided?
+          filter[:uuid] = target.id
+        elsif target.indexes_provided?
+          filter[:index] = target.indexes
         end
 
-        if job
-          filter[:job] = job
-        end
+        filter[:job] = target.job if target.job
 
         instances = @instance_manager.filter_by(filter)
 
         ssh_info = instances.map do |instance|
           agent = @instance_manager.agent_client_for(instance)
 
-          logger.info("ssh #{@command} `#{instance.job}/#{instance.index}'")
+          logger.info("ssh #{@command} `#{instance.job}/#{instance.uuid}'")
           result = agent.ssh(@command, @params)
-          result["index"] = instance.index
+          if target.id_provided?
+            result["id"] = instance.uuid
+          else
+            result["index"] = instance.index
+          end
 
           if Config.default_ssh_options
             result["gateway_host"] = Config.default_ssh_options["gateway_host"]
@@ -58,6 +61,26 @@ module Bosh::Director
 
         # task result
         nil
+      end
+
+      private
+
+      class Target
+        attr_reader :job, :indexes, :id
+
+        def initialize(target_payload)
+          @job = target_payload['job']
+          @indexes = target_payload['indexes']
+          @id = target_payload['id']
+        end
+
+        def id_provided?
+          @id && !@id.empty?
+        end
+
+        def indexes_provided?
+          @indexes && @indexes.size > 0
+        end
       end
     end
   end
--- bosh-director/spec/unit/jobs/ssh_spec.rb
+++ bosh-director/spec/unit/jobs/ssh_spec.rb
@@ -1,35 +1,46 @@
 require 'spec_helper'
+require 'fakefs/spec_helpers'
 
 module Bosh::Director
   describe Jobs::Ssh do
     include FakeFS::SpecHelpers
-    subject(:job) { described_class.new('FAKE_DEPLOYMENT_ID', {"target" => {}, "command" => "fake-command", "params" => {}, :blobstore => {}}) }
-    let (:agent) { instance_double (AgentClient)}
-    let (:config) {double(Config)}
-    let (:instance_manager) { instance_double (Api::InstanceManager)}
-    let (:instance) { instance_double(Models::Instance)}
-    let (:result_file) { instance_double(TaskResultFile) }
+    subject(:job) { described_class.new(deployment.id, {'target' => target, 'command' => 'fake-command', 'params' => {}, :blobstore => {}}) }
+    let(:agent) { double(AgentClient) }
+    let(:config) { double(Config) }
+    let(:instance_manager) { Api::InstanceManager.new }
+    let(:deployment) { Models::Deployment.make }
+    let(:result_file) { instance_double(TaskResultFile) }
+    let(:target) { {'job' => 'fake-job', 'indexes' => 5} }
     describe 'Resque job class expectations' do
       let(:job_type) { :ssh }
       it_behaves_like 'a Resque job'
     end
 
-    it 'returns default_ssh_options if they exist' do
+    before do
+      Models::Instance.make(job: 'fake-job', index: 5, deployment: deployment, uuid: 'fake-uuid')
       allow(Api::InstanceManager).to receive(:new).and_return(instance_manager)
-      allow(instance_manager).to receive(:filter_by).and_return([instance])
-      allow(instance).to receive(:index).and_return(0)
-      allow(instance).to receive(:job).and_return("a job")
       allow(instance_manager).to receive(:agent_client_for).and_return(agent)
-      allow(agent).to receive(:method_missing).and_return({})
-      Config.default_ssh_options = {"gateway_host" => "fake-host", "gateway_user" => "vcap"}
+      allow(agent).to receive(:ssh).and_return({})
+      Config.default_ssh_options = {'gateway_host' => 'fake-host', 'gateway_user' => 'vcap'}
       allow(TaskResultFile).to receive(:new).and_return(result_file)
-      allow(result_file).to receive(:write).with("\n");
-
-      expect(result_file).to receive(:write).with(Yajl::Encoder.encode([{"index" => 0, "gateway_host" => "fake-host", "gateway_user" => "vcap"}]))
+      allow(result_file).to receive(:write).with("\n")
       Config.result = result_file
+    end
+
+    it 'returns default_ssh_options if they exist' do
+      expect(result_file).to receive(:write).with(Yajl::Encoder.encode([{'index' => 5, 'gateway_host' => 'fake-host', 'gateway_user' => 'vcap'}]))
 
       job.perform
+    end
 
+    context 'when instance id was passed in' do
+      let(:target) { {'job' => 'fake-job', 'id' => 'fake-uuid'} }
+
+      it 'finds instance by its id and generates response with id' do
+        expect(result_file).to receive(:write).with(Yajl::Encoder.encode([{'id' => 'fake-uuid', 'gateway_host' => 'fake-host', 'gateway_user' => 'vcap'}]))
+
+        job.perform
+      end
     end
   end
 end
