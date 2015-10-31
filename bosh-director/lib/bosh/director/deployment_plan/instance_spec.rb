ae1e4dd7583abbb9b0af13259ab2782e4b851429
Maria Shaldibina <mshaldibina@pivotal.io>
2015-10-30 17:10:03 -0700

    Apply and template spec reuse the same spec
    Signed-off-by: Jan von Loewenstein <jan.von.loewenstein@sap.com>

--- bosh-director/lib/bosh/director/deployment_plan/instance_spec.rb
+++ bosh-director/lib/bosh/director/deployment_plan/instance_spec.rb
@@ -10,6 +10,50 @@ module Bosh::Director
       end
 
       def template_spec
+        keys = [
+          'deployment',
+          'job',
+          'index',
+          'bootstrap',
+          'id',
+          'availability_zone',
+          'networks',
+          'vm_type',
+          'stemcell',
+          'env',
+          'packages',
+          'properties',
+          'dns_domain_name',
+          'links',
+          'persistent_disk',
+          'persistent_disk_pool',
+          'persistent_disk_type',
+          'template_hashes'
+        ]
+        full_spec.select {|k,v| keys.include?(k) }
+      end
+
+      def apply_spec
+        keys = [
+          'deployment',
+          'job',
+          'index',
+          'id',
+          'networks',
+          'vm_type',
+          'stemcell',
+          'env',
+          'packages',
+          'dns_domain_name',
+          'configuration_hash',
+          'persistent_disk',
+          'template_hashes',
+          'rendered_templates_archive',
+        ]
+        full_spec.select {|k,v| keys.include?(k) }
+      end
+
+      def full_spec
         spec = {
           'deployment' => @deployment_name,
           'job' => @job.spec,
@@ -24,6 +68,7 @@ module Bosh::Director
           'packages' => @job.package_spec,
           'properties' => @job.properties,
           'dns_domain_name' => @dns_manager.dns_domain_name,
+          'configuration_hash' => @instance.configuration_hash,
           'links' => @job.link_spec,
         }
 
@@ -34,37 +79,6 @@ module Bosh::Director
           # keep disk pool for backwards compatibility
           spec['persistent_disk_pool'] = @job.persistent_disk_type.spec
           spec['persistent_disk_type'] = @job.persistent_disk_type.spec
-        else
-          spec['persistent_disk'] = 0
-        end
-
-        if @instance.template_hashes
-          spec['template_hashes'] = @instance.template_hashes
-        end
-
-        spec
-      end
-
-      def apply_spec
-        spec = {
-          'deployment' => @deployment_name,
-          'job' => @job.spec,
-          'index' => @instance.index,
-          'id' => @instance.uuid,
-          'networks' => @instance_plan.network_settings_hash,
-          'vm_type' => @job.vm_type.spec,
-          'stemcell' => @job.stemcell.spec,
-          'packages' => @job.package_spec,
-          'configuration_hash' => @instance.configuration_hash,
-          'dns_domain_name' => @dns_manager.dns_domain_name,
-        }
-
-        if @job.env
-          spec['env'] = @job.env.spec
-        end
-
-        if @job.persistent_disk_type
-          spec['persistent_disk'] = @job.persistent_disk_type.disk_size
         else
           spec['persistent_disk'] = 0
         end
