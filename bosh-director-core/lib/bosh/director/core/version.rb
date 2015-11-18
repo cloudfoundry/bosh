c5d4eb4fc61d87d4989a6763abe39d6883544199
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-11-17 18:47:40 -0800

    Adding final release for build 3140

--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.3139.0'
+      VERSION = '1.3140.0'
     end
   end
 end
--- simple_blobstore_server/lib/simple_blobstore_server/version.rb
+++ simple_blobstore_server/lib/simple_blobstore_server/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module SimpleBlobstoreServer
-    VERSION = '1.3139.0'
+    VERSION = '1.3140.0'
   end
 end
