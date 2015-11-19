ece07c869d893fdced1dbb76b1dbfbddd4e8a9cd
caleb miles <cmiles@pivotal.io>
2015-11-18 16:03:59 -0800

    Do not try and build s3 cli in stemcell builder.
    It's already a binary.
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- stemcell_builder/stages/aws_cli/apply.sh
+++ stemcell_builder/stages/aws_cli/apply.sh
@@ -8,7 +8,5 @@ source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
 cd $assets_dir/s3cli
-bin/build
-
-mv out/s3 $chroot/var/vcap/bosh/bin/bosh-blobstore-s3
+mv s3cli $chroot/var/vcap/bosh/bin/bosh-blobstore-s3
 chmod +x $chroot/var/vcap/bosh/bin/bosh-blobstore-s3
