189131492d342a04c0659ab9e4ab72236e3c6347
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-11-30 15:37:35 -0800

    Adding final release for build 3145

--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module AwsCliPlugin
-    VERSION = '1.3144.0'
+    VERSION = '1.3145.0'
   end
 end
--- bosh_cli_plugin_micro/lib/bosh/deployer/version.rb
+++ bosh_cli_plugin_micro/lib/bosh/deployer/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Deployer
-    VERSION = '1.3144.0'
+    VERSION = '1.3145.0'
   end
 end
