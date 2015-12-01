b3d1d5f114872c1bd05316c5a5d943de6525d1c7
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-12-01 13:41:31 -0800

    Adding final release for build 3146

--- agent_client/lib/agent_client/version.rb
+++ agent_client/lib/agent_client/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Agent
     class Client
-      VERSION = '1.3145.0'
+      VERSION = '1.3146.0'
     end
   end
 end
--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.3145.0'
+      VERSION = '1.3146.0'
     end
   end
 end
