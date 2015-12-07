7c3bc8344837c2a55d0429155cc50c7ad2e0b41b
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2015-12-07 11:06:02 -0800

    Adding final release for build 3148

--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.3147.0'
+      VERSION = '1.3148.0'
     end
   end
 end
--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module AwsCliPlugin
-    VERSION = '1.3147.0'
+    VERSION = '1.3148.0'
   end
 end
