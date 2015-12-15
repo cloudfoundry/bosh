1b61b1e216f84a64f2fb0fdc7577b16fe89ee2bc
Corey Innis <cinnis@pivotal.io>
2015-12-14 17:17:40 -0800

    Bump s3cli in stemcell builder.

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -9,6 +9,6 @@ source $base_dir/lib/prelude_config.bash
 cd $assets_dir
 rm -rf s3cli
 mkdir s3cli
-current_version=0.0.15
+current_version=0.0.20
 curl -L -o s3cli/s3cli https://s3.amazonaws.com/s3cli-artifacts/s3cli-${current_version}-linux-amd64
-echo "1feaf640828484cdc975056605f754ca71b7fb3d s3cli/s3cli" | sha1sum -c -
+echo "b168931ed916e1c86563986d3ec06ac6fd613fa0 s3cli/s3cli" | sha1sum -c -
