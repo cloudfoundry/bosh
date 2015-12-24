3f95908d010dafd9d2a9501d05773b100ee9cf28
Danny Berger <dberger@pivotal.io>
2015-12-23 16:51:57 -0800

    Refactoring package_compile_step_spec away from using a mock CompilationConfig.
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- bosh-director/spec/unit/deployment_plan/steps/package_compile_step_spec.rb
+++ bosh-director/spec/unit/deployment_plan/steps/package_compile_step_spec.rb
@@ -11,8 +11,18 @@ module Bosh::Director
     let(:job_renderer) { instance_double(JobRenderer, render_job_instance: nil) }
     let(:disk_manager) {DiskManager.new(cloud, logger)}
     let(:release_version_model) { Models::ReleaseVersion.make }
+    let(:reuse_compilation_vms) { false }
+    let(:number_of_workers) { 3 }
     let(:compilation_config) do
-      instance_double('Bosh::Director::DeploymentPlan::CompilationConfig')
+      compilation_spec = {
+        'workers' => number_of_workers,
+        'network' => 'default',
+        'env' => {},
+        'cloud_properties' => {},
+        'reuse_compilation_vms' => reuse_compilation_vms,
+        'az' => '',
+      }
+      DeploymentPlan::CompilationConfig.new(compilation_spec, {}, [])
     end
     let(:deployment) { Models::Deployment.make(name: 'mycloud') }
     let(:plan) do
@@ -58,17 +68,6 @@ module Bosh::Director
       allow(@director_job).to receive(:task_cancelled?).and_return(false)
 
       allow(plan).to receive(:network).with('default').and_return(network)
-
-      @n_workers = 3
-      allow(compilation_config).to receive_messages(
-          network_name: 'default',
-          env: {},
-          cloud_properties: {},
-          workers: @n_workers,
-          reuse_compilation_vms: false,
-          availability_zone: nil,
-          vm_type: nil
-        )
 
       allow(Config).to receive(:use_compiled_package_cache?).and_return(false)
       @all_packages = []
@@ -336,6 +335,8 @@ module Bosh::Director
     end
 
     context 'when the deploy is cancelled and there is a pending compilation' do
+      let(:reuse_compilation_vms) { true }
+      let(:number_of_workers) { 1 }
       # this can happen when the cancellation comes in when there is a package to be compiled,
       # and the compilation is not even in-flight. e.g.
       # - you have 3 compilation workers, but you've got 5 packages to compile; or
@@ -356,7 +357,6 @@ module Bosh::Director
         )
 
         network = double('network', name: 'network_name')
-        compilation_config = instance_double('Bosh::Director::DeploymentPlan::CompilationConfig', cloud_properties: {}, env: {}, workers: 1, reuse_compilation_vms: true, network_name: 'network_name')
         release_version_model = instance_double('Bosh::Director::Models::ReleaseVersion', dependencies: Set.new, transitive_dependencies: Set.new)
         release_version = instance_double('Bosh::Director::DeploymentPlan::ReleaseVersion', name: 'release_name', model: release_version_model)
         stemcell = make_stemcell
@@ -375,6 +375,7 @@ module Bosh::Director
     end
 
     describe 'with reuse_compilation_vms option set' do
+      let(:reuse_compilation_vms) { true }
       let(:initial_state) {
         {
           'deployment' => 'mycloud',
@@ -393,7 +394,6 @@ module Bosh::Director
 
       it 'reuses compilation VMs' do
         prepare_samples
-        allow(compilation_config).to receive_messages(reuse_compilation_vms: true)
 
         expect(vm_creator).to receive(:create_for_instance_plan).exactly(1).times do |instance_plan, _|
           vm_model = Models::Vm.make
@@ -451,9 +451,6 @@ module Bosh::Director
 
       it 'cleans up compilation vms if there is a failing compilation' do
         prepare_samples
-
-        allow(compilation_config).to receive_messages(reuse_compilation_vms: true)
-        allow(compilation_config).to receive_messages(workers: 1)
 
         vm_cid = 'vm-cid-1'
         agent = instance_double('Bosh::Director::AgentClient')
@@ -528,13 +525,12 @@ module Bosh::Director
       end
 
       context 'reuse_compilation_vms is true' do
-        before { allow(compilation_config).to receive_messages(reuse_compilation_vms: true) }
         it_tears_down_vm_exactly_once(RpcTimeout)
         it_tears_down_vm_exactly_once(TaskCancelled)
       end
 
       context 'reuse_compilation_vms is false' do
-        before { allow(compilation_config).to receive_messages(reuse_compilation_vms: false) }
+        let(:reuse_compilation_vms) { false }
         it_tears_down_vm_exactly_once(RpcTimeout)
         it_tears_down_vm_exactly_once(TaskCancelled)
       end
@@ -614,16 +610,9 @@ module Bosh::Director
     end
 
     describe '#prepare_vm' do
-      let(:compilation_config) do
-        config = double('compilation_config')
-        allow(config).to receive_messages(network_name: 'default')
-        allow(config).to receive_messages(cloud_properties: double('cloud_properties'))
-        allow(config).to receive_messages(env: double('env'))
-        allow(config).to receive_messages(workers: 2)
-        config
-      end
+      let(:number_of_workers) { 2 }
       let(:plan) do
-        double('Bosh::Director::DeploymentPlan',
+        instance_double('Bosh::Director::DeploymentPlan::Planner',
           compilation: compilation_config,
           model: Models::Deployment.make,
           name: 'fake-deployment',
@@ -635,11 +624,11 @@ module Bosh::Director
       let(:instance) { instance_double(DeploymentPlan::Instance, vm: vm) }
 
       context 'with reuse_compilation_vms' do
-        let(:network) { double('network', name: 'default', network_settings: nil) }
+        let(:reuse_compilation_vms) { true }
+        let(:network) { instance_double('Bosh::Director::DeploymentPlan::ManualNetwork', name: 'default', network_settings: nil) }
         let(:instance_reuser) { instance_double('Bosh::Director::InstanceReuser') }
 
         before do
-          allow(compilation_config).to receive_messages(reuse_compilation_vms: true)
           allow(plan).to receive(:network).with('default').and_return(network)
         end
 
@@ -651,11 +640,11 @@ module Bosh::Director
           allow(instance_reuser).to receive_messages(get_instance: nil)
           allow(instance_reuser).to receive_messages(get_num_instances: 0)
           allow(instance_reuser).to receive_messages(add_in_use_instance: instance)
-          allow(network).to receive(:reserve).with(instance_of(Bosh::Director::DesiredNetworkReservation))
+          allow(ip_provider).to receive(:reserve).with(instance_of(Bosh::Director::DesiredNetworkReservation))
 
           expect(instance_reuser).to receive(:remove_instance).ordered
           expect(instance_deleter).to receive(:delete_instance_plan).ordered
-          allow(network).to receive(:release)
+          allow(ip_provider).to receive(:release)
 
           expect {
             compiler.prepare_vm(stemcell) do
