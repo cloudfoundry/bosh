6cd7a8a4a207248c991248fb452a39b36f84f6b5
Maria Shaldibina <mshaldibina@pivotal.io>
2016-01-04 17:35:33 -0800

    Update OS images tests for latest Ubuntu
    Signed-off-by: Wagner Camarao <wcamarao@vmware.com>

--- bosh-stemcell/spec/support/os_image_linux_kernel_3x_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_linux_kernel_3x_shared_examples.rb
@@ -46,13 +46,13 @@ shared_examples_for 'a Linux kernel 3.x based OS image' do
     end
 
     context 'installed by system_ixgbevf' do
-      describe package('dkms') do
+      describe package('dkms'), exclude_on_ppc64le: true do
         it { should be_installed }
       end
 
-      describe 'the ixgbevf kernel module' do
+      describe 'the ixgbevf kernel module', exclude_on_ppc64le: true  do
         it 'is installed with the right version' do
-          expect(file("/var/lib/dkms/ixgbevf/2.16.1/#{kernel_version}/x86_64/module/ixgbevf.ko")).to be_a_file
+          expect(file("/var/lib/dkms/ixgbevf/3.1.1/#{kernel_version}/x86_64/module/ixgbevf.ko")).to be_a_file
         end
       end
     end
