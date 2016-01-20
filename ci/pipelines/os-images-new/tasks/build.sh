189f290ffa6a2f6e57b75042a8b09a6d022d8557
Yu Zhang <yu.zhang2@emc.com>
2016-01-19 17:03:14 -0800

    create new os image build that runs in docker without vagrant vm
    [#111450566](https://www.pivotaltracker.com/story/show/111450566)
    
    Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

--- ci/pipelines/os-images-new/tasks/build.sh
+++ ci/pipelines/os-images-new/tasks/build.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-source bosh-src/ci/tasks/utils.sh
+cd bosh-src
+source ci/tasks/utils.sh
 check_param BOSH_AWS_ACCESS_KEY_ID
 check_param BOSH_AWS_SECRET_ACCESS_KEY
 check_param OPERATING_SYSTEM_NAME
@@ -10,6 +11,7 @@ check_param OPERATING_SYSTEM_VERSION
 check_param OS_IMAGE_S3_BUCKET_NAME
 check_param OS_IMAGE_S3_KEY
 
+sudo chown -R ubuntu .
 sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
     bundle install --local
     bundle exec rake stemcell:build_os_image[$OPERATING_SYSTEM_NAME,$OPERATING_SYSTEM_VERSION,tmp/bosh-$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION-os-image.tgz]
