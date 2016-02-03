60a113b768ae9e2960ff1890a1638a49848ea72e
Shatarupa Nandi <snandi@pivotal.io>
2016-02-02 16:10:24 -0800

    Integration tests will fail if local postgres version and postgres version in the bosh release do not match.
    [#111924795](https://www.pivotaltracker.com/story/show/111924795)

--- bosh-dev/lib/bosh/dev/postgres_version.rb
+++ bosh-dev/lib/bosh/dev/postgres_version.rb
@@ -3,27 +3,6 @@ require 'pg'
 require 'bosh/director/config'
 
 module Bosh::Dev
-  # class RubyVersion
-  #   class << self
-  #     def legacy_version
-  #       '1.9.3'
-  #     end
-  #
-  #     def release_version
-  #       @release_version ||= begin
-  #         spec_path = '../../../../release/packages/ruby/spec'
-  #         ruby_spec = YAML.load_file(File.join(File.dirname(__FILE__), spec_path))
-  #         ruby_spec['files'].find { |f| f =~ /ruby-(.*).tar.gz/ }
-  #         $1
-  #       end
-  #     end
-  #
-  #     def supported
-  #       @supported ||= [legacy_version, release_version]
-  #     end
-  #   end
-  # end
-
   class PostgresVersion
     class << self
 
@@ -33,7 +12,7 @@ module Bosh::Dev
 
       def release_version
         @release_version ||= begin
-          postgres_release_config = YAML::load(File.open('../release/jobs/postgres/spec.yml'))
+          postgres_release_config = YAML::load_file(File.join(File.dirname(__FILE__), '../../../../release/jobs/postgres/spec.yml'))
           postgres_version = postgres_release_config['packages'].sort.last
 
           postgres_version.split('-').last
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ require 'nats/client'
 require 'restclient'
 require 'bosh/director'
 require 'blue-shell'
+require_relative '../bosh-dev/lib/bosh/dev/postgres_version'
 
 Dir.glob(File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require(f) }
 
@@ -34,6 +35,12 @@ RSpec.configure do |c|
       unless system(agent_build_cmd)
         raise 'Bosh agent build failed'
       end
+    end
+
+    local_major_and_minor_version = Bosh::Dev::PostgresVersion.local_version.split('.')[0..1]
+    release_major_and_minor_version = Bosh::Dev::PostgresVersion.release_version.split('.')[0..1]
+    unless local_major_and_minor_version == release_major_and_minor_version
+      raise "Postgres version mismatch: release version is #{release_major_and_minor_version}; local version is #{local_major_and_minor_version}"
     end
   end
 end
