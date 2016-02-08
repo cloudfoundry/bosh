6adffb596e72f752b9d8a898bb7b3e7c887b3181
Danny Berger <dberger@pivotal.io>
2016-02-08 09:51:57 -0800

    WIP
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- release/spec/postgres_upgrade.sh_spec.rb
+++ release/spec/postgres_upgrade.sh_spec.rb
@@ -1,46 +1,29 @@
-# require 'rspec'
-# require 'yaml'
-# require 'bosh/template/evaluation_context'
-# require 'json'
-#
-# describe 'postgres_ctl.erb' do
-#   let(:deployment_manifest_fragment) do
-#     {
-#       'properties' => {
-#         'postgres' => {
-#           'host' => '127.0.0.1',
-#           'user' => 'postgres',
-#           'password' => 'postgres-password',
-#           'database' => 'bosh',
-#           'adapter' => 'postgres',
-#
-#         }
-#       }
-#     }
-#   end
-#
-#   let(:postgres_upgrade_sh) { File.join(File.dirname(__FILE__), '../jobs/postgres/templates/postgres_upgrade.sh.erb') }
-#
-#   subject(:parsed_yaml) do
-#     binding = Bosh::Template::EvaluationContext.new(deployment_manifest_fragment).get_binding
-#     system(ERB.new(postgres_upgrade_sh).result(binding))
-#   end
-#
-#   context 'given a generally valid manifest' do
-#     it "should contain NATS's bare minimum" do
-#       expect(parsed_yaml['net']).to eq('0.0.0.0')
-#       expect(parsed_yaml['port']).to eq(4222)
-#       expect(parsed_yaml['logtime']).to satisfy { |v| v == true || v == false }
-#       expect(parsed_yaml['no_epoll']).to eq(false)
-#       expect(parsed_yaml['no_kqueue']).to eq(false)
-#       expect(parsed_yaml['ping']['interval']).to eq(5)
-#       expect(parsed_yaml['ping']['max_outstanding']).to eq(10)
-#       expect(parsed_yaml['pid_file']).to be_a(String)
-#       expect(parsed_yaml['log_file']).to be_a(String)
-#       expect(parsed_yaml['authorization']['user']).to eq('my-user')
-#       expect(parsed_yaml['authorization']['password']).to eq('my-password')
-#       expect(parsed_yaml['authorization']['timeout']).to eq(10)
-#       expect(parsed_yaml.has_key?('http')).to eq(false)
-#     end
-#   end
-# end
+require 'open3'
+
+describe 'postgres_ctl.erb' do
+  let(:postgres_upgrade_sh) { File.join(File.dirname(__FILE__), '../jobs/postgres/templates/postgres_upgrade.sh') }
+
+  context 'running postgres_upgrade.sh' do
+    before do
+      FileUtils.mkdir_p 'tmp/store/postgres'
+      FileUtils.mkdir_p 'tmp/sys/run/postgres'
+      FileUtils.touch 'tmp/store/postgres/postgresql.conf'
+      FileUtils.mkdir_p 'tmp/packages/postgres-9.4.5/bin'
+      FileUtils.touch 'tmp/packages/postgres-9.4.5/bin/pg_upgrade'
+      File.chmod(777, 'tmp/packages/postgres-9.4.5/bin/pg_upgrade')
+      ENV['BASE_DIR'] = 'tmp'
+    end
+
+    after do
+      FileUtils.rm_rf 'tmp'
+    end
+
+    it 'should create a backup directory before migrating' do
+      _, _, status = Open3.capture3('jobs/postgres/templates/postgres_upgrade.sh.erb')
+
+      expect(status).to eq(0)
+      expect(Dir.exists?('tmp/store/postgres'))
+      expect(Dir.exists?('tmp/store/postgres-9.4.5'))
+    end
+  end
+end
