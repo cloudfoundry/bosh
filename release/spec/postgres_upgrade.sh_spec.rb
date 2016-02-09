1bd9539bb3c821e3d73db172033ab7644bff7af4
Brian Cunnie <bcunnie@pivotal.io>
2016-02-09 10:30:39 -0800

    Include tests for postgres upgrade
    [#111924795](https://www.pivotaltracker.com/story/show/111924795)
    
    Signed-off-by: William Lindner <wlindner@pivotal.io>

--- release/spec/postgres_upgrade.sh_spec.rb
+++ release/spec/postgres_upgrade.sh_spec.rb
@@ -6,11 +6,12 @@ describe 'postgres_ctl.erb' do
   context 'running postgres_upgrade.sh' do
     before do
       FileUtils.mkdir_p 'tmp/store/postgres'
+      File.open('tmp/store/postgres/PG_VERSION', 'a+') {|f| f.write '9.0'}
       FileUtils.mkdir_p 'tmp/sys/run/postgres'
       FileUtils.touch 'tmp/store/postgres/postgresql.conf'
-      FileUtils.mkdir_p 'tmp/packages/postgres-9.4.5/bin'
-      FileUtils.touch 'tmp/packages/postgres-9.4.5/bin/pg_upgrade'
-      File.chmod(777, 'tmp/packages/postgres-9.4.5/bin/pg_upgrade')
+      FileUtils.mkdir_p 'tmp/jobs/postgres/bin'
+      FileUtils.touch 'tmp/jobs/postgres/bin/really_upgrade_postgres.sh'
+      File.chmod(777, 'tmp/jobs/postgres/bin/really_upgrade_postgres.sh')
       ENV['BASE_DIR'] = 'tmp'
     end
 
@@ -22,8 +23,8 @@ describe 'postgres_ctl.erb' do
       _, _, status = Open3.capture3('jobs/postgres/templates/postgres_upgrade.sh.erb')
 
       expect(status).to eq(0)
-      expect(Dir.exists?('tmp/store/postgres'))
-      expect(Dir.exists?('tmp/store/postgres-9.4.5'))
+      expect(Dir.exists?('tmp/store/postgres-previous'))
+      expect(File.read('tmp/store/postgres-previous/PG_VERSION')).to eq('9.0')
     end
   end
 end
