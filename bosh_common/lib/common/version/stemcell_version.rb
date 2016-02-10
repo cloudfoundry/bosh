e5e2d816177f38e2b6d0967df1814c52c4267ba8
Devin Fallak <dfallak@pivotal.io>
2016-02-10 11:41:08 -0800

    Changing stemcell version matching to non-major only
    [#113302989](https://www.pivotaltracker.com/story/show/113302989)
    
    Signed-off-by: Wagner Camarao <wcamarao@vmware.com>

--- bosh_common/lib/common/version/stemcell_version.rb
+++ bosh_common/lib/common/version/stemcell_version.rb
@@ -14,24 +14,15 @@ module Bosh::Common
         self.new(SemiSemantic::Version.parse(version))
       end
 
-      def self.match_patch_versions(str_a, str_b)
+      def self.match(str_a, str_b)
         version_a, version_b = parse(str_a), parse(str_b)
-        version_a.matches_patch_version(version_b)
+        version_a.matches(version_b)
       end
 
-      def matches_patch_version(other)
-        return true if self == other
-
+      def matches(other)
         release_self = self.version.release
         release_other = other.version.release
-
-        min_size = [release_self.components.size, release_other.components.size].min
-        cut_point = [0, min_size - 2].max
-
-        trimmed_self = release_self.components[0..cut_point].join('.')
-        trimmed_other = release_other.components[0..cut_point].join('.')
-
-        self.class.parse(trimmed_self) == self.class.parse(trimmed_other)
+        release_self.components[0] == release_other.components[0]
       end
 
       private
