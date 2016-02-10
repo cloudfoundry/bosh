4984cc823eb4b428a8366a8a9413ebd971f6fff2
Devin Fallak <dfallak@pivotal.io>
2016-02-10 11:18:57 -0800

    Add method to match stemcells by patch versions
    [#113302989](https://www.pivotaltracker.com/story/show/113302989)
    
    Signed-off-by: Wagner Camarao <wcamarao@vmware.com>

--- bosh_common/lib/common/version/stemcell_version.rb
+++ bosh_common/lib/common/version/stemcell_version.rb
@@ -14,6 +14,26 @@ module Bosh::Common
         self.new(SemiSemantic::Version.parse(version))
       end
 
+      def self.match_patch_versions(str_a, str_b)
+        version_a, version_b = parse(str_a), parse(str_b)
+        version_a.matches_patch_version(version_b)
+      end
+
+      def matches_patch_version(other)
+        return true if self == other
+
+        release_self = self.version.release
+        release_other = other.version.release
+
+        min_size = [release_self.components.size, release_other.components.size].min
+        cut_point = [0, min_size - 2].max
+
+        trimmed_self = release_self.components[0..cut_point].join('.')
+        trimmed_other = release_other.components[0..cut_point].join('.')
+
+        self.class.parse(trimmed_self) == self.class.parse(trimmed_other)
+      end
+
       private
 
       def default_post_release_segment
