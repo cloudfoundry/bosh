72ac42544d5403c973b1842250506e7759d7b630
Ben Levy <blevy@pivotal.io>
2016-02-12 14:41:03 -0500

    user can attach disk with cid that does not exist in an orphaned list to an instance that was soft stopped
    [#111985863]
    https://www.pivotaltracker.com/story/show/111985863
    
    Signed-off-by: Sukhil Suresh <ssuresh@pivotal.io>

--- bosh-director/lib/bosh/director/jobs/attach_disk.rb
+++ bosh-director/lib/bosh/director/jobs/attach_disk.rb
@@ -21,28 +21,28 @@ module Bosh::Director
       end
 
       def perform
-        instance = Models::Instance.filter(job: @job_name, uuid: @instance_id).to_a.first
+        instance = query_instance_model
 
         if instance.nil? || instance.deployment.name != @deployment_name
           raise AttachDiskErrorUnknownInstance, "Instance '#{@job_name}/#{@instance_id}' in deployment '#{@deployment_name}' was not found"
+        end
+
+        if instance.state != 'detached' && instance.state != 'stopped'
+          raise AttachDiskInvalidInstanceState, "Instance '#{@job_name}/#{@instance_id}' in deployment '#{@deployment_name}' must be in 'bosh stopped' state"
         end
 
         if instance.persistent_disk.nil?
           raise AttachDiskNoPersistentDisk, "Job '#{@job_name}' is not configured with a persistent disk"
         end
 
-        if instance.state != 'detached'
-          raise AttachDiskInvalidInstanceState, "Instance '#{@job_name}/#{@instance_id}' in deployment '#{@deployment_name}' must be in 'bosh stopped --hard' state"
-        end
-
+        previous_persistent_disk = instance.persistent_disk
         @transactor.retryable_transaction(instance.db) do
-
           instance.persistent_disk.update(active: false)
+
           orphan_disk = Models::OrphanDisk[:disk_cid => @disk_cid]
-
           if orphan_disk
-            current_disk = Models::PersistentDisk[instance_id: instance.id]
 
+            current_disk = Models::PersistentDisk[instance_id: instance.id]
             if current_disk
               create_orphan_disk_from_current_disk(current_disk)
             end
@@ -51,7 +51,18 @@ module Bosh::Director
           else
             Models::PersistentDisk.create(disk_cid: @disk_cid, instance_id: instance.id, active: true, size: 1, cloud_properties: {})
           end
+        end
 
+        if instance.state == 'stopped'
+          instance = query_instance_model
+          deployment_plan_instance = deployment_plan_instance(instance)
+          if deployment_plan_instance.nil?
+            raise AttachDiskErrorUnknownInstance, "Deployment plan instance not found for instance model with id #{@instance_id}"
+          end
+
+          disk_manager = DiskManager.new(Config.cloud, logger)
+          disk_manager.orphan_mounted_persistent_disk(deployment_plan_instance, previous_persistent_disk)
+          disk_manager.attach_disk(instance)
         end
 
         "attached disk '#{@disk_cid}' to '#{@job_name}/#{@instance_id}' in deployment '#{@deployment_name}'"
@@ -91,6 +102,21 @@ module Bosh::Director
         end
 
         orphan_disk.destroy
+      end
+
+      def query_instance_model
+        Models::Instance.filter(job: @job_name, uuid: @instance_id).to_a.first
+      end
+
+      def deployment_plan_instance(instance)
+        deployment_model = Api::DeploymentLookup.new.by_name(@deployment_name)
+        planner_factory = DeploymentPlan::PlannerFactory.create(logger)
+        deployment_plan = planner_factory.create_from_model(deployment_model)
+        job = deployment_plan.job(@job_name)
+
+        deployment_plan_instance = DeploymentPlan::Instance.create_from_job(job, 0, instance.state, deployment_model, instance.state, instance.availability_zone, logger)
+        deployment_plan_instance.bind_existing_instance_model(instance)
+        deployment_plan_instance
       end
     end
   end
--- spec/integration/attach_disk_spec.rb
+++ spec/integration/attach_disk_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe 'attach disk', type: :integration do
   with_reset_sandbox_before_each
 
-  it 'Attaches a disk to a hard stopped instance' do
+  let(:simple_manifest) {
     manifest_hash = Bosh::Spec::Deployments.simple_manifest
     manifest_hash['releases'].first['version'] = 'latest'
     manifest_hash['jobs'][0]['instances'] = 1
     manifest_hash['jobs'][0]['persistent_disk'] = 1000
-    deploy_from_scratch(manifest_hash: manifest_hash)
+    manifest_hash
+  }
+
+  it 'Attaches a disk to a hard stopped instance' do
+
+    deploy_from_scratch(manifest_hash: simple_manifest)
 
     instance = director.instances.first
 
@@ -31,6 +36,30 @@ describe 'attach disk', type: :integration do
     first_disk_migration = disk_migrations.first
 
     # disk_migrations.json example: "[{'FromDiskCid': 'foo-cid', 'ToDiskCid': 'bar-cid'}]"
+    expect(first_disk_migration['FromDiskCid']).to eq('disk-cid-abc123')
+    expect(instance.disk_cid).to eq(first_disk_migration['ToDiskCid'])
+  end
+
+  it 'Attaches a disk that does not exist in an orphaned list to a soft stopped instance' do
+
+    deploy_from_scratch(manifest_hash: simple_manifest)
+    instance = director.instances.first
+
+    bosh_runner.run("stop foobar #{instance.id} --soft")
+    bosh_runner.run("attach disk foobar #{instance.id} disk-cid-abc123")
+    bosh_runner.run("start foobar #{instance.id}")
+
+    orphan_disk_output = bosh_runner.run('disks --orphaned')
+    expect(cid_from(orphan_disk_output)).to eq(instance.disk_cid)
+
+    instance = director.instances.first
+    expect(current_sandbox.cpi.disk_attached_to_vm?(instance.vm_cid, instance.disk_cid)).to eq(true)
+
+    agent_dir = current_sandbox.cpi.agent_dir_for_vm_cid(instance.vm_cid)
+    disk_migrations = JSON.parse(File.read("#{agent_dir}/bosh/disk_migrations.json"))
+    expect(disk_migrations.count).to eq(1)
+
+    first_disk_migration = disk_migrations.first
     expect(first_disk_migration['FromDiskCid']).to eq('disk-cid-abc123')
     expect(instance.disk_cid).to eq(first_disk_migration['ToDiskCid'])
   end
