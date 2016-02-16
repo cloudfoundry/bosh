b3122f03ec74c227dad8d6f6c5e730bc4eeafca4
Tyler Schultz <tschultz@pivotal.io>
2016-02-16 15:44:38 -0800

    Update launchpad url
    [#113881681](https://www.pivotaltracker.com/story/show/113881681)

--- stemcell_builder/stages/system_kernel/apply.sh
+++ stemcell_builder/stages/system_kernel/apply.sh
@@ -12,7 +12,7 @@ mkdir -p $chroot/tmp
 
 pkg_mgr install apt-transport-https
 run_in_chroot $chroot "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 363E55AB"
-echo -e "\ndeb https://ealeyner:2ZHkq83hDnKsDfBLVc24@private-ppa.launchpad.net/canonical-support-eng/sf94036/ubuntu trusty main" >> $chroot/etc/apt/sources.list
+echo -e "\ndeb https://ealeyner:tKj51WjcFXjdfCb28Cpp@private-ppa.launchpad.net/canonical-support-eng/sf94036/ubuntu trusty main" >> $chroot/etc/apt/sources.list
 pkg_mgr install linux-headers-3.19.0-49-generic=3.19.0-49.55~14.04.1hf1533043v20160201b1
 pkg_mgr install linux-image-3.19.0-49-generic=3.19.0-49.55~14.04.1hf1533043v20160201b1
 pkg_mgr install linux-image-extra-3.19.0-49-generic=3.19.0-49.55~14.04.1hf1533043v20160201b1
