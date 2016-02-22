b6beeb10932b59c63f0320626c69398fb0915529
William Lindner <wlindner@pivotal.io>
2016-02-22 14:49:01 -0800

    Remove foreign key constraint on 'compiled_packages' table for MySQL in migration script
    fixes:
    
    ```
    Mysql2::Error: Cannot drop index 'stemcell_id': needed in a foreign key constraint (Sequel::DatabaseError)
    ```
    
    [#113954017](https://www.pivotaltracker.com/story/show/113954017)
    
    Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

--- bosh-director/db/migrations/director/20160210201838_denormalize_compiled_package_stemcell_id_to_stemcell_name_and_version.rb
+++ bosh-director/db/migrations/director/20160210201838_denormalize_compiled_package_stemcell_id_to_stemcell_name_and_version.rb
@@ -11,9 +11,16 @@ Sequel.migration do
       stemcell = self[:stemcells].filter(id: compiled_package[:stemcell_id]).first
 
       self[:compiled_packages].filter(id: compiled_package[:id]).update(
-          stemcell_os: stemcell[:operating_system],
-          stemcell_version: stemcell[:version]
+        stemcell_os: stemcell[:operating_system],
+        stemcell_version: stemcell[:version]
       )
+    end
+
+    if [:mysql2, :mysql].include?(adapter_scheme)
+      foreign_key = foreign_key_list(:compiled_packages).find { |constraint| constraint.fetch(:columns) == [:stemcell_id] }.fetch(:name)
+      alter_table(:compiled_packages) do
+        drop_constraint(foreign_key, {type: :foreign_key})
+      end
     end
 
     alter_table(:compiled_packages) do
