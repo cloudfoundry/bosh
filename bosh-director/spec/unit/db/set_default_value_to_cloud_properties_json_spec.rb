3fab7a0e354f203c04ef08b6af58386b9d562698
Matt Cui <mattcui@gmail.com>
2016-03-03 16:29:40 +0800

    Keep consistent with AttachDisk testcase

--- bosh-director/spec/unit/db/set_default_value_to_cloud_properties_json_spec.rb
+++ bosh-director/spec/unit/db/set_default_value_to_cloud_properties_json_spec.rb
@@ -8,7 +8,26 @@ module Bosh::Director
     before { DBSpecHelper.migrate_all_before(migration_file) }
 
     it 'runs set_value_to_cloud_properties_json migration' do
-      db[:persistent_disks] << {id: 1, cloud_properties_json: ''}
+      db[:deployments] << {id: 1, name: 'fake-deployment-name', manifest: '{}'}
+      db[:vms] << {
+          id: 1,
+          agent_id: 'fake-agent-id',
+          cid: 'fake-vm-cid',
+          env_json: 'fake-env-json',
+          trusted_certs_sha1: 'fake-trusted-certs-sha1',
+          credentials_json: 'fake-credentials-json',
+          deployment_id: 1
+      }
+      db[:instances] << {id: 1, job: 'fake-job', index: 1, deployment_id: 1, vm_id: 1, state: 'started'}
+
+      db[:persistent_disks] << {
+
+          instance_id: 1,
+          disk_cid: 'original-disk-cid',
+          size: 50,
+          active: true,
+          cloud_properties_json: ''
+      }
       expect(db[:persistent_disks].first[:cloud_properties_json]).to eq('')
 
       DBSpecHelper.migrate(migration_file)
