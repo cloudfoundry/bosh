cloudfoundry/bosh

commit b6139abee5cc513172cbd5e37dc25fa5e0141ce5

BOSH drain script signals delayed job workers to stop accepting jobs
- refactored bosh-director-worker into separate class: Bosh::Director::Worker
- added additional test for bash function list_child_processes()

[#113099771](https://www.pivotaltracker.com/story/show/113099771)
Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

bosh-director/bin/bosh-director-worker
@@ -49,38 +49,8 @@ begin
     config = Bosh::Director::Config.load_file(config_file)
   end
   config.db
-  Delayed::Worker.logger = config.worker_logger
 
-  index = 0 if index.nil?
-  Bosh::Director::App.new(config)
-
-  Delayed::Worker.backend = :sequel
-  Delayed::Worker.destroy_failed_jobs = true
-  Delayed::Worker.sleep_delay = ENV['INTERVAL'] || 1
-  Delayed::Worker.max_attempts = 0
-  Delayed::Worker.max_run_time = 31536000
-
-  worker = nil
-  queues = (ENV['QUEUES'] || ENV['QUEUE']).to_s.split(',')
-
-  worker = Delayed::Worker.new({:queues => queues})
-
-  worker.name = "worker_#{index}"
-  worker.logger.info("Starting worker #{worker.name}.")
-  begin
-    @worker_retries ||= 0
-
-    # raise Exception.new if @worker_retries<5
-    worker.start
-
-  rescue Exception => e
-    worker.logger.error("Something goes wrong during worker start. Attempt #{@worker_retries}. Error: #{e.inspect}")
-    while @worker_retries < 10
-      @worker_retries += 1
-      sleep 0.5
-      retry
-    end
-    worker.logger.error("Max retries reached. Error: #{e.inspect}")
-    raise e
-  end
+  worker = Bosh::Director::Worker.new(config)
+  worker.prep
+  worker.start
 end

bosh-director/lib/bosh/director/worker.rb
@@ -1,0 +1,51 @@
+module Bosh::Director
+  class Worker
+
+    def initialize(config)
+      @config = config
+    end
+
+    def prep
+      Delayed::Worker.logger = @config.worker_logger
+
+      @index = 0 if @index.nil?
+      Bosh::Director::App.new(@config)
+
+      Delayed::Worker.backend = :sequel
+      Delayed::Worker.destroy_failed_jobs = true
+      Delayed::Worker.sleep_delay = ENV['INTERVAL'] || 1
+      Delayed::Worker.max_attempts = 0
+      Delayed::Worker.max_run_time = 31536000
+
+      @delayed_job_worker = nil
+      queues = (ENV['QUEUES'] || ENV['QUEUE']).to_s.split(',')
+
+      @delayed_job_worker = Delayed::Worker.new({:queues => queues})
+      trap('USR2') { @delayed_job_worker.queues = [] } #stop accepting new jobs when USR2 is sent
+    end
+
+    def start
+      @delayed_job_worker.name = "worker_#{@index}"
+      @delayed_job_worker.logger.info("Starting worker #{@delayed_job_worker.name}.")
+
+      begin
+        @delayed_job_worker_retries ||= 0
+        @delayed_job_worker.start
+
+      rescue Exception => e
+        @delayed_job_worker.logger.error("Something goes wrong during worker start. Attempt #{@delayed_job_worker_retries}. Error: #{e.inspect}")
+        while @delayed_job_worker_retries < 10
+          @delayed_job_worker_retries += 1
+          sleep 0.5
+          retry
+        end
+        @delayed_job_worker.logger.error("Max retries reached. Error: #{e.inspect}")
+        raise e
+      end
+    end
+
+    def queues
+      @delayed_job_worker.queues
+    end
+  end
+end

bosh-director/lib/bosh/director.rb
@@ -63,6 +63,7 @@ require 'bosh/director/compile_task'
 require 'bosh/director/job_renderer'
 require 'bosh/director/cycle_helper'
 require 'bosh/director/encryption_helper'
+require 'bosh/director/worker'
 require 'bosh/director/vm_creator'
 require 'bosh/director/vm_recreator'
 require 'bosh/director/vm_deleter'

bosh-director/spec/unit/worker_spec.rb
@@ -1,0 +1,29 @@
+require 'spec_helper'
+
+describe 'worker' do
+  subject(:worker) { Bosh::Director::Worker.new(config) }
+  let(:config_hash) do
+    {
+      'dir' => '/tmp/boshdir ',
+      'db' => {
+        'adapter' => 'sqlite'
+      },
+      'blobstore' => {
+        'provider' => 'simple',
+        'options' => {}
+      }
+    }
+  end
+  let(:config) { Bosh::Director::Config.load_hash(config_hash) }
+
+  describe 'when workers is sent USR2' do
+    let(:queues) { worker.queues }
+    it 'should not pick up new tasks' do
+      ENV['QUEUE']='normal'
+      worker.prep
+      expect(worker.queues).to eq(['normal'])
+      Process.kill('USR2', Process.pid)
+      expect(worker.queues).to eq([])
+    end
+  end
+end

release/jobs/director/templates/drain
@@ -18,7 +18,7 @@ stop_workers()
     if [ -f "$pid_file" ] ; then
       worker_pid=`cat ${pid_file}`
       # Stop DJ workers from accepting any new jobs
-      kill -s INT ${worker_pid}
+      kill -s USR2 ${worker_pid}
     fi
   done
 }

release/jobs/director/templates/ps_utils.sh
@@ -22,8 +22,7 @@ function kill_process {
 function list_child_processes {
   ps -eo pid,command |
     grep bosh-director-worker |
-    grep "worker_$INDEX.yml" |
+    grep "worker_$1.yml" |
     awk '{print $1}' |
     grep -v ^$1$
 }
-

release/jobs/director/templates/worker_ctl.erb
@@ -63,7 +63,7 @@ case $1 in
   stop)
     PID=$(head -1 $PIDFILE)
     kill_process $PID # prevent the parent from fork()ing new children
-    for CHILD in list_child_processes; do
+    for CHILD in $(list_child_processes $INDEX); do
       kill_process $CHILD
     done
     rm -f $PIDFILE

release/spec/ps_utils_tests.sh
@@ -36,5 +36,28 @@ function test_kill_process {
   fi
 }
 
+function test_list_child_processes {
+  function ps {
+    cat <<EOF
+21 ruby /var/vcap/packages/director/bin/bosh-director-worker -c /var/vcap/jobs/director/config/worker_1.yml -i 1
+22 ruby /var/vcap/packages/director/bin/bosh-director-worker -c /var/vcap/jobs/director/config/worker_2.yml -i 2
+23 ruby /var/vcap/packages/director/bin/bosh-director-worker -c /var/vcap/jobs/director/config/worker_2.yml -i 2
+1     init
+EOF
+  }
+
+  CHILD_PID=$(list_child_processes 2)
+
+  if [ "$CHILD_PID" = "22
+23" ]; then
+    echo "list_child_processes finds its 2 children"
+  else
+    echo "FAILED: list_child_processes can't find its children"
+  fi
+}
+
 test_pid_exists
 test_kill_process
+test_list_child_processes
+unset kill
+unset ps

commit 30a8c83ce3726168bcdafc8b98e544fd5a47533f

delayed_job workers have proper names
previously forked workers had same name 'worker_0', which
caused confusion, multiple workers taking the same task;
broken by b6139ab

[#114963037](https://www.pivotaltracker.com/story/show/114963037)

Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

bosh-director/bin/bosh-director-worker
@@ -50,7 +50,7 @@ begin
   end
   config.db
 
-  worker = Bosh::Director::Worker.new(config)
+  worker = Bosh::Director::Worker.new(config, index)
   worker.prep
   worker.start
 end

bosh-director/lib/bosh/director/worker.rb
@@ -1,14 +1,14 @@
 module Bosh::Director
   class Worker
 
-    def initialize(config)
+    def initialize(config, index=0)
       @config = config
+      @index = index
     end
 
     def prep
       Delayed::Worker.logger = @config.worker_logger
 
-      @index = 0 if @index.nil?
       Bosh::Director::App.new(@config)
 
       Delayed::Worker.backend = :sequel