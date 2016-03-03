634a08b17ae1f6d0402a5f0e2945574d59564e54
Matt Cui <mattcui@gmail.com>
2016-03-03 15:56:18 +0800

    Use the existing DB data

--- bosh-director/spec/unit/db/set_default_value_to_cloud_properties_json_spec.rb
+++ bosh-director/spec/unit/db/set_default_value_to_cloud_properties_json_spec.rb
@@ -8,26 +8,8 @@ module Bosh::Director
     before { DBSpecHelper.migrate_all_before(migration_file) }
 
     it 'runs set_value_to_cloud_properties_json migration' do
-      db[:deployments] << {id: 1, name: 'fake-deployment-name', manifest: '{}'}
-      db[:vms] << {
-          id: 1,
-          agent_id: 'fake-agent-id',
-          cid: 'fake-vm-cid',
-          env_json: 'fake-env-json',
-          trusted_certs_sha1: 'fake-trusted-certs-sha1',
-          credentials_json: 'fake-credentials-json',
-          deployment_id: 1
-      }
-      db[:instances] << {id: 1, job: 'fake-job', index: 1, deployment_id: 1, vm_id: 1, state: 'started'}
-
-      db[:persistent_disks] << {
-          id: 1,
-          instance_id: 1,
-          disk_cid: 'fake-disk-id',
-          size: 20480,
-          active: true,
-          cloud_properties_json: ''
-      }
+      db[:persistent_disks] << {id: 1, cloud_properties_json: ''}
+      expect(db[:persistent_disks].first[:cloud_properties_json]).to eq('')
 
       DBSpecHelper.migrate(migration_file)
 
