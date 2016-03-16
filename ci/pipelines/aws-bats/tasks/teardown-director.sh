c7f18fe916abdf94a79f0e9634a33cafd7e24e26
Tyler Schultz <tylerschultz@gmail.com>
2016-03-16 16:25:38 -0700

    Use correct folder path
    [#115463713](https://www.pivotaltracker.com/story/show/115463713)

--- ci/pipelines/aws-bats/tasks/teardown-director.sh
+++ ci/pipelines/aws-bats/tasks/teardown-director.sh
@@ -5,13 +5,13 @@ set -e
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
 
-initver=$(cat setup-director/bosh-init/version)
-initexe="${PWD}/setup-director/bosh-init/bosh-init-${initver}-linux-amd64"
+initver=$(cat setup-director-output/bosh-init/version)
+initexe="${PWD}/setup-director-output/bosh-init/bosh-init-${initver}-linux-amd64"
 chmod +x ${initexe}
 
 echo "using bosh-init CLI version..."
 $initexe version
 
-director_manifest_file=${PWD}/setup-director/deployment/director-manifest.yml
+director_manifest_file=${PWD}/setup-director-output/deployment/director-manifest.yml
 echo "deleting existing BOSH Director VM..."
 $initexe delete ${director_manifest_file}
