eeeadce0f74845ee77860685e5693dc03394c541
Tyler Schultz <tschultz@pivotal.io>
2016-03-29 17:48:40 -0700

    Update migration to try alternate index deletion strategy
    when index is not returned from Sequel
    
    [#116575441](https://www.pivotaltracker.com/story/show/116575441)

--- bosh-director/db/migrations/director/20160210201838_denormalize_compiled_package_stemcell_id_to_stemcell_name_and_version.rb
+++ bosh-director/db/migrations/director/20160210201838_denormalize_compiled_package_stemcell_id_to_stemcell_name_and_version.rb
@@ -23,30 +23,28 @@ Sequel.migration do
       stemcell_foreign_key_name = stemcell_foreign_keys.first.fetch(:name)
 
       build_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :build] }
-      raise 'Failed to run migration, found more than 1 build index' if build_indexes.size != 1
-      # build_indexes is an array, where each element is an array with first element being the name of index
-      build_index = build_indexes.first.first
-
-      stemcell_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :dependency_key_sha1]}
-      raise 'Failed to run migration, found more than 1 stemcell_id index' if stemcell_indexes.size != 1
-      # stemcell_indexes is an array, where each element is an array with first element being the name of index
-      stemcell_index = stemcell_indexes.first.first
-
       if [:mysql2, :mysql].include?(adapter_scheme)
+        raise 'Failed to run migration, found more than 1 build index' if build_indexes.size != 1
+        # build_indexes is an array, where each element is an array with first element being the name of index
+        build_index = build_indexes.first.first
         alter_table(:compiled_packages) do
           drop_index(nil, name: build_index)
         end
       elsif [:postgres].include?(adapter_scheme)
+        build_index = build_indexes.empty? ? 'compiled_packages_package_id_stemcell_id_build_key' : build_indexes.first.first
         alter_table(:compiled_packages) do
           drop_constraint(build_index)
         end
       end
 
+      stemcell_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :dependency_key_sha1]}
+      stemcell_index = stemcell_indexes.empty? ? 'package_stemcell_dependency_key_sha1_idx' : stemcell_indexes.first.first
+
       alter_table(:compiled_packages) do
         drop_constraint(stemcell_foreign_key_name, :type => :foreign_key)
+        drop_index(nil, name: stemcell_index)
         add_index [:package_id, :stemcell_os, :stemcell_version, :build], unique: true, name: 'package_stemcell_build_idx'
         add_index [:package_id, :stemcell_os, :stemcell_version, :dependency_key_sha1], unique: true, name: 'package_stemcell_dependency_idx'
-        drop_index(nil, name: stemcell_index)
       end
     end
 
