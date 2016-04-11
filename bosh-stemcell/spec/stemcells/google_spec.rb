52f4480028970c86eb5a291d631173380b45d57f
Lyle Franklin <lfranklin@pivotal.io>
2016-04-11 09:41:39 -0700

    Add tests to ensure centos/rhel google packages are installed
    [#116463189](https://www.pivotaltracker.com/story/show/11646318)

--- bosh-stemcell/spec/stemcells/google_spec.rb
+++ bosh-stemcell/spec/stemcells/google_spec.rb
@@ -16,19 +16,34 @@ describe 'Google Stemcell', stemcell_image: true do
   end
 
   context 'installed by system_google_packages' do
-    describe file('/etc/init/google-accounts-manager-service.conf') do
-      it { should be_file }
-      it { should be_executable }
-    end
+    case ENV['OS_NAME']
+      when 'ubuntu'
+        describe file('/etc/init/google-accounts-manager-service.conf') do
+          it { should be_file }
+          it { should be_executable }
+        end
 
-    describe file('/etc/init/google-accounts-manager-task.conf') do
-      it { should be_file }
-      it { should be_executable }
-    end
+        describe file('/etc/init/google-accounts-manager-task.conf') do
+          it { should be_file }
+          it { should be_executable }
+        end
 
-    describe file('/etc/init/google-clock-sync-manager.conf') do
-      it { should be_file }
-      it { should be_executable }
+        describe file('/etc/init/google-clock-sync-manager.conf') do
+          it { should be_file }
+          it { should be_executable }
+        end
+      when 'centos', 'rhel'
+        describe file('/usr/lib/systemd/system/google-accounts-manager.service') do
+          it { should be_file }
+        end
+
+        describe file('/usr/lib/systemd/system/google-accounts-manager.service') do
+          it { should be_file }
+        end
+
+        describe file('/usr/lib/systemd/system/google-clock-sync-manager.service') do
+          it { should be_file }
+        end
     end
   end
 end
