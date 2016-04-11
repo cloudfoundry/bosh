4faf7384332719518be58d430eb3da91e82d62c6
Lyle Franklin <lfranklin@pivotal.io>
2016-04-11 08:57:30 -0700

    Re-add google-address-manager service
    - Confirmed with Evan that this is required for GCE load balancers to
      function
    
    [#116463189](https://www.pivotaltracker.com/story/show/116463189)

--- stemcell_builder/stages/system_google_packages/apply.sh
+++ stemcell_builder/stages/system_google_packages/apply.sh
@@ -14,15 +14,14 @@ if [ -f $chroot/etc/debian_version ] # Ubuntu
 then
   # Run google-accounts-manager and google-clock-sync-manager with upstart
   cp $assets_dir/etc/init/google-accounts-manager-{task,service}.conf $chroot/etc/init/
+  cp $assets_dir/google-address-manager.conf $chroot/etc/init/
   cp $assets_dir/google-clock-sync-manager.conf $chroot/etc/init/
 elif [ -f $chroot/etc/redhat-release ] # Centos or RHEL
 then
   run_in_chroot $chroot "/bin/systemctl enable /usr/lib/systemd/system/google-accounts-manager.service"
+  run_in_chroot $chroot "/bin/systemctl enable /usr/lib/systemd/system/google-address-manager.service"
   run_in_chroot $chroot "/bin/systemctl enable /usr/lib/systemd/system/google-clock-sync-manager.service"
 else
   echo "Unknown OS, exiting"
   exit 2
 fi
-
-# Hack: replace google metadata hostname with ip address (bosh agent might set a dns that it's unable to resolve the hostname)
-run_in_chroot $chroot "find /usr/share/google -type f -exec sed -i 's/metadata.google.internal/169.254.169.254/g' {} +"
