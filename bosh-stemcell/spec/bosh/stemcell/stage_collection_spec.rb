e23c1ca55d18ebef00630634a242dd02e08cedba
Peter Kalambet <peter.kalambet@ru.ibm.com>
2016-04-12 07:07:41 +0000

    Create path for local blobstore inside stemcell for micro_bosh_go disabled build Return back skiped stages for `ppc64le` in favor of micro_bosh_enabled variable
    Signed-off-by: Yulia Gaponenko <yulia.gaponenko@ru.ibm.com>

--- bosh-stemcell/spec/bosh/stemcell/stage_collection_spec.rb
+++ bosh-stemcell/spec/bosh/stemcell/stage_collection_spec.rb
@@ -121,7 +121,7 @@ module Bosh::Stemcell
           :aws_cli,
           :logrotate_config,
           :dev_tools_config,
-        ].reject{ |s| Bosh::Stemcell::Arch.ppc64le? and [:bosh_ruby, :bosh_micro_go].include?(s) }
+        ]
       end
 
       it 'returns the correct stages' do
