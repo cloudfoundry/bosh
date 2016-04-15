ff3f1277a456579fe0900c80d9c6632982c3d821
Sukhil Suresh <ssuresh@pivotal.io>
2016-04-15 08:54:06 -0400

    Use common event format (CEF) when emitting audit events
    [#116909807]
    https://www.pivotaltracker.com/story/show/116909807
    
    Signed-off-by: Saman Alvi <salvi@pivotal.io>

--- bosh-director/lib/bosh/director/api/extensions/syslog_request_logger.rb
+++ bosh-director/lib/bosh/director/api/extensions/syslog_request_logger.rb
@@ -7,62 +7,66 @@ end
 
 require 'socket'
 
+# auth.auth_type
+
 module Bosh::Director
   module Api
     module Extensions
       module SyslogRequestLogger
-        DESIRED_HEADERS = [
-          'HTTP_HOST',
-          'HTTP_X_REAL_IP',
-          'HTTP_X_FORWARDED_FOR',
-          'HTTP_X_FORWARDED_PROTO',
-          'HTTP_USER_AGENT',
-          'HTTP_X_BOSH_UPLOAD_REQUEST_TIME'
-        ]
+
+        DESIRED_HEADERS = %w(
+          HTTP_HOST
+          HTTP_X_REAL_IP
+          HTTP_X_FORWARDED_FOR
+          HTTP_X_FORWARDED_PROTO
+          HTTP_USER_AGENT
+          HTTP_X_BOSH_UPLOAD_REQUEST_TIME
+        )
 
         def log_request_to_syslog
-
-          before do
-            # TODO: the ruby version checks can be removed when we stop testing w 1.9
-            if @config.log_access_events_to_syslog && RUBY_VERSION.to_i > 1
-              @timestamp = Time.now.utc
-            end
-          end
-
           after do
             if @config.log_access_events_to_syslog && RUBY_VERSION.to_i > 1
-              request_hash = {'type' => 'api'}
+              header_string = ''
+              filtered_headers = request.env.select { |key, _| DESIRED_HEADERS.include?(key) }
+                                     .collect { |key, value| [key.sub(/^HTTP_/, ''), value] }
+              filtered_headers.each do |header_set|
+                header_string += header_set[0] + "\=" + header_set[1] + "&"
+              end
+              header_string = header_string[0..-2] if !header_string.empty?
 
-              request_hash['timestamp'] = @timestamp
-
-              auth_type = current_user.nil? ? 'none' : identity_provider.client_info['type']
-              request_hash['auth'] = {'type' => auth_type}
-              request_hash['auth']['user'] = @user.username if @user && @user.username
-              request_hash['auth']['client'] = @user.client if @user && @user.client
-
-              request_hash['http'] = {
-                'verb' => request.request_method,
-                'path' => request.path
-              }
-              request_hash['http']['query'] = request.query_string unless request.query_string.blank?
-              request_hash['client'] = {'ip' => request.ip}
-              response_status = response.status
-              request_hash['http']['status'] = {'code' => response_status}
-              request_hash['http']['status']['reason'] = response.body.join('')[0...500] if response_status >= 400
-
-              request_hash['http']['headers'] = request.env
-                                                  .select { |key, _| DESIRED_HEADERS.include?(key) }
-                                                  .collect { |key, value| [key.sub(/^HTTP_/, ''), value] }
-
-              request_hash['component'] = {'name' => 'director'}
-              request_hash['component']['version'] = Bosh::Director::VERSION
-              request_hash['component']['port'] = @config.port
-              request_hash['component']['hostname'] = Socket.gethostname
-              request_hash['component']['ips'] = Socket.ip_address_list
+              filtered_ip_list = Socket.ip_address_list
                         .reject { |addr| !addr.ip? || addr.ipv4_loopback? || addr.ipv6_loopback? }
                         .map { |addr| addr.ip_address }
 
-              Syslog::Logger.new('bosh.director').info(JSON.dump(request_hash))
+              cef_version = 0
+              device_vendor = 'CloudFoundry'
+              device_product = 'BOSH'
+              device_version = Bosh::Director::VERSION
+              signature_id = 'director_api'
+              name = "#{request.path}"
+              severity = response.status >= 400 ?  7 : 1
+
+              extension = ''
+              if @user
+                extension += "duser=#{@user.username} " if @user.username
+                extension += "requestClientApplication=#{@user.client} " if @user.client
+              end
+
+              extension += "requestMethod=#{request.request_method} src=#{request.ip} spt=#{@config.port}" +
+                  " shost=#{Socket.gethostname}" +
+                  " cs1=#{filtered_ip_list.join(',')} cs1Label=ips" +
+                  " cs2=#{header_string} cs2Label=httpHeaders" +
+                  " cs3=#{current_user.nil? ? 'none' : identity_provider.client_info['type']} cs3Label=authType" +
+                  " cs4=#{response.status} cs4Label=responseStatus"
+              if response.status >= 400
+                extension += " cs5=#{response.body.join('')[0...500].strip} cs5Label=statusReason"
+              end
+
+              cef_log = 'CEF:%i|%s|%s|%s|%s|%s|%s|%s' % [cef_version, device_vendor, device_product,
+                                                         device_version, signature_id, name, severity, extension]
+              cef_log_encoded = cef_log.force_encoding(Encoding::UTF_8)
+
+              Syslog::Logger.new('bosh.director').info(cef_log_encoded)
             end
           end
         end
