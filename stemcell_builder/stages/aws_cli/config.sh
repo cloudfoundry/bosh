b416e8a4d608575efc1b40a7294166273c08c1fe
Kam Leung <kleung@pivotal.io>
2016-04-19 14:12:28 -0700

    Bump s3cli to use 0.0.31 in stemcell builder step
    [#117934455](https://www.pivotaltracker.com/story/show/117934455)

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -9,6 +9,6 @@ source $base_dir/lib/prelude_config.bash
 cd $assets_dir
 rm -rf s3cli
 mkdir s3cli
-current_version=0.0.24
+current_version=0.0.31
 curl -L -o s3cli/s3cli https://s3.amazonaws.com/s3cli-artifacts/s3cli-${current_version}-linux-amd64
-echo "a1035806703d7909f278e5c9e6d975289067c8c8 s3cli/s3cli" | sha1sum -c -
+echo "32130fe8d775c33695dc83ec21ae41c97d804023 s3cli/s3cli" | sha1sum -c -
