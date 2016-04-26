695bed549bd93dd263285b6da08b4c23fe24e98c
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2016-04-26 14:41:22 -0700

    Adding final release for build 3232

--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.3215.4.0'
+      VERSION = '1.3232.0'
     end
   end
 end
--- bosh_common/lib/common/version.rb
+++ bosh_common/lib/common/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Common
-    VERSION = '1.3215.4.0'
+    VERSION = '1.3232.0'
   end
 end
