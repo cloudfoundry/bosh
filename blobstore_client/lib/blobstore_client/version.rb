13b17a98bf759e2c8b0e8c8e65b0f7b5ae71d3fb
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2016-04-29 11:46:02 -0700

    Adding final release for build 3232.1

--- blobstore_client/lib/blobstore_client/version.rb
+++ blobstore_client/lib/blobstore_client/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Blobstore
     class Client
-      VERSION = '1.3232.0'
+      VERSION = '1.3232.1.0'
     end
   end
 end
--- bosh_cli_plugin_micro/lib/bosh/deployer/version.rb
+++ bosh_cli_plugin_micro/lib/bosh/deployer/version.rb
@@ -1,5 +1,5 @@
 module Bosh
   module Deployer
-    VERSION = '1.3232.0'
+    VERSION = '1.3232.1.0'
   end
 end
