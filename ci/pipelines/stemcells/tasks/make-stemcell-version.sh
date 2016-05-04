1b40636457e39b10472a0c43a2848d510c078821
dmitriy kalinin <dkalinin@pivotal.io>
2016-05-03 18:41:00 -0700

    fix .0 stemcell versions in azure pipeline

--- ci/pipelines/stemcells/tasks/make-stemcell-version.sh
+++ ci/pipelines/stemcells/tasks/make-stemcell-version.sh
@@ -4,4 +4,11 @@ set -e -x
 
 [ -f published-stemcell/version ] || exit 1
 
-echo "$(cat published-stemcell/version).0.0" > semver
+published_version=$(cat published-stemcell/version)
+
+# check for minor (only supports x and x.x)
+if [[ "$published_version" == *.* ]]; then
+	echo "${published_version}.0" > semver # fill in patch
+else
+	echo "${published_version}.0.0" > semver # fill in minor.patch
+fi
