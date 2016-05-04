d2b46ee8ffbb398ab9ce526a5c4c3ed0659d170d
Danny Berger <dberger@pivotal.io>
2016-05-04 09:22:07 -0700

    Install specific version of bundler for os-image

--- ci/pipelines/os-image/tasks/build.sh
+++ ci/pipelines/os-image/tasks/build.sh
@@ -17,6 +17,6 @@ set_up_vagrant_private_key
 cd bosh-src
 print_git_state
 
-gem install bundler
+gem install bundler --version 1.11.2 --no-ri --no-rdoc
 bundle install --local
 bundle exec rake --trace ci:publish_os_image_in_vm[$OPERATING_SYSTEM_NAME,$OPERATING_SYSTEM_VERSION,remote,$OS_IMAGE_S3_BUCKET_NAME,$OS_IMAGE_S3_KEY]
