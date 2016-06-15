cd968da70c1106d857bb4b083ecb685e184f5aaa
Tyler Schultz <tschultz@pivotal.io>
2016-06-15 16:54:00 -0700

    Bump s3cli to 0.0.41
    [#120724729](https://www.pivotaltracker.com/story/show/120724729)
    
    Signed-off-by: Michael Trestman <mtrestman@pivotal.io>

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -9,6 +9,6 @@ source $base_dir/lib/prelude_config.bash
 cd $assets_dir
 rm -rf s3cli
 mkdir s3cli
-current_version=0.0.40
+current_version=0.0.41
 curl -L -o s3cli/s3cli https://s3.amazonaws.com/s3cli-artifacts/s3cli-${current_version}-linux-amd64
-echo "c6472e3a522ac3a047a404a6865a97b6377cf3a8 s3cli/s3cli" | sha1sum -c -
+echo "04a38c1f69167af64eb52b0226232dd30223bce4 s3cli/s3cli" | sha1sum -c -
