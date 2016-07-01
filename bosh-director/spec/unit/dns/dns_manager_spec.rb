cloudfoundry/bosh

commit 85bb2ecfce08bdc022be31ed8dbfcbfd02568164

Avoid using separate dns records table; call publish when dns is changing

bosh-director/lib/bosh/director/dns/dns_manager.rb
@@ -1,76 +1,28 @@
 module Bosh::Director
   class DnsManagerProvider
     def self.create
-      dns_enabled = !!Config.dns_db # to be consistent with current behavior
-      if dns_enabled
-        dns_config = Config.dns || {}
-        logger = Config.logger
-        local_dns_repo = LocalDnsRepo.new(logger)
-        dns_domain_name = Canonicalizer.canonicalize(dns_config.fetch('domain_name', 'bosh'), :allow_dots => true)
-        dns_provider = PowerDns.new(dns_domain_name, logger)
+      dns_config = Config.dns || {}
 
-        EnabledDnsManager.new(dns_domain_name, dns_config, dns_provider, local_dns_repo, logger)
-      else
-        DisabledDnsManager.new
-      end
-    end
-  end
+      logger = Config.logger
+      dns_domain_name = Canonicalizer.canonicalize(dns_config.fetch('domain_name', 'bosh'), :allow_dots => true)
+      local_dns_repo = LocalDnsRepo.new(logger)
 
-  private
+      dns_publisher = BlobstoreDnsPublisher.new(App.instance.blobstores.blobstore, dns_domain_name) if Config.local_dns
+      dns_provider = PowerDns.new(dns_domain_name, logger) if !!Config.dns_db
 
-  class DnsManager
-    def configure_nameserver ; end
-
-    def delete_dns_for_instance(instance_model) ; end
-
-    def dns_record_name(hostname, job_name, network_name, deployment_name) ; end
-
-    # build a list of dns servers to use
-    def dns_servers(network, dns_spec, add_default_dns = true)
-      servers = nil
-
-      if dns_spec
-        servers = []
-        dns_spec.each do |dns|
-          dns = NetAddr::CIDR.create(dns)
-          unless dns.size == 1
-            raise NetworkInvalidDns,
-              "Invalid DNS for network '#{network}': must be a single IP"
-          end
-
-          servers << dns.ip
-        end
-      end
-
-      return servers unless add_default_dns
-      add_default_dns_server(servers)
-    end
-
-    def find_dns_record(dns_record_name, ip_address) ; end
-
-    def find_dns_record_names_by_instance(instance_model) ; end
-
-    def flush_dns_cache ; end
-
-    def migrate_legacy_records(instance_model) ; end
-
-    def update_dns_record_for_instance(instance_model, dns_names_to_ip) ; end
-
-    private
-
-    def add_default_dns_server(servers)
-      servers
+      DnsManager.new(dns_domain_name, dns_config, dns_provider, dns_publisher, local_dns_repo, logger)
     end
   end
 
   public
 
-  class EnabledDnsManager < DnsManager
+  class DnsManager
     attr_reader :dns_domain_name
 
-    def initialize(dns_domain_name, dns_config, dns_provider, local_dns_repo, logger)
+    def initialize(dns_domain_name, dns_config, dns_provider, dns_publisher, local_dns_repo, logger)
       @dns_domain_name = dns_domain_name
       @dns_provider = dns_provider
+      @dns_publisher = dns_publisher
       @default_server = dns_config['server']
       @flush_command = dns_config['flush_command']
       @ip_address = dns_config['address']
@@ -79,19 +31,19 @@ module Bosh::Director
     end
 
     def dns_enabled?
-      true
+      !@dns_provider.nil?
+    end
+
+    def publisher_enabled?
+      !@dns_publisher.nil?
     end
 
     def configure_nameserver
-      @dns_provider.create_or_update_nameserver(@ip_address)
+      @dns_provider.create_or_update_nameserver(@ip_address) if dns_enabled?
     end
 
     def find_dns_record(dns_record_name, ip_address)
       @dns_provider.find_dns_record(dns_record_name, ip_address)
-    end
-
-    def find_dns_record_names_by_instance(instance_model)
-      instance_model.nil? ? [] : instance_model.dns_record_names.to_a.compact
     end
 
     def update_dns_record_for_instance(instance_model, dns_names_to_ip)
@@ -108,6 +60,7 @@ module Bosh::Director
 
     def migrate_legacy_records(instance_model)
       return if @local_dns_repo.find(instance_model).any?
+      return unless dns_enabled?
 
       index_pattern_for_all_networks = dns_record_name(
         instance_model.index,
@@ -181,6 +134,18 @@ module Bosh::Director
           @logger.warn("Failed to flush DNS cache: #{stderr.chomp}")
         end
       end
+      publish_dns_records
+    end
+
+    def publish_dns_records
+      if publisher_enabled?
+        dns_records = @dns_publisher.export_dns_records
+        @dns_publisher.publish(dns_records)
+      end
+    end
+
+    def find_dns_record_names_by_instance(instance_model)
+      instance_model.nil? ? [] : instance_model.dns_record_names.to_a.compact
     end
 
     def dns_record_name(hostname, job_name, network_name, deployment_name)
@@ -204,16 +169,6 @@ module Bosh::Director
       end
 
       servers
-    end
-  end
-
-  class DisabledDnsManager < DnsManager
-    def dns_domain_name
-      nil
-    end
-
-    def dns_enabled?
-      false
     end
   end
 end

bosh-director/spec/unit/dns/dns_manager_spec.rb
@@ -1,47 +1,174 @@
 require 'spec_helper'
+require 'blobstore_client/null_blobstore_client'
 
 module Bosh::Director
   describe DnsManager do
+    subject(:dns_manager) { described_class.new(domain.name, dns_config, dns_provider, dns_publisher, local_dns_repo, logger) }
+
     let(:instance_model) { Models::Instance.make(uuid: 'fake-uuid', index: 0, job: 'job-a', deployment: deployment_model) }
     let(:deployment_model) { Models::Deployment.make(name:'dep') }
     let(:local_dns_repo) { LocalDnsRepo.new(logger) }
     let(:domain) { Models::Dns::Domain.make(name: 'bosh', type: 'NATIVE') }
+    let(:dns_config) { {} }
+    let(:dns_provider) { nil }
+    let(:dns_publisher) { nil }
 
-    describe EnabledDnsManager do
-      subject(:dns_manager) { described_class.new(domain.name, dns_config, dns_provider, local_dns_repo, logger) }
+    describe '#dns_servers' do
+      it 'should return nil when there are no DNS servers' do
+        expect(dns_manager.dns_servers('network', nil)).to be_nil
+      end
+
+      it 'should return an array of DNS servers' do
+        expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8])).to eq(%w[1.2.3.4 5.6.7.8])
+      end
+
+      it "should raise an error if a DNS server isn't specified with as an IP" do
+        expect {
+          dns_manager.dns_servers('network', %w[1.2.3.4 foo.bar])
+        }.to raise_error
+      end
+
+      context 'when there is a default server' do
+        let(:dns_config) { {'domain_name' => domain.name, 'server' => '9.10.11.12'} }
+
+        it 'should add default dns server when there are no DNS servers' do
+          expect(dns_manager.dns_servers('network', [])).to eq(%w[9.10.11.12])
+        end
+
+        it 'should add default dns server to an array of DNS servers' do
+          expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8])).to eq(%w[1.2.3.4 5.6.7.8 9.10.11.12])
+        end
+
+        it 'should not add default dns server to an array of DNS servers' do
+          expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8], false)).to eq(%w[1.2.3.4 5.6.7.8])
+        end
+
+        it 'should add default dns server to an array of DNS servers' do
+          expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8])).to eq(%w[1.2.3.4 5.6.7.8 9.10.11.12])
+        end
+
+        it 'should not add default dns server if already set' do
+          expect(dns_manager.dns_servers('network', %w[1.2.3.4 9.10.11.12])).to eq(%w[1.2.3.4 9.10.11.12])
+        end
+
+        context 'when dns server is 127.0.0.1' do
+          let(:dns_config) { {'domain_name' => domain.name, 'server' => '127.0.0.1'} }
+
+          it 'should not add default dns server if it is 127.0.0.1' do
+            expect(dns_manager.dns_servers('network', %w[1.2.3.4])).to eq(%w[1.2.3.4])
+          end
+        end
+      end
+    end
+
+    describe '#flush_dns_cache' do
+      let(:dns_config) { {'domain_name' => domain.name, 'flush_command' => flush_command} }
+      let(:flush_command) { nil }
+
+      context 'when flush command is present' do
+        let(:flush_command) { "echo \"7\" && exit 0" }
+
+        it 'logs success' do
+          expect(logger).to receive(:debug).with("Flushed 7 records from DNS cache")
+          dns_manager.flush_dns_cache
+        end
+      end
+
+      context 'when running flush command fails' do
+        let(:flush_command) { "echo fake failure >&2 && exit 1" }
+
+        it 'logs an error' do
+          expect(logger).to receive(:warn).with("Failed to flush DNS cache: fake failure")
+          dns_manager.flush_dns_cache
+        end
+      end
+
+      context 'when flush command is not present' do
+        it 'does not do anything' do
+          expect(Open3).to_not receive(:capture3)
+          expect {
+            dns_manager.flush_dns_cache
+          }.to_not raise_error
+        end
+      end
+
+      context 'when dns_publisher is enabled' do
+        it 'calls export_dns_records and publish on the dns_publisher' do
+          expect(dns_manager).to receive(:publish_dns_records)
+          dns_manager.flush_dns_cache
+        end
+      end
+
+      context 'when dns_publisher is disabled' do
+        it 'calls nothing on the dns_publisher' do
+          dns_manager.flush_dns_cache
+        end
+      end
+    end
+
+    describe '#publish_dns_records' do
+      context 'when dns_publisher is enabled' do
+        let(:blobstore) { Bosh::Blobstore::NullBlobstoreClient.new }
+        let(:dns_publisher) { BlobstoreDnsPublisher.new(blobstore, 'fake-domain-name') }
+
+        it 'calls export_dns_records and publish on the dns_publisher' do
+          expect(dns_publisher).to receive(:export_dns_records).and_return([])
+          expect(dns_publisher).to receive(:publish).with([])
+          dns_manager.flush_dns_cache
+        end
+      end
+
+      context 'when dns_publisher is disabled' do
+        it 'calls nothing on the dns_publisher' do
+          dns_manager.flush_dns_cache
+        end
+      end
+    end
+
+    describe '#find_dns_record_names_by_instance' do
+      context 'instance model is not set' do
+        let(:instance_model) { nil }
+
+        it 'returns an empty list' do
+          expect(dns_manager.find_dns_record_names_by_instance(instance_model)).to eq([])
+        end
+      end
+
+      context 'instance model is set' do
+        let(:instance_model) { Models::Instance.make(uuid: 'fake-uuid', index: 0, job: 'job-a', deployment: deployment_model, dns_records: '["test1.example.com","test2.example.com"]') }
+
+        it 'returns an empty list' do
+          expect(dns_manager.find_dns_record_names_by_instance(instance_model)).to eq(['test1.example.com', 'test2.example.com'])
+        end
+      end
+    end
+
+    describe '#dns_record_name' do
+      context 'when network is a wildcard' do
+        it 'does not escape the network segment' do
+          expect(dns_manager.dns_record_name('hostname', 'job_Name', '%', 'deployment_Name')).to eq('hostname.job-name.%.deployment-name.bosh')
+        end
+      end
+
+      context 'when special tokens are used' do
+        it 'all segments are escaped' do
+          expect(dns_manager.dns_record_name('hostname', 'job_Name', 'network_Name', 'deployment_Name')).to eq('hostname.job-name.network-name.deployment-name.bosh')
+        end
+      end
+
+      context 'when fields are normal' do
+        it 'does not escape the network segment' do
+          expect(dns_manager.dns_record_name('hostname', 'job-name', 'network-name', 'deployment-name')).to eq('hostname.job-name.network-name.deployment-name.bosh')
+        end
+      end
+    end
+
+    context 'when PowerDNS is enabled' do
       let(:dns_provider) { PowerDns.new(domain.name, logger) }
-      let(:dns_config) { {} }
 
-      describe '#flush_dns_cache' do
-        let(:dns_config) { {'domain_name' => domain.name, 'flush_command' => flush_command} }
-
-        context 'when flush command is present' do
-          let(:flush_command) { "echo \"7\" && exit 0" }
-
-          it 'logs success' do
-            expect(logger).to receive(:debug).with("Flushed 7 records from DNS cache")
-            dns_manager.flush_dns_cache
-          end
-        end
-
-        context 'when running flush command fails' do
-          let(:flush_command) { "echo fake failure >&2 && exit 1" }
-
-          it 'logs an error' do
-            expect(logger).to receive(:warn).with("Failed to flush DNS cache: fake failure")
-            dns_manager.flush_dns_cache
-          end
-        end
-
-        context 'when flush command is not present' do
-          let(:flush_command) { nil }
-
-          it 'does not do anything' do
-            expect(Open3).to_not receive(:capture3)
-            expect {
-              dns_manager.flush_dns_cache
-            }.to_not raise_error
-          end
+      describe '#dns_enabled?' do
+        it 'should be true' do
+          expect(dns_manager.dns_enabled?).to eq(true)
         end
       end
 
@@ -89,56 +216,6 @@ module Bosh::Director
             expect(a_record.content).to eq('1.2.3.4')
             expect(soa_record.content).to eq(PowerDns::SOA)
             expect(domain).to_not eq(nil)
-          end
-        end
-
-
-      end
-
-      describe '#dns_servers' do
-        it 'should return nil when there are no DNS servers' do
-          expect(dns_manager.dns_servers('network', nil)).to be_nil
-        end
-
-        it 'should return an array of DNS servers' do
-          expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8])).to eq(%w[1.2.3.4 5.6.7.8])
-        end
-
-        it "should raise an error if a DNS server isn't specified with as an IP" do
-          expect {
-            dns_manager.dns_servers('network', %w[1.2.3.4 foo.bar])
-          }.to raise_error
-        end
-
-        context 'when there is a default server' do
-          let(:dns_config) { {'domain_name' => domain.name, 'server' => '9.10.11.12'} }
-
-          it 'should add default dns server when there are no DNS servers' do
-            expect(dns_manager.dns_servers('network', [])).to eq(%w[9.10.11.12])
-          end
-
-          it 'should add default dns server to an array of DNS servers' do
-            expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8])).to eq(%w[1.2.3.4 5.6.7.8 9.10.11.12])
-          end
-
-          it 'should not add default dns server to an array of DNS servers' do
-            expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8], false)).to eq(%w[1.2.3.4 5.6.7.8])
-          end
-
-          it 'should add default dns server to an array of DNS servers' do
-            expect(dns_manager.dns_servers('network', %w[1.2.3.4 5.6.7.8])).to eq(%w[1.2.3.4 5.6.7.8 9.10.11.12])
-          end
-
-          it 'should not add default dns server if already set' do
-            expect(dns_manager.dns_servers('network', %w[1.2.3.4 9.10.11.12])).to eq(%w[1.2.3.4 9.10.11.12])
-          end
-
-          context 'when dns server is 127.0.0.1' do
-            let(:dns_config) { {'domain_name' => domain.name, 'server' => '127.0.0.1'} }
-
-            it 'should not add default dns server if it is 127.0.0.1' do
-              expect(dns_manager.dns_servers('network', %w[1.2.3.4])).to eq(%w[1.2.3.4])
-            end
           end
         end
       end
@@ -210,7 +287,6 @@ module Bosh::Director
               ])
         end
 
-
         context 'when local repo has dns records' do
           before do
             local_dns_repo.create_or_update(instance_model, ['anything'])
@@ -222,11 +298,37 @@ module Bosh::Director
           end
         end
       end
+
+      context 'when LocalDNS is enabled' do
+        let(:blobstore) { Bosh::Blobstore::NullBlobstoreClient.new }
+        let(:dns_publisher) { BlobstoreDnsPublisher.new(blobstore, 'fake-domain-name') }
+
+        describe '#publisher_enabled?' do
+          it 'should be true' do
+            expect(dns_manager.publisher_enabled?).to eq(true)
+          end
+        end
+      end
+
+      context 'when LocalDNS is disabled' do
+        let(:dns_publisher) { nil }
+
+        describe '#publisher_enabled?' do
+          it 'should be false' do
+            expect(dns_manager.publisher_enabled?).to eq(false)
+          end
+        end
+      end
     end
 
-    describe DisabledDnsManager do
-      subject(:dns_manager) { described_class.new }
+    context 'when PowerDNS is disabled' do
       let(:instance_model) { Models::Instance.make(uuid: 'fake-uuid', index: 0, job: 'job-a', deployment: deployment_model) }
+
+      describe '#dns_enabled?' do
+        it 'should be false' do
+          expect(dns_manager.dns_enabled?).to eq(false)
+        end
+      end
 
       describe '#migrate_legacy_records' do
         it 'does not migrate' do
@@ -234,7 +336,6 @@ module Bosh::Director
           expect(local_dns_repo.find(instance_model)).to match_array([])
         end
       end
-
 
       describe '#dns_servers' do
         it 'should not add default dns server when dns is not enabled' do
@@ -251,6 +352,27 @@ module Bosh::Director
           expect(ns_record).to eq(nil)
           expect(a_record).to eq(nil)
           expect(soa_record).to eq(nil)
+        end
+      end
+
+      context 'when LocalDNS is enabled' do
+        let(:blobstore) { Bosh::Blobstore::NullBlobstoreClient.new }
+        let(:dns_publisher) { BlobstoreDnsPublisher.new(blobstore, 'fake-domain-name') }
+
+        describe '#publisher_enabled?' do
+          it 'should be true' do
+            expect(dns_manager.publisher_enabled?).to eq(true)
+          end
+        end
+      end
+
+      context 'when LocalDNS is disabled' do
+        let(:dns_publisher) { nil }
+
+        describe '#publisher_enabled?' do
+          it 'should be false' do
+            expect(dns_manager.publisher_enabled?).to eq(false)
+          end
         end
       end
     end

bosh-director/db/migrations/director/20160531164756_add_local_dns_blobs.rb
@@ -1,0 +1,14 @@
+Sequel.migration do
+  up do
+    create_table :local_dns_blobs do
+      primary_key :id
+      String :blobstore_id, :null => false
+      String :sha1, :null => false
+      Time :created_at, null: false
+    end
+  end
+
+  down do
+    drop_table :local_dns_blobs
+  end
+end

bosh-director/db/migrations/director/20160531164756_add_local_dns_records.rb
@@ -1,23 +1,0 @@
-Sequel.migration do
-  up do
-    create_table :local_dns_records do
-      primary_key :id
-      Time :timestamp, :index => true, :null => false
-      # @todo should name+ip be unique?
-      String :name, :unique => true, :null => false
-      String :ip, :unique => false, :null => false
-    end
-
-    create_table :local_dns_blobs do
-      primary_key :id
-      String :blobstore_id, :null => false
-      String :sha1, :null => false
-      Time :created_at, null: false
-    end
-  end
-
-  down do
-    drop_table :local_dns_records
-    drop_table :local_dns_blobs
-  end
-end

bosh-director/lib/bosh/director/dns/blobstore_dns_publisher.rb
@@ -1,7 +1,8 @@
 module Bosh::Director
   class BlobstoreDnsPublisher
-    def initialize(blobstore)
+    def initialize(blobstore, domain_name)
       @blobstore = blobstore
+      @domain_name = domain_name
     end
 
     def publish(dns_records)
@@ -12,23 +13,14 @@ module Bosh::Director
     end
 
     def export_dns_records
-      Models::LocalDnsRecord.all.map do |record|
-        [record.ip, record.name]
+      hosts = []
+      Models::Instance.all.map do |instance|
+        spec = instance.spec
+        spec['networks'].each do |network_name, network|
+          hosts << [ network['ip'], spec['id'] + '.' + spec['name'] + '.' + network_name + '.' + spec['deployment'] + '.' + @domain_name ]
+        end
       end
-    end
-
-    def persist_dns_record(ip, name)
-      existing = Models::LocalDnsRecord.find(:name => name)
-
-      if existing.nil?
-        Models::LocalDnsRecord.create(:ip => ip, :name => name, :timestamp => Time.new)
-      else
-        existing.update(:ip => ip, :timestamp => Time.new)
-      end
-    end
-
-    def delete_dns_record(name)
-      Models::LocalDnsRecord.grep(:name, name).delete
+      hosts
     end
 
     def cleanup_blobs

bosh-director/lib/bosh/director/instance_deleter.rb
@@ -33,6 +33,7 @@ module Bosh::Director
 
           error_ignorer.with_force_check do
             @dns_manager.delete_dns_for_instance(instance_model)
+            @dns_manager.publish_dns_records
           end
 
           error_ignorer.with_force_check do

bosh-director/lib/bosh/director/models.rb
@@ -11,7 +11,6 @@ require 'bosh/director/models/director_attribute'
 require 'bosh/director/models/instance'
 require 'bosh/director/models/ip_address'
 require 'bosh/director/models/local_dns_blob'
-require 'bosh/director/models/local_dns_record'
 require 'bosh/director/models/log_bundle'
 require 'bosh/director/models/orphan_disk'
 require 'bosh/director/models/orphan_snapshot'

bosh-director/lib/bosh/director/models/local_dns_record.rb
@@ -1,4 +1,0 @@
-module Bosh::Director::Models
-  class LocalDnsRecord < Sequel::Model(Bosh::Director::Config.db)
-  end
-end

commit 2d056b25acb6aa3fd2c461d834ebd0cbb6bfcac4

Fix dns manager when PowerDNS is disabled
broke in 85bb2ecfce08bdc022be31ed8dbfcbfd02568164

[#123993701](https://www.pivotaltracker.com/story/show/123993701)

Signed-off-by: Danny Berger <dberger@pivotal.io>

bosh-director/lib/bosh/director/dns/dns_manager.rb
@@ -51,8 +51,10 @@ module Bosh::Director
       new_dns_records = []
       dns_names_to_ip.each do |record_name, ip_address|
         new_dns_records << record_name
-        @logger.info("Updating DNS for: #{record_name} to #{ip_address}")
-        @dns_provider.create_or_update_dns_records(record_name, ip_address)
+        if dns_enabled?
+          @logger.info("Updating DNS for: #{record_name} to #{ip_address}")
+          @dns_provider.create_or_update_dns_records(record_name, ip_address)
+        end
       end
       dns_records = (current_dns_records + new_dns_records).uniq
       @local_dns_repo.create_or_update(instance_model, dns_records)

bosh-director/spec/unit/dns/dns_manager_spec.rb
@@ -318,7 +318,7 @@ module Bosh::Director
         end
       end
 
-      context 'when LocalDNS is enabled' do
+      context 'when blobstore DNS publisher is enabled' do
         let(:blobstore) { Bosh::Blobstore::NullBlobstoreClient.new }
         let(:dns_publisher) { BlobstoreDnsPublisher.new(blobstore, 'fake-domain-name') }
 
@@ -329,7 +329,7 @@ module Bosh::Director
         end
       end
 
-      context 'when LocalDNS is disabled' do
+      context 'when blobstore DNS publisher is disabled' do
         let(:dns_publisher) { nil }
 
         describe '#publisher_enabled?' do
@@ -380,7 +380,22 @@ module Bosh::Director
         end
       end
 
-      context 'when LocalDNS is enabled' do
+      describe '#update_dns_record_for_instance' do
+        before do
+          dns_manager.update_dns_record_for_instance(instance_model, {'fake-dns-name-1' => '1.2.3.4','fake-dns-name-2' => '5.6.7.8'})
+        end
+
+        context 'when IPs/hosts change' do
+          it 'updates dns records for instance in local repo' do
+            expect(local_dns_repo.find(instance_model)).to eq(['fake-dns-name-1','fake-dns-name-2'])
+            dns_manager.update_dns_record_for_instance(instance_model, {'fake-dns-name-1' => '11.22.33.44', 'new-fake-dns-name' => '99.88.77.66'})
+            expect(local_dns_repo.find(instance_model)).to eq(['fake-dns-name-1','fake-dns-name-2', 'new-fake-dns-name'])
+            expect(Models::Dns::Record.all.count).to eq(0)
+          end
+        end
+      end
+
+      context 'when blobstore DNS publisher is enabled' do
         let(:blobstore) { Bosh::Blobstore::NullBlobstoreClient.new }
         let(:dns_publisher) { BlobstoreDnsPublisher.new(blobstore, 'fake-domain-name') }
 
@@ -391,7 +406,7 @@ module Bosh::Director
         end
       end
 
-      context 'when LocalDNS is disabled' do
+      context 'when blobstore DNS publisher is disabled' do
         let(:dns_publisher) { nil }
 
         describe '#publisher_enabled?' do