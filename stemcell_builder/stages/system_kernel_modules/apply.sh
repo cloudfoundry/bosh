e8bd825f995748d0ec38fbb11df1a899d124759a
dmitriy kalinin <dkalinin@pivotal.io>
2016-07-05 10:11:16 -0700

    re-enable udf for dvd reading done by azure agent

--- stemcell_builder/stages/system_kernel_modules/apply.sh
+++ stemcell_builder/stages/system_kernel_modules/apply.sh
@@ -17,6 +17,5 @@ install jffs2 /bin/true
 install hfs /bin/true
 install hfsplus /bin/true
 install squashfs /bin/true
-install udf /bin/true
 install rds /bin/true
 options ipv6 disable=1' >> $chroot/etc/modprobe.d/blacklist.conf
