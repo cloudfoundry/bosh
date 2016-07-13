2e6b9d1782c8faef71e12d52e80e1a9d6e899992
Saman Alvi <salvi@pivotal.io>
2016-07-13 10:56:31 -0400

    Links properties works with config server values
    Links properties can be fetched from the config server
    This commit does not include removing the resolved values
    from the DB. They still exist in the spec_json row of the
    instances table.
    
    [#119700369]
    https://www.pivotaltracker.com/story/show/119700369
    
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- spec/integration/config_server/config_server_spec.rb
+++ spec/integration/config_server/config_server_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe 'using director with config server', type: :integration do
+  def upload_links_release
+    FileUtils.cp_r(LINKS_RELEASE_TEMPLATE, ClientSandbox.links_release_dir, :preserve => true)
+    bosh_runner.run_in_dir('create release --force', ClientSandbox.links_release_dir)
+    bosh_runner.run_in_dir('upload release', ClientSandbox.links_release_dir)
+  end
 
   let (:manifest_hash) { Bosh::Spec::Deployments.simple_manifest }
   let (:cloud_config)  { Bosh::Spec::Deployments.simple_cloud_config }
-  let(:config_server_helper) { Bosh::Spec::ConfigServerHelper.new(current_sandbox.port_provider.get_port(:config_server_port)) }
+  let (:config_server_helper) { Bosh::Spec::ConfigServerHelper.new(current_sandbox.port_provider.get_port(:config_server_port)) }
 
-  context 'when config server cretificates are not trusted' do
+  context 'when config server certificates are not trusted' do
     with_reset_sandbox_before_each(config_server_enabled: true, with_config_server_trusted_certs: false)
 
     before do
@@ -21,7 +26,6 @@ describe 'using director with config server', type: :integration do
       expect(exit_code).to_not eq(0)
       expect(output).to include('Error 100: SSL certificate verification failed')
     end
-
   end
 
   context 'when config server certificates are trusted' do
@@ -84,6 +88,108 @@ describe 'using director with config server', type: :integration do
         errand_result = bosh_runner.run('run errand fake-errand-name --keep-alive')
 
         expect(errand_result).to include('test value')
+      end
+    end
+  end
+
+  context 'when links exist' do
+    with_reset_sandbox_before_each(config_server_enabled: true)
+
+    let(:cloud_config) do
+      cloud_config_hash = Bosh::Spec::Deployments.simple_cloud_config
+      cloud_config_hash['azs'] = [{ 'name' => 'z1' }]
+      cloud_config_hash['networks'].first['subnets'].first['static'] = ['192.168.1.10', '192.168.1.11', '192.168.1.12', '192.168.1.13']
+      cloud_config_hash['networks'].first['subnets'].first['az'] = 'z1'
+      cloud_config_hash['compilation']['az'] = 'z1'
+      cloud_config_hash['networks'] << {
+        'name' => 'dynamic-network',
+        'type' => 'dynamic',
+        'subnets' => [{'az' => 'z1'}]
+      }
+
+      cloud_config_hash
+    end
+
+    let(:my_job) do
+      job_spec = Bosh::Spec::Deployments.simple_job(
+        name: 'my_job',
+        templates: [
+          {'name' => 'http_server_with_provides'},
+          {'name' => 'http_proxy_with_requires'},
+        ],
+        instances: 1
+      )
+      job_spec['azs'] = ['z1']
+      job_spec['properties'] = {'listen_port' => 9035, 'name_space' => {'fibonacci' => '((fibonacci_placeholder))'}}
+      job_spec
+    end
+
+    let(:manifest) do
+      manifest = Bosh::Spec::NetworkingManifest.deployment_manifest
+      manifest['jobs'] = [my_job]
+      manifest['properties'] = {'listen_port' => 9999}
+      manifest
+    end
+
+    before do
+      target_and_login
+      upload_links_release
+      upload_stemcell
+
+      upload_cloud_config(cloud_config_hash: cloud_config)
+    end
+
+    it 'replaces the placeholder values of properties consumed through links' do
+      config_server_helper.put_value('fibonacci_placeholder', 'fibonacci_value')
+      deploy_simple_manifest(manifest_hash: manifest)
+
+      link_vm = director.vm('my_job', '0')
+      template = YAML.load(link_vm.read_job_template('http_proxy_with_requires', 'config/config.yml'))
+      expect(template['links']['properties']['fibonacci']).to eq('fibonacci_value')
+    end
+
+    context 'when manual links are involved' do
+      let (:job_with_manual_consumes_link) do
+        job_spec = Bosh::Spec::Deployments.simple_job(
+          name: 'property_job',
+          templates: [{
+            'name' => 'consumer',
+            'consumes' => {
+              'provider' => {
+                'properties' => {'a' => '((a_placeholder))', 'b' => '((b_placeholder))', 'c' => '((c_placeholder))'},
+                'instances' => [{'name' => 'external_db', 'address' => '192.168.15.4'}],
+                'networks' => {'network_1' => 2, 'network_2' => 3}
+              }
+            }
+          }],
+          instances: 1,
+          static_ips: ['192.168.1.10'],
+          properties: {}
+        )
+        job_spec['azs'] = ['z1']
+        job_spec['networks'] << {
+          'name' => 'dynamic-network',
+          'default' => ['dns', 'gateway']
+        }
+        job_spec
+      end
+
+      it 'resolves the properties defined inside the links section of the deployment manifest' do
+        config_server_helper.put_value('a_placeholder', 'a_value')
+        config_server_helper.put_value('b_placeholder', 'b_value')
+        config_server_helper.put_value('c_placeholder', 'c_value')
+
+        manifest['jobs'] = [job_with_manual_consumes_link]
+
+        deploy_simple_manifest(manifest_hash: manifest)
+
+        link_vm = director.vm('property_job', '0')
+
+        template = YAML.load(link_vm.read_job_template('consumer', 'config.yml'))
+
+        expect(template['a']).to eq('a_value')
+        expect(template['b']).to eq('b_value')
+        expect(template['c']).to eq('c_value')
       end
     end
   end
