b2900d4d60c0678588c19d9199d361dab759edf4
Mikhail Vyshegorodtsev <mvyshegorodtse@vmware.com>
2016-07-19 14:10:58 -0700

    change bosh cli to stop resolving dns entries up front
    [#126523527](https://www.pivotaltracker.com/story/show/126523527)

--- bosh_cli/spec/unit/director_task_spec.rb
+++ bosh_cli/spec/unit/director_task_spec.rb
@@ -1,35 +1,34 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Bosh::Cli::DirectorTask do
   before do
-    expect(URI).to receive(:parse).with("http://target.example.com").and_call_original
-    expect(Resolv).to receive(:getaddresses).with("target.example.com").and_return(["127.0.0.1"])
+    expect(URI).to receive(:parse).with('http://target.example.com').and_call_original
     credentials = Bosh::Cli::Client::BasicCredentials.new('user', 'pass')
-    @director = Bosh::Cli::Client::Director.new("http://target.example.com", credentials)
+    @director = Bosh::Cli::Client::Director.new('http://target.example.com', credentials)
   end
 
-  it "tracks partial output responses from director" do
+  it 'tracks partial output responses from director' do
     @task = Bosh::Cli::DirectorTask.new(@director, 10)
 
     allow(@director).to receive(:get).
-      with("/tasks/10/output", nil, nil, "Range" => "bytes=0-").
-      and_return([206, "test\nout", { :content_range => "bytes 0-7/100" }])
+      with('/tasks/10/output', nil, nil, 'Range' => 'bytes=0-').
+      and_return([206, "test\nout", { :content_range => 'bytes 0-7/100'}])
 
     allow(@director).to receive(:get).
-      with("/tasks/10/output", nil, nil, "Range" => "bytes=8-").
-      and_return([206, "put", { :content_range => "bytes 8-10/100" }])
+      with('/tasks/10/output', nil, nil, 'Range' => 'bytes=8-').
+      and_return([206, 'put', { :content_range => 'bytes 8-10/100'}])
 
     allow(@director).to receive(:get).
-      with("/tasks/10/output", nil, nil, "Range" => "bytes=11-").
-      and_return([206, " success\n", { :content_range => "bytes 11-19/100" }])
+      with('/tasks/10/output', nil, nil, 'Range' => 'bytes=11-').
+      and_return([206, " success\n", { :content_range => 'bytes 11-19/100'}])
 
     allow(@director).to receive(:get).
-      with("/tasks/10/output", nil, nil, "Range" => "bytes=20-").
-      and_return([416, "Byte range unsatisfiable", { :content_range => "bytes */100" }])
+      with('/tasks/10/output', nil, nil, 'Range' => 'bytes=20-').
+      and_return([416, 'Byte range unsatisfiable', { :content_range => 'bytes */100'}])
 
     allow(@director).to receive(:get).
-      with("/tasks/10/output", nil, nil, "Range" => "bytes=20-").
-      and_return([206, "done", {}])
+      with('/tasks/10/output', nil, nil, 'Range' => 'bytes=20-').
+      and_return([206, 'done', {}])
 
     expect(@task.output).to eq("test\n")
     expect(@task.output).to eq(nil)     # No newline yet
@@ -37,12 +36,12 @@ describe Bosh::Cli::DirectorTask do
     expect(@task.output).to eq("done\n") # Flushed
   end
 
-  it "supports explicit output flush" do
+  it 'supports explicit output flush' do
     @task = Bosh::Cli::DirectorTask.new(@director, 10)
 
     allow(@director).to receive(:get).
-      with("/tasks/10/output", nil, nil, "Range" => "bytes=0-").
-      and_return([206, "test\nout", { :content_range => "bytes 0-7/100" }])
+      with('/tasks/10/output', nil, nil, 'Range' => 'bytes=0-').
+      and_return([206, "test\nout", { :content_range => 'bytes 0-7/100'}])
 
     expect(@task.output).to eq("test\n")
     expect(@task.flush_output).to eq("out\n")
