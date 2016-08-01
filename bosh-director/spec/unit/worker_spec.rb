34dc955e72b025ac259d0aa08f8f87df961295da
Shatarupa Nandi <snandi@pivotal.io>
2016-08-01 14:23:10 -0700

    Revert "Setting Delayed::Worker.max_attempts to 5."
    This reverts commit f7d39424347c89dab0d6408b76cd84399918100a.
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- bosh-director/spec/unit/worker_spec.rb
+++ bosh-director/spec/unit/worker_spec.rb
@@ -19,44 +19,33 @@ describe 'worker' do
   end
   let(:config) { Bosh::Director::Config.load_hash(config_hash) }
 
-  describe '#prep' do
-    describe 'Delayed::Worker configuration' do
-      it 'sets the max_attempts to 5' do
-        worker.prep
+  describe 'when workers is sent USR1' do
+    let(:queues) { worker.queues }
+    before { allow(Socket).to receive(:gethostname).and_return(nil) }
+    it 'should not pick up new tasks' do
+      ENV['QUEUE']='normal'
+      worker.prep
+      expect(worker.queues).to eq(['normal'])
+      Process.kill('USR1', Process.pid)
+      sleep 0.1 # Ruby 1.9 fails without the sleep due to a race condition between rspec assertion and actually killing the process
+      expect(worker.queues).to eq(['non_existent_queue'])
+    end
+  end
 
-        expect(Delayed::Worker.max_attempts).to eq(5)
-      end
+  describe 'when deployment has director_pool' do
+    let(:queues) { worker.queues }
+    before { allow(Socket).to receive(:gethostname).and_return('local.hostname') }
 
-      context 'when workers is sent USR1' do
-        let(:queues) { worker.queues }
-        before { allow(Socket).to receive(:gethostname).and_return(nil) }
+    it 'should have new queue' do
+      ENV['QUEUE']='normal'
+      worker.prep
+      expect(worker.queues).to eq(['normal', 'local.hostname'])
+    end
 
-        it 'should not pick up new tasks' do
-          ENV['QUEUE']='normal'
-          worker.prep
-          expect(worker.queues).to eq(['normal'])
-          Process.kill('USR1', Process.pid)
-          sleep 0.1 # Ruby 1.9 fails without the sleep due to a race condition between rspec assertion and actually killing the process
-          expect(worker.queues).to eq(['non_existent_queue'])
-        end
-      end
-
-      context 'when deployment has director_pool' do
-        let(:queues) { worker.queues }
-        before { allow(Socket).to receive(:gethostname).and_return('local.hostname') }
-
-        it 'should have new queue' do
-          ENV['QUEUE']='normal'
-          worker.prep
-          expect(worker.queues).to eq(['normal', 'local.hostname'])
-        end
-
-        it 'should not have new queue in case of urgent' do
-          ENV['QUEUE']='urgent'
-          worker.prep
-          expect(worker.queues).to eq(['urgent'])
-        end
-      end
+    it 'should not have new queue in case of urgent' do
+      ENV['QUEUE']='urgent'
+      worker.prep
+      expect(worker.queues).to eq(['urgent'])
     end
   end
 end
