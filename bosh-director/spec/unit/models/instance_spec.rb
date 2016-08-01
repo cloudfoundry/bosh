0af79a6e1124407066f2908eedf7aadfe06550c8
Beyhan Veli <beyhan.veli@sap.com>
2016-08-01 14:32:08 +0200

    `bosh cck` will find all instances with expected VMs
    Problem scanner doesn't filter anymore for VMs with vm_cid==nil.
    It calculates whether an instance should have a VM based on
    lifecycle and instance state:
    - If an instance has lifecycle type
      'service' and is in state 'started' or 'stopped' but has no VM a
      :missing_vm problem is reported.
    - errands are excluded
    - detached instances are excluded
    - ignored instances are excluded
    
    This does preparation work to fix issue
    https://github.com/cloudfoundry/bosh/issues/1223
    where vm_cid incorrectly was set to nil.
    
    [#114101259](https://www.pivotaltracker.com/story/show/114101259)
    
    Signed-off-by: Cornelius Schumacher <cschum@suse.com>

--- bosh-director/spec/unit/models/instance_spec.rb
+++ bosh-director/spec/unit/models/instance_spec.rb
@@ -324,5 +324,118 @@ module Bosh::Director::Models
         end
       end
     end
+
+
+    context 'with deployment_plan' do
+      subject { described_class.make(deployment: deployment, job: 'job-1') }
+
+      let(:instance_groups) {
+        [{
+             'name' => 'job-1',
+             'lifecycle' => lifecycle,
+             'instances' => 1,
+             'jobs' => [],
+             'vm_type' => 'm1.small',
+             'stemcell' => 'stemcell',
+             'networks' => [{'name' => 'network'}]
+         }]
+      }
+
+      let(:manifest) {
+        {
+            'name' => 'something',
+            'releases' => [],'instance_groups' => instance_groups,
+            'update' => {
+                'canaries' => 1,
+                'max_in_flight' => 1,
+                'canary_watch_time' => 20,
+                'update_watch_time' => 20
+            },
+            'stemcells' => [{
+                                'name' => 'stemcell',
+                                'alias' => 'stemcell'
+                            }]
+        }
+      }
+
+      let(:cloud_config_hash) {
+        {
+            'compilation' => {
+                'network' => 'network',
+                'workers' => 1
+            },
+            'networks' => [{
+                               'name' => 'network',
+                               'subnets' => []
+
+                           }],
+            'vm_types' => [{
+                               'name' => 'm1.small'
+                           }]
+
+        }
+      }
+      let(:manifest_text) { manifest.to_yaml }
+      let(:cloud_config) { CloudConfig.make(manifest: cloud_config_hash) }
+      let(:deployment) { Deployment.make(name: 'deployment', manifest: manifest_text, cloud_config: cloud_config) }
+
+      describe '#lifecycle' do
+        context "when lifecycle is 'service'" do
+          let(:lifecycle) { 'service' }
+          it "returns 'service'" do
+            expect(subject.lifecycle).to eq('service')
+          end
+        end
+
+        context "when lifecycle is 'errand'" do
+          let(:lifecycle) { 'errand' }
+          it "returns 'errand'" do
+            expect(subject.lifecycle).to eq('errand')
+          end
+        end
+
+        context 'when no manifest is stored in the database' do
+          let(:manifest_text) { nil }
+          it "returns 'nil'" do
+            expect(subject.lifecycle).to be_nil
+          end
+        end
+      end
+
+      describe '#expects_vm?' do
+
+        context "when lifecycle is 'errand'" do
+          let(:lifecycle) { 'errand' }
+
+          it "doesn't expect vm" do
+            expect(subject.expects_vm?).to eq(false)
+          end
+        end
+
+        context "when lifecycle is 'service'" do
+          let(:lifecycle) { 'service' }
+
+          ['started', 'stopped'].each do |state|
+
+            context "when state is '#{state}'" do
+              subject { described_class.make(deployment: deployment, job: 'job-1', state: "#{state}") }
+
+              it 'expects a vm' do
+                expect(subject.expects_vm?).to eq(true)
+              end
+            end
+          end
+
+          context "when state is 'detached'" do
+            subject { described_class.make(deployment: deployment, job: 'job-1', state: 'detached') }
+
+            it "doesn't expect vm" do
+              expect(subject.expects_vm?).to eq(false)
+            end
+          end
+
+        end
+      end
+    end
   end
 end
