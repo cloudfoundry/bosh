ac4b7586cbe72397d56e7b03c02eb66a75414c89
AndrewS / SukhilS <asu@pivotal.io>
2016-08-10 13:55:07 -0400

    Revert "Switch vivid to xenial"
    This reverts commit 1a8f6b6c8d68ca3d27f6fe5df4fed934dc1363a1.

--- bosh-stemcell/spec/os_image/ubuntu_trusty_spec.rb
+++ bosh-stemcell/spec/os_image/ubuntu_trusty_spec.rb
@@ -16,7 +16,7 @@ describe 'Ubuntu 14.04 OS image', os_image: true do
   end
 
   context 'installed by system_kernel' do
-    describe package('linux-generic-lts-xenial') do
+    describe package('linux-generic-lts-vivid') do
       it { should be_installed }
     end
   end
