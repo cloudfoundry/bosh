cc49a8ee67434b3ac89deae81724403283c51bfa
BOSH Jenkins <bosh@jenkins.sf.pivotal.io>
2016-08-30 13:52:10 -0700

    Adding final release for build 3262.9
    Cherry-picked from c53e6bc291a4f482bf534a59173832c3cbbc6a8a
    
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- agent_client/lib/agent_client/version.rb
+++ agent_client/lib/agent_client/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Agent
     class Client
-      VERSION = '1.3262.0'
+      VERSION = '1.3262.9.0'
     end
   end
 end
--- bosh-director-core/lib/bosh/director/core/version.rb
+++ bosh-director-core/lib/bosh/director/core/version.rb
@@ -1,7 +1,7 @@
 module Bosh
   module Director
     module Core
-      VERSION = '1.3262.0'
+      VERSION = '1.3262.9.0'
     end
   end
 end
