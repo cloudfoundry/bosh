134658e7254a4a61ad394e60dd61c61ebbff945b
Zachary Auerbach <zauerbach@pivotal.io>
2016-09-02 12:30:37 -0700

    bosh create release needs to run from release/

--- ci/pipelines/alpha-releases/tasks/make.sh
+++ ci/pipelines/alpha-releases/tasks/make.sh
@@ -8,6 +8,8 @@ chruby 2.1.2
 cd bosh-src
 bundle install
 bundle exec rake release:create_dev_release
+
+cd release
 bundle exec bosh create release --force  --with-tarball --timestamp-version
 
-mv dev_releases/bosh/*.tgz ../release/
+mv dev_releases/bosh/*.tgz ../../release/
