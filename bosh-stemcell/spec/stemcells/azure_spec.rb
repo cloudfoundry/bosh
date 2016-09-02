f8ce735628195417492fa8cdd7c2a110d3cec3ed
Vikram Yadav <vyadav@pivotal.io>
2016-09-02 11:30:03 -0400

    Re-enable UDF for Azure infrastructure.
    - Only check for CIS-2.14 on non-azure infrastructures.
    - Updated azure network stage to use the correct hostname.
    
    [#129004005] (https://www.pivotaltracker.com/story/show/129004005)
    
    Signed-off-by: Andrew Su <asu@pivotal.io>

--- bosh-stemcell/spec/stemcells/azure_spec.rb
+++ bosh-stemcell/spec/stemcells/azure_spec.rb
@@ -10,8 +10,13 @@ describe 'Azure Stemcell', stemcell_image: true do
   context 'installed by bosh_disable_password_authentication' do
     describe 'disallows password authentication' do
       subject { file('/etc/ssh/sshd_config') }
-
       it { should contain /^PasswordAuthentication no$/ }
+    end
+  end
+
+  context 'udf module should be enabled' do
+    describe file('/etc/modprobe.d/blacklist.conf') do
+      it { should_not contain 'install udf /bin/true' }
     end
   end
 
--- bosh-stemcell/spec/stemcells/photonos_TP2_spec.rb
+++ bosh-stemcell/spec/stemcells/photonos_TP2_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'Photonos 1 stemcell', stemcell_image: true do
 
   it_behaves_like 'All Stemcells'
+  it_behaves_like 'udf module is disabled'
   
   context 'installed by system_parameters' do
     describe file('/etc/photon-release') do
