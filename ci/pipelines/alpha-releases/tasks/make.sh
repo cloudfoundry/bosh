9331a2dfd1b5c9878dbbe2c0e2248f14dca706f8
Zachary Auerbach <zauerbach@pivotal.io>
2016-09-02 12:12:01 -0700

    add alpha-releases pipelines
    Signed-off-by: dmitriy kalinin <dkalinin@pivotal.io>

---
+++ ci/pipelines/alpha-releases/tasks/make.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eux
+
+source /etc/profile.d/chruby.sh
+chruby 2.1.2
+
+cd bosh-src
+bundle install
+bundle exec rake release:create_dev_release
+bundle exec bosh create release --force  --with-tarball --timestamp-version
+
+mv dev_releases/bosh/*.tgz ../release/
