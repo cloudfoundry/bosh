cf8603224cb08cd7ba8d7de868f78d68e420f67b
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2016-09-13 10:51:11 -0700

    Move testing of tag functionality to cpi_spec
    [#129890801](https://www.pivotaltracker.com/story/show/129890801)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>
    Signed-off-by: Michael Maximilien <maxim@us.ibm.com>

--- spec/integration/cpi_spec.rb
+++ spec/integration/cpi_spec.rb
@@ -10,6 +10,13 @@ describe 'CPI calls', type: :integration do
   describe 'deploy' do
     it 'sends correct CPI requests' do
       manifest_hash = Bosh::Spec::NetworkingManifest.deployment_manifest(instances: 1)
+      manifest_hash.merge!({
+        'tags' => [{
+          'key' => 'tag1',
+          'value' => 'value1',
+        }],
+      })
+
       deploy_from_scratch(manifest_hash: manifest_hash)
 
       invocations = current_sandbox.cpi.invocations
@@ -59,7 +66,7 @@ describe 'CPI calls', type: :integration do
           'job' => /compilation-.*/,
           'index' => '0',
           'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
-          'name' => /compilation-.*\/[0-9a-f]{8}-[0-9a-f-]{27}/
+          'name' => /compilation-.*\/[0-9a-f]{8}-[0-9a-f-]{27}/,
         }
       })
       expect_name(invocations[2])
@@ -77,7 +84,7 @@ describe 'CPI calls', type: :integration do
           'index' => '0',
           'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
           'name' => /compilation-.*\/[0-9a-f]{8}-[0-9a-f-]{27}/
-      }
+        }
       })
       expect_name(invocations[3])
       expect(invocations[4].method_name).to eq('delete_vm')
@@ -99,7 +106,7 @@ describe 'CPI calls', type: :integration do
           }
         },
         'disk_cids' => [],
-        'env' => { 'bosh' => {
+        'env' => {'bosh' => {
           'group' => String,
           'groups' => [
             'testdirector',
@@ -109,7 +116,7 @@ describe 'CPI calls', type: :integration do
             /simple-compilation-.*/,
             /testdirector-simple-compilation-.*/
           ]
-        } }
+        }}
       })
 
       expect(invocations[6].method_name).to eq('set_vm_metadata')
@@ -151,19 +158,19 @@ describe 'CPI calls', type: :integration do
       expect(invocations[9].inputs).to match({
         'agent_id' => String,
         'stemcell_id' => String,
-        'cloud_properties' =>{},
+        'cloud_properties' => {},
         'networks' => {
           'a' => {
             'ip' => '192.168.1.2',
             'netmask' => '255.255.255.0',
             'default' => ['dns', 'gateway'],
-            'cloud_properties' =>{},
-            'dns' =>['192.168.1.1', '192.168.1.2'],
+            'cloud_properties' => {},
+            'dns' => ['192.168.1.1', '192.168.1.2'],
             'gateway' => '192.168.1.1',
           }
         },
         'disk_cids' => [],
-        'env' => {'bosh' =>{'password' => 'foobar', 'group' => 'testdirector-simple-foobar',
+        'env' => {'bosh' => {'password' => 'foobar', 'group' => 'testdirector-simple-foobar',
           'groups' => [
             'testdirector',
             'simple',
@@ -185,7 +192,8 @@ describe 'CPI calls', type: :integration do
           'job' => 'foobar',
           'index' => '0',
           'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
-          'name' => /foobar\/[0-9a-f]{8}-[0-9a-f-]{27}/
+          'name' => /foobar\/[0-9a-f]{8}-[0-9a-f-]{27}/,
+          'tag1' => 'value1',
         }
       })
       expect_name(invocations[10])
@@ -202,9 +210,16 @@ describe 'CPI calls', type: :integration do
             static_ips: ['192.168.1.10'],
             instances: 1,
             templates: ['name' => 'foobar_without_packages'],
-            persistent_disk_pool: Bosh::Spec::Deployments.disk_pool['name']
+            persistent_disk_pool: Bosh::Spec::Deployments.disk_pool['name'],
           )
         ]
+        manifest_hash.merge!({
+          'tags' => [{
+            'key' => 'tag1',
+            'value' => 'value1',
+          }],
+        })
+
         cloud_config_hash = Bosh::Spec::Deployments.simple_cloud_config
         cloud_config_hash['networks'].first['subnets'].first['static'] = ['192.168.1.10', '192.168.1.11']
         cloud_config_hash['disk_pools'] = [Bosh::Spec::Deployments.disk_pool]
@@ -236,7 +251,7 @@ describe 'CPI calls', type: :integration do
             }
           },
           'disk_cids' => [],
-          'env' => {'bosh' =>{'password' => 'foobar', 'group' => 'testdirector-simple-first-job', 'groups' =>['testdirector', 'simple', 'first-job', 'testdirector-simple', 'simple-first-job', 'testdirector-simple-first-job']}}
+          'env' => {'bosh' => {'password' => 'foobar', 'group' => 'testdirector-simple-first-job', 'groups' => ['testdirector', 'simple', 'first-job', 'testdirector-simple', 'simple-first-job', 'testdirector-simple-first-job']}}
         })
 
         expect(first_deploy_invocations[2].method_name).to eq('set_vm_metadata')
@@ -249,7 +264,8 @@ describe 'CPI calls', type: :integration do
             'job' => 'first-job',
             'index' => '0',
             'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
-            'name' => /first-job\/[0-9a-f]{8}-[0-9a-f-]{27}/
+            'name' => /first-job\/[0-9a-f]{8}-[0-9a-f-]{27}/,
+            'tag1' => 'value1',
           }
         })
         expect_name(first_deploy_invocations[2])
@@ -269,6 +285,7 @@ describe 'CPI calls', type: :integration do
         })
         disk_cid = first_deploy_invocations[4].inputs['disk_id']
 
+        # force a recreate by changing IP address
         manifest_hash['jobs'].first['networks'].first['static_ips'] = ['192.168.1.11']
         deploy_simple_manifest(manifest_hash: manifest_hash)
 
@@ -321,7 +338,8 @@ describe 'CPI calls', type: :integration do
             'job' => 'first-job',
             'index' => '0',
             'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
-            'name' => /first-job\/[0-9a-f]{8}-[0-9a-f-]{27}/
+            'name' => /first-job\/[0-9a-f]{8}-[0-9a-f-]{27}/,
+            'tag1' => 'value1',
           }
         })
 
@@ -336,5 +354,197 @@ describe 'CPI calls', type: :integration do
         })
       end
     end
+
+    context 'when recreating via cck' do
+      let(:runner) { bosh_runner_in_work_dir(ClientSandbox.test_release_dir) }
+
+      it 'recreates VM with correct CPI requests' do
+        manifest_hash = Bosh::Spec::NetworkingManifest.deployment_manifest
+        manifest_hash['jobs'] = [
+          Bosh::Spec::Deployments.simple_job(
+            name: 'first-job',
+            static_ips: ['192.168.1.10'],
+            instances: 1,
+            templates: ['name' => 'foobar_without_packages'],
+            persistent_disk_pool: Bosh::Spec::Deployments.disk_pool['name'],
+          )
+        ]
+        manifest_hash.merge!({
+          'tags' => [{
+            'key' => 'tag1',
+            'value' => 'value1',
+          }],
+        })
+
+        cloud_config_hash = Bosh::Spec::Deployments.simple_cloud_config
+        cloud_config_hash['networks'].first['subnets'].first['static'] = ['192.168.1.10', '192.168.1.11']
+        cloud_config_hash['disk_pools'] = [Bosh::Spec::Deployments.disk_pool]
+        deploy_from_scratch(manifest_hash: manifest_hash, cloud_config_hash: cloud_config_hash)
+        first_deploy_invocations = current_sandbox.cpi.invocations
+
+        expect(first_deploy_invocations[0].method_name).to eq('create_stemcell')
+        expect(first_deploy_invocations[0].inputs).to match({
+          'image_path' => String,
+          'cloud_properties' => {
+            'property1' => 'test',
+            'property2' => 'test'
+          }
+        })
+
+        expect(first_deploy_invocations[1].method_name).to eq('create_vm')
+        expect(first_deploy_invocations[1].inputs).to match({
+          'agent_id' => String,
+          'stemcell_id' => String,
+          'cloud_properties' => {},
+          'networks' => {
+            'a' => {
+              'ip' => '192.168.1.10',
+              'netmask' => '255.255.255.0',
+              'cloud_properties' => {},
+              'default' => ['dns', 'gateway'],
+              'dns' => ['192.168.1.1', '192.168.1.2'],
+              'gateway' => '192.168.1.1',
+            }
+          },
+          'disk_cids' => [],
+          'env' => {'bosh' => {'password' => 'foobar', 'group' => 'testdirector-simple-first-job', 'groups' => ['testdirector', 'simple', 'first-job', 'testdirector-simple', 'simple-first-job', 'testdirector-simple-first-job']}}
+        })
+
+        expect(first_deploy_invocations[2].method_name).to eq('set_vm_metadata')
+        expect(first_deploy_invocations[2].inputs).to match({
+          'vm_cid' => String,
+          'metadata' => {
+            'director' => 'Test Director',
+            'created_at' => kind_of(String),
+            'deployment' => 'simple',
+            'job' => 'first-job',
+            'index' => '0',
+            'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
+            'name' => /first-job\/[0-9a-f]{8}-[0-9a-f-]{27}/,
+            'tag1' => 'value1',
+          }
+        })
+        expect_name(first_deploy_invocations[2])
+        vm_cid = first_deploy_invocations[2].inputs['vm_cid']
+
+        expect(first_deploy_invocations[3].method_name).to eq('create_disk')
+        expect(first_deploy_invocations[3].inputs).to match({
+          'size' => 123,
+          'cloud_properties' => {},
+          'vm_locality' => vm_cid
+        })
+
+        expect(first_deploy_invocations[4].method_name).to eq('attach_disk')
+        expect(first_deploy_invocations[4].inputs).to match({
+          'vm_cid' => vm_cid,
+          'disk_id' => String
+        })
+        disk_cid = first_deploy_invocations[4].inputs['disk_id']
+
+        current_sandbox.cpi.vm_cids.each do |vm_cid|
+          current_sandbox.cpi.delete_vm(vm_cid)
+        end
+
+        pre_cck_invocations = current_sandbox.cpi.invocations
+
+        cloudcheck_response = bosh_run_cck_with_auto
+        expect(cloudcheck_response).to match(regexp('missing.'))
+        expect(cloudcheck_response).to match(regexp('Applying resolutions...'))
+        expect(cloudcheck_response).to match(regexp('Cloudcheck is finished'))
+        expect(cloudcheck_response).to_not match(regexp('No problems found'))
+        expect(cloudcheck_response).to_not match(regexp('1. Skip for now
+  2. Reboot VM
+  3. Recreate VM using last known apply spec
+  4. Delete VM
+  5. Delete VM reference (DANGEROUS!)'))
+
+        expect(runner.run('cloudcheck --report')).to match(regexp('No problems found'))
+
+        second_deploy_invocations = current_sandbox.cpi.invocations.drop(pre_cck_invocations.size)
+
+        expect(second_deploy_invocations[0].method_name).to eq('has_vm?')
+        expect(second_deploy_invocations[0].inputs).to match({
+          'vm_cid' => vm_cid
+        })
+
+        expect(second_deploy_invocations[1].method_name).to eq('has_disk?')
+        expect(second_deploy_invocations[1].inputs).to match({
+          'disk_id' => disk_cid
+        })
+
+        expect(second_deploy_invocations[2].method_name).to eq('delete_vm')
+        expect(second_deploy_invocations[2].inputs).to match({
+          'vm_cid' => vm_cid
+        })
+
+        expect(second_deploy_invocations[3].method_name).to eq('create_vm')
+        expect(second_deploy_invocations[3].inputs).to match({
+          'agent_id' => String,
+          'stemcell_id' => String,
+          'cloud_properties' => {},
+          'networks' => {
+            'a' => {
+              'ip' => '192.168.1.10',
+              'netmask' => '255.255.255.0',
+              'cloud_properties' => {},
+              'default' => ['dns', 'gateway'],
+              'dns' => ['192.168.1.1', '192.168.1.2'],
+              'gateway' => '192.168.1.1',
+            }
+          },
+          'disk_cids' => [disk_cid],
+          'env' => {'bosh' => {'password' => 'foobar', 'group' => 'testdirector-simple-first-job', 'groups' => ['testdirector', 'simple', 'first-job', 'testdirector-simple', 'simple-first-job', 'testdirector-simple-first-job']}}
+        })
+
+        expect(second_deploy_invocations[4].method_name).to eq('set_vm_metadata')
+        expect(second_deploy_invocations[4].inputs).to match({
+          'vm_cid' => String,
+          'metadata' => {
+            'director' => 'Test Director',
+            'created_at' => kind_of(String),
+            'deployment' => 'simple',
+            'job' => 'first-job',
+            'index' => '0',
+            'id' => /[0-9a-f]{8}-[0-9a-f-]{27}/,
+            'name' => /first-job\/[0-9a-f]{8}-[0-9a-f-]{27}/,
+            'tag1' => 'value1',
+          }
+        })
+
+        expect_name(second_deploy_invocations[4])
+
+        new_vm_cid = second_deploy_invocations[4].inputs['vm_cid']
+
+        expect(second_deploy_invocations[5].method_name).to eq('attach_disk')
+        expect(second_deploy_invocations[5].inputs).to match({
+          'vm_cid' => new_vm_cid,
+          'disk_id' => disk_cid
+        })
+
+        expect(second_deploy_invocations[6].method_name).to eq('has_disk?')
+        expect(second_deploy_invocations[6].inputs).to match({
+          'disk_id' => disk_cid
+        })
+      end
+    end
+  end
+
+  private
+
+  def bosh_run_cck_with_resolution(num_errors, option=1)
+    resolution_selections = "#{option}\n"*num_errors + "yes"
+    output = `echo "#{resolution_selections}" | bosh -c #{ClientSandbox.bosh_config} cloudcheck`
+    if $?.exitstatus != 0
+      fail("Cloud check failed, output: #{output}")
+    end
+    output
+  end
+
+  def bosh_run_cck_with_auto
+    output = `bosh -c #{ClientSandbox.bosh_config} cloudcheck --auto`
+    if $?.exitstatus != 0
+      fail("Cloud check failed, output: #{output}")
+    end
+    output
   end
 end
