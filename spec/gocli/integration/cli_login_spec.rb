ce851d6a0b1a8021cfaea1b13aafda558fb5d7dc
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2016-09-16 13:39:07 -0700

    Remove tests for removed gocli functionality
    [#125440211](https://www.pivotaltracker.com/story/show/125440211)
    
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- spec/gocli/integration/cli_login_spec.rb
+++ spec/gocli/integration/cli_login_spec.rb
@@ -30,34 +30,4 @@ describe 'cli: login using the directors built-in user DB', type: :integration d
         .to raise_error(RuntimeError, /Invalid credentials/)
     end
   end
-
-  context 'when users are not specified in manifest' do
-    with_reset_sandbox_before_each(users_in_manifest: false)
-
-    it 'can log in as a default user, create another user and delete created user' do
-      pending('cli2: #130549577: backport delete-user command')
-      pending('cli2: #130549503: backport create-user command')
-
-      bosh_runner.run("env #{current_sandbox.director_url}")
-      bosh_runner.run('log-in', user: 'admin', password: 'admin')
-      expect(bosh_runner.run('create-user john john-pass')).to match(/User 'john' has been created/)
-
-      john_login_output = bosh_runner.run('log-in', user: 'john', password: 'john-pass')
-      expect(john_login_output).to match(/Logged in/)
-      expect(john_login_output).to match(/as user 'john'/)
-      expect(bosh_runner.run('create-user jane jane-pass')).to match(/user 'jane' has been created/)
-      bosh_runner.run('logout')
-
-      jane_login_output = bosh_runner.run('log-in', user: 'jane', password: 'jane-pass')
-      expect(jane_login_output).to match(/Logged in/)
-      expect(jane_login_output).to match(/as user 'jane'/)
-      expect(bosh_runner.run('delete-user john')).to match(/User 'john' has been deleted/)
-      bosh_runner.run('logout')
-
-      expect(bosh_runner.run('log-in', user: 'john', password: 'john-pass', failure_expected: true)).to match(/Invalid credentials/)
-      jane_login_output = bosh_runner.run('log-in', user: 'jane', password: 'jane-pass')
-      expect(jane_login_output).to match(/Logged in/)
-      expect(jane_login_output).to match(/as user 'jane'/)
-    end
-  end
 end
