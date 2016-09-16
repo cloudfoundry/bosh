321972712b7fb5f95a666523a12c7be599de25ec
Ming Xiao <mxiao@pivotal.io>
2016-09-16 13:36:17 -0700

    Un-pend external CPI tests for gocli
    [#125440211](https://www.pivotaltracker.com/story/show/125440211)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- spec/gocli/integration/external_cpi_spec.rb
+++ spec/gocli/integration/external_cpi_spec.rb
@@ -3,10 +3,6 @@ require_relative '../spec_helper'
 describe 'external CPIs', type: :integration do
   with_reset_sandbox_before_each(external_cpi_enabled: true)
 
-  before do
-    pending('cli2: #130127863: `bosh task` should show the last task to run, not just the latest running task')
-  end
-
   describe 'director configured to use external dummy CPI' do
     before { deploy_from_scratch }
 
