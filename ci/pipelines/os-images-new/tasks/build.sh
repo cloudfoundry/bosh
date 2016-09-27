fde63034f8a7381056716119bfc0aaecf77a3b98
Ming Xiao <mxiao@pivotal.io>
2016-09-27 09:38:05 -0700

    Switch to using versioned files with uniform file names
    [#129714329](https://www.pivotaltracker.com/story/show/129714329)
    
    Signed-off-by: Zachary Auerbach <zauerbach@pivotal.io>

--- ci/pipelines/os-images-new/tasks/build.sh
+++ ci/pipelines/os-images-new/tasks/build.sh
@@ -11,7 +11,7 @@ check_param OPERATING_SYSTEM_NAME
 check_param OPERATING_SYSTEM_VERSION
 
 OS_IMAGE_NAME=$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION
-OS_IMAGE=$TASK_DIR/os-image/$OS_IMAGE_NAME-$( cat $TASK_DIR/version/number ).tgz
+OS_IMAGE=$TASK_DIR/os-image/$OS_IMAGE_NAME.tgz
 
 sudo chown -R ubuntu .
 sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
