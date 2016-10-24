25b0089236315e6676f9ab7699943bc26a27672f
Tom Viehman <tviehman@pivotal.io>
2016-10-24 09:54:57 -0700

    Add CPI release as input to teardown job
    [#132602755](https://www.pivotaltracker.com/story/show/132602755)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- ci/pipelines/stemcell/tasks/teardown.sh
+++ ci/pipelines/stemcell/tasks/teardown.sh
@@ -15,17 +15,8 @@ if [ ! -e "${input_dir}/director-state.json" ]; then
   exit 0
 fi
 
-if [ -d "${input_dir}/.bosh" ]; then
-  # reuse compiled packages
-  cp -r ${input_dir}/.bosh $HOME/
-fi
-
 pushd ${input_dir} > /dev/null
   # configuration
-  export BOSH_DIRECTOR_IP=$DIRECTOR_IP
-  export BOSH_USER=$BOSH_USER
-  export BOSH_PASSWORD=$BOSH_PASSWORD
-
   echo "deleting existing BOSH Director VM..."
   $bosh_cli -n delete-env director.yml
 popd
