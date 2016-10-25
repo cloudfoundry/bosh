1bdb361462bdb36400b97a05e4584e20f984f5d5
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2016-10-25 11:09:52 -0700

    Remove candidate branch from pipeline
    [#132799605](https://www.pivotaltracker.com/story/show/132799605)
    
    Signed-off-by: Danny Berger <dberger@pivotal.io>

--- ci/tasks/merge-master.sh
+++ ci/tasks/merge-master.sh
@@ -5,27 +5,16 @@ set -e -x
 export PATH=$GOPATH/bin:$PATH
 
 DEVELOP=$PWD/bosh-src-develop
-CANDIDATE=$PWD/bosh-src-candidate
 
 git config --global user.email "ci@localhost"
 git config --global user.name "CI Bot"
 
-merge_branch() {
-  branch_name=$1
+git clone ./bosh-src-master ./bosh-src-develop-merged
+cd bosh-src-develop-merged
 
-  repo_name="bosh-src-${branch_name}-merged"
-  git clone ./bosh-src-master ./$repo_name
-  cd $repo_name
+git remote add local $DEVELOP
 
-  git remote add local $2
+git fetch local
+git checkout local/develop
 
-  git fetch local
-  git checkout local/$branch_name
-
-  git merge --no-edit master
-
-  cd -
-}
-
-merge_branch develop $DEVELOP
-merge_branch candidate $CANDIDATE
+git merge --no-edit master
