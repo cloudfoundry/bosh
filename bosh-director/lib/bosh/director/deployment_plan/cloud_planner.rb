efae26e12fd145a91aebb9965ce102e763158c1e
Tom Viehman <tviehman@pivotal.io>
2016-11-28 10:26:04 -0800

    Extract CloudPlanner into its own file/spec
    Signed-off-by: Danny Berger <dberger@pivotal.io>

---
+++ bosh-director/lib/bosh/director/deployment_plan/cloud_planner.rb
@@ -0,0 +1,99 @@
+module Bosh::Director
+  module DeploymentPlan
+    class CloudPlanner
+      attr_accessor :compilation
+
+      def initialize(options)
+        @networks = self.class.index_by_name(options.fetch(:networks))
+        @global_network_resolver = options.fetch(:global_network_resolver)
+        @resource_pools = self.class.index_by_name(options.fetch(:resource_pools))
+        @vm_types = self.class.index_by_name(options.fetch(:vm_types, {}))
+        @vm_extensions = self.class.index_by_name(options.fetch(:vm_extensions, {}))
+        @disk_types = self.class.index_by_name(options.fetch(:disk_types))
+        @availability_zones = options.fetch(:availability_zones_list)
+        @compilation = options.fetch(:compilation)
+        @ip_provider_factory = options.fetch(:ip_provider_factory)
+        @logger = options.fetch(:logger)
+      end
+
+      def ip_provider
+        @ip_provider ||= @ip_provider_factory.new_ip_provider(@networks)
+      end
+
+      def deleted_network(name)
+        ManualNetwork.parse(
+          {'subnets' => [], 'name' => name},
+          [],
+          @global_network_resolver,
+          @logger
+        )
+      end
+
+      def availability_zone(name)
+        @availability_zones[name]
+      end
+
+      def availability_zones
+        @availability_zones.values
+      end
+
+      def resource_pools
+        @resource_pools.values
+      end
+
+      def resource_pool(name)
+        @resource_pools[name]
+      end
+
+      def vm_types
+        @vm_types.values
+      end
+
+      def vm_type(name)
+        @vm_types[name]
+      end
+
+      def vm_extensions
+        @vm_extensions.values
+      end
+
+      def vm_extension(name)
+        unless @vm_extensions.has_key?(name)
+          raise "The vm_extension '#{name}' has not been configured in cloud-config."
+        end
+
+        @vm_extensions[name]
+      end
+
+      def add_resource_pool(resource_pool)
+        @resource_pools[resource_pool.name] = resource_pool
+      end
+
+      def networks
+        @networks.values
+      end
+
+      def network(name)
+        @networks[name]
+      end
+
+      def disk_types
+        @disk_types.values
+      end
+
+      def disk_type(name)
+        @disk_types[name]
+      end
+
+      def using_global_networking?
+        false
+      end
+
+      def self.index_by_name(collection)
+        collection.inject({}) do |index, item|
+          index.merge(item.name => item)
+        end
+      end
+    end
+  end
+end
---
+++ bosh-director/spec/unit/deployment_plan/cloud_planner_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+module Bosh::Director
+  module DeploymentPlan
+    describe CloudPlanner do
+      subject { described_class.new({
+        :networks => {},
+        :global_network_resolver => [],
+        :resource_pools => [],
+        :disk_types => [],
+        :availability_zones_list => [],
+        :compilation => {},
+        :vm_extensions => vm_extensions,
+        :ip_provider_factory => nil,
+        :logger => nil,
+      }) }
+
+      context '#vm_extension' do
+        let(:vm_extensions) do
+          [
+            VmExtension.new({
+              'name' => 'test1',
+              'cloud_properties' => {
+                'fake-property' => 'fake-value',
+              }
+            })
+          ]
+        end
+
+        it 'returns a defined vm_extension' do
+          expect(subject.vm_extension('test1').cloud_properties['fake-property']).to eq('fake-value')
+        end
+
+        it 'raises for an undefined vm_extension' do
+          expect { subject.vm_extension('non-existant') }.to raise_error("The vm_extension 'non-existant' has not been configured in cloud-config.")
+        end
+      end
+    end
+  end
+end
