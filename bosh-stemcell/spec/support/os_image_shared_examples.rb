4a7957a0bff602927d88d1b652785aa40f6980c4
Ming Xiao <mxiao@pivotal.io>
2016-11-29 11:39:00 -0800

    Set vcap home directory permission to 700
    [#117393839](https://www.pivotaltracker.com/story/show/117393839)
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- bosh-stemcell/spec/support/os_image_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_shared_examples.rb
@@ -58,7 +58,7 @@ shared_examples_for 'every OS image' do
     end
 
     describe command('stat -c %a ~vcap') do
-      it { should return_stdout('755') }
+      it { should return_stdout('700') }
     end
   end
 
