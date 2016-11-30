a8223c8e93a71f1e96f8640de23c1abbc019b389
Tyler Schultz <tschultz@pivotal.io>
2016-11-29 16:25:35 -0800

    Add stemcell pipeline configuration script for 3312.x
    [#135184699](https://www.pivotaltracker.com/story/show/135184699)
    
    Signed-off-by: Dmitriy Kalinin <dkalinin@pivotal.io>

---
+++ ci/pipelines/stemcells/configure-pipeline.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu
+
+fly -t production set-pipeline \
+ -p bosh:stemcells:3312.x \
+ -v stemcell_branch=3312.x \
+ -v stemcell_version_key=bosh-stemcell/version-3312.x \
+ -v stemcell_version_semver_bump=minor \
+ -c ci/pipelines/stemcells/pipeline.yml \
+ -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
+ -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
