82497b3f79792c19fc8bba1458a05d1d890b26e0
Tom Viehman <tviehman@pivotal.io>
2016-12-09 09:31:09 -0800

    Shasum dependencies
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- src/bosh-dev/lib/bosh/dev/legacy_agent_manager.rb
+++ src/bosh-dev/lib/bosh/dev/legacy_agent_manager.rb
@@ -4,13 +4,37 @@ module Bosh::Dev
   class LegacyAgentManager
     S3_BUCKET_BASE_URL = 'https://s3.amazonaws.com/bosh-dependencies/legacy-agents'
 
+    class LegacyAgentInfo < Struct.new(:agent_name_rev, :darwin_sha256, :linux_sha256)
+      def sha256
+        darwin? ? darwin_sha256 : linux_sha256
+      end
+
+      def platform
+        darwin? ? 'darwin' : 'linux'
+      end
+
+      def file_name_to_download
+        "agent-#{agent_name_rev}-#{platform}-amd64"
+      end
+
+      private
+
+      def darwin?
+        RUBY_PLATFORM =~ /darwin/
+      end
+    end
+
     # When testing with a legacy agent, it should be compiled (for both Darwin and Linux) and uploaded to the S3 bucket.
     # For clarity, the file name should contain a description of the agent's purpose and the agent's git commit hash of
     # when it was built.
     #
     # To use, you must specify the cloud property "legacy_agent_path" and use the method "get_legacy_agent_path(agent_name)"
     LEGACY_AGENTS_LIST = [
-      'no-upload-blob-action-e82bdd1c'
+      LegacyAgentInfo.new(
+        'no-upload-blob-action-e82bdd1c',
+        'b791ca6d4841478dcf25bfca91c96dc7b1cc72b719f4488c190d93ca33386fa9', # darwin
+        '2a6f42496f1eed4b88871825511af97e9c6c691ab2402c13ea43748dd2873fa4' # linux
+      )
     ]
 
     REPO_ROOT = File.expand_path('../../../../', File.dirname(__FILE__))
@@ -19,47 +43,36 @@ module Bosh::Dev
     def self.install
       FileUtils.mkdir_p(INSTALL_DIR)
 
-      LEGACY_AGENTS_LIST.each do |agent_name|
-        executable_file_path = generate_executable_full_path(agent_name)
-        unless File.exist? executable_file_path
-          downloaded_file_path = download(agent_name)
-          FileUtils.copy(downloaded_file_path, executable_file_path)
-          FileUtils.remove(downloaded_file_path, :force => true)
-        end
-        File.chmod(0777, executable_file_path)
+      LEGACY_AGENTS_LIST.each do |legacy_agent_info|
+        executable_file_path = generate_executable_full_path(legacy_agent_info.agent_name_rev)
+        downloaded_file_path = download(legacy_agent_info)
+        FileUtils.copy(downloaded_file_path, executable_file_path)
+        FileUtils.remove(downloaded_file_path, :force => true)
+        File.chmod(0700, executable_file_path)
       end
     end
 
     def self.generate_executable_full_path(agent_name)
-      raise "Unable to find legacy agent with name #{agent_name}" unless LEGACY_AGENTS_LIST.include? agent_name
+      raise "Unable to find legacy agent with name #{agent_name}" unless LEGACY_AGENTS_LIST.map(&:agent_name_rev).include? agent_name
       File.expand_path(File.join(INSTALL_DIR, agent_name), REPO_ROOT)
     end
 
     private
 
-    def self.download(agent_name)
-      file_to_download = self.generate_file_name_to_download(agent_name)
-      downloaded_file_full_path = File.join(INSTALL_DIR, file_to_download)
+    def self.download(agent_info)
+      destination_path = File.join(INSTALL_DIR, agent_info.file_name_to_download)
 
-      unless File.exist?(downloaded_file_full_path)
+      unless File.exist?(destination_path)
         retryable.retryer do
-          `curl --output #{downloaded_file_full_path} -L #{S3_BUCKET_BASE_URL}/#{file_to_download}`
+          `#{File.dirname(__FILE__)}/sandbox/services/install_binary.sh #{agent_info.file_name_to_download} #{destination_path} #{agent_info.sha256} bosh-dependencies/legacy-agents`
           $? == 0
         end
       end
-      downloaded_file_full_path
+      destination_path
     end
 
     def self.retryable
       Bosh::Retryable.new({tries: 6})
-    end
-
-    def self.generate_file_name_to_download(agent_name)
-      if RUBY_PLATFORM =~ /darwin/
-        "agent-#{agent_name}-darwin-amd64"
-      else
-        "agent-#{agent_name}-linux-amd64"
-      end
     end
   end
 end
--- src/bosh-dev/lib/bosh/dev/sandbox/services/uaa_service.rb
+++ src/bosh-dev/lib/bosh/dev/sandbox/services/uaa_service.rb
@@ -31,21 +31,16 @@ module Bosh::Dev::Sandbox
 
     def self.install
       webapp_path = File.join(TOMCAT_DIR, 'webapps', UAA_FILENAME)
-      return if File.exist?(webapp_path)
 
       FileUtils.mkdir_p(TOMCAT_DIR)
 
-      tomcat_url = "https://s3.amazonaws.com/bosh-dependencies/#{TOMCAT_VERSIONED_FILENAME}.tar.gz"
-
       retryable.retryer do
-        `curl -L #{tomcat_url} | (cd #{INSTALL_DIR} && tar xfz -)`
+        `#{File.dirname(__FILE__)}/install_tomcat.sh #{INSTALL_DIR} #{TOMCAT_VERSIONED_FILENAME} 957e88df8a9c3fc6b786321c4014b44c5c775773`
         $? == 0
       end
 
-      uaa_url = "https://s3.amazonaws.com/bosh-dependencies/#{UAA_VERSION}.war"
-
       retryable.retryer do
-        `curl --output #{webapp_path} -L #{uaa_url}`
+        `#{File.dirname(__FILE__)}/install_binary.sh #{UAA_VERSION}.war #{webapp_path} 6167d1b5afe3e12c26482fcb45c0056475cb3e1b9ca2996707d9ac9c22f60dc9 bosh-dependencies`
         $? == 0
       end
     end
