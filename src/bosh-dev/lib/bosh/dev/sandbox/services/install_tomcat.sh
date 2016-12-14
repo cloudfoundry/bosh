bdc6d233485b796f682d71149d2bb1c293fc3a59
Tyler Schultz <tschultz@pivotal.io>
2016-12-13 16:04:14 -0800

    Using shasum instead of sha1sum
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- src/bosh-dev/lib/bosh/dev/sandbox/services/install_tomcat.sh
+++ src/bosh-dev/lib/bosh/dev/sandbox/services/install_tomcat.sh
@@ -10,6 +10,6 @@ ARCHIVE_URL=https://s3.amazonaws.com/bosh-dependencies/${VERSIONED_FILENAME}.tar
 ARCHIVE=/tmp/$(basename $ARCHIVE_URL)
 
 wget -q $ARCHIVE_URL -O $ARCHIVE
-echo "${ARCHIVE_SHA} ${ARCHIVE}" | sha1sum -c -
+echo "${ARCHIVE_SHA}  ${ARCHIVE}" | shasum -c -
 tar xf $ARCHIVE -C $INSTALL_DIR
 rm -f $ ARCHIVE
