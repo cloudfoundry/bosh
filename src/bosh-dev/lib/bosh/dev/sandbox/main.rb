0d3e769617c3227972cfa751d002181aa5588e37
Dale Wick <dwick@pivotal.io>
2017-01-23 09:30:07 -0500

    Isolated the instances of the uaa server in a multithreaded environment.
    [#136635365](https://www.pivotaltracker.com/story/show/136635365)
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

--- src/bosh-dev/lib/bosh/dev/sandbox/main.rb
+++ src/bosh-dev/lib/bosh/dev/sandbox/main.rb
@@ -86,7 +86,7 @@ module Bosh::Dev::Sandbox
 
       setup_nats
 
-      @uaa_service = UaaService.new(@port_provider, base_log_path, @logger)
+      @uaa_service = UaaService.new(@port_provider, sandbox_root, base_log_path, @logger)
       @config_server_service = ConfigServerService.new(@port_provider, base_log_path, @logger, test_env_number)
       @nginx_service = NginxService.new(sandbox_root, director_port, director_ruby_port, @uaa_service.port, @logger)
 
