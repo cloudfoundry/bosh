12666377587f692ad6153af997a5e08a4d30dd02
Tyler Schultz <tschultz@pivotal.io>
2017-02-13 10:25:11 -0800

    Update integration test assertion to use new Digest header
    [#139527805]
    
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

--- src/spec/gocli/integration/release/inspect_release_spec.rb
+++ src/spec/gocli/integration/release/inspect_release_spec.rb
@@ -18,42 +18,42 @@ describe 'inspect release', type: :integration do
         {
           'Job'=>'job_using_pkg_1/9a5f09364b2cdc18a45172c15dca21922b3ff196',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'a7d51f65cda79d2276dc9cc254e6fec523b07b02',
+          'Digest'=>'a7d51f65cda79d2276dc9cc254e6fec523b07b02',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_1_and_2/673c3689362f2adb37baed3d8d4344cf03ff7637',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'c9acbf245d4b4721141b54b26bee20bfa58f4b54',
+          'Digest'=>'c9acbf245d4b4721141b54b26bee20bfa58f4b54',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_2/8e9e3b5aebc7f15d661280545e9d1c1c7d19de74',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'79475b0b035fe70f13a777758065210407170ec3',
+          'Digest'=>'79475b0b035fe70f13a777758065210407170ec3',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_3/54120dd68fab145433df83262a9ba9f3de527a4b',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'ab4e6077ecf03399f215e6ba16153fd9ebbf1b5f',
+          'Digest'=>'ab4e6077ecf03399f215e6ba16153fd9ebbf1b5f',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_4/0ebdb544f9c604e9a3512299a02b6f04f6ea6d0c',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'1ff32a12e0c574720dd8e5111834bac67229f5c1',
+          'Digest'=>'1ff32a12e0c574720dd8e5111834bac67229f5c1',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_5/fb41300edf220b1823da5ab4c243b085f9f249af',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'37350e20c6f78ab96a1191e5d97981a8d2831665',
+          'Digest'=>'37350e20c6f78ab96a1191e5d97981a8d2831665',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
@@ -61,31 +61,31 @@ describe 'inspect release', type: :integration do
           'Package'=>'pkg_1/16b4c8ef1574b3f98303307caad40227c208371f',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'93fade7dd8950d8a1dd2bf5ec751e478af3150e9'
+          'Digest'=>'93fade7dd8950d8a1dd2bf5ec751e478af3150e9'
         },
         {
           'Package'=>'pkg_2/f5c1c303c2308404983cf1e7566ddc0a22a22154',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'b2751daee5ef20b3e4f3ebc3452943c28f584500'
+          'Digest'=>'b2751daee5ef20b3e4f3ebc3452943c28f584500'
         },
         {
           'Package'=>'pkg_3_depends_on_2/413e3e9177f0037b1882d19fb6b377b5b715be1c',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'62fff2291aac72f5bd703dba0c5d85d0e23532e0'
+          'Digest'=>'62fff2291aac72f5bd703dba0c5d85d0e23532e0'
         },
         {
           'Package'=>'pkg_4_depends_on_3/9207b8a277403477e50cfae52009b31c840c49d4',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'603f212d572b0307e4c51807c5e03c47944bb9c3'
+          'Digest'=>'603f212d572b0307e4c51807c5e03c47944bb9c3'
         },
         {
           'Package'=>'pkg_5_depends_on_4_and_1/3cacf579322370734855c20557321dadeee3a7a4',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'ad733ca76ab4747747d8f9f1ddcfa568519a2e00'
+          'Digest'=>'ad733ca76ab4747747d8f9f1ddcfa568519a2e00'
         }
       ])
     end
@@ -99,42 +99,42 @@ describe 'inspect release', type: :integration do
         {
           'Job'=>'job_using_pkg_1/9a5f09364b2cdc18a45172c15dca21922b3ff196',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'a7d51f65cda79d2276dc9cc254e6fec523b07b02',
+          'Digest'=>'a7d51f65cda79d2276dc9cc254e6fec523b07b02',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_1_and_2/673c3689362f2adb37baed3d8d4344cf03ff7637',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'c9acbf245d4b4721141b54b26bee20bfa58f4b54',
+          'Digest'=>'c9acbf245d4b4721141b54b26bee20bfa58f4b54',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_2/8e9e3b5aebc7f15d661280545e9d1c1c7d19de74',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'79475b0b035fe70f13a777758065210407170ec3',
+          'Digest'=>'79475b0b035fe70f13a777758065210407170ec3',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_3/54120dd68fab145433df83262a9ba9f3de527a4b',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'ab4e6077ecf03399f215e6ba16153fd9ebbf1b5f',
+          'Digest'=>'ab4e6077ecf03399f215e6ba16153fd9ebbf1b5f',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_4/0ebdb544f9c604e9a3512299a02b6f04f6ea6d0c',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'1ff32a12e0c574720dd8e5111834bac67229f5c1',
+          'Digest'=>'1ff32a12e0c574720dd8e5111834bac67229f5c1',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
         {
           'Job'=>'job_using_pkg_5/fb41300edf220b1823da5ab4c243b085f9f249af',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'37350e20c6f78ab96a1191e5d97981a8d2831665',
+          'Digest'=>'37350e20c6f78ab96a1191e5d97981a8d2831665',
           'Links Consumed' => '',
           'Links Provided' => ''
         },
@@ -142,61 +142,61 @@ describe 'inspect release', type: :integration do
           'Package'=>'pkg_1/16b4c8ef1574b3f98303307caad40227c208371f',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'',
-          'SHA1'=>''
+          'Digest'=>''
         },
         {
           'Package'=>'pkg_1/16b4c8ef1574b3f98303307caad40227c208371f',
           'Compiled for'=>'centos-7/3001',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'735987b52907d970106f38413825773eec7cc577'
+          'Digest'=>'735987b52907d970106f38413825773eec7cc577'
         },
         {
           'Package'=>'pkg_2/f5c1c303c2308404983cf1e7566ddc0a22a22154',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'',
-          'SHA1'=>''
+          'Digest'=>''
         },
         {
           'Package'=>'pkg_2/f5c1c303c2308404983cf1e7566ddc0a22a22154',
           'Compiled for'=>'centos-7/3001',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'5b21895211d8592c129334e3d11bd148033f7b82'
+          'Digest'=>'5b21895211d8592c129334e3d11bd148033f7b82'
         },
         {
           'Package'=>'pkg_3_depends_on_2/413e3e9177f0037b1882d19fb6b377b5b715be1c',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'',
-          'SHA1'=>''
+          'Digest'=>''
         },
         {
           'Package'=>'pkg_3_depends_on_2/413e3e9177f0037b1882d19fb6b377b5b715be1c',
           'Compiled for'=>'centos-7/3001',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'f5cc94a01d2365bbeea00a4765120a29cdfb3bd7'
+          'Digest'=>'f5cc94a01d2365bbeea00a4765120a29cdfb3bd7'
         },
         {
           'Package'=>'pkg_4_depends_on_3/9207b8a277403477e50cfae52009b31c840c49d4',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'',
-          'SHA1'=>''
+          'Digest'=>''
         },
         {
           'Package'=>'pkg_4_depends_on_3/9207b8a277403477e50cfae52009b31c840c49d4',
           'Compiled for'=>'centos-7/3001',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'f21275861158ad864951faf76da0dce9c1b5f215'
+          'Digest'=>'f21275861158ad864951faf76da0dce9c1b5f215'
         },
         {
           'Package'=> 'pkg_5_depends_on_4_and_1/3cacf579322370734855c20557321dadeee3a7a4',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'',
-          'SHA1'=>''
+          'Digest'=>''
         },
         {
           'Package'=> 'pkg_5_depends_on_4_and_1/3cacf579322370734855c20557321dadeee3a7a4',
           'Compiled for'=>'centos-7/3001',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'002deec46961440df01c620be491e5b12246c5df'
+          'Digest'=>'002deec46961440df01c620be491e5b12246c5df'
         }
       ])
     end
@@ -209,28 +209,28 @@ describe 'inspect release', type: :integration do
         {
           'Job'=>'api_server/a910dd03699797ba52f73f56f70475c794f9ff43',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'1e1ccbc60fefa8713c9c52510968bf0399f7dfda',
+          'Digest'=>'1e1ccbc60fefa8713c9c52510968bf0399f7dfda',
           'Links Consumed' => "- name: db\n  type: db\n  optional: true\n- name: backup_db\n  type: db",
           'Links Provided' => ''
         },
         {
           'Job'=>'backup_database/2ea09882747364709dad9f45267965ac176ae5ad',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'46965969e112507cfb29119052a864e40f785d80',
+          'Digest'=>'46965969e112507cfb29119052a864e40f785d80',
           'Links Consumed' => '',
           'Links Provided' => "- name: backup_db\n  type: db"
         },
         {
           'Job'=>'database/a9f952f94a82c13a3129ac481030f704a33d027f',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'9fbd6941108389f769c8f7dafc93410c386f1bd5',
+          'Digest'=>'9fbd6941108389f769c8f7dafc93410c386f1bd5',
           'Links Consumed' => '',
           'Links Provided' => "- name: db\n  type: db"
         },
         {
           'Job'=>'mongo_db/1a57f0be3eb19e263261536693db0d5a521261a6',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'36705751b05ab71b9580d82f8347b71f7de31f0b',
+          'Digest'=>'36705751b05ab71b9580d82f8347b71f7de31f0b',
           'Links Consumed' => '',
           'Links Provided' => "- name: read_only_db\n  type: db"
 
@@ -238,7 +238,7 @@ describe 'inspect release', type: :integration do
         {
           'Job'=>'node/20df71b951455e465845eab32743822693977b30',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'db867641e909c4ab1d22bb86037489530ee7d7f1',
+          'Digest'=>'db867641e909c4ab1d22bb86037489530ee7d7f1',
           'Links Consumed' => "- name: node1\n  type: node1\n- name: node2\n  type: node2\n  optional: true",
           'Links Provided' => "- name: node1\n  type: node1\n- name: node2\n  type: node2"
         },
@@ -246,19 +246,19 @@ describe 'inspect release', type: :integration do
           'Package'=>'pkg_1/16b4c8ef1574b3f98303307caad40227c208371f',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'cbf4e5b05e58f31076c124c343485f6a758b2c34'
+          'Digest'=>'cbf4e5b05e58f31076c124c343485f6a758b2c34'
         },
         {
           'Package'=>'pkg_2/4b74be7d5aa14487c7f7b0d4516875f7c0eeb010',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'c49e3985d896d13d7da7fc99f66b0ad43c0ac417'
+          'Digest'=>'c49e3985d896d13d7da7fc99f66b0ad43c0ac417'
         },
         {
           'Package'=>'pkg_3_depends_on_2/413e3e9177f0037b1882d19fb6b377b5b715be1c',
           'Compiled for'=>'(source)',
           'Blobstore ID'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-          'SHA1'=>'3ba0f08957427b0e7f060ee059473a632a712d1f'
+          'Digest'=>'3ba0f08957427b0e7f060ee059473a632a712d1f'
         }
 			])
     end
