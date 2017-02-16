91ab75f3755fcb5dfb4ee5f2e6d1f411be3d4314
Dennis Leon <dleon@pivotal.io>
2017-02-16 12:04:14 -0800

    Drop blobstore_id_idx index from local_dns_blobs
    This migration drops an index that used to be in a previous migration.
    The creation of the index has been removed from the migration as part of
    repairing the migration. This index may not exist if the migration fix
    has been run.
    
    [#140068815](https://www.pivotaltracker.com/story/show/140068815)
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- src/bosh-director/spec/unit/db/migrations/director/add_data_migration_spec.rb
+++ src/bosh-director/spec/unit/db/migrations/director/add_data_migration_spec.rb
@@ -9,7 +9,7 @@ module Bosh::Director
       # populated with data. This test will fail every time a new migration script is added. Change
       # the file name below to the latest when a test is added.
       # Look at tests in this directory for similar examples: bosh-director/spec/unit/db/migrations/director
-      expect(latest_db_migration_file).to eq('20170203212124_add_variables.rb')
+      expect(latest_db_migration_file).to eq('20170216194502_remove_blobstore_id_idx_from_local_dns_blobs.rb')
     end
   end
 end
