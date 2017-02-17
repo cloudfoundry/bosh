5187b2981be9159c7910e7d91b753259c4e342f6
James Quintana <jquintana@pivotal.io>
2017-02-17 12:22:45 -0800

    Change CI to use master. Remove merge-master job.
    [#136624141](https://www.pivotaltracker.com/story/show/136624141)
    
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- ci/tasks/finalize-bosh-release.sh
+++ ci/tasks/finalize-bosh-release.sh
@@ -14,7 +14,7 @@ chmod +x $GO_CLI_PATH
 
 export DEV_RELEASE_PATH=$ROOT_PATH/bosh-dev-release/bosh*.tgz
 
-git clone ./bosh-src-develop $PROMOTED_REPO
+git clone ./bosh-src-master $PROMOTED_REPO
 
 pushd $PROMOTED_REPO
   git status
