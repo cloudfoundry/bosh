8375727e1c1783afcfbd6c31809b590b741dc07f
Wang Jian Qiu <wangjq@cn.ibm.com>
2017-02-27 16:48:39 -0800

    Refactor setting up environment for brats into a BeforeSuite
    [#135429409](https://www.pivotaltracker.com/story/show/135429409)
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/brats_suite_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/brats_suite_test.go
@@ -3,11 +3,43 @@ package brats_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"os"
 
+	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+
+	"fmt"
+	"github.com/cloudfoundry/bosh-utils/system"
 	"testing"
 )
+
+const BLOBSTORE_ACCESS_LOG = "/var/vcap/sys/log/blobstore/blobstore_access.log"
 
 func TestBrats(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Brats Suite")
+}
+
+var cmdRunner system.CmdRunner
+var boshBinaryPath, directorIp, sshPrivateKeyPath string
+
+var _ = BeforeSuite(func() {
+	cmdRunner = system.NewExecCmdRunner(boshlog.NewLogger(boshlog.LevelNone))
+	boshBinaryPath = assertEnvExists("BOSH_BINARY_PATH")
+	directorIp = assertEnvExists("BOSH_DIRECTOR_IP")
+	sshPrivateKeyPath = assertEnvExists("BOSH_SSH_PRIVATE_KEY_PATH")
+
+	assertEnvExists("BOSH_CLIENT")
+	assertEnvExists("BOSH_CLIENT")
+	assertEnvExists("BOSH_CLIENT_SECRET")
+	assertEnvExists("BOSH_CA_CERT")
+	assertEnvExists("BOSH_ENVIRONMENT")
+
+})
+
+func assertEnvExists(envName string) string {
+	val, found := os.LookupEnv(envName)
+	if !found {
+		Fail(fmt.Sprintf("Expected %s", envName))
+	}
+	return val
 }
--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/brats_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/brats_test.go
@@ -3,50 +3,39 @@ package brats_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/cloudfoundry/bosh-utils/system"
 
-	boshlog "github.com/cloudfoundry/bosh-utils/logger"
 	"fmt"
-	"os"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 )
 
 var _ = Describe("Brats", func() {
 
-	Context("nginx", func() {
-		It("Should log in cef format", func() {
-			cmdRunner := system.NewExecCmdRunner(boshlog.NewLogger(boshlog.LevelNone))
-			boshBinaryPath := os.Getenv("BOSH_BINARY_PATH")
-			directorIp := os.Getenv("BOSH_DIRECTOR_IP")
-			sshPrivateKeyPath := os.Getenv("BOSH_SSH_PRIVATE_KEY_PATH")
-			blobstoreAccessLog := "/var/vcap/sys/log/blobstore/blobstore_access.log"
+	Context("blobstore nginx", func() {
+		Context("When nginx writes to access log", func() {
+			var tempBlobstoreDir string
 
-			assertEnvExists("BOSH_CLIENT")
-			assertEnvExists("BOSH_CLIENT_SECRET")
-			assertEnvExists("BOSH_CA_CERT")
-			assertEnvExists("BOSH_ENVIRONMENT")
+			BeforeEach(func() {
+				var err error
+				tempBlobstoreDir, err = ioutil.TempDir(os.TempDir(), "blobstore_access")
+				Expect(err).ToNot(HaveOccurred())
 
-			tempBlobstoreDir, err := ioutil.TempDir(os.TempDir(), "blobstore_access")
-			Expect(err).ToNot(HaveOccurred())
+				_, _, exitCode, err := cmdRunner.RunCommand(boshBinaryPath, "upload-release", "-n", "https://bosh.io/d/github.com/cloudfoundry/os-conf-release?v=11")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(exitCode).To(Equal(0))
 
-			_, _, exitCode, err := cmdRunner.RunCommand(boshBinaryPath, "upload-release", "-n", "https://bosh.io/d/github.com/cloudfoundry/os-conf-release?v=11")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(exitCode).To(Equal(0))
+				_, _, exitCode, err = cmdRunner.RunCommand("scp", "-B", "-i", sshPrivateKeyPath, fmt.Sprintf("jumpbox@%s:%s", directorIp, BLOBSTORE_ACCESS_LOG), tempBlobstoreDir)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(exitCode).To(Equal(0))
 
-			_, _, exitCode, err = cmdRunner.RunCommand("scp", "-B", "-i", sshPrivateKeyPath, fmt.Sprintf("jumpbox@%s:%s", directorIp, blobstoreAccessLog), tempBlobstoreDir)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(exitCode).To(Equal(0))
+			})
 
-			accessContent, err := ioutil.ReadFile(filepath.Join(tempBlobstoreDir, "blobstore_access.log"))
-			Expect(err).ToNot(HaveOccurred())
-			Expect(string(accessContent)).To(ContainSubstring("CEF:0|CloudFoundry|BOSH|-|blobstore_api|"))
+			It("Should log in correct format", func() {
+				accessContent, err := ioutil.ReadFile(filepath.Join(tempBlobstoreDir, "blobstore_access.log"))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(accessContent)).To(ContainSubstring("CEF:0|CloudFoundry|BOSH|-|blobstore_api|"))
+			})
 		})
 	})
 })
-
-func assertEnvExists(envName string) {
-	if _, found := os.LookupEnv(envName); !found {
-		Fail(fmt.Sprintf("Expected %s", envName))
-	}
-}
