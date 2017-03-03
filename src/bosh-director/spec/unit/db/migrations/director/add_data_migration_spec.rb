03260dfc8d87e195dde87bef213ae085914e00fe
Adrian Zankich <azankich@pivotal.io>
2017-03-03 15:03:11 -0800

    Add 20170303175054_expand_template_json_column_lengths migration
    - This migration fixes a MySQL column length error when a release
    provides a large number of links.
    
    [#140975885](https://www.pivotaltracker.com/story/show/140975885)
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- src/bosh-director/spec/unit/db/migrations/director/add_data_migration_spec.rb
+++ src/bosh-director/spec/unit/db/migrations/director/add_data_migration_spec.rb
@@ -9,7 +9,7 @@ module Bosh::Director
       # populated with data. This test will fail every time a new migration script is added. Change
       # the file name below to the latest when a test is added.
       # Look at tests in this directory for similar examples: bosh-director/spec/unit/db/migrations/director
-      expect(latest_db_migration_file).to eq('20170217000000_variables_instance_table_foreign_key_update.rb')
+      expect(latest_db_migration_file).to eq('20170303175054_expand_template_json_column_lengths.rb')
     end
   end
 end
