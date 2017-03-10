4195090e62ae125578815c8cfc54dec0918a1881
Shatarupa Nandi <snandi@pivotal.io>
2017-03-10 14:17:32 -0800

    Bump up timeout for lock to 4 minutes.
    [#141366821](https://www.pivotaltracker.com/story/show/141366821)

--- src/bosh-director/lib/bosh/director/lock.rb
+++ src/bosh-director/lib/bosh/director/lock.rb
@@ -15,7 +15,7 @@ module Bosh::Director
     def initialize(name, opts = {})
       @name = name
       @uid = SecureRandom.uuid
-      @timeout = opts[:timeout] || 1.0
+      @timeout = opts[:timeout] || 240.0
       @expiration = opts[:expiration] || 10.0
       @logger = Config.logger
       @refresh_thread = nil
