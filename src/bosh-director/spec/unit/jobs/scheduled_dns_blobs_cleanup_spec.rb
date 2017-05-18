5163739be5cf833e32398b6d3fb02de6f581c772
J Evelyn Quintana <jquintana@pivotal.io>
2017-05-18 12:48:12 -0700

    avoid polluting time progression between unit tests

--- src/bosh-director/spec/unit/jobs/scheduled_dns_blobs_cleanup_spec.rb
+++ src/bosh-director/spec/unit/jobs/scheduled_dns_blobs_cleanup_spec.rb
@@ -24,6 +24,10 @@ module Bosh::Director
       Timecop.freeze(Time.now)
     end
 
+    after do
+      Timecop.return
+    end
+
     describe '.has_work' do
       context 'when there are more than the given num_dns_blobs_to_keep' do
         describe 'when there an old blob' do
