af6d1899ad1f9135f45e4c1dd7e295caa85076e1
Danny Berger <dberger@pivotal.io>
2017-05-22 11:53:39 -0700

    main-bosh-docker: apt-get update before trying to install packages
    [#145876517](https://www.pivotaltracker.com/story/show/145876517)
    
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- ci/docker/main-bosh-docker/install-docker.sh
+++ ci/docker/main-bosh-docker/install-docker.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 set -xe
+
 source /etc/profile.d/chruby.sh
 chruby 2.3.1
+
+apt-get update
 
 apt-get install -y --no-install-recommends \
 		apt-transport-https \
 		ca-certificates \
 		curl \
-		software-properties-common && apt-get clean
+		software-properties-common
+
+apt-get clean
 
 curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 apt-key fingerprint | grep 'Key fingerprint = 5811 8E89 F3A9 1289 7C07  0ADB F762 2157 2C52 609D'
