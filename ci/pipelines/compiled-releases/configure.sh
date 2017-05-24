92ef04e75447c1cb7a231323fcb31381990680d4
Danny Berger <dberger@pivotal.io>
2017-05-24 14:07:38 -0700

    moving compiled releases to bosh-deployment repo
    Signed-off-by: dmitriy kalinin <dkalinin@pivotal.io>

--- ci/pipelines/compiled-releases/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-fly -t production set-pipeline \
- -p compiled-releases \
- -c ./pipeline.yml \
- -l <(lpass show --note "concourse:production pipeline:compiled-releases")
