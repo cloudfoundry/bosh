7b0d5319afb404784665864f8ef5375f3de27e9d
Danny Berger <dberger@pivotal.io>
2017-05-25 16:44:56 -0700

    Suffix delayed job worker names with UUID
    To avoid duplicate worker processes accidentally picking up an already
    running job.
    
    [#146054509](https://www.pivotaltracker.com/story/show/146054509)

--- src/bosh-director/lib/bosh/director/worker.rb
+++ src/bosh-director/lib/bosh/director/worker.rb
@@ -4,6 +4,7 @@ module Bosh::Director
     def initialize(config, index=0)
       @config = config
       @index = index
+      @uuid = SecureRandom.uuid
     end
 
     def prep
@@ -22,13 +23,14 @@ module Bosh::Director
       queues << @config.director_pool unless @config.director_pool.nil? || (queues - ['urgent']).empty?
 
       @delayed_job_worker = Delayed::Worker.new({:queues => queues})
+      @delayed_job_worker.name = "worker_#{@index}-#{@uuid}"
+
       trap('USR1') {
         @delayed_job_worker.queues = [ 'non_existent_queue' ]
       } #stop accepting new jobs when USR1 is sent
     end
 
     def start
-      @delayed_job_worker.name = "worker_#{@index}"
       @delayed_job_worker.logger.info("Starting worker #{@delayed_job_worker.name}.")
 
       begin
--- src/bosh-director/spec/unit/worker_spec.rb
+++ src/bosh-director/spec/unit/worker_spec.rb
@@ -1,54 +1,79 @@
 require 'spec_helper'
 
-describe 'worker' do
-  subject(:worker) { Bosh::Director::Worker.new(config) }
-  let(:config_hash) do
-    {
-      'dir' => '/tmp/boshdir ',
-      'db' => {
-        'adapter' => 'sqlite'
-      },
-      'verify_multidigest_path' => '/some/path',
-      'blobstore' => {
-        'provider' => 's3cli',
-        'options' => {
-          's3cli_path' => true
-        }
-      },
-      'config_server' => {
-        'enabled' => false
+module Bosh::Director
+  describe Worker do
+    subject(:worker) { described_class.new(config) }
+
+    let(:config_hash) do
+      {
+        'dir' => '/tmp/boshdir ',
+        'db' => {
+          'adapter' => 'sqlite'
+        },
+        'verify_multidigest_path' => '/some/path',
+        'blobstore' => {
+          'provider' => 's3cli',
+          'options' => {
+            's3cli_path' => true
+          }
+        },
+        'config_server' => {
+          'enabled' => false
+        },
+        'uuid' => 'fake-director-uuid',
       }
-    }
-  end
-  let(:config) { Bosh::Director::Config.load_hash(config_hash) }
-
-  describe 'when workers is sent USR1' do
-    let(:queues) { worker.queues }
-    before { allow(Socket).to receive(:gethostname).and_return(nil) }
-    it 'should not pick up new tasks' do
-      ENV['QUEUE']='normal'
-      worker.prep
-      expect(worker.queues).to eq(['normal'])
-      Process.kill('USR1', Process.pid)
-      sleep 0.1 # Ruby 1.9 fails without the sleep due to a race condition between rspec assertion and actually killing the process
-      expect(worker.queues).to eq(['non_existent_queue'])
     end
-  end
+    let(:config) { Bosh::Director::Config.load_hash(config_hash) }
 
-  describe 'when deployment has director_pool' do
-    let(:queues) { worker.queues }
-    before { allow(Socket).to receive(:gethostname).and_return('local.hostname') }
+    describe 'when workers is sent USR1' do
+      let(:queues) { worker.queues }
+      before { allow(Socket).to receive(:gethostname).and_return(nil) }
 
-    it 'should have new queue' do
-      ENV['QUEUE']='normal'
-      worker.prep
-      expect(worker.queues).to eq(['normal', 'local.hostname'])
+      it 'should not pick up new tasks' do
+        ENV['QUEUE']='normal'
+        worker.prep
+        expect(worker.queues).to eq(['normal'])
+        Process.kill('USR1', Process.pid)
+        expect(worker.queues).to eq(['non_existent_queue'])
+      end
     end
 
-    it 'should not have new queue in case of urgent' do
-      ENV['QUEUE']='urgent'
-      worker.prep
-      expect(worker.queues).to eq(['urgent'])
+    describe 'when deployment has director_pool' do
+      let(:queues) { worker.queues }
+      before { allow(Socket).to receive(:gethostname).and_return('local.hostname') }
+
+      it 'should have new queue' do
+        ENV['QUEUE']='normal'
+        worker.prep
+        expect(worker.queues).to eq(['normal', 'local.hostname'])
+      end
+
+      it 'should not have new queue in case of urgent' do
+        ENV['QUEUE']='urgent'
+        worker.prep
+        expect(worker.queues).to eq(['urgent'])
+      end
+    end
+
+    describe 'worker index' do
+      let(:djworker) { Delayed::Worker.new }
+      before { allow(Delayed::Worker).to receive(:new).and_return(djworker) }
+
+      it 'should use a default name prefix' do
+        worker.prep
+
+        expect(djworker.name).to match(/^worker_0-([a-f0-9\-]{36})$/)
+      end
+
+      describe 'when explicitly set' do
+        subject(:worker) { described_class.new(config, '99') }
+
+        it 'should use it for worker name prefix' do
+          worker.prep
+
+          expect(djworker.name).to match(/^worker_99-([a-f0-9\-]{36})$/)
+        end
+      end
     end
   end
 end
