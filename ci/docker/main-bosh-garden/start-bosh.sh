fe3785b7871946791541e6030c39e5661e4d1dea
Luan Santos <lsantos@pivotal.io>
2017-06-09 10:53:03 -0700

    Extract start-garden into own script
    Also fix bosh-deployment path
    
    [#146710227](https://www.pivotaltracker.com/story/show/146710227)
    
    Signed-off-by: James Young <jayoung@pivotal.io>

--- ci/docker/main-bosh-garden/start-bosh.sh
+++ ci/docker/main-bosh-garden/start-bosh.sh
@@ -2,96 +2,6 @@
 
 set -e
 
-permit_device_control() {
-  local devices_mount_info=$(cat /proc/self/cgroup | grep devices)
-
-  if [ -z "$devices_mount_info" ]; then
-    # cgroups not set up; must not be in a container
-    return
-  fi
-
-  local devices_subsytems=$(echo $devices_mount_info | cut -d: -f2)
-  local devices_subdir=$(echo $devices_mount_info | cut -d: -f3)
-
-  if [ "$devices_subdir" = "/" ]; then
-    # we're in the root devices cgroup; must not be in a container
-    return
-  fi
-
-  RUN_DIR=$(mktemp -d)
-  cgroup_dir=${RUN_DIR}/devices-cgroup
-
-  if [ ! -e ${cgroup_dir} ]; then
-    # mount our container's devices subsystem somewhere
-    mkdir ${cgroup_dir}
-  fi
-
-  if ! mountpoint -q ${cgroup_dir}; then
-    mount -t cgroup -o $devices_subsytems none ${cgroup_dir}
-  fi
-
-  # permit our cgroup to do everything with all devices
-  echo a > ${cgroup_dir}${devices_subdir}/devices.allow
-
-  umount ${cgroup_dir}
-}
-
-create_loop_devices() {
-  set +e
-  amt=${1:-256}
-  for i in $(seq 0 $amt); do
-    if ! mknod -m 0660 /dev/loop$i b 7 $i; then
-      break
-    fi
-  done
-  set -e
-}
-
-start_garden() {
-  echo "nameserver 8.8.8.8" > /etc/resolv.conf
-
-  # check for /proc/sys being mounted readonly, as systemd does
-  if ! grep -qs '/sys' /proc/mounts; then
-    mount -t sysfs sysfs /sys
-  fi
-
-  # shellcheck source=/dev/null
-  permit_device_control
-  create_loop_devices 256
-
-  local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
-  local tmpdir=$(mktemp -d)
-
-  local depot_path=$tmpdir/depot
-
-  mkdir -p $depot_path
-
-  export TMPDIR=$tmpdir
-  export TEMP=$tmpdir
-  export TMP=$tmpdir
-
-  # GARDEN_GRAPH_PATH is the root of the docker image filesystem
-  export GARDEN_GRAPH_PATH=/tmp/garden/graph
-  mkdir -p "${GARDEN_GRAPH_PATH}"
-  truncate -s 4G /tmp/garden/graph-sparse
-  yes | mkfs -t ext4 /tmp/garden/graph-sparse
-  mount -t ext4 /tmp/garden/graph-sparse "${GARDEN_GRAPH_PATH}"
-
-  # we need a non-layered filesystem to be able to nest garden
-  mkdir -p /var/vcap
-  truncate -s 12G /tmp/garden/var-vcap
-  yes | mkfs -t ext4 /tmp/garden/var-vcap
-  mount -t ext4 /tmp/garden/var-vcap /var/vcap
-
-  /opt/garden/bin/gdn server \
-    --allow-host-access \
-    --depot $depot_path \
-    --bind-ip 0.0.0.0 --bind-port 7777 \
-    --mtu $mtu \
-    --graph=$GARDEN_GRAPH_PATH \
-    &
-}
-
 main() {
   source /etc/profile.d/chruby.sh
   chruby 2.3.1
@@ -102,7 +12,7 @@ main() {
 
   export GARDEN_HOST=${OUTER_CONTAINER_IP}
 
-  start_garden
+  start-garden
 
   local local_bosh_dir
   local_bosh_dir="/tmp/local-bosh/director"
--- ci/pipelines/docker-images/tasks/copy-bosh-garden-dependencies.sh
+++ ci/pipelines/docker-images/tasks/copy-bosh-garden-dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-garden/bosh
+cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-docker/bosh
 
-mkdir bosh-src/ci/docker/main-bosh-garden-deployment
-cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-garden-deployment
+mkdir bosh-src/ci/docker/main-bosh-garden/bosh-deployment
+cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-garden/bosh-deployment
 
 cp -R bosh-src/* bosh-src-with-garden-dependencies
