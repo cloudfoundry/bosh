384aa82d364558e3924641036ee2fdda38d94fa8
Luan Santos <lsantos@pivotal.io>
2017-06-09 08:29:09 -0700

    Fix main-bosh-garden build
    [#146710227](https://www.pivotaltracker.com/story/show/146710227)

---
+++ ci/pipelines/docker-images/tasks/copy-bosh-garden-dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-garden/bosh
+
+mkdir bosh-src/ci/docker/main-bosh-garden-deployment
+cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-garden-deployment
+
+cp -R bosh-src/* bosh-src-with-garden-dependencies
