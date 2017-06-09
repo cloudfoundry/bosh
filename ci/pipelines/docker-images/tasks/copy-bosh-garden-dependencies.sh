e1cdefc5f4cc8fdb1b3ed8e4d740feff7a58b8f1
James Young <jayoung@pivotal.io>
2017-06-09 12:21:01 -0700

    fix copy-bosh-garden-dependencies
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- ci/pipelines/docker-images/tasks/copy-bosh-garden-dependencies.sh
+++ ci/pipelines/docker-images/tasks/copy-bosh-garden-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-docker/bosh
+cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-garden/bosh
 
 mkdir bosh-src/ci/docker/main-bosh-garden/bosh-deployment
 cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-garden/bosh-deployment
