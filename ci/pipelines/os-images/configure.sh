6255d717ab4c6735b9d761c6c3ef985b5eceaa94
Tyler Schultz <tschultz@pivotal.io>
2017-06-20 15:11:10 -0700

    Remove CentOS OS image building
    [#147560333](https://www.pivotaltracker.com/story/show/147560333)
    
    Signed-off-by: Danny Berger <dberger@pivotal.io>

--- ci/pipelines/os-images/configure.sh
+++ ci/pipelines/os-images/configure.sh
@@ -3,4 +3,4 @@
 fly -t production \
 set-pipeline -p bosh:os-image:3363.x \
 -c ci/pipelines/os-images/pipeline.yml \
---load-vars-from <(lpass show -G "concourse:production pipeline:os-images" --notes)
+--load-vars-from <(lpass show -G "concourse:production pipeline:os-images" --id 3790608950700570527 --notes)
