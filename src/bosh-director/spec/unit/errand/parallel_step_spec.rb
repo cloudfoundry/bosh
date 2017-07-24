cloudfoundry/bosh

commit 6e762a8a43e4c2b47826edb7180d99ccc294cff4

run errand with parallel step
[#147959537](https://www.pivotaltracker.com/story/show/147959537)

Signed-off-by: Danny Berger <dberger@pivotal.io>

src/bosh-director/lib/bosh/director/errand/parallel_step.rb
@@ -1,0 +1,28 @@
+module Bosh::Director
+  class Errand::ParallelStep
+    attr_reader :steps
+
+    def initialize(max_in_flight, steps)
+      @max_in_flight = max_in_flight
+      @steps = steps
+    end
+
+    def run(&checkpoint_block)
+      results = []
+      mutex = Mutex.new
+
+      Bosh::ThreadPool.new(max_threads: @max_in_flight, logger: Config.logger).wrap do |pool|
+        @steps.each do |step|
+          pool.process do
+            result = step.run(&checkpoint_block)
+
+            mutex.synchronize do
+              results << result
+            end
+          end
+        end
+      end
+      results.join("\n")
+    end
+  end
+end

src/bosh-director/spec/unit/errand/parallel_step_spec.rb
@@ -1,0 +1,37 @@
+require 'spec_helper'
+
+module Bosh::Director
+  describe Errand::ParallelStep do
+    subject(:parallel_step) { Errand::ParallelStep.new(4, [ errand_step1, errand_step2 ]) }
+    let(:errand_step1) { instance_double(Errand::ErrandStep) }
+    let(:errand_step2) { instance_double(Errand::ErrandStep) }
+    let(:checkpoint_block) { Proc.new{} }
+
+    describe '#run' do
+      it 'runs all the steps in parallel' do
+        mutex = Mutex.new
+        resource = ConditionVariable.new
+
+        expect(errand_step1).to receive(:run) do |args, &blk|
+          expect(blk).to be(checkpoint_block)
+
+          mutex.synchronize do
+            resource.wait(mutex)
+          end
+          'step1'
+        end
+        expect(errand_step2).to receive(:run) do |args, &blk|
+          expect(blk).to be(checkpoint_block)
+
+          mutex.synchronize {
+            resource.signal
+          }
+          'step2'
+        end
+
+        results = subject.run(&checkpoint_block)
+        expect(results.split("\n")).to contain_exactly('step1', 'step2')
+      end
+    end
+  end
+end

src/bosh-director/lib/bosh/director/errand.rb
@@ -7,3 +7,4 @@ require 'bosh/director/errand/runner'
 require 'bosh/director/errand/result'
 require 'bosh/director/errand/errand_provider'
 require 'bosh/director/errand/errand_step'
+require 'bosh/director/errand/parallel_step'

src/bosh-director/lib/bosh/director/errand/errand_provider.rb
@@ -38,7 +38,7 @@ module Bosh::Director
 
         runner = Errand::Runner.new(errand_name, errand_is_job_name, @task_result, @instance_manager, @logs_fetcher)
 
-        return Errand::ErrandStep.new(
+        errand_step = Errand::ErrandStep.new(
           runner,
           deployment_planner,
           errand_name,
@@ -48,6 +48,8 @@ module Bosh::Director
           keep_alive,
           deployment_name,
           @logger)
+
+        return Errand::ParallelStep.new(Config.max_threads, [errand_step])
       end
     end
 

src/bosh-director/spec/unit/errand/errand_provider_spec.rb
@@ -43,7 +43,8 @@ module Bosh::Director
             runner, deployment_planner, job_name, instance, instance_group, false, keep_alive, deployment_name, logger
           ).and_return(errand_step)
           returned_errand = subject.get(deployment_name, 'errand-job-name', when_changed, keep_alive)
-          expect(returned_errand).to eq(errand_step)
+          expect(returned_errand.steps[0]).to eq(errand_step)
+
         end
       end
 
@@ -86,7 +87,7 @@ module Bosh::Director
               runner, deployment_planner, instance_group_name, instance, instance_group, false, keep_alive, deployment_name, logger
             ).and_return(errand_step)
             returned_errand = subject.get(deployment_name, instance_group_name, when_changed, keep_alive)
-            expect(returned_errand).to eq(errand_step)
+            expect(returned_errand.steps[0]).to eq(errand_step)
           end
 
           context 'and the lifecycle errand instance group name is the same as the job name' do
@@ -102,7 +103,7 @@ module Bosh::Director
                 runner, deployment_planner, instance_group_name, instance, instance_group, false, keep_alive, deployment_name, logger
               ).and_return(errand_step)
               returned_errand = subject.get(deployment_name, instance_group_name, when_changed, keep_alive)
-              expect(returned_errand).to eq(errand_step)
+              expect(returned_errand.steps[0]).to eq(errand_step)
             end
           end
         end

src/bosh-director/spec/unit/jobs/run_errand_spec.rb
@@ -9,6 +9,7 @@ module Bosh::Director
     let(:task_result) { Bosh::Director::TaskDBWriter.new(:result_output, task.id) }
     let(:task_writer) { Bosh::Director::TaskDBWriter.new(:event_output, task.id) }
     let(:event_log) {Bosh::Director::EventLog::Log.new(task_writer)}
+    let(:thread_pool) { double(Bosh::ThreadPool) }
 
     before do
       allow(App).to receive_message_chain(:instance, :blobstores, :blobstore).and_return(blobstore)
@@ -17,6 +18,12 @@ module Bosh::Director
       allow(Config).to receive(:current_job).and_return(job)
       allow(Config).to receive(:event_log).and_return(event_log)
       allow(Config).to receive(:result).and_return(task_result)
+      allow(Bosh::ThreadPool).to receive(:new).and_return(thread_pool)
+
+      allow(thread_pool).to receive(:wrap) do |&blk|
+        blk.call(thread_pool) if blk
+      end
+      allow(thread_pool).to receive(:process).and_yield
     end
 
     let(:task) { Bosh::Director::Models::Task.make(:id => 42, :username => 'user') }
@@ -301,8 +308,6 @@ module Bosh::Director
                 end
 
                 context 'when the errand fails to run' do
-                  let(:task_manager) { instance_double('Bosh::Director::Api::TaskManager', find_task: task) }
-
                   it 'cleans up the vms anyway' do
                     error = Exception.new
                     allow(job_manager).to receive(:create_missing_vms).with(no_args).ordered

commit 41ac0b93385e84557cdeee634e3db2d8c690a8d9

Parallel step should check cancelability of steps
Broken by 6e762a8a43e4c2b47826edb7180d99ccc294cff4

[#147959537](https://www.pivotaltracker.com/story/show/147959537)

src/bosh-director/lib/bosh/director/errand/parallel_step.rb
@@ -24,5 +24,9 @@ module Bosh::Director
       end
       results.join("\n")
     end
+
+    def ignore_cancellation?
+      @steps.any?(&:ignore_cancellation?)
+    end
   end
 end

src/bosh-director/spec/unit/errand/parallel_step_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 module Bosh::Director
   describe Errand::ParallelStep do
     subject(:parallel_step) { Errand::ParallelStep.new(4, [ errand_step1, errand_step2 ]) }
-    let(:errand_step1) { instance_double(Errand::ErrandStep) }
-    let(:errand_step2) { instance_double(Errand::ErrandStep) }
+    let(:errand_step1) { instance_double(Errand::ErrandStep, ignore_cancellation?: false) }
+    let(:errand_step2) { instance_double(Errand::ErrandStep, ignore_cancellation?: false) }
     let(:checkpoint_block) { Proc.new{} }
 
     describe '#run' do
@@ -31,6 +31,22 @@ module Bosh::Director
 
         results = subject.run(&checkpoint_block)
         expect(results.split("\n")).to contain_exactly('step1', 'step2')
+      end
+    end
+
+    describe '#ignore_cancellation?' do
+      context 'no steps ignore cancellation' do
+        it 'does not ignore cancellation' do
+          expect(subject.ignore_cancellation?).to be(false)
+        end
+      end
+
+      context 'at least one step ignores cancellation' do
+        it 'ignores cancellation' do
+          allow(errand_step1).to receive(:ignore_cancellation?).and_return(true)
+
+          expect(subject.ignore_cancellation?).to be(true)
+        end
       end
     end
   end