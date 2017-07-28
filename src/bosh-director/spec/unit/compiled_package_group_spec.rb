264764b3cefabfb5a2894173af6997d1a244ac36
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2017-07-28 10:09:04 -0700

    Revert removal of transitive compiled packages
    This reverts commits:
    
     * 03906a25f9a3c6d72d8d4967493eaaa5eaa331d3
     * 1fb0020ae0c1266a0e0d2903a004d39fd0920021
     * 3d32eff2477da9e1efcde55b49b8d0a530fbc685
    
    [#148638363](https://www.pivotaltracker.com/story/show/148638363)
    
    Signed-off-by: Danny Berger <dberger@pivotal.io>

--- src/bosh-director/spec/unit/compiled_package_group_spec.rb
+++ src/bosh-director/spec/unit/compiled_package_group_spec.rb
@@ -12,39 +12,25 @@ module Bosh::Director
     subject(:package_group) { CompiledPackageGroup.new(release_version, stemcell) }
 
     describe '#compiled_packages' do
-      let(:job) { Models::Template.make(release: release, package_names_json: package_names.to_json) }
-      let(:job2) { Models::Template.make(release: release, package_names_json: package_names.to_json) }
-      let(:package_names) { [package1.name, package2.name] }
       let(:package1) { Models::Package.make(release: release, dependency_set_json: ['pkg-2'].to_json) }
       let(:package2) { Models::Package.make(name: 'pkg-2', version: '2', release: release) }
 
       let!(:compiled_package1) { Models::CompiledPackage.make(package: package1, stemcell_os: stemcell.os, stemcell_version: stemcell.version, dependency_key: '[["pkg-2","2"]]') }
-      let!(:compiled_package2) { Models::CompiledPackage.make(package: package2, stemcell_os: stemcell.os, stemcell_version: stemcell.version) }
 
       before do
-        release_version.add_template(job)
-        release_version.add_template(job2)
         release_version.add_package(package1)
         release_version.add_package(package2)
       end
 
-      it 'returns list of packages for the jobs in the given release version and stemcell' do
-        expect(package_group.compiled_packages).to eq([compiled_package1, compiled_package2])
+      it 'returns list of packages for the given release version and stemcell' do
+        expect(package_group.compiled_packages).to eq([compiled_package1])
       end
 
       it 'only queries database once' do
         allow(Models::CompiledPackage).to receive(:[]).and_call_original
         package_group.compiled_packages
         package_group.compiled_packages
-        expect(Models::CompiledPackage).to have_received(:[]).exactly(4).times
-      end
-
-      context 'when jobs include packages with transitive dependencies' do
-        let(:package_names) { [package1.name] }
-
-        it 'does not include second and higher order dependencies' do
-          expect(package_group.compiled_packages).to eq([compiled_package1])
-        end
+        expect(Models::CompiledPackage).to have_received(:[]).twice
       end
     end
 
