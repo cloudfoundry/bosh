227199c07ef4b4394d2d3ff65b46e9148aed1290
Yulia Gaponenko <yulia.gaponenko1@de.ibm.com>
2017-07-30 10:55:24 +0200

    Fix case when no agent_ids for deplyoment are available, but vms should be recreated
    [#149000461](https://www.pivotaltracker.com/story/show/149000461)

--- src/spec/gocli/integration/vm_delete_spec.rb
+++ src/spec/gocli/integration/vm_delete_spec.rb
@@ -6,19 +6,24 @@ describe 'vm delete', type: :integration do
   with_reset_hm_before_each
 
   it 'delete the vm by its vm_cid' do
-    deploy_from_scratch
-
     #reference to instance
+
+    manifest_hash = Bosh::Spec::Deployments.simple_manifest
+    cloud_config_hash = Bosh::Spec::Deployments.simple_cloud_config
+
+    manifest_hash['jobs'].first['instances'] = 1
+    deploy_from_scratch(cloud_config_hash: cloud_config_hash, manifest_hash: manifest_hash)
+
     instance = director.instances.first
     expect(current_sandbox.cpi.has_vm(instance.vm_cid)).to be_truthy
     bosh_runner.run("delete-vm #{instance.vm_cid}", deployment_name: 'simple')
     expect(current_sandbox.cpi.has_vm(instance.vm_cid)).not_to be_truthy
-    expect(director.vms.count).to eq(2)
+    expect(director.vms.count).to eq(0)
 
     #wait for resurrection
     resurrected_instance = director.wait_for_vm(instance.job_name, instance.index, 300, deployment_name: 'simple')
     expect(resurrected_instance.vm_cid).to_not eq(instance.vm_cid)
-    expect(director.vms.count).to eq(3)
+    expect(director.vms.count).to eq(1)
 
     #no reference to instance
     network ={'a' => {'ip' => '192.168.1.5', 'type' => 'dynamic'}}
