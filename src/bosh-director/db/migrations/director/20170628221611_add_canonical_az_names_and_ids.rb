032cf85a79f4a28d560d82ec8e01dc6e7bb55738
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2017-08-01 10:58:42 -0700

    Stop setting compiled_packages.dependency type to longtext
    * Was due to copy/paste error

--- src/bosh-director/db/migrations/director/20170628221611_add_canonical_az_names_and_ids.rb
+++ src/bosh-director/db/migrations/director/20170628221611_add_canonical_az_names_and_ids.rb
@@ -4,9 +4,5 @@ Sequel.migration do
       primary_key :id
       String :name, unique: true, null: false
     end
-
-    if [:mysql2, :mysql].include?(adapter_scheme)
-      set_column_type :compiled_packages, :dependency_key, 'longtext'
-    end
   end
 end
