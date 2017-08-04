cd1f355fad9845bc7394d273bbb4d89da263d33c
Danny Berger <dberger@pivotal.io>
2017-08-04 16:35:23 -0700

    Remove unneeded before_create hook
    [#147867865](https://www.pivotaltracker.com/story/show/147867865)
    
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

--- src/bosh-director/lib/bosh/director/models/vm.rb
+++ src/bosh-director/lib/bosh/director/models/vm.rb
@@ -2,8 +2,4 @@ module Bosh::Director::Models
   class Vm < Sequel::Model(Bosh::Director::Config.db)
     many_to_one :instance
   end
-
-  def before_create
-    self.created_at ||= Time.now
-  end
 end
--- src/bosh-director/spec/unit/models/vm_spec.rb
+++ src/bosh-director/spec/unit/models/vm_spec.rb
@@ -12,12 +12,5 @@ module Bosh::Director::Models
       expect(described_class.find(id: 1).instance).to eq(instance)
       expect(described_class.find(id: 2).instance).to eq(instance)
     end
-
-    describe '#before_create' do
-      it 'should set created_at' do
-        vm = described_class.make(instance_id: instance.id, id: 1)
-        expect(vm.created_at).not_to be_nil
-      end
-    end
   end
 end
