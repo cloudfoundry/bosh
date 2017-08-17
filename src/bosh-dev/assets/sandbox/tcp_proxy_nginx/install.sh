7557ba476b61c590dae05609206f8c65cbfdd4cd
Tom Viehman <tviehman@pivotal.io>
2017-08-17 16:52:33 -0700

    Bump nginx 1.9.6 -> 1.12.1 for integration test tcp proxy
    Now matches the version that we install in the bosh release
    
    [#150347118](https://www.pivotaltracker.com/story/show/150347118)

--- src/bosh-dev/assets/sandbox/tcp_proxy_nginx/install.sh
+++ src/bosh-dev/assets/sandbox/tcp_proxy_nginx/install.sh
@@ -1,15 +1,17 @@
 set -e -x
 
 echo "Extracting nginx..."
-tar xzvf nginx-1.9.6.tar.gz
+tar xzvf nginx-1.12.1.tar.gz
 
 echo "Building nginx..."
 
-pushd nginx-1.9.6
+pushd nginx-1.12.1
   ./configure \
     --prefix=${BOSH_INSTALL_TARGET} \
     --with-stream \
-    --without-http_rewrite_module
+    --without-http_rewrite_module \
+    --with-cc-opt="-I/usr/local/opt/openssl/include" \
+    --with-ld-opt="-L/usr/local/opt/openssl/lib"
 
   make
   make install
