9aaa5ae83f351e0b008ca01d790a52e80249ca1f
Bozhidar Lenchov <blenchov@pivotal.io>
2017-08-25 15:24:09 -0400

    Some formatting changes
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- src/bosh-director/lib/bosh/director/nats_client_cert_generator.rb
+++ src/bosh-director/lib/bosh/director/nats_client_cert_generator.rb
@@ -5,27 +5,30 @@ module Bosh::Director
       @logger = logger
 
       if Config.nats_client_ca_private_key_path.nil?
-        raise DeploymentGeneratorCAInvalid, "Client certificate generation error. Config for nats_client_ca_private_key_path is nil."
+        raise DeploymentGeneratorCAInvalid, 'Client certificate generation error. Config for nats_client_ca_private_key_path is nil.'
       end
+
       if Config.nats_client_ca_certificate_path.nil?
-        raise DeploymentGeneratorCAInvalid, "Client certificate generation error. Config for nats_client_ca_certificate_path is nil."
+        raise DeploymentGeneratorCAInvalid, 'Client certificate generation error. Config for nats_client_ca_certificate_path is nil.'
       end
+
       if !File.exists?(Config.nats_client_ca_private_key_path)
-        raise DeploymentGeneratorCAInvalid, "Client certificate generation error. Config for nats_client_ca_private_key_path is not found."
+        raise DeploymentGeneratorCAInvalid, 'Client certificate generation error. Config for nats_client_ca_private_key_path is not found.'
       end
+
       if !File.exists?(Config.nats_client_ca_certificate_path)
-        raise DeploymentGeneratorCAInvalid, "Client certificate generation error. Config for nats_client_ca_certificate_path is not found."
+        raise DeploymentGeneratorCAInvalid, 'Client certificate generation error. Config for nats_client_ca_certificate_path is not found.'
       end
 
       @root_ca = load_cert(Config.nats_client_ca_certificate_path)
-      @root_key = load_key( Config.nats_client_ca_private_key_path)
+      @root_key = load_key(Config.nats_client_ca_private_key_path)
+
       if !verify(@root_ca, @root_key)
-        raise DeploymentGeneratorCAInvalid, "Configured nats_client_ca_certificate_path points to an invalid certificate."
+        raise DeploymentGeneratorCAInvalid, 'Configured nats_client_ca_certificate_path points to an invalid certificate.'
       end
     end
 
     def generate_nats_client_certificate(common_name)
-
       key = OpenSSL::PKey::RSA.new 2048
       cert = OpenSSL::X509::Certificate.new
       cert.version = 2
@@ -40,18 +43,18 @@ module Bosh::Director
       ef = OpenSSL::X509::ExtensionFactory.new
       ef.subject_certificate = cert
       ef.issuer_certificate = @root_ca
-      cert.add_extension(ef.create_extension("keyUsage","digitalSignature", true))
-      cert.add_extension(ef.create_extension("basicConstraints","CA:false",true))
-      cert.add_extension(ef.create_extension("extendedKeyUsage","clientAuth",true))
+      cert.add_extension(ef.create_extension('keyUsage', 'digitalSignature', true))
+      cert.add_extension(ef.create_extension('basicConstraints', 'CA:false', true))
+      cert.add_extension(ef.create_extension('extendedKeyUsage', 'clientAuth', true))
       cert.sign(@root_key, OpenSSL::Digest::SHA256.new)
 
-      { :cert => cert, :key => key, :ca_key => @root_key.public_key }
+      {:cert => cert, :key => key, :ca_key => @root_key.public_key}
     end
 
-  private
+    private
 
     def load_cert(path)
-      cert = OpenSSL::X509::Certificate.new(File.read(path))
+      OpenSSL::X509::Certificate.new(File.read(path))
     end
 
     def load_key(path)
