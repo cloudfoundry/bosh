2dccee81703c326a1ae502e44a9a861191e54759
Tom Viehman <tviehman@pivotal.io>
2017-08-29 13:53:45 -0700

    Update logrotate configuration to rotate /var/log/wtmp and /var/log/btmp
    Patched from: cloudfoundry/bosh-linux-stemcell-builder:b88f45d63f9ac533cb41b98edef91d82167cc817
    
    [#150111353](https://www.pivotaltracker.com/story/show/150111353)
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- src/stemcell_builder/stages/logrotate_config/apply.sh
+++ src/stemcell_builder/stages/logrotate_config/apply.sh
@@ -5,6 +5,17 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
+# The logrotate.conf supplied by the default image is about to be stomped.
+# Make sure it hasn't changed. If it has changed, the contents of the new file should
+# evaluated to see if the replacement file should be updated.
+if [ "$(get_os_type)" == "centos" ]; then
+  echo "3129afc4edd2483030c6be7e7e7e1a7bfb3f110a  $chroot/etc/logrotate.conf" | sha1sum -c
+  cp $assets_dir/centos-logrotate.conf $chroot/etc/logrotate.conf
+elif [ "$(get_os_type)" == "ubuntu" ]; then
+  echo "7e52df7373b42a36b2bdde9bc88315e828cdc61e  $chroot/etc/logrotate.conf" | sha1sum -c
+  cp $assets_dir/ubuntu-logrotate.conf $chroot/etc/logrotate.conf
+fi
+
 mv $chroot/etc/cron.daily/logrotate $chroot/usr/bin/logrotate-cron
 echo '0,15,30,45 * * * * root /usr/bin/logrotate-cron' > $chroot/etc/cron.d/logrotate
 
