3299bc483311343252c129d99c5f629ab1924d20
J Evelyn Quintana <jquintana@pivotal.io>
2017-09-06 11:28:07 -0700

    add integration test for duplicate networks
    - unable to reproduce reported issue
    - test asserts that behavior is consistently correct
    
    [#150804533](https://www.pivotaltracker.com/story/show/150804533)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- src/spec/gocli/integration/network_configuration_spec.rb
+++ src/spec/gocli/integration/network_configuration_spec.rb
@@ -244,6 +244,27 @@ describe 'network configuration', type: :integration do
           expect(first_instance_ips.length).to eq 2
         end
       end
+
+      context 'when the manifest only refers to one of the networks' do
+        it 'does not recreate VM' do
+          deploy_from_scratch(cloud_config_hash: cloud_config_hash, manifest_hash: manifest_hash)
+          agent_id = director.instances.first.agent_id
+          output = table(bosh_runner.run('vms', deployment_name: 'simple', json: true))
+          expect(output.length).to eq 1
+          ips = output[0]['ips'].split("\n")
+          expect(ips.length).to eq 1
+
+          deploy_simple_manifest(manifest_hash: manifest_hash)
+          expect(director.instances.map(&:agent_id)).to eq([agent_id])
+
+          output = table(bosh_runner.run('vms', deployment_name: 'simple', json: true))
+          expect(output.length).to eq 1
+
+          new_ips = output[0]['ips'].split("\n")
+          expect(new_ips.length).to eq 1
+          expect(new_ips[0]).to eq ips[0]
+        end
+      end
     end
   end
 
