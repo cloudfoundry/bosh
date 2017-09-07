0ab77c2edd2915d530324e42d98534034f764685
Beyhan Veli <beyhan.veli@sap.com>
2017-09-07 16:59:48 +0200

    Redact basic auth credentials in release URLs
    [#150527298](https://www.pivotaltracker.com/story/show/150527298)

--- src/bosh-director/lib/bosh/director/manifest/redactor.rb
+++ src/bosh-director/lib/bosh/director/manifest/redactor.rb
@@ -5,24 +5,29 @@ module Bosh::Director
       properties
       bosh
     )
+    REDACT_STRING = '<redacted>'
 
     # redacts properties from ruby object to avoid having to use a regex to redact properties from diff output
     # please do not use regexes for diffing/redacting
     def redact_properties!(object)
+      redact_known_key_properties!(object)
+      redact_credentials_in_release_urls!(object)
+      object
+    end
+
+    private
+
+    def redact_known_key_properties!(object)
       if iterable?(object)
         object.each{ |item|
           if hash?(object) && key_to_redact?(item_key(item)) && hash?(item_value(item))
             redact_values!(item_value(item))
           else
-            redact_properties!(item.respond_to?(:last) ? item_value(item) : item)
+            redact_known_key_properties!(item.respond_to?(:last) ? item_value(item) : item)
           end
         }
       end
-
-      object
     end
-
-    private
 
     def redact_values!(object)
       if hash?(object)
@@ -30,7 +35,7 @@ module Bosh::Director
           if iterable?(object[key])
             redact_values!(object[key])
           else
-            object[key] = '<redacted>'
+            object[key] = REDACT_STRING
           end
         }
       elsif array?(object)
@@ -38,10 +43,28 @@ module Bosh::Director
           if iterable?(object[i])
             redact_values!(object[i])
           else
-            object[i] = '<redacted>'
+            object[i] = REDACT_STRING
           end
         }
       end
+    end
+
+    def redact_credentials_in_release_urls!(object)
+      releases = hash?(object) ? object['releases'] : nil
+      return if releases.nil? || !array?(releases)
+
+      releases.each_index{ |i|
+        release_url_value = releases[i]['url']
+        next if release_url_value.nil?
+        begin
+          release_uri = URI.parse(release_url_value)
+
+          release_url_value.sub!(release_uri.user, REDACT_STRING) if release_uri.user
+          release_url_value.sub!(release_uri.password, REDACT_STRING) if release_uri.password
+        rescue URI::Error
+          # ignore bad urls
+        end
+      }
     end
 
     def hash?(obj)
--- src/bosh-director/spec/unit/manifest/redactor_spec.rb
+++ src/bosh-director/spec/unit/manifest/redactor_spec.rb
@@ -141,5 +141,154 @@ module Bosh::Director
         expect(redactor.redact_properties!(manifest_obj)).to eq(manifest_obj)
       end
     end
+
+    context 'redact release urls' do
+      context 'when credentials in release urls' do
+        it 'redacts the credentials' do
+          manifest_obj = {
+              'name' => 'test_name',
+              'uuid' => '12324234234234234234',
+              'env' => {
+                  'bosh' => {
+                      'one' => [1, 2, {'three' => 3}],
+                      'two' => 2,
+                      'three' => 3
+                  },
+                  'c' => 'dont-redact-me',
+                  'e' => 'i-am-not-secret'
+              },
+              'releases' => [
+                  {
+                      'name' => 'bosh',
+                      'version' => 263,
+                      'url' => 'https://user@bosh-foo.com',
+                      'sha1' => 'bosh-sha1-value'
+                  },
+                  {
+                      'name' => 'cpi',
+                      'version' => 33,
+                      'url' => 'https://user:password@cpi-foo.com',
+                      'sha1' => 'sha1-value'
+                  }
+              ]
+          }
+
+          expect(redactor.redact_properties!(manifest_obj)).to eq({
+              'name' => 'test_name',
+              'uuid' => '12324234234234234234',
+              'env' => {
+                  'bosh' => {
+                      'one' => ['<redacted>', '<redacted>', {'three' => '<redacted>'}],
+                      'two' => '<redacted>',
+                      'three' => '<redacted>'
+                  },
+                  'c' => 'dont-redact-me',
+                  'e' => 'i-am-not-secret'
+              },
+              "releases"=> [
+                  {
+                      'name' => 'bosh',
+                      'version' => 263,
+                      'url' => 'https://<redacted>@bosh-foo.com',
+                      'sha1' => 'bosh-sha1-value'
+                  },
+                  {
+                      'name' => 'cpi',
+                      'version' => 33,
+                      'url' => 'https://<redacted>:<redacted>@cpi-foo.com',
+                      'sha1' => 'sha1-value'
+                  }
+              ]
+          })
+        end
+      end
+      context 'when one release has no url' do
+        it 'redacts all other release urls' do
+          manifest_obj = {
+              'releases' => [
+                  {
+                      'name' => 'bosh',
+                      'version' => 263,
+                  },
+                  {
+                      'name' => 'cpi',
+                      'version' => 33,
+                      'url' => 'https://user:password@cpi-foo.com',
+                      'sha1' => 'sha1-value'
+                  }
+              ]
+          }
+
+          expect(redactor.redact_properties!(manifest_obj)).to eq({
+          'releases' => [
+                {
+                    'name' => 'bosh',
+                    'version' => 263,
+                },
+                {
+                    'name' => 'cpi',
+                    'version' => 33,
+                    'url' => 'https://<redacted>:<redacted>@cpi-foo.com',
+                    'sha1' => 'sha1-value'
+                }
+            ]
+          })
+        end
+      end
+
+      context 'when one release has a malformed url' do
+        it 'redacts all other releases' do
+          manifest_obj = {
+              'releases' => [
+                  {
+                      'name' => 'cpi',
+                      'version' => 33,
+                      'url' => 'https:// user:password@cpi-foo.com',
+                      'sha1' => 'sha1-value'
+                  },
+                  {
+                      'name' => 'bosh',
+                      'version' => 263,
+                      'url' => 'https://user:password@bosh-foo.com',
+                      'sha1' => 'bosh-sha1-value'
+                  }
+              ]
+          }
+
+          expect(redactor.redact_properties!(manifest_obj)).to eq({
+              'releases' => [
+                  {
+                      'name' => 'cpi',
+                      'version' => 33,
+                      'url' => 'https:// user:password@cpi-foo.com',
+                      'sha1' => 'sha1-value'
+                  },
+                  {
+                      'name' => 'bosh',
+                      'version' => 263,
+                      'url' => 'https://<redacted>:<redacted>@bosh-foo.com',
+                      'sha1' => 'bosh-sha1-value'
+                  }
+              ]
+          })
+        end
+      end
+
+      context 'when releases is malformed' do
+        it 'does not fail' do
+          manifest_obj = { 'releases' => 'malformed' }
+
+          expect(redactor.redact_properties!(manifest_obj)).to eq({ 'releases' => 'malformed' })
+        end
+      end
+
+      context 'when releases is an array' do
+        it 'does not fail' do
+          manifest_obj = ['releases' , 'malformed' ]
+
+          expect(redactor.redact_properties!(manifest_obj)).to eq(['releases' , 'malformed' ])
+        end
+      end
+    end
   end
 end
