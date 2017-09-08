c17f4bf52fac9c3f8c7f6a77c5cf46cb7bff9a9c
Tanzeeb Khalili <tkhalili@pivotal.io>
2017-09-08 11:19:53 -0400

    Use semver-ish versioning for gnatsd binary because s3-resource requires it
    [#150714777](https://www.pivotaltracker.com/story/show/150714777)
    
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- ci/pipelines/gnatsd/tasks/build.sh
+++ ci/pipelines/gnatsd/tasks/build.sh
@@ -11,14 +11,15 @@ cd gopath/src/github.com/nats-io/gnatsd
 
 out="${base}/compiled-${GOOS}"
 
+semver=`grep "VERSION =" /tmp/const.go | cut -d\" -f2`
 timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 git_rev=`git rev-parse --short HEAD`
 
-version="${git_rev}-${timestamp}"
+version="${semver}-${git_rev}-${timestamp}"
 
 filename="gnatsd-${version}-${GOOS}-${GOARCH}"
 
 echo "building ${filename} with version ${version}"
-sed -i "s/VERSION = \"\(.*\)\"/VERSION = \"\1+${version}\"/" server/const.go
+sed -i "s/VERSION = \".*\"/VERSION = \"${version}\"/" server/const.go
 
 go build -o ${out}/${filename} github.com/nats-io/gnatsd
