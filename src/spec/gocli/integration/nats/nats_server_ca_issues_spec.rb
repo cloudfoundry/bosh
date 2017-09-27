ec467dee2ab5c7d0546f3331850c1f0f1a51e9b4
Jamil Shamy <jshamy@pivotal.io>
2017-09-27 11:32:18 -0400

    Minimize run time for NATs expected failure test
    [#150858517](https://www.pivotaltracker.com/story/show/150858517)
    
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- src/spec/gocli/integration/nats/nats_server_ca_issues_spec.rb
+++ src/spec/gocli/integration/nats/nats_server_ca_issues_spec.rb
@@ -5,12 +5,12 @@ describe 'using director with nats server', type: :integration do
     with_reset_sandbox_before_each(with_incorrect_nats_server_ca: true)
 
     it 'throws certificate validator error' do
-      manifest_hash = Bosh::Spec::Deployments.simple_manifest
+      # This test does not upload the specific release intentionally to force a failure
+      upload_cloud_config(cloud_config_hash: Bosh::Spec::Deployments.simple_cloud_config)
 
-      _, exit_code = deploy_from_scratch(manifest_hash: manifest_hash, failure_expected: true, return_exit_code: true)
-      expect(exit_code).to_not eq(0)
+      output = deploy_simple_manifest(manifest_hash: Bosh::Spec::Deployments.simple_manifest, no_track: true)
+      task_id = Bosh::Spec::OutputParser.new(output).task_id('*')
 
-      task_id = bosh_runner.get_most_recent_task_id
       debug_output = bosh_runner.run("task #{task_id} --debug", failure_expected: true)
       expect(debug_output).to include('NATS client error: TLS Verification failed checking issuer based on CA')
     end
