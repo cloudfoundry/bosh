cef1c328faccafbc3219312a2af5365a5854a39b
Bozhidar Lenchov <blenchov@pivotal.io>
2017-09-29 15:06:22 -0400

    Fix integration test.
    [#150858517](https://www.pivotaltracker.com/story/show/150858517)
    
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- src/spec/gocli/integration/nats/nats_server_spec.rb
+++ src/spec/gocli/integration/nats/nats_server_spec.rb
@@ -76,7 +76,7 @@ describe 'nats server', type: :integration do
     with_reset_sandbox_before_each
     context 'and connecting agent is legacy' do
       it 'should fail the deployment' do
-        output = deploy_from_scratch(manifest_hash: manifest_hash, cloud_config_hash: cloud_config, failure_expected: true)
+        output = deploy_from_scratch(manifest_hash: manifest_hash, cloud_config_hash: cloud_config_to_enable_legacy_agent, failure_expected: true)
         expect(output).to match(/Timed out pinging to \b.+\b after \b.+\b seconds/)
       end
     end
