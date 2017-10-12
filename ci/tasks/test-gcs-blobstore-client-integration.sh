8e5a0f9f4346e2a57a275a36f195fd933d19dc92
Joshua Aresty <joshua.aresty@emc.com>
2017-10-12 13:57:42 -0700

    Simplify the bucket setup to make it work better
    [#151930831](https://www.pivotaltracker.com/story/show/151930831)
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- ci/tasks/test-gcs-blobstore-client-integration.sh
+++ ci/tasks/test-gcs-blobstore-client-integration.sh
@@ -17,13 +17,7 @@ popd
 
 function clean_up_bucket {
   local bucket=$1
-  gsutil rm gs://${bucket_name}/public # This may fail, which is fine.
-  gsutil rb "gs://${bucket}"
-}
-
-function clean_up {
-  local bucket=$1
-  clean_up_bucket ${bucket}
+  gsutil rm -rf gs://${bucket_name}/
 }
 
 gcloud config set project $google_project
@@ -36,29 +30,22 @@ export GCS_SERVICE_ACCOUNT_KEY=$google_json_key_data
 pushd bosh-src/src
   bundle install
   pushd bosh-director
-
-    # Create bucket in US region
     bucket_name="bosh-blobstore-bucket-$RANDOM"
     echo -n "foobar" > public
     gsutil mb -c MULTI_REGIONAL -l us gs://${bucket_name}
-    gsutil iam ch allUsers:objectViewer gs://${bucket_name}
-    gsutil iam ch allUsers:legacyObjectReader gs://${bucket_name}
-		gsutil iam ch allUsers:legacyBucketReader gs://${bucket_name}
-		echo "waiting for IAM to propagate" && \
-		until curl -s \
-			https://storage.googleapis.com/${bucket_name}/non-existent \
-			| grep -q "NoSuchKey"; do sleep 1; done; \
-    trap 'clean_up_bucket ${bucket_name}; exit 1' ERR
+
+    trap 'clean_up_bucket ${bucket_name}; exit 1' EXIT
+
+    gsutil acl ch -u allUsers:R gs://${bucket_name}
     gsutil cp public gs://${bucket_name}/public
-    retry_command "gsutil acl ch -r -u AllUsers:R gs://${bucket_name}/public"
-    trap 'clean_up ${bucket_name}; exit 1' ERR
+
+    echo "waiting for IAM to propagate" && \
+      until curl -s \
+        https://storage.googleapis.com/${bucket_name}/non-existent \
+          | grep -q "NoSuchKey"; do sleep 1; done; \
 
     export GCS_BUCKET_NAME=${bucket_name}
 
     bundle exec rspec spec/functional/gcs_spec.rb --tag general_gcs
-
-    trap - ERR
-    clean_up ${bucket_name}
-
   popd
 popd
