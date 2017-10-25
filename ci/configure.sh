26e6fa19775b6af49c43fefbbe7811bff19ed521
Jamil Shamy <jshamy@pivotal.io>
2017-10-25 17:10:30 -0400

    Remove non-existing note from the CI configuration script
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,5 +5,4 @@ set -eu
 fly -t production set-pipeline -p bosh:263.x \
     -c ci/pipeline.yml \
     -l <(lpass show -G "bosh concourse secrets" --notes) \
-    -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
-    -l <(lpass show --note "bosh concourse 263.x secrets")
+    -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
