075916f1513446411fa68ca927ae4214495f238d
Jamil Shamy <jshamy@pivotal.io>
2017-11-02 13:18:17 -0400

    NATs agent certificate generation does not work if the signing certificate is not a root CA
    [#152295888](https://www.pivotaltracker.com/story/show/152295888)
    
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- src/bosh-director/lib/bosh/director/errors.rb
+++ src/bosh-director/lib/bosh/director/errors.rb
@@ -238,7 +238,7 @@ module Bosh::Director
   DeploymentIgnoredInstancesModification = err(190020)
   DeploymentIgnoredInstancesDeletion = err(190021)
   DeploymentDuplicateVariableName = err(190022)
-  DeploymentGeneratorCAInvalid = err(190023)
+  DeploymentNATSClientCertificateGenerationError= err(190023)
 
   # DiskType
   DiskTypeInvalidDiskSize = err(200001)
