3314948790f951cf4c321041baa8ea3c4429a35e
Tom Viehman <tviehman@pivotal.io>
2017-11-03 12:15:22 -0700

    Update pipeline files to use 264.x branch
    [#152552698](https://www.pivotaltracker.com/story/show/152552698)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-fly -t production set-pipeline -p bosh \
+fly -t production set-pipeline -p bosh:264.x \
     -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
