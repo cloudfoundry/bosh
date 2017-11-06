8990e8b3a16c96be0de43a961cfa6a20536a1fa5
Danny Berger <dberger@pivotal.io>
2017-11-06 14:18:01 -0800

    Revert "Integration tests for bosh-cli vms --cloud-properties"
    It seems like we are not passing the `vm_resources` option to
    `simple_manifest_with_instance_groups_and_vm_resources`, but it also is
    not accounting for `vm_type` and `vm_resources` being mutually
    exclusive.
    
    Tests are currently failing...
    
         # https://main.bosh-ci.cf-app.com/teams/main/pipelines/bosh:cli/jobs/integration-2.3-postgres-gocli/builds/994
         Failure/Error: expect(vms.map { |vm| vm[:cloud_properties]}).to eq([cloud_props_from_dummy_cpi, cloud_props_from_dummy_cpi, cloud_props_from_dummy_cpi])
    
           expected: ["ephemeral_disk:\n  size: 10\ninstance_type: dummy", "ephemeral_disk:\n  size: 10\ninstance_type: dummy", "ephemeral_disk:\n  size: 10\ninstance_type: dummy"]
                got: ["", "", ""]
    
           (compared using ==)
    
           Diff:
           @@ -1,4 +1 @@
           -ephemeral_disk:\n  size: 10\ninstance_type: dummy
           -ephemeral_disk:\n  size: 10\ninstance_type: dummy
           -ephemeral_disk:\n  size: 10\ninstance_type: dummy
    
         # ./spec/gocli/integration/cli_vms_spec.rb:43:in `block (2 levels) in <top (required)>'
    
    This reverts commit a1cb7aa5f00512f52fdf3e41a3ae942cea07bca3.
    
    [#151157765](https://www.pivotaltracker.com/story/show/151157765)
    
    Signed-off-by: Kai Hofstetter <kai.hofstetter@sap.com>

--- src/spec/support/new_deployments.rb
+++ src/spec/support/new_deployments.rb
@@ -114,11 +114,6 @@ module Bosh::Spec
         instance_group_hash['properties'] = opts[:properties]
       end
 
-      if opts.has_key?(:vm_resources)
-        instance_group_hash.delete(:vm_type)
-        instance_group_hash['vm_resources'] = opts[:vm_resources]
-      end
-
       instance_group_hash
     end
 
@@ -182,16 +177,6 @@ module Bosh::Spec
     def self.simple_manifest_with_instance_groups
       test_release_manifest_with_stemcell.merge({
         'instance_groups' => [simple_instance_group]
-      })
-    end
-
-    def self.simple_manifest_with_instance_groups_and_vm_resources
-      test_release_manifest_with_stemcell.merge({
-        'instance_groups' => [simple_instance_group({
-          'cpu' => 2,
-          'ram' => 1024,
-          'ephemeral_disk_size' => 10
-        })]
       })
     end
 
