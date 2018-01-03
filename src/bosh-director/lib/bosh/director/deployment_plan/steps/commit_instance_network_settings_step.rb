f0e413f0dc6e9e5e28840ced16f87177119d19a0
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2018-01-03 12:13:14 -0800

    Release obsolete network plans in network settings step
    [#139870257](https://www.pivotaltracker.com/story/show/139870257)
    
    Signed-off-by: Chris Dutra, cdutra <c.dutra, cdutra@pivotal.io>

--- src/bosh-director/lib/bosh/director/deployment_plan/steps/commit_instance_network_settings_step.rb
+++ src/bosh-director/lib/bosh/director/deployment_plan/steps/commit_instance_network_settings_step.rb
@@ -1,8 +1,18 @@
 module Bosh::Director
   module DeploymentPlan::Steps
     class CommitInstanceNetworkSettingsStep
+      def initialize(ip_provider)
+        @ip_provider = ip_provider
+      end
+
       def perform(report)
         report.network_plans.select(&:desired?).each { |network_plan| network_plan.existing = true }
+
+        report.network_plans.select(&:obsolete?).each do |network_plan|
+          reservation = network_plan.reservation
+          @ip_provider.release(reservation)
+        end
+        report.network_plans.delete_if(&:obsolete?)
       end
     end
   end
--- src/bosh-director/spec/unit/deployment_plan/steps/commit_instance_network_settings_step_spec.rb
+++ src/bosh-director/spec/unit/deployment_plan/steps/commit_instance_network_settings_step_spec.rb
@@ -4,26 +4,30 @@ module Bosh::Director
   module DeploymentPlan
     module Steps
       describe CommitInstanceNetworkSettingsStep do
-        subject(:step) { described_class.new }
+        subject(:step) { described_class.new(ip_provider) }
         let(:report) { Stages::Report.new }
         let(:network_plans) do
           [
             NetworkPlanner::Plan.new(reservation: nil, existing: true),
-            NetworkPlanner::Plan.new(reservation: nil, obsolete: true),
+            NetworkPlanner::Plan.new(reservation: obsolete_reservation, obsolete: true),
             NetworkPlanner::Plan.new(reservation: nil),
           ]
         end
+        let(:obsolete_reservation) { double('reservation') }
+        let(:ip_provider) { instance_double(IpProvider) }
 
         before { report.network_plans = network_plans }
 
         describe '#perform' do
-          it 'marks the desired plans as existing' do
+          it 'marks the desired plans as existing and releases obsolete plans' do
+            expect(ip_provider).to receive(:release).with(obsolete_reservation)
+
             step.perform(report)
-            expect(report.network_plans.length).to eq(3)
+
+            expect(report.network_plans.length).to eq(2)
 
             expect(report.network_plans[0].existing?).to eq(true)
-            expect(report.network_plans[1].obsolete?).to eq(true)
-            expect(report.network_plans[2].existing?).to eq(true)
+            expect(report.network_plans[1].existing?).to eq(true)
           end
         end
       end
