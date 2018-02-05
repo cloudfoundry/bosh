da059a48cb35e8ee9e8851deef7791fe9c2a8c33
Joshua Aresty <joshua.aresty@emc.com>
2018-02-05 13:41:04 -0800

    Use a slack attachment instead of a build-metadata resource

--- ci/tasks/prep-slack-alert.sh
+++ ci/tasks/prep-slack-alert.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-echo '<!here>: $(cat build-metadata/build-pipeline-name)/$(cat build-metadata/build-job-name) failed in
-<https://main.bosh-ci.cf-app.com/teams/main/pipelines/$(cat build-metadata/build-pipeline-name)/jobs/$(cat build-metadata/build-job-name)/builds/$(cat build-metadata/build-name)|build $(cat build-metadata/build-name)>' > slack-message/message
+echo '<!here>: Build failed on with latest commit:' > slack-message/message
 
 pushd bosh-src
   echo '```' >> ../slack-message/message
