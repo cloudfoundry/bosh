15f7e82f1abb6470afbc0c1e8c6f3d6e856ea641
Luan Santos <lsantos@pivotal.io>
2018-02-06 06:53:40 -0800

    Fix mysql column format
    [#154157545](https://www.pivotaltracker.com/story/show/154157545)
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- src/bosh-director/db/migrations/director/20180203000738_create_orphaned_vms.rb
+++ src/bosh-director/db/migrations/director/20180203000738_create_orphaned_vms.rb
@@ -1,5 +1,7 @@
 Sequel.migration do
   up do
+    adapter_scheme = self.adapter_scheme
+
     alter_table :ip_addresses do
       add_column :orphaned_vm_id, Integer
     end
@@ -9,7 +11,11 @@ Sequel.migration do
       table.String :cid, null: false
       table.Integer :instance_id, null: false
       table.String :availability_zone
-      table.String :cloud_properties, text: true
+      if %i[mysql2 mysql].include?(adapter_scheme)
+        table.longtext :cloud_properties
+      else
+        table.text :cloud_properties
+      end
       table.String :cpi
       table.Time :orphaned_at, null: false
     end
