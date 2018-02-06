0f9fb07b11ec083d5a24f0a79bf6a921846ae97b
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2018-02-06 14:51:08 -0800

    Borrow prep-slack-alert from kubo
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/tasks/prep-slack-alert.sh
+++ ci/tasks/prep-slack-alert.sh
@@ -1,11 +1,72 @@
-#!/usr/bin/env bash
+set -exu -o pipefail
 
-set -e
+ROOT="$(pwd)"
 
-echo '<!here>: Build failed on with latest commit:' > slack-message/message
+SLACK_ATTACHMENT_TEMPLATE='{
+    "color": "#ff0000",
+    "title": $title,
+    "mrkdwn_in": ["fields"],
+    "fields": [
+        {"title": "Author", "short": true, "value": $author},
+        {"title": "Committer", "short": true, "value": $committer}
+    ]
+}'
 
-pushd bosh-src
-  echo '```' >> ../slack-message/message
-  git log -1 --format=fuller >> ../slack-message/message
-  echo '```' >> ../slack-message/message
-popd
+function main() {
+  local attachments="[]"
+  for repo in ${ROOT}/bosh-src; do
+    local attachment="$(jq -n \
+      --arg title "$(basename "${repo}") $(get_commit_link "${repo}") - $(get_commit_message "${repo}") ($(get_commit_date "${repo}"))" \
+      --arg author "$(get_author_name "${repo}")" \
+      --arg committer "$(get_committer_name "${repo}")" \
+      "${SLACK_ATTACHMENT_TEMPLATE}")"
+
+    attachments="$(echo "${attachments}" | jq \
+        --argjson attachment "${attachment}" \
+        '. += [$attachment]')"
+  done
+
+  echo "${attachments}" \
+    > "${ROOT}/slack-notification/attachments"
+}
+
+function get_repo_ref() {
+  local repo="${1}"
+  git -C "${repo}" show -s --format=%h "$(cat "${repo}/.git/ref")"
+}
+
+function get_commit_link() {
+  local repo="${1}"
+  local ref="$(get_repo_ref ${repo})"
+  echo "<https://$(git -C "${repo}" remote get-url origin | cut -d@ -f2 | sed -e 's|:|/|' -e 's|.git$||')/commit/${ref}|${ref}>"
+}
+
+function get_commit_date() {
+  local repo="${1}"
+  git -C "${repo}" show -s --format="%cr" "$(get_repo_ref "${repo}")"
+}
+
+function get_commit_message() {
+  local repo="${1}"
+  git -C "${repo}" show -s --format="%s" "$(get_repo_ref "${repo}")"
+}
+
+function get_author_name() {
+  local repo="${1}"
+  local author=$(git -C "${repo}" show -s --format="%ae" "$(get_repo_ref "${repo}")")
+
+  get_slacker_name "${author}"
+}
+
+function get_committer_name() {
+  local repo="${1}"
+  local committer=$(git -C "${repo}" show -s --format="%ce" "$(get_repo_ref "${repo}")")
+
+  get_slacker_name "${committer}"
+}
+
+function get_slacker_name() {
+  echo "<@here>"
+}
+
+main
