ffcd723e597ce4def14625417af9fb990e8bec90
Danny Berger <dberger@pivotal.io>
2018-02-14 14:50:09 -0800

    Refactor log filters to be referenced separately
    [#154988907](https://www.pivotaltracker.com/story/show/154988907)

--- src/bosh_common/lib/common/logging/filters.rb
+++ src/bosh_common/lib/common/logging/filters.rb
@@ -1,14 +1,19 @@
 require 'common/logging/regex_filter'
 
 module Bosh::Common::Logging
-  def self.default_filters
-    [
-      Bosh::Common::Logging::RegexFilter.new(
-        [
-          { /^\(\d+\.\d+s\) \(conn: \d+\) SELECT NULL$/ => nil },
-          { /^(\(\d+\.\d+s\) \(conn: \d+\) (INSERT INTO "[^"]+"|UPDATE "[^"]+"|DELETE FROM "[^"]+")).+/m => '\1 <redacted>' },
-        ],
-      ),
-    ]
+  def self.null_query_filter
+    Bosh::Common::Logging::RegexFilter.new(
+      [
+        { /^\(\d+\.\d+s\) \(conn: \d+\) SELECT NULL$/ => nil },
+      ],
+    )
+  end
+
+  def self.query_redaction_filter
+    Bosh::Common::Logging::RegexFilter.new(
+      [
+        { /^(\(\d+\.\d+s\) \(conn: \d+\) (INSERT INTO "[^"]+"|UPDATE "[^"]+"|DELETE FROM "[^"]+")).+/m => '\1 <redacted>' },
+      ],
+    )
   end
 end
--- src/bosh_common/spec/unit/logging/filters_spec.rb
+++ src/bosh_common/spec/unit/logging/filters_spec.rb
@@ -4,47 +4,55 @@ require 'common/logging/filters'
 require 'logging/log_event'
 
 describe Bosh::Common::Logging do
-  subject { described_class.default_filters[0] }
-
   let(:event) { Logging::LogEvent.new(nil, 100, event_data, false) }
 
-  describe 'select null' do
-    let(:event_data) { '(1.0001s) (conn: 123123123) SELECT NULL' }
+  describe '#null_query_filter' do
+    let(:subject) { described_class.null_query_filter }
 
-    it 'drops them' do
-      expect(subject.allow(event)).to eq(nil)
+    describe 'select null' do
+      let(:event_data) { '(1.0001s) (conn: 123123123) SELECT NULL' }
+
+      it 'drops them' do
+        expect(subject.allow(event)).to eq(nil)
+      end
     end
   end
 
-  describe 'insert into statements' do
-    let(:event_data) { '(1.001s) (conn: 123123) INSERT INTO "tablefoo" VALUES ("sensitive")' }
+  describe '#query_redaction_filter' do
+    let(:subject) { described_class.query_redaction_filter }
 
-    it 'redacts them' do
-      expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) INSERT INTO "tablefoo" <redacted>')
+    describe 'insert db queries' do
+      describe 'insert into statements' do
+        let(:event_data) { '(1.001s) (conn: 123123) INSERT INTO "tablefoo" VALUES ("sensitive")' }
+
+        it 'redacts them' do
+          expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) INSERT INTO "tablefoo" <redacted>')
+        end
+      end
+
+      describe 'multiline insert into statements' do
+        let(:event_data) { "(1.001s) (conn: 123123) INSERT INTO \"tablefoo\"\nVALUES (\"sensitive\")" }
+
+        it 'redacts them' do
+          expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) INSERT INTO "tablefoo" <redacted>')
+        end
+      end
     end
-  end
 
-  describe 'multiline insert into statements' do
-    let(:event_data) { "(1.001s) (conn: 123123) INSERT INTO \"tablefoo\"\nVALUES (\"sensitive\")" }
+    describe 'update db queries' do
+      let(:event_data) { '(1.001s) (conn: 123123) UPDATE "tablefoo" SET secret = "sensitive" WHERE secret = "c1oudc0w"' }
 
-    it 'redacts them' do
-      expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) INSERT INTO "tablefoo" <redacted>')
+      it 'redacts them' do
+        expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) UPDATE "tablefoo" <redacted>')
+      end
     end
-  end
 
-  describe 'update statements' do
-    let(:event_data) { '(1.001s) (conn: 123123) UPDATE "tablefoo" SET secret = "sensitive" WHERE secret = "c1oudc0w"' }
+    describe 'delete db queries' do
+      let(:event_data) { '(1.001s) (conn: 123123) DELETE FROM "tablefoo" WHERE secret = "sensitive"' }
 
-    it 'redacts them' do
-      expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) UPDATE "tablefoo" <redacted>')
-    end
-  end
-
-  describe 'delete statements' do
-    let(:event_data) { '(1.001s) (conn: 123123) DELETE FROM "tablefoo" WHERE secret = "sensitive"' }
-
-    it 'redacts them' do
-      expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) DELETE FROM "tablefoo" <redacted>')
+      it 'redacts them' do
+        expect(subject.allow(event).data).to eq('(1.001s) (conn: 123123) DELETE FROM "tablefoo" <redacted>')
+      end
     end
   end
 end
