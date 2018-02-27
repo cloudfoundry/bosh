30ae7cce992339f92eac1c09092d7867cc5bec74
Joshua Aresty <joshua.aresty@emc.com>
2018-02-27 10:34:10 -0800

    Log to sandbox log instead of STDOUT
    [#155538073](https://www.pivotaltracker.com/story/show/155538073)
    
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- src/bosh-dev/lib/bosh/dev/sandbox/main.rb
+++ src/bosh-dev/lib/bosh/dev/sandbox/main.rb
@@ -71,26 +71,25 @@ module Bosh::Dev::Sandbox
       }
       db_opts[:password] = ENV['DB_PASSWORD'] if ENV['DB_PASSWORD']
 
-      logger = Logging.logger(STDOUT)
-      logger.level = ENV.fetch('LOG_LEVEL', 'ERROR')
-
       new(
         db_opts,
         ENV['DEBUG'],
         ENV['TEST_ENV_NUMBER'].to_i,
-        logger,
       )
     end
 
-    def initialize(db_opts, debug, test_env_number, logger)
+    def initialize(db_opts, debug, test_env_number)
       @debug = debug
-      @logger = logger
       @name = SecureRandom.uuid.gsub('-', '')
 
       @port_provider = PortProvider.new(test_env_number)
 
       @logs_path = sandbox_path('logs')
       FileUtils.mkdir_p(@logs_path)
+
+      @sandbox_log_file = File.open(sandbox_path('sandbox.log'), 'w+')
+      @logger = Logging.logger(@sandbox_log_file)
+      @logger.level = ENV.fetch('LOG_LEVEL', 'DEBUG')
 
       @dns_db_path = sandbox_path('director-dns.sqlite')
       @task_logs_dir = sandbox_path('boshdir/tasks')
@@ -267,7 +266,9 @@ module Bosh::Dev::Sandbox
       @config_server_service.stop
 
       @database.drop_db
-      @database_proxy && @database_proxy.stop
+      @database_proxy&.stop
+
+      @sandbox_log_file.close
 
       FileUtils.rm_f(dns_db_path)
       FileUtils.rm_rf(agent_tmp_path)
