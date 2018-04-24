776b72aab62e8e85401511a1b60b392f85df03ee
Michael Xu <mxu@pivotal.io>
2018-04-24 13:51:42 -0700

    Add BRATS to ensure scheduler job properly runs
    [#157060206](https://www.pivotaltracker.com/story/show/157060206)
    
    Co-authored-by: Danny Berger <dberger@pivotal.io>

---
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/scheduler_job_test.go
@@ -0,0 +1,24 @@
+package brats_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Scheduled jobs", func() {
+	BeforeEach(func() {
+		startInnerBosh("-o", assetPath("ops-frequent-scheduler-job.yml"))
+	})
+
+	AfterEach(func() {
+		stopInnerBosh()
+	})
+
+	It("schedules jobs on intervals", func() {
+		session := outerBosh("-d", "bosh", "ssh", "-c", `sudo grep Bosh::Director::Jobs::ScheduledOrphanedVMCleanup.has_work:false /var/vcap/sys/log/director/scheduler.stdout.log`)
+		Eventually(session, 3*time.Minute).Should(gexec.Exit(0))
+	})
+})
