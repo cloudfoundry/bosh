c0abdd35824935feabd2a514b85795034f5aa6ff
Luan Santos <lsantos@pivotal.io>
2018-04-27 13:46:36 -0700

    Add `/orphaned_vms` endpoint
    [#141345561](https://www.pivotaltracker.com/story/show/141345561)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- src/bosh-director/lib/bosh/director/api/route_configuration.rb
+++ src/bosh-director/lib/bosh/director/api/route_configuration.rb
@@ -19,6 +19,7 @@ module Bosh
           controllers['/deployments'] = Bosh::Director::Api::Controllers::DeploymentsController.new(@config)
           controllers['/disks'] = Bosh::Director::Api::Controllers::DisksController.new(@config)
           controllers['/orphan_disks'] = Bosh::Director::Api::Controllers::OrphanDisksController.new(@config)
+          controllers['/orphaned_vms'] = Bosh::Director::Api::Controllers::OrphanedVmsController.new(@config)
           controllers['/info'] = Bosh::Director::Api::Controllers::InfoController.new(@config)
           controllers['/locks'] = Bosh::Director::Api::Controllers::LocksController.new(@config)
           controllers['/packages'] = Bosh::Director::Api::Controllers::PackagesController.new(@config)
