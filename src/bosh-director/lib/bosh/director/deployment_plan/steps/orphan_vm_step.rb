b56a6faa8f73ce1b8be9e868957263e85431ac64
Luan Santos <lsantos@pivotal.io>
2018-04-27 10:48:00 -0700

    Orphan VMs in a transaction
    [#157068092](https://www.pivotaltracker.com/story/show/157068092)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- src/bosh-director/lib/bosh/director/deployment_plan/steps/orphan_vm_step.rb
+++ src/bosh-director/lib/bosh/director/deployment_plan/steps/orphan_vm_step.rb
@@ -4,25 +4,28 @@ module Bosh::Director
       class OrphanVmStep
         def initialize(vm)
           @vm = vm
+          @transactor = Transactor.new
         end
 
         def perform(_)
-          orphaned_vm = Models::OrphanedVm.create(
-            availability_zone: @vm.instance.availability_zone,
-            cid: @vm.cid,
-            cloud_properties: @vm.instance.cloud_properties,
-            cpi: @vm.cpi,
-            stemcell_api_version: @vm.stemcell_api_version,
-            orphaned_at: Time.now,
-          )
+          @transactor.retryable_transaction(Bosh::Director::Config.db) do
+            orphaned_vm = Models::OrphanedVm.create(
+              availability_zone: @vm.instance.availability_zone,
+              cid: @vm.cid,
+              cloud_properties: @vm.instance.cloud_properties,
+              cpi: @vm.cpi,
+              stemcell_api_version: @vm.stemcell_api_version,
+              orphaned_at: Time.now,
+            )
 
-          @vm.ip_addresses_dataset.all.each do |ip_address|
-            ip_address.instance = nil
-            ip_address.orphaned_vm = orphaned_vm
-            ip_address.save
+            @vm.ip_addresses_dataset.all.each do |ip_address|
+              ip_address.instance = nil
+              ip_address.orphaned_vm = orphaned_vm
+              ip_address.save
+            end
+
+            @vm.destroy
           end
-
-          @vm.destroy
         end
       end
     end
