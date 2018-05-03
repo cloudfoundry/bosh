6a1630447d5b839755fe54220c38b3408eb7a477
Danny Berger <dberger@pivotal.io>
2018-05-02 18:36:27 -0700

    Add events when orphaning VMs
    [finishes #156747535](https://www.pivotaltracker.com/story/show/156747535)
    
    Co-authored-by: Chris Dutra <cdutra@pivotal.io>

--- src/bosh-director/lib/bosh/director/deployment_plan/steps/orphan_vm_step.rb
+++ src/bosh-director/lib/bosh/director/deployment_plan/steps/orphan_vm_step.rb
@@ -9,25 +9,50 @@ module Bosh::Director
 
         def perform(_)
           @transactor.retryable_transaction(Bosh::Director::Config.db) do
-            orphaned_vm = Models::OrphanedVm.create(
-              availability_zone: @vm.instance.availability_zone,
-              cid: @vm.cid,
-              cloud_properties: @vm.instance.cloud_properties,
-              cpi: @vm.cpi,
-              deployment_name: @vm.instance.deployment.name,
-              instance_name: @vm.instance.name,
-              orphaned_at: Time.now,
-              stemcell_api_version: @vm.stemcell_api_version,
-            )
+            begin
+              parent_id = add_event(@vm.instance.deployment.name, @vm.instance.name, @vm.cid)
 
-            @vm.ip_addresses_dataset.all.each do |ip_address|
-              ip_address.instance = nil
-              ip_address.orphaned_vm = orphaned_vm
-              ip_address.save
+              orphaned_vm = Models::OrphanedVm.create(
+                availability_zone: @vm.instance.availability_zone,
+                cid: @vm.cid,
+                cloud_properties: @vm.instance.cloud_properties,
+                cpi: @vm.cpi,
+                deployment_name: @vm.instance.deployment.name,
+                instance_name: @vm.instance.name,
+                orphaned_at: Time.now,
+                stemcell_api_version: @vm.stemcell_api_version,
+              )
+
+              @vm.ip_addresses_dataset.all.each do |ip_address|
+                ip_address.instance = nil
+                ip_address.orphaned_vm = orphaned_vm
+                ip_address.save
+              end
+
+              @vm.destroy
+            rescue Exception => e
+              raise e
+            ensure
+              add_event(@vm.instance.deployment.name, @vm.instance.name, @vm.cid, parent_id, e)
             end
-
-            @vm.destroy
           end
+        end
+
+        private
+
+        def add_event(deployment_name, instance_name, object_name = nil, parent_id = nil, error = nil)
+          event = Config.current_job.event_manager.create_event(
+            parent_id:   parent_id,
+            user:        Config.current_job.username,
+            action:      'orphan',
+            object_type: 'vm',
+            object_name: object_name,
+            task:        Config.current_job.task_id,
+            deployment:  deployment_name,
+            instance:    instance_name,
+            error:       error,
+          )
+          event.id
         end
       end
     end
