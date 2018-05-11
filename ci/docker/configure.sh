0dd92fc427589ee21f074293b6bc791577444ff3
Danny Berger <dberger@pivotal.io>
2018-05-11 11:31:07 -0700

    Consolidate docker-images pipeline-related files
    It was confusing having a separate docker-images pipelines directory.
    
    Also cleanup the duplicate garden/docker BOSH image tasks.

--- ci/docker/configure.sh
+++ ci/docker/configure.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 exec fly -t production set-pipeline \
   -p bosh:docker-images \
-  -c ./pipeline.yml \
+  -c pipeline.yml \
   --load-vars-from <(lpass show --note "bosh:docker-images concourse secrets")
--- ci/pipelines/docker-images/tasks/copy-bosh-docker-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-docker/bosh
-
-mkdir bosh-src/ci/docker/main-bosh-docker/bosh-deployment
-cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-docker/bosh-deployment
-
-cp -R bosh-src/* bosh-src-with-docker-dependencies
