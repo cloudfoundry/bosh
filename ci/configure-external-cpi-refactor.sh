9fa76e41c5371b7285c14f8381862a7208268cf9
Gaurab Dey <gdey@pivotal.io>
2018-06-13 18:05:53 -0400

    Add job to create candidate bosh release
    [#158342948](https://www.pivotaltracker.com/story/show/158342948)

--- ci/configure-external-cpi-refactor.sh
+++ ci/configure-external-cpi-refactor.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
-fly -t production set-pipeline -p bosh:external-cpi-refactor \
+fly -t production set-pipeline -p bosh:registry-removal \
     -c ci/pipeline-external-cpi-refactor.yml \
-    --load-vars-from <(lpass show -G "bosh concourse secrets" --notes)
+    --load-vars-from <(lpass show -G "bosh concourse secrets" --notes) \
+    --load-vars-from <(lpass show -G "bosh aws cpi v2 ci secrets" --notes)
