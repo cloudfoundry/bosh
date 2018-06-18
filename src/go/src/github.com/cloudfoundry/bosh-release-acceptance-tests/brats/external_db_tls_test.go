f47dbfb93694d45e1fff370a7a95b55e526e1626
Michael Xu <mxu@pivotal.io>
2018-06-18 15:44:30 -0700

    Use external_db helper methods in brats tests
    [#157568932](https://www.pivotaltracker.com/story/show/157568932)
    
    Co-authored-by: Josh Russett <jrussett@pivotal.io>

--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/external_db_tls_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/external_db_tls_test.go
@@ -2,15 +2,13 @@ package brats_test
 
 import (
 	"fmt"
-	"strings"
+	"os"
 
 	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Director external database TLS connections", func() {
@@ -19,58 +17,18 @@ var _ = Describe("Director external database TLS connections", func() {
 	})
 
 	testDBConnectionOverTLS := func(databaseType string, mutualTLSEnabled bool, useIncorrectCA bool) {
-		external_db_host := assertEnvExists(fmt.Sprintf("%s_EXTERNAL_DB_HOST", strings.ToUpper(databaseType)))
-		external_db_user := assertEnvExists(fmt.Sprintf("%s_EXTERNAL_DB_USER", strings.ToUpper(databaseType)))
-		external_db_password := assertEnvExists(fmt.Sprintf("%s_EXTERNAL_DB_PASSWORD", strings.ToUpper(databaseType)))
-		external_db_name := assertEnvExists(fmt.Sprintf("%s_EXTERNAL_DB_NAME", strings.ToUpper(databaseType)))
+		tmpCertDir, err := ioutil.TempDir("", "db_tls")
+		Expect(err).ToNot(HaveOccurred())
 
-		connectionOptions := fmt.Sprintf("external_db/%s_connection_options.yml", databaseType)
-		connectionVarFile := fmt.Sprintf("external_db/%s.yml", databaseType)
+		defer os.RemoveAll(tmpCertDir)
+
+		config := loadExternalDBConfig(databaseType, mutualTLSEnabled, tmpCertDir)
 
 		if useIncorrectCA {
-			connectionVarFile = fmt.Sprintf("external_db/%s_invalid_ca.yml", databaseType)
+			config.ConnectionVarFile = fmt.Sprintf("external_db/%s_invalid_ca.yml", databaseType)
 		}
 
-		startInnerBoshArgs := []string{
-			fmt.Sprintf("-o %s", boshDeploymentAssetPath("misc/external-db.yml")),
-			fmt.Sprintf("-o %s", boshDeploymentAssetPath("experimental/db-enable-tls.yml")),
-			fmt.Sprintf("-o %s", assetPath(connectionOptions)),
-			fmt.Sprintf("--vars-file %s", assetPath(connectionVarFile)),
-			fmt.Sprintf("-v external_db_host=%s", external_db_host),
-			fmt.Sprintf("-v external_db_user=%s", external_db_user),
-			fmt.Sprintf("-v external_db_password=%s", external_db_password),
-			fmt.Sprintf("-v external_db_name=%s", external_db_name),
-		}
-
-		if mutualTLSEnabled {
-			external_db_client_certificate := assertEnvExists(fmt.Sprintf("%s_EXTERNAL_DB_CLIENT_CERTIFICATE", strings.ToUpper(databaseType)))
-			external_db_client_private_key := assertEnvExists(fmt.Sprintf("%s_EXTERNAL_DB_CLIENT_PRIVATE_KEY", strings.ToUpper(databaseType)))
-
-			tempDir, err := ioutil.TempDir("", "bosh_db_tls")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			defer os.RemoveAll(tempDir)
-
-			external_db_client_certificate_file := filepath.Join(tempDir, "external_db_client_certificate")
-			if err := ioutil.WriteFile(external_db_client_certificate_file, []byte(external_db_client_certificate), 0666); err != nil {
-				log.Fatal(err)
-			}
-
-			external_db_client_private_key_file := filepath.Join(tempDir, "external_db_client_private_key")
-			if err := ioutil.WriteFile(external_db_client_private_key_file, []byte(external_db_client_private_key), 0666); err != nil {
-				log.Fatal(err)
-			}
-
-			mutualTLSArgs := []string{
-				fmt.Sprintf("-o %s", boshDeploymentAssetPath("experimental/db-enable-mutual-tls.yml")),
-				fmt.Sprintf("--var-file=db_client_certificate=%s", external_db_client_certificate_file),
-				fmt.Sprintf("--var-file=db_client_private_key=%s", external_db_client_private_key_file),
-			}
-
-			startInnerBoshArgs = append(startInnerBoshArgs, mutualTLSArgs...)
-		}
+		startInnerBoshArgs := innerBoshWithExternalDBOptions(config)
 
 		if useIncorrectCA {
 			startInnerBoshWithExpectation(true, "Error: 'bosh/[0-9a-f]{8}-[0-9a-f-]{27} \\(0\\)' is not running after update", startInnerBoshArgs...)
