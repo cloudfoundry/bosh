3127798de09116db61749712339b15e06d902c03
Michael Xu <mxu@pivotal.io>
2018-06-20 13:56:12 -0700

    Ensure that local-bosh-release ops file is last applied
    This change moves the `source-releases/bosh.yml` opsfile before the
    `local-bosh-release` ops file. When they were in the reverse order, the
    source releases would cause 266.2 to be deployed.
    
    [#158412036](https://www.pivotaltracker.com/story/show/158412036)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ci/docker/main-bosh-docker/start-inner-bosh.sh
+++ ci/docker/main-bosh-docker/start-inner-bosh.sh
@@ -26,9 +26,9 @@ bosh int bosh.yml \
   -v docker_host="${DOCKER_HOST}" \
   -v network=director_network \
   -v docker_tls="${DOCKER_CERTS}" \
+  -o "/usr/local/bosh-deployment/misc/source-releases/bosh.yml" \
   -o /usr/local/bosh-deployment/local-bosh-release.yml \
   -v local_bosh_release="${src_dir}" \
-  -o "/usr/local/bosh-deployment/misc/source-releases/bosh.yml" \
   ${@} > "${inner_bosh_dir}/bosh-director.yml"
 
 bosh upload-stemcell \
