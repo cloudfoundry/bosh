386adace5952349fd31fd87d0b59c42da751cc16
Joshua Aresty <joshua.aresty@emc.com>
2018-06-22 11:54:40 -0700

    Remove vm from instance model when deleting
    - the compilation double-delete bug was happening because the instance
    model maintained a stale reference to the VM record that was destroyed
    in the DeleteVmStep.  By removing that VM from the instance at deletion
    time, we prevent that from being wrongly deleted again later.
    
    [#158130962](https://www.pivotaltracker.com/story/show/158130962)
    
    Co-Authored-By: Andrew Gilbert <agilbert@pivotal.io>

---
+++ src/spec/gocli/integration/unresponsive_compilation_vm_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../spec_helper'
+
+describe 'when compilation vm fails to respond', type: :integration do
+  with_reset_sandbox_before_each(agent_wait_timeout: 1)
+
+  before do
+    create_and_upload_test_release
+    upload_stemcell
+  end
+
+  it 'deletes compilation VM only once' do
+    cloud_config = Bosh::Spec::NewDeployments.simple_cloud_config
+    upload_cloud_config(cloud_config_hash: cloud_config)
+
+    Thread.current[:sandbox].instance_variable_get(:@nats_process).stop # make the agent unresponsive
+
+    deploy_simple_manifest(
+      manifest_hash: Bosh::Spec::NewDeployments.simple_manifest_with_instance_groups(instances: 1),
+      failure_expected: true,
+    )
+
+    task_debug_logs = bosh_runner.run('task --debug 3', failure_expected: true)
+    expect(task_debug_logs).to_not include('Attempt to delete object did not result in a single'\
+                                           ' row modification')
+    expect(task_debug_logs).to include('Timed out pinging to')
+  end
+end
