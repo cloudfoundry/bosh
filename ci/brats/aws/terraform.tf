d68a7256114da88248f89c2fb4ccf41e2526ada7
Joshua Aresty <joshua.aresty@emc.com>
2018-06-25 16:10:28 -0700

    Enable dns hostnames for the BRATS RDS VPC
    [#158168890](https://www.pivotaltracker.com/story/show/158168890)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ci/brats/aws/terraform.tf
+++ ci/brats/aws/terraform.tf
@@ -8,7 +8,8 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "zonea" {
