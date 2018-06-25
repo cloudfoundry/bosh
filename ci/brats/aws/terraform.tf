5403c4964195fc8dd33675a51fafbc00d30bd413
James Myers <jmyers@pivotal.io>
2018-06-25 11:17:46 -0700

    Add postgres to terraform template
    - for spinning up an RDS to run brats
    
    [#158168890](https://www.pivotaltracker.com/story/show/158168890)
    
    Co-Authored-By: Joshua Aresty <joshua.aresty@emc.com>

--- ci/brats/aws/terraform.tf
+++ ci/brats/aws/terraform.tf
@@ -1,6 +1,3 @@
-variable "rds_mysql_username" {}
-variable "rds_mysql_password" {}
-variable "rds_mysql_databasename" {}
 variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
 
@@ -10,7 +7,11 @@ provider "aws" {
   region     = "us-west-1"
 }
 
-resource "aws_db_instance" "default" {
+variable "rds_mysql_username" {}
+variable "rds_mysql_password" {}
+variable "rds_mysql_databasename" {}
+
+resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
   storage_type         = "gp2"
   engine               = "mysql"
@@ -23,6 +24,27 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
 }
 
-output "endpoint" {
-  value = "${aws_db_instance.default.endpoint}"
+output "aws_mysql_endpoint" {
+  value = "${aws_db_instance.mysql.endpoint}"
+}
+
+variable "rds_postgres_username" {}
+variable "rds_postgres_password" {}
+variable "rds_postgres_databasename" {}
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "9.6.8"
+  instance_class       = "db.t2.micro"
+  skip_final_snapshot  = true
+  name                 = "${var.rds_postgres_databasename}"
+  username             = "${var.rds_postgres_username}"
+  password             = "${var.rds_postgres_password}"
+  parameter_group_name = "default.postgres9.6"
+}
+
+output "aws_postgres_endpoint" {
+  value = "${aws_db_instance.postgres.endpoint}"
 }
