5a6bd0fd168379936ef6d599bf9961ddaf9166d4
Joshua Aresty <joshua.aresty@emc.com>
2018-06-27 17:08:13 -0700

    Refactor instance_updater
    - Chose this class because it had a high flog score and high churn
    - Reduced complexity of all methods
    - Broke into three classes -- InstanceUpdater, RecreateHandler, and
    UpdateProcedure
    - Passes all unit and integration tests
    
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- src/bosh-director/lib/bosh/director/instance_updater.rb
+++ src/bosh-director/lib/bosh/director/instance_updater.rb
@@ -48,156 +48,54 @@ module Bosh::Director
       instance = instance_plan.instance
       @links_manager = Bosh::Director::Links::LinksManager.new(instance.deployment_model.links_serial_id)
       instance_report = DeploymentPlan::Stages::Report.new.tap { |r| r.vm = instance.model.active_vm }
-      action, context = get_action_and_context(instance_plan)
-      parent_id = add_event(instance.deployment_model.name, action, instance.model.name, context) if instance_plan.changed?
+
+      update_procedure = get_update_procedure(instance, instance_plan, options, instance_report)
+
+      if instance_plan.changed?
+        parent_id = add_event(instance.deployment_model.name,
+                              update_procedure.action,
+                              instance.model.name,
+                              update_procedure.context)
+      end
       @logger.info("Updating instance #{instance}, changes: #{instance_plan.changes.to_a.join(', ').inspect}")
 
-      update_procedure = lambda do
-        # Optimization to only update DNS if nothing else changed.
-        if dns_change_only?(instance_plan)
-          @logger.debug('Only change is DNS configuration')
-          update_dns(instance_plan)
-          return
-        end
+      InstanceUpdater::InstanceState.with_instance_update_and_event_creation(
+        instance.model,
+        parent_id,
+        instance.deployment_model.name,
+        update_procedure.action,
+        &update_procedure
+      )
+    end
 
-        links_bound_to_instance = false
-
-        unless instance_plan.already_detached?
-          # Rendered templates are persisted here, in the case where a vm is already soft stopped
-          # It will update the rendered templates on the VM
-          unless Config.enable_nats_delivered_templates && needs_recreate?(instance_plan)
-            @rendered_templates_persistor.persist(instance_plan)
-            @links_manager.bind_links_to_instance(instance)
-            instance.update_variable_set
-            links_bound_to_instance = true
-          end
-
-          unless instance_plan.needs_shutting_down? || instance.state == 'detached'
-            DeploymentPlan::Steps::PrepareInstanceStep.new(instance_plan).perform(instance_report)
-          end
-
-          # current state
-          if instance.model.state != 'stopped'
-            stop(instance_plan)
-            take_snapshot(instance)
-          end
-
-          # desired state
-          if instance.state == 'stopped'
-            # Command issued: `bosh stop`
-            instance.update_state
-            return
-          end
-        end
-
-        if instance.state == 'detached'
-          # Command issued: `bosh stop --hard`
-          @logger.info("Detaching instance #{instance}")
-          unless instance_plan.already_detached?
-            instance_model = instance_plan.new? ? instance_plan.instance.model : instance_plan.existing_instance
-            DeploymentPlan::Steps::UnmountInstanceDisksStep.new(instance_model).perform(instance_report)
-            DeploymentPlan::Steps::DeleteVmStep.new(true, false, Config.enable_virtual_delete_vms)
-                                               .perform(instance_report)
-          end
-          instance_plan.release_obsolete_network_plans(@ip_provider)
-          @links_manager.bind_links_to_instance(instance)
-          instance.update_state
-          instance.update_variable_set
-          update_dns(instance_plan)
-          return
-        end
-
-        recreated = false
-        deleted_vm = false
-        deleted_vm_id = -1
-
-        instance_model = instance_plan.instance.model
-
-        if needs_recreate?(instance_plan) || (instance_plan.should_create_swap_delete? && instance_model.vms.count > 1)
-          new_vm = instance_model.most_recent_inactive_vm || instance_model.active_vm
-
-          @logger.debug('Failed to update in place. Recreating VM')
-          if instance_plan.unresponsive_agent?
-            deleted_vm_id = instance_report.vm.id
-            deleted_vm = true
-
-            DeploymentPlan::Steps::DeleteVmStep
-              .new(true, false, Config.enable_virtual_delete_vms)
-              .perform(instance_report)
-          else
-            DeploymentPlan::Steps::UnmountInstanceDisksStep.new(instance_model).perform(instance_report)
-            DeploymentPlan::Steps::DetachInstanceDisksStep.new(instance_model).perform(instance_report)
-          end
-
-          tags = instance_plan.tags
-
-          disks = instance_model.active_persistent_disks.collection
-                                .map(&:model)
-                                .map(&:disk_cid).compact
-
-          if instance_plan.should_create_swap_delete? && instance_model.vms.count > 1
-            instance_report.vm = new_vm
-            DeploymentPlan::Steps::ElectActiveVmStep.new.perform(instance_report)
-
-            instance_model.vms.reject { |vm| vm.id == new_vm.id || vm.id == deleted_vm_id }.each do |inactive_vm|
-              ips = inactive_vm.ip_addresses.map(&:address_str)
-              DeploymentPlan::Steps::OrphanVmStep.new(inactive_vm).perform(instance_report)
-              instance_plan.remove_obsolete_network_plans_for_ips(ips)
-            end
-
-            instance_report.vm = instance_model.active_vm
-            if instance_plan.needs_disk?
-              DeploymentPlan::Steps::AttachInstanceDisksStep.new(instance_model, instance_plan.tags).perform(instance_report)
-              DeploymentPlan::Steps::MountInstanceDisksStep.new(instance_model).perform(instance_report)
-            end
-          else
-            unless deleted_vm
-              DeploymentPlan::Steps::DeleteVmStep
-                .new(true, false, Config.enable_virtual_delete_vms)
-                .perform(instance_report)
-            end
-
-            @vm_creator.create_for_instance_plan(instance_plan, @ip_provider, disks, tags)
-          end
-
-          recreated = true
-        end
-
-        instance_plan.release_obsolete_network_plans(@ip_provider)
-        instance_report.vm = instance_model.active_vm
-
-        update_dns(instance_plan)
-        @disk_manager.update_persistent_disk(instance_plan)
-
-        instance.update_instance_settings unless recreated
-
-        cleaner = RenderedJobTemplatesCleaner.new(instance.model, @blobstore, @logger)
-
-        @rendered_templates_persistor.persist(instance_plan)
-        instance.update_variable_set
-        @links_manager.bind_links_to_instance(instance) unless links_bound_to_instance
-
-        state_applier = InstanceUpdater::StateApplier.new(
-          instance_plan,
-          agent(instance),
-          cleaner,
-          @logger,
-          canary: options[:canary],
-        )
-        state_applier.apply(instance_plan.desired_instance.instance_group.update)
+    def needs_recreate?(instance_plan)
+      if instance_plan.needs_shutting_down?
+        @logger.debug('VM needs to be shutdown before it can be updated.')
+        return true
       end
 
-      InstanceUpdater::InstanceState
-        .with_instance_update_and_event_creation(
-          instance.model,
-          parent_id,
-          instance.deployment_model.name,
-          action,
-          &update_procedure
-        )
+      false
     end
 
     private
+
+    def get_update_procedure(instance, instance_plan, options, instance_report)
+      UpdateProcedure.new(
+        instance,
+        instance_plan,
+        options,
+        @blobstore,
+        needs_recreate?(instance_plan),
+        instance_report,
+        @disk_manager,
+        @rendered_templates_persistor,
+        @vm_creator,
+        @links_manager,
+        @ip_provider,
+        @dns_state_updater,
+        @logger,
+      )
+    end
 
     def add_event(deployment_name,
                   action, instance_name = nil,
@@ -217,65 +115,6 @@ module Bosh::Director
         context: context ? context : {},
       )
       event.id
-    end
-
-    def get_action_and_context(instance_plan)
-      changes = instance_plan.changes
-      context = {}
-      if changes.size == 1 && %i[state restart].include?(changes.first)
-        action = case instance_plan.instance.virtual_state
-                 when 'started'
-                   'start'
-                 when 'stopped'
-                   'stop'
-                 when 'detached'
-                   'stop'
-                 else
-                   instance_plan.instance.virtual_state
-                 end
-      else
-        context['az'] = instance_plan.desired_az_name if instance_plan.desired_az_name
-        if instance_plan.new?
-          action = 'create'
-        else
-          context['changes'] = changes.to_a unless changes.size == 1 && changes.first == :recreate
-          action = needs_recreate?(instance_plan) ? 'recreate' : 'update'
-        end
-      end
-      [action, context]
-    end
-
-    def stop(instance_plan)
-      instance = instance_plan.instance
-      stopper = Stopper.new(instance_plan, instance.state, Config, @logger)
-      stopper.stop
-    end
-
-    def take_snapshot(instance)
-      Api::SnapshotManager.take_snapshot(instance.model, clean: true)
-    end
-
-    def update_dns(instance_plan)
-      return unless instance_plan.dns_changed?
-
-      @dns_state_updater.update_dns_for_instance(instance_plan.instance.model, instance_plan.network_settings.dns_record_info)
-    end
-
-    def dns_change_only?(instance_plan)
-      instance_plan.changes.include?(:dns) && instance_plan.changes.size == 1
-    end
-
-    def needs_recreate?(instance_plan)
-      if instance_plan.needs_shutting_down?
-        @logger.debug('VM needs to be shutdown before it can be updated.')
-        return true
-      end
-
-      false
-    end
-
-    def agent(instance)
-      AgentClient.with_agent_id(instance.model.agent_id)
     end
   end
 end
