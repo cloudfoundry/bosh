a7f03e9747fed50b87ef14c226ac61870269250d
Josh Russett <jrussett@pivotal.io>
2018-07-11 15:06:29 -0700

    [brats] make terraform resource name branch-unique
    - modify the terraform resource names to include the name of the branch
    in anticipation for the next LTS bosh releases, e.g.
    env_name: brats-ubuntu-trusty  becomes
    env_name: brats-ubuntu-trusty-267.x
    
    [finishes #158168890](https://www.pivotaltracker.com/story/show/158168890)
    
    Co-authored-by: Michael Xu <mxu@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,8 +2,11 @@
 
 set -eu
 
+branch="master"
+
 fly -t production set-pipeline -p bosh \
     -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
-    -l <(lpass show --note "tracker-bot-story-delivery")
+    -l <(lpass show --note "tracker-bot-story-delivery") \
+    --var=branch_name=${branch}
