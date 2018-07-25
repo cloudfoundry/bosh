4abe434bcfdbaaae06a7398f971dea47d423e62b
Luan Santos <lsantos@pivotal.io>
2018-07-25 16:17:20 -0700

    [brats] delete inner bosh on external db tests
    since we may be switching databases entirely after/before that, it's
    safer to destroy the inner bosh between those tests to avoid conflicts
    
    [#158518127](https://www.pivotaltracker.com/story/show/158518127)

--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/external_db_tls_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/external_db_tls_test.go
@@ -18,30 +18,22 @@ var _ = Describe("Director external database TLS connections", func() {
 		Expect(err).ToNot(HaveOccurred())
 		dbConfig := bratsutils.LoadExternalDBConfig(databaseType, mutualTLSEnabled, tmpCertDir)
 		bratsutils.CreateDB(dbConfig)
-		defer bratsutils.DeleteDB(dbConfig)
 		defer os.RemoveAll(tmpCertDir)
-
-		if useIncorrectCA {
-			dbConfig.CACertPath = bratsutils.AssetPath("external_db/invalid_ca_cert.pem")
-		}
+		defer bratsutils.DeleteDB(dbConfig)
 
 		startInnerBoshArgs := bratsutils.InnerBoshWithExternalDBOptions(dbConfig)
 
-		if useIncorrectCA {
-			bratsutils.StartInnerBoshWithExpectation(true, "Error: 'bosh/[0-9a-f]{8}-[0-9a-f-]{27} \\(0\\)' is not running after update", startInnerBoshArgs...)
-		} else {
-			bratsutils.StartInnerBosh(startInnerBoshArgs...)
-			bratsutils.UploadRelease("https://bosh.io/d/github.com/cloudfoundry/syslog-release?v=11")
-		}
+		bratsutils.StartInnerBosh(startInnerBoshArgs...)
+		defer bratsutils.StopInnerBosh()
+		bratsutils.UploadRelease("https://bosh.io/d/github.com/cloudfoundry/syslog-release?v=11")
 	}
 
 	Context("RDS", func() {
 		var mutualTLSEnabled = false
-		var useIncorrectCA = false
 
 		DescribeTable("Regular TLS", testDBConnectionOverTLS,
-			Entry("allows TLS connections to POSTGRES", "rds_postgres", mutualTLSEnabled, useIncorrectCA),
-			Entry("allows TLS connections to MYSQL", "rds_mysql", mutualTLSEnabled, useIncorrectCA),
+			Entry("allows TLS connections to POSTGRES", "rds_postgres", mutualTLSEnabled),
+			Entry("allows TLS connections to MYSQL", "rds_mysql", mutualTLSEnabled),
 		)
 	})
 
@@ -49,22 +41,20 @@ var _ = Describe("Director external database TLS connections", func() {
 		Context("Regular TLS", func() {
 			Context("With valid CA", func() {
 				var mutualTLSEnabled = false
-				var useIncorrectCA = false
 
 				DescribeTable("DB Connections", testDBConnectionOverTLS,
-					Entry("allows TLS connections to MYSQL", "gcp_mysql", mutualTLSEnabled, useIncorrectCA),
-					Entry("allows TLS connections to POSTGRES", "gcp_postgres", mutualTLSEnabled, useIncorrectCA),
+					Entry("allows TLS connections to MYSQL", "gcp_mysql", mutualTLSEnabled),
+					Entry("allows TLS connections to POSTGRES", "gcp_postgres", mutualTLSEnabled),
 				)
 			})
 		})
 
 		Context("Mutual TLS", func() {
 			var mutualTLSEnabled = true
-			var useIncorrectCA = false
 
 			DescribeTable("DB Connections", testDBConnectionOverTLS,
-				Entry("allows TLS connections to MYSQL", "gcp_mysql", mutualTLSEnabled, useIncorrectCA),
-				Entry("allows TLS connections to POSTGRES", "gcp_postgres", mutualTLSEnabled, useIncorrectCA),
+				Entry("allows TLS connections to MYSQL", "gcp_mysql", mutualTLSEnabled),
+				Entry("allows TLS connections to POSTGRES", "gcp_postgres", mutualTLSEnabled),
 			)
 		})
 	})
