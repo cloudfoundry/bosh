5913d277301bf29cf685391181a0ce9ed50ff780
Chris Dutra <cdutra@pivotal.io>
2018-07-25 17:42:27 -0700

    Move bosh:aws-ubuntu-bats to bosh master pipeline
    - aws terraform environments are ephemeral
    
    [finishes #159212146](https://www.pivotaltracker.com/story/show/159212146)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- ci/bats/iaas/aws/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-fly -t production set-pipeline \
-  -p bosh:aws-ubuntu-bats \
-  -c pipeline.yml \
-  -l <(lpass show -G "bosh:aws-ubuntu-bats concourse secrets" --notes)
