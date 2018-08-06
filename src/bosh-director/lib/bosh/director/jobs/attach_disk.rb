ff3b73fe273a71da5d607fa56180ede18226b32c
Murali Suresh <muralisc@gmail.com>
2018-08-06 22:33:37 +0530

    Change flag name from copy to disk_properties
    disk_properties accepts values copy and default

--- src/bosh-director/lib/bosh/director/jobs/attach_disk.rb
+++ src/bosh-director/lib/bosh/director/jobs/attach_disk.rb
@@ -8,16 +8,16 @@ module Bosh::Director
         :attach_disk
       end
 
-      def self.enqueue(username, deployment, job_name, instance_id, disk_cid, copy, job_queue)
-        job_queue.enqueue(username, Jobs::AttachDisk, "attach disk '#{disk_cid}' to '#{job_name}/#{instance_id}'", [deployment.name, job_name, instance_id, disk_cid, copy], deployment)
+      def self.enqueue(username, deployment, job_name, instance_id, disk_cid, disk_properties, job_queue)
+        job_queue.enqueue(username, Jobs::AttachDisk, "attach disk '#{disk_cid}' to '#{job_name}/#{instance_id}'", [deployment.name, job_name, instance_id, disk_cid, disk_properties], deployment)
       end
 
-      def initialize(deployment_name, job_name, instance_id, disk_cid, copy)
+      def initialize(deployment_name, job_name, instance_id, disk_cid, disk_properties)
         @deployment_name = deployment_name
         @job_name = job_name
         @instance_id = instance_id
         @disk_cid = disk_cid
-        @copy = copy
+        @disk_properties = disk_properties
         @size = 1
         @cloud_properties = {}
         @transactor = Transactor.new
@@ -61,7 +61,7 @@ module Bosh::Director
       def handle_previous_disk(instance)
         previous_persistent_disk = instance.managed_persistent_disk
         previous_persistent_disk.update(active: false)
-        if @copy == true
+        if @disk_properties == "copy"
           @size = previous_persistent_disk.size
           @cloud_properties = previous_persistent_disk.cloud_properties
         end
