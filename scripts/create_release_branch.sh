2c38ba474f12382ac693c963308d219dd2e85e4f
Luan Santos <lsantos@pivotal.io>
2018-08-06 11:03:38 -0700

    Add export-final-release to branching ops file
    [#159144425](https://www.pivotaltracker.com/story/show/159144425)

--- scripts/create_release_branch.sh
+++ scripts/create_release_branch.sh
@@ -4,17 +4,20 @@ main() {
   set -e
 
   local scripts_path=$(dirname $0)
-  local start_point=${2:-'HEAD'}
-  local release_patch_file=$scripts_path/create_release_branch.patch
-  local finalize_job_patch_file=$scripts_path/add_finalize_release_job.patch
+  local start_point=${3:-'HEAD'}
 
   local BRANCH_NAME=${1?'Branch name is required.'}
+  local STEMCELL_LINE=${2?'Stemcell line is required.'}
 
   git checkout -b ${BRANCH_NAME} $start_point
 
   local BRANCH_VERSION=${OVERRIDE_VERSION:-$(echo ${BRANCH_NAME} | cut -d '.' -f1)}
 
-  bosh int -o scripts/create-release-branch-ops.yml <(git show origin/master:ci/pipeline.yml) -v branchver=${BRANCH_VERSION} -v branchname=${BRANCH_NAME} > ci/pipeline.yml
+  bosh int -o scripts/create-release-branch-ops.yml <(git show origin/master:ci/pipeline.yml) \
+    -v branchver="${BRANCH_VERSION}" \
+    -v branchname="${BRANCH_NAME}" \
+    -v stemcell_line="${STEMCELL_LINE}" \
+    > ci/pipeline.yml
 
   git add -A .
   git ci -m "Create release branch $BRANCH_NAME"
