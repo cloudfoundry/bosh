78e5e067ef8dc14ea03269ec68bd2cf604dc0981
Josh Russett <jrussett@pivotal.io>
2018-09-06 15:12:15 -0700

    Update finalize-release files to correct version
    - On the previous commit, an issue with vim overwrote our actual
    changes.
    
    [#160077524](https://www.pivotaltracker.com/story/show/160077524)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- ci/tasks/finalize-bosh-release.sh
+++ ci/tasks/finalize-bosh-release.sh
@@ -6,38 +6,15 @@ export FULL_VERSION=$(cat version/version)
 
 export ROOT_PATH=$PWD
 PROMOTED_MASTER=$PWD/bosh-master-with-final
-PROMOTED_SRC=$PWD/bosh-src-with-bumped-version
+TARBALL_DIR=$PWD/release-tarball
 
 mv bosh-cli/alpha-bosh-cli-*-linux-amd64 bosh-cli/bosh-cli
 export GO_CLI_PATH=$ROOT_PATH/bosh-cli/bosh-cli
 chmod +x $GO_CLI_PATH
 
-git clone ./bosh-src $PROMOTED_SRC
-
-pushd $PROMOTED_SRC
-  git status
-
-  sed -i "s/\['version'\] = ..*/['version'] = '$FULL_VERSION'/" jobs/director/templates/director.yml.erb
-  sed -i "s/\['version'\])\.to eq..*/['version']).to eq('$FULL_VERSION')/" spec/director.yml.erb_spec.rb
-
-  git add -A
-  git status
-
-  git config --global user.email "ci@localhost"
-  git config --global user.name "CI Bot"
-
-  git commit -m "Bump version $FULL_VERSION via concourse"
-
-  $GO_CLI_PATH create-release --version $FULL_VERSION --tarball=/tmp/tarball.tgz
-popd
-
 git clone ./bosh-master $PROMOTED_MASTER
 
 pushd $PROMOTED_MASTER
-  if [[ "$is_master" == "true" ]]; then
-    git pull file:///$PROMOTED_SRC
-  fi
-
   git status
 
   cat >> config/private.yml <<EOF
@@ -49,7 +26,7 @@ blobstore:
     secret_access_key: "$BLOBSTORE_SECRET_ACCESS_KEY"
 EOF
 
-  $GO_CLI_PATH finalize-release --version $FULL_VERSION /tmp/tarball.tgz
+  $GO_CLI_PATH finalize-release --version $FULL_VERSION $TARBALL_DIR/tarball.tgz
 
   git add -A
   git status
@@ -61,10 +38,10 @@ EOF
 
 popd
 
-cat <<EOF >bosh-src-with-bumped-version-tag/tag-name
+cat <<EOF >bosh-master-with-final/tag-name
 v${FULL_VERSION}
 EOF
 
-cat <<EOF >bosh-src-with-bumped-version-tag/annotate-msg
+cat <<EOF >bosh-master-with-final/annotate-msg
 Final release $FULL_VERSION tagged via concourse
 EOF
