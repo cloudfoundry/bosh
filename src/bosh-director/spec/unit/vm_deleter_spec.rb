a588dc34d4491b620a4c939cf955633df0fabcd0
Michael Xu <mxu@pivotal.io>
2018-09-25 12:11:05 -0700

    Dont skip VM deletion by cid when using cpi-config
    Currently the cloud_factory will return 1 cpi based on the name and
    stemcell. The previous concern of calling delete on multiple cpi's is no
    longer an issue.
    
    [#160764550](https://www.pivotaltracker.com/story/show/160764550)

--- src/bosh-director/spec/unit/vm_deleter_spec.rb
+++ src/bosh-director/spec/unit/vm_deleter_spec.rb
@@ -89,39 +89,15 @@ module Bosh
       end
 
       describe '#delete_vm_by_cid' do
-        context('multiple clouds are configured') do
+        context('deletes the vm using the appropriate cpi') do
           before do
-            allow(cloud_factory).to receive(:uses_cpi_config?).and_return(true)
+            allow(cloud_factory).to receive(:get).with('cpi1', 25).and_return(cloud)
           end
 
-          it 'does not call delete_vm ' do
+          it 'calls delete_vm' do
             expect(logger).to receive(:info).with('Deleting VM')
-            expect(cloud).to_not receive(:delete_vm).with(vm_model.cid)
-            subject.delete_vm_by_cid(vm_model.cid, nil)
-          end
-        end
-
-        context('multiple clouds are not configured') do
-          before do
-            allow(cloud_factory).to receive(:uses_cpi_config?).and_return(false)
-          end
-
-          context('when stemcell_api_version is not nil') do
-            it 'calls delete_vm if only one cloud is configured with stemcell_api_version' do
-              allow(cloud_factory).to receive(:get).with(nil, 25).and_return(cloud)
-
-              expect(logger).to receive(:info).with('Deleting VM')
-              expect(cloud).to receive(:delete_vm).with(vm_model.cid)
-              subject.delete_vm_by_cid(vm_model.cid, 25)
-            end
-          end
-
-          context('when stemcell_api_version is nil') do
-            it 'calls delete_vm if only one cloud is configured without stemcell_api_version' do
-              expect(logger).to receive(:info).with('Deleting VM')
-              expect(cloud).to receive(:delete_vm).with(vm_model.cid)
-              subject.delete_vm_by_cid(vm_model.cid, nil)
-            end
+            expect(cloud).to receive(:delete_vm).with(vm_model.cid)
+            subject.delete_vm_by_cid(vm_model.cid, 25, 'cpi1')
           end
 
           context 'when virtual delete is enabled' do
@@ -134,7 +110,6 @@ module Bosh
             end
           end
         end
-
       end
     end
   end
