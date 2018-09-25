800cb4242027ae33c5a734676ab6c3abec6ec795
Christopher Brown <cbrown@pivotal.io>
2018-09-25 15:47:55 -0700

    Update dockerfiles for bosh-load-tests
    Remove attempted fix to allow mysql to startup in the ubuntu-xenial
    docker image. Instead we need to add:
    `find /var/lib/mysql/mysql -exec touch -c -a {} +` before starting the
    mysql server in the test script. This script currently lives in the
    bosh-load-tests-workspace repository.
    
    [#160767158](https://www.pivotaltracker.com/story/show/160767158)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ci/docker/main-bosh-garden/install-garden.sh
+++ ci/docker/main-bosh-garden/install-garden.sh
@@ -5,10 +5,10 @@ set -xe
 mkdir -p /opt/garden/bin
 cd /opt/garden
 
-curl -o bin/gdn -L https://github.com/cloudfoundry/garden-runc-release/releases/download/v1.12.1/gdn-1.12.1
+curl -o bin/gdn -L https://github.com/cloudfoundry/garden-runc-release/releases/download/v1.16.4/gdn-1.16.4
 chmod +x bin/gdn
 
-test "$(sha256sum bin/gdn)" == "ad82bc5b597ea356e738df2dc6b1f9e1ef619d6610fd5546512116bb98ce3921  bin/gdn"
+test "$(sha256sum bin/gdn)" == "c4c37b9e9efd482876b9af82273f8dc4015302f5715da524e0d6b7eb7071a703  bin/gdn"
 
 curl -o bin/gaol -L https://github.com/contraband/gaol/releases/download/2016-8-22/gaol_linux
 chmod +x bin/gaol
