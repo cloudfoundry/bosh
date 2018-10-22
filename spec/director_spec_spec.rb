e973d988a0467b7e12ba85ee14b40ee1255fea7f
Vladimir Videlov <vladimir.videlov@sap.com>
2018-10-22 16:12:02 +0200

    Add ngx_http_stub_status_module to director and blobstore
    Compile nginx package with ngx_http_stub_status_module and enable '/stats'
    nginx URL for retriving basic metrics on current usage which is only
    accessible from the localhost itself on the VM. Both director and blobstore
    have new properties in job spec for enabling this feature:
    
    director.nginx.enable_metrics_endpoint = false(default) / true
    blobstore.nginx.enable_metrics_endpoint = false(default) / true
    
    [#160850781](https://www.pivotaltracker.com/story/show/1160850781)
    
    Co-authored-by: Florian Nachtigall <florian.nachtigall@sap.com>

--- spec/director_spec_spec.rb
+++ spec/director_spec_spec.rb
@@ -12,4 +12,8 @@ describe 'director job spec' do
   it 'defaults director.ignore_missing_gateway to false' do
     expect(spec_yaml['properties']['director.ignore_missing_gateway']['default']).to eq(false)
   end
+
+  it 'defaults director.nginx.enable_metrics_endpoint to false' do
+    expect(spec_yaml['properties']['director.nginx.enable_metrics_endpoint']['default']).to eq(false)
+  end
 end
