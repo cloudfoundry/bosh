724c38cefe2ccb993360864c15a78b56b11f82a6
Andrew Su <asu@pivotal.io>
2018-10-25 15:23:57 -0400

    Update gnatsd to 1.3.0
    [#161490609](https://www.pivotaltracker.com/story/show/161490609)
    
    Signed-off-by: Denise Yu <dyu@pivotal.io>

--- src/bosh-dev/lib/bosh/dev/gnatsd_manager.rb
+++ src/bosh-dev/lib/bosh/dev/gnatsd_manager.rb
@@ -3,9 +3,9 @@ require_relative './artifact'
 
 module Bosh::Dev
   class GnatsdManager
-    VERSION = '0.9.6-bosh.13'
-    DARWIN_SHA256 = '4f30811155694ac057b269c9235629789d89296381d7e61c9adad345acdc6afa'
-    LINUX_SHA256 = 'aed21d73b0155db2871e724e3f1ed033433e7ea2d814406b1451de53edc20d6d'
+    VERSION = '1.3.0-bosh.2'.freeze
+    DARWIN_SHA256 = 'd1ea29e83a26635cf2301b771c47ed30c332056a66931cec8f7091d7dd163401'.freeze
+    LINUX_SHA256 = '528ebd92d5909535763e7c9439f9564403fd663c0f64e869d568187bb6823ea0'.freeze
     BUCKET_NAME = 'bosh-gnatsd'
 
     REPO_ROOT = File.expand_path('../../../../', File.dirname(__FILE__))
