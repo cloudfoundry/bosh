52ac620f58664045cb1b8f610ab873fa2207e03e
Andrew Su <asu@pivotal.io>
2018-11-12 17:46:10 -0500

    Remove legacy dependency on bosh-cpi package in registry
    [#157669754](https://www.pivotaltracker.com/story/show/157669754)
    
    Signed-off-by: Gaurab Dey <gdey@pivotal.io>

--- src/bosh-registry/lib/bosh/registry/client.rb
+++
@@ -1,8 +0,0 @@
-require 'bosh/cpi/registry_client'
-
-# Shim for legacy usage (https://www.pivotaltracker.com/story/show/116920309)
-# Also, note that the dependency on 'bosh_cpi' gem can be removed once this
-# shim class is deleted.
-module Bosh::Registry
-  class Client < Bosh::Cpi::RegistryClient ; end
-end
