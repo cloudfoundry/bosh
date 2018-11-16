cloudfoundry/bosh

commit 5e7131549937aba82a471a0516c5e92d4e8e87f5

Allow parallel upload of the same stemcell
[#118979067](https://www.pivotaltracker.com/story/show/118979067)

Co-authored-by: Sebastian Heid <sebastian.heid@sap.com>

src/bosh-director/lib/bosh/director/jobs/update_stemcell.rb
@@ -3,6 +3,7 @@ require 'securerandom'
 module Bosh::Director
   module Jobs
     class UpdateStemcell < BaseJob
+      include Bosh::Director::LockHelper
       include ValidationHelper
       include DownloadHelper
       @queue = :normal
@@ -75,58 +76,60 @@ module Bosh::Director
 
         stemcell = nil
         cloud_factory = CloudFactory.create
-        cloud_factory.all_names.each do |cpi|
-          cloud = cloud_factory.get(cpi)
-          cpi_suffix = " (cpi: #{cpi})" unless cpi.blank?
+        with_stemcell_lock(@name, @version, timeout: 900) do
+          cloud_factory.all_names.each do |cpi|
+            cloud = cloud_factory.get(cpi)
+            cpi_suffix = " (cpi: #{cpi})" unless cpi.blank?
 
-          unless is_supported?(cloud, cpi)
-            logger.info("#{cpi_suffix} cpi does not support stemcell format")
+            unless is_supported?(cloud, cpi)
+              logger.info("#{cpi_suffix} cpi does not support stemcell format")
+              Models::StemcellUpload.find_or_create(
+                name: @name,
+                cpi: cpi,
+                version: @version,
+              )
+              next
+            end
+
+            track_and_log("Checking if this stemcell already exists#{cpi_suffix}") do
+              begin
+                stemcell = @stemcell_manager.find_by_name_and_version_and_cpi(@name, @version, cpi)
+              rescue StemcellNotFound
+                stemcell = Models::Stemcell.new
+                stemcell.name = @name
+                stemcell.operating_system = @operating_system
+                stemcell.version = @version
+                stemcell.sha1 = @sha1
+                stemcell.cpi = cpi
+                stemcell.api_version = @api_version
+              end
+            end
+
+            needs_upload = @fix || !stemcell.cid
+            upload_suffix = ' (already exists, skipped)' unless needs_upload
+            track_and_log("Uploading stemcell #{@name}/#{@version} to the cloud#{cpi_suffix}#{upload_suffix}") do
+              if needs_upload
+                stemcell.cid = cloud.create_stemcell(@stemcell_image, @cloud_properties)
+                logger.info("Cloud created stemcell#{cpi_suffix}: #{stemcell.cid}")
+              else
+                logger.info("Skipping stemcell upload, already exists#{cpi_suffix}")
+              end
+            end
+
+            track_and_log("Save stemcell #{@name}/#{@version} (#{stemcell.cid})#{cpi_suffix}#{upload_suffix}") do
+              if needs_upload
+                stemcell.save
+              else
+                logger.info("Skipping stemcell save, already exists#{cpi_suffix}")
+              end
+            end
+
             Models::StemcellUpload.find_or_create(
               name: @name,
               cpi: cpi,
               version: @version,
             )
-            next
           end
-
-          track_and_log("Checking if this stemcell already exists#{cpi_suffix}") do
-            begin
-              stemcell = @stemcell_manager.find_by_name_and_version_and_cpi(@name, @version, cpi)
-            rescue StemcellNotFound
-              stemcell = Models::Stemcell.new
-              stemcell.name = @name
-              stemcell.operating_system = @operating_system
-              stemcell.version = @version
-              stemcell.sha1 = @sha1
-              stemcell.cpi = cpi
-              stemcell.api_version = @api_version
-            end
-          end
-
-          needs_upload = @fix || !stemcell.cid
-          upload_suffix = ' (already exists, skipped)' unless needs_upload
-          track_and_log("Uploading stemcell #{@name}/#{@version} to the cloud#{cpi_suffix}#{upload_suffix}") do
-            if needs_upload
-              stemcell.cid = cloud.create_stemcell(@stemcell_image, @cloud_properties)
-              logger.info("Cloud created stemcell#{cpi_suffix}: #{stemcell.cid}")
-            else
-              logger.info("Skipping stemcell upload, already exists#{cpi_suffix}")
-            end
-          end
-
-          track_and_log("Save stemcell #{@name}/#{@version} (#{stemcell.cid})#{cpi_suffix}#{upload_suffix}") do
-            if needs_upload
-              stemcell.save
-            else
-              logger.info("Skipping stemcell save, already exists#{cpi_suffix}")
-            end
-          end
-
-          Models::StemcellUpload.find_or_create(
-            name: @name,
-            cpi: cpi,
-            version: @version,
-          )
         end
 
         if stemcell.nil?

src/bosh-director/spec/unit/jobs/update_stemcell_spec.rb
@@ -15,6 +15,7 @@ describe Bosh::Director::Jobs::UpdateStemcell do
   describe '#perform' do
     let(:cloud) { instance_double(Bosh::Clouds::ExternalCpi) }
 
+    let(:task) { Bosh::Director::Models::Task.make(state: 'processing') }
     let(:event_log) { Bosh::Director::EventLog::Log.new }
     let(:event_log_stage) { instance_double(Bosh::Director::EventLog::Stage) }
     let(:verify_multidigest_exit_status) { instance_double(Process::Status, exitstatus: 0) }
@@ -34,6 +35,9 @@ describe Bosh::Director::Jobs::UpdateStemcell do
       allow(event_log).to receive(:begin_stage).and_return(event_log_stage)
       allow(event_log_stage).to receive(:advance_and_track).and_yield [nil]
       allow(Open3).to receive(:capture3).and_return([nil, 'some error', verify_multidigest_exit_status])
+
+      allow(Bosh::Director::Config).to receive_message_chain(:current_job, :username).and_return(task.username)
+      allow(Bosh::Director::Config).to receive_message_chain(:current_job, :task_id).and_return(task.id)
     end
 
     context 'when the stemcell tarball is valid' do
@@ -67,6 +71,7 @@ describe Bosh::Director::Jobs::UpdateStemcell do
           expect(event_log_stage).to receive(:advance_and_track).exactly(expected_steps).times
 
           update_stemcell_job = Bosh::Director::Jobs::UpdateStemcell.new(@stemcell_file.path)
+          expect(update_stemcell_job).to receive(:with_stemcell_lock).with('jeos', '5', timeout: 900).and_yield
           update_stemcell_job.perform
 
           stemcell = Bosh::Director::Models::Stemcell.find(name: 'jeos', version: '5')

commit 4a03ec68388807b384009cee9443bb01d8a72933

Fix broken tests; improve stemcell timeout
Since we are locking the stemcell upload, new events for acquiring and
releasing the locks are generated. In case of multi-CPI, we increase the
timeout depending on the number of CPIs.

Broken since 5e7131549937aba82a471a0516c5e92d4e8e87f5

[#118979067](https://www.pivotaltracker.com/story/show/118979067)

Co-authored-by: Danny Berger <dberger@pivotal.io>

src/bosh-director/lib/bosh/director/jobs/update_stemcell.rb
@@ -76,7 +76,7 @@ module Bosh::Director
 
         stemcell = nil
         cloud_factory = CloudFactory.create
-        with_stemcell_lock(@name, @version, timeout: 900) do
+        with_stemcell_lock(@name, @version, timeout: 900 * cloud_factory.all_names.length) do
           cloud_factory.all_names.each do |cpi|
             cloud = cloud_factory.get(cpi)
             cpi_suffix = " (cpi: #{cpi})" unless cpi.blank?

src/bosh-director/spec/unit/jobs/update_stemcell_spec.rb
@@ -308,7 +308,7 @@ describe Bosh::Director::Jobs::UpdateStemcell do
           expect(cloud2).to receive(:info).and_return('stemcell_formats' => ['dummy1'])
           expect(cloud3).to receive(:info).and_return('stemcell_formats' => ['dummy'])
 
-          expect(cloud_factory).to receive(:all_names).twice.and_return(%w[cloud1 cloud2 cloud3])
+          expect(cloud_factory).to receive(:all_names).exactly(3).times.and_return(%w[cloud1 cloud2 cloud3])
           expect(cloud_factory).to receive(:get).with('cloud1').and_return(cloud1)
           expect(cloud_factory).to receive(:get).with('cloud2').and_return(cloud2)
           expect(cloud_factory).to receive(:get).with('cloud3').and_return(cloud3)
@@ -330,6 +330,8 @@ describe Bosh::Director::Jobs::UpdateStemcell do
           end
           # seems that rspec already subtracts the expected messages above, so we have to subtract them from the expected overall count
           expect(event_log_stage).to receive(:advance_and_track).exactly(expected_steps - step_messages.count - 3).times
+
+          expect(subject).to receive(:with_stemcell_lock).with('jeos', '5', timeout: 900 * 3).and_yield
 
           subject.perform
 
@@ -358,7 +360,7 @@ describe Bosh::Director::Jobs::UpdateStemcell do
           expect(cloud1).to receive(:create_stemcell).with(anything, 'ram' => '2gb').and_raise('I am flaky')
           expect(cloud1).to receive(:info).and_return('stemcell_formats' => ['dummy'])
           expect(cloud_factory).to receive(:get).with('cloud1').and_return(cloud1)
-          expect(cloud_factory).to receive(:all_names).twice.and_return(['cloud1'])
+          expect(cloud_factory).to receive(:all_names).exactly(3).times.and_return(['cloud1'])
 
           expect { subject.perform }.to raise_error 'I am flaky'
 
@@ -372,7 +374,7 @@ describe Bosh::Director::Jobs::UpdateStemcell do
           end
 
           it 'creates one stemcell and one stemcell match per cpi' do
-            expect(cloud_factory).to receive(:all_names).twice.and_return(%w[cloud1 cloud2 cloud3])
+            expect(cloud_factory).to receive(:all_names).exactly(3).times.and_return(%w[cloud1 cloud2 cloud3])
             expect(cloud_factory).to receive(:get).with('cloud1').and_return(cloud1)
             expect(cloud_factory).to receive(:get).with('cloud2').and_return(cloud2)
             expect(cloud_factory).to receive(:get).with('cloud3').and_return(cloud3)
@@ -410,7 +412,7 @@ describe Bosh::Director::Jobs::UpdateStemcell do
           expect(cloud).to receive(:create_stemcell).with(anything, 'ram' => '2gb').and_return('stemcell-cid')
 
           expect(cloud_factory).to receive(:get_cpi_aliases).with('').and_return([''])
-          expect(cloud_factory).to receive(:all_names).twice.and_return([''])
+          expect(cloud_factory).to receive(:all_names).exactly(3).times.and_return([''])
           expect(cloud_factory).to receive(:get).with('').and_return(cloud)
 
           expected_steps = 5

src/spec/gocli/integration/cli_events_spec.rb
@@ -86,6 +86,8 @@ describe 'cli: events', type: :integration do
       {'action' => 'acquire', 'context' => '', 'deployment' => 'simple', 'error' => '', 'instance' => '', 'object_name' => 'lock:deployment:simple', 'object_type' => 'lock'},
       {'action' => 'release', 'context' => '', 'deployment' => '', 'error' => '', 'instance' => '', 'object_name' => 'lock:release:bosh-release', 'object_type' => 'lock'},
       {'action' => 'acquire', 'context' => '', 'deployment' => '', 'error' => '', 'instance' => '', 'object_name' => 'lock:release:bosh-release', 'object_type' => 'lock'},
+      {'action' => 'release', 'context' => '', 'deployment' => '', 'error' => '', 'instance' => '', 'object_name' => 'lock:stemcells:ubuntu-stemcell:1', 'object_type' => 'lock'},
+      {'action' => 'acquire', 'context' => '', 'deployment' => '', 'error' => '', 'instance' => '', 'object_name' => 'lock:stemcells:ubuntu-stemcell:1', 'object_type' => 'lock'},
     )
 
     bosh_events_output

src/spec/gocli/integration/events_access_spec.rb
@@ -29,21 +29,21 @@ describe 'events endpoint access', type: :integration do
     output, exit_code = run_events_cmd(team_client_read_env)
 
     expect(exit_code).to eq(0)
-    expect(output).to include '37 events'
+    expect(output).to include '39 events'
   end
 
   it 'bosh.teams.X.admin should be able to see all events in the director' do
     output, exit_code = run_events_cmd(team_client_admin_env)
 
     expect(exit_code).to eq(0)
-    expect(output).to include '37 events'
+    expect(output).to include '39 events'
   end
 
   it 'bosh.X.admin should be able to see all events in the director' do
     output, exit_code = run_events_cmd(director_client_env)
 
     expect(exit_code).to eq(0)
-    expect(output).to include '37 events'
+    expect(output).to include '39 events'
   end
 
   it 'no-access should not be able to see all events in the director' do