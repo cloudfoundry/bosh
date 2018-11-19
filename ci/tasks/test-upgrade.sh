11308755ce3d91152b763f1b7cf5674b52f73aa0
Danny Berger <dberger@pivotal.io>
2018-11-19 10:15:15 -0800

    Use generic version of postgres in the Docker image
    This reverts 9466e42bd6d575027a4fe0edc16cbe2a84edf7d7
    
    [#161943047](https://www.pivotaltracker.com/story/show/161943047)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ci/tasks/test-upgrade.sh
+++ ci/tasks/test-upgrade.sh
@@ -18,7 +18,7 @@ case "$DB" in
     sudo service mysql start
     ;;
   postgresql)
-    export PATH=/usr/lib/postgresql/9.4/bin:$PATH
+    export PATH=$( echo /usr/lib/postgresql/*/bin ):$PATH
 
     mkdir /tmp/postgres
     mount -t tmpfs -o size=512M tmpfs /tmp/postgres
@@ -26,7 +26,7 @@ case "$DB" in
     chown postgres:postgres /tmp/postgres/data
 
     su postgres -c '
-      export PATH=/usr/lib/postgresql/9.4/bin:$PATH
+      export PATH=$( echo /usr/lib/postgresql/*/bin ):$PATH
       export PGDATA=/tmp/postgres/data
       export PGLOGS=/tmp/log/postgres
       mkdir -p $PGDATA
