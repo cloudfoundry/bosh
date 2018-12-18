cloudfoundry/bosh

commit 39748765525aa13f084a27f0903c65671f978ff8

Use consistent names for generating link group IDs
Previously we were conflating the link provider intent's original name
and alias name when generating and storing link group IDs. This change
introduces the canonical name for a link provider intent, which is used
in all contexts to enforce consistency. The canonical name is the alias
name for explicit links and the original name for implicit links.

[#161942444](https://www.pivotaltracker.com/story/show/161942444)

Co-authored-by: Belinda Liu <bliu@pivotal.io>
Co-authored-by: Josh Aresty <joshua.aresty@emc.com>

src/bosh-director-core/lib/bosh/director/core/templates/job_template_loader.rb
@@ -8,9 +8,10 @@ module Bosh::Director
 
   module Core::Templates
     class JobTemplateLoader
-      def initialize(logger, template_blob_cache, dns_encoder)
+      def initialize(logger, template_blob_cache, link_provider_intents, dns_encoder)
         @logger = logger
         @template_blob_cache = template_blob_cache
+        @link_provider_intents = link_provider_intents
         @dns_encoder = dns_encoder
       end
 
@@ -30,7 +31,13 @@ module Bosh::Director
           source_erbs << SourceErb.new(src_name, dest_name, erb_file, job_template.name)
         end
 
-        JobTemplateRenderer.new(job_template, template_name, monit_source_erb, source_erbs, @logger, @dns_encoder)
+        JobTemplateRenderer.new(job_template: job_template,
+                                template_name: template_name,
+                                monit_erb: monit_source_erb,
+                                source_erbs: source_erbs,
+                                logger: @logger,
+                                link_provider_intents: @link_provider_intents,
+                                dns_encoder: @dns_encoder)
       ensure
         FileUtils.rm_rf(template_dir) if template_dir
       end

src/bosh-director-core/lib/bosh/director/core/templates/job_template_renderer.rb
@@ -11,7 +11,13 @@ module Bosh::Director::Core::Templates
 
     attr_reader :monit_erb, :source_erbs
 
-    def initialize(job_template, template_name, monit_erb, source_erbs, logger, dns_encoder = nil)
+    def initialize(job_template:,
+                   template_name:,
+                   monit_erb:,
+                   source_erbs:,
+                   logger:,
+                   link_provider_intents:,
+                   dns_encoder: nil)
       @links_provided = job_template.model.provides
       @name = job_template.name
       @release = job_template.release
@@ -19,6 +25,7 @@ module Bosh::Director::Core::Templates
       @monit_erb = monit_erb
       @source_erbs = source_erbs
       @logger = logger
+      @link_provider_intents = link_provider_intents
       @dns_encoder = dns_encoder
     end
 
@@ -67,9 +74,7 @@ module Bosh::Director::Core::Templates
     private
 
     def namespace_links_to_current_job(spec)
-      if spec.nil?
-        return nil
-      end
+      return nil if spec.nil?
 
       modified_spec = Bosh::Common::DeepCopy.copy(spec)
 
@@ -85,9 +90,7 @@ module Bosh::Director::Core::Templates
     end
 
     def remove_unused_properties(spec)
-      if spec.nil?
-        return nil
-      end
+      return nil if spec.nil?
 
       modified_spec = Bosh::Common::DeepCopy.copy(spec)
 
@@ -102,13 +105,13 @@ module Bosh::Director::Core::Templates
     end
 
     def links_data(spec)
-      data = @links_provided.map do |provide|
+      data = @link_provider_intents.map do |provider_intent|
         {
-          'name' => provide['name'],
-          'type' => provide['type'],
+          'name' => provider_intent.canonical_name,
+          'type' => provider_intent.type,
           'group' => @dns_encoder.id_for_group_tuple(
             'link',
-            "#{provide['name']}-#{provide['type']}",
+            provider_intent.group_name,
             spec['deployment'],
           ),
         }

src/bosh-director-core/spec/bosh/director/core/templates/job_template_loader_spec.rb
@@ -68,10 +68,11 @@ module Bosh::Director::Core::Templates
   describe JobTemplateLoader do
     describe '#process' do
       subject(:job_template_loader) do
-        JobTemplateLoader.new(logger, TemplateBlobCache.new, dns_encoder)
+        JobTemplateLoader.new(logger, TemplateBlobCache.new, link_provider_intents, dns_encoder)
       end
 
       let(:logger) { double('Logger', debug: nil) }
+      let(:link_provider_intents) { [] }
       let(:dns_encoder) { double('fake dns encoder') }
       let(:release) {double('Bosh::Director::DeploymentPlan::ReleaseVersion', name: 'fake-release-name', version:'0.1')}
 
@@ -116,7 +117,8 @@ module Bosh::Director::Core::Templates
           monit_erb,
           [job_template_erb],
           logger,
-          dns_encoder
+          link_provider_intents,
+          dns_encoder,
         ).and_return fake_renderer
 
         generated_renderer = job_template_loader.process(job)
@@ -150,7 +152,8 @@ module Bosh::Director::Core::Templates
           monit_erb,
           [],
           logger,
-          dns_encoder
+          link_provider_intents,
+          dns_encoder,
         ).and_return fake_renderer
 
         generated_renderer = job_template_loader.process(job)

src/bosh-director-core/spec/bosh/director/core/templates/job_template_renderer_spec.rb
@@ -38,9 +38,18 @@ module Bosh::Director::Core::Templates
       end
       let(:logger) { instance_double('Logger', debug: nil) }
       let(:dns_encoder) { double('some DNS encoder') }
+      let(:link_provider_intents) { [] }
 
       subject(:job_template_renderer) do
-        JobTemplateRenderer.new(job_template, 'template-name', monit_erb, [source_erb], logger, dns_encoder)
+        JobTemplateRenderer.new(
+          job_template: job_template,
+          template_name: 'template-name',
+          monit_erb: monit_erb,
+          source_erbs: [source_erb],
+          logger: logger,
+          link_provider_intents: link_provider_intents,
+          dns_encoder: dns_encoder,
+        )
       end
 
       context 'when templates do not contain local properties' do
@@ -179,12 +188,14 @@ module Bosh::Director::Core::Templates
           }
         end
 
+        let(:provider_intent) do
+          double('provider_intent', canonical_name: 'my-link', type: 'type1', group_name: 'my-link-type1')
+        end
+
+        let(:link_provider_intents) { [provider_intent] }
+
         before do
           allow(Bosh::Template::EvaluationContext).to receive(:new)
-          allow(job_template_model).to receive(:provides).and_return([{
-            'name' => 'LINKNAME',
-            'type' => 'LINKTYPE',
-          }])
           allow(dns_encoder).to receive(:id_for_group_tuple).and_return('group_id')
         end
 
@@ -197,7 +208,7 @@ module Bosh::Director::Core::Templates
           rendered_files = job_template_renderer.render(raw_spec).templates
           expect(dns_encoder).to have_received(:id_for_group_tuple).with(
             'link',
-            'LINKNAME-LINKTYPE',
+            provider_intent.group_name,
             nil,
           )
 
@@ -206,8 +217,8 @@ module Bosh::Director::Core::Templates
           expect(rendered_links_file.dest_name).to(eq('.bosh/links.json'))
 
           expect(JSON.parse(rendered_links_file.contents)).to(eq([{
-            'name' => 'LINKNAME',
-            'type' => 'LINKTYPE',
+            'name' => provider_intent.canonical_name,
+            'type' => provider_intent.type,
             'group' => 'group_id',
           }]))
         end

src/bosh-director/lib/bosh/director/cloudcheck_helper.rb
@@ -66,13 +66,14 @@ module Bosh::Director
       instance_plan_to_create = create_instance_plan(instance_model)
 
       Bosh::Director::Core::Templates::TemplateBlobCache.with_fresh_cache do |template_cache|
-        vm_creator(template_cache, dns_encoder).create_for_instance_plan(
-          instance_plan_to_create,
-          planner.ip_provider,
-          Array(instance_model.managed_persistent_disk_cid),
-          instance_plan_to_create.tags,
-          true,
-        )
+        vm_creator(template_cache, dns_encoder, planner.link_provider_intents)
+          .create_for_instance_plan(
+            instance_plan_to_create,
+            planner.ip_provider,
+            Array(instance_model.managed_persistent_disk_cid),
+            instance_plan_to_create.tags,
+            true,
+          )
 
         powerdns_manager = PowerDnsManagerProvider.create
         local_dns_manager = LocalDnsManager.create(Config.root_domain, @logger)
@@ -197,9 +198,9 @@ module Bosh::Director
       @vm_deleter ||= VmDeleter.new(@logger, false, Config.enable_virtual_delete_vms)
     end
 
-    def vm_creator(template_cache, dns_encoder)
+    def vm_creator(template_cache, dns_encoder, link_provider_intents)
       agent_broadcaster = AgentBroadcaster.new
-      @vm_creator ||= VmCreator.new(@logger, template_cache, dns_encoder, agent_broadcaster)
+      @vm_creator ||= VmCreator.new(@logger, template_cache, dns_encoder, agent_broadcaster, link_provider_intents)
     end
 
     def validate_spec(spec)

src/bosh-director/lib/bosh/director/deployment_plan/compilation_instance_pool.rb
@@ -32,6 +32,7 @@ module Bosh::Director
               deployment_plan.template_blob_cache,
               LocalDnsEncoderManager.create_dns_encoder(deployment_plan.use_short_dns_addresses?, deployment_plan.use_link_dns_names?),
               AgentBroadcaster.new,
+              deployment_plan.link_provider_intents,
             ),
             logger,
           )

src/bosh-director/lib/bosh/director/deployment_plan/planner.rb
@@ -324,6 +324,10 @@ module Bosh::Director
       def is_deploy?
         @is_deploy_action
       end
+
+      def link_provider_intents
+        model.link_providers.flat_map(&:intents)
+      end
     end
   end
 end

src/bosh-director/lib/bosh/director/deployment_plan/stages/update_stage.rb
@@ -2,12 +2,13 @@ module Bosh::Director
   module DeploymentPlan
     module Stages
       class UpdateStage
-        def initialize(base_job, deployment_plan, multi_instance_group_updater, dns_encoder)
+        def initialize(base_job, deployment_plan, multi_instance_group_updater, dns_encoder, link_provider_intents)
           @base_job = base_job
           @logger = base_job.logger
           @deployment_plan = deployment_plan
           @multi_instance_group_updater = multi_instance_group_updater
           @dns_encoder = dns_encoder
+          @link_provider_intents = link_provider_intents
         end
 
         def perform
@@ -33,7 +34,13 @@ module Bosh::Director
           template_blob_cache = @deployment_plan.template_blob_cache
           agent_broadcaster = AgentBroadcaster.new
           vm_deleter = Bosh::Director::VmDeleter.new(@logger, false, Config.enable_virtual_delete_vms)
-          @vm_creator = Bosh::Director::VmCreator.new(@logger, template_blob_cache, @dns_encoder, agent_broadcaster)
+          @vm_creator = Bosh::Director::VmCreator.new(
+            @logger,
+            template_blob_cache,
+            @dns_encoder,
+            agent_broadcaster,
+            @link_provider_intents,
+          )
         end
 
         def setup_stage

commit bbaba85e9bc8228dc7f25bab0a7db7a998fa855c

Fix update_stage_functional_spec.rb
Broken since 39748765525aa13f084a27f0903c65671f978ff8

[#161942444](https://www.pivotaltracker.com/story/show/161942444)

Co-authored-by: Belinda Liu <bliu@pivotal.io>

src/bosh-director/spec/unit/deployment_plan/stages/update_stage_functional_spec.rb
@@ -7,7 +7,10 @@ module Bosh::Director::DeploymentPlan::Stages
 
     let(:agent_client) { instance_double(Bosh::Director::AgentClient) }
     let(:dns_encoder) { Bosh::Director::DnsEncoder.new({}) }
-    let(:update_step) { UpdateStage.new(base_job, deployment_plan, multi_instance_group_updater, dns_encoder) }
+    let(:link_provider_intents) { [] }
+    let(:update_step) do
+      UpdateStage.new(base_job, deployment_plan, multi_instance_group_updater, dns_encoder, link_provider_intents)
+    end
 
     let(:base_job) { Bosh::Director::Jobs::BaseJob.new }
     let(:assembler) { Assembler.new(deployment_plan, nil, nil, variables_interpolator) }
@@ -181,7 +184,12 @@ module Bosh::Director::DeploymentPlan::Stages
     context 'when the director database contains no instances' do
       let(:multi_instance_group_updater) do
         Bosh::Director::DeploymentPlan::SerialMultiInstanceGroupUpdater.new(
-          Bosh::Director::InstanceGroupUpdaterFactory.new(logger, deployment_plan.template_blob_cache, dns_encoder)
+          Bosh::Director::InstanceGroupUpdaterFactory.new(
+            logger,
+            deployment_plan.template_blob_cache,
+            dns_encoder,
+            link_provider_intents,
+          ),
         )
       end
 