22a6cb46c36f08356e0ec6802046246ff6eb9df1
Miguel Verissimo <mverissimo@pivotal.io>
2019-01-15 11:57:55 -0500

    Bring back the db migration test
    - deleted by accident in 983b1ba4e
    
    Signed-off-by: Gaurab Dey <gdey@pivotal.io>

---
+++ src/bosh-director/spec/unit/db/migrations/director/add_data_migration_spec.rb
@@ -0,0 +1,15 @@
+require 'db_spec_helper'
+
+module Bosh::Director
+  describe 'Reminder to add test covering db migration while data exist' do
+    it 'should have written a test for latest migration script that was added' do
+      latest_db_migration_file = DBSpecHelper.get_latest_migration_script
+
+      # This is an explicit reminder to write a test which covers migrating DB while it was already
+      # populated with data. This test will fail every time a new migration script is added. Change
+      # the file name below to the latest when a test is added.
+      # Look at tests in this directory for similar examples: bosh-director/spec/unit/db/migrations/director
+      expect(latest_db_migration_file).to eq('20190114153103_add_index_to_tasks.rb')
+    end
+  end
+end
