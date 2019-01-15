765b9b3eb863abdf18c648e5eb90477ceb23af54
Joshua Aresty <joshua.aresty@emc.com>
2019-01-15 15:13:26 -0800

    Consolidate metrics brats tes into a single test
    This change updates the metrics tests to all be tested in a single It
    block. These tests are expensive, so we should only need to deploy the
    inner bosh deployment once.
    
    [#163241835](https://www.pivotaltracker.com/story/show/163241835)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/metrics_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/metrics_test.go
@@ -12,21 +12,23 @@ import (
 var _ = Describe("nginx with ngx_http_stub_status_module compiled", func() {
 	BeforeEach(func() {
 		bratsutils.StartInnerBosh(
-			"-o", bratsutils.AssetPath("ops-nginx-enable-metrics.yml"),
+			"-o", bratsutils.AssetPath("ops-enable-metrics.yml"),
 		)
 	})
 
-	It("returns metrics when curling /stats director nginx endpoint", func() {
+	It("returns metrics when curling metrics endpoints for nginx, blobstore, and nats", func() {
 		session := bratsutils.OuterBosh("-d", bratsutils.InnerBoshDirectorName(), "ssh", "bosh", "-c", "curl -vk https://127.0.0.1:25555/stats")
 		Eventually(session, time.Minute).Should(gexec.Exit(0))
 		Expect(string(session.Out.Contents())).To(ContainSubstring("HTTP/1.1 200 OK"))
 		Expect(string(session.Out.Contents())).To(ContainSubstring("server accepts handled requests"))
-	})
 
-	It("returns metrics when curling /stats bloblstore nginx endpoint", func() {
 		session := bratsutils.OuterBosh("-d", bratsutils.InnerBoshDirectorName(), "ssh", "bosh", "-c", "curl -vk https://127.0.0.1:25250/stats")
 		Eventually(session, time.Minute).Should(gexec.Exit(0))
 		Expect(string(session.Out.Contents())).To(ContainSubstring("HTTP/1.1 200 OK"))
 		Expect(string(session.Out.Contents())).To(ContainSubstring("server accepts handled requests"))
+
+		session := bratsutils.OuterBosh("-d", bratsutils.InnerBoshDirectorName(), "ssh", "bosh", "-c", "curl -v http://localhost:8222")
+		Eventually(session, time.Minute).Should(gexec.Exit(0))
+		Expect(string(session.Out.Contents())).To(ContainSubstring("HTTP/1.1 200 OK"))
 	})
 })
