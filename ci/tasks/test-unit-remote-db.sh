4fabf8915fa4c71109bccf2b84c02f5626f79625
James Myers <jfmyers9@gmail.com>
2019-01-15 22:45:17 -0800

    Use `--no-document` rather than `--no-ri --no-rdoc`

--- ci/tasks/test-unit-remote-db.sh
+++ ci/tasks/test-unit-remote-db.sh
@@ -19,7 +19,7 @@ bundle exec rake --trace spec:unit:migrations
 
 if [ "$DB" = "mysql" ]; then
 	#Restart rds mysql
-	gem install aws-sdk --no-ri --no-rdoc
-	gem install aws-sdk-core --no-ri --no-rdoc
+	gem install aws-sdk --no-document
+	gem install aws-sdk-core --no-document
 	ruby ../ci/tasks/test-unit-remote-reboot-db.rb
 fi
