f4be4f8ac3f827eff6d342fc94b4ce48e378319b
Sebastian Heid <sebastian.heid@sap.com>
2019-01-29 17:33:32 +0100

    Spike: Fix corrupt state issue in attach_disk
    [#162780889](https://www.pivotaltracker.com/story/show/162780889)
    
    Co-authored-by: Beyhan Veli <beyhan.veli@sap.com>

--- src/bosh-director/lib/bosh/director/disk_manager.rb
+++ src/bosh-director/lib/bosh/director/disk_manager.rb
@@ -10,6 +10,7 @@ module Bosh::Director
     def update_persistent_disk(instance_plan)
       @logger.info('Updating persistent disk')
 
+      reattach_disks_if_needed(instance_plan)
       check_persistent_disk(instance_plan) unless has_multiple_persistent_disks?(instance_plan)
 
       return unless instance_plan.persistent_disk_changed?
@@ -44,6 +45,17 @@ module Bosh::Director
         detach_disk(disk)
         @orphan_disk_manager.orphan_disk(disk)
       end
+    end
+
+    def reattach_disks_if_needed(instance_plan)
+      instance = instance_plan.instance
+      return if instance.model.persistent_disks.empty?
+
+      agent_disk_cid = agent_mounted_disks(instance.model).first
+      return unless agent_disk_cid.nil? && agent_disk_cid != instance.model.managed_persistent_disk_cid
+
+      @logger.info("Re-attaching existing persistent disk #{instance.model.managed_persistent_disk_cid}")
+      attach_disks_if_needed(instance_plan)
     end
 
     def attach_disks_if_needed(instance_plan)
