71c2ab6ecb2de5e94379a1f7e07c57ca1823a3a9
Maya Rosecrance <mrosecrance@pivotal.io>
2019-03-15 14:44:13 -0700

    Allow operators to opt out of blobstore backup and restore
    - blobstore backups can be very large and cause backups and restores to
    take an unacceptably long time.
    
    More discussion on the topic here:
    https://github.com/cloudfoundry/bosh/issues/2032
    
    [#159826426](https://www.pivotaltracker.com/story/show/159826426)
    
    Co-authored-by: Danny Berger <dberger@pivotal.io>

---
+++ spec/blobstore_bbr_backup_restore_spec.rb
@@ -0,0 +1,69 @@
+require 'rspec'
+require 'tempfile'
+require 'bosh/template/test'
+
+describe 'blobstore' do
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '..')) }
+  let(:job) { release.job('blobstore') }
+  let(:rendered) { template.render(properties) }
+  let(:tempfile) { Tempfile.new('bbr_backup') }
+  let(:tmpdir) { Dir.mktmpdir }
+  let(:properties) { {} }
+
+  after do
+    File.unlink(tempfile.path)
+    FileUtils.remove_entry(tmpdir)
+  end
+
+  describe 'bin/bbr/backup' do
+    let(:template) { job.template('bin/bbr/backup') }
+
+    context 'blobstore.bbr.enabled = false' do
+      let(:properties) do
+        { 'blobstore' => { 'bbr' => { 'enabled' => false } } }
+      end
+
+      it 'logs that it skipped and does not backup the blobstore' do
+        tempfile.write(rendered)
+        tempfile.close
+
+        File.chmod(0o755, tempfile.path)
+
+        output = `ARTIFACT_DIRECTORY=/#{tmpdir} bash #{tempfile.path} 2>&1`
+        expect(output).to eq "job property 'blobstore.bbr.enabled' is disabled\n"
+      end
+    end
+  end
+
+  describe 'bin/bbr/restore' do
+    let(:template) { job.template('bin/bbr/restore') }
+
+    context 'blobstore.bbr.enabled = false' do
+      let(:properties) do
+        { 'blobstore' => { 'bbr' => { 'enabled' => false } } }
+      end
+
+      it 'logs that it skipped and does not restore the blobstore' do
+        tempfile.write(rendered)
+        tempfile.close
+        File.chmod(0o755, tempfile.path)
+
+        output = `bash #{tempfile.path} 2>&1`
+        expect(output).to eq "restore skipped because job property 'blobstore.bbr.enabled' is disabled\n"
+      end
+    end
+
+    context 'bbr is enabled but the backup artifact had bbr disabled' do
+      it 'logs that it skipped and does not restore the blobstore' do
+        tempfile.write(rendered)
+        tempfile.close
+        File.chmod(0o755, tempfile.path)
+
+        File.open("#{tmpdir}/backup-skipped", 'w') { |w| w.write('fake-reason') }
+
+        output = `ARTIFACT_DIRECTORY=/#{tmpdir} bash #{tempfile.path} 2>&1`
+        expect(output).to eq "restore skipped: backup artifact says: fake-reason\n"
+      end
+    end
+  end
+end
