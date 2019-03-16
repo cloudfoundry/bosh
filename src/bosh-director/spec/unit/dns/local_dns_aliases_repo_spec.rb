492aa24fb5b7c1fa5a69681194a92868a0bb92aa
James Myers <jmyers@pivotal.io>
2019-03-15 18:12:46 -0700

    Fix broken unit test

--- src/bosh-director/spec/unit/dns/local_dns_aliases_repo_spec.rb
+++ src/bosh-director/spec/unit/dns/local_dns_aliases_repo_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module Bosh::Director
   describe LocalDnsAliasesRepo do
-    subject(:local_dns_aliases_repo) { LocalDnsAliasesRepo.new(nil, root_domain) }
+    subject(:local_dns_aliases_repo) { LocalDnsAliasesRepo.new(logger, root_domain) }
     let(:root_domain) { 'bosh1.tld' }
 
     describe '#update_for_deployment' do
