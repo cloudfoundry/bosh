e088fdd3837c2993dc5458f3fb2957520835a648
Charles Hansen <chansen@pivotal.io>
2019-03-19 10:23:43 -0700

    Add factories for ManualNetwork and JobNetwork

--- src/bosh-director/spec/factories.rb
+++ src/bosh-director/spec/factories.rb
@@ -11,12 +11,34 @@ FactoryBot.define do
   end
 end
 
+FactoryBot.define do
+  factory :manual_network, class: Bosh::Director::DeploymentPlan::ManualNetwork do
+    name { 'manual-network-name' }
+    subnets { [] }
+    logger { Logging::Logger.new('TestLogger') }
+    managed { false }
+
+    initialize_with { new(name, subnets, logger, managed) }
+  end
+end
+
+FactoryBot.define do
+  factory :job_network, class: Bosh::Director::DeploymentPlan::JobNetwork do
+    name { 'job-network-name' }
+    static_ips { [] }
+    default_for { [] }
+    association :deployment_network, factory: :manual_network, strategy: :build
+
+    initialize_with { new(name, static_ips, default_for, deployment_network) }
+  end
+end
+
 module Bosh::Director
   module DeploymentPlan
-    [Stemcell].each do |klass|
+    [Stemcell, ManualNetwork, JobNetwork].each do |klass|
       klass.class_eval do
         def self.make(*args)
-          FactoryBot.build(name.demodulize.downcase.to_sym, *args)
+          FactoryBot.build(name.demodulize.underscore.to_sym, *args)
         end
       end
     end
