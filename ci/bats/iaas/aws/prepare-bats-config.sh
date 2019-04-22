58e9d35e38a7e91dd8469d68df1a666f57a23e9e
Joshua Aresty <joshua.aresty@emc.com>
2019-04-22 16:59:01 -0700

    Take bosh-cli as an input
    This was failing because it relied on a CLI that was embedded in the
    docker image instead of taking one in as a concourse input. Now it
    behaves like the rest of the tasks in the pipeline and takes one in from
    outside.
    
    [#165478841]

--- ci/bats/iaas/aws/prepare-bats-config.sh
+++ ci/bats/iaas/aws/prepare-bats-config.sh
@@ -5,6 +5,9 @@ set -e
 state_path() { bosh-cli int director-state/director.yml --path="$1" ; }
 creds_path() { bosh-cli int director-state/director-creds.yml --path="$1" ; }
 
+chmod +x bosh-cli/alpha-bosh-cli-*-linux-amd64
+bosh_cli="$(command -v bosh-cli/alpha-bosh-cli-*-linux-amd64)"
+
 cat > bats-config/bats.env <<EOF
 export BOSH_ENVIRONMENT="$( state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null )"
 export BOSH_CLIENT="admin"
@@ -46,7 +49,7 @@ properties:
       security_groups: ((SecurityGroupID))
 EOF
 
-bosh-cli interpolate \
+"$bosh_cli" interpolate \
  --vars-file environment/metadata \
  -v STEMCELL_NAME=$STEMCELL_NAME \
  interpolate.yml \
