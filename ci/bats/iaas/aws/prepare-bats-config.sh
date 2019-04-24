d7819ef5446f1fae334c4671da10187a0652a992
Belinda Liu <bliu@pivotal.io>
2019-04-24 10:28:26 -0700

    Backport: switch compiled-release to main-ruby-go docker
    The old bosh/compiled-release docker image would install the old
    ruby-cli which we no longer want to include. Switching to the main-ruby-go
    image also requires taking in the bosh-cli as an input for builds.
    
    [#165497438](https://www.pivotaltracker.com/story/show/165497438)

--- ci/bats/iaas/aws/prepare-bats-config.sh
+++ ci/bats/iaas/aws/prepare-bats-config.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-state_path() { bosh-cli int director-state/director.yml --path="$1" ; }
-creds_path() { bosh-cli int director-state/director-creds.yml --path="$1" ; }
+mv ./bosh-cli/*bosh-cli-*-linux-amd64 /usr/local/bin/bosh
+chmod +x /usr/local/bin/bosh
+
+state_path() { bosh int director-state/director.yml --path="$1" ; }
+creds_path() { bosh int director-state/director-creds.yml --path="$1" ; }
 
 cat > bats-config/bats.env <<EOF
 export BOSH_ENVIRONMENT="$( state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null )"
@@ -46,7 +49,7 @@ properties:
       security_groups: ((SecurityGroupID))
 EOF
 
-bosh-cli interpolate \
+bosh interpolate \
  --vars-file environment/metadata \
  -v STEMCELL_NAME=$STEMCELL_NAME \
  interpolate.yml \
