4a56ee1cc7cc1134cd985ba40b120337089ea528
Belinda Liu <bliu@pivotal.io>
2019-05-01 14:32:02 -0700

    Fix unit tests + rubocop errors
    These unit tests were relying on resource_pools as both a top level key
    and an instance group configuration. This is no longer allowed, as the
    instance group level key has been deprecated.
    
    [#164668124](https://www.pivotaltracker.com/story/show/164668124)
    
    Co-authored-by: Peter Tran <ptran@pivotal.io>

--- src/bosh-director/spec/unit/deployment_plan/stages/update_stage_functional_spec.rb
+++ src/bosh-director/spec/unit/deployment_plan/stages/update_stage_functional_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 module Bosh::Director::DeploymentPlan::Stages
   describe 'deployment prepare & update', truncation: true, :if => ENV.fetch('DB', 'sqlite') != 'sqlite' do
-    let(:deployment) { Bosh::Director::Models::Deployment.make(name: 'fake-deployment') }
-    let!(:stemcell) { Bosh::Director::Models::Stemcell.make({'name' => 'fake-stemcell', 'version' => 'fake-stemcell-version'}) }
+    let(:deployment) { Bosh::Director::Models::Deployment.make(name: deployment_manifest['name']) }
+    let!(:stemcell) { Bosh::Director::Models::Stemcell.make('name' => 'ubuntu-stemcell', 'version' => '1') }
 
     let(:agent_client) { instance_double(Bosh::Director::AgentClient) }
     let(:dns_encoder) { Bosh::Director::DnsEncoder.new({}) }
@@ -14,101 +14,36 @@ module Bosh::Director::DeploymentPlan::Stages
 
     let(:base_job) { Bosh::Director::Jobs::BaseJob.new }
     let(:assembler) { Assembler.new(deployment_plan, nil, nil, variables_interpolator) }
-    let(:cloud_config) { nil }
+    let(:cloud_config) { Bosh::Spec::NewDeployments.simple_cloud_config }
     let(:runtime_configs) { [] }
 
     let(:variables_interpolator) { instance_double(Bosh::Director::ConfigServer::VariablesInterpolator) }
 
     let(:deployment_plan) do
       planner_factory = Bosh::Director::DeploymentPlan::PlannerFactory.create(logger)
-      manifest = Bosh::Director::Manifest.new(deployment_manifest, YAML.dump(deployment_manifest), nil, nil)
-      deployment_plan = planner_factory.create_from_manifest(manifest, cloud_config, runtime_configs, {})
+      manifest = Bosh::Director::Manifest.new(deployment_manifest, YAML.dump(deployment_manifest), cloud_config, nil)
+      deployment_plan = planner_factory.create_from_manifest(manifest, nil, runtime_configs, {})
       Bosh::Director::DeploymentPlan::Assembler.create(deployment_plan, variables_interpolator).bind_models
       deployment_plan
     end
+    let(:static_ip) { Bosh::Spec::NewDeployments.subnet['static'].first }
     let(:deployment_manifest) do
-      {
-        'name' => 'fake-deployment',
-        'instance_groups' => [
-          {
-            'name' => 'fake-job',
-            'instances' => 1,
-            'jobs' => [
-              {
-                'name' => 'fake-template',
-                'release' => 'fake-release',
-              }
-            ],
-            'resource_pool' => 'fake-resource-pool',
-            'networks' => [
-              {
-                'name' => 'fake-network',
-                'static_ips' => ['127.0.0.1']
-              }
-            ],
-          }
-        ],
-        'resource_pools' => [
-          {
-            'name' => 'fake-resource-pool',
-            'size' => 1,
-            'cloud_properties' => {},
-            'stemcell' => {
-              'name' => 'fake-stemcell',
-              'version' => 'fake-stemcell-version',
-            },
-            'network' => 'fake-network',
-            'jobs' => []
-          }
-        ],
-        'networks' => [
-          {
-            'name' => 'fake-network',
-            'type' => 'manual',
-            'cloud_properties' => {},
-            'subnets' => [
-              {
-                'name' => 'fake-subnet',
-                'range' => '127.0.0.0/20',
-                'gateway' => '127.0.0.2',
-                'cloud_properties' => {},
-                'static' => ['127.0.0.1'],
-              }
-            ]
-          }
-        ],
-        'releases' => [
-          {
-            'name' => 'fake-release',
-            'version' => '1.0.0',
-          }
-        ],
-        'compilation' => {
-          'workers' => 1,
-          'network' => 'fake-network',
-          'cloud_properties' => {},
-        },
-        'update' => {
-          'canaries' => 1,
-          'max_in_flight' => 1,
-          'canary_watch_time' => 1,
-          'update_watch_time' => 1,
-        },
-      }
+      Bosh::Spec::NewDeployments.simple_manifest_with_instance_groups(instances: 1, static_ips: static_ip)
     end
-
     let(:cloud) { instance_double(Bosh::Clouds::ExternalCpiResponseWrapper) }
 
-    let(:task) { Bosh::Director::Models::Task.make(:id => 42, :username => 'user') }
+    let(:task) { Bosh::Director::Models::Task.make(id: 42, username: 'user') }
     let(:task_writer) { Bosh::Director::TaskDBWriter.new(:event_output, task.id) }
     let(:event_log) { Bosh::Director::EventLog::Log.new(task_writer) }
     let(:blobstore) { instance_double(Bosh::Blobstore::Sha1VerifiableBlobstoreClient) }
 
     before do
-      release = Bosh::Director::Models::Release.make(name: 'fake-release')
-      release_version = Bosh::Director::Models::ReleaseVersion.make(version: '1.0.0')
+      release = Bosh::Director::Models::Release.make(name: 'bosh-release')
+      release_version = Bosh::Director::Models::ReleaseVersion.make(version: '0.1-dev')
       release.add_version(release_version)
-      template = Bosh::Director::Models::Template.make(name: 'fake-template')
+      template = Bosh::Director::Models::Template.make(
+        name: Bosh::Spec::NewDeployments.simple_instance_group['jobs'].first['name'],
+      )
       release_version.add_template(template)
 
       allow(Bosh::Director::AgentClient).to receive(:with_agent_id).and_return(agent_client)
@@ -135,10 +70,8 @@ module Bosh::Director::DeploymentPlan::Stages
       allow(Bosh::Director::Config).to receive(:name).and_return('fake-director-name')
       allow(Bosh::Director::Config).to receive(:event_log).and_return(event_log)
       allow(Bosh::Director::Config).to receive(:uuid).and_return('meow-uuid')
-      allow(Bosh::Director::Config).to receive(:cloud_options).and_return({'provider' => {'path' => '/path/to/default/cpi'}})
+      allow(Bosh::Director::Config).to receive(:cloud_options).and_return('provider' => { 'path' => '/path/to/default/cpi' })
       allow(Bosh::Director::Config).to receive(:preferred_cpi_api_version).and_return(1)
-      # allow(cloud).to receive(:info)
-      # allow(cloud).to receive(:set_vm_metadata)
       allow(Bosh::Clouds::ExternalCpiResponseWrapper).to receive(:new).with(anything, anything).and_return(cloud)
       allow(variables_interpolator).to receive(:interpolate_template_spec_properties).and_return({})
       allow(variables_interpolator).to receive(:interpolated_versioned_variables_changed?).and_return(false)
@@ -157,7 +90,9 @@ module Bosh::Director::DeploymentPlan::Stages
 
       context 'the agent on the existing VM has the requested static ip but no job instance assigned (due to deploy failure)' do
         context 'the new deployment manifest specifies 1 instance of a job with a static ip' do
-          let(:multi_instance_group_updater) { instance_double('Bosh::Director::DeploymentPlan::SerialMultiInstanceGroupUpdater', run: nil) }
+          let(:multi_instance_group_updater) do
+            instance_double('Bosh::Director::DeploymentPlan::SerialMultiInstanceGroupUpdater', run: nil)
+          end
 
           before do
             deployment.add_job_instance(instance_model)
@@ -170,7 +105,7 @@ module Bosh::Director::DeploymentPlan::Stages
                 anything,
                 stemcell.cid,
                 anything,
-                { 'fake-network' => hash_including('ip' => '127.0.0.1') },
+                { Bosh::Spec::NewDeployments.network['name'] => hash_including('ip' => static_ip) },
                 anything,
                 anything,
               )
@@ -202,7 +137,7 @@ module Bosh::Director::DeploymentPlan::Stages
       end
 
       before do
-        allow(agent_client).to receive(:get_state).and_return({'job_state' => 'running'})
+        allow(agent_client).to receive(:get_state).and_return('job_state' => 'running')
         allow(agent_client).to receive(:prepare)
         allow(agent_client).to receive(:run_script)
         allow(agent_client).to receive(:start)
