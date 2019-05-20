8b342000d0869e83626f3cf3530f5dfe7b3e6221
Maya Rosecrance <mrosecrance@pivotal.io>
2019-05-20 14:30:29 -0700

    Consume lts trusty stemcell for producing compiled releases
    Co-authored-by: Charles Hansen <chansen@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
-fly -t production set-pipeline -p bosh:266.x \
+fly -t pcf set-pipeline -p bosh:266.x \
     -c ci/pipeline.yml \
-    --load-vars-from <(lpass show -G "bosh concourse secrets" --notes) \
+    -l <(lpass show --note "concourse:production pipeline:bosh:stemcells lts") \
+    -l <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
+    -l <(lpass show --note "bosh-lts concourse secrets") \
     -l <(lpass show --note "tracker-bot-story-delivery")
---
+++ ci/tasks/push-bosh-compiled-release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu -o pipefail
+
+cat <<EOF > settings.json
+{
+  "params": {
+    "files": [ "compiled-release/*.tgz" ],
+    "version": "candidate-version/version",
+    "rename_from_file": "metalink-path/file-path"
+  },
+  "source": {
+    "uri": "git+ssh://git@github.com:pivotal-cf/bosh-lts-compiled-releases.git",
+    "mirror_files": [
+      {
+        "destination": "s3://s3-external-1.amazonaws.com/bosh-compiled-releases/bosh/{{.Version}}/{{.Name}}",
+        "env": {
+          "AWS_ACCESS_KEY_ID": "$AWS_ACCESS_KEY_ID",
+          "AWS_SECRET_ACCESS_KEY": "$AWS_SECRET_ACCESS_KEY"
+        }
+      }
+    ],
+    "options": {
+      "private_key": "$(echo "$git_private_key" | tr '\n' '#'|  sed 's/#/\\n/g')"
+    }
+  }
+}
+EOF
+
+cat settings.json | /opt/resource/out $PWD
