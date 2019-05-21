b21384ca0c46a0d83ce9893da2f842474f52cfd2
Rebecca Putinski <rputinski@pivotal.io>
2019-05-21 11:01:11 -0400

    Set host to single space to delete sql user
    Looks like the user is created without a host, and so deleting naively
    with a blank host doesn't work with Google. Setting to a single space
    seems to work on the CLI.

--- ci/tasks/destroy-brats-sql-users.sh
+++ ci/tasks/destroy-brats-sql-users.sh
@@ -12,5 +12,5 @@ mysql_instance_name=$(jq -r .gcp_mysql_instance_name terraform-output/metadata )
 gcloud auth activate-service-account --key-file=$cred_file
 gcloud config set project cf-bosh-core
 
-gcloud sql users delete -q "${GCP_POSTGRES_USERNAME}" --instance "${postgres_instance_name}"
-gcloud sql users delete -q "${GCP_MYSQL_USERNAME}" --instance "${mysql_instance_name}"
+gcloud sql users delete -q "${GCP_POSTGRES_USERNAME}" --instance "${postgres_instance_name}" --host ' '
+gcloud sql users delete -q "${GCP_MYSQL_USERNAME}" --instance "${mysql_instance_name}" --host ' '
