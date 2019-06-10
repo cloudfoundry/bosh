9f4045631e0c5791e394b0d57a8693eedf33160f
James Myers <jmyers@pivotal.io>
2019-06-10 14:25:58 -0700

    Move pipeline to the pcf team
    [#166391315](https://www.pivotaltracker.com/story/show/166391315)

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-fly -t production set-pipeline -p bosh:265.x \
+fly -t pcf set-pipeline -p bosh:265.x \
     -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
