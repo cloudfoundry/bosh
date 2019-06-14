97c12d012d05141b0dd4876422ee8be37c89c8eb
Charles Hansen <chansen@pivotal.io>
2019-06-14 13:06:15 -0700

    Refactor PackageProcessor to not use state
    For improved code readability.
    partially to use as an example for a related PR

--- src/bosh-director/lib/bosh/director/jobs/update_release/package_processor.rb
+++ src/bosh-director/lib/bosh/director/jobs/update_release/package_processor.rb
@@ -2,106 +2,67 @@ module Bosh::Director
   module Jobs
     class UpdateRelease
       class PackageProcessor
-        def initialize(
-            release_version_model,
-            release_model,
-            name,
-            version,
-            manifest_packages,
-            logger
-        )
+        class << self
+          def process(release_version_model, release_model, name, version, manifest_packages, logger)
+            new_packages = []
+            existing_packages = []
+            registered_packages = []
 
-          @release_version_model = release_version_model
-          @release_model = release_model
-          @name = name
-          @version = version
-          @manifest_packages = manifest_packages
-          @logger = logger
+            logger.info('Checking for new packages in release')
 
-          @new_packages = []
-          @existing_packages = []
-          @registered_packages = []
-        end
+            manifest_packages.each do |package_meta|
+              package_meta['compiled_package_sha1'] = package_meta['sha1']
 
-        attr_reader :logger, :manifest_packages
+              validate_package_fingerprint!(package_meta, release_version_model, name, version)
 
-        def self.process(*args)
-          PackageProcessor.new(*args).process
-        end
+              packages = Models::Package.where(fingerprint: package_meta['fingerprint']).order_by(:id).all
 
-        def process
-          logger.info('Checking for new packages in release')
+              existing_package = packages.find do |package|
+                package.release_id == release_model.id &&
+                  package.name == package_meta['name'] &&
+                  package.version == package_meta['version']
+              end
 
-          manifest_packages.each do |package_meta|
-            package_meta['compiled_package_sha1'] = package_meta['sha1']
+              reuse_package_matching_fingerprint(packages, package_meta) unless existing_package&.blobstore_id
 
-            validate_package_fingerprint!(package_meta)
+              unless existing_package
+                new_packages << package_meta
+                next
+              end
 
-            packages = Models::Package.where(fingerprint: package_meta['fingerprint']).order_by(:id).all
-
-            if packages.empty?
-              @new_packages << package_meta
-              next
+              if existing_package.release_versions.include?(release_version_model)
+                registered_packages << [existing_package, package_meta]
+              else
+                existing_packages << [existing_package, package_meta]
+              end
             end
 
-            existing_package = packages.find do |package|
-              package.release_id == @release_model.id &&
-                package.name == package_meta['name'] &&
-                package.version == package_meta['version']
-            end
+            [new_packages, existing_packages, registered_packages]
+          end
 
-            if existing_package
-              use_existing_package(existing_package, packages, package_meta)
-            else
-              reuse_package_matching_fingerprint(packages, package_meta)
+          private
+
+          def validate_package_fingerprint!(package_meta, release_version_model, name, version)
+            # Checking whether we might have the same bits somewhere (in any release, not just the one being uploaded)
+            release_version_model.packages.select { |pv| pv.name == package_meta['name'] }.each do |package|
+              next unless package.fingerprint != package_meta['fingerprint']
+
+              msg = "package '#{package_meta['name']}' had different fingerprint "\
+                    "in previously uploaded release '#{name}/#{version}'"
+              raise ReleaseInvalidPackage, msg
             end
           end
 
-          return [@new_packages, @existing_packages, @registered_packages]
-        end
+          def reuse_package_matching_fingerprint(packages, package_meta)
+            # We found a package with the same fingerprint but different
+            # (release, name, version) tuple, so we need to make a copy
+            # of the package blob and create a new db entry for it
+            reusable_matching_package = packages.find(&:blobstore_id)
+            return unless reusable_matching_package
 
-        private
-
-        def validate_package_fingerprint!(package_meta)
-          # Checking whether we might have the same bits somewhere (in any release, not just the one being uploaded)
-          @release_version_model.packages.select { |pv| pv.name == package_meta['name'] }.each do |package|
-            if package.fingerprint != package_meta['fingerprint']
-              raise ReleaseInvalidPackage, "package '#{package_meta['name']}' had different fingerprint in previously uploaded release '#{@name}/#{@version}'"
-            end
+            package_meta['blobstore_id'] = reusable_matching_package.blobstore_id
+            package_meta['sha1'] = reusable_matching_package.sha1
           end
-        end
-
-        def use_existing_package(existing_package, packages, package_meta)
-          if existing_package.blobstore_id.nil?
-            packages.each do |package|
-              next if package.blobstore_id.nil?
-
-              package_meta['blobstore_id'] = package.blobstore_id
-              package_meta['sha1'] = package.sha1
-              break
-            end
-          end
-
-          if existing_package.release_versions.include?(@release_version_model)
-            @registered_packages << [existing_package, package_meta]
-          else
-            @existing_packages << [existing_package, package_meta]
-          end
-        end
-
-        def reuse_package_matching_fingerprint(packages, package_meta)
-          # We found a package with the same fingerprint but different
-          # (release, name, version) tuple, so we need to make a copy
-          # of the package blob and create a new db entry for it
-          packages.each do |package|
-            next if package.blobstore_id.nil?
-
-            package_meta['blobstore_id'] = package.blobstore_id
-            package_meta['sha1'] = package.sha1
-            break
-          end
-
-          @new_packages << package_meta
         end
       end
     end
