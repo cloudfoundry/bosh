0c229cb5c343659f5d903647c55bd273d48cf7d3
Rebecca Putinski <rputinski@pivotal.io>
2019-08-16 16:35:55 -0400

    Update cloud config before deploying zookeeper

--- ci/tasks/deploy-zookeeper.sh
+++ ci/tasks/deploy-zookeeper.sh
@@ -19,6 +19,12 @@ export BOSH_ENVIRONMENT=$(get_bosh_environment)
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$(bosh-cli int director-state/director-creds.yml --path /admin_password)
 export BOSH_CA_CERT=$(bosh-cli int director-state/director-creds.yml --path /director_ssl/ca)
+export BOSH_NON_INTERACTIVE=true
 
-bosh-cli upload-stemcell stemcell/*.tgz -n
-bosh-cli -d zookeeper deploy --recreate zookeeper-release/manifests/zookeeper.yml -n
+
+CPI=$(bosh-cli env --json | jq -r ".Tables[].Rows[].cpi" | cut -d'_' -f1)
+bosh-cli update-cloud-config bosh-deployment/$CPI/cloud-config.yml \
+  --vars-file <( bosh-src/ci/bats/iaas/$CPI/director-vars )
+
+bosh-cli upload-stemcell stemcell/*.tgz
+bosh-cli -d zookeeper deploy --recreate zookeeper-release/manifests/zookeeper.yml
