377e945a7f1b1fe9733c33b97caf4edfecfc83b4
Belinda Liu <bliu@pivotal.io>
2019-08-28 15:32:26 -0700

    Move fake_job into its own helper file
    Accidentally removed in previous commit
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

---
+++ src/bosh-director/spec/support/fake_job.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Support
+  module FakeJob
+    def fake_job
+      Bosh::Director::Config.current_job = Bosh::Director::Jobs::BaseJob.new
+      Bosh::Director::Config.current_job.task_id = 'fake-task-id'
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(Support::FakeJob)
+end
