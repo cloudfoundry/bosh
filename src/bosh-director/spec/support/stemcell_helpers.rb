84d7687047167e6ecf2c869f255cf407e03340c4
Belinda Liu <bliu@pivotal.io>
2019-08-28 15:34:14 -0700

    Use FactoryBot in stemcell_helpers
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- src/bosh-director/spec/support/stemcell_helpers.rb
+++ src/bosh-director/spec/support/stemcell_helpers.rb
@@ -3,8 +3,9 @@ module Support
     def make_stemcell(options = {})
       model = Bosh::Director::Models::Stemcell.make(options)
       deployment = Bosh::Director::Models::Deployment.make
-      stemcell = Bosh::Director::DeploymentPlan::Stemcell.parse(
-        {'name' => model.name, 'version' => model.version}
+      stemcell = Bosh::Director::DeploymentPlan::Stemcell.make(
+        name: model.name,
+        version: model.version,
       )
       stemcell.bind_model(deployment)
       stemcell
