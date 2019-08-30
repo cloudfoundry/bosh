c4fef3c4e768f7d43f608e17f871ccd9ae3e748c
Rebecca Putinski <rputinski@pivotal.io>
2019-08-30 16:47:26 -0400

    Remain on trusty for some images, xenial for mysql
    - Created a manual tag for main-ruby-go:trusty
    - main-ruby-go in pipeline will use xenial
    - main-base -> main will use main-ruby-go:trusty
    - main-mysql-5.7 is its own dockerfile based on xenial, including parts
    from its dependent images
    
    [#168222310](https://www.pivotaltracker.com/story/show/168222310)

---
+++ ci/docker/main-mysql/install-ruby.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+set -x
+
+CHRUBY_VER="0.3.9"
+CHRUBY_URL=https://github.com/postmodern/chruby/archive/v${CHRUBY_VER}.tar.gz
+RUBY_INSTALL_VER="0.5.0"
+RUBY_INSTALL_URL=https://github.com/postmodern/ruby-install/archive/v${RUBY_INSTALL_VER}.tar.gz
+
+echo "Installing chruby v${CHRUBY_VER}..."
+wget -O chruby-${CHRUBY_VER}.tar.gz https://github.com/postmodern/chruby/archive/v${CHRUBY_VER}.tar.gz
+tar -xzvf chruby-${CHRUBY_VER}.tar.gz
+cd chruby-${CHRUBY_VER}/
+scripts/setup.sh
+cd ..
+rm -rf chruby-${CHRUBY_VER}/
+rm chruby-${CHRUBY_VER}.tar.gz
+
+echo "Installing ruby-install v${RUBY_INSTALL_VER}..."
+wget -O ruby-install-${RUBY_INSTALL_VER}.tar.gz $RUBY_INSTALL_URL
+tar -xzvf ruby-install-${RUBY_INSTALL_VER}.tar.gz
+cd ruby-install-${RUBY_INSTALL_VER}/
+make install
+cd ..
+rm -rf ruby-install-${RUBY_INSTALL_VER}/
+rm ruby-install-${RUBY_INSTALL_VER}.tar.gz
+
+install_ruby() {
+    local version=$1
+    local sha=$2
+
+    echo "Installing ruby $version..."
+    ruby-install --cleanup --sha256 "$sha" ruby "$version"
+
+    source /etc/profile.d/chruby.sh
+
+    chruby "ruby-$version"
+    ruby -v
+    gem update --system
+
+}
+
+install_ruby 2.4.4 45a8de577471b90dc4838c5ef26aeb253a56002896189055a44dc680644243f1
+install_ruby 2.4.5 276c8e73e51e4ba6a0fe81fb92669734e741ccea86f01c45e99f2c7ef7bcd1e3
+install_ruby 2.6.3 dd638bf42059182c1d04af0d5577131d4ce70b79105231c4cc0a60de77b14f2e
