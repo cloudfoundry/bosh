22a81e7322dd0afd9eabdeecf139c89e24f596c4
Conor Nosal <cnosal@pivotal.io>
2019-09-09 17:58:50 -0400

    Update sandbox certs to match old org names

--- src/bosh-dev/assets/sandbox/ca/generate.sh
+++ src/bosh-dev/assets/sandbox/ca/generate.sh
@@ -9,27 +9,23 @@ function cleanup () { name=$1
   rm ${name}.csr
 }
 
-function createSigningRequest () { name=$1
-  openssl req -nodes -new -newkey rsa:1024 -out ${name}.csr -keyout ${name}.key -subj '/C=US/O=Pivotal/CN=127.0.0.1'
-}
-
 function createCertWithCA () { name=$1
   echo "Generating $name cert signed by CA..."
-  createSigningRequest $name
+  openssl req -nodes -new -newkey rsa:1024 -out ${name}.csr -keyout ${name}.key -subj '/C=US/O=BOSH/CN=127.0.0.1/subjectAltName=DNS.1=127.0.0.1,DNS.2=0.0.0.0'
   openssl x509 -req -in ${name}.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out ${name}.crt -days 99999
   cleanup $name
 }
 
 function createSelfSignedCert () { name=$1
   echo "Generating self-signed $name cert..."
-  createSigningRequest $name
+  openssl req -nodes -new -newkey rsa:1024 -out ${name}.csr -keyout ${name}.key -subj '/C=US/O=Pivotal/CN=127.0.0.1/subjectAltName=DNS.1=127.0.0.1,DNS.2=0.0.0.0'
   openssl x509 -req -in ${name}.csr -signkey ${name}.key -out ${name}.crt -days 99999
   cleanup $name
 }
 
 echo "Generating CA..."
 openssl genrsa -out rootCA.key 1024
-openssl req -x509 -new -nodes -key rootCA.key -days 99999 -out rootCA.pem -subj '/C=US/O=Pivotal'
+openssl req -x509 -new -nodes -key rootCA.key -days 99999 -out rootCA.pem -subj '/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd'
 
 createCertWithCA server
 createSelfSignedCert serverWithWrongCA
