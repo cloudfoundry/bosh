33a9b8a7ed17621036d8c7c80e7c4e2dc4c68c6e
Miguel Verissimo <mverissimo@pivotal.io>
2019-09-10 16:57:31 -0400

    Add all localhost aliases to subject alt names in sandbox certificates
    Some TLS clients (bosh-cli) won't accept an IP address in the
    certificate common name.
    
    Co-authored-by: Conor Nosal <cnosal@pivotal.io>

--- src/bosh-dev/assets/sandbox/ca/generate.sh
+++ src/bosh-dev/assets/sandbox/ca/generate.sh
@@ -12,19 +12,19 @@ function cleanup () { name=$1
 function createCertWithCA () { name=$1
   echo "Generating $name cert signed by CA..."
   openssl req -nodes -new -newkey rsa:1024 -out ${name}.csr -keyout ${name}.key -subj '/C=US/O=BOSH/CN=127.0.0.1'
-  openssl x509 -req -in ${name}.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out ${name}.crt -days 99999
+  openssl x509 -req -in ${name}.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out ${name}.crt -days 99999 -extfile ../../openssl_san.cnf
   cleanup $name
 }
 
 function createSelfSignedCert () { name=$1
   echo "Generating self-signed $name cert..."
   openssl req -nodes -new -newkey rsa:1024 -out ${name}.csr -keyout ${name}.key -subj '/C=US/O=Pivotal/CN=127.0.0.1'
-  openssl x509 -req -in ${name}.csr -signkey ${name}.key -out ${name}.crt -days 99999
+  openssl x509 -req -in ${name}.csr -signkey ${name}.key -out ${name}.crt -days 99999 -extfile ../../openssl_san.cnf
   cleanup $name
 }
 
 echo "Generating CA..."
-openssl genrsa -out rootCA.key 1024
+openssl genrsa -out rootCA.key 1024  -config ../../openssl_san.cnf
 openssl req -x509 -new -nodes -key rootCA.key -days 99999 -out rootCA.pem -subj '/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd'
 
 createCertWithCA server
