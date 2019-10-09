9fdd21eb2dddf816a90ae6522623cbe9669bf222
Conor Nosal <cnosal@pivotal.io>
2019-10-09 12:07:01 -0400

    Add url signing to prepare instance step
    [#168818010](https://www.pivotaltracker.com/story/show/168818010)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- src/bosh-director/lib/bosh/director/deployment_plan/steps/prepare_instance_step.rb
+++ src/bosh-director/lib/bosh/director/deployment_plan/steps/prepare_instance_step.rb
@@ -5,6 +5,7 @@ module Bosh::Director
         def initialize(instance_plan, use_active_vm: true)
           @instance_plan = instance_plan
           @use_active_vm = use_active_vm
+          @blobstore = App.instance.blobstores.blobstore
         end
 
         def perform(_report)
@@ -16,6 +17,8 @@ module Bosh::Director
             agent_id = instance_model.agent_id
             name = instance_model.name
             raise 'no active VM available to prepare for instance' if agent_id.nil?
+
+            stemcell_api_version = instance_model.active_vm.stemcell_api_version
           else
             spec = spec.as_jobless_apply_spec
             vm = instance_model.most_recent_inactive_vm
@@ -23,9 +26,20 @@ module Bosh::Director
 
             agent_id = vm.agent_id
             name = vm.instance.name
+            stemcell_api_version = vm.stemcell_api_version
           end
 
+          add_signed_urls(spec) if @blobstore.signing_enabled? && stemcell_api_version >= 3
+
           AgentClient.with_agent_id(agent_id, name).prepare(spec)
+        end
+
+        def add_signed_urls(spec)
+          spec['packages'].each do |package|
+            package['signed_url'] = @blobstore.sign(package['blobstore_id'], 'get')
+          end
+          archive = spec['rendered_templates_archive']
+          archive['signed_url'] = @blobstore.sign(archive['blobstore_id'], 'get') unless archive.nil?
         end
       end
     end
--- src/bosh-director/spec/unit/deployment_plan/steps/prepare_instance_step_spec.rb
+++ src/bosh-director/spec/unit/deployment_plan/steps/prepare_instance_step_spec.rb
@@ -9,96 +9,248 @@ module Bosh::Director
         let(:instance) { Models::Instance.make }
         let(:deployment_instance) { instance_double(Instance, model: instance) }
         let(:instance_plan) { instance_double(InstancePlan, instance: deployment_instance) }
-        let(:spec) { instance_double(InstanceSpec, as_apply_spec: 'apply-me', as_jobless_apply_spec: 'unemployed') }
+        let(:apply_spec) do
+          {
+            'test' => 'apply-me',
+            'packages' => [{ 'blobstore_id' => 'blob1' }],
+            'rendered_templates_archive' => { 'blobstore_id' => 'blob2' },
+          }
+        end
+        let(:jobless_apply_spec) do
+          {
+            'test' => 'unemployed',
+            'packages' => [{ 'blobstore_id' => 'blob3' }],
+            'rendered_templates_archive' => { 'blobstore_id' => 'blob4' },
+          }
+        end
+        let(:signed_apply_spec) do
+          {
+            'test' => 'apply-me',
+            'packages' => [{ 'blobstore_id' => 'blob1', 'signed_url' => 'http://sig1' }],
+            'rendered_templates_archive' => { 'blobstore_id' => 'blob2', 'signed_url' => 'http://sig2' },
+          }
+        end
+        let(:signed_jobless_apply_spec) do
+          {
+            'test' => 'unemployed',
+            'packages' => [{ 'blobstore_id' => 'blob3', 'signed_url' => 'http://sig3' }],
+            'rendered_templates_archive' => { 'blobstore_id' => 'blob4', 'signed_url' => 'http://sig4' },
+          }
+        end
+        let(:spec) { instance_double(InstanceSpec, as_apply_spec: apply_spec, as_jobless_apply_spec: jobless_apply_spec) }
         let(:report) { Stages::Report.new }
+        let(:blobstore) { instance_double(Bosh::Blobstore::BaseClient) }
 
         before do
           allow(InstanceSpec).to receive(:create_from_instance_plan).with(instance_plan).and_return spec
+          allow(App).to receive_message_chain(:instance, :blobstores, :blobstore).and_return(blobstore)
         end
 
         describe '#perform' do
-          context 'with an instance plan referring to an instance with both new and old vms' do
-            let(:active_agent)  { instance_double(AgentClient) }
-            let(:lazy_agent)  { instance_double(AgentClient) }
-
+          context 'with url signing disabled' do
             before do
-              Models::Vm.make(instance: instance, agent_id: 'active-agent', active: true)
-              Models::Vm.make(instance: instance, agent_id: 'lazy-agent', active: false)
-              allow(AgentClient).to receive(:with_agent_id).with('active-agent', instance.name).and_return(active_agent)
-              allow(AgentClient).to receive(:with_agent_id).with('lazy-agent', instance.name).and_return(lazy_agent)
+              allow(blobstore).to receive(:signing_enabled?).and_return(false)
             end
 
-            context 'when preparing the active vm' do
-              let(:use_active_vm) { true }
+            context 'with an instance plan referring to an instance with both new and old vms' do
+              let(:active_agent) { instance_double(AgentClient) }
+              let(:lazy_agent) { instance_double(AgentClient) }
 
-              it 'sends the full spec to the active vms agent' do
-                expect(active_agent).to receive(:prepare).with('apply-me')
+              before do
+                active = Models::Vm.make(instance: instance, agent_id: 'active-agent', active: true, stemcell_api_version: 3)
+                Models::Vm.make(instance: instance, agent_id: 'lazy-agent', active: false, stemcell_api_version: 3)
+                allow(AgentClient).to receive(:with_agent_id).with('active-agent', instance.name).and_return(active_agent)
+                allow(AgentClient).to receive(:with_agent_id).with('lazy-agent', instance.name).and_return(lazy_agent)
+                allow(instance).to receive(:active_vm).and_return(active)
+              end
 
-                step.perform(report)
+              context 'when preparing the active vm' do
+                let(:use_active_vm) { true }
+
+                it 'sends the full spec to the active vms agent' do
+                  expect(active_agent).to receive(:prepare).with(
+                    'test' => 'apply-me',
+                    'packages' => [{ 'blobstore_id' => 'blob1' }],
+                    'rendered_templates_archive' => { 'blobstore_id' => 'blob2' },
+                  )
+
+                  step.perform(report)
+                end
+              end
+
+              context 'when preparing the inactive vm' do
+                let(:use_active_vm) { false }
+
+                it 'sends the jobless spec to the other vms agent' do
+                  expect(lazy_agent).to receive(:prepare).with(
+                    'test' => 'unemployed',
+                    'packages' => [{ 'blobstore_id' => 'blob3' }],
+                    'rendered_templates_archive' => { 'blobstore_id' => 'blob4' },
+                  )
+
+                  step.perform(report)
+                end
               end
             end
 
-            context 'when preparing the inactive vm' do
-              let(:use_active_vm) { false }
+            context 'with an instance plan with only an old vm' do
+              let(:old_agent) { instance_double(AgentClient) }
 
-              it 'sends the jobless spec to the other vms agent' do
-                expect(lazy_agent).to receive(:prepare).with('unemployed')
+              before do
+                active = Models::Vm.make(instance: instance, agent_id: 'old-agent', active: true, stemcell_api_version: 3)
+                allow(AgentClient).to receive(:with_agent_id).with('old-agent', instance.name).and_return(old_agent)
+                allow(instance).to receive(:active_vm).and_return(active)
+              end
 
-                step.perform(report)
+              context 'when preparing the active vm' do
+                let(:use_active_vm) { true }
+
+                it 'sends the full spec to the active vms agent' do
+                  expect(old_agent).to receive(:prepare).with(
+                    'test' => 'apply-me',
+                    'packages' => [{ 'blobstore_id' => 'blob1' }],
+                    'rendered_templates_archive' => { 'blobstore_id' => 'blob2' },
+                  )
+
+                  step.perform(report)
+                end
+              end
+
+              context 'when preparing the inactive vm' do
+                let(:use_active_vm) { false }
+
+                it 'raises error' do
+                  expect { step.perform(report) }.to raise_error('no inactive VM available to prepare for instance')
+                end
+              end
+            end
+
+            context 'with an instance plan with only a new vm' do
+              let(:new_agent) { instance_double(AgentClient) }
+
+              before do
+                Models::Vm.make(instance: instance, agent_id: 'new-agent', active: false, stemcell_api_version: 3)
+                allow(AgentClient).to receive(:with_agent_id).with('new-agent', instance.name).and_return(new_agent)
+              end
+
+              context 'when preparing the active vm' do
+                let(:use_active_vm) { true }
+
+                it 'raises error' do
+                  expect { step.perform(report) }.to raise_error('no active VM available to prepare for instance')
+                end
+              end
+
+              context 'when preparing the inactive vm' do
+                let(:use_active_vm) { false }
+
+                it 'sends the jobless spec to the other vms agent' do
+                  expect(new_agent).to receive(:prepare).with(
+                    'test' => 'unemployed',
+                    'packages' => [{ 'blobstore_id' => 'blob3' }],
+                    'rendered_templates_archive' => { 'blobstore_id' => 'blob4' },
+                  )
+
+                  step.perform(report)
+                end
               end
             end
           end
 
-          context 'with an instance plan with only an old vm' do
-            let(:old_agent) { instance_double(AgentClient) }
-
+          context 'with url signing enabled' do
             before do
-              Models::Vm.make(instance: instance, agent_id: 'old-agent', active: true)
-              allow(AgentClient).to receive(:with_agent_id).with('old-agent', instance.name).and_return(old_agent)
+              allow(blobstore).to receive(:signing_enabled?).and_return(true)
+              allow(blobstore).to receive(:sign).with('blob1', 'get').and_return('http://sig1')
+              allow(blobstore).to receive(:sign).with('blob2', 'get').and_return('http://sig2')
+              allow(blobstore).to receive(:sign).with('blob3', 'get').and_return('http://sig3')
+              allow(blobstore).to receive(:sign).with('blob4', 'get').and_return('http://sig4')
             end
 
-            context 'when preparing the active vm' do
-              let(:use_active_vm) { true }
+            context 'with an instance plan referring to an instance with both new and old vms' do
+              let(:active_agent) { instance_double(AgentClient) }
+              let(:lazy_agent) { instance_double(AgentClient) }
 
-              it 'sends the full spec to the active vms agent' do
-                expect(old_agent).to receive(:prepare).with('apply-me')
+              before do
+                active = Models::Vm.make(instance: instance, agent_id: 'active-agent', active: true, stemcell_api_version: 3)
+                Models::Vm.make(instance: instance, agent_id: 'lazy-agent', active: false, stemcell_api_version: 3)
+                allow(AgentClient).to receive(:with_agent_id).with('active-agent', instance.name).and_return(active_agent)
+                allow(AgentClient).to receive(:with_agent_id).with('lazy-agent', instance.name).and_return(lazy_agent)
+                allow(instance).to receive(:active_vm).and_return(active)
+              end
 
-                step.perform(report)
+              context 'when preparing the active vm' do
+                let(:use_active_vm) { true }
+
+                it 'sends the full spec to the active vms agent' do
+                  expect(active_agent).to receive(:prepare).with(signed_apply_spec)
+
+                  step.perform(report)
+                end
+              end
+
+              context 'when preparing the inactive vm' do
+                let(:use_active_vm) { false }
+
+                it 'sends the jobless spec to the other vms agent' do
+                  expect(lazy_agent).to receive(:prepare).with(signed_jobless_apply_spec)
+
+                  step.perform(report)
+                end
               end
             end
 
-            context 'when preparing the inactive vm' do
-              let(:use_active_vm) { false }
+            context 'with an instance plan with only an old vm' do
+              let(:old_agent) { instance_double(AgentClient) }
 
-              it 'raises error' do
-                expect { step.perform(report) }.to raise_error('no inactive VM available to prepare for instance')
+              before do
+                active = Models::Vm.make(instance: instance, agent_id: 'old-agent', active: true, stemcell_api_version: 3)
+                allow(AgentClient).to receive(:with_agent_id).with('old-agent', instance.name).and_return(old_agent)
+                allow(instance).to receive(:active_vm).and_return(active)
+              end
+
+              context 'when preparing the active vm' do
+                let(:use_active_vm) { true }
+
+                it 'sends the full spec to the active vms agent' do
+                  expect(old_agent).to receive(:prepare).with(signed_apply_spec)
+
+                  step.perform(report)
+                end
+              end
+
+              context 'when preparing the inactive vm' do
+                let(:use_active_vm) { false }
+
+                it 'raises error' do
+                  expect { step.perform(report) }.to raise_error('no inactive VM available to prepare for instance')
+                end
               end
             end
-          end
 
-          context 'with an instance plan with only a new vm' do
-            let(:new_agent) { instance_double(AgentClient) }
+            context 'with an instance plan with only a new vm' do
+              let(:new_agent) { instance_double(AgentClient) }
 
-            before do
-              Models::Vm.make(instance: instance, agent_id: 'new-agent', active: false)
-              allow(AgentClient).to receive(:with_agent_id).with('new-agent', instance.name).and_return(new_agent)
-            end
-
-            context 'when preparing the active vm' do
-              let(:use_active_vm) { true }
-
-              it 'raises error' do
-                expect { step.perform(report) }.to raise_error('no active VM available to prepare for instance')
+              before do
+                Models::Vm.make(instance: instance, agent_id: 'new-agent', active: false, stemcell_api_version: 3)
+                allow(AgentClient).to receive(:with_agent_id).with('new-agent', instance.name).and_return(new_agent)
               end
-            end
 
-            context 'when preparing the inactive vm' do
-              let(:use_active_vm) { false }
+              context 'when preparing the active vm' do
+                let(:use_active_vm) { true }
 
-              it 'sends the jobless spec to the other vms agent' do
-                expect(new_agent).to receive(:prepare).with('unemployed')
+                it 'raises error' do
+                  expect { step.perform(report) }.to raise_error('no active VM available to prepare for instance')
+                end
+              end
 
-                step.perform(report)
+              context 'when preparing the inactive vm' do
+                let(:use_active_vm) { false }
+
+                it 'sends the jobless spec to the other vms agent' do
+                  expect(new_agent).to receive(:prepare).with(signed_jobless_apply_spec)
+
+                  step.perform(report)
+                end
               end
             end
           end
