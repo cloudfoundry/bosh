87d05c1cd3fa591f451073fbfe4a0e1fa5ee811f
Charles Hansen <chansen@pivotal.io>
2019-10-09 14:58:12 -0700

    Add get resurrection endpoint
    returns {resurrection: true/false}
    to be consumed by prometheus exporter
    
    [#168964490](https://www.pivotaltracker.com/story/show/168964490)
    
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>

--- src/bosh-director/lib/bosh/director/api/controllers/resurrection_controller.rb
+++ src/bosh-director/lib/bosh/director/api/controllers/resurrection_controller.rb
@@ -3,6 +3,10 @@ require 'bosh/director/api/controllers/base_controller'
 module Bosh::Director
   module Api::Controllers
     class ResurrectionController < BaseController
+      get '/' do
+        json_encode(resurrection: !@resurrector_manager.pause_for_all?)
+      end
+
       put '/', consumes: :json do
         payload = json_decode(request.body.read)
 
--- src/bosh-director/spec/unit/api/controllers/resurrection_controller_spec.rb
+++ src/bosh-director/spec/unit/api/controllers/resurrection_controller_spec.rb
@@ -16,19 +16,43 @@ module Bosh::Director
       end
 
       describe 'API calls' do
-        describe 'resurrection' do
+        describe 'get /' do
+          context 'when global resurrection is on' do
+            before do
+              Models::DirectorAttribute.create(name: 'resurrection_paused', value: false)
+            end
+
+            it 'says the resurrection is on' do
+              get '/'
+              expect(last_response.status).to eq(200)
+              expect(last_response.body).to eq(JSON.generate(resurrection: true))
+            end
+          end
+
+          context 'when global resurrection is off' do
+            before do
+              Models::DirectorAttribute.create(name: 'resurrection_paused', value: true)
+            end
+
+            it 'says the resurrection is off' do
+              get '/'
+              expect(last_response.status).to eq(200)
+              expect(last_response.body).to eq(JSON.generate(resurrection: false))
+            end
+          end
+        end
+        describe 'put /' do
           it 'sets global resurrection to true' do
-            put '/', JSON.generate('resurrection_paused' => true), { 'CONTENT_TYPE' => 'application/json' }
+            put '/', JSON.generate('resurrection_paused' => true), 'CONTENT_TYPE' => 'application/json'
             expect(last_response.status).to eq(200)
             expect(Models::DirectorAttribute.first(name: 'resurrection_paused').value).to eq('true')
           end
           it 'sets global resurrection to false' do
-            put '/', JSON.generate('resurrection_paused' => false), { 'CONTENT_TYPE' => 'application/json' }
+            put '/', JSON.generate('resurrection_paused' => false), 'CONTENT_TYPE' => 'application/json'
             expect(last_response.status).to eq(200)
             expect(Models::DirectorAttribute.first(name: 'resurrection_paused').value).to eq('false')
           end
         end
-
       end
     end
   end
