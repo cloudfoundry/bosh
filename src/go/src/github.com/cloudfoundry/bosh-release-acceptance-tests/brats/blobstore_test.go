202023232b1d90dadc1ddc0cc5c985d4841f00b1
Maya Rosecrance <mrosecrance@pivotal.io>
2019-10-24 11:54:16 -0700

    Brat test CPI blobstore cred behavior
    - Documents existing behavior when the CPI passes blobstore creds to the
    agent despite director configuration specifying the use of signed urls.
    
    - This story https://www.pivotaltracker.com/story/show/169347469 may
    change this behavior going forwards.
    
    [#168818010](https://www.pivotaltracker.com/story/show/168818010)
    
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>

--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/blobstore_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/blobstore_test.go
@@ -182,6 +182,60 @@ var _ = Describe("Blobstore", func() {
 			Expect(records).To(MatchRegexp("syslog-forwarder")) // presence of anything is shows it has been updated
 		})
 
+		// This test is documenting existing non-ideal behavior; if it were easy to change
+		// this then it would not be a problem.
+		It("Does not strip blobstore credentials from VMs when only the CPI config changes", func() {
+			bratsutils.StartInnerBosh(
+				fmt.Sprintf("-o %s", bratsutils.AssetPath("ops-enable-signed-urls.yml")),
+				fmt.Sprintf("-o %s", bratsutils.AssetPath("ops-remove-agent-blobstore-credentials.yml")),
+			)
+			bratsutils.UploadRelease("https://bosh.io/d/github.com/cloudfoundry/syslog-release?v=11")
+			bratsutils.UploadStemcell(candidateWardenLinuxStemcellPath)
+
+			By("compiling (by deploying)")
+			session := bratsutils.Bosh("-n", "deploy", bratsutils.AssetPath("syslog-manifest.yml"),
+				"-d", "syslog-deployment",
+				"-v", fmt.Sprintf("stemcell-os=%s", bratsutils.StemcellOS()),
+			)
+			Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
+
+			bratsutils.StartInnerBosh(
+				fmt.Sprintf("-o %s", bratsutils.AssetPath("ops-enable-signed-urls.yml")),
+				fmt.Sprintf("-o %s", bratsutils.AssetPath("ops-enable-signed-urls-cpi.yml")),
+			)
+			session = bratsutils.Bosh("-n", "deploy", bratsutils.AssetPath("syslog-manifest.yml"),
+				"-d", "syslog-deployment",
+				"-v", fmt.Sprintf("stemcell-os=%s", bratsutils.StemcellOS()),
+			)
+			Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
+
+			By("ensuring that expected credentials are on disk")
+			session = bratsutils.BoshQuiet("-d", "syslog-deployment", "ssh", "syslog_forwarder/0", "-r", "--json", "-c", "sudo cat /var/vcap/bosh/settings.json")
+			Eventually(session, 30*time.Second).Should(gexec.Exit(0))
+			c := config{}
+			json.Unmarshal(getStdout(session.Out.Contents()), &c)
+			fmt.Printf("%+v\n", c)
+			Expect(c.Env.AgentEnv.BlobstoresConfig[0].Options.Password).To(Equal(""))
+			Expect(c.Env.AgentEnv.BlobstoresConfig[0].Options.User).To(Equal(""))
+
+			// Not removed since the CPI config change does not make the VM recreate
+			Expect(c.BlobstoreConfig.Options.User).ToNot(Equal(""))
+			Expect(c.BlobstoreConfig.Options.Password).ToNot(Equal(""))
+
+			session = bratsutils.BoshQuiet("-d", "syslog-deployment", "ssh", "syslog_forwarder/0", "-r", "--json", "-c", "sudo cat /var/vcap/bosh/warden-cpi-agent-env.json")
+			Eventually(session, 30*time.Second).Should(gexec.Exit(0))
+			c = config{}
+			json.Unmarshal(getStdout(session.Out.Contents()), &c)
+			fmt.Printf("%+v\n", c)
+			Expect(c.Env.AgentEnv.BlobstoresConfig[0].Options.Password).To(Equal(""))
+			Expect(c.Env.AgentEnv.BlobstoresConfig[0].Options.User).To(Equal(""))
+
+			// Not removed since the CPI config change does not make the VM recreate
+			Expect(c.BlobstoreConfig.Options.User).ToNot(Equal(""))
+			Expect(c.BlobstoreConfig.Options.Password).ToNot(Equal(""))
+		})
+	})
+
 	Context("Access Log", func() {
 		var tempBlobstoreDir string
 
