f20c1e28191ea81f0d09fa988747a4e10c45a0b1
Charles Hansen <chansen@pivotal.io>
2019-10-24 08:46:34 -0700

    Refactor fake scheduler in metrics collector spec
    [#168964490](https://www.pivotaltracker.com/story/show/168964490)

--- src/bosh-director/spec/unit/metrics_collector_spec.rb
+++ src/bosh-director/spec/unit/metrics_collector_spec.rb
@@ -3,7 +3,19 @@ require 'spec_helper'
 module Bosh
   module Director
     describe MetricsCollector do
-      let(:scheduler) { double(Rufus::Scheduler) }
+      class FakeRufusScheduler
+        attr_reader :interval
+        def every(interval, &blk)
+          @interval = interval
+          @blk = blk
+        end
+
+        def tick
+          @blk.call
+        end
+      end
+
+      let(:scheduler) { FakeRufusScheduler.new }
       let(:metrics_collector) { MetricsCollector.new(Config.load_hash(SpecHelper.spec_get_director_config)) }
       let(:resurrector_manager) { instance_double(Api::ResurrectorManager) }
 
@@ -22,14 +34,10 @@ module Bosh
       describe 'start' do
         describe 'resurrection_enabled' do
           it 'populates the metrics every 30 seconds' do
-            allow(scheduler).to receive(:every).with('30s') do |&blk|
-              expect(Prometheus::Client.registry.get(:resurrection_enabled).get).to eq(1)
-              blk.call
-            end
-
             metrics_collector.start
-
-            expect(scheduler).to have_received(:every)
+            expect(scheduler.interval).to eq('30s')
+            expect(Prometheus::Client.registry.get(:resurrection_enabled).get).to eq(1)
+            scheduler.tick
             expect(Prometheus::Client.registry.get(:resurrection_enabled).get).to eq(0)
           end
         end
@@ -40,17 +48,14 @@ module Bosh
           let!(:task3) { Models::Task.make(state: 'processing') }
 
           it 'populates the metrics every 30 seconds' do
-            allow(scheduler).to receive(:every).with('30s') do |&blk|
-              expect(Prometheus::Client.registry.get(:queued_tasks).get).to eq(2)
-              expect(Prometheus::Client.registry.get(:processing_tasks).get).to eq(1)
-              task2.update(state: 'processing')
-
-              blk.call
-            end
-
             metrics_collector.start
 
-            expect(scheduler).to have_received(:every)
+            expect(Prometheus::Client.registry.get(:queued_tasks).get).to eq(2)
+            expect(Prometheus::Client.registry.get(:processing_tasks).get).to eq(1)
+
+            task2.update(state: 'processing')
+            scheduler.tick
+
             expect(Prometheus::Client.registry.get(:queued_tasks).get).to eq(1)
             expect(Prometheus::Client.registry.get(:processing_tasks).get).to eq(2)
           end
