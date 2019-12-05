2b709181b0e9cf7b0cfbf1ff86bc2879a3aa5b96
Rebecca Putinski <rputinski@pivotal.io>
2019-12-05 15:23:31 -0500

    Enable signed urls by default for acceptance

--- ci/acceptance/create-director.sh
+++ ci/acceptance/create-director.sh
@@ -13,6 +13,7 @@ bosh create-env \
   -o  ${BBL_STATE_DIR}/bosh-deployment/experimental/blobstore-https.yml \
   -o  ${BBL_STATE_DIR}/bbl-ops-files/gcp/bosh-director-ephemeral-ip-ops.yml \
   -o  ${BBL_STATE_DIR}/bbl-ops-files/enable-hm-alerts.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/enable-signed-urls.yml \
   --var-file  gcp_credentials_json="${BBL_GCP_SERVICE_ACCOUNT_KEY_PATH}" \
   -v project_id="${BBL_GCP_PROJECT_ID}" \
   -v zone="${BBL_GCP_ZONE}" \
