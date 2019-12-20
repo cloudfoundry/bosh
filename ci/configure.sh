b43b58182a9a6fb14d82266e057fb111a4920c16
Charles Hansen <chansen@pivotal.io>
2019-12-20 09:44:49 -0800

    Add main-ruby-go build task to main pipeline
    create "dockerfile" directory to store versioned dockerfiles for now
    
    [#170088778](https://www.pivotaltracker.com/story/show/170088778)

--- ci/configure.sh
+++ ci/configure.sh
@@ -16,8 +16,9 @@ fi
 
 fly -t director set-pipeline -p "${pipeline}" \
     -c ci/pipeline.yml \
-    --load-vars-from <(lpass show -G "bosh concourse secrets" --notes) \
+    -l <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
     -l <(lpass show --note "tracker-bot-story-delivery") \
     -l <(lpass show -G "bosh:aws-ubuntu-bats concourse secrets" --notes) \
+    -l <(lpass show --note "bosh:docker-images concourse secrets") \
     --var=branch_name=${branch}
