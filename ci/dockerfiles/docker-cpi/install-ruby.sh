6977ff4828e62146dd056e6ae00355afd297629b
Charles Hansen <chansen@pivotal.io>
2020-01-08 17:08:00 -0800

    bump ruby in docker-cpi dockerfile
    It doesn't really matter, but may as well be consistent.
    
    [#170088778](https://www.pivotaltracker.com/story/show/170088778)

--- ci/dockerfiles/docker-cpi/install-ruby.sh
+++ ci/dockerfiles/docker-cpi/install-ruby.sh
@@ -3,19 +3,8 @@
 set -e
 set -x
 
-CHRUBY_VER="0.3.9"
-CHRUBY_URL=https://github.com/postmodern/chruby/archive/v${CHRUBY_VER}.tar.gz
-RUBY_INSTALL_VER="0.5.0"
+RUBY_INSTALL_VER="0.7.0"
 RUBY_INSTALL_URL=https://github.com/postmodern/ruby-install/archive/v${RUBY_INSTALL_VER}.tar.gz
-
-echo "Installing chruby v${CHRUBY_VER}..."
-wget -O chruby-${CHRUBY_VER}.tar.gz https://github.com/postmodern/chruby/archive/v${CHRUBY_VER}.tar.gz
-tar -xzvf chruby-${CHRUBY_VER}.tar.gz
-cd chruby-${CHRUBY_VER}/
-scripts/setup.sh
-cd ..
-rm -rf chruby-${CHRUBY_VER}/
-rm chruby-${CHRUBY_VER}.tar.gz
 
 echo "Installing ruby-install v${RUBY_INSTALL_VER}..."
 wget -O ruby-install-${RUBY_INSTALL_VER}.tar.gz $RUBY_INSTALL_URL
@@ -31,14 +20,11 @@ install_ruby() {
     local sha=$2
 
     echo "Installing ruby $version..."
-    ruby-install --cleanup --sha256 "$sha" ruby "$version"
+    ruby-install --jobs=2 --cleanup --system --sha256 "$sha" ruby "$version" -- --disable-install-rdoc
 
-    source /etc/profile.d/chruby.sh
-
-    chruby "ruby-$version"
     ruby -v
     gem update --system
 
 }
 
-install_ruby 2.6.3 dd638bf42059182c1d04af0d5577131d4ce70b79105231c4cc0a60de77b14f2e
+install_ruby 2.6.5 97ddf1b922f83c1f5c50e75bf54e27bba768d75fea7cda903b886c6745e60f0a
