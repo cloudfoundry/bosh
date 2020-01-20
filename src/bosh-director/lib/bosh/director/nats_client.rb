4f61ad0c7fd9484490990b53ef938733cc833510
Oleksandr Slynko <oslynko@pivotal.io>
2020-01-20 22:52:30 +0000

    Remove NatsClient adapter
    [#167423026]
    
    Co-authored-by: Oleksandr Slynko <oslynko@pivotal.io>

--- src/bosh-director/lib/bosh/director/nats_client.rb
+++ src/bosh-director/lib/bosh/director/nats_client.rb
@@ -2,42 +2,6 @@ module Bosh::Director
   MAX_RECONNECT_ATTEMPTS = 4
 
   class NatsClient
-    # TODO: make it initialize
-    def initialize(nats_options)
-      @options = nats_options
-    end
-
-    def connect
-      @nats = NATS::IO::Client.new
-      @nats.on_error(&@error_handler) if @error_handler
-      @nats.connect(@options)
-    end
-
-    def on_error(&callback)
-      @error_handler = callback
-    end
-
-    def schedule(&blk)
-      blk.call
-    end
-
-    def subscribe(subject, &callback)
-      @nats.subscribe(subject, &callback)
-    end
-
-    def not_connected?
-      @nats.nil?
-    end
-
-    def publish(subject_name, request_body, &blk)
-      @nats.publish(subject_name, request_body, &blk)
-    end
-
-    def flush(&blk)
-      @nats.flush
-      blk.call
-    end
-
     def self.options(nats_uri, nats_client_private_key_path, nats_client_certificate_path, nats_server_ca_path)
       tls_context = create_tls_context(nats_client_private_key_path, nats_client_certificate_path, nats_server_ca_path)
       {
