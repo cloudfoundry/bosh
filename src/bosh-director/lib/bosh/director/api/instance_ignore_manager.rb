a273bdfd75fa76d6134c2b5b6bde825aa7585633
Fenggang Wang <ginobiliwang@gmail.com>
2020-02-24 12:30:14 +0800

    To add batch function for ignore
    Update instance_ignore_manager.rb to add batch function for ignore.

--- src/bosh-director/lib/bosh/director/api/instance_ignore_manager.rb
+++ src/bosh-director/lib/bosh/director/api/instance_ignore_manager.rb
@@ -2,9 +2,17 @@ module Bosh::Director
   module Api
     class InstanceIgnoreManager
       def set_ignore_state_for_instance(deployment, instance_group_name, index_or_id, state)
-        instance = Api::InstanceManager.new.find_by_name(deployment, instance_group_name, index_or_id)
-        instance.ignore = state
-        instance.save
+        if index_or_id == "*"
+           instances = Api::InstanceManager.new.find_instances_by_job(deployment, instance_group_name)
+           instances.each do |instance|
+             instance.ignore = state
+             instance.save
+           end
+        else
+           instance = Api::InstanceManager.new.find_by_name(deployment, instance_group_name, index_or_id)
+           instance.ignore = state
+           instance.save
+        end
       end
     end
   end
