25c02c701d671e9b23928166fd8ca989a3204964
Brian Upton <bupton@vmware.com>
2021-09-22 16:06:44 -0700

    Pre-populate blobstore and nats shas in migration
    This pre-populates the new blobstore_config_sha1 and nats_config_sha1
    columns with the current SHAs when the database is migrated to add the
    columns. These values are read off of the Bosh::Director::Config global
    class because:
    
    1. The Sequel gem's migration system does not allow outside parameters
       to be passed to the migrations. So it is not possible to pass in the
       full config object directly.
    2. The config file path is passed in to the migrate script, so we cannot
       load the file directly in the migration (unless we assume the config
       file path)
    
    Loading the global state onto the Config method required calling the
    aptly named `configure_evil_config_singleton!` method, previously only
    called when the BOSH Director app is launched.
    
    [#179396629] TAS-29 Rotation of certificates/credentials stored in the bosh-agent metadata can be updated without recreating the VM

--- src/bosh-director/db/migrations/director/20210902232124_add_blobstore_and_nats_shas_to_vms.rb
+++ src/bosh-director/db/migrations/director/20210902232124_add_blobstore_and_nats_shas_to_vms.rb
@@ -4,5 +4,9 @@ Sequel.migration do
       add_column :blobstore_config_sha1, String, size: 50
       add_column :nats_config_sha1, String, size: 50
     end
+    self[:vms].update(
+      blobstore_config_sha1: Bosh::Director::Config.blobstore_config_fingerprint,
+      nats_config_sha1: Bosh::Director::Config.nats_config_fingerprint,
+    )
   end
 end
