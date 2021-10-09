9a28a2af328273c9b65a93b1a44cae1219b9b8b3
Kenneth Lakin <kennethlakin@gmail.com>
2021-10-09 16:04:23 -0700

    Changes to make spec:unit:parallel more useful
    This commit makes changes that make it possible for a user of the
    `spec:unit:parallel` rake task to be able to see all test failures in
    one go, rather than playing whack-a-mole with test failures due to
    `rspec` and the test runner aborting test runs early.
    
    Specifically:
    
    The minor changes:
    1) Tests that fail now print "BEGIN" and "END" blocks, as well as test
       output.
    2) The phrase printed when a test encountered an error has been changed
       a little to indicate that the failure could be a build _or_ a run
       failure.
    
    The major changes:
    1) Adds `--no-fail-fast` to the `rspec` argument list. Because rspec's
       default behavior is to exit on the first failure, this causes rspec
       to run all tests in the suite, so all errors are gathered up in one
       go.
    2) Tests that fail no longer raise an exception on failure, but rather
       set a "something went wrong" boolean, which causes the creator of the
       threads to raise an exception if _any_ thread encountered an error.
       This allows all other tests to run, rather than aborting all future
       test runs just because one test suite failed.
    3) Adds a "Test output writing mutex" that wraps the `puts` call that
       writes out the test output.  This ensures that the output from one
       test doesn't get interleaved with the output from another. Visual
       inspection of the output reveals that this change is effective. No
       "BEGIN" prints appear until the "END" print that matches the previous
       "BEGIN" print is emitted. Additionally, a quick check with `htop`
       reveals that this has minimal-to-no impact on the ability to run test
       suites in parallel.

--- src/bosh-dev/lib/bosh/dev/test_runner.rb
+++ src/bosh-dev/lib/bosh/dev/test_runner.rb
@@ -3,35 +3,53 @@ require 'common/thread_pool'
 module Bosh::Dev
   class TestRunner
     def ruby(parallel: false)
+      error_happened = false
       log_dir = Dir.mktmpdir
       puts "Logging spec results in #{log_dir}"
+      test_output_lock = Mutex.new
 
       max_threads = ENV.fetch('BOSH_MAX_THREADS', 10).to_i
       null_logger = Logging::Logger.new('Ignored')
       Bosh::ThreadPool.new(max_threads: max_threads, logger: null_logger).wrap do |pool|
         unit_builds.each do |build|
           pool.process do
-            unit_exec(build, log_file: "#{log_dir}/#{build}.log", parallel: parallel)
+            test_return = unit_exec(build, log_file: "#{log_dir}/#{build}.log", parallel: parallel)
+            if test_return[:error] == true
+              error_happened = true
+            end
+            test_output_lock.synchronize {
+              puts test_return[:lines]
+            }
           end
         end
 
         pool.wait
       end
+
+      if error_happened
+        raise "Failed while running tests. See output above for more information."
+      end
     end
 
     def unit_exec(build, log_file: nil, parallel: false)
+      lines = []
       command = parallel ? unit_parallel(build, log_file) : unit_cmd(log_file)
 
       # inject command name so coverage results for each component don't clobber others
       if Kernel.system({'BOSH_BUILD_NAME' => build}, "cd #{build} && #{command}")
-        puts "----- BEGIN #{build}"
-        puts "            #{command}"
-        print File.read(log_file) if log_file && File.exists?(log_file)
-        puts "----- END   #{build}\n\n"
+        lines.append "----- BEGIN #{build}"
+        lines.append "            #{command}"
+        lines.append(File.read(log_file)) if log_file && File.exists?(log_file)
+        lines.append "----- END   #{build}\n\n"
+        return {:lines => lines, :error => false}
       else
-        error_message = "#{build} failed to build unit tests"
+        lines.append "----- BEGIN #{build}"
+        lines.append "            #{command}"
+        error_message = "#{build} failed to build or run unit tests"
         error_message += ": #{File.read(log_file)}" if log_file && File.exists?(log_file)
-        raise error_message
+        lines.append "            #{error_message}\n"
+        lines.append "----- END   #{build}\n\n"
+        return {:lines => lines, :error => true}
       end
     end
 
@@ -43,7 +61,7 @@ module Bosh::Dev
     end
 
     def unit_parallel(build_name, log_file = nil)
-      cmd = "parallel_test --type rspec --runtime-log /tmp/bosh_#{build_name}_parallel_runtime_rspec.log spec"
+      cmd = "parallel_test --test-options '--no-fail-fast' --type rspec --runtime-log /tmp/bosh_#{build_name}_parallel_runtime_rspec.log spec"
       cmd << " > #{log_file} 2>&1" if log_file
       cmd
     end
