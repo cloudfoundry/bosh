68e18fdbdf3ad2eb8f7b4673913b81fdd1a4f11a
Camila Londoño <londonoc@vmware.com>
2022-01-27 15:19:50 -0500

    Changed bosh-master to bosh-main
    Signed-off-by: Joseph Palermo <jpalermo@vmware.com>

--- ci/tasks/finalize-bosh-release.sh
+++ ci/tasks/finalize-bosh-release.sh
@@ -5,18 +5,14 @@ set -eux
 export FULL_VERSION=$(cat version/version)
 
 export ROOT_PATH=$PWD
-PROMOTED_MASTER=$PWD/bosh-master-with-final
 TARBALL_DIR=$PWD/release-tarball
 
 mv bosh-cli/alpha-bosh-cli-*-linux-amd64 bosh-cli/bosh-cli
 export GO_CLI_PATH=$ROOT_PATH/bosh-cli/bosh-cli
 chmod +x $GO_CLI_PATH
 
-git clone ./bosh-master $PROMOTED_MASTER
 
-pushd $PROMOTED_MASTER
-  git pull origin master
-  git status
+pushd bosh-main
 
   set +x
   echo "$RELEASE_PRIVATE_YML" >> "config/private.yml"
@@ -34,10 +30,10 @@ pushd $PROMOTED_MASTER
 
 popd
 
-cat <<EOF >bosh-master-with-final/tag-name
+cat <<EOF >bosh-main/tag-name
 v${FULL_VERSION}
 EOF
 
-cat <<EOF >bosh-master-with-final/annotate-msg
+cat <<EOF >bosh-main/annotate-msg
 Final release $FULL_VERSION tagged via concourse
 EOF
