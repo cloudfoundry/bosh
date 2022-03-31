c3181380174a54aabe1e71afc163a92e3f0d182b
Brian Cunnie <bcunnie@vmware.com>
2022-03-31 13:51:45 -0700

    🐞 Load expected version of postgres
    Likely due to previous pinning of Postgres to v10 the
    Bosh::Dev::PostgresVersion helper was loading the
    src/jobs/postgres-10/spec.yaml.
    
    Since we have unpinned Postgres this helper should now be loading the
    "regular" Postgres spec.yaml file.

--- src/bosh-dev/lib/bosh/dev/postgres_version.rb
+++ src/bosh-dev/lib/bosh/dev/postgres_version.rb
@@ -5,14 +5,18 @@ require 'bosh/director/config'
 module Bosh::Dev
   class PostgresVersion
     class << self
-
       def local_version
         `postgres --version`.chomp.split(' ').last
       end
 
       def release_version
         @release_version ||= begin
-          postgres_release_config = YAML.load_file(File.join(File.dirname(__FILE__), '../../../../../jobs/postgres-10/spec.yml'), permitted_classes: [Symbol], aliases: true)
+          postgres_release_config =
+            YAML.load_file(
+              File.join(File.dirname(__FILE__), '../../../../../jobs/postgres/spec.yml'),
+              permitted_classes: [Symbol],
+              aliases: true,
+            )
 
           # sort alphanumerics correctly, i.e 10 > 9
           postgres_version = postgres_release_config['packages'].max_by { |s| s.scan(/\d+/).first.to_i }
