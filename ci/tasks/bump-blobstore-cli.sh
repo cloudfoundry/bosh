f2b8c033b0c751063cacdc0e535c927159e91856
aram price <pricear@vmware.com>
2022-09-14 12:26:03 -0700

    Add blobstore CLI update task - commit disabled
    The task does not _yet_ commit changes

---
+++ ci/tasks/bump-blobstore-cli.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "${PRIVATE_YML}" > bosh-src/config/private.yml
+
+case "${BLOBSTORE_TYPE}" in
+dav)
+  cli_name="davcli";;
+gcs)
+  cli_name="bosh-gcscli";;
+s3)
+  cli_name="s3cli";;
+*)
+  echo "Error: unknown BLOBSTORE_TYPE='${BLOBSTORE_TYPE}'; exiting"
+  exit 1;;
+esac
+
+pushd bosh-src
+  LATEST_CLI_BLOB_PATH=$(ls ../bosh-blobstore-cli/*cli*)
+  LATEST_CLI_BLOB_KEY="${cli_name}/$( basename "${LATEST_CLI_BLOB_PATH}" )"
+
+  EXISTING_CLI_BLOB_KEY=$(bosh blobs | cut -f1 | grep "${cli_name}")
+
+   if [ "${EXISTING_CLI_BLOB_KEY}" != "${LATEST_CLI_BLOB_KEY}" ]; then
+    bosh add-blob --sha2 "${LATEST_CLI_BLOB_PATH}" "${LATEST_CLI_BLOB_KEY}"
+    bosh remove-blob "${EXISTING_CLI_BLOB_KEY}"
+
+    git add .
+
+    git --no-pager diff --cached
+#    TODO - uncomment
+#    if [[ "$( git status --porcelain )" != "" ]]; then
+#      git config user.name "${GIT_USER_NAME}"
+#      git config user.email "${GIT_USER_EMAIL}"
+#      git commit --message "Updating blob ${EXISTING_CLI_BLOB_KEY} -> ${LATEST_CLI_BLOB_KEY}"
+#
+#      bosh upload-blobs
+#    fi
+  fi
+popd
