769e13503c3cef9fe438d366f2f7e998ca5080c3
aram price <pricear@vmware.com>
2022-09-16 15:19:21 -0700

    Auto-bump the mariadb-connector blob
    This commit adds a CI (concourse) task which checks for newer releases
    of the MariaDB `C` connector source code and updates the coresponding
    blob in the bosh release with the latest version listed at
    
    - https://mariadb.org/connector-c/all-releases/
    
    which is marked as "Stable". Refer to the definition of the
    `mariadb-connector-c-resource` for details.
    
    [#181103446]

---
+++ ci/tasks/bump-mariadb-connector.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+pushd bosh-src
+  echo "${PRIVATE_YML}" > config/private.yml
+
+  CONNECTOR_PREFIX="mariadb-connector-c"
+
+  LATEST_MARIADB_CONNECTOR_BLOB_PATH=$(ls ../${CONNECTOR_PREFIX}-resource/*.tar.gz)
+  LATEST_MARIADB_CONNECTOR_BLOB_KEY="mysql/$( basename "${LATEST_MARIADB_CONNECTOR_BLOB_PATH}" )"
+
+  EXISTING_MARIADB_CONNECTOR_BLOB_KEY=$( bosh blobs | cut -d ' ' -f1 | grep "${CONNECTOR_PREFIX}" )
+
+   if [ "${EXISTING_MARIADB_CONNECTOR_BLOB_KEY}" != "${LATEST_MARIADB_CONNECTOR_BLOB_KEY}" ]; then
+    bosh add-blob --sha2 "${LATEST_MARIADB_CONNECTOR_BLOB_PATH}" "${LATEST_MARIADB_CONNECTOR_BLOB_KEY}"
+    bosh remove-blob "${EXISTING_MARIADB_CONNECTOR_BLOB_KEY}"
+    bosh upload-blobs
+
+    git add .
+
+    if [[ "$( git status --porcelain )" != "" ]]; then
+      update_message="Updating blob ${EXISTING_MARIADB_CONNECTOR_BLOB_KEY} -> ${LATEST_MARIADB_CONNECTOR_BLOB_KEY}"
+      git config user.name "${GIT_USER_NAME}"
+      git config user.email "${GIT_USER_EMAIL}"
+
+      echo ""
+      echo "### Commit info"
+      echo "    Message: '${update_message}'"
+      echo ""
+      git --no-pager diff --cached
+      echo "^^^ Commit info"
+      echo ""
+
+      git commit --message "${update_message}"
+    fi
+  fi
+popd
