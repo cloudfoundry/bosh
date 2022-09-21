9705cf103a965d3d7a77242be375e3bb71d9b8c9
aram price <pricear@vmware.com>
2022-09-21 11:14:02 -0700

    cleanup bash-isms in db cleanup script

--- ci/tasks/test-brats-db-cleanup.sh
+++ ci/tasks/test-brats-db-cleanup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-set -eu
+set -euo pipefail
 
 function cleanup_mysql() {
   echo 'Cleanup MYSQL ============'
@@ -9,7 +8,7 @@ function cleanup_mysql() {
   export MYSQL_PWD=$3
   database_name=$4
 
-  mysql -h ${hostname} -P 3306 --user=${username} -e "drop database ${database_name};" || true
+  mysql -h "${hostname}" -P 3306 --user="${username}" -e "drop database ${database_name};" || true
 }
 
 function cleanup_postgres() {
@@ -21,7 +20,7 @@ function cleanup_postgres() {
 
   # Assumption: we are deleting inner-bosh in AfterEach so all connection will be terminated,
   #             so we dont need to revoke connection
-  dropdb -U ${username} -p 5432 -h ${hostname} ${database_name} || true
+  dropdb -U "${username}" -p 5432 -h "${hostname}" "${database_name}" || true
 }
 
 RDS_MYSQL_EXTERNAL_DB_HOST="$(jq -r .aws_mysql_endpoint database-metadata/metadata | cut -d':' -f1)"
@@ -47,10 +46,13 @@ export GCP_POSTGRES_EXTERNAL_DB_CLIENT_CERTIFICATE
 export GCP_POSTGRES_EXTERNAL_DB_CLIENT_PRIVATE_KEY
 
 echo 'Cleanup RDS ============================'
-cleanup_mysql $RDS_MYSQL_EXTERNAL_DB_HOST $RDS_MYSQL_EXTERNAL_DB_USER $RDS_MYSQL_EXTERNAL_DB_PASSWORD $RDS_MYSQL_EXTERNAL_DB_NAME
-cleanup_postgres $RDS_POSTGRES_EXTERNAL_DB_HOST $RDS_POSTGRES_EXTERNAL_DB_USER $RDS_POSTGRES_EXTERNAL_DB_PASSWORD $RDS_POSTGRES_EXTERNAL_DB_NAME
+cleanup_mysql "${RDS_MYSQL_EXTERNAL_DB_HOST}" "${RDS_MYSQL_EXTERNAL_DB_USER}" \
+              "${RDS_MYSQL_EXTERNAL_DB_PASSWORD}" "${RDS_MYSQL_EXTERNAL_DB_NAME}"
+cleanup_postgres "${RDS_POSTGRES_EXTERNAL_DB_HOST}" "${RDS_POSTGRES_EXTERNAL_DB_USER}" \
+                  "${RDS_POSTGRES_EXTERNAL_DB_PASSWORD}" "${RDS_POSTGRES_EXTERNAL_DB_NAME}"
 
 echo 'Cleanup GCP ============================'
-cleanup_mysql $GCP_MYSQL_EXTERNAL_DB_HOST $GCP_MYSQL_EXTERNAL_DB_USER $GCP_MYSQL_EXTERNAL_DB_PASSWORD $GCP_MYSQL_EXTERNAL_DB_NAME
-cleanup_postgres $GCP_POSTGRES_EXTERNAL_DB_HOST $GCP_POSTGRES_EXTERNAL_DB_USER $GCP_POSTGRES_EXTERNAL_DB_PASSWORD $GCP_POSTGRES_EXTERNAL_DB_NAME
-
+cleanup_mysql "${GCP_MYSQL_EXTERNAL_DB_HOST}" "${GCP_MYSQL_EXTERNAL_DB_USER}" \
+              "${GCP_MYSQL_EXTERNAL_DB_PASSWORD}" "${GCP_MYSQL_EXTERNAL_DB_NAME}"
+cleanup_postgres "${GCP_POSTGRES_EXTERNAL_DB_HOST}" "${GCP_POSTGRES_EXTERNAL_DB_USER}" \
+                  "${GCP_POSTGRES_EXTERNAL_DB_PASSWORD}" "${GCP_POSTGRES_EXTERNAL_DB_NAME}"
