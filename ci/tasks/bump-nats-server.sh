f86ea6cfdfc0d3b2c96b626eb13c878a872a8c31
aram price <pricear@vmware.com>
2022-09-23 11:52:58 -0700

    Add CI tooling to bump nats-server blob

---
+++ ci/tasks/bump-nats-server.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+pushd bosh-src
+  echo "${PRIVATE_YML}" > config/private.yml
+
+  BLOB_PREFIX="nats"
+
+  LATEST_BLOB_PATH=$(ls ../nats-server-github-release/nats-server-*)
+  LATEST_BLOB_KEY="${BLOB_PREFIX}/$( basename "${LATEST_BLOB_PATH}" )"
+
+  EXISTING_BLOB_KEY=$(bosh blobs | cut -f1 | grep "${BLOB_PREFIX}" |  tr -d '[:space:]')
+
+   if [ "${EXISTING_BLOB_KEY}" != "${LATEST_BLOB_KEY}" ]; then
+    bosh add-blob --sha2 "${LATEST_BLOB_PATH}" "${LATEST_BLOB_KEY}"
+    bosh remove-blob "${EXISTING_BLOB_KEY}"
+    echo "bosh upload-blobs"
+
+    git add .
+
+    if [[ "$( git status --porcelain )" != "" ]]; then
+      update_message="Updating blob ${EXISTING_BLOB_KEY} -> ${LATEST_BLOB_KEY}"
+      git config user.name "${GIT_USER_NAME}"
+      git config user.email "${GIT_USER_EMAIL}"
+
+      echo ""
+      echo "### Commit info"
+      echo "    Message: '${update_message}'"
+      echo ""
+      git --no-pager diff --cached
+      echo "^^^ Commit info"
+      echo ""
+
+      echo "git commit --message ${update_message}"
+    fi
+  fi
+popd
+exit 1 # for testing
