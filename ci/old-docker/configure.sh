a8864b154816c52ffbd0ba26e6996ff9e5f2f874
Long Nguyen <nguyenlo@vmware.com>
2022-11-09 14:25:00 -0500

    Adding boshcpi/light-stemcell-builder to bosh-docker-image pipeline
    Moved credentials from lastpass to credhub

--- ci/old-docker/configure.sh
+++ ci/old-docker/configure.sh
@@ -4,5 +4,4 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 fly -t bosh-ecosystem set-pipeline \
   -p bosh-docker-images \
-  -c pipeline.yml \
-  --load-vars-from <(lpass show --note "bosh:docker-images concourse secrets")
+  -c pipeline.yml 
