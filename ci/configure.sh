1dfef2df8dbcae5245f7c175ce1a78f8c7d070a3
Joseph Palermo <jpalermo@pivotal.io>
2023-01-03 18:39:05 -0800

    Remove unused lastpass entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -8,9 +8,7 @@ fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p bosh-director \
     -c ci/pipeline.yml \
     -l <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
-    -l <(lpass show --note "bats-concourse-pool:vsphere nimbus secrets" ) \
     -l <(lpass show --note "tracker-bot-story-delivery") \
     -l <(lpass show -G "bosh:aws-ubuntu-bats concourse secrets" --notes) \
-    -l <(lpass show --note "bosh:docker-images concourse secrets") \
     -l <(lpass show -G "bosh-utils concourse secrets" --notes) \
     --var=branch_name=main
