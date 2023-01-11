406b0954f2f7ff45be0158eb1d1ad33a887e00e2
Joseph Palermo <jpalermo@pivotal.io>
2023-01-11 14:21:30 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -9,5 +9,4 @@ fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p bosh-director \
     -l <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
     -l <(lpass show --note "tracker-bot-story-delivery") \
-    -l <(lpass show -G "bosh:aws-ubuntu-bats concourse secrets" --notes) \
     --var=branch_name=main
