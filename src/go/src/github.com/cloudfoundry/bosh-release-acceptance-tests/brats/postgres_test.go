67ca732cc735bbf0cd929a2953564cdb79c09ae6
Long Nguyen <nguyenlo@vmware.com>
2023-01-31 09:37:17 -0500

    Adding support for upgrade from postgres 10 to 15
    We were too optimistic in deprecating postgres 10

--- src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/postgres_test.go
+++ src/go/src/github.com/cloudfoundry/bosh-release-acceptance-tests/brats/postgres_test.go
@@ -16,10 +16,11 @@ var _ = Describe("postgres", func() {
 		var (
 			postgres15Manifest     string
 			postgres13Manifest     string
+			postgres10Manifest     string
 			postgresDeploymentName string
 		)
 
-		BeforeEach(func() {
+		It("Upgrades from 13", func() {
 			var err error
 
 			postgresDeploymentName, err = uuid.NewGenerator().Generate()
@@ -34,10 +35,32 @@ var _ = Describe("postgres", func() {
 				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
 			)
 			Eventually(session, 15*time.Minute).Should(gexec.Exit(0))
+
+			session = bratsutils.OuterBosh("deploy", "-n", postgres15Manifest,
+				"-d", postgresDeploymentName,
+				"-v", fmt.Sprintf("stemcell-os=%s", bratsutils.StemcellOS()),
+				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
+			)
+			Eventually(session, 15*time.Minute).Should(gexec.Exit(0))
 		})
 
-		It("Upgrades from 13", func() {
-			session := bratsutils.OuterBosh("deploy", "-n", postgres15Manifest,
+		It("Upgrades from 10", func() {
+			var err error
+
+			postgresDeploymentName, err = uuid.NewGenerator().Generate()
+			Expect(err).NotTo(HaveOccurred())
+
+			postgres15Manifest = bratsutils.AssetPath("postgres-manifest.yml")
+			postgres10Manifest = bratsutils.AssetPath("postgres-10-manifest.yml")
+
+			session := bratsutils.OuterBosh("deploy", "-n", postgres10Manifest,
+				"-d", postgresDeploymentName,
+				"-v", fmt.Sprintf("stemcell-os=%s", bratsutils.StemcellOS()),
+				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
+			)
+			Eventually(session, 15*time.Minute).Should(gexec.Exit(0))
+
+			session = bratsutils.OuterBosh("deploy", "-n", postgres15Manifest,
 				"-d", postgresDeploymentName,
 				"-v", fmt.Sprintf("stemcell-os=%s", bratsutils.StemcellOS()),
 				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
