5b60ea1e4d7b5af8129df615bd8fb00fbe9e434c
ramonskie <ramonmakkelie@gmail.com>
2023-03-20 13:26:30 +0100

    drop table when downgrading bosh. will fix #2432

--- src/bosh-director/db/migrations/director/20230103143246_add_permanent_nats_credentials_to_vms.rb
+++ src/bosh-director/db/migrations/director/20230103143246_add_permanent_nats_credentials_to_vms.rb
@@ -4,4 +4,9 @@ Sequel.migration do
       add_column :permanent_nats_credentials, 'boolean', null: false, default: false
     end
   end
+  down do
+    alter_table(:vms) do
+      drop_column :permanent_nats_credentials
+    end
+  end
 end
