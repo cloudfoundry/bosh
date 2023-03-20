da89b599ab41d0fb4c575de781598a6d93c47393
Long Nguyen <nguyenlo@vmware.com>
2023-03-20 08:36:06 -0400

    Speed up template rendering by not using marshal load/dump

--- src/bosh_common/lib/common/deep_copy.rb
+++ src/bosh_common/lib/common/deep_copy.rb
@@ -1,7 +1,22 @@
 module Bosh::Common
   module DeepCopy
     def self.copy(object)
-      Marshal.load(Marshal.dump(object))
+      deep_copy(object)
+    end
+
+    def self.deep_copy(object)
+      if object.is_a?(Hash)
+        result = object.clone
+        object.each{|k, v| result[k] = self.deep_copy(v)}
+        result
+      elsif object.is_a?(Array)
+        result = object.clone
+        result.clear
+        object.each{|v| result << self.deep_copy(v)}
+        result
+      else
+        object.clone
+      end
     end
   end
 end
