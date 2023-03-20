f52274cf71c979d466869d6cb761f66c97adc6cb
Joseph Palermo <jpalermo@pivotal.io>
2023-03-20 14:19:50 -0700

    Revert "Speed up template rendering by not using marshal load/dump"
    These speed improvements were actually a masked bug in the new deep copy code.
    
    The code didn't handle complex objects and just shallow cloned them. Since most deep_copy
    during rendering happens on an EvanulationContext object, we were simply not doing any
    copying of the data.
    
    This does still show the expensive costs we have from copying the spec context objects
    around. Perhaps a future solution could simply not copy and instead freeze the objects.
    
    This reverts commit da89b599ab41d0fb4c575de781598a6d93c47393.

--- src/bosh_common/lib/common/deep_copy.rb
+++ src/bosh_common/lib/common/deep_copy.rb
@@ -1,22 +1,7 @@
 module Bosh::Common
   module DeepCopy
     def self.copy(object)
-      deep_copy(object)
-    end
-
-    def self.deep_copy(object)
-      if object.is_a?(Hash)
-        result = object.clone
-        object.each{|k, v| result[k] = self.deep_copy(v)}
-        result
-      elsif object.is_a?(Array)
-        result = object.clone
-        result.clear
-        object.each{|v| result << self.deep_copy(v)}
-        result
-      else
-        object.clone
-      end
+      Marshal.load(Marshal.dump(object))
     end
   end
 end
