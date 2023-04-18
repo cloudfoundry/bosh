b00b1a221e4873e0059e373e550bb5f4995b20c4
Long Nguyen <nguyenlo@vmware.com>
2023-04-18 14:43:43 -0400

    Adding support for assume roles in pipelines

--- ci/brats/aws_creds.tf
+++ ci/brats/aws_creds.tf
@@ -4,8 +4,15 @@ variable "aws_access_key_id" {
 variable "aws_secret_access_key" {
 }
 
+variable "aws_role_arn" {
+}
+
 provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
   region     = "us-west-1"
+
+  assume_role {
+    role_arn     = var.aws_role_arn
+  }
 }
