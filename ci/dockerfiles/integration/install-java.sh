92c8827451e9f8a06dde3bfe348086c258e12c30
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2023-05-09 09:36:24 +0200

    set the JAVA_HOME without knowing version
    this removes the necessity to update the file name when changing the
    version of the jdk

--- ci/dockerfiles/integration/install-java.sh
+++ ci/dockerfiles/integration/install-java.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-PACKAGE_NAME="zulu8.70.0.23-ca-jre8.0.372-linux_x64.tar.gz"
-PACKAGE_MD5="50763488aa8aef344a408115e709226a"
+PACKAGE_NAME="zulu8.70.0.23-ca-jdk8.0.372-linux_x64.tar.gz"
+PACKAGE_MD5="751deaa644a70fe7daa3d26bd3eb8e86"
 PACKAGE_TMP="/tmp/${PACKAGE_NAME}"
 PACKAGE_URL="http://cdn.azul.com/zulu/bin/${PACKAGE_NAME}"
 INSTALL_PREFIX="/usr/lib/jvm"
@@ -18,4 +18,5 @@ echo "${PACKAGE_MD5}  ${PACKAGE_TMP}" | md5sum -c -
 echo "Extracting Java..."
 mkdir -p $INSTALL_PREFIX
 tar xf $PACKAGE_TMP -C $INSTALL_PREFIX
+mv $INSTALL_PREFIX/*/* $INSTALL_PREFIX
 rm $PACKAGE_TMP
