652c8ee94ceebaafd06964f9eb07d5eeb806a72d
Tom Viehman <tviehman@vmware.com>
2023-05-17 16:12:23 -0700

    Remove unused creds from pipeline configure script
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -8,5 +8,4 @@ fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p bosh-director \
     -c ci/pipeline.yml \
     -l <(lpass show -G "bosh concourse secrets" --notes) \
     -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
-    -l <(lpass show --note "tracker-bot-story-delivery") \
     --var=branch_name=main
