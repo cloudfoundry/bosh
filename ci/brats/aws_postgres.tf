24e250ad71828a2977b2db0e4550a13506be0a26
Long Nguyen <nguyenlo@vmware.com>
2023-06-08 07:18:13 -0400

    Update to correct parameter group

--- ci/brats/aws_postgres.tf
+++ ci/brats/aws_postgres.tf
@@ -8,7 +8,7 @@ variable "rds_postgres_databasename" {
 }
 
 resource "aws_db_instance" "postgres" {
-  allocated_storage      = 10
+  allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "postgres"
   engine_version         = "15.3"
@@ -19,7 +19,7 @@ resource "aws_db_instance" "postgres" {
   password               = var.rds_postgres_password
   vpc_security_group_ids = [aws_security_group.allow-db-access.id]
   db_subnet_group_name   = aws_db_subnet_group.default.id
-  parameter_group_name   = "default.postgres10"
+  parameter_group_name   = "postgres15"
   publicly_accessible    = true
 }
 
