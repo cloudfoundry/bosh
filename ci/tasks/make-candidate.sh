0af9455730ce90c49ec7bcae8538646d6c0bf665
Joseph Palermo <jpalermo@vmware.com>
2023-08-23 14:30:32 -0700

    Remove version from make-candidate job
    The version resource was only being used to update some ERB templates
    with the "right" version. This version doesn't seem to be used anywhere,
    and the finalize-release job does similar things with the real version
    number.
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- ci/tasks/make-candidate.sh
+++ ci/tasks/make-candidate.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-export version=$(cat version/version)
 export ROOT_PATH=$PWD
 
 mv bosh-cli/alpha-bosh-cli-*-linux-amd64 bosh-cli/bosh-cli
@@ -10,8 +9,5 @@ export GO_CLI_PATH=$ROOT_PATH/bosh-cli/bosh-cli
 chmod +x $GO_CLI_PATH
 
 cd bosh-src
-
-sed -i "s/\['version'\] = ..*/['version'] = '$version'/" jobs/director/templates/director.yml.erb
-sed -i "s/version: ..*/version: $version/" jobs/director/templates/indicator.yml.erb
 
 $GO_CLI_PATH create-release --tarball=../release/bosh-dev-release.tgz --timestamp-version --force
