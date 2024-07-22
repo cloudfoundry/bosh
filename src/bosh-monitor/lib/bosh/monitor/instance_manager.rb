cloudfoundry/bosh

commit 9bbafa5af9716de110f37ec1b27b52a1ee6f2792

Brings up Health Monitor HTTP server faster
This commit adds a guard around the '/unresponsive_agents' endpoint so
that it will return a "not successful" HTTP status code (in this case,
503) if the initial "query all Deployments and their Instances" action
run by the Director has not yet completed.

To support that, it pushes down the guts of the fetch_deployments
function into the InstanceManager class.

This commit also moves the start of the Health Monitor HTTP server to
nearly the top of the 'Monitor#run' function. This should get the
'/healthz' endpoint started as quickly as possible so that we don't get
terminated by monit just because querying the state of the
Director-managed deployments takes longer than ten seconds.

Why do this?

We've discovered that Monit gives a monitored service ~10 seconds to
bring up its health-checking HTTP server before Monit declares the
service dead and restarts it. What this means is that Director Health
Monitors that are on underprovisioned VMs and/or that have large numbers
of Deployments and/or Instances make take longer than 10 seconds to come
up, and may find themselves in an unending restart loop.

The guard around the '/unresponsive_agents' endpoint is added to
preserve the previous behavior of "Calls to /unresponsive_agents do not
succeed until the initial query of Director-managed deployments
completes.".

[#187938284] [JIRA] BOSH-296 "Health monitor continuously exiting for foundation with large number of VMs"

Signed-off-by: Aram Price <aram.price@broadcom.com>

src/bosh-monitor/lib/bosh/monitor/instance_manager.rb
@@ -3,6 +3,7 @@ module Bosh::Monitor
     attr_reader :heartbeats_received
     attr_reader :alerts_received
     attr_reader :alerts_processed
+    attr_reader :director_initial_deployment_sync_done
 
     attr_accessor :processor
 
@@ -16,8 +17,26 @@ module Bosh::Monitor
       @heartbeats_received = 0
       @alerts_received = 0
       @alerts_processed = 0
+      @director_initial_deployment_sync_done = false
 
       @processor = event_processor
+    end
+
+    def fetch_deployments(director)
+      deployments = director.deployments
+
+      @instance_manager.sync_deployments(deployments)
+
+      deployments.each do |deployment|
+        deployment_name = deployment['name']
+
+        @logger.info("Found deployment '#{deployment_name}'")
+
+        @logger.debug("Fetching instances information for '#{deployment_name}'...")
+        instances_data = director.get_deployment_instances(deployment_name)
+        @instance_manager.sync_deployment_state(deployment, instances_data)
+      end
+      @director_initial_deployment_sync_done = true
     end
 
     # Get a hash of agent id -> agent object for all agents associated with the deployment

src/bosh-monitor/lib/bosh/monitor/api_controller.rb
@@ -34,7 +34,11 @@ module Bosh::Monitor
     end
 
     get '/unresponsive_agents' do
-      JSON.generate(@instance_manager.unresponsive_agents)
+      if @instance_manager.director_initial_deployment_sync_done
+        JSON.generate(@instance_manager.unresponsive_agents)
+      else
+        status(503)
+      end
     end
   end
 end

src/bosh-monitor/lib/bosh/monitor/runner.rb
@@ -21,12 +21,12 @@ module Bosh::Monitor
       @logger.info('HealthMonitor starting...')
 
       Sync do
+        start_http_server
         connect_to_mbus
         @director_monitor = DirectorMonitor.new(Bhm)
         @director_monitor.subscribe
         @instance_manager.setup_events
         setup_timers
-        start_http_server
         update_resurrection_config
         @logger.info("BOSH HealthMonitor #{Bhm::VERSION} is running...")
       rescue => e
@@ -177,19 +177,7 @@ module Bosh::Monitor
     end
 
     def fetch_deployments
-      deployments = @director.deployments
-
-      @instance_manager.sync_deployments(deployments)
-
-      deployments.each do |deployment|
-        deployment_name = deployment['name']
-
-        @logger.info("Found deployment '#{deployment_name}'")
-
-        @logger.debug("Fetching instances information for '#{deployment_name}'...")
-        instances_data = @director.get_deployment_instances(deployment_name)
-        @instance_manager.sync_deployment_state(deployment, instances_data)
-      end
+      @instance_manager.fetch_deployments(@director)
     rescue Bhm::DirectorError => e
       log_exception(e)
       alert_director_error(e.message)

src/bosh-monitor/spec/functional/notifying_plugins_spec.rb
@@ -64,9 +64,18 @@ describe 'notifying plugins' do
         reactor.with_timeout(5) do
           loop do
             sleep(0.1)
-            alert = get_alert
             called = true
-            break if alert&.attributes == payload
+
+            get_alerts.each do |a|
+              if a&.attributes == payload
+                alert = a
+                break;
+              end
+            end
+
+            if alert != nil
+              break
+            end
           end
         end
       end.wait
@@ -142,7 +151,12 @@ describe 'notifying plugins' do
   end
 
   def get_alert
+    ret = get_alerts
+    ret.first if ret
+  end
+
+  def get_alerts
     dummy_plugin = Bosh::Monitor.event_processor.plugins[:alert].first
-    return dummy_plugin.events.first if dummy_plugin.events
+    return dummy_plugin.events if dummy_plugin.events
   end
 end

src/bosh-monitor/spec/unit/bosh/monitor/api_controller_spec.rb
@@ -65,12 +65,25 @@ describe Bosh::Monitor::ApiController do
     end
     before do
       allow(Bosh::Monitor.instance_manager).to receive(:unresponsive_agents).and_return(unresponsive_agents)
+      allow(Bosh::Monitor.instance_manager).to receive(:director_initial_deployment_sync_done).and_return(true)
     end
 
     it 'renders the unresponsive agents' do
       get '/unresponsive_agents'
       expect(last_response.status).to eq(200)
       expect(last_response.body).to eq(JSON.generate(unresponsive_agents))
+    end
+
+    context 'When director initial deployment sync has not completed' do
+      before do
+        allow(Bosh::Monitor.instance_manager).to receive(:director_initial_deployment_sync_done).and_return(false)
+      end
+
+      it 'returns 503 when /unresponsive_agents is requested' do
+        get '/unresponsive_agents'
+        expect(last_response.status).to eq(503)
+      end
+
     end
   end
 end

commit 38f4740a6073a824b5343bcd77feb2acb529e5bb

Corrects instance_manager#fetch_deployments
Commit 9bbafa5af9716de110f37ec1b27b52a1ee6f2792 introduced an error in
our relocated fetch_deployments function. Specifically, because we
copied and pasted the code from elsewhere with insufficient caution, we
referred to an instance variable ('@instance_manager') that does not
appear in the new scope.

This commit corrects that error.

[#187938284] [JIRA] BOSH-296 "Health monitor continuously exiting for foundation with large number of VMs"

Signed-off-by: Rajath Agasthya <rajath.agasthya@broadcom.com>

src/bosh-monitor/lib/bosh/monitor/instance_manager.rb
@@ -25,7 +25,7 @@ module Bosh::Monitor
     def fetch_deployments(director)
       deployments = director.deployments
 
-      @instance_manager.sync_deployments(deployments)
+      sync_deployments(deployments)
 
       deployments.each do |deployment|
         deployment_name = deployment['name']
@@ -34,7 +34,7 @@ module Bosh::Monitor
 
         @logger.debug("Fetching instances information for '#{deployment_name}'...")
         instances_data = director.get_deployment_instances(deployment_name)
-        @instance_manager.sync_deployment_state(deployment, instances_data)
+        sync_deployment_state(deployment, instances_data)
       end
       @director_initial_deployment_sync_done = true
     end