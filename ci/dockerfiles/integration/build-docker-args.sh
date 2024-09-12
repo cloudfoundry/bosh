4a443b5f02bc3b02ea0237c2f573dd3edb184c03
Ming Xiao <ming.xiao@broadcom.com>
2024-09-12 14:58:10 -0700

    Add `yq` to the bosh/integration image
    Signed-off-by: aram price <aram.price@broadcom.com>

--- ci/dockerfiles/integration/build-docker-args.sh
+++ ci/dockerfiles/integration/build-docker-args.sh
@@ -5,6 +5,8 @@ bosh_cli_url="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -s https:/
                 | jq -r '.assets[] | select(.name | contains ("linux-amd64")) | .browser_download_url')"
 meta4_cli_url="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -s https://api.github.com/repos/dpb587/metalink/releases/latest \
                 | jq -r '.assets[] | select(.name | match("meta4-[0-9]+.[0-9]+.[0-9]+-linux-amd64")) | .browser_download_url')"
+yq_cli_url="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -s https://api.github.com/repos/mikefarah/yq/releases/latest \
+                | jq -r '.assets[] | select(.name | endswith ("linux_amd64")) | .browser_download_url')"
 ruby_install_url="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -s https://api.github.com/repos/postmodern/ruby-install/releases/latest \
                     | jq -r '.assets[] | select(.name | endswith ("tar.gz")) | .browser_download_url')"
 
@@ -20,6 +22,7 @@ cat << JSON > docker-build-args/docker-build-args.json
 {
   "BOSH_CLI_URL": "${bosh_cli_url}",
   "META4_CLI_URL": "${meta4_cli_url}",
+  "YQ_CLI_URL": "${yq_cli_url}",
 
   "RUBY_INSTALL_URL": "${ruby_install_url}",
   "RUBY_VERSION": "${ruby_version}",
