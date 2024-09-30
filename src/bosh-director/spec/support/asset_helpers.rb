a41f1f5952a082b2b563b64b4cc58302546986a2
aram price <aram.price@broadcom.com>
2024-09-30 15:11:06 -0700

    Remove `Bosh::Director::Test::` from `AssetHelpers`

--- src/bosh-director/spec/support/asset_helpers.rb
+++ src/bosh-director/spec/support/asset_helpers.rb
@@ -1,15 +1,13 @@
-module Bosh::Director::Test
-  module AssetHelpers
-    def spec_asset(filename)
-      File.read(asset(filename))
-    end
+module AssetHelpers
+  def spec_asset(filename)
+    File.read(asset(filename))
+  end
 
-    def asset(filename)
-      File.expand_path("../../assets/#{filename}", __FILE__)
-    end
+  def asset(filename)
+    File.expand_path("../../assets/#{filename}", __FILE__)
   end
 end
 
 RSpec.configure do |config|
-  config.include(Bosh::Director::Test::AssetHelpers)
+  config.include(AssetHelpers)
 end
