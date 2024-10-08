38c2105593afc0a3281a63116a7e489fcef4b854
aram price <aram.price@broadcom.com>
2024-10-07 22:13:38 -0700

    `#fake_app` method returns a stricter instance double
    Previously a generic `double()` was being returned `as_null_object`
    which allowed any number of method chains to be called. This commit
    changes the object to be a set of instance doubles with one specific
    method `#can_sign_urls?` explicitly stubbed on
    `Bosh::Blobstore::BaseClient` as `false` which would have been the
    value returned in the "null object" case.

--- src/bosh-director/spec/support/app_helper.rb
+++ src/bosh-director/spec/support/app_helper.rb
@@ -1,7 +1,13 @@
 module Support
   module AppHelpers
     def fake_app
-      allow(Bosh::Director::App).to receive_messages(instance: double('App Instance').as_null_object)
+      client_double = instance_double(Bosh::Blobstore::BaseClient, can_sign_urls?: false)
+
+      blobstores_double = instance_double(Bosh::Director::Blobstores, blobstore: client_double)
+
+      app_double = instance_double(Bosh::Director::App, blobstores: blobstores_double)
+
+      allow(Bosh::Director::App).to receive_messages(instance: app_double)
     end
   end
 end
