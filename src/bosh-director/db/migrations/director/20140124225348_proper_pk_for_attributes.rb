fbd5d15e69ccb5e8cfe98095c6939438e2bdb698
aram price <aram.price@broadcom.com>
2024-10-22 17:27:38 -0700

    Upgrade `sequel` from `5.29` => `5.85`
    Because sequel now handles migrations ~better~ differently with sqlite
    some older migrations had to be modified to accommodate the stricter
    behavior/expectations of sequel + sqlite. As a consquence in the
    migrations which were not functioning in sqlite we resorted to a backup,
    recreate, repopulate strategy since `alter_table` and friends were not
    behaving as expected with sqlite.
    
    Sqlite was added to the Github Actions test matrix for the bosh-director
    internal gem.
    
    Signed-off-by: Brian Upton <brian.upton@broadcom.com>

--- src/bosh-director/db/migrations/director/20140124225348_proper_pk_for_attributes.rb
+++ src/bosh-director/db/migrations/director/20140124225348_proper_pk_for_attributes.rb
@@ -1,15 +1,37 @@
 Sequel.migration do
   change do
-    add_column :director_attributes, :temp_name, String, null: true
-    self[:director_attributes].update(temp_name: :name)
+    if [:sqlite].include?(adapter_scheme)
 
-    alter_table :director_attributes do
-      drop_column :name
-      rename_column :temp_name, :name
-      add_index [:name], unique:true, name: 'unique_attribute_name'
-      set_column_not_null :name
+      rename_table(:director_attributes, :old_director_attributes)
 
-      add_primary_key :id
+      create_table(:director_attributes) do
+        primary_key :id
+        String :name, unique: true, null: false
+        String :value
+      end
+
+      # Some directors might have created multiple records.
+      # Only the first record is ever used after every director restart.
+      self[:old_director_attributes].each do |old_director_attributes_row|
+        self[:director_attributes].insert(old_director_attributes_row)
+      end
+
+      drop_table(:old_director_attributes)
+
+    else # mysql + postgresql
+
+      add_column :director_attributes, :temp_name, String, null: true
+      self[:director_attributes].update(temp_name: :name)
+
+      alter_table :director_attributes do
+        drop_column :name
+        rename_column :temp_name, :name
+        add_index [:name], unique:true, name: 'unique_attribute_name'
+        set_column_not_null :name
+
+        add_primary_key :id
+      end
+
     end
   end
 end
--- src/bosh-director/db/migrations/director/20160210201838_denormalize_compiled_package_stemcell_id_to_stemcell_name_and_version.rb
+++ src/bosh-director/db/migrations/director/20160210201838_denormalize_compiled_package_stemcell_id_to_stemcell_name_and_version.rb
@@ -1,55 +1,92 @@
 Sequel.migration do
   change do
-    alter_table(:compiled_packages) do
-      add_column :stemcell_os, String
-      add_column :stemcell_version, String
-    end
+    if [:sqlite].include?(adapter_scheme)
 
-    self[:compiled_packages].each do |compiled_package|
-      next unless compiled_package[:stemcell_id]
+      rename_table(:compiled_packages, :old_compiled_packages)
 
-      stemcell = self[:stemcells].filter(id: compiled_package[:stemcell_id]).first
-      stemcell_os = stemcell[:operating_system].to_s.empty? ? stemcell[:name] : stemcell[:operating_system]
-
-      self[:compiled_packages].filter(id: compiled_package[:id]).update(
-        stemcell_os: stemcell_os,
-        stemcell_version: stemcell[:version]
-      )
-    end
-
-    if [:mysql2, :mysql, :postgres].include?(adapter_scheme)
-      stemcell_foreign_keys = foreign_key_list(:compiled_packages).select { |constraint| constraint.fetch(:columns).include?(:stemcell_id) }
-      raise 'Failed to run migration, found more than 1 stemcell foreign key' if stemcell_foreign_keys.size != 1
-      stemcell_foreign_key_name = stemcell_foreign_keys.first.fetch(:name)
-
-      build_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :build] }
-      if [:mysql2, :mysql].include?(adapter_scheme)
-        raise 'Failed to run migration, found more than 1 build index' if build_indexes.size != 1
-        # build_indexes is an array, where each element is an array with first element being the name of index
-        build_index = build_indexes.first.first
-        alter_table(:compiled_packages) do
-          drop_index(nil, name: build_index)
-        end
-      elsif [:postgres].include?(adapter_scheme)
-        build_index = build_indexes.empty? ? 'compiled_packages_package_id_stemcell_id_build_key' : build_indexes.first.first
-        alter_table(:compiled_packages) do
-          drop_constraint(build_index)
-        end
+      create_table(:compiled_packages) do
+        primary_key :id
+        String :blobstore_id, null: false
+        String :sha1, null: false
+        String :dependency_key, null: false
+        Integer :build, unsigned: true, null: false
+        String :dependency_key_sha1, null: false
+        String :stemcell_os
+        String :stemcell_version
+        foreign_key :package_id, :packages, null: false
+        unique [:package_id, :stemcell_os, :stemcell_version, :build]
+        unique [:package_id, :stemcell_os, :stemcell_version, :dependency_key_sha1]
       end
 
-      stemcell_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :dependency_key_sha1]}
-      stemcell_index = stemcell_indexes.empty? ? 'package_stemcell_dependency_key_sha1_idx' : stemcell_indexes.first.first
+      self[:old_compiled_packages].each do |old_compiled_packages_row|
+        if (stemcell_id = old_compiled_packages_row.delete(:stemcell_id))
+          stemcell = self[:stemcells].filter(id: stemcell_id).first
+          stemcell_os_or_name =
+            "#{stemcell[:operating_system]}".empty? ? stemcell[:name] : stemcell[:operating_system]
+
+          old_compiled_packages_row[:stemcell_os] = stemcell_os_or_name
+          old_compiled_packages_row[:stemcell_version] =  stemcell[:version]
+        end
+
+        self[:compiled_packages].insert(old_compiled_packages_row)
+      end
+
+      drop_table(:old_compiled_packages)
+
+    else # mysql + postgresql
 
       alter_table(:compiled_packages) do
-        drop_constraint(stemcell_foreign_key_name, :type => :foreign_key)
-        add_index [:package_id, :stemcell_os, :stemcell_version, :build], unique: true, name: 'package_stemcell_build_idx'
-        add_index [:package_id, :stemcell_os, :stemcell_version, :dependency_key_sha1], unique: true, name: 'package_stemcell_dependency_idx'
-        drop_index(nil, name: stemcell_index)
+        add_column :stemcell_os, String
+        add_column :stemcell_version, String
       end
-    end
 
-    alter_table(:compiled_packages) do
-      drop_column :stemcell_id
+      self[:compiled_packages].each do |compiled_package|
+        next unless compiled_package[:stemcell_id]
+
+        stemcell = self[:stemcells].filter(id: compiled_package[:stemcell_id]).first
+        stemcell_os = stemcell[:operating_system].to_s.empty? ? stemcell[:name] : stemcell[:operating_system]
+
+        self[:compiled_packages].filter(id: compiled_package[:id]).update(
+          stemcell_os: stemcell_os,
+          stemcell_version: stemcell[:version]
+        )
+      end
+
+      if [:mysql2, :mysql, :postgres].include?(adapter_scheme)
+        stemcell_foreign_keys = foreign_key_list(:compiled_packages).select { |constraint| constraint.fetch(:columns).include?(:stemcell_id) }
+        raise 'Failed to run migration, found more than 1 stemcell foreign key' if stemcell_foreign_keys.size != 1
+        stemcell_foreign_key_name = stemcell_foreign_keys.first.fetch(:name)
+
+        build_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :build] }
+        if [:mysql2, :mysql].include?(adapter_scheme)
+          raise 'Failed to run migration, found more than 1 build index' if build_indexes.size != 1
+          # build_indexes is an array, where each element is an array with first element being the name of index
+          build_index = build_indexes.first.first
+          alter_table(:compiled_packages) do
+            drop_index(nil, name: build_index)
+          end
+        elsif [:postgres].include?(adapter_scheme)
+          build_index = build_indexes.empty? ? 'compiled_packages_package_id_stemcell_id_build_key' : build_indexes.first.first
+          alter_table(:compiled_packages) do
+            drop_constraint(build_index)
+          end
+        end
+
+        stemcell_indexes = indexes(:compiled_packages).select { |_, value| value.fetch(:columns) == [:package_id, :stemcell_id, :dependency_key_sha1]}
+        stemcell_index = stemcell_indexes.empty? ? 'package_stemcell_dependency_key_sha1_idx' : stemcell_indexes.first.first
+
+        alter_table(:compiled_packages) do
+          drop_constraint(stemcell_foreign_key_name, :type => :foreign_key)
+          add_index [:package_id, :stemcell_os, :stemcell_version, :build], unique: true, name: 'package_stemcell_build_idx'
+          add_index [:package_id, :stemcell_os, :stemcell_version, :dependency_key_sha1], unique: true, name: 'package_stemcell_dependency_idx'
+          drop_index(nil, name: stemcell_index)
+        end
+      end
+
+      alter_table(:compiled_packages) do
+        drop_column :stemcell_id
+      end
+
     end
   end
 end
