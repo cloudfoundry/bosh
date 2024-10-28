0b5df41e740965c5efa54f2ba9725460a62379d1
aram price <aram.price@broadcom.com>
2024-10-28 14:13:10 -0700

    Remove unnecessary require

--- src/bosh-dev/spec/spec_helper.rb
+++ src/bosh-dev/spec/spec_helper.rb
@@ -4,7 +4,6 @@ require 'rake'
 require 'fakefs/spec_helpers'
 require 'webmock/rspec'
 require 'sequel'
-require 'sequel/adapters/sqlite'
 require 'support/buffered_logger'
 
 Dir.glob(File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require(f) }
