60f7c1a75a63f00528352ce84fbf01fc52136f78
aram price <aram.price@broadcom.com>
2024-10-29 22:06:16 -0700

    Simplify simplecov configuration

--- src/spec/shared/shared_support/simplecov.rb
+++ src/spec/shared/shared_support/simplecov.rb
@@ -1,13 +1,8 @@
 require 'simplecov'
 
-SimpleCov.configure do
-  add_filter '/spec/'
-  add_filter '/vendor/'
-end
-
 SimpleCov.start do
-  root          BOSH_REPO_SRC_DIR
-  merge_timeout 3600
-  # command name is injected by the spec.rake runner
-  command_name ENV['BOSH_BUILD_NAME'] if ENV['BOSH_BUILD_NAME']
+  add_filter('/spec/')
+  add_filter('/vendor/')
+  merge_timeout(3600)
+  root(BOSH_REPO_SRC_DIR)
 end
