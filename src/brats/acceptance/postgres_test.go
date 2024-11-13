0a0df88c3f0489cb3735729fd039b36229a074bf
aram price <aram.price@broadcom.com>
2024-11-12 17:10:00 -0800

    Remove postgres-10 from bosh release
    Items of note
    - worker_ctl.erb had a reference to `postgres-10/bin` which was changed
      to `postgres-13/bin`; we suspect this may not be needed in $PATH
    - a warning about upgrading `postgres-13` was commented out in the
      prestart script since we currently still support upgrades from v13
    
    Signed-off-by: Brian Upton <brian.upton@broadcom.com>

--- src/brats/acceptance/postgres_test.go
+++ src/brats/acceptance/postgres_test.go
@@ -17,7 +17,6 @@ var _ = Describe("postgres", func() {
 		var (
 			postgres15Manifest     string
 			postgres13Manifest     string
-			postgres10Manifest     string
 			postgresDeploymentName string
 		)
 
@@ -31,30 +30,6 @@ var _ = Describe("postgres", func() {
 			postgres13Manifest = utils.AssetPath("postgres-13-manifest.yml")
 
 			session := utils.OuterBosh("deploy", "-n", postgres13Manifest,
-				"-d", postgresDeploymentName,
-				"-v", fmt.Sprintf("stemcell-os=%s", utils.StemcellOS()),
-				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
-			)
-			Eventually(session, 15*time.Minute).Should(gexec.Exit(0))
-
-			session = utils.OuterBosh("deploy", "-n", postgres15Manifest,
-				"-d", postgresDeploymentName,
-				"-v", fmt.Sprintf("stemcell-os=%s", utils.StemcellOS()),
-				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
-			)
-			Eventually(session, 15*time.Minute).Should(gexec.Exit(0))
-		})
-
-		It("Upgrades from 10", func() {
-			var err error
-
-			postgresDeploymentName, err = uuid.NewGenerator().Generate()
-			Expect(err).NotTo(HaveOccurred())
-
-			postgres15Manifest = utils.AssetPath("postgres-manifest.yml")
-			postgres10Manifest = utils.AssetPath("postgres-10-manifest.yml")
-
-			session := utils.OuterBosh("deploy", "-n", postgres10Manifest,
 				"-d", postgresDeploymentName,
 				"-v", fmt.Sprintf("stemcell-os=%s", utils.StemcellOS()),
 				"-v", fmt.Sprintf("deployment-name=%s", postgresDeploymentName),
