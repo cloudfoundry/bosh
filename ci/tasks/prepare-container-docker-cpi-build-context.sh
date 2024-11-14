f88f334d4f97b4f38efd4a2a412ab719a30b4199
aram price <aram.price@broadcom.com>
2024-11-14 11:26:22 -0800

    Replace prepare-container-docker-cpi-build-context with generic task
    This job now uses: prepare-build-context-for-container-cpi-image

--- ci/tasks/prepare-container-docker-cpi-build-context.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -rp bosh-ci-dockerfiles/ci/dockerfiles/docker-cpi/* docker-build-context
-
-cp bosh-cli/*bosh* docker-build-context/bosh
-
-mkdir docker-build-context/bosh-deployment
-cp -R bosh-deployment/* docker-build-context/bosh-deployment
