8a005e0e5f5061d9b763130c2c8085313fd74ffb
aram price <aram.price@broadcom.com>
2024-11-14 11:44:40 -0800

    CI: warden cpi image build uses common prepare task
    Signed-off-by: Brian Upton <brian.upton@broadcom.com>

--- ci/dockerfiles/warden-cpi/prepare-container-build-context.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-cp -rp bosh-ci-dockerfiles/ci/dockerfiles/warden-cpi/* docker-build-context
-
-mkdir docker-build-context/bosh-deployment
-cp -R bosh-deployment/* docker-build-context/bosh-deployment
