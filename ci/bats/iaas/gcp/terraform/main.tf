16a587e7e1c04a7c589966f93b119d8bdb85bdea
Brian Upton <brian.upton@broadcom.com>
2024-11-14 11:21:08 -0800

    Give private IP an environment specific name
    If both the upgrade-postgres and upgrade-mysql jobs ran at the same
    time, the private-ip-address resource will have conflicting names and
    cause terraform to fail with an error like:
    │ Error: Error creating GlobalAddress: googleapi: Error 409: The resource 'projects/cloud-foundry-310819/global/addresses/private-ip-address' already exists, alreadyExists

--- ci/bats/iaas/gcp/terraform/main.tf
+++ ci/bats/iaas/gcp/terraform/main.tf
@@ -44,7 +44,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 }
 
 resource "google_compute_global_address" "private_ip_address" {
-  name          = "private-ip-address"
+  name          = "${var.name}-private-ip-address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
