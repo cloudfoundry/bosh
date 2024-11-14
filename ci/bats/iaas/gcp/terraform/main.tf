fd21de858ad6990c36a6f785663e67ecfda92862
aram price <aram.price@broadcom.com>
2024-11-13 19:45:20 -0800

    BATS terraform: add reserved_peering_ranges to google_service_networking_connection

--- ci/bats/iaas/gcp/terraform/main.tf
+++ ci/bats/iaas/gcp/terraform/main.tf
@@ -40,6 +40,7 @@ resource "google_compute_subnetwork" "bosh-second-subnet" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.network.id
   service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
 resource "google_compute_global_address" "private_ip_address" {
