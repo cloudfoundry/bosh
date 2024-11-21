60163e52c8682a7bcf7ba60aad3d9e7fea7631e7
aram price <aram.price@broadcom.com>
2024-11-21 12:42:36 -0800

    Spec: nets-sync define paths in terms of `SPEC_ROOT`
    - add require for `tempfile`

--- src/bosh-nats-sync/spec/spec_helper.rb
+++ src/bosh-nats-sync/spec/spec_helper.rb
@@ -1,11 +1,15 @@
-require File.expand_path('../../spec/shared/spec_helper', __dir__)
+SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
+
+require File.join(SPEC_ROOT, '../../spec/shared/spec_helper')
+
 require 'nats_sync'
 require 'webmock/rspec'
+require 'tempfile'
 
-Dir.glob(File.expand_path('support/**/*.rb', __dir__)).each { |f| require(f) }
+Dir.glob(File.join(SPEC_ROOT, 'support/**/*.rb')).each { |f| require(f) }
 
 def asset_path(filename)
-  File.expand_path(File.join(File.dirname(__FILE__), 'assets', filename))
+  File.join(SPEC_ROOT, 'assets', filename)
 end
 
 def sample_config
