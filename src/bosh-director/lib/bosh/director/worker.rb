c6097fb9392655f447a79d91f9be4e81e9e2054d
aram price <aram.price@broadcom.com>
2024-11-21 16:57:40 -0800

    Remove unneeded require since PR was merged
    See: https://github.com/collectiveidea/delayed_job/pull/1185

--- src/bosh-director/lib/bosh/director/worker.rb
+++ src/bosh-director/lib/bosh/director/worker.rb
@@ -1,7 +1,5 @@
 require 'db_migrator'
 require 'delayed_job'
-#TODO: remove when https://github.com/collectiveidea/delayed_job/pull/1185 is merged
-require "active_support/core_ext/array/extract_options"
 
 module Bosh
   module Director
