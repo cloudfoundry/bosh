39d760ac6478f3cb8d4e2d7f4fc78cf43a21aff9
Rajath Agasthya <rajath.agasthya@broadcom.com>
2024-12-04 14:01:30 -0600

    Add leftovers script for cleaning up mysql/postgres resources
    Cleans up resources daily after upgrade-{mysql,postgres} jobs.
    
    Signed-off-by: Aram Price <aram.price@broadcom.com>

---
+++ ci/tasks/cleanup-leftover-environments.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+export BBL_GCP_SERVICE_ACCOUNT_KEY="gcp_service_account.json"
+echo "${GCP_SERVICE_ACCOUNT_JSON}" > ${BBL_GCP_SERVICE_ACCOUNT_KEY}
+
+go run github.com/genevieve/leftovers/cmd/leftovers@latest -- -n -i gcp -f "${LEFTOVERS_PREFIX}"
