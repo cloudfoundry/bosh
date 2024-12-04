d395b25b24d7baecac4216224989f1f2446bb4a8
aram price <aram.price@broadcom.com>
2024-12-04 13:06:09 -0800

    CI: correctly print out leftovers command...

--- ci/tasks/cleanup-leftovers.sh
+++ ci/tasks/cleanup-leftovers.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
 
 export BBL_GCP_SERVICE_ACCOUNT_KEY="gcp_service_account.json"
 
+set +x # hide credentials
 echo "${GCP_JSON_KEY}" > ${BBL_GCP_SERVICE_ACCOUNT_KEY}
-
-set +x
+set -x
 
 go run "github.com/genevieve/leftovers/cmd/leftovers@${LEFTOVERS_VERSION}" -- \
   --no-confirm \
