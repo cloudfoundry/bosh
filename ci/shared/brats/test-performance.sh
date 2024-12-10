3296b5c72afe380968706eec521091d91e4feac1
aram price <aram.price@broadcom.com>
2024-12-09 17:41:29 -0800

    CI: switch `bosh-ci`/`bosh` resource optionality
    BRATs specs only need one of these copies of the resource, prefer
    `bosh-ci` since scripts only read test assets, not production code.
    
    Update variable names in scripts for clarity.

--- ci/shared/brats/test-performance.sh
+++ ci/shared/brats/test-performance.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-bosh_repo_dir="$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)")"
-bosh_repo_parent_dir="$(realpath "${bosh_repo_dir}/..")"
+bosh_ci_dir="$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)")"
+bosh_ci_parent_dir="$(realpath "${bosh_ci_dir}/..")"
 
 export BOSH_DEPLOYMENT_PATH="/usr/local/bosh-deployment"
 
-source "${bosh_repo_dir}/ci/dockerfiles/docker-cpi/start-bosh.sh"
+source "${bosh_ci_dir}/ci/dockerfiles/docker-cpi/start-bosh.sh"
 source /tmp/local-bosh/director/env
 
 bosh int /tmp/local-bosh/director/creds.yml --path /jumpbox_ssh/private_key > /tmp/jumpbox_ssh_key.pem
@@ -16,11 +16,11 @@ export BOSH_DIRECTOR_IP="10.245.0.3"
 
 BOSH_BINARY_PATH=$(which bosh)
 export BOSH_BINARY_PATH
-BOSH_DIRECTOR_TARBALL_PATH="$(find "${bosh_repo_parent_dir}/bosh-release" -maxdepth 1 -path '*.tgz')"
+BOSH_DIRECTOR_TARBALL_PATH="$(find "${bosh_ci_parent_dir}/bosh-release" -maxdepth 1 -path '*.tgz')"
 export BOSH_DIRECTOR_TARBALL_PATH
-export BOSH_DIRECTOR_RELEASE_PATH="${bosh_repo_parent_dir}/bosh"
-export CF_DEPLOYMENT_RELEASE_PATH="${bosh_repo_parent_dir}/cf-deployment"
-CANDIDATE_STEMCELL_TARBALL_PATH="$(find "${bosh_repo_parent_dir}/stemcell" -maxdepth 1 -path '*.tgz')"
+export BOSH_DIRECTOR_RELEASE_PATH="${bosh_ci_parent_dir}/bosh"
+export CF_DEPLOYMENT_RELEASE_PATH="${bosh_ci_parent_dir}/cf-deployment"
+CANDIDATE_STEMCELL_TARBALL_PATH="$(find "${bosh_ci_parent_dir}/stemcell" -maxdepth 1 -path '*.tgz')"
 export CANDIDATE_STEMCELL_TARBALL_PATH
 export STEMCELL_OS=ubuntu-jammy
 
@@ -31,7 +31,7 @@ export DOCKER_HOST
 
 bosh -n update-cloud-config \
   "${BOSH_DEPLOYMENT_PATH}/docker/cloud-config.yml" \
-  -o "${bosh_repo_dir}/ci/dockerfiles/docker-cpi/outer-cloud-config-ops.yml" \
+  -o "${bosh_ci_dir}/ci/dockerfiles/docker-cpi/outer-cloud-config-ops.yml" \
   -v network=director_network
 
 bosh -n upload-stemcell "${CANDIDATE_STEMCELL_TARBALL_PATH}"
@@ -41,7 +41,7 @@ bosh upload-release "$(bosh int ${BOSH_DEPLOYMENT_PATH}/docker/cpi.yml --path /n
 bosh upload-release "$(bosh int ${BOSH_DEPLOYMENT_PATH}/jumpbox-user.yml --path /release=os-conf/value/url)" \
   --sha1 "$(bosh int ${BOSH_DEPLOYMENT_PATH}/jumpbox-user.yml --path /release=os-conf/value/sha1)"
 
-pushd "${bosh_repo_dir}/src/brats/performance"
+pushd "${bosh_ci_dir}/src/brats/performance"
   go run github.com/onsi/ginkgo/v2/ginkgo \
     -r -v --race --timeout=24h \
     --randomize-suites --randomize-all \
--- ci/shared/brats/test-utils.sh
+++ ci/shared/brats/test-utils.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-bosh_repo_dir="$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)")"
+bosh_ci_dir="$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)")"
 
-pushd "${bosh_repo_dir}/src/brats/utils"
+pushd "${bosh_ci_dir}/src/brats/utils"
   go run github.com/onsi/ginkgo/v2/ginkgo \
     -r -v --race --timeout=24h \
     --randomize-suites --randomize-all \
