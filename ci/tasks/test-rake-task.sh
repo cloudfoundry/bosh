8631cfd0d01f31da348bc81201b6724346215c44
aram price <aram.price@broadcom.com>
2025-01-10 11:50:14 -0800

    Spec: test-rake-task can run integration spec task

--- ci/tasks/test-rake-task.sh
+++ ci/tasks/test-rake-task.sh
@@ -3,6 +3,10 @@ set -eu -o pipefail
 
 source bosh-ci/ci/tasks/utils.sh
 
+if [ -d bosh-cli ] ; then
+  install bosh-cli/*bosh-cli-*-linux-amd64 "/usr/local/bin/bosh"
+fi
+
 start_db "${DB}"
 
 pushd bosh/src
