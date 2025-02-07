e20b3db7e9958d5d0e2e3183ea8ab528feaf5820
Dennis Ahaus <dennis.ahaus@sap.com>
2025-02-07 13:41:35 +0100

    Adding runtime config tags on update stemcell

--- src/bosh-director/lib/bosh/director/jobs/update_stemcell.rb
+++ src/bosh-director/lib/bosh/director/jobs/update_stemcell.rb
@@ -109,7 +109,16 @@ module Bosh::Director
             upload_suffix = ' (already exists, skipped)' unless needs_upload
             track_and_log("Uploading stemcell #{@name}/#{@version} to the cloud#{cpi_suffix}#{upload_suffix}") do
               if needs_upload
-                stemcell.cid = cloud.create_stemcell(@stemcell_image, @cloud_properties, {})
+                tags = {}
+                runtime_configs = Bosh::Director::Api::RuntimeConfigManager.new.list(1, "default")
+                if !runtime_configs.nil? && !runtime_configs[0].nil?
+                  raw = runtime_configs[0]
+                  if !raw.nil? 
+                    hash = raw.to_hash
+                    tags.merge!(hash["tags"])
+                  end
+                end
+                stemcell.cid = cloud.create_stemcell(@stemcell_image, @cloud_properties, {"tags" => tags})
                 logger.info("Cloud created stemcell#{cpi_suffix}: #{stemcell.cid}")
               else
                 logger.info("Skipping stemcell upload, already exists#{cpi_suffix}")
