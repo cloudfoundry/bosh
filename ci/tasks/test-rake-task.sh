989e4639a23701e5b3ffef01cb701baeb0924344
Joseph Palermo <joseph.palermo@broadcom.com>
2025-03-03 11:38:58 -0800

    Makes /tmp/postgres owned by 'postgres'
    We're not sure why the subsequent 'mkdir /tmp/postgres/data' worked just
    fine when this was on 'tmpfs', but now the directory that we create
    needs to be owned by 'postgres'.
    
    So, this commit makes it so.
    
    Signed-off-by: Kenneth Lakin <kenneth.lakin@broadcom.com>

--- ci/tasks/test-rake-task.sh
+++ ci/tasks/test-rake-task.sh
@@ -85,7 +85,7 @@ start_db() {
 
       export POSTGRES_ROOT="/tmp/postgres"
       if [ ! -d "${POSTGRES_ROOT}" ]; then # PostgreSQL hasn't been set up
-        mkdir -p "${POSTGRES_ROOT}"
+        run_as postgres mkdir -p "${POSTGRES_ROOT}"
 
         export PGDATA="${POSTGRES_ROOT}/data"
         export PGLOGS="/tmp/log/postgres"
