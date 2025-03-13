641128f33611118ee61bfbe42629824c5202d226
aram price <aram.price@broadcom.com>
2025-03-13 16:32:27 -0700

    Director spec directories reflect code dirs
    Signed-off-by: Brian Upton <brian.upton@broadcom.com>

--- src/bosh-director/spec/unit/yml_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'YAML parser' do
-  it 'does not use Psych' do
-    code_path = File.expand_path('../..', __dir__)
-    expect(`cd #{code_path}; git grep Psych| grep -v 'spec/unit/yml.spec'`).to eq('')
-  end
-end
