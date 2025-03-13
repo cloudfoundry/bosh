96372bfd161051ba03d8bbe7db98d7485a942783
Dennis Ahaus <dennis.ahaus@sap.com>
2025-03-13 08:18:17 +0100

    Fix review comments

--- src/bosh-director/spec/unit/clouds/external_cpi_response_wrapper_spec.rb
+++ src/bosh-director/spec/unit/clouds/external_cpi_response_wrapper_spec.rb
@@ -138,7 +138,6 @@ describe Bosh::Clouds::ExternalCpiResponseWrapper do
         elsif method == :create_disk
           expected_arguments[1] = { 'type' => '<redacted>' }
         elsif method == :create_stemcell && cpi_api_version >= 3
-          # expected_arguments[2] = { "tags" => {"any":"value"} }
         end
 
         expected_stdin = %({"method":"#{method}","arguments":#{arguments.to_json},"context":#{context.to_json},"api_version":#{cpi_api_version}})
