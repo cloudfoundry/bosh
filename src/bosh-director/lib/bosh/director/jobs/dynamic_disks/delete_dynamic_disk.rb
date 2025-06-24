af754e972e10a8d4765369a84cc17424b2aae562
Maria Shaldybin <maria.shaldybin@broadcom.com>
2025-06-24 22:02:31 +0000

    Add dynamic disks delete request handler
    * Handle NATS API request to delete dynamic disk
    * Send CPI request to delete disk
    * Remove disk from dynamic_disks table
    
    Signed-off-by: Nishad Mathur <nishad.mathur@broadcom.com>
    Co-authored-by: Nishad Mathur <nishad.mathur@broadcom.com>

--- src/bosh-director/lib/bosh/director/jobs/dynamic_disks/delete_dynamic_disk.rb
+++ src/bosh-director/lib/bosh/director/jobs/dynamic_disks/delete_dynamic_disk.rb
@@ -1,73 +1,39 @@
 module Bosh::Director
   module Jobs::DynamicDisks
     class DeleteDynamicDisk < Jobs::BaseJob
+      include Jobs::Helpers::DynamicDiskHelpers
+
       @queue = :normal
 
       def self.job_type
-        :provide_dynamic_disk
+        :delet_dynamic_disk
       end
 
-      def initialize(nats_rpc, reply, payload)
+      def initialize(reply, disk_name)
         super()
-        @nats_rpc = nats_rpc
         @reply = reply
-        @payload = payload
+        @disk_name = disk_name
       end
 
       def perform
-      #   validate_message(@payload)
-
-      #   cloud_properties = find_disk_cloud_properties(@payload['disk_pool_name'])
-
-      #   cloud = Bosh::Director::CloudFactory.create.get(nil)
-      #   unless cloud.has_disk(@payload['disk_name'])
-      #     # TODO raise or exit early
-      #     raise "TODO"
-      #   end
-
-      #   # TODO what to do when a disk is still attached? Maybe add a force param?
-      #   # TODO Map name => cid
-      #   cloud.delete_disk(disk_name)
-
-      #   response = {
-      #     'error' => nil,
-      #   }
-      #   @nats_rpc.send_message(@reply, response)
-
-      #   "deleted disk '#{disk_name}' in deployment '#{@payload['deployment']}'"
-      # rescue => e
-      #   @nats_rpc.send_message(@reply, { 'error' => e.message })
-      #   raise e
-      end
-
-      private
-      def nats_client
-        Config.nats_rpc
-      end
-
-      def find_disk_cloud_properties(disk_pool_name)
-        configs = Models::Config.latest_set('cloud')
-        raise 'No cloud configs provided' if configs.empty?
-
-        consolidated_configs = Bosh::Director::CloudConfig::CloudConfigsConsolidator.new(configs)
-        cloud_config_disk_type = DeploymentPlan::CloudManifestParser.new(logger).parse(consolidated_configs.raw_manifest).disk_type(disk_pool_name)
-        raise "Could not find disk pool by name `#{disk_pool_name}`" if cloud_config_disk_type.nil?
-
-        cloud_config_disk_type.cloud_properties
-      end
-
-      def validate_message(payload)
-        if payload['deployment'].nil? || payload['deployment'].empty?
-          raise 'Invalid request: `deployment` must be provided'
-        elsif payload['disk_name'].nil? || payload['disk_name'].empty?
-          raise 'Invalid request: `disk_name` must be provided'
-        elsif payload['disk_size'].nil? || payload['disk_size'] == 0
-          raise 'Invalid request: `disk_size` must be provided'
-        elsif payload['disk_pool_name'].nil? || payload['disk_pool_name'].empty?
-          raise 'Invalid request: `disk_pool_name` must be provided'
-        elsif @reply.nil? || @reply.empty?
-          raise 'Invalid request: `disk_pool_name` must be provided'
+        disk_model = Models::DynamicDisk.find(name: @disk_name)
+        if disk_model != nil
+          cloud = Bosh::Director::CloudFactory.create.get(disk_model.cpi)
+          if cloud.has_disk(disk_model.disk_cid)
+            cloud.delete_disk(disk_model.disk_cid)
+          end
+          Models::DynamicDisk.where(id: disk_model.id).delete
         end
+
+        response = {
+          'error' => nil,
+        }
+        nats_rpc.send_message(@reply, response)
+
+        "deleted disk '#{@disk_name}'"
+      rescue => e
+        nats_rpc.send_message(@reply, { 'error' => e.message })
+        raise e
       end
     end
   end
