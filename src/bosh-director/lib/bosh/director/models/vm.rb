d350ba7ec6ffa8b4d09fdbee73c183a353c059cd
Felix Moehler <felix.moehler@sap.com>
2025-07-30 13:55:56 +0200

    refactor to_cidr_s pt2 and use constants

--- src/bosh-director/lib/bosh/director/models/vm.rb
+++ src/bosh-director/lib/bosh/director/models/vm.rb
@@ -20,7 +20,7 @@ module Bosh::Director::Models
 
     def ips
       ips_cidr.map do | cidr_ip |
-       if ( cidr_ip.include?(':') && cidr_ip.include?('/128') ) || ( cidr_ip.include?('.')  && cidr_ip.include?('/32') )
+       if ( cidr_ip.include?(':') && cidr_ip.include?("/#{Bosh::Director::DeploymentPlan::Network::IPV6_DEFAULT_PREFIX_SIZE}") ) || ( cidr_ip.include?('.')  && cidr_ip.include?("/#{Bosh::Director::DeploymentPlan::Network::IPV4_DEFAULT_PREFIX_SIZE}") )
         cidr_ip.split('/')[0]
        else
         cidr_ip
@@ -42,9 +42,9 @@ module Bosh::Director::Models
       network_spec.map do |_, network|
         prefix = network['prefix'].to_s
         if network['ip'].include?(':') && prefix.empty?
-          prefix = '128'
+          prefix = Bosh::Director::DeploymentPlan::Network::IPV6_DEFAULT_PREFIX_SIZE
         elsif network['ip'].include?('.') && prefix.empty?
-          prefix = '32'
+          prefix = Bosh::Director::DeploymentPlan::Network::IPV4_DEFAULT_PREFIX_SIZE
         end
         "#{network['ip']}/#{prefix}"
       end
