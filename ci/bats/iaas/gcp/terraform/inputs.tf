7502e9a1e3b81e34e4daf1511bca63aceae08ae2
aram price <aram.price@broadcom.com>
2025-10-20 11:24:18 -0700

    CI: switch internal CIDR away from 10.0.0.0
    This is confilicting with something in CI[1] changing to a less common
    prefix in hopes of avoiding conflicts.
    
    [1]
    - https://bosh.ci.cloudfoundry.org/teams/main/pipelines/bosh-director/jobs/upgrade-mysql/builds/195#L68edf430:455:469
    - https://bosh.ci.cloudfoundry.org/teams/main/pipelines/bosh-director/jobs/upgrade-mysql/builds/196#L68edfda8:346:351

--- ci/bats/iaas/gcp/terraform/inputs.tf
+++ ci/bats/iaas/gcp/terraform/inputs.tf
@@ -11,11 +11,11 @@ variable "name" {
 }
 
 variable "internal_cidr" {
-  default = "10.0.0.0/24"
+  default = "10.0.11.0/24"
 }
 
 variable "second_internal_cidr" {
-  default = "10.0.1.0/24"
+  default = "10.0.22.0/24"
 }
 
 variable "zone" {
