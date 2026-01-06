86dd6fe6fd9aec49ea7d6d873b59f3e9d22eceb2
aram price <aram.price@broadcom.com>
2026-01-06 14:25:17 -0800

    CI: preliminary pipeline changes
    - rename some job
    - remove (ubuntu-)jammy references
    - replace jammy with noble
    - replace concours `var` with YAML anchor(s)

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 fly -t "${CONCOURSE_TARGET:-bosh}" set-pipeline -p bosh-director \
-    -c "${REPO_ROOT}/ci/pipeline.yml" \
-    --var=branch_name=main
+    -c "${REPO_ROOT}/ci/pipeline.yml"
