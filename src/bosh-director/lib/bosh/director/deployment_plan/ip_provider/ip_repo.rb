6f689a9184da557dc745157f800c66c60397d15f
Ned Petrov <71276794+neddp@users.noreply.github.com>
2026-02-02 07:54:24 +0200

    Update src/bosh-director/lib/bosh/director/deployment_plan/ip_provider/ip_repo.rb
    Co-authored-by: Ivaylo Ivanov <ivaylogi98@gmail.com>

--- src/bosh-director/lib/bosh/director/deployment_plan/ip_provider/ip_repo.rb
+++ src/bosh-director/lib/bosh/director/deployment_plan/ip_provider/ip_repo.rb
@@ -97,7 +97,8 @@ module Bosh::Director::DeploymentPlan
       end
 
       # Sort by address first, then by prefix (smaller prefix = larger block = earlier)
-      sorted_ips = addresses_we_cant_allocate.sort_by { |ip| [ip.to_i, ip.prefix] }
+      uniq_ips = addresses_we_cant_allocate.uniq
+      sorted_ips = uniq_ips.sort_by { |ip| [ip.to_i, ip.prefix] }
 
       # Remove IPs contained within larger CIDR blocks
       sorted_ips = sorted_ips.reject.with_index do |ip, index|
